ec4039c2503775f08371db50ce1ff4938742b1df
andy <github@anarkystic.com>
2010-08-03 16:02:25 +0200

    moved around the directories a bit

--- run_tests.py
+++ run_tests.py
@@ -1,9 +1,9 @@
-"""
-Shortly, this will run tests. 
-For now, it returns 0.
-"""
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
 
+# TODO(termie): This will have tests soon
+
+import unittest
 import sys
 
 if __name__ == '__main__':
-    sys.exit(0)
+  pass
