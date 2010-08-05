928c3f2016c1ca9e6e5ce946590252649270c512
andy <github@anarkystic.com>
2010-08-05 16:14:02 +0200

    added a bunch of demo classes

---
+++ opengem/parser/shakemap.py
@@ -0,0 +1,14 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+from opengem import producer
+from opengem import test
+
+class ShakemapFile(test.WordProducer):
+    """This is a demo class.
+
+    A real class should choose which kind of parser to use based on the
+    file extension and probably has to do something more interesting than
+    just splitting lines.
+
+    """
+    pass
