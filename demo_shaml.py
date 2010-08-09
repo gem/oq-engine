457657ca803a8afcb06981e46cb3ae56dff7687b
andy <github@anarkystic.com>
2010-08-09 15:54:07 +0200

    added a simple kml output

--- demo_shaml.py
+++ demo_shaml.py
@@ -3,11 +3,14 @@
 import sys
 
 from opengem.parser import shaml
+from opengem.output import kml
 
 if __name__ == '__main__':
     name = 'tests/data/NshmpWusFaults.xml'
     if len(sys.argv) > 1:
         name = sys.argv[1]
     s = shaml.ShamlFile(name)
-    for cell, k in s:
-        print cell, k
+    k = kml.KmlFile('test.kml')
+    for cell, value in s:
+        k.write(cell, value)
+    k.close()
