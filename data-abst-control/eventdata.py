bab08578a9bb00b41be13cdcadb137b936f468ce
Aurea Moemke <aurea.moemke@sed.ethz.ch>
2010-08-12 17:44:01 +0200

    Now it works. SQL Alchemy and Geo Alchemy --OR mapping eq catalog and event data/spatial data to opengemdb

--- data-abst-control/eventdata.py
+++ data-abst-control/eventdata.py
@@ -8,7 +8,7 @@ from datetime import datetime
 class EventData:
     
     def __init__(self):
-        self.eventtimestamp = datetime.now()
+        self.eventtimestamp = None
         self.eventyear = 0
         self.eventmonth = 0
         self.eventday = 0
@@ -26,18 +26,18 @@ class EventData:
         print "Now inside init of EventData!!!"
         
     def parseEventData(self, line):
-        if len(line[4:9]) == 0: #empty string
+        if len(line[4:9].strip()) == 0: #empty string
             self.eventyear=0
         else:
             self.eventyear = int(line[4:9].strip()) #strip removes trailing space on both sides, rstrip on right, lstrip on left
         
-        if len(line[9:11])==0:
+        if len(line[9:11].strip())==0:
             self.eventmonth=1 #empty, thus set to January
             self.eventerrorcode = 1 # no month set
         else:
             self.eventmonth = int(line[9:11].strip())
             
-        if len(line[11:13])==0:
+        if len(line[11:13].strip())==0:
             self.eventday=1 #empty, thus set to 1
             self.eventerrorcode = 2 # no day
         else:
@@ -45,23 +45,23 @@ class EventData:
             if self.eventday==0:
                 self.eventday=1
         
-        if len(line[13:15])==0:
+        if len(line[13:15].strip())==0:
             self.eventhour=0
         else:
             self.eventhour = int(line[13:15].strip())
        
-        if len(line[15:17])==0:
+        if len(line[15:17].strip())==0:
             self.eventmins=0
         else:
             self.eventmins = int(line[15:17].strip())
             
-        if len(line[17:19])==0:
+        if len(line[17:19].strip())==0:
             self.eventsecs=0
         else:
-            print self.eventsecs
+            print "eventsecs", line[17:19].strip()
             self.eventsecs = int(line[17:19].strip())
             
-        if len(line[20:21])==0:
+        if len(line[20:21].strip())==0:
             self.eventnanosecs=0
         else:
             self.eventnanosecs = int(line[20:21].strip())
@@ -78,14 +78,18 @@ class EventData:
         
         self.eventlon = float(line[27:34].strip())
         self.eventlat = float(line[21:27].strip())
-        self.eventdepth= float(line[39,43].strip())
-        self.eventmagnitude= float(line[49,53].strip())
-        self.eventremarks= line[64,74].strip()
+        self.eventdepth= float(line[39:43].strip())
+        self.eventmagnitude= float(line[49:53].strip())
+        self.eventremarks= line[64:74].strip()
         
-        self.eventpoint="POINT("+self.eventlon+" "+self.eventlat+")"
+        self.eventpoint="POINT("+str(self.eventlon)+" "+str(self.eventlat)+")"
         
-        if self.eventyear < -4713:
-            self.eventerrorcode = 3 #out of range
+        if self.eventyear < 0 or self.eventmonth <1 or self.eventmonth > 12 :
+            if self.eventyear <-4713:
+                self.eventerrorcode = 3 #out of range
+            else:
+                self.eventerrorcode = 4 #year is negative
+
         else:
             self.eventtimestamp = datetime(self.eventyear,self.eventmonth,self.eventday,self.eventhour,self.eventmins,self.eventsecs,self.eventnanosecs)
             
--- data-abst-control/parseeqcat.py
+++ data-abst-control/parseeqcat.py
@@ -79,21 +79,6 @@ class EqCatParser:
        
         return eventData
     
-    def parse(self, node):
-        """parse a single XML node
-        
-        A parsed XML document (from minidom.parse) is a tree of nodes
-        of various types.  Each node is represented by an instance of the
-        corresponding Python class (Element for a tag, Text for
-        text data, Document for the top-level document).  The following
-        statement constructs the name of a class method based on the type
-        of node we're parsing ("parse_Element" for an Element node,
-        "parse_Text" for a Text node, etc.) and then calls the method.
-        """
-        parseMethod = getattr(self, "parse_%s" % node.__class__.__name__)
-        parseMethod(node)
-        
-   
 def usage():
     print __doc__
 
@@ -182,7 +167,7 @@ def main(argv):
         eventData = f.loadEventData(line)
         event = Event(ecid=eqcatid,evtimestamp=eventData.eventtimestamp,evlat=eventData.eventlat,evlong=eventData.eventlon,\
                           evmagnitude=eventData.eventmagnitude,evdepth=eventData.eventdepth,evremarks=eventData.eventremarks,\
-                          evpoint=eventData.eventPoint,evpgpoint=WKTSpatialElement(eventData.eventPoint))
+                          evpoint=eventData.eventpoint,evpgpoint=WKTSpatialElement(eventData.eventpoint))
         session.add(event)
         session.commit()
 
