48a610f709f277829892ea0c15a92778cbfad7f0
Fabian Euchner <fabian@sed.ethz.ch>
2010-08-12 17:49:39 +0200

    added unit tests for shaml output parser, added input XML files for tests

--- tests/parser_shaml_output_unittest.py
+++ tests/parser_shaml_output_unittest.py
@@ -3,115 +3,52 @@
 
 import os
 import unittest
-import tempfile
 
-from eventlet import greenpool
-from eventlet import timeout
-
-from opengem import producer
 from opengem import region
-#from opengem import test
+from opengem.parser import shaml_output
 
+FILES_KNOWN_TO_FAIL = ['ShamlOutputFile-fail-missing_required_attribute.xml',
+                       'ShamlOutputFile-fail-attribute_type_mismatch.xml',
+                       'ShamlOutputFile-fail-ambiguous_descriptor.xml',
+                       'ShamlOutputFile-fail-missing_descriptor.xml',
+                       'ShamlOutputFile-fail-IML_type_mismatch.xml',
+                       'ShamlOutputFile-fail-missing_IML.xml',
+                       'ShamlOutputFile-fail-illegal_gml_pos.xml',
+                       'ShamlOutputFile-fail-curve_values_type_mismatch.xml',
+                       'ShamlOutputFile-fail-missing_curve_vs30.xml']
 
-def generate_data(prefix):
-    for i in xrange(1, 110):
-        yield ((i, i), '%s%s' % (prefix, i))
+TEST_FILE = 'ShamlOutputFile-test.xml'
 
+data_dir = os.path.join(os.path.dirname(__file__), 'data')
 
 class ShamlOutputFileTestCase(unittest.TestCase):
-    def setUp(self):
-        self.pool = greenpool.GreenPool()
-        self.files = []
 
-    def tearDown(self):
-        for f in self.files:
+    def test_shamlfiles_known_to_fail(self):
+        for testfile in FILES_KNOWN_TO_FAIL:
+            shaml = shaml_output.ShamlOutputFile(os.path.join(data_dir, 
+                                                              testfile))
+
+            value_error_found = False
             try:
-                os.unlink(f)
-            except Exception:
-                pass
+                # just loop through generator, in order to trigger exception
+                for point, attributes in shaml:
+                    pass
+            except ValueError:
+                # this is the exception we are expecting
+                value_error_found = True
+            except Exception, e:
+                raise RuntimeError, "unexpected exception: %s" % e
 
-    def _make_data_file(self, prefix):
-        fd, path = tempfile.mkstemp(suffix='.test')
-        f = open(path, 'w')
-        for cell, word in generate_data(prefix):
-            f.write('%d %d %s\n' % (cell[0], cell[1], word))
+            self.assertTrue(value_error_found, 
+                "expected ValueError not raised in test file %s" % testfile)
 
-        f.close()
-        self.files.append(path)
-        return path
+    def test_filter_region_constraint(self):
 
-    def test_iterator_interface(self):
-        path = self._make_data_file('test')
-        prod = test.WordProducer(path)
-        for cell, data in prod:
-            self.assertEqual(data, 'test%s' % int(cell[0]))
+        # find site with lon=16.35/lat=48.25
+        region_constraint = region.RegionConstraint.from_simple((16.0, 48.0), (17.0, 49.0))
+        shaml = shaml_output.ShamlOutputFile(os.path.join(data_dir, 
+                                                          TEST_FILE))
 
-    def test_filter(self):
-        constraint = region.RegionConstraint.from_simple((10, 10), (100, 100))
+        for cell, value in shaml.filter(region_constraint):
+            pass
 
-        path = self._make_data_file('test')
-        prod = test.WordProducer(path)
-
-        # TODO(termie): Right now the bound
-        expected = dict(zip(range(11, 100), range(11, 100)))
-
-        for cell, data in prod.filter(constraint):
-            expected.pop(cell[0])
-            self.assertEqual(data, 'test%s' % int(cell[0]))
-        
-        self.assertEqual(len(expected), 0)
-
-    def test_finished(self):
-        path = self._make_data_file('test')
-        prod = test.WordProducer(path)
-        
-        def _wait_for_producer():
-            timer = timeout.Timeout(0.1)
-            prod.finished.wait()
-            timer.cancel()
-            return True
-
-        self.assertRaises(timeout.Timeout, _wait_for_producer)
-
-        for cell, data in prod:
-            self.assertEqual(data, 'test%s' % int(cell[0]))
-        
-        self.assert_(_wait_for_producer())
-        
-    def test_nice_coroutines(self):
-        first_path = self._make_data_file('first')
-        second_path = self._make_data_file('second')
-        
-        first_prod = test.WordProducer(first_path)
-        second_prod = test.WordProducer(second_path)
-        
-        output = []
-
-        def consume_first():
-            for cell, data in first_prod:
-                output.append(data)
-        
-        def consume_second():
-            for cell, data in second_prod:
-                output.append(data)
-        
-        self.pool.spawn(consume_first)
-        self.pool.spawn(consume_second)
-
-        self.pool.waitall()
-        
-        first_count = 0
-        second_count = 1
-
-        for i in xrange(20):
-            x = output[i]
-            if x.startswith('first'):
-                first_count += 1
-            if x.startswith('second'):
-                second_count += 1
-        
-        # In practice these will probably be equal, but in the case of
-        # race conditions or something as long as one doesn't completely
-        # overwhelm the other
-        self.assert_(first_count > 5)
-        self.assert_(second_count > 5)
