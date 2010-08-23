99eb0eb883a7dc0a9fd539c65bd06826a8264685
Roland <roland@siegert-desktop.(none)>
2010-08-23 17:20:11 +0200

    A python program that uses jpype to instantiate java classes and to call its methods

---
+++ bin/cli_jpype.py
@@ -0,0 +1,32 @@
+import os
+import jpype
+
+def startCli() :      
+    # These are the lines to start up the the java <--> python bridge
+    # We add the lib directory to the jar path, it expects there to be a jar
+    # of the OpenSHA + gem build.
+    jarpath = os.path.join(os.path.abspath('..'), 'lib')
+    # print "xxr jarpath = " + jarpath
+    jpype.startJVM(jpype.getDefaultJVMPath(), '-Djava.ext.dirs=%s' % jarpath)
+    #
+    # These lines are the same as the imports in Java
+    CalculatorConfigData = jpype.JClass(
+	    'org.opensha.gem.GEM1.calc.gemCommandLineCalculator.CalculatorConfigData')
+    CommandLineCalculator = jpype.JClass(
+	    'org.opensha.gem.GEM1.calc.gemCommandLineCalculator.CommandLineCalculator')
+    ErfLogicTreeData = jpype.JClass(
+	    'org.opensha.gem.GEM1.calc.gemCommandLineCalculator.ErfLogicTreeData')
+    GmpeLogicTreeData = jpype.JClass(
+	    'org.opensha.gem.GEM1.calc.gemCommandLineCalculator.GmpeLogicTreeData')
+    InputModelData = jpype.JClass(
+	    'org.opensha.gem.GEM1.calc.gemCommandLineCalculator.InputModelData')
+    # We can instantiate python classes as python-normal
+    # calculator = CommandLineCalculator()
+    calcConfigData = CalculatorConfigData()
+    # We can also access members of imported classes and call methods on our
+    # instances of java classes.
+    #calculator.doCalculation()
+                                            
+if __name__ == "__main__" :
+    startCli()	
+
