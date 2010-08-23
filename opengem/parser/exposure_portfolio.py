d59db8fc0083f73206e24f9ff50cf77e6c0d01d1
bwyss <bmwyss@gmail.com>
2010-08-23 16:39:20 +0200

    _:

--- opengem/parser/exposure_portfolio.py
+++ opengem/parser/exposure_portfolio.py
@@ -2,14 +2,95 @@
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 
 from lxml import etree
-import sys
-sys.path.append("/Users/benwyss/Projects/opengem")
+
 from opengem import producer
 from opengem import region
 
 
 # do not use namespace for now
 RISKML_NS=''
+
+class VulnerabilityModel(producer.FileProducer):
+	""" This class parses an ExposurePortfolio XML (part of riskML?) file.
+    The contents of such a file is meant to be used as input for the risk 
+    engine. The class is implemented as a generator. 
+    For each 'AssetInstance' element in the parsed 
+    instance document, it yields a pair of objects, of which the
+    first one is a shapely.geometry object of type Point (representing a
+    geographical site as WGS84 lon/lat), and the second one
+    is a dictionary with exposure-related attribute values for this site.
+    
+    The attribute dictionary looks like
+    {'PortfolioID': 'PAV01',
+     'PortfolioDescription': 'Collection of existing buildings in downtown Pavia',
+     'AssetID': '01',
+     'AssetDescription': 'Moment-resisting non-ductile concrete frame low rise',
+     'AssetValue': 150000,
+     'VulnerabilityFunction': 'RC/DMRF-D/LR'
+    }
+
+    Note: at the time of writing this class the author has no access to the
+    XML Schema, so all XML attributes from the example instance documents are
+    assumed to be mandatory
+    """
+	def _parse(self):
+		for event, element in etree.iterparse(self.file,
+                                              events=('start', 'end')):
+			if event == 'start' and element.tag == 'VulnerabilityModel':
+				self._set_portfolio_meta(element)
+			elif event == 'end' and element.tag == 'VulnerabilityFunction':
+				yield (self._to_site_attributes(element))
+
+	def _set_portfolio_meta(self, portfolio_element):
+
+		self._current_portfolio_meta = {}
+
+		for required_attribute in (('Asset', str), 
+                                   ('Loss', str)):
+			attr_value = portfolio_element.get(required_attribute[0])
+			if attr_value is not None:
+				self._current_portfolio_meta[required_attribute[0]] = \
+					required_attribute[1](attr_value)
+			else:
+				error_str = "element ExposurePortfolio: missing required " \
+					"attribute %s" % required_attribute[0]
+				raise ValueError(error_str) 
+
+
+	def _to_site_attributes(self, element):
+
+		site_attributes = {}
+
+        # consider all attributes of AssetInstance element as mandatory
+		for required_attribute in (('ID', str), 
+                                   ('IntensityMeasureType', str),
+                                   ('ProbabilisticDistribution', str)):
+			attr_value = element.get(required_attribute[0])
+			if attr_value is not None:
+				site_attributes[required_attribute[0]] = \
+					required_attribute[1](attr_value)
+			else:
+				error_str = "element AssetInstance: missing required " \
+					"attribute %s" % required_attribute[0]
+				raise ValueError(error_str) 
+
+		try:
+			site_attributes.update(self._current_portfolio_meta)
+		except Exception:
+			error_str = "root element (VulnerabilityModel) is missing"
+			raise ValueError(error_str)
+
+		return site_attributes
+
+	def filter(self, region_constraint, attribute_constraint=None):
+		for next in iter(self):
+			if (attribute_constraint is not None and \
+					region_constraint.match(next[0]) and \
+					attribute_constraint.match(next[1])) or \
+				(attribute_constraint is None and \
+					region_constraint.match(next[0])):
+				yield next
+
 
 class ExposurePortfolioFile(producer.FileProducer):
     """ This class parses an ExposurePortfolio XML (part of riskML?) file.
