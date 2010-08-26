98c627a49dea4e67e42e9d902eb8efaf4b1d77f6
bwyss <bmwyss@gmail.com>
2010-08-26 17:31:40 +0200

    an exempt to expand upon the venerability parser to parse children of venerability function

--- run_tests.py
+++ run_tests.py
@@ -21,12 +21,13 @@ import sys
 
 from tests.computation_unittest import *
 from tests.example_unittest import *
+from tests.flags_unittest import *
 from tests.parser_exposure_portfolio_unittest import *
-from tests.parser_vulnerability_model_unittest import *
 from tests.parser_shaml_output_unittest import *
+from tests.parser_vulnerability_model_unittest import *
 from tests.producer_unittest import *
 from tests.region_unittest import *
-from tests.flags_unittest import *
+
 
 if __name__ == '__main__':
   unittest.main()
