7ba4b215991e0081cb4f47b8e90a21731f5956a3
bwyss <bmwyss@gmail.com>
2010-09-06 10:44:29 +0200

    mid-step commit
    Former-commit-id: 8e135692172cb92cc4fc98953d6f93a306cb8949

--- opengem/risk/loss.py
+++ opengem/risk/loss.py
@@ -1,11 +1,24 @@
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
+"""Deterministic Risk Computations based on Hazard, Exposure and Vulnerability
+
+Expects to receive:
+    loss-ratio curves that are store in XML
+    grid of columns and rows from region of coordinates
+
+It can receive region, grid, and site from shapes.py
+
+Expects to compute:
+    compute the loss ratio at each site that corresponds to a certain 
+    probability of exceedance.
+
 """
-This module will read the loss ratio curves currently stored in the xml 
-format, and compute the loss/loss ratio at each site that corresponds to a 
-certain probability of exceedance
-"""
+import sys
+sys.path.append("/Users/benwyss/Projects/opengem")
 
 from opengem import computation
+from opengem.parser import shaml_output
+
+
 
 class LossComputation(computation.Computation):
     """Example Loss Computation
