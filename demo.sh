55eeb87be28e31389e68fe7819262cd4078af522
Joshua McKenty <jmckenty@gmail.com>
2010-09-07 11:42:28 +0200

    Updating demo to run against vulnerability file that matches the exposure portfolio we're using.

--- demo.sh
+++ demo.sh
@@ -2,13 +2,13 @@
 
 DATA_DIR=./tests/data
 
-echo "Generating demo data files..."
-python tests/generate_demo_files.py
+# echo "Generating demo data files..."
+# python tests/generate_demo_files.py
 
 echo "Running demo..."
 python bin/gem-risk-deterministic \
   --exposure $DATA_DIR/FakeExposurePortfolio.xml \
-  --vulnerability $DATA_DIR/VulnerabilityModelFile-test.xml \
+  --vulnerability $DATA_DIR/VulnerabilityModel-Buildings.xml \
   --hazard_curves $DATA_DIR/ShamlOutputFile-PASS.xml \
   --region $DATA_DIR/rect.region $@
  
