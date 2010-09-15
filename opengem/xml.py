1d34b1cf55d70275187305dd088de5779887a9fc
Joshua McKenty <jmckenty@gmail.com>
2010-09-15 21:41:14 +0200

    More pylint cleanup
    Former-commit-id: b8ef9bb872f377b75aeca95a13e33ecec71feec2

--- opengem/xml.py
+++ opengem/xml.py
@@ -1,3 +1,5 @@
+"""Constants and helper functions for XML processing,
+including namespaces, and namespace maps."""
 
-SHAML_NS='http://opengem.org/xmlns/shaml/0.1'
-GML_NS='http://www.opengis.net/gml'
+SHAML_NS = 'http://opengem.org/xmlns/shaml/0.1'
+GML_NS = 'http://www.opengis.net/gml'
