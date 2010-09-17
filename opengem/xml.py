aaba0a8a587586295fef7d045fb340fe1a4cb6a0
Andrea Cerisara <andrea.cerisara@globalquakemodel.org>
2010-09-17 16:21:08 +0200

    Fixed some stuff following Josh' comments
    Former-commit-id: fbd2a7eadca6dd2836c80bc5b4307d0f927108ae

--- opengem/xml.py
+++ opengem/xml.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+"""
+XML namespace definitions and other constants
+related to xml stuff.
 
-SHAML_NS='http://opengem.org/xmlns/shaml/0.1'
-GML_NS='http://www.opengis.net/gml'
+"""
+
+SHAML_NS = 'http://opengem.org/xmlns/shaml/0.1'
+GML_NS = 'http://www.opengis.net/gml'
+GML = "{%s}" % GML_NS
+SHAML = "{%s}" % SHAML_NS
+NSMAP = {"shaml" : SHAML_NS, "gml" : GML_NS}
