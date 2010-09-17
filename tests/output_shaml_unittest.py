c02ba54066703f7b546f17718e41190d3f526e9d
Andrea Cerisara <andrea.cerisara@globalquakemodel.org>
2010-09-17 12:20:13 +0200

    First sketch of the shaml serialization

--- tests/output_shaml_unittest.py
+++ tests/output_shaml_unittest.py
@@ -5,11 +5,19 @@ import os
 import unittest
 
 from opengem import test
+from opengem import shapes
 from opengem.output import shaml
+from opengem.parser import shaml_output
 
 TEST_FILE = "shaml_test_result.xml"
 
+XML_METADATA = "<?xml version='1.0' encoding='UTF-8'?>"
+EMPTY_RESULT = '<shaml:HazardResultList xmlns:shaml="http://opengem.org/xmlns/shaml/0.1" xmlns:gml="http://www.opengis.net/gml"/>'
+
 class OutputShamlTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.writer = shaml.ShamlWriter(os.path.join(test.DATA_DIR, TEST_FILE))
 
     def tearDown(self):
         try:
@@ -23,33 +31,54 @@ class OutputShamlTestCase(unittest.TestCase):
         self.assertFalse("#test>" in "<a><test></a>")
     
     def test_writes_the_file_when_closed(self):
-        writer = shaml.ShamlWriter(os.path.join(test.DATA_DIR, TEST_FILE))
-        writer.close()
+        self.writer.close()
     
         # with no written file we would have an IOError
         result = open(os.path.join(test.DATA_DIR, TEST_FILE))
         result.close()
 
     def test_writes_the_xml_metadata(self):
-        writer = shaml.ShamlWriter(os.path.join(test.DATA_DIR, TEST_FILE))
-        writer.close()
-        
-        self.assertTrue(shaml.XML_METADATA in self._result_as_string())
+        self.writer.close()
+        self.assertTrue(XML_METADATA in self._result_as_string())
 
     def test_writes_an_empty_list_with_no_output(self):
-        writer = shaml.ShamlWriter(os.path.join(test.DATA_DIR, TEST_FILE))
-        writer.close()
+        self.writer.close()
+        self.assertTrue(EMPTY_RESULT in self._result_as_string())
 
-        self.assertTrue(shaml.HEADER in self._result_as_string())
-        self.assertTrue(shaml.FOOTER in self._result_as_string())
+    def test_writes_a_single_result_in_a_single_model(self):
+        point = shapes.Site(16.35, 48.25)
+        values = {"IMT": "MMI",
+                "IDmodel": "MMI_3_1",
+                "timeSpanDuration": 50.0,
+                "endBranchLabel": "3_1",
+                "IML": [10.0, 20.0, 30.0],
+                "maxProb": 0.9,
+                "minProb": 0.1,
+                "Values": [0.005, 0.007, 0.009],
+                "vs30": 760.0}
 
-    def test_writes_a_single_result(self):
-        pass
+        self.writer.write(point, values)
+        self.writer.close()
+        
+        # reading
+        constraint = shapes.RegionConstraint.from_simple(
+                (16.0, 49.0), (17.0, 48.0))
+        
+        reader = shaml_output.ShamlOutputFile(
+                os.path.join(test.DATA_DIR, TEST_FILE))
+
+        counter = None
+
+        # checking againts readed values
+        for counter, (shaml_point, shaml_values) in enumerate(reader.filter(constraint)):
+            counter += 1
+            
+            self.assertEqual(point, shaml_point)
+            self.assertEqual(values, shaml_values)
+
+        self.assertEqual(1, counter, "one element should be readed!")
 
     def test_writes_multiple_results(self):
-        pass
-
-    def test_end_to_end_reading_and_writing(self):
         pass
 
     def _result_as_string(self):
