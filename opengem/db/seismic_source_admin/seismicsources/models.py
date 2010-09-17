cfd2ae8eaaaf811b4d332668cd9b9fea9a8eacd7
Aurea Moemke <aurea.moemke@sed.ethz.ch>
2010-09-17 12:54:24 +0200

    Revisions to Seismic Source Admin

--- opengem/db/seismic_source_admin/seismicsources/models.py
+++ opengem/db/seismic_source_admin/seismicsources/models.py
@@ -20,23 +20,23 @@ class SourceGeometryCatalog(models.Model):
         ( 4, u'ESRI Shapefile'),
         ( 5, u'Others'),
     )
-    #Regular django fields to correspond to attributes in srcgeomcat shapefile
     scid = models.AutoField("Source Geometry Catalog Id", primary_key=True)
     scprivatetag = models.BooleanField("Private to Opengem", default=True)
     scshortname = models.CharField(max_length=20)
     scname = models.CharField(max_length=50)
     scdesc = models.CharField(max_length=100,blank = True)
-    sctypecode = models.CharField("Seismic Source Catalog Types", max_length=5, 
-                      choices=SOURCEGEOMCATTYPE_CHOICES, default='I')
+    sctypecode = models.CharField("Seismic Source Catalog Types", 
+              max_length=5, choices=SOURCEGEOMCATTYPE_CHOICES, default='I')
     scareapolygon = models.CharField("Polygon WKT", max_length=5120)
-    scareamultipolygon = models.CharField("Multipolygon WKT", max_length=5120)
+    scareamultipolygon = models.CharField("Multipolygon WKT", 
+              max_length=5120)
     scstartdate = models.DateTimeField("Catalog Start Date", blank=True)
     scenddate = models.DateTimeField("Catalog End Date", blank=True)
-    scsources = models.CharField("Sources of data", max_length=255, blank=True) 
+    scsources = models.CharField("Sources of data", max_length=255, 
+              blank=True) 
     scorigformatid = models.IntegerField("Original Format Type",
-                     choices=ORIGFORMATTYPE_CHOICES,default=u'O')
+              choices=ORIGFORMATTYPE_CHOICES,default=u'O')
     scremarks = models.CharField("Remarks", max_length=255, blank=True)
-    #scadddate = models.DateTimeField("Catalog Add Date", default=datetime.now())
 
     #Geodjango-specific: a geometry field, 
     #and overriding the default manager with a GeoManager instance.
@@ -63,7 +63,7 @@ class SeismicSource(models.Model):
         (2, 'Multilinestring'),
         (3, 'Polygon'),
         (4, 'Multipolygon'),
-   )
+    )
     scid = models.ForeignKey(SourceGeometryCatalog, db_column = 'scid')
     ssid = models.AutoField("Seismic Source Id", primary_key=True)
     sssrctypecode = models.IntegerField("Seismic Source Type Code", 
@@ -87,11 +87,9 @@ class SeismicSource(models.Model):
     sspolygon = models.CharField(max_length = 5120)
     ssmultipolygon = models.CharField(max_length = 5120)
     sspoint = models.CharField(max_length = 255)
-
    
     #Geodjango-specific: several geometry fields-is this possible?
-    #overrideing the default manager with a GeoManager instance
-
+    #overriding the default manager with a GeoManager instance
     sspgmultilinestring = models.MultiLineStringField()
     sspgtopmultilinestring = models.MultiLineStringField()
     sspgbottommultilinestring = models.MultiLineStringField()
--- opengem/db/seismic_source_admin/settings.py
+++ opengem/db/seismic_source_admin/settings.py
@@ -20,11 +20,11 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'postgresql_psycopg2'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'seissrctestdb'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'postgres'             # Not used with sqlite3.
-DATABASE_PASSWORD = '20SommerlAUch10'         # Not used with sqlite3.
-DATABASE_HOST = 'localhost'             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = '5432'             # Set to empty string for default. Not used with sqlite3.
+DATABASE_NAME = 'seismicsourcedb'             # Or path to database file if using sqlite3.
+DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 # Not used at this point but you'll need it here if you 
 # want to enable a google maps baselayer within your
