9d36c513e7d216e4aef07b230ff5743376b1e203
Andrea Cerisara <andrea.cerisara@globalquakemodel.org>
2010-09-20 13:49:14 +0200

    Added documentation
    Former-commit-id: 3436839a2fb8139e6bcc7cd2d07237f3e436f713

--- opengem/writer.py
+++ opengem/writer.py
@@ -3,25 +3,42 @@
 from eventlet import event
 from eventlet import tpool
 
+# TODO Does still make sense to use eventlet here?
 class FileWriter(object):
+    """Base class for objects that are capable of writing results."""
+    
     def __init__(self, path):
         self.finished = event.Event()
         self.path = path
         self._init_file()
 
     def _init_file(self):
+        """Initializes the file."""
+
         # file i/o will tend to block, wrap it in a thread so it will
         # play nice with ohters
         self.file = tpool.Proxy(open(self.path, "w"))
 
-    def write(self, cell, value):
+    def write(self, point, value):
+        """Writes a single value.
+        
+        Subclasses have to implement this method with their own
+        writing logic.
+        
+        """
+
         raise NotImplementedError
 
     def close(self):
+        """Closes the file."""
+        
         self.file.close()
         self.finished.send(True)
 
     def serialize(self, iterable):
+        """Writes all the elements passed and closes the stream."""
+        
         for key, val in iterable.items():
             self.write(key, val)
+
         self.close()
