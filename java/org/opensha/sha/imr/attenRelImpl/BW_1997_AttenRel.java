9ac81e51cfc8c0e5501014255248a69e212d705c
Roland <roland@siegert-desktop.(none)>
2010-09-20 10:39:10 +0200

    the formula of the equation

--- java/org/opensha/sha/imr/attenRelImpl/BW_1997_AttenRel.java
+++ java/org/opensha/sha/imr/attenRelImpl/BW_1997_AttenRel.java
@@ -9,8 +9,23 @@ import org.opensha.sha.imr.ScalarIntensityMeasureRelationshipAPI;
 public class BW_1997_AttenRel extends AttenuationRelationship implements
 ScalarIntensityMeasureRelationshipAPI,
 NamedObjectAPI, ParameterChangeListener {
+	
+	/**
+	 * Name of equation:
+	 * Bakun and Wentworth (1997) [Subduction zones]
+	 * Formula:
+	 * Immi = 3.67 + 1.17 magnitude - 3.19 log(epicentralDistance)
+	 * @param magnitude
+	 * @param epicentralDistance
+	 * @return Immi
+	 */
 	public double getMean(double magnitude, double epicentralDistance) {
 		double result = 0.0;
+		final float offset = 3.67f;
+		final float coefficient1 = 1.17f;
+		final float coefficient2 = 3.19f;
+		// Immi = 3.67 + 1.17 magnitude - 3.19 log(epicentralDistance) 
+		result = offset + coefficient1 * magnitude - coefficient2 * Math.log10(epicentralDistance); 
 		return result;
 	} // getMean()
 
