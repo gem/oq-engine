c6b76a4a29d02c56ca682121a159dee007966c9a
Joshua McKenty <jmckenty@gmail.com>
2010-09-20 12:03:37 +0200

    Adding loss curves to schema

--- opengem/output/risk.py
+++ opengem/output/risk.py
@@ -30,7 +30,7 @@ class LossCurveXMLWriter(RiskXMLWriter):
             point = point.site.point
         if isinstance(point, shapes.Site):
             point = point.point
-        node = lxml.etree.SubElement(self.root_node, SHAML + "cell", nsmap=NSMAP)
+        node = lxml.etree.SubElement(self.parent_node, SHAML + "cell", nsmap=NSMAP)
         node.attrib[SHAML + 'latitude'] = str(point.y)
         node.attrib[SHAML + 'longitude'] = str(point.x)
         RISK_LOG.debug("Writing loss xml, value is %s", value)
@@ -43,10 +43,11 @@ class LossCurveXMLWriter(RiskXMLWriter):
             subnode_pe.text += " %s" % (val)
     
     def write_header(self):
-	"""Write out the file header"""
-        self.root_node = lxml.etree.Element(SHAML + "LossCurveList", nsmap=NSMAP)
+        """Write out the file header"""
+        self.root_node = lxml.etree.Element(SHAML + "RiskResult", nsmap=NSMAP)
+        self.parent_node = lxml.etree.SubElement(self.root_node, SHAML + "LossCurveList", nsmap=NSMAP)
 
     def write_footer(self):
-	"""Write out the file footer"""
+        """Write out the file footer"""
         et = lxml.etree.ElementTree(self.root_node)
         et.write(self.file, pretty_print=True)
