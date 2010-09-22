42cf7342d5c13fb7fb8a27384ef1c6632c791c43
Andrea Cerisara <andrea.cerisara@globalquakemodel.org>
2010-09-22 12:26:05 +0200

    Hazard curve memcached serialization
    Former-commit-id: 20287bcf0b63cc3a7cac526a15acd695b7b8e1cc

--- java/org/gem/engine/hazard/memcached/HazardCurveSerializer.java
+++ java/org/gem/engine/hazard/memcached/HazardCurveSerializer.java
@@ -6,23 +6,44 @@ import java.util.List;
 import org.opensha.commons.data.Site;
 import org.opensha.gem.GEM1.calc.gemOutput.GEMHazardCurveRepository;
 
+/**
+ * A simple JSON hazard curve serializer.
+ * 
+ * @author Andrea Cerisara
+ */
 public class HazardCurveSerializer
 {
 
     private final String key;
     private final Cache cache;
 
+    /**
+     * Main constructor.
+     * 
+     * @param key the key used to store data in cache
+     * @param cache the cache used
+     */
     public HazardCurveSerializer(String key, Cache cache)
     {
         this.key = key;
         this.cache = cache;
     }
 
+    /**
+     * Serializes a single hazard curve.
+     * 
+     * @param hazardCurve the curve to serialize
+     */
     public void serialize(HazardCurveDTO hazardCurve)
     {
         cache.set(key, hazardCurve.toJSON());
     }
 
+    /**
+     * Serializes the list of hazard curves.
+     * 
+     * @param hazardCurves the list of curves to serialize
+     */
     public void serialize(List<HazardCurveDTO> hazardCurves)
     {
         for (HazardCurveDTO hazardCurve : hazardCurves)
@@ -31,6 +52,11 @@ public class HazardCurveSerializer
         }
     }
 
+    /**
+     * Serializes all the curves stored in the repository
+     * 
+     * @param repository the repository used as source
+     */
     public void serialize(GEMHazardCurveRepository repository)
     {
         for (int i = 0; i < repository.getGridNode().size(); i++)
