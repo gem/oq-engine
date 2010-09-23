59cc983328a8e472ffa1dbee3bb481375d9090d3
Joshua McKenty <jmckenty@gmail.com>
2010-09-23 09:33:40 +0200

    First pass at layout of converter functionality. Changed java build to put the opengem-minimal.jar into the lib folder.
    Former-commit-id: a625a347ee0641bda4611cabdeee25e0cb47b690

---
+++ opengem/converter.py
@@ -0,0 +1,38 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+"""
+Converter library, takes things and makes NRML.
+"""
+
+import os
+
+import jpype
+
+from opengem import logs
+from opengem.parser import nshmp
+from opengem.parser import esri
+from opengem.parser import nrml
+
+log = logs.LOG
+
+PARSERS = {
+    'NSHMP' : nshmp,
+    'ESRI' : esri,
+    'NRML' : nrml,
+}
+
+
+
+def convert(input_path, input_module, output_path, output_module):
+    log.info("Starting conversion run...")
+    jarpath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../lib")
+    jpype.startJVM(jpype.getDefaultJVMPath(), "-Djava.ext.dirs=%s" % jarpath)
+    
+    # TODO(JMC): Make this support non-Java input parsers, too
+    java_class = jpype.JClass(input_module.JAVA_CLASS)
+    
+    
+    
+    # All the GEM1 parsers take a bounding box for the ctor
+    (latmin, latmax, lonmin, lonmax) = input_module.BOUNDING_BOX
+    input_parser = java_class(latmin, latmax, lonmin, lonmax)
+    
---
+++ opengem/parser/nshmp.py
@@ -0,0 +1,4 @@
+
+JAVA_CLASS = "org.gem.engine.hazard.models.nshmp.us.NshmpUsData"
+BOUNDING_BOX = (24.6, 50.0, -125.0, -65.5)
+# BOUNDING_BOX = (35.0, 35.0, -120, -120.0)
