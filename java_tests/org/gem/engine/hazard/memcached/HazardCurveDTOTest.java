187597cdfbbdc3f386ca76d6e54f58a658a9f8bc
Andrea Cerisara <andrea.cerisara@globalquakemodel.org>
2010-09-24 13:24:47 +0200

    Hazard curves serialization

---
+++ java_tests/org/gem/engine/hazard/memcached/HazardCurveDTOTest.java
@@ -0,0 +1,20 @@
+package org.gem.engine.hazard.memcached;
+
+import org.junit.Test;
+
+public class HazardCurveDTOTest
+{
+
+    @Test
+    public void serializesTheLowestProbOfExc()
+    {
+        
+    }
+    
+    @Test
+    public void serializesTheGreatestProbOfExc()
+    {
+        
+    }
+    
+}
