43e0e1d3a09f199a6183e63622a16fa31097224f
Aurea Moemke <aurea.moemke@sed.ethz.ch>
2010-09-24 10:44:27 +0200

    removed some copyrights

--- java/org/opensha/sha/imr/attenRelImpl/AW_2010_AttenRel.java
+++ java/org/opensha/sha/imr/attenRelImpl/AW_2010_AttenRel.java
@@ -1,22 +1,3 @@
-/*******************************************************************************
- * Copyright 2009 OpenSHA.org in partnership with
- * the Southern California Earthquake Center (SCEC, http://www.scec.org)
- * at the University of Southern California and the UnitedStates Geological
- * Survey (USGS; http://www.usgs.gov)
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ******************************************************************************/
-
 package org.opensha.sha.imr.attenRelImpl;
 
 import java.net.MalformedURLException;
@@ -71,7 +52,7 @@ import org.opensha.sha.imr.param.PropagationEffectParams.DistanceRupParameter;
  * sheet
  * </p>
  * 
- * @authors		Aurea Moemke, Damiano Monelli
+ * @authors	Aurea Moemke, Damiano Monelli
  * @created 	22 September 2010, Additional AW_2010 implementation
  * @version 	1.0
  */
