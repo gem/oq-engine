5adb7c45340046e8518901a58fb51ffcc0956f99
Andrea Cerisara <andrea.cerisara@globalquakemodel.org>
2010-09-27 11:57:05 +0200

    Added doc
    Former-commit-id: af1cbe6831bf7350bb9c0c17e3f0c048dd7b12fe

--- opengem/memcached.py
+++ opengem/memcached.py
@@ -11,11 +11,20 @@ import shapes
 
 
 class Reader(object):
+    """Read objects from memcached and translate them in
+    our object model.
+    
+    """
     
     def __init__(self, client):
         self.client = client
     
     def as_curve(self, name):
+        """Read serialized versions of hazard curves
+        and produce shapes.FastCurve objects.
+        
+        TODO (ac): How should we handle other metadata?
+        """
         value = self.client.get(name)
         
         if not value:
