3e2128b8ac3903249db13f3310546e1c8cd59c66
Joshua McKenty <jmckenty@gmail.com>
2010-09-27 15:45:57 +0200

    Making heap size really big.
    Former-commit-id: 9774c089903c45525609322d280776f6f33ce383

--- opengem/converter.py
+++ opengem/converter.py
@@ -22,7 +22,7 @@ def convert(input_path, input_module, output_path, output_module):
     log.info("Starting conversion run...")
     jarpath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../lib")
     log.debug("Jarpath is %s", jarpath)
-    jpype.startJVM(jpype.getDefaultJVMPath(), "-Djava.ext.dirs=%s" % jarpath)
+    jpype.startJVM(jpype.getDefaultJVMPath(), " -Xms2048m -Xmx2048m ", "-Djava.ext.dirs=%s" % jarpath)
     input_module.init_paths(input_path, jpype)
     
     # All the GEM1 parsers take a bounding box for the ctor
