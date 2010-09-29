2ae288d7a12211875ca7c0419ea3ce9a7a0458be
Roland <roland@siegert-desktop.(none)>
2010-09-29 14:45:05 +0200

    state of the work

--- java/org/opensha/sha/imr/attenRelImpl/BW_1997_AttenRel.java
+++ java/org/opensha/sha/imr/attenRelImpl/BW_1997_AttenRel.java
@@ -1,21 +1,29 @@
 package org.opensha.sha.imr.attenRelImpl;
 
 import org.opensha.commons.data.NamedObjectAPI;
+import org.opensha.commons.param.DoubleConstraint;
 import org.opensha.commons.param.event.ParameterChangeEvent;
 import org.opensha.commons.param.event.ParameterChangeListener;
+import org.opensha.commons.param.event.ParameterChangeWarningListener;
 import org.opensha.sha.imr.AttenuationRelationship;
 import org.opensha.sha.imr.ScalarIntensityMeasureRelationshipAPI;
+import org.opensha.sha.imr.param.EqkRuptureParams.MagParam;
 import org.opensha.sha.imr.param.PropagationEffectParams.DistanceEpicentralParameter;
+import org.opensha.sha.imr.param.PropagationEffectParams.DistanceJBParameter;
 
 public class BW_1997_AttenRel extends AttenuationRelationship implements
 ScalarIntensityMeasureRelationshipAPI,
 NamedObjectAPI, ParameterChangeListener {
-	
+	private static final Double DISTANCE_EPI_WARN_MIN = 0.0;
+	private static final Double DISTANCE_EPI_WARN_MAX = 500.0;
+	private static final Double MAG_WARN_MIN = 4.4;
+	private static final Double MAG_WARN_MAX = 6.9;
+	// UI listener that is only introduced for consistency
+	private ParameterChangeWarningListener warningListener = null;
 	// Info by Damiano:
 	// In case of this equation the standard deviation is not provided 
 	// and therefore the method getStandardDeviation() should return 0
 	private final double standardDeviation = 0.0d;
-	private DistanceEpicentralParameter distanceEpicentralParameter = new DistanceEpicentralParameter();
 	
 	/**
 	 * Name of equation:
@@ -46,26 +54,36 @@ NamedObjectAPI, ParameterChangeListener {
 
 	@Override
 	protected void initEqkRuptureParams() {
-		// TODO:
-		// Implement this method in an Adapter class.
+		magParam = new MagParam(MAG_WARN_MIN, MAG_WARN_MAX);
+        eqkRuptureParams.clear();
+        eqkRuptureParams.addParameter(magParam);
 	}
 
 	@Override
 	protected void initPropagationEffectParams() {
-		// TODO:
-		// Implement this method in an Adapter class.
-	}
+		distanceEpicentralParameter = new DistanceEpicentralParameter();
+		distanceEpicentralParameter = new DistanceEpicentralParameter(0.0);
+	    distanceEpicentralParameter.addParameterChangeWarningListener(warningListener);
+	    DoubleConstraint warn = new DoubleConstraint(DISTANCE_EPI_WARN_MIN,
+	                                                 DISTANCE_EPI_WARN_MAX);
+	    warn.setNonEditable();
+	    distanceEpicentralParameter.setWarningConstraint(warn);
+	    distanceEpicentralParameter.setNonEditable();
+	    propagationEffectParams.addParameter(distanceEpicentralParameter);
+	 } // initPropagationEffectParams()
 
 	@Override
 	protected void initSiteParams() {
-		// TODO:
-		// Implement this method in an Adapter class.
+		siteParams.clear();
 	}
 
 	@Override
 	protected void initSupportedIntensityMeasureParams() {
-		// TODO:
-		// Implement this method in an Adapter class.
+		mmiParam = new MMI_Param();
+        mmiParam.setNonEditable();
+        mmiParam.addParameterChangeWarningListener(warningListener);
+        supportedIMParams.clear();
+        supportedIMParams.addParameter(mmiParam);
 	}
 
 	@Override
