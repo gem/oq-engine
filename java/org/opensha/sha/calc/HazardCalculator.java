ffb033c4fbeda831dd4afe2c9ecfac80399585d4
Damiano Monelli <monelli.damiano@gmail.com>
2010-10-15 13:51:02 +0200

    single thread hazard curves calculator

---
+++ java/org/opensha/sha/calc/HazardCalculator.java
@@ -0,0 +1,93 @@
+package org.opensha.sha.calc;
+
+import java.rmi.RemoteException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.opensha.commons.data.Site;
+import org.opensha.commons.data.function.ArbitrarilyDiscretizedFunc;
+import org.opensha.commons.data.function.DiscretizedFuncAPI;
+import org.opensha.gem.GEM1.scratch.HazardCurveCalculator;
+import org.opensha.sha.earthquake.EqkRupForecastAPI;
+import org.opensha.sha.imr.ScalarIntensityMeasureRelationshipAPI;
+import org.opensha.sha.util.TectonicRegionType;
+
+/**
+ * This class provides methods for hazard calculations.
+ * 
+ * @author damianomonelli
+ * 
+ */
+
+public class HazardCalculator {
+
+    /**
+     * Calculate hazard curves for a set of sites from an earthquake rupture
+     * forecast using the classical PSHA approach
+     * 
+     * @param siteList
+     *            : list of sites ({@link Site}) where to compute hazard curves
+     * @param erf
+     *            : earthquake rupture forecast {@link EqkRupForecastAPI}
+     * @param gmpeMap
+     *            : map associating tectonic region types (
+     *            {@link TectonicRegionType}) with attenuation relationships (
+     *            {@link ScalarIntensityMeasureRelationshipAPI})
+     * @param imlVals
+     *            : intensity measure levels (double[]) for which calculating
+     *            probabilities of exceedence
+     * @param integrationDistance
+     *            : maximum distance used for integration
+     * @return
+     */
+    public static
+            Map<Site, DiscretizedFuncAPI>
+            getHazardCurves(
+                    List<Site> siteList,
+                    EqkRupForecastAPI erf,
+                    Map<TectonicRegionType, ScalarIntensityMeasureRelationshipAPI> gmpeMap,
+                    double[] imlVals, double integrationDistance) {
+        validateInput(siteList, gmpeMap, imlVals);
+        Map<Site, DiscretizedFuncAPI> results =
+                new HashMap<Site, DiscretizedFuncAPI>();
+        DiscretizedFuncAPI hazardCurve = new ArbitrarilyDiscretizedFunc();
+        for (double val : imlVals)
+            hazardCurve.set(val, 1.0);
+        HazardCurveCalculator curveCalculator = null;
+        try {
+            curveCalculator = new HazardCurveCalculator();
+            curveCalculator.setMaxSourceDistance(integrationDistance);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        for (Site site : siteList) {
+            try {
+                curveCalculator.getHazardCurve(hazardCurve, site, gmpeMap, erf);
+                results.put(site, hazardCurve);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+        return results;
+    }
+
+    private static
+            void
+            validateInput(
+                    List<Site> siteList,
+                    Map<TectonicRegionType, ScalarIntensityMeasureRelationshipAPI> gmpeMap,
+                    double[] imlVals) {
+        if (siteList == null) {
+            throw new IllegalArgumentException("List of sites cannot be null");
+        }
+        if (siteList.isEmpty()) {
+            throw new IllegalArgumentException(
+                    "List of sites must contain at least one site");
+        }
+        if (imlVals == null) {
+            throw new IllegalArgumentException(
+                    "Array of intensity measure levels cannot be null");
+        }
+    }
+}
