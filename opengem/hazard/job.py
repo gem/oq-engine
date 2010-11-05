4d2c24ac9672f1e04134a6ee570ccc0e022313ee
Christopher MacGown <ignoti+github@gmail.com>
2010-11-05 15:58:34 +0100

    Mixin magic
    Former-commit-id: 208a19164458afc84228c257ef08316b8f1c6511

--- opengem/hazard/job.py
+++ opengem/hazard/job.py
@@ -1,8 +1,14 @@
 from opengem.job.mixins import Mixin
 
 Mixin.register(HazJobMixin)
+HazJobMixin.register(MonteCarloMixin)
 
-class HazJobMixin:
+
+class HazJobMixin(Mixin):
+    mixins = []
+
+
+class MonteCarloMixin:
     def preload(self, fn):
         def preloader(self, *args, *kwargs):
             return fn(*args, **kwargs)
