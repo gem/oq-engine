d18a75554df8dade49e56ee6bb4a37800ed4c545
Joshua McKenty <jmckenty@gmail.com>
2010-11-05 13:20:37 +0100

    Don't ever start JVM more than once. Try a JPackage load and start in the exception handler. (Maybe patch JPype later?)
    Former-commit-id: 947a9a2f7c65ff28dcddb581661a0ac29e9376e1

--- opengem/java.py
+++ opengem/java.py
@@ -7,16 +7,18 @@ import jpype
 
 from opengem.logs import LOG
 
-JVM = None
 
 
+
+        
 def jvm(max_mem=4000):
-    global JVM
-    if JVM is None:
-        jarpaths = (os.path.join(os.path.abspath(__file__), "../lib"), 
-                    os.path.join(os.path.abspath(__file__), "../dist"))
-        LOG.debug("Jarpath is %s", jarpaths)
+    jarpaths = (os.path.abspath(os.path.join(os.path.dirname(__file__), "../lib")), 
+                os.path.abspath(os.path.join(os.path.dirname(__file__), "../dist")))
+    LOG.debug("Jarpath is %s", jarpaths)
+    try:
+        foo = jpype.JPackage("java.lang.System.out")
+    except Exception, e:
+        print "Default JVM path is %s" % jpype.getDefaultJVMPath()
         jpype.startJVM(jpype.getDefaultJVMPath(), 
-                        "-Djava.ext.dirs=%s" % jarpaths, "-Xmx%sM" % max_mem)
-        JVM = jpype
-    return JVM
+                        "-Djava.ext.dirs=%s:%s" % jarpaths, "-Xmx%sM" % max_mem) # "-Djava.ext.dirs=%s:%s" % jarpaths
+    return jpype
