9eba006b9b135e049edb97e2b97e3f937528d960
Damiano Monelli <monelli.damiano@gmail.com>
2010-11-12 13:51:29 +0100

    able to read gmpe hash map from cache

--- java/org/gem/JsonSerializer.java
+++ java/org/gem/JsonSerializer.java
@@ -2,11 +2,15 @@ package org.gem;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.gem.engine.hazard.memcached.Cache;
 import org.opensha.sha.earthquake.rupForecastImpl.GEM1.SourceData.GEMSourceData;
+import org.opensha.sha.imr.ScalarIntensityMeasureRelationshipAPI;
+import org.opensha.sha.util.TectonicRegionType;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
 public class JsonSerializer {
@@ -32,5 +36,22 @@ public class JsonSerializer {
         Type listType = new TypeToken<ArrayList<GEMSourceData>>() {
         }.getType();
         return new Gson().fromJson((String) cache.get(key), listType);
+    }
+
+    public static
+            HashMap<TectonicRegionType, ScalarIntensityMeasureRelationshipAPI>
+            getGmpeMapFromCache(Cache cache, String key) {
+
+        GsonBuilder gson = new GsonBuilder();
+        gson.registerTypeAdapter(ScalarIntensityMeasureRelationshipAPI.class,
+                new ScalarIntensityMeasureRelationshipApiDeserializer());
+        gson.registerTypeAdapter(TectonicRegionType.class,
+                new TectonicRegionTypeDeserializer());
+
+        Type hashType =
+                new TypeToken<HashMap<TectonicRegionType, ScalarIntensityMeasureRelationshipAPI>>() {
+                }.getType();
+
+        return gson.create().fromJson((String) cache.get(key), hashType);
     }
 }
