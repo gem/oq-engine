ba743d63468d860b6037f99503436d3b1a21acd9
Lars Butler <Lars.Butler@gmail.com>
2010-11-21 01:44:31 +0100

    Squashed commit of the following:
    commit db2370bcd6a83baf93639465abda9035dbb9fe0d
    Author: Lars Butler <Lars.Butler@gmail.com>
    Date:   Sun Nov 21 01:43:39 2010 +0100
    
        Fixed a bug with validate logger creation (loggers are now created with name=job_id)
    
        Added tests for multiple loggers/log files
    
    commit d2c8e6de760a5c61ebfe3a32f3cd831a71fc4cf4
    Merge: e83f9ea c4c63cb
    Author: Lars Butler <Lars.Butler@gmail.com>
    Date:   Sun Nov 21 00:32:09 2010 +0100
    
        Merge branch 'master' into validation-logging
    
    commit e83f9eab19b6b084e6a90645fec0c4a816559d61
    Author: Lars Butler <Lars.Butler@gmail.com>
    Date:   Sat Nov 20 20:24:51 2010 +0100
    
        Added test_validation_logger_write_many_lines test case
    
    commit 7e4e9b33e1f3b65de8f2333fc5063021ddfe6a49
    Author: Lars Butler <Lars.Butler@gmail.com>
    Date:   Sat Nov 20 19:36:07 2010 +0100
    
        Added make_job_logger to logs module
    
        closures and monkey patching FTW
    
        Added LogsTestCase class with simple single log entry test
    
    
    Former-commit-id: 4c6c54f6aff15c28ac174b8658c54c55810ef078

---
+++ tests/logs_unittest.py
@@ -0,0 +1,111 @@
+import os
+import unittest
+
+from opengem import logs
+
+FAKE_JOB_ID = "8675309"
+LOG_FILE_PATH = os.path.join(os.getcwd(), '%s.log' % FAKE_JOB_ID)
+
+
+def _remove_log_file(job_id):
+    """Remove a log file (in the CWD) by job_id."""
+    log_path = '%s.log' % job_id
+    if os.path.exists(log_path):
+        os.system('rm %s' % log_path)
+
+class LogsTestCase(unittest.TestCase):
+    
+    
+    def setUp(self):
+        # clean up the default test log file
+        _remove_log_file(FAKE_JOB_ID)
+
+    def tearDown(self):
+        pass
+
+
+    def test_validation_logger_write_single_line(self):
+        """Test procedure:
+        1) Make up some fake job ID
+        2) Call logs.make_job_logger(job_id) to make a logger obj
+        3) Write one line to the file (file can just be in the CWD) using the 
+        'validate' logger method
+        4) Verify the contents of the log file
+        """
+        logger = logs.make_job_logger(FAKE_JOB_ID)
+        expected_log_text = "This is a test log entry for job_id = %s." % \
+FAKE_JOB_ID
+        # write to the log using a monkey patched validate() logging method
+        logger.validate(expected_log_text)
+       
+        self.assertTrue(os.path.exists(LOG_FILE_PATH)) 
+        # open the log file
+        # read contents and compare
+        log_file = open(LOG_FILE_PATH, 'r')
+        
+        actual_log_text = log_file.readlines()
+        # should only be 1 line of text
+        self.assertEqual(1, len(actual_log_text))
+        self.assertEqual(actual_log_text[0], expected_log_text + '\n')
+
+
+    def test_validation_logger_write_many_lines(self):
+        """Test procedure:
+        Basically the same as the single line logger test, except we'll log 
+        multiple lines of text."""
+        logger = logs.make_job_logger(FAKE_JOB_ID)
+        # generate 100 fake log entries
+        expected_log_text = [ 'Log entry number %d' % \
+x for x in range(100)]
+        
+        # add the example log text to the log file
+        for entry in expected_log_text:
+            logger.validate(entry)
+
+        self.assertTrue(os.path.exists(LOG_FILE_PATH))
+        # open the log file
+        # read the contents and compare
+        log_file = open(LOG_FILE_PATH, 'r')
+
+        actual_log_text = log_file.readlines()
+        # verify the log file contents
+        # append a '\n' to each item of the expected_log_text; this should make
+        # the expected and actual match
+        self.assertEqual([x + '\n' for x in expected_log_text],\
+actual_log_text)
+
+
+    def test_multiple_loggers(self):
+        """Test procedure:
+        1) Create 2 loggers (with different job_ids)
+        2) Log multiple messages at the 'validate' level to each log file
+        3) Verify the contents"""
+
+        job_id_1 = "job314"
+        job_id_2 = "job315"
+        # first thing, remove logs (if existing) from previous test runs
+        _remove_log_file(job_id_1)
+        _remove_log_file(job_id_2)
+
+        logger1 = logs.make_job_logger(job_id_1)
+        logger2 = logs.make_job_logger(job_id_2)
+
+        # alternate loggers to make sure log entries reach the proper file
+        log_1_text = ["Log 1, entry 1", "Log 1, entry 2"]
+        log_2_text = ["Log 2, entry 1", "Log 2, entry 2"]
+
+        logger1.validate(log_1_text[0])
+        logger2.validate(log_2_text[0])
+        logger1.validate(log_1_text[1])
+        logger2.validate(log_2_text[1])
+
+        # read the log files and verify the contents
+        log_file_1 = open('%s.log' % job_id_1, 'r')
+        log_file_2 = open('%s.log' % job_id_2, 'r')
+
+        log_file_1_text = log_file_1.readlines()
+        self.assertEqual([x + '\n' for x in log_1_text], log_file_1_text)
+        log_file_2_text = log_file_2.readlines()
+        self.assertEqual([x + '\n' for x in log_2_text], log_file_2_text)
+
+         
