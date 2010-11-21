412a42078a2fe00c822a9e61c0cb31ec55b71a0d
Joshua McKenty <jmckenty@gmail.com>
2010-11-21 18:56:40 +0100

    Intermediate commit
    Former-commit-id: 9dd3fa994ffd6e19057b7a0c64942736d52e5ae0 [formerly 9dd3fa994ffd6e19057b7a0c64942736d52e5ae0 [formerly 41e83fa0d7066fb59732f63ddf2cac48ac75b99b]]
    Former-commit-id: 17c836deaeb84be4f342407ef6967239590298a8
    Former-commit-id: b7d7b5444e30f4b4d4628133638d5f208d933ec9

--- opengem/risk/job/__init__.py
+++ opengem/risk/job/__init__.py
@@ -4,21 +4,6 @@ Risk Job Mixin decorators """
 from opengem.job.mixins import Mixin
 
 
-def preload(fn):
-    """ Preload decorator """
-    def preloader(self, *args, **kwargs):
-        """A decorator for preload steps that must run on the Jobber"""
-        # Do preload stuff
-
-        self.store_region_constraint()
-        self.store_sites_and_hazard_curve()
-        self.store_exposure_assets()
-        self.store_vulnerability_model()
-
-        return fn(self, *args, **kwargs)
-    return preloader
-
-
 def output(fn):
     """ Decorator for output """
     def output_writer(self, *args, **kwargs):
@@ -28,10 +13,37 @@ def output(fn):
         # TODO(chris): Should we use the returned result?
         if result:
             # pylint: disable-msg=W0212
-            self._write_output_for_block(self.job_id, self.block_id)
+            _write_output_for_block(self.job_id, self.block_id)
         return result
 
     return output_writer
+
+
+def _write_output_for_block(job_id, block_id):
+    """note: this is usable only for one block"""
+    
+    # produce output for one block
+    loss_curves = []
+
+    sites = kvs.get_sites_from_memcache(job_id, block_id)
+
+    for (gridpoint, (site_lon, site_lat)) in sites:
+        key = kvs.generate_product_key(job_id, 
+            risk.LOSS_CURVE_KEY_TOKEN, block_id, gridpoint)
+        loss_curve = kvs.get_value_json_decoded(key)
+        loss_curves.append((shapes.Site(site_lon, site_lat), 
+                            loss_curve))
+
+    LOGGER.debug("serializing loss_curves")
+    output_generator = RiskXMLWriter(settings.LOSS_CURVES_OUTPUT_FILE)
+    output_generator.serialize(loss_curves)
+    
+    #output_generator = output.SimpleOutput()
+    #output_generator.serialize(ratio_results)
+    
+    #output_generator = geotiff.GeoTiffFile(output_file, 
+    #    region_constraint.grid)
+    #output_generator.serialize(losses_one_perc)
 
 
 class RiskJobMixin(Mixin):
--- opengem/risk/tasks.py
+++ opengem/risk/tasks.py
@@ -12,87 +12,21 @@ from celery.decorators import task
 
 from opengem import flags
 from opengem import job
+from opengem.job import mixins
 from opengem import kvs 
 from opengem import risk
 
 from opengem.risk import engines
+from opengem.risk import job as riskjob
 
 FLAGS = flags.FLAGS
 
-DEFAULT_conditional_loss_poe = 0.01
 
 
 @task
 def compute_risk(job_id, block_id, conditional_loss_poe=None, **kwargs):
-    """This task computes risk for a block of sites. It requires to have
-    pre-initialized in memcache:
-     1) list of sites
-     2) hazard curves
-     3) exposure portfolio (=assets)
-     4) vulnerability
+    engine = job.Job.from_kvs(job_id)
+    with mixins.Mixin(engine, riskjob.RiskJobMixin, key="risk") as mixed:
+        mixed.compute_risk(block_id, conditional_loss_poe, **kwargs)
 
-    TODO(fab): make conditional_loss_poe (set of probabilities of exceedance
-    for which the loss computation is done) a list of floats, and read it from
-    the job configuration.
-    """
 
-    logger = compute_risk.get_logger(**kwargs)
-
-    if conditional_loss_poe is None:
-        conditional_loss_poe = DEFAULT_conditional_loss_poe
-
-    # start up memcache client
-    memcache_client = kvs.get_client(binary=False)
-
-    # load risk engine, make it use memcache
-    # uses hazard, exposure from memcache
-    risk_engine = engines.ClassicalPSHABasedLossRatioCalculator(
-            job_id, block_id, memcache_client)
-
-    # loop over sites for this block
-    # assumes that hazard, assets, and output risk grid are the same
-    # (no nearest-neighbour search to find hazard)
-    block = job.Block.from_kvs(block_id)
-    sites_list = block.sites # kvs.get_sites_from_memcache(job_id, block_id)
-
-    logger.debug("sites list for this task w/ job_id %s, block_id %s:\n%s" % (
-        job_id, block_id, sites_list))
-
-    for (gridpoint, site) in sites_list:
-
-        logger.debug("processing gridpoint %s, site %s" % (gridpoint, site))
-        loss_ratio_curve = risk_engine.compute_loss_ratio_curve(gridpoint)
-
-        if loss_ratio_curve is not None:
-
-            # write to memcache: loss_ratio
-            key = kvs.generate_product_key(job_id,
-                risk.LOSS_RATIO_CURVE_KEY_TOKEN, block_id, gridpoint)
-
-            logger.debug("RESULT: loss ratio curve is %s, write to key %s" % (
-                loss_ratio_curve, key))
-            memcache_client.set(key, loss_ratio_curve)
-            
-            # compute loss curve
-            loss_curve = risk_engine.compute_loss_curve(gridpoint, 
-                                                        loss_ratio_curve)
-            key = kvs.generate_product_key(job_id, 
-                risk.LOSS_CURVE_KEY_TOKEN, block_id, gridpoint)
-
-            logger.debug("RESULT: loss curve is %s, write to key %s" % (
-                loss_curve, key))
-            memcache_client.set(key, loss_curve)
-            
-            # compute conditional loss
-            loss_conditional = engines.compute_loss(loss_curve, 
-                                                    conditional_loss_poe)
-            key = kvs.generate_product_key(job_id, 
-                risk.CONDITIONAL_LOSS_KEY_TOKEN, block_id, gridpoint)
-
-            logger.debug("RESULT: conditional loss is %s, write to key %s" % (
-                loss_conditional, key))
-            memcache_client.set(key, loss_conditional)
-
-    # assembling final product needs to be done by jobber, collecting the
-    # results from all tasks
-    return True
