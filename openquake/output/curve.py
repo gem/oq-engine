810e3f3202ddb7e5c2634be69d11e95bd5976ff3
Fabian Euchner <fabian@sed.ethz.ch>
2010-11-26 14:05:37 +0100

    creation of SVG plots for loss and loss ratio curves from NRML
    Former-commit-id: 4d467594ef099516a55434119f3ba2cd316aa8a7 [formerly b07c6f663753153f4a8e2ca71ec099423fed4363]
    Former-commit-id: 287984049cdb7663d787055e5dbe5587815fcf03

--- openquake/output/curve.py
+++ openquake/output/curve.py
@@ -18,9 +18,12 @@ from openquake import shapes
 from openquake import writer
 
 from openquake.parser import hazard as hazard_parser
+from openquake.parser import risk as risk_parser
 
 IMAGE_FORMAT = "SVG"
 COLOR_CODES = ('k', 'r', 'g', 'b')
+
+CURVE_BRANCH_PLACEHOLDER = 'Curve'
 
 
 class CurvePlotter(object):
@@ -28,7 +31,7 @@ class CurvePlotter(object):
     For a specific curve plot (hazard/loss/loss ratio), 
     a class has to be derived from this one."""
 
-    def __init__(self, output_base_path, nrml_input_path):
+    def __init__(self, output_base_path, nrml_input_path, curve_title=None):
 
         # remove trailing .svg extension from output file name (if existing)
         self.output_base_path = output_base_path
@@ -36,6 +39,7 @@ class CurvePlotter(object):
             self.output_base_path = self.output_base_path[0:-4]
 
         self.nrml_input_path = nrml_input_path
+        self.curve_title = curve_title
         self.data = {}
         self.svg_filenames = []
 
@@ -46,11 +50,15 @@ class CurvePlotter(object):
         be implemented by the derived class."""
         pass
 
-    def plot(self):
-        # create a plot for each site
+    def plot(self, autoscale_y=True):
+        """Create a plot for each site in the dataset.
+        The argument autoscale_y auto-scales the ordinate axis (default).
+        If set to false, the displayed ordinate range is 0...1.
+        TODO(fab): let user specify abscissa and ordinate range for plot."""
+
         for site_hash in self.data:
             plot = CurvePlot(self.data[site_hash]['path'])
-            plot.write(self.data[site_hash]['curves'])
+            plot.write(self.data[site_hash]['curves'], autoscale_y)
             plot.close()
 
     def filenames(self):
@@ -63,14 +71,71 @@ class CurvePlotter(object):
             self.output_base_path, site_lon, site_lat)
 
 
+class RiskCurvePlotter(CurvePlotter):
+    """This class plots loss/loss ratio curves as read from an NRML file. For
+    each Site listed in the NRML file, a separate plot is created. A plot
+    contains only one curve, multiple curves per plot are currently not
+    supported by the NRML schema.
+    TODO(fab): In the future, we may want to plot loss/loss ratio curves
+    per asset, multiple assets per site (according to Vitor)."""
+
+    def __init__(self, output_base_path, nrml_input_path, mode='loss', 
+                 curve_title=None):
+        """mode can be 'loss' or 'loss_ratio', 'loss' is the default."""
+        self.mode = mode
+
+        if curve_title is None:
+            if self.mode == 'loss_ratio':
+                curve_title = 'Loss Ratio Curve'
+            else:
+                curve_title = 'Loss Curve'
+
+        super(RiskCurvePlotter, self).__init__(output_base_path, 
+            nrml_input_path, curve_title)
+
+    def _parse_nrml_file(self):
+        """Parse loss/loss ratio curve data from NRML file into a dictionary."""
+
+        nrml_element = risk_parser.NrmlFile(self.nrml_input_path, mode=self.mode)
+
+        # loss/loss ratio curves have a common *ordinate* for all curves
+        # in an NRML file
+        for (nrml_point, nrml_attr) in nrml_element.filter(
+            region_constraint=None):
+
+            site_hash = nrml_point.hash()
+
+            if site_hash not in self.data:
+                self.data[site_hash] = {
+                    # capture SVG filename for each site
+                    'path': self._generate_filename(site_hash),
+                    'curves': {}}
+
+            if CURVE_BRANCH_PLACEHOLDER not in self.data[site_hash]['curves']:
+                self.data[site_hash]['curves'][CURVE_BRANCH_PLACEHOLDER] = {}
+
+            self.data[site_hash]['curves'][CURVE_BRANCH_PLACEHOLDER] = {
+                'abscissa': nrml_attr[nrml_element.abscissa_output_key],
+                'abscissa_property': nrml_element.abscissa_property,
+                'ordinate': nrml_attr[nrml_element.ordinate_output_key],
+                'ordinate_property': nrml_element.ordinate_property,
+                'Site': nrml_point,
+                'curve_title': self.curve_title}
+
+
 class HazardCurvePlotter(CurvePlotter):
     """This class plots hazard curves as read from an NRML file. For
     each Site listed in the NRML file, a separate plot is created. A plot
     can contain multiple hazard curves, one for each logic tree end branch 
     given in the NRML file."""
 
-    def __init__(self, output_base_path, nrml_input_path):
-        super(HazardCurvePlotter, self).__init__(output_base_path, nrml_input_path)
+    def __init__(self, output_base_path, nrml_input_path, curve_title=None):
+
+        if curve_title is None:
+            curve_title = 'Hazard Curves'
+
+        super(HazardCurvePlotter, self).__init__(output_base_path, 
+            nrml_input_path, curve_title)
 
     def _parse_nrml_file(self):
         """Parse hazard curve data from NRML file into a dictionary."""
@@ -102,7 +167,7 @@ class HazardCurvePlotter(CurvePlotter):
                 'ordinate': nrml_attr['Values'],
                 'ordinate_property': 'Probability of Exceedance',
                 'Site': nrml_point,
-                'curve_title': 'Hazard Curves'}
+                'curve_title': self.curve_title}
 
 
 class CurvePlot(writer.FileWriter):
@@ -156,7 +221,7 @@ class CurvePlot(writer.FileWriter):
         pylab.ax = pylab.subplot(111)
         pylab.clf()
 
-    def write(self, data):
+    def write(self, data, autoscale_y=True):
         """The method expects a dictionary that holds the labels for the
         separate curves to be plotted as keys. For each key, the corresponding
         value is a dictionary that holds lists for abscissa and ordinate values,
@@ -171,7 +236,10 @@ class CurvePlot(writer.FileWriter):
                        label=curve)
 
         # set x and y dimension of plot
-        pylab.ylim(self._plotAxes['ymin'], self._plotAxes['ymax'])
+        if autoscale_y is False:
+            pylab.ylim(self._plotAxes['ymin'], self._plotAxes['ymax'])
+        else:
+            ymin, ymax = pylab.ylim()
         xmin, xmax = pylab.xlim()
 
         pylab.xlabel(data[curve]['abscissa_property'], self._plotLabelsFont)
