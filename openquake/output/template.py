f875729ff4ce9eea7d0c00145c4c50a4a440e721
Christopher MacGown <ignoti+github@gmail.com>
2010-12-06 11:37:11 +0100

    Add loss map color scaling.
    Squashed commit of the following:
    
    commit 4f4c6a04d5c72bed7a5f4fd39485a16678829bc9
    Author: Christopher MacGown <ignoti+github@gmail.com>
    Date:   Mon Dec 6 11:10:40 2010 +0100
    
        pylint cleanups
    
    commit c337a2054777c1707dd30a9c0fa17c7e5d9b465e
    Merge: 41aeeb3 b912e20
    Author: Christopher MacGown <ignoti+github@gmail.com>
    Date:   Mon Dec 6 10:59:42 2010 +0100
    
        Merge master post pylint cleanup
    
    commit 41aeeb3b737a7e6790bb3e31b15852d420fb8f4c
    Author: Christopher MacGown <ignoti+github@gmail.com>
    Date:   Tue Nov 30 23:09:02 2010 +0100
    
        Skip job test for now
    
    commit b3ad334325c70299e4cc717a2f45c26585cdfe84
    Author: Christopher MacGown <ignoti+github@gmail.com>
    Date:   Tue Nov 30 23:00:07 2010 +0100
    
        Add exposure so tests pass
    
    commit a5d4cb1c13599c53d2f2075c3baf8a9b3e1a74dd
    Author: Christopher MacGown <ignoti+github@gmail.com>
    Date:   Tue Nov 30 22:54:54 2010 +0100
    
        Add loss map color scaling
    
    
    Former-commit-id: b357b7b76e52badbdb9d75ff27110f8c61f5a425

--- openquake/output/template.py
+++ openquake/output/template.py
@@ -1,6 +1,72 @@
 # -*- coding: utf-8 -*-
 """HTML template to embed geotiffs of GMF maps."""
 
+HTML_TEMPLATE_LOSSRATIO = """<!DOCTYPE html PUBLIC 
+    "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en-US" xml:lang="en-US" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>GMF</title>
+</head>
+<body>
+<center>
+<img width="PLACEHOLDER_WIDTH" height="PLACEHOLDER_HEIGHT" 
+    src="PLACEHOLDER_IMAGE_NAME" type="image/tiff" negative="yes"/>
+<br/>
+<table cellspacing="0" border="1" width="20%">
+<tr>
+<th width="10%">Color</th>
+<th width="10%">Percentage</th>
+</tr>
+<tr>
+<td bgcolor="#000000">&nbsp;</td><td>0-6</td>
+</tr>
+<tr>
+<td bgcolor="#0f0000">&nbsp;</td><td>7-12</td>
+</tr>
+<tr>
+<td bgcolor="#1f0000">&nbsp;</td><td>13-18</td>
+</tr>
+<tr>
+<td bgcolor="#2f0000">&nbsp;</td><td>19-25</td>
+</tr>
+<tr>
+<td bgcolor="#3f0000">&nbsp;</td><td>26-31</td>
+</tr>
+<tr>
+<td bgcolor="#4f0000">&nbsp;</td><td>32-37</td>
+</tr>
+<tr>
+<td bgcolor="#5f0000">&nbsp;</td><td>38-43</td>
+</tr>
+<tr>
+<td bgcolor="#6f0000">&nbsp;</td><td>44-50</td>
+</tr>
+<tr>
+<td bgcolor="#7f0000">&nbsp;</td><td>51-56</td>
+</tr>
+<tr>
+<td bgcolor="#8f0000">&nbsp;</td><td>57-62</td>
+</tr>
+<tr>
+<td bgcolor="#9f0000">&nbsp;</td><td>63-69</td>
+</tr>
+<tr>
+<td bgcolor="#af0000">&nbsp;</td><td>70-75</td>
+</tr>
+<tr>
+<td bgcolor="#bf0000">&nbsp;</td><td>76-81</td>
+</tr>
+<tr>
+<td bgcolor="#cf0000">&nbsp;</td><td>82-87</td>
+</tr>
+<tr>
+<td bgcolor="#df0000">&nbsp;</td><td>88-94</td>
+</tr>
+<tr>
+<td bgcolor="#ef0000">&nbsp;</td><td>95-100</td>
+</tr>
+"""
+
 HTML_TEMPLATE_GREENRED = """<!DOCTYPE html PUBLIC 
     "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang="en-US" xml:lang="en-US" xmlns="http://www.w3.org/1999/xhtml">
@@ -92,10 +158,10 @@ HTML_TEMPLATE_GREENRED = """<!DOCTYPE html PUBLIC
 </html>
 """
 
-def generate_html(path, width="", height=""):
+def generate_html(path, width="", height="", template=HTML_TEMPLATE_GREENRED):
     """ Method takes an HTML template and returns the HTML with a GEOTIFF 
     linked """
-    curr_html = HTML_TEMPLATE_GREENRED
+    curr_html = template
     for (token, new_value) in (
         ('PLACEHOLDER_IMAGE_NAME', path), ('PLACEHOLDER_WIDTH', width),
         ('PLACEHOLDER_HEIGHT', height)):
