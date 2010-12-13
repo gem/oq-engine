aa124f3f35683de2d5babe7a1f98ade5b3ae7978
Damiano Monelli <monelli.damiano@gmail.com>
2010-12-13 17:39:56 +0100

    first working version of the source reader
    Former-commit-id: ff9748ab9c89ae54302984920984e9816dd528d5 [formerly ff9748ab9c89ae54302984920984e9816dd528d5 [formerly 4ee49fcc52a2a5e6b4aaca08824b2efc84a73235]]
    Former-commit-id: 105d8182bdb642f82dd4d4b9d91fbc191bc914fe
    Former-commit-id: d9791db8789df13b56531c7e0c99c46e7960e5fc

--- java_tests/org/gem/engine/SourceModelReaderTest.java
+++ java_tests/org/gem/engine/SourceModelReaderTest.java
@@ -1,12 +1,27 @@
 package org.gem.engine;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
+import org.opensha.commons.data.function.ArbitrarilyDiscretizedFunc;
+import org.opensha.commons.geo.BorderType;
 import org.opensha.commons.geo.Location;
+import org.opensha.commons.geo.LocationList;
+import org.opensha.commons.geo.Region;
+import org.opensha.sha.earthquake.FocalMechanism;
+import org.opensha.sha.earthquake.griddedForecast.HypoMagFreqDistAtLoc;
+import org.opensha.sha.earthquake.griddedForecast.MagFreqDistsForFocalMechs;
+import org.opensha.sha.earthquake.rupForecastImpl.GEM1.SourceData.GEMAreaSourceData;
 import org.opensha.sha.earthquake.rupForecastImpl.GEM1.SourceData.GEMFaultSourceData;
+import org.opensha.sha.earthquake.rupForecastImpl.GEM1.SourceData.GEMPointSourceData;
 import org.opensha.sha.earthquake.rupForecastImpl.GEM1.SourceData.GEMSourceData;
+import org.opensha.sha.earthquake.rupForecastImpl.GEM1.SourceData.GEMSubductionFaultSourceData;
 import org.opensha.sha.faultSurface.FaultTrace;
+import org.opensha.sha.magdist.GutenbergRichterMagFreqDist;
 import org.opensha.sha.magdist.IncrementalMagFreqDist;
 import org.opensha.sha.util.TectonicRegionType;
 
@@ -15,11 +30,94 @@ public class SourceModelReaderTest {
     @Test
     public void readSourceModel() {
 
-        ArrayList<GEMSourceData> srcList = new ArrayList<GEMSourceData>();
+        List<GEMSourceData> srcList = new ArrayList<GEMSourceData>();
+        srcList.add(getFaultSourceData());
+        srcList.add(getComplexFaultData());
+        srcList.add(getAreaSource());
+        srcList.add(getPointSource());
 
-        GEMFaultSourceData faultSrc = getFaultSourceData();
-        srcList.add(faultSrc);
+        SourceModelReader srcModelReader =
+                new SourceModelReader("java_tests/data/source_model.xml", 0.1);
 
+        assertEquals(srcList.size(), srcModelReader.getSourceList().size());
+        for (int i = 0; i < srcList.size(); i++) {
+            assertTrue(srcModelReader.getSourceList().get(i)
+                    .equals(srcList.get(i)));
+        }
+
+    }
+
+    private GEMPointSourceData getPointSource() {
+        String ID = "src04";
+        String name = "point";
+        TectonicRegionType tectReg = TectonicRegionType.ACTIVE_SHALLOW;
+        Location loc = new Location(38.0, -122.0);
+        IncrementalMagFreqDist[] magDists = new IncrementalMagFreqDist[1];
+        double bValue = 0.8;
+        double aValueCumulative = 0.5;
+        double min = 5.0;
+        double max = 7.0;
+        double delta = 0.1;
+        IncrementalMagFreqDist magFreqDist =
+                getGutenbergRichterMagFreqDist(bValue, aValueCumulative, min,
+                        max, delta);
+        magDists[0] = magFreqDist;
+        FocalMechanism[] focMechs = new FocalMechanism[1];
+        double strike = 0.0;
+        double dip = 90.0;
+        double rake = 0.0;
+        FocalMechanism focMech = new FocalMechanism(strike, dip, rake);
+        focMechs[0] = focMech;
+        HypoMagFreqDistAtLoc hypoMagFreqDistAtLoc =
+                new HypoMagFreqDistAtLoc(magDists, loc, focMechs);
+        ArbitrarilyDiscretizedFunc aveRupTopVsMag =
+                new ArbitrarilyDiscretizedFunc();
+        aveRupTopVsMag.set(6.0, 5.0);
+        aveRupTopVsMag.set(6.5, 3.0);
+        aveRupTopVsMag.set(7.0, 0.0);
+        double aveHypoDepth = 5.0;
+        return new GEMPointSourceData(ID, name, tectReg, hypoMagFreqDistAtLoc,
+                aveRupTopVsMag, aveHypoDepth);
+
+    }
+
+    private GEMAreaSourceData getAreaSource() {
+        String ID = "src03";
+        String name = "Quito";
+        TectonicRegionType tectReg = TectonicRegionType.ACTIVE_SHALLOW;
+        LocationList regionBoundary = new LocationList();
+        regionBoundary.add(new Location(37.5, -122.5));
+        regionBoundary.add(new Location(37.5, -121.5));
+        regionBoundary.add(new Location(38.5, -121.5));
+        regionBoundary.add(new Location(38.5, -122.5));
+        Region reg = new Region(regionBoundary, BorderType.GREAT_CIRCLE);
+        double bValue = 0.8;
+        double aValueCumulative = 5.0;
+        double min = 5.0;
+        double max = 7.0;
+        double delta = 0.1;
+        IncrementalMagFreqDist magFreqDist =
+                getGutenbergRichterMagFreqDist(bValue, aValueCumulative, min,
+                        max, delta);
+        double strike = 0.0;
+        double dip = 90.0;
+        double rake = 0.0;
+        FocalMechanism focalMechanism = new FocalMechanism(strike, dip, rake);
+        MagFreqDistsForFocalMechs magfreqDistFocMech =
+                new MagFreqDistsForFocalMechs(magFreqDist, focalMechanism);
+        ArbitrarilyDiscretizedFunc aveRupTopVsMag =
+                new ArbitrarilyDiscretizedFunc();
+        aveRupTopVsMag.set(6.0, 5.0);
+        aveRupTopVsMag.set(6.5, 3.0);
+        aveRupTopVsMag.set(7.0, 0.0);
+        double aveHypoDepth = 5.0;
+        GEMAreaSourceData areaSrc =
+                new GEMAreaSourceData(ID, name, tectReg, reg,
+                        magfreqDistFocMech, aveRupTopVsMag, aveHypoDepth);
+        return areaSrc;
+    }
+
+    private GEMSubductionFaultSourceData getComplexFaultData() {
         String name = "Cascadia Megathrust";
         String ID = "src02";
         TectonicRegionType tectRegType =
@@ -41,23 +139,51 @@ public class SourceModelReaderTest {
         topTrace.add(new Location(48.287, -126.660, 0.4125160E+01));
         topTrace.add(new Location(48.711, -127.037, 0.4583670E+01));
         topTrace.add(new Location(49.279, -127.605, 0.4761580E+01));
-        // GEMSubductionFaultSourceData complexFaultSrc = new
-        // GEMSubductionFaultSourceData(FaultTrace TopTrace, FaultTrace
-        // BottomTrace,
-        // double rake, IncrementalMagFreqDist mfd, boolean floatRuptureFlag);
+        FaultTrace bottomTrace = new FaultTrace("");
+        bottomTrace.add(new Location(40.347, -123.829, 0.2038490E+02));
+        bottomTrace.add(new Location(41.218, -124.137, 0.1741390E+02));
+        bottomTrace.add(new Location(42.115, -124.252, 0.1752740E+02));
+        bottomTrace.add(new Location(42.984, -124.313, 0.1729190E+02));
+        bottomTrace.add(new Location(43.868, -124.263, 0.1856200E+02));
+        bottomTrace.add(new Location(44.740, -124.213, 0.1977810E+02));
+        bottomTrace.add(new Location(45.494, -124.099, 0.2230320E+02));
+        bottomTrace.add(new Location(46.369, -123.853, 0.2575860E+02));
+        bottomTrace.add(new Location(46.811, -123.644, 0.2711490E+02));
+        bottomTrace.add(new Location(47.300, -123.423, 0.2761730E+02));
+        bottomTrace.add(new Location(47.792, -123.440, 0.2750930E+02));
+        bottomTrace.add(new Location(48.221, -124.075, 0.2602160E+02));
+        bottomTrace.add(new Location(48.560, -124.773, 0.2572870E+02));
+        bottomTrace.add(new Location(48.873, -125.409, 0.2544710E+02));
+        bottomTrace.add(new Location(49.244, -126.117, 0.2471340E+02));
+        bottomTrace.add(new Location(49.687, -126.911, 0.2275770E+02));
+        double rake = 0.0;
+        double bValue = 0.8;
+        double aValueCumulative = 1.0;
+        double min = 6.5;
+        double max = 9.0;
+        double delta = 0.1;
+        IncrementalMagFreqDist magFreqDist =
+                getGutenbergRichterMagFreqDist(bValue, aValueCumulative, min,
+                        max, delta);
+        boolean floatRuptureFlag = true;
+        GEMSubductionFaultSourceData complexFaultSrc =
+                new GEMSubductionFaultSourceData(ID, name, tectRegType,
+                        topTrace, bottomTrace, rake, magFreqDist,
+                        floatRuptureFlag);
+        return complexFaultSrc;
+    }
 
-        // GEMAreaSourceData areaSrc = new GEMAreaSourceData(String id, String
-        // name, TectonicRegionType tectReg,
-        // Region reg, MagFreqDistsForFocalMechs magfreqDistFocMech,
-        // ArbitrarilyDiscretizedFunc aveRupTopVsMag, double aveHypoDepth);
-
-        // GEMPointSourceData pointSrc = new GEMPointSourceData(String id,
-        // String name, TectonicRegionType tectReg,
-        // HypoMagFreqDistAtLoc hypoMagFreqDistAtLoc,
-        // ArbitrarilyDiscretizedFunc aveRupTopVsMag, double aveHypoDepth);
-
-        SourceModelReader srcModelReader =
-                new SourceModelReader("java_tests/data/source_model.xml", 0.1);
+    private GutenbergRichterMagFreqDist getGutenbergRichterMagFreqDist(
+            double bValue, double aValueCumulative, double min, double max,
+            double delta) {
+        double totCumRate =
+                Math.pow(10, aValueCumulative - bValue * min)
+                        - Math.pow(10, aValueCumulative - bValue * max);
+        min = min + delta / 2;
+        max = max - delta / 2;
+        int num = (int) Math.round((max - min) / delta + 1);
+        return new GutenbergRichterMagFreqDist(bValue, totCumRate, min, max,
+                num);
     }
 
     private GEMFaultSourceData getFaultSourceData() {
@@ -81,8 +207,8 @@ public class SourceModelReaderTest {
         boolean floatRuptureFlag = true;
         GEMFaultSourceData faultSrc =
                 new GEMFaultSourceData(ID, name, tectRegType, magFreqDist,
-                        trace, dip, rake, upperSeismogenicDepth,
-                        lowerSeismogenicDepth, floatRuptureFlag);
+                        trace, dip, rake, lowerSeismogenicDepth,
+                        upperSeismogenicDepth, floatRuptureFlag);
         return faultSrc;
     }
 }
