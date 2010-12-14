f4bb4980abf6c0dbe8167dca817a9b8ebd7632e0
Andrea Cerisara <andreacerisara@gmail.com>
2010-12-14 20:55:17 +0100

    Deleted unused pylibmc and pyyaml dependencies. Closes #9
    Former-commit-id: b7ba53525591cd6e33499456b5b4f1a890155c80 [formerly 76d91b295070fd2f29ba3a709b7d7a17a134d0a0]
    Former-commit-id: 26ddcb03fc9652c68b1c248a5a6bc8fae617ea5b

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(name='openquake',
       data_files=[('/etc/openquake', ['celeryconfig.py']),
                   ('lib', libs),('dist', dist)],
       scripts=scripts,
-      install_requires=["pyyaml", "shapely", "python-gflags", "pylibmc==0.9.2",
+      install_requires=["shapely", "python-gflags",
                         "lxml", "sphinx", "eventlet", "guppy", "libLAS",
                         "numpy", "scipy", "celery", "nose", "django",
                         "ordereddict"])
