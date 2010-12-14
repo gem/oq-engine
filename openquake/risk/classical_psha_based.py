b9595f4347322c83f67e320176a6d247ad3aacfc
Andrea Cerisara <andreacerisara@gmail.com>
2010-12-14 20:23:32 +0100

    Introduced numpy
    Former-commit-id: 2ae7a8b1355b8afe3473129dca70ba64bfc6fa2b [formerly 2437b4306ee7ef49a9a1a7364e985ddddf73c8e8]
    Former-commit-id: 288b9d72b3334c754ce3b894247b62826a05ad11

--- openquake/risk/classical_psha_based.py
+++ openquake/risk/classical_psha_based.py
@@ -7,6 +7,7 @@ using the classical psha based approach.
 from numpy import linspace # pylint: disable=F0401,E0611
 import scipy # pylint: disable=F0401
 
+from numpy import empty # pylint: disable=F0401,E0611
 from numpy import isnan # pylint: disable=F0401,E0611
 from scipy import sqrt # pylint: disable=F0401,E0611
 from scipy import stats # pylint: disable=F0401,E0611
@@ -15,6 +16,7 @@ from scipy import log # pylint: disable=F0401,E0611
 from openquake import shapes
 
 STEPS_PER_INTERVAL = 5
+
 
 def compute_loss_ratio_curve(vuln_function, hazard_curve):
     """Compute a loss ratio curve for a specific hazard curve (e.g., site),
@@ -33,34 +35,23 @@ def compute_loss_ratio_curve(vuln_function, hazard_curve):
 
 
 def _compute_lrem_po(vuln_function, lrem, hazard_curve):
-    """Compute the loss ratio * probability of occurrence matrix.""" 
+    """Compute the loss ratio * PoEs matrix.""" 
 
-    current_column = 0
-    lrem_po = [None] * len(lrem)
     imls = vuln_function.abscissae
+    lrem_po = empty((len(lrem), len(imls)), float)
 
-    for iml in imls:
+    for idx, iml in enumerate(imls):
         prob_occ = hazard_curve.ordinate_for(iml)
+
         for row in range(len(lrem_po)):
-            if not lrem_po[row]: 
-                lrem_po[row] = [None] * len(vuln_function.abscissae)
-            lrem_po[row][current_column] = lrem[row][current_column] * prob_occ
-        current_column += 1
+            lrem_po[row][idx] = lrem[row][idx] * prob_occ
     
     return lrem_po
 
 
 def _compute_loss_ratio_curve_from_lrem_po(loss_ratios, lrem_po):
     """Compute the loss ratio curve."""
-    
-    data = []
-    for row in range(len(lrem_po)-1):
-        prob_occ = 0.0
-        for column in range(len(lrem_po[row])):
-            prob_occ += lrem_po[row][column]
-        data.append((loss_ratios[row], prob_occ))
-
-    return shapes.Curve(data)
+    return shapes.Curve(zip(loss_ratios, lrem_po.sum(axis=1)[:-1]))
 
 
 # @state.memoize
@@ -88,12 +79,10 @@ def _compute_lrem(vuln_function, distribution=None):
         # this is so we can memoize the thing
 
     loss_ratios = _generate_loss_ratios(vuln_function)
+    lrem = empty((len(loss_ratios) + 1, len(vuln_function.abscissae)), float)
 
-    current_column = 0
-    lrem = [None] * (len(loss_ratios)+1)
-
-    def fix_value(prob):
-        """Fix negative probabilities for values close to zero."""
+    def fix_prob(prob):
+        """Fix probabilities for values close to zero."""
         if isnan(prob):
             return 0.0
         if prob < 0.00001: 
@@ -101,28 +90,24 @@ def _compute_lrem(vuln_function, distribution=None):
         else: 
             return prob
 
-    for iml in vuln_function.abscissae:
+    for idx, iml in enumerate(vuln_function.abscissae):
         mean = vuln_function.ordinate_for(iml)
         cov = vuln_function.ordinate_for(iml, 1)
+
         stddev = cov * mean
         variance = stddev ** 2.0
         mu = log(mean ** 2.0 / sqrt(variance + mean ** 2.0) )
         sigma = sqrt(log((variance / mean ** 2.0) + 1.0))
-        
-        for row in range(len(loss_ratios)+1):
-            if not lrem[row]: 
-                lrem[row] = [None] * len(vuln_function.abscissae)
+
+        for row in range(len(lrem)):
             # last loss ratio is fixed to be 1
             if row < len(loss_ratios): 
                 next_ratio = loss_ratios[row]
             else: 
                 next_ratio = 1.0
             
-            lrem[row][current_column] = fix_value(
-                    distribution.sf(next_ratio, 
+            lrem[row][idx] = fix_prob(distribution.sf(next_ratio,
                     sigma, scale=scipy.exp(mu)))
-
-        current_column += 1
 
     return lrem
 
