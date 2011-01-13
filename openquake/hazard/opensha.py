450909bf80ae77ebdb1088e81340a536919f68c9
Lars Butler <Lars.Butler@gmail.com>
2011-01-13 10:47:07 +0100

    Fixed a pylint error
    Former-commit-id: ecb80dbc19447f6fff7546b1ce182af678d2ab6e [formerly ecb80dbc19447f6fff7546b1ce182af678d2ab6e [formerly 8dc2350fe861d5a4ae0432d543650af93359300f]]
    Former-commit-id: 07fc910bf50692de8d711c3e5674d11a73b1ae11
    Former-commit-id: aa6badac6698c4b41b9be5076c9fb33bd938a414

--- openquake/hazard/opensha.py
+++ openquake/hazard/opensha.py
@@ -82,7 +82,8 @@ class MonteCarloMixin: # pylint: disable=W0232
         results = []
         
         source_model_generator = random.Random()
-        source_model_generator.seed(self.params.get('SOURCE_MODEL_LT_RANDOM_SEED', None))
+        source_model_generator.seed(
+                self.params.get('SOURCE_MODEL_LT_RANDOM_SEED', None))
         
         gmpe_generator = random.Random()
         gmpe_generator.seed(self.params.get('GMPE_LT_RANDOM_SEED', None))
