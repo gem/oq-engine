18811a69b6175cefe746bde8a2dcec0a9ddc91f4
Fabian Euchner <fabian@sed.ethz.ch>
2011-01-25 10:50:48 +0100

    changed format of hc iterable in unit test

--- tests/hazard_nrml_unittest.py
+++ tests/hazard_nrml_unittest.py
@@ -73,8 +73,8 @@ class HazardCurveXMLWriterTestCase(unittest.TestCase):
         self.assertRaises(RuntimeError, self.writer.close)
 
     def test_writes_a_single_result(self):
-        data = {shapes.Site(-122.5000, 37.5000): {
-                    "IDmodel": "MMI_3_1",
+        data = [(shapes.Site(-122.5000, 37.5000), 
+                {   "IDmodel": "MMI_3_1",
                     "investigationTimeSpan": 50.0,
                     "endBranchLabel": "3_1",
                     "IML": [5.0000e-03, 7.0000e-03, 1.3700e-02, 
@@ -89,7 +89,7 @@ class HazardCurveXMLWriterTestCase(unittest.TestCase):
                     9.0326e-01, 8.1956e-01, 6.9192e-01, 5.2866e-01, 
                     3.6143e-01, 2.4231e-01, 2.2452e-01, 1.2831e-01, 
                     7.0352e-02, 3.6060e-02, 1.6579e-02, 6.4213e-03, 
-                    2.0244e-03, 4.8605e-04, 8.1752e-05, 7.3425e-06]}}
+                    2.0244e-03, 4.8605e-04, 8.1752e-05, 7.3425e-06]})]
 
         path = test.test_output_file(TEST_FILE_SINGLE_RESULT)
         self._remove_and_write_file(path)
@@ -105,8 +105,8 @@ class HazardCurveXMLWriterTestCase(unittest.TestCase):
         #self._count_and_check_readed_data(data, curves, 1)
 
     def test_writes_multiple_results_with_one_branch_level(self):
-        data = {shapes.Site(-122.5000, 37.5000): {
-                    "IDmodel": "MMI_3_1",
+        data = [(shapes.Site(-122.5000, 37.5000), 
+                 {  "IDmodel": "MMI_3_1",
                     "investigationTimeSpan": 50.0,
                     "endBranchLabel": "3_1",
                     "IML": [5.0000e-03, 7.0000e-03, 1.3700e-02, 
@@ -121,9 +121,9 @@ class HazardCurveXMLWriterTestCase(unittest.TestCase):
                     9.0326e-01, 8.1956e-01, 6.9192e-01, 5.2866e-01, 
                     3.6143e-01, 2.4231e-01, 2.2452e-01, 1.2831e-01, 
                     7.0352e-02, 3.6060e-02, 1.6579e-02, 6.4213e-03, 
-                    2.0244e-03, 4.8605e-04, 8.1752e-05, 7.3425e-06]},
-                shapes.Site(-122.4000, 37.5000): {
-                    "IDmodel": "MMI_3_1",
+                    2.0244e-03, 4.8605e-04, 8.1752e-05, 7.3425e-06]}),
+                (shapes.Site(-122.4000, 37.5000), 
+                 {  "IDmodel": "MMI_3_1",
                     "investigationTimeSpan": 50.0,
                     "endBranchLabel": "3_1",
                     "IML": [5.0000e-03, 7.0000e-03, 1.3700e-02, 
@@ -138,7 +138,7 @@ class HazardCurveXMLWriterTestCase(unittest.TestCase):
                     9.1955e-01, 8.5019e-01, 7.4038e-01, 5.9153e-01, 
                     4.2626e-01, 2.9755e-01, 2.7731e-01, 1.6218e-01, 
                     8.8035e-02, 4.3499e-02, 1.9065e-02, 7.0442e-03, 
-                    2.1300e-03, 4.9498e-04, 8.1768e-05, 7.3425e-06]}}
+                    2.1300e-03, 4.9498e-04, 8.1768e-05, 7.3425e-06]})]
 
         path = test.test_output_file(TEST_FILE_MULTIPLE_ONE_BRANCH)
         self._remove_and_write_file(path)
@@ -151,7 +151,8 @@ class HazardCurveXMLWriterTestCase(unittest.TestCase):
         #self._count_and_check_readed_data(data, curves, 2)
 
     def test_writes_multiple_results_with_statistics(self):
-        data = {shapes.Site(-122.5000, 37.5000): {
+        data = [(shapes.Site(-122.5000, 37.5000), 
+                 {
                     "nrml_id": "nrml_instance_1",
                     "hazres_id": "hazard_result_0001",
                     "hcfield_id": "hazard_field_one",
@@ -172,9 +173,9 @@ class HazardCurveXMLWriterTestCase(unittest.TestCase):
                     9.0326e-01, 8.1956e-01, 6.9192e-01, 5.2866e-01, 
                     3.6143e-01, 2.4231e-01, 2.2452e-01, 1.2831e-01, 
                     7.0352e-02, 3.6060e-02, 1.6579e-02, 6.4213e-03, 
-                    2.0244e-03, 4.8605e-04, 8.1752e-05, 7.3425e-06]},
-                shapes.Site(-122.4000, 37.5000): {
-                    "IDmodel": "foo",
+                    2.0244e-03, 4.8605e-04, 8.1752e-05, 7.3425e-06]}),
+                (shapes.Site(-122.4000, 37.5000), 
+                 {  "IDmodel": "foo",
                     "investigationTimeSpan": 50.0,
                     "statistics": "quantile",
                     "quantileValue": "0.5",
@@ -190,7 +191,7 @@ class HazardCurveXMLWriterTestCase(unittest.TestCase):
                     9.1955e-01, 8.5019e-01, 7.4038e-01, 5.9153e-01, 
                     4.2626e-01, 2.9755e-01, 2.7731e-01, 1.6218e-01, 
                     8.8035e-02, 4.3499e-02, 1.9065e-02, 7.0442e-03, 
-                    2.1300e-03, 4.9498e-04, 8.1768e-05, 7.3425e-06]}}
+                    2.1300e-03, 4.9498e-04, 8.1768e-05, 7.3425e-06]})]
 
         path = test.test_output_file(TEST_FILE_STATISTICS)
         self._remove_and_write_file(path)
@@ -203,25 +204,25 @@ class HazardCurveXMLWriterTestCase(unittest.TestCase):
         #self._count_and_check_readed_data(data, curves, 2)
 
     def test_writes_the_config_only_once(self):
-        data = {shapes.Site(-122.5000, 37.5000): {
-                        "IDmodel": "MMI_3_1",
+        data = [(shapes.Site(-122.5000, 37.5000), 
+                 {      "IDmodel": "MMI_3_1",
                         "investigationTimeSpan": 50.0,
                         "endBranchLabel": "3_1",
                         "IML": [5.0, 6.0, 7.0],
                         "saPeriod": 0.1,
                         "saDamping": 1.0,
                         "IMT": "PGA",
-                        "poE": [0.1, 0.2, 0.3]},
-                shapes.Site(-122.4000, 37.5000): {
-                        "IDmodel": "MMI_3_1",
+                        "poE": [0.1, 0.2, 0.3]}),
+                (shapes.Site(-122.4000, 37.5000), 
+                 {      "IDmodel": "MMI_3_1",
                         "investigationTimeSpan": 50.0,
                         "endBranchLabel": "3_2",
                         "IML": [5.0, 6.0, 7.0],
                         "saPeriod": 0.1,
                         "saDamping": 1.0,
                         "IMT": "PGA",
-                        "poE": [0.4, 0.5, 0.6]}
-                }
+                        "poE": [0.4, 0.5, 0.6]})
+                ]
         
         path = test.test_output_file(TEST_FILE_CONFIG_ONCE)
         self._remove_and_write_file(path)
@@ -230,24 +231,33 @@ class HazardCurveXMLWriterTestCase(unittest.TestCase):
         self._is_xml_valid(path)
 
     def test_writes_multiple_results_with_different_branch_levels(self):
-        data = {shapes.Site(-122.5000, 37.5000): {
-                    "IDmodel": "MMI_3_1",
+        data = [(shapes.Site(-122.5000, 37.5000), 
+                 {  "IDmodel": "MMI_3_1",
                     "investigationTimeSpan": 50.0,
                     "endBranchLabel": "3_1",
                     "IML": [5.0, 6.0, 7.0],
                     "saPeriod": 0.1,
                     "saDamping": 1.0,
                     "IMT": "PGA",
-                    "poE": [0.1, 0.2, 0.3]},
-                shapes.Site(-122.4000, 37.5000): {
-                    "IDmodel": "MMI_3_1",
+                    "poE": [0.1, 0.2, 0.3]}),
+                (shapes.Site(-122.5000, 37.5000), 
+                 {  "IDmodel": "MMI_3_1",
+                    "investigationTimeSpan": 50.0,
+                    "endBranchLabel": "3_2",
+                    "IML": [5.0, 6.0, 7.0],
+                    "saPeriod": 0.1,
+                    "saDamping": 1.0,
+                    "IMT": "PGA",
+                    "poE": [0.1, 0.2, 0.3]}),
+                (shapes.Site(-122.4000, 37.5000), 
+                 {  "IDmodel": "MMI_3_1",
                     "investigationTimeSpan": 50.0,
                     "endBranchLabel": "3_2",
                     "IML": [8.0, 9.0, 10.0],
                     "saPeriod": 0.1,
                     "saDamping": 1.0,
                     "IMT": "PGA",
-                    "poE": [0.1, 0.2, 0.3]}}
+                    "poE": [0.1, 0.2, 0.3]})]
 
         path = test.test_output_file(TEST_FILE_MULTIPLE_DIFFERENT_BRANCHES)
         self._remove_and_write_file(path)
