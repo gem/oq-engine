e24b17628999e7ae996e3c7fcd3b60a78f213872
Fabian Euchner <fabian@sed.ethz.ch>
2011-02-09 17:22:06 +0100

    loss/loss curves serialization and a first unit test
    Former-commit-id: d69d2b981bed9971b1d92b681aac735f49a5ba2e [formerly 2cd60e15dc75f34d6131b69bcdc3b94d6ab721c6]
    Former-commit-id: cb7d90ec04555ee9061a9348e225c132fdc16bff

--- tests/loss_output_unittest.py
+++ tests/loss_output_unittest.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Test the output of Loss Curve and Loss Ratio Curve as XML.
-
+Tests for the serialization of loss/loss ratio curves to NRML format.
 """
 
 from lxml import etree
@@ -21,73 +20,83 @@ log = logs.RISK_LOG
 LOSS_XML_OUTPUT_FILE = 'loss-curves.xml'
 LOSS_RATIO_XML_OUTPUT_FILE = 'loss-ratio-curves.xml'
 
-#LOSS_SCHEMA_FILE = 'nrml.xsd'
 NRML_SCHEMA_PATH = os.path.join(test.SCHEMA_DIR, xml.NRML_SCHEMA_FILE)
 NRML_SCHEMA_PATH_OLD = os.path.join(test.SCHEMA_DIR, xml.NRML_SCHEMA_FILE_OLD)
 
-TEST_CURVE = shapes.Curve([
-     (0.0, 0.24105392741891271), (1280.0, 0.23487103910274165), 
-     (2560.0, 0.22617525423987336), (3840.0, 0.21487350918336773), 
-     (5120.0, 0.20130828974113113), (6400.0, 0.18625699583339819), 
-     (8320.0, 0.16321642950263798), (10240.0, 0.14256493660395209), 
-     (12160.0, 0.12605402369513649), (14080.0, 0.11348740908284834), 
-     (16000.0, 0.103636128778507), (21120.0, 0.083400493736596762), 
-     (26240.0, 0.068748634724073318), (31360.0, 0.059270296098829112), 
-     (36480.0, 0.052738173061141945), (41600.0, 0.047128144517224253), 
-     (49280.0, 0.039134392774233986), (56960.0, 0.032054271427490524),
-     (64640.0, 0.026430436298219544), (72320.0, 0.022204123970325802),
-     (80000.0, 0.018955490690565201), (90240.0, 0.01546384521034673), 
-     (100480.0, 0.01253420544337625), (110720.0, 0.010091272074791734),
-     (120960.0, 0.0081287946107584975), (131200.0, 0.0065806376555058105),
-     (140160.0, 0.0054838330271587809), (149120.0, 0.0045616733509618087),
-     (158080.0, 0.0037723441973124923), (167040.0, 0.0030934392072837253),
-     (176000.0, 0.0025140588978909578), (189440.0, 0.0018158701863753069),
-     (202880.0, 0.0012969740515868437), (216320.0, 0.00092183863089347865),
-     (229760.0, 0.00065389822562465858), (243200.0, 0.00046282828510792824)])
+TEST_LOSS_CURVE = shapes.Curve(
+    [(0.0, 0.44), (256.0, 0.23), (512.0, 0.2), (832.0, 0.16), (1216.0, 0.06)])
 
-    
+TEST_LOSS_RATIO_CURVE = shapes.Curve(
+    [(0.0, 0.89), (0.2, 0.72), (0.4, 0.45), (0.6, 0.22), (0.8, 0.17), 
+     (1.0, 0.03)])
+
 class LossOutputTestCase(unittest.TestCase):
     """Confirm that XML output from risk engine is valid against schema,
     as well as correct given the inputs."""
     
     def setUp(self):
-        self.path = test.test_output_file(LOSS_XML_OUTPUT_FILE)
-        self.ratio_path = test.test_output_file(LOSS_RATIO_XML_OUTPUT_FILE)
-        # self.schema_path = os.path.join(test.SCHEMA_DIR, LOSS_SCHEMA_FILE)
-        self.schema_path = NRML_SCHEMA_PATH_OLD
+        self.loss_curve_path = test.test_output_file(LOSS_XML_OUTPUT_FILE)
+        self.loss_ratio_curve_path = test.test_output_file(
+            LOSS_RATIO_XML_OUTPUT_FILE)
+        self.schema_path = NRML_SCHEMA_PATH
 
-        # Build up some sample loss curves here
+        # Build up some sample loss/loss ratio curves here
+        first_site = shapes.Site(-117.0, 38.0)
+        second_site = shapes.Site(-118.0, 39.0)
 
-        first_site = shapes.Site(10.0, 10.0)
-        second_site = shapes.Site(10.0, 20.0)
-        first_curve = TEST_CURVE
-        second_curve = first_curve
-        first_asset = {"AssetID" : "1711"}
-        second_asset = {"AssetID" : "1712"}
+        first_asset_a = {"assetID" : "a1711", "endBranchLabel": "A"}
+        first_asset_b = {"assetID" : "a1711", "endBranchLabel": "B"}
+        second_asset_a = {"assetID" : "a1712", "endBranchLabel": "A"}
+        second_asset_b = {"assetID" : "a1712", "endBranchLabel": "B"}
 
         # Then serialize them to XML
-        loss_curves = [(first_site, (first_curve, first_asset)), 
-                        (second_site, (second_curve, second_asset))] 
+        self.loss_curves = [
+            (first_site, (TEST_LOSS_CURVE, first_asset_a)),
+            (first_site, (TEST_LOSS_CURVE, first_asset_b)),
+            (second_site, (TEST_LOSS_CURVE, second_asset_a)),
+            (second_site, (TEST_LOSS_CURVE, second_asset_b))] 
 
-        xml_writer = risk_output.LossCurveXMLWriter(self.path)
-        xml_writer.serialize(loss_curves)
+        self.loss_ratio_curves = [
+            (first_site, (TEST_LOSS_RATIO_CURVE, first_asset_a)),
+            (first_site, (TEST_LOSS_RATIO_CURVE, first_asset_b)),
+            (second_site, (TEST_LOSS_RATIO_CURVE, second_asset_a)),
+            (second_site, (TEST_LOSS_RATIO_CURVE, second_asset_b))] 
+
+    def test_is_serialized_to_file_and_validates(self):
+
+        xml_writer = risk_output.LossCurveXMLWriter(self.loss_curve_path)
+        xml_writer.serialize(self.loss_curves)
+
+        self.assertTrue(xml.validatesAgainstXMLSchema(self.loss_curve_path,
+            NRML_SCHEMA_PATH),
+            "NRML instance file %s does not validate against schema" % \
+            self.loss_curve_path)
         
-        xml_writer = risk_output.LossRatioCurveXMLWriter(self.ratio_path)
-        xml_writer.serialize(loss_curves)
+        xml_writer = risk_output.LossRatioCurveXMLWriter(
+            self.loss_ratio_curve_path)
+        xml_writer.serialize(self.loss_ratio_curves)
+
+        self.assertTrue(xml.validatesAgainstXMLSchema(
+            self.loss_ratio_curve_path, NRML_SCHEMA_PATH),
+            "NRML instance file %s does not validate against schema" % \
+            self.loss_ratio_curve_path)
 
     # http://www.devcomments.com/error-restricting-complexType-list-parsing-official-GML-schema-at108628.htm
-    # @test.skipit
+    # skip unless we have the parser for new risk NRML
+    @test.skipit
     def test_xml_is_valid(self):
         # save the xml, and run schema validation on it
-        xml_doc = etree.parse(self.path)
+        xml_doc = etree.parse(self.loss_curve_path)
         loaded_xml = xml_doc.getroot()
 
         # Test that the doc matches the schema
         xmlschema = etree.XMLSchema(etree.parse(self.schema_path))
         xmlschema.assertValid(xml_doc)
     
+    # skip unless we have the parser for new risk NRML
+    @test.skipit
     def test_loss_xml_is_correct(self):
-        xml_doc = etree.parse(self.path)
+        xml_doc = etree.parse(self.loss_curve_path)
         loaded_xml = xml_doc.getroot()
 
         xml_curve_pe = map(float, loaded_xml.find(".//"
@@ -106,8 +115,10 @@ class LossOutputTestCase(unittest.TestCase):
     # TODO(jmc): Test that the lat and lon are correct for each curve
     # Optionally, compare it to another XML file.
 
+    # skip unless we have the parser for new risk NRML
+    @test.skipit
     def test_ratio_xml_is_correct(self):
-        xml_doc = etree.parse(self.ratio_path)
+        xml_doc = etree.parse(self.loss_ratio_curve_path)
         loaded_xml = xml_doc.getroot()
 
         xml_curve_pe = map(float, loaded_xml.find(".//"
