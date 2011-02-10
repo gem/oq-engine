c53c4cb3e02e66bf6aca6f6b9dfd65b5529942f0
Fabian Euchner <fabian@sed.ethz.ch>
2011-02-10 13:02:20 +0100

    finished loss/loss ratio curves parser
    Former-commit-id: f1ad147e2f503a7b1a6443eadb5a2182c7e6fb82

--- openquake/parser/risk.py
+++ openquake/parser/risk.py
@@ -15,16 +15,6 @@ from openquake import xml
 
 from openquake.parser import nrml
 
-#from openquake.xml import NRML_NS_OLD, GML_NS_OLD, NRML_OLD
-
-# TODO(fab): use same function from xml module
-def _to_site(element):
-    """Convert current GML attributes to Site object."""
-    # lon/lat are in XML attributes 'Longitude' and 'Latitude'
-    # consider them as mandatory
-    pos_el = element.xpath("gml:pos", namespaces={"gml": xml.GML_NS_OLD})
-    coord = [float(x) for x in pos_el[0].text.strip().split()]
-    return shapes.Site(coord[0], coord[1])
 
 class RiskXMLReader(producer.FileProducer):
     """ This class parses a NRML loss/loss ratio curve file. 
@@ -36,85 +26,98 @@ class RiskXMLReader(producer.FileProducer):
     is a dictionary with risk-related attribute values for this site.
     
     The attribute dictionary looks like
-    {'POE': [0.2, 0.02, ...], 
-     'Values': [0.0, 1280.0, ...],
-     'Property': 'Loss' # 'Loss Ratio'
+    {'assetID': 'a100',
+     'poE': [0.2, 0.02, ...], 
+     'loss': [0.0, 1280.0, ...], # for loss
+     'lossRatio': [0.0, 0.1, ...], # for loss ratio
+     'endBranchLabel': '1_1',
+     'property': 'Loss' # 'Loss Ratio'
     }
     """
 
     # these tag names and properties have to be redefined in the 
-    # derived classes
+    # derived classes for loss and loss ratio
+    container_tag = None
+    curves_tag = None
+    curve_tag = None
+    abscissa_tag = None
     abscissa_property = None
-    ordinate_element = None
-    abscissa_container = None
 
-    ordinate_property = 'Probability of Exceedance'
-    abscissa_element = 'Values'
+    # common names
+    ordinate_property = xml.RISK_CURVE_ORDINATE_PROPERTY
+    ordinate_tag = xml.RISK_POE_TAG
 
-    abscissa_output_key = 'Values'
-    ordinate_output_key = 'POE'
-    property_output_key = 'Property'
+    property_output_key = 'property'
 
-    #def __init__(self, path):
-        #super(RiskXMLReader, self).__init__(path)
+    def __init__(self, path):
+        self.abscissa_output_key = xml.strip_namespace_from_tag(
+            self.abscissa_tag, xml.NRML)
+        self.ordinate_output_key = xml.strip_namespace_from_tag(
+            self.ordinate_tag, xml.NRML)
+        super(RiskXMLReader, self).__init__(path)
 
     def _parse(self):
         for event, element in etree.iterparse(
                 self.file, events=('start', 'end')):
-            if event == 'end' and \
-                element.tag == xml.NRML_OLD + self.abscissa_container:
-                yield (shapes.Site(xml.lon_lat_from_site(element)), 
-                       self._to_attributes(element))
+            if event == 'start' and element.tag == xml.RISK_ASSET_TAG:
+                self._to_asset_attributes(element)
 
-    def _to_attributes(self, element):
-        """ Build an attributes dict from XML element """
+            elif event == 'end' and element.tag == self.curve_tag:
+
+                curr_attributes = self._to_curve_attributes(element)
+
+                # free memory of just parsed element
+                del element
+                yield (self._current_site, curr_attributes)
+
+    def _to_asset_attributes(self, element):
+        """Collect metadata attributes for new asset element."""
+
+        # assetID
+        self._current_asset_meta = {
+            'assetID': element.attrib[xml.GML_ID_ATTR_NAME]}
+
+        # get site
+        lon, lat = xml.lon_lat_from_site(element)
+        self._current_site = shapes.Site(lon, lat)
+
+    def _to_curve_attributes(self, element):
+        """Build an attributes dict from NRML curve element (this can be
+        'lossCurve' or 'lossRatioCurve'.
+        This element contains abscissae (loss/loss ratio) and ordinate 
+        (PoE) values of a curve.
+        """
         
         attributes = {self.property_output_key: self.abscissa_property}
-        
-        float_strip = lambda x: [float(o) for o in x[0].text.strip().split()]
-        # TODO(JMC): This is hardly efficient, but it's simple for the moment...
-        
-        for (child_el, child_key, etl) in (
-            ('nrml:%s' % self.abscissa_element, self.abscissa_output_key, 
-                float_strip),
-            ('../nrml:Common/nrml:%s/nrml:Values' % self.ordinate_element,
-                self.ordinate_output_key, float_strip)):
-            
-            child_node = element.xpath(child_el, 
-                namespaces={"gml": xml.GML_NS_OLD, "nrml": xml.NRML_NS_OLD})
 
-            try:
-                attributes[child_key] = etl(child_node)
-            except Exception:
-                error_str = "invalid or missing %s value" % child_key
-                raise ValueError(error_str) 
-        
-        attributes["AssetID"] = element.attrib.get("AssetID", None)
+        abscissa_el_txt = element.findtext(self.abscissa_tag)
+        attributes[self.abscissa_output_key] = [float(x) \
+            for x in abscissa_el_txt.strip().split()]
+
+        ordinate_el_txt = element.findtext(self.ordinate_tag)
+        attributes[self.ordinate_output_key] = [float(x) \
+            for x in ordinate_el_txt.strip().split()]
+
+        if xml.RISK_END_BRANCH_ATTR_NAME in element.attrib: 
+            attributes[xml.RISK_END_BRANCH_ATTR_NAME] = \
+                element.attrib[xml.RISK_END_BRANCH_ATTR_NAME]
+ 
+        attributes.update(self._current_asset_meta)
         return attributes
 
 
 class LossCurveXMLReader(RiskXMLReader):
     """NRML parser class for loss curves"""
-    
-    abscissa_property = 'Loss'
-    ordinate_element = 'LossCurvePE'
-    abscissa_container = 'LossCurve'
-
-    #container_tag = "%slossCurveList" % xml.NRML
-    #curves_tag = "%slossCurves" % xml.NRML
-    #curve_tag = "%slossCurve" % xml.NRML
-    #abscissa_tag = "%sloss" % xml.NRML
-    
+    container_tag = xml.RISK_LOSS_CONTAINER_TAG
+    curves_tag = xml.RISK_LOSS_CURVES_TAG
+    curve_tag = xml.RISK_LOSS_CURVE_TAG
+    abscissa_tag = xml.RISK_LOSS_ABSCISSA_TAG
+    abscissa_property = xml.RISK_LOSS_ABSCISSA_PROPERTY
 
 class LossRatioCurveXMLReader(RiskXMLReader):
     """NRML parser class for loss ratio curves"""
-
-    abscissa_property = 'Loss Ratio'
-    ordinate_element = 'LossRatioCurvePE'
-    abscissa_container = 'LossRatioCurve'
-
-    #container_tag = "%slossRatioCurveList" % xml.NRML
-    #curves_tag = "%slossRatioCurves" % xml.NRML
-    #curve_tag = "%slossRatioCurve" % xml.NRML
-    #abscissa_tag = "%slossRatio" % xml.NRML
-
+    container_tag = xml.RISK_LOSS_RATIO_CONTAINER_TAG
+    curves_tag = xml.RISK_LOSS_RATIO_CURVES_TAG
+    curve_tag = xml.RISK_LOSS_RATIO_CURVE_TAG
+    abscissa_tag = xml.RISK_LOSS_RATIO_ABSCISSA_TAG
+    abscissa_property = xml.RISK_LOSS_RATIO_ABSCISSA_PROPERTY
