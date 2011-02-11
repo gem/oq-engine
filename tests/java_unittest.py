f57f87425cbeac1cbd3f2e2af2137c525d4090cc
Muharem Hrnjadovic <mh@foldr3.com>
2011-02-11 11:54:46 +0100

    first test added
    Former-commit-id: 7bba080c0a5f9a09ea4800d13a3d8dac80117402

--- tests/java_unittest.py
+++ tests/java_unittest.py
@@ -4,23 +4,23 @@
 Tests for the Python-Java code layer.
 """
 
-import jpype
 import os
 import unittest
 
-from mock import Mock
 from openquake import java
 
 
 class JvmMaxMemTestCase(unittest.TestCase):
     """Tests related to the JVM's maximum memory setting"""
 
-    def test_jvm_setup_without_maxmem_environ_var_and_param(self):
+    def test_jvm_maxmem_with_no_environ_var_and_no_param(self):
         """
         If the OQ_JVM_MAXMEM environment variable is not set and the 'max_mem'
         parameter is not passed the default value should be used when starting
         the JVM.
         """
+        self.assertIs(None, os.environ.get("OQ_JVM_MAXMEM"))
+        self.assertEqual(java.DEFAULT_JVM_MAX_MEM, java.get_jvm_max_mem(None))
 
     def test_jvm_setup_without_maxmem_environ_var_but_with_param(self):
         """
