c83d00a1bf22c3b8727292964b96856cb3aa0e6a
Andrea Cerisara <andreacerisara@gmail.com>
2011-02-21 18:48:42 +0100

    Refactored aggregate loss curve computation
    Former-commit-id: 0cbc095de0dd9d744573f99777dcaf038a1ab4d0

--- openquake/risk/job/aggregate_loss_curve.py
+++ openquake/risk/job/aggregate_loss_curve.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-A mixin that is able to compute and plot an aggregate loss curve.
+Module to compute and plot an aggregate loss curve.
 """
 
 import os
@@ -10,12 +10,12 @@ from openquake.output import curve
 from openquake.risk import probabilistic_event_based as prob
 
 
-def filename(job_id):
+def _filename(job_id):
     """Return the name of the generated file."""
     return "%s-aggregate-loss-curve.svg" % job_id
 
 
-def for_plotting(loss_curve):
+def _for_plotting(loss_curve):
     """Translate a loss curve into a dictionary compatible to
     the interface defined in CurvePlot.write."""
     data = {}
@@ -30,32 +30,30 @@ def for_plotting(loss_curve):
     return data
 
 
-class AggregateLossCurveMixin:
-    """This class computes and plots an aggregate loss curve given a set
-    of pre computed curves stored in the underlying kvs system."""
+def compute_aggregate_curve(job):
+    """Compute and plot an aggreate loss curve.
     
-    def __init__(self):
-        pass
+    This function expects to find in kvs a set of pre computed
+    GMFs and assets. 
+
+    This function is trigger only if the AGGREGATE_LOSS_CURVE
+    parameter has been specified in the configuration file.
+    """
     
-    def execute(self):
-        """Execute the logic of this mixin."""
+    if not job.has("AGGREGATE_LOSS_CURVE"):
+        LOG.debug("AGGREGATE_LOSS_CURVE parameter not specified, " \
+                "skipping aggregate loss curve computation...")
 
-        if not self.has("AGGREGATE_LOSS_CURVE"):
-            LOG.debug("AGGREGATE_LOSS_CURVE parameter not specified, " \
-                    "skipping aggregate loss curve computation...")
+        return
 
-            return []
+    aggregate_loss_curve = prob.AggregateLossCurve.from_kvs(job.id)
+    
+    path = os.path.join(job.base_path,
+            job.params["OUTPUT_DIR"], _filename(job.id))
 
-        aggregate_loss_curve = prob.AggregateLossCurve.from_kvs(self.id)
-        
-        path = os.path.join(self.base_path,
-                self.params["OUTPUT_DIR"], filename(self.id))
+    plotter = curve.CurvePlot(path)
+    plotter.write(_for_plotting(
+            aggregate_loss_curve.compute()), autoscale_y=False)
 
-        plotter = curve.CurvePlot(path)
-        plotter.write(for_plotting(
-                aggregate_loss_curve.compute()), autoscale_y=False)
-
-        plotter.close()
-        LOG.debug("Aggregate loss curve stored at %s" % path)
-
-        return [path] # why?
+    plotter.close()
+    LOG.debug("Aggregate loss curve stored at %s" % path)
