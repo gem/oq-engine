1be8a32a7e38298a461def5b100a35acc0eb56df
Andrea Cerisara <andreacerisara@gmail.com>
2011-02-23 17:05:24 +0100

    Implemented review comments

--- openquake/risk/job/probabilistic.py
+++ openquake/risk/job/probabilistic.py
@@ -31,6 +31,7 @@ DEFAULT_CONDITIONAL_LOSS_POE = 0.01
 
 def preload(fn):
     """ Preload decorator """
+
     def preloader(self, *args, **kwargs):
         """A decorator for preload steps that must run on the Jobber"""
 
@@ -57,7 +58,7 @@ class ProbabilisticEventMixin:
         results = []
         tasks = []
         for block_id in self.blocks_keys:
-            LOGGER.debug("starting task block, block_id = %s of %s" 
+            LOGGER.debug("starting task block, block_id = %s of %s"
                         % (block_id, len(self.blocks_keys)))
             # pylint: disable-msg=E1101
             tasks.append(risk_job.compute_risk.delay(self.id, block_id))
@@ -90,7 +91,7 @@ class ProbabilisticEventMixin:
         histories = int(self['NUMBER_OF_SEISMICITY_HISTORIES'])
         realizations = int(self['NUMBER_OF_LOGIC_TREE_SAMPLES'])
         num_ses = histories * realizations
-        
+
         block = job.Block.from_kvs(block_id)
         sites_list = block.sites
         gmfs = {}
@@ -98,13 +99,13 @@ class ProbabilisticEventMixin:
             risk_point = self.region.grid.point_at(site)
             key = "%s!%s" % (risk_point.row, risk_point.column)
             gmfs[key] = []
-            
+
         for i in range(0, histories):
             for j in range(0, realizations):
                 key = kvs.generate_product_key(
-                        self.id, kvs.tokens.STOCHASTIC_SET_TOKEN, "%s!%s" % 
+                        self.id, kvs.tokens.STOCHASTIC_SET_TOKEN, "%s!%s" %
                             (i, j))
-                fieldset = shapes.FieldSet.from_json(kvs.get(key), 
+                fieldset = shapes.FieldSet.from_json(kvs.get(key),
                     self.region.grid)
 
                 for field in fieldset:
@@ -115,17 +116,17 @@ class ProbabilisticEventMixin:
         for key, gmf_slice in gmfs.items():
             (row, col) = key.split("!")
             key_gmf = kvs.tokens.gmfs_key(self.id, col, row)
-            LOGGER.debug( "GMF_SLICE for %s X %s : \n\t%s" % (
+            LOGGER.debug("GMF_SLICE for %s X %s : \n\t%s" % (
                     col, row, gmf_slice))
             timespan = float(self['INVESTIGATION_TIME'])
-            gmf = {"IMLs": gmf_slice, "TSES": num_ses * timespan, 
+            gmf = {"IMLs": gmf_slice, "TSES": num_ses * timespan,
                     "TimeSpan": timespan}
             kvs.set_value_json_encoded(key_gmf, gmf)
 
     def store_exposure_assets(self):
         """ Load exposure assets and write to memcache """
-        
-        exposure_parser = exposure.ExposurePortfolioFile("%s/%s" % 
+
+        exposure_parser = exposure.ExposurePortfolioFile("%s/%s" %
             (self.base_path, self.params[job.EXPOSURE]))
 
         for site, asset in exposure_parser.filter(self.region):
@@ -133,7 +134,7 @@ class ProbabilisticEventMixin:
             asset['lat'] = site.latitude
             asset['lon'] = site.longitude
             gridpoint = self.region.grid.point_at(site)
-            asset_key = kvs.tokens.asset_key(self.id, gridpoint.row, 
+            asset_key = kvs.tokens.asset_key(self.id, gridpoint.row,
                 gridpoint.column)
             kvs.get_client().rpush(asset_key, json.JSONEncoder().encode(asset))
 
@@ -141,7 +142,7 @@ class ProbabilisticEventMixin:
         """ load vulnerability and write to memcache """
         vulnerability.load_vulnerability_model(self.id,
             "%s/%s" % (self.base_path, self.params["VULNERABILITY"]))
-    
+
     def compute_risk(self, block_id, **kwargs): #pylint: disable=W0613
         """This task computes risk for a block of sites. It requires to have
         pre-initialized in kvs:
@@ -150,27 +151,27 @@ class ProbabilisticEventMixin:
          3) exposure portfolio (=assets)
          4) vulnerability
 
-        TODO(fab): make conditional_loss_poe (set of probabilities of exceedance
-        for which the loss computation is done) a list of floats, and read it from
-        the job configuration.
+        TODO(fab): make conditional_loss_poe (set of probabilities of
+        exceedance for which the loss computation is done)
+        a list of floats, and read it from the job configuration.
         """
 
         conditional_loss_poes = [float(x) for x in self.params.get(
                     'CONDITIONAL_LOSS_POE', "0.01").split()]
         self.slice_gmfs(block_id)
 
-        #pylint: disable=W0201 
+        #pylint: disable=W0201
         self.vuln_curves = \
                 vulnerability.load_vuln_model_from_kvs(self.job_id)
 
         # TODO(jmc): DONT assumes that hazard and risk grid are the same
         block = job.Block.from_kvs(block_id)
-        
+
         for point in block.grid(self.region):
-            key = kvs.generate_product_key(self.job_id, 
+            key = kvs.generate_product_key(self.job_id,
                 kvs.tokens.GMF_KEY_TOKEN, point.column, point.row)
             gmf_slice = kvs.get_value_json_decoded(key)
-            
+
             asset_key = kvs.tokens.asset_key(self.id, point.row, point.column)
             asset_list = kvs.get_client().lrange(asset_key, 0, -1)
             for asset in [json.JSONDecoder().decode(x) for x in asset_list]:
@@ -181,26 +182,29 @@ class ProbabilisticEventMixin:
 
                     # compute loss curve
                     loss_curve = self.compute_loss_curve(
-                            point.column, point.row, 
+                            point.column, point.row,
                             loss_ratio_curve, asset)
-                    
+
                     for loss_poe in conditional_loss_poes:
                         self.compute_conditional_loss(point.column, point.row,
                                 loss_curve, asset, loss_poe)
         return True
-    
+
     def compute_conditional_loss(self, col, row, loss_curve, asset, loss_poe):
-        """ Compute the conditional loss for a loss curve and probability of 
+        """ Compute the conditional loss for a loss curve and probability of
         exceedance """
 
-        loss_conditional = common.compute_conditional_loss(loss_curve, loss_poe)
-        key = kvs.tokens.loss_key(self.id, row, col, asset["assetID"], loss_poe)
+        loss_conditional = common.compute_conditional_loss(
+                loss_curve, loss_poe)
+
+        key = kvs.tokens.loss_key(
+                self.id, row, col, asset["assetID"], loss_poe)
 
         LOGGER.debug("RESULT: conditional loss is %s, write to key %s" % (
             loss_conditional, key))
         kvs.set(key, loss_conditional)
 
-    def compute_loss_ratio_curve(self, col, row, asset, gmf_slice ): # site_id
+    def compute_loss_ratio_curve(self, col, row, asset, gmf_slice): # site_id
         """Compute the loss ratio curve for a single site."""
         # If the asset has a vuln function code we don't have loaded, return
         # fail
@@ -217,10 +221,10 @@ class ProbabilisticEventMixin:
         if not False in (loss_ratio_curve.ordinates == 0.0):
             return None
         key = kvs.tokens.loss_ratio_key(self.id, row, col, asset["assetID"])
-        
+
         LOGGER.warn("RESULT: loss ratio curve is %s, write to key %s" % (
                 loss_ratio_curve, key))
-            
+
         kvs.set(key, loss_ratio_curve.to_json())
         return loss_ratio_curve
 
@@ -228,7 +232,7 @@ class ProbabilisticEventMixin:
         """Compute the loss curve for a single site."""
         if asset is None:
             return None
-        
+
         loss_curve = loss_ratio_curve.rescale_abscissae(asset["assetValue"])
         key = kvs.tokens.loss_curve_key(self.id, row, column, asset["assetID"])
 
@@ -245,7 +249,7 @@ class ProbabilisticEventMixin:
         In the opposite case ("perfectly correlated" assets) we sample for each
         building typology i.e. two assets with the same typology will "share"
         the same standard normal distribution sample.
-        
+
         Two assets are considered to be of the same building typology if their
         structure category is the same. The asset's `structureCategory` is
         only needed for correlated jobs and unlikely to be available for
