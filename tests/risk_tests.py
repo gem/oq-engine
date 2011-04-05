54047e77cd2d55d3e481762feb799efe6457eb40
root <root@roundabout.(none)>
2011-04-05 15:17:29 +0000

    Squashed commit of the following:
    commit cb3b6dd4bde387b4ce5d208310d047cb5f2f9231
    Author: Andrea Cerisara <andreacerisara@gmail.com>
    Date:   Tue Apr 5 14:44:17 2011 +0200
    
        Fixed test, doc
    
    commit 54278d4080d997c1eabc3e7cac1d5455d656450d
    Author: Andrea Cerisara <andreacerisara@gmail.com>
    Date:   Tue Apr 5 14:16:42 2011 +0200
    
        Improved doc
    
    commit 35621aaa7981d13196de5b29e2eaf969f62f3b9d
    Author: Andrea Cerisara <andreacerisara@gmail.com>
    Date:   Tue Apr 5 13:44:18 2011 +0200
    
        Added doc
    
    commit ad70987e80e6ae220ff68864a5afa01e2f1348d8
    Author: Andrea Cerisara <andreacerisara@gmail.com>
    Date:   Tue Apr 5 11:43:46 2011 +0200
    
        Updated test cases
    
    commit e202eea70ca0d74935feced63bc47cb530dd96df
    Author: Andrea Cerisara <andreacerisara@gmail.com>
    Date:   Mon Apr 4 19:15:07 2011 +0200
    
        First implementation of the mean/stddev losses per ground motion field
    
    
    Former-commit-id: 687e0a0061879906bd283cf1a4f2a09eef257fb5

--- tests/risk_tests.py
+++ tests/risk_tests.py
@@ -1128,3 +1128,94 @@ class DeterministicEventBasedTestCase(unittest.TestCase):
         self.assertTrue(numpy.allclose([1.631],
                         det.compute_stddev_loss(self.vuln_function, self.gmfs,
                         epsilon_provider, asset), atol=0.002))
+
+    def test_calls_the_loss_ratios_calculator_correctly(self):
+        gmfs = {"IMLs": ()}
+        epsilon_provider = object()
+        vuln_model = {"ID": self.vuln_function}
+        asset = {"assetValue": 10, "vulnerabilityFunctionReference": "ID"}
+
+        def loss_ratios_calculator(
+            vuln_function, ground_motion_field_set, epsilon_provider, asset):
+
+            self.assertTrue(asset == asset)
+            self.assertTrue(epsilon_provider == epsilon_provider)
+            self.assertTrue(ground_motion_field_set == gmfs)
+            self.assertTrue(vuln_function == self.vuln_function)
+
+            return numpy.array([])
+
+        calculator = det.SumPerGroundMotionField(
+            vuln_model, epsilon_provider, lr_calculator=loss_ratios_calculator)
+
+        calculator.add(gmfs, asset)
+
+    def test_keeps_track_of_the_sum_of_the_losses(self):
+        loss_ratios = [
+            [0.140147324, 0.151530140, 0.016176042, 0.101786402, 0.025190577],
+            [0.154760019, 0.001203867, 0.370820698, 0.220145117, 0.067291408],
+            [0.010945875, 0.413257970, 0.267141193, 0.040157738, 0.001981645]]
+
+        def loss_ratios_calculator(
+            vuln_function, ground_motion_field_set, epsilon_provider, asset):
+
+            return loss_ratios.pop(0)
+
+        vuln_model = {"ID": self.vuln_function}
+        asset = {"assetValue": 100, "vulnerabilityFunctionReference": "ID"}
+
+        calculator = det.SumPerGroundMotionField(
+            vuln_model, None, lr_calculator=loss_ratios_calculator)
+
+        self.assertEqual(None, calculator.losses)
+
+        calculator.add(None, asset)
+        asset = {"assetValue": 300, "vulnerabilityFunctionReference": "ID"}
+        calculator.add(None, asset)
+        asset = {"assetValue": 200, "vulnerabilityFunctionReference": "ID"}
+        calculator.add(None, asset)
+
+        expected_sum = [62.63191284, 98.16576808,
+                        166.2920523, 84.25372286, 23.10280904]
+
+        self.assertTrue(numpy.allclose(expected_sum, calculator.losses))
+
+    def test_computes_the_mean_from_the_current_sum(self):
+        calculator = det.SumPerGroundMotionField(None, None)
+
+        sum_of_losses = numpy.array(
+            [62.63191284, 98.16576808, 166.2920523, 84.25372286, 23.10280904])
+
+        calculator.losses = sum_of_losses
+
+        self.assertTrue(numpy.allclose([86.88925302], calculator.mean))
+
+    def test_computes_the_stddev_from_the_current_sum(self):
+        calculator = det.SumPerGroundMotionField(None, None)
+
+        sum_of_losses = numpy.array(
+            [62.63191284, 98.16576808, 166.2920523, 84.25372286, 23.10280904])
+
+        calculator.losses = sum_of_losses
+
+        self.assertTrue([52.66886967], calculator.stddev)
+
+    def test_skips_the_distribution_with_unknown_vuln_function(self):
+        """The asset refers to an unknown vulnerability function.
+
+        In case the asset defines an unknown vulnerability function
+        (key 'vulnerabilityFunctionReference') the given ground
+        motion field set is ignored.
+        """
+        vuln_model = {"ID": self.vuln_function}
+        asset = {"assetValue": 100, "assetID": "ID",
+                 "vulnerabilityFunctionReference": "XX"}
+
+        calculator = det.SumPerGroundMotionField(vuln_model, None)
+
+        self.assertEqual(None, calculator.losses)
+
+        calculator.add(None, asset)
+
+        # still None, no losses are added
+        self.assertEqual(None, calculator.losses)
