58a402d3d1309b428aac1dc2394493339dae1e2d
root <root@roundabout.(none)>
2011-04-11 13:43:07 +0000

    Squashed commit of the following:
    commit b108ef8152860328a9f513dc812803e5fdccf619
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Mon Apr 11 15:30:57 2011 +0200
    
        removed breakpoint
    
    commit 5e7a562936cd04bba1f20a225eef725e4651594f
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Sat Apr 9 08:57:36 2011 +0200
    
        added breakpoint in failing code
    
    commit 6f1a47911d98e0ea89493c665b15e961dfb9a6e9
    Merge: 1b1de0f df5e5a8
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Fri Apr 8 15:06:44 2011 +0200
    
        Merge branch 'master' into version-info
    
    commit 1b1de0f365a63db2c1b833ea486d4c69f7f680a0
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Fri Apr 8 10:48:52 2011 +0200
    
        pylint fix, ctd.
    
    commit f7844fe30566658dad5e20eb47f8effefb034c33
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Fri Apr 8 10:46:50 2011 +0200
    
        pylint fix
    
    commit 2d1db27672680af8be3a10dab36a4cb3a6b5cd04
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Fri Apr 8 10:45:31 2011 +0200
    
        minor fixes
    
    commit 500fb00e07af62109cd69ebeb7c462df8bb9819d
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Fri Apr 8 10:25:28 2011 +0200
    
        bin/openquake --version
    
    commit 0a2b898a54b046d5903ec1f0a6e47e3fd52db01a
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Fri Apr 8 09:53:36 2011 +0200
    
        PEP8 fixes
    
    commit 3431245d6da6090d3d983003a20dd94f807488ef
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Fri Apr 8 09:42:44 2011 +0200
    
        implementation complete
    
    commit 8d06fc0c25cd6e897ff2caee1438c7db5509e925
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Fri Apr 8 08:23:33 2011 +0200
    
        implemented everything but the release date.
    
    commit 3bb1be4615bd8d15843ff823102563add7dee385
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Fri Apr 8 08:14:41 2011 +0200
    
        implemented first checks.
    
    commit 3c1b88af2898c9365ac8a76ea06088d19ef91581
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Fri Apr 8 08:07:30 2011 +0200
    
        added utils.version module and tests
    
    commit ecf81c86f408d182560d92d29564c466bf191bd2
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Fri Apr 8 07:36:16 2011 +0200
    
        added version info tuple

---
+++ openquake/utils/version.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright (c) 2010-2011, GEM Foundation.
+#
+# OpenQuake is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3
+# only, as published by the Free Software Foundation.
+#
+# OpenQuake is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License version 3 for more details
+# (a copy is included in the LICENSE file that accompanied this code).
+#
+# You should have received a copy of the GNU Lesser General Public License
+# version 3 along with OpenQuake.  If not, see
+# <http://www.gnu.org/licenses/lgpl-3.0.txt> for a copy of the LGPLv3 License.
+
+
+"""
+Utility functions related to OpenQuake version information.
+"""
+
+
+from datetime import datetime
+from datetime import timedelta
+
+
+def info(version_data):
+    """Return a string with the OpenQuake version infomation.
+
+    Version info data set to -1 will be ignored and assumed to have value
+    zero.
+    Release dates that lie more than 30 days in the future are ignored.
+
+    :param version_data: A 4-tuple of integers that are the major, minor and
+        sprint number respectively. The last datum is the number of seconds
+        since epoch and represents the release date.
+
+    :returns: A string with human readable OpenQuake version information.
+    """
+    error = "The OpenQuake version is not available."
+    if not isinstance(version_data, tuple) or len(version_data) != 4:
+        return error
+
+    data = []
+    for datum in version_data:
+        if not isinstance(datum, int):
+            return error
+        if datum < -1:
+            return error
+        data.append(str(datum if datum > 0 else 0))
+
+    result = "OpenQuake version %s" % ".".join(data[:3])
+
+    seconds_since_epoch = version_data[-1]
+
+    # Our versioning start on the date below.
+    start = int(datetime(year=2011, month=4, day=8).strftime("%s"))
+    # The release date should not be more than 30 days in the future.
+    end = int((datetime.today() + timedelta(days=30)).strftime("%s"))
+
+    if end > seconds_since_epoch > start:
+        release_date = datetime.fromtimestamp(seconds_since_epoch).isoformat()
+        result += ", released %s" % release_date
+
+    return result
