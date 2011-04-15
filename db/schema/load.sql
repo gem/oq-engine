0dbd2a0f56001f6d29aa016622d1457f7df02b20
Muharem Hrnjadovic <mh@foldr3.com>
2011-04-15 08:40:42 +0200

    static data is now defined in a separate file.

---
+++ db/schema/load.sql
@@ -0,0 +1,26 @@
+/*
+  Static data for the OpenQuake database schema.
+
+    Copyright (c) 2010-2011, GEM Foundation.
+
+    OpenQuake is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License version 3
+    only, as published by the Free Software Foundation.
+
+    OpenQuake is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License version 3 for more details
+    (a copy is included in the LICENSE file that accompanied this code).
+
+    You should have received a copy of the GNU Lesser General Public License
+    version 3 along with OpenQuake.  If not, see
+    <http://www.gnu.org/licenses/lgpl-3.0.txt> for a copy of the LGPLv3 License.
+*/
+
+
+INSERT INTO pshai.tectonic_region(name) VALUES('Active Shallow Crust');
+INSERT INTO pshai.tectonic_region(name) VALUES('Stable Shallow Crust');
+INSERT INTO pshai.tectonic_region(name) VALUES('Subduction Interface');
+INSERT INTO pshai.tectonic_region(name) VALUES('Subduction IntraSlab');
+INSERT INTO pshai.tectonic_region(name) VALUES('Volcanic');
