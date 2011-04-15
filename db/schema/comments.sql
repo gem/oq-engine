716627047d3db20bbfa11eeec06788ad960263ca
Muharem Hrnjadovic <mh@foldr3.com>
2011-04-15 08:33:40 +0200

    improved db creation script, comments file.

---
+++ db/schema/comments.sql
@@ -0,0 +1,55 @@
+/*
+  Add Comments to the OpenQuake database. Please keep these alphabetical by
+  table.
+
+    Copyright (c) 2010-2011, GEM Foundation.
+
+    OpenQuake is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License version 3
+    only, as published by the Free Software Foundation.
+
+    OpenQuake is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License version 3 for more details
+    (a copy is included in the LICENSE file that accompanied this code).
+
+    You should have received a copy of the GNU Lesser General Public License
+    version 3 along with OpenQuake.  If not, see
+    <http://www.gnu.org/licenses/lgpl-3.0.txt> for a copy of the LGPLv3 License.
+*/
+
+-- Complex (fault) geometry
+COMMENT ON TABLE pshai.complex_geom IS 'A complex (fault) geometry, in essence a sequence of fault edges.';
+COMMENT ON COLUMN pshai.complex_geom.gid IS 'An alpha-numeric identifier for this complex fault geometry.';
+
+-- Fault edge
+COMMENT ON TABLE pshai.fault_edge IS 'Part of a complex (fault) geometry, describes the top and the bottom seismic edges.';
+
+-- Magnitude frequency distribution
+COMMENT ON TABLE pshai.mfd IS 'Magnitude frequency distribution.';
+
+-- Magnitude frequency distribution, evenly discretized
+COMMENT ON TABLE pshai.mfd_evd IS 'Magnitude frequency distribution, evenly discretized.';
+
+-- Magnitude frequency distribution, truncated Gutenberg-Richter
+COMMENT ON TABLE pshai.mfd_tgr IS 'Magnitude frequency distribution, truncated Gutenberg-Richter.';
+
+-- Rupture depth distribution
+COMMENT ON TABLE pshai.rdd IS 'Rupture depth distribution.';
+
+-- Rupture
+COMMENT ON TABLE pshai.rupture IS 'A rupture, can be complex or simple.';
+
+-- Simple (fault) geometry
+COMMENT ON TABLE pshai.simple_geom IS 'A simple fault geometry.';
+
+-- Fault source
+COMMENT ON TABLE pshai.source IS 'A fault source, can be complex or simple.';
+
+-- Strike, dip and rake
+COMMENT ON TABLE pshai.strike_dip_rake IS 'Holds strike, dip and rake values with the respective constraints.';
+
+-- Tectonic region (enumeration)
+COMMENT ON TABLE pshai.tectonic_region IS 'An enumeration of known tectonic region values';
+
