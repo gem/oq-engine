8b56c67286d7a068219bfd4020654444a4b36a90
root <root@roundabout.(none)>
2011-04-20 11:23:34 +0000

    Squashed commit of the following:
    commit 00650a7b8aa92439a7cd1be238bfd9a001aef541
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Wed Apr 20 13:08:16 2011 +0200
    
        added the 'data_is_open' flag
    
    commit f80e4f33c400934d3d6e606c93600403e3aab532
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Wed Apr 20 10:24:22 2011 +0200
    
        added more comments
    
    commit d3231a968fa53685538e2e2e99ee58dab63d40ab
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Wed Apr 20 09:47:59 2011 +0200
    
        No oq user inserts in static/enum tables allowed
    
    commit e1c71fdfb912d514bb7b8901c73f812f3a7ff904
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Wed Apr 20 09:45:58 2011 +0200
    
        add comment
    
    commit e1b9ec16dfb5e9a645f5155b77452f076e7768c8
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Wed Apr 20 09:38:24 2011 +0200
    
        added index for the 'owner_id' foreign key
    
    commit b018a633b338d062a0815aa10900509275db808a
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Wed Apr 20 09:34:54 2011 +0200
    
        added missing FK
    
    commit 8ebb183af9d35725c5e6dd6d56397ac38496d0ba
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Wed Apr 20 09:25:19 2011 +0200
    
        added unique index on oq_user.user_name
    
    commit be2c9637905388534581ae90706487cca47560d8
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Wed Apr 20 09:21:19 2011 +0200
    
        added missing privileges
    
    commit 5ef30de96b43ee3375d0778f1838fb4aa28a9e1d
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Wed Apr 20 09:02:31 2011 +0200
    
        added reader roles
    
    commit 05978a7267dfadc3a776b46b3a4c7321aada50d6
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Wed Apr 20 08:57:26 2011 +0200
    
        a word of caution
    
    commit c5f039776bd3524045a8369ef9f27a69ae1d2047
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Wed Apr 20 08:54:03 2011 +0200
    
        openquake db indexes are applied
    
    commit 8cca7182f7db487aabf929efabacd156906b5a95
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Wed Apr 20 08:16:03 2011 +0200
    
        openquake db roles are created, privileges granted.
    
    commit 034bb0ff4dfcc487ad9f58928d3befc3683b20a4
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Wed Apr 20 07:52:12 2011 +0200
    
        started adding access permissions
    
    commit a19e287492a441fb333a15b61ef166d7e35ca02e
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Wed Apr 20 07:32:03 2011 +0200
    
        add comments
    
    commit 127c1ea1a573bf64f8bda894fa75ea70eaf24b0d
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Wed Apr 20 07:21:26 2011 +0200
    
        add missing foreign key constraint

---
+++ db/schema/indexes.sql
@@ -0,0 +1,50 @@
+/*
+  Indexes for the OpenQuake database.
+
+    Copyright (c) 2010-2011, GEM Foundation.
+
+    OpenQuake is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License version 3
+    only, as published by the Free Software Foundation.
+
+    OpenQuake is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License version 3 for more details
+   a copy is included in the LICENSE file that accompanied this code).
+
+    You should have received a copy of the GNU Lesser General Public License
+    version 3 along with OpenQuake.  If not, see
+    <http://www.gnu.org/licenses/lgpl-3.0.txt> for a copy of the LGPLv3 License.
+*/
+
+-- admin.oq_user
+CREATE UNIQUE INDEX admin_oq_user_user_name_uniq_idx ON admin.oq_user(user_name);
+
+-- pshai.fault_edge
+CREATE INDEX pshai_fault_edge_bottom_idx ON pshai.fault_edge USING gist(bottom);
+CREATE INDEX pshai_fault_edge_top_idx ON pshai.fault_edge USING gist(top);
+
+-- pshai.rupture
+CREATE INDEX pshai_rupture_point_idx ON pshai.rupture USING gist(point);
+
+-- pshai.simple_fault
+CREATE INDEX pshai_simple_fault_geom_idx ON pshai.simple_fault USING gist(geom);
+
+-- pshai.source
+CREATE INDEX pshai_source_area_idx ON pshai.source USING gist(area);
+CREATE INDEX pshai_source_point_idx ON pshai.source USING gist(point);
+
+-- index for the 'owner_id' foreign key
+CREATE INDEX pshai_complex_fault_owner_id_idx on pshai.complex_fault(owner_id);
+CREATE INDEX pshai_fault_edge_owner_id_idx on pshai.fault_edge(owner_id);
+CREATE INDEX pshai_focal_mechanism_owner_id_idx on pshai.focal_mechanism(owner_id);
+CREATE INDEX pshai_magnitude_type_owner_id_idx on pshai.magnitude_type(owner_id);
+CREATE INDEX pshai_mfd_evd_owner_id_idx on pshai.mfd_evd(owner_id);
+CREATE INDEX pshai_mfd_tgr_owner_id_idx on pshai.mfd_tgr(owner_id);
+CREATE INDEX pshai_r_depth_distr_owner_id_idx on pshai.r_depth_distr(owner_id);
+CREATE INDEX pshai_r_rate_mdl_owner_id_idx on pshai.r_rate_mdl(owner_id);
+CREATE INDEX pshai_rupture_owner_id_idx on pshai.rupture(owner_id);
+CREATE INDEX pshai_simple_fault_owner_id_idx on pshai.simple_fault(owner_id);
+CREATE INDEX pshai_source_owner_id_idx on pshai.source(owner_id);
+CREATE INDEX pshai_tectonic_region_owner_id_idx on pshai.tectonic_region(owner_id);
