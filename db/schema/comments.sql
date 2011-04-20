ee94f0dbd6de0cf74015720b10bbb5756a676f65
root <root@roundabout.(none)>
2011-04-20 11:23:34 +0000

    Squashed commit of the following:
    commit 00650a7b8aa92439a7cd1be238bfd9a001aef541
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Wed Apr 20 13:08:16 2011 +0200
    
        added the 'data_is_open' flag
    
    commit f80e4f33c400934d3d6e606c93600403e3aab532
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Wed Apr 20 10:24:22 2011 +0200
    
        added more comments
    
    commit d3231a968fa53685538e2e2e99ee58dab63d40ab
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Wed Apr 20 09:47:59 2011 +0200
    
        No oq user inserts in static/enum tables allowed
    
    commit e1c71fdfb912d514bb7b8901c73f812f3a7ff904
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Wed Apr 20 09:45:58 2011 +0200
    
        add comment
    
    commit e1b9ec16dfb5e9a645f5155b77452f076e7768c8
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Wed Apr 20 09:38:24 2011 +0200
    
        added index for the 'owner_id' foreign key
    
    commit b018a633b338d062a0815aa10900509275db808a
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Wed Apr 20 09:34:54 2011 +0200
    
        added missing FK
    
    commit 8ebb183af9d35725c5e6dd6d56397ac38496d0ba
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Wed Apr 20 09:25:19 2011 +0200
    
        added unique index on oq_user.user_name
    
    commit be2c9637905388534581ae90706487cca47560d8
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Wed Apr 20 09:21:19 2011 +0200
    
        added missing privileges
    
    commit 5ef30de96b43ee3375d0778f1838fb4aa28a9e1d
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Wed Apr 20 09:02:31 2011 +0200
    
        added reader roles
    
    commit 05978a7267dfadc3a776b46b3a4c7321aada50d6
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Wed Apr 20 08:57:26 2011 +0200
    
        a word of caution
    
    commit c5f039776bd3524045a8369ef9f27a69ae1d2047
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Wed Apr 20 08:54:03 2011 +0200
    
        openquake db indexes are applied
    
    commit 8cca7182f7db487aabf929efabacd156906b5a95
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Wed Apr 20 08:16:03 2011 +0200
    
        openquake db roles are created, privileges granted.
    
    commit 034bb0ff4dfcc487ad9f58928d3befc3683b20a4
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Wed Apr 20 07:52:12 2011 +0200
    
        started adding access permissions
    
    commit a19e287492a441fb333a15b61ef166d7e35ca02e
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Wed Apr 20 07:32:03 2011 +0200
    
        add comments
    
    commit 127c1ea1a573bf64f8bda894fa75ea70eaf24b0d
    Author: Muharem Hrnjadovic <mh@foldr3.com>
    Date:   Wed Apr 20 07:21:26 2011 +0200
    
        add missing foreign key constraint
    
    
    Former-commit-id: 347d8d6a53c0f5272bf273016b3ff0bccb7393dd [formerly 8b56c67286d7a068219bfd4020654444a4b36a90]
    Former-commit-id: de6a101837a729862a75dcd60fe67f9857cb428b

--- db/schema/comments.sql
+++ db/schema/comments.sql
@@ -1,6 +1,6 @@
 /*
-  Add Comments to the OpenQuake database. Please keep these alphabetical by
-  table.
+  Documentation for the OpenQuake database schema.
+  Please keep these alphabetical by table.
 
     Copyright (c) 2010-2011, GEM Foundation.
 
@@ -21,6 +21,7 @@
 
 COMMENT ON TABLE admin.organization IS 'An organization that is utilising the OpenQuake database';
 COMMENT ON TABLE admin.oq_user IS 'An OpenQuake user that is utilising the OpenQuake database';
+COMMENT ON COLUMN admin.oq_user.data_is_open IS 'Whether the data owned by the user is visible to the general public.';
 
 COMMENT ON TABLE pshai.complex_fault IS 'A complex (fault) geometry, in essence a sequence of fault edges.';
 COMMENT ON COLUMN pshai.complex_fault.gid IS 'An alpha-numeric identifier for this complex fault geometry.';
@@ -45,7 +46,8 @@ COMMENT ON TABLE pshai.r_depth_distr IS 'Rupture depth distribution.';
 
 COMMENT ON TABLE pshai.r_rate_mdl IS 'Rupture rate model.';
 
-COMMENT ON TABLE pshai.rupture IS 'A rupture, can be complex or simple.';
+COMMENT ON TABLE pshai.rupture IS 'A rupture, can be based on a point or a complex or simple fault.';
+COMMENT ON COLUMN pshai.rupture.si_type IS 'The rupture''s seismic input type: can be one of: point, complex or simple.';
 
 COMMENT ON TABLE pshai.simple_fault IS 'A simple fault geometry.';
 COMMENT ON COLUMN pshai.simple_fault.dip IS 'The fault''s inclination angle with respect to the plane.';
@@ -53,6 +55,7 @@ COMMENT ON COLUMN pshai.simple_fault.upper_depth IS 'The upper seismogenic depth
 COMMENT ON COLUMN pshai.simple_fault.lower_depth IS 'The lower seismogenic depth.';
 COMMENT ON COLUMN pshai.simple_fault.outline IS 'The outline of the fault surface, computed by using the dip and the upper/lower seismogenic depth.';
 
-COMMENT ON TABLE pshai.source IS 'A fault source, can be complex or simple.';
+COMMENT ON TABLE pshai.source IS 'A seismic source, can be based on a point, area or a complex or simple fault.';
+COMMENT ON COLUMN pshai.source.si_type IS 'The source''s seismic input type: can be one of: area, point, complex or simple.';
 
 COMMENT ON TABLE pshai.tectonic_region IS 'Enumeration of tectonic region types';
