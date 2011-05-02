597ce1025a9e508ad04714b39defeb73ff10811d
Lars Butler <Lars.Butler@gmail.com>
2011-05-02 19:13:05 +0200

    started filling out simple fault source reader stuff
    it's more complicated than I thought
    
    
    Former-commit-id: 2a266dea25b64eb788359db7dfaa2b28a500b322 [formerly 7df5815d0f57c647ff3e20738f3d396be03ecdbc] [formerly 7df5815d0f57c647ff3e20738f3d396be03ecdbc [formerly 0974b653804f8d857c41ecd976c382ceb2e23df9]]
    Former-commit-id: e4bbcaaa67d29cbd6c1f6056ae931107c9e89417
    Former-commit-id: 1a6fcce5e978b2b952557c437da0952b2052598e

--- openquake/utils/db/__init__.py
+++ openquake/utils/db/__init__.py
@@ -1,11 +1,43 @@
-from sqlalchemy.databases import postgres
+import sqlalchemy
 import geoalchemy
+
+from sqlalchemy.databases import postgres
 
 # This allows us to reflect 'geometry' columns from PostGIS tables.
 # This is slightly hack-ish, but it's either this or we have to declare
 # columns with the appropriate type manually (yuck).
 postgres.ischema_names['geometry'] = geoalchemy.Geometry
 
+# Tablespaces
+PSHAI_TS = 'pshai'
+ADMIN_TS = 'admin'
+EQCAT_TS = 'eqcat'
+
+
+# Table/column dicts.
+# These can be used as a template for doing db inserts.
+SIMPLE_FAULT = dict.fromkeys(
+    # required:
+    'owner_id', 'gid', 'dip', 'upper_depth', 'lower_depth', 'geom',
+    # xor:
+    'mfd_tgr_id', 'mgf_evd_id',
+    # optional:
+    'name', 'description', 'outline')
+SOURCE = dict.fromkeys(
+    # required:
+    'owner_id', 'simple_fault_id', 'gid', 'si_type', 'tectonic_region',
+    # optional:
+    'name', 'description', 'rake', 'hypocentral_depth')
+MFD_EVD = dict.fromkeys(
+    # required:
+    'owner_id', 'gid', 'magnitude_type', 'min_val', 'bin_size', 'mfd_values'
+    # optional:
+    'name', 'description', 'total_cumulative_rate', 'total_moment_rate')
+MFD_TGR = dict.fromkeys(
+    # required:
+    'owner_id', 'gid', 'magnitude_type', 'min_val', 'max_val', 'a_val', 'b_val'
+    # optional:
+    'name', 'description', 'total_cumulative_rate', 'total_moment_rate')
 
 
 def create_engine(
