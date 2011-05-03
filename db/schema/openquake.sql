4298256b075d6d50e41ca2c4b13572debaab03e3
Muharem Hrnjadovic <mh@foldr3.com>
2011-05-03 11:15:31 +0200

    Added a aximum magnitude value for evenly discretized magnitude frequency distributions
    Former-commit-id: 201ac403bcceeb174ab1169fb0432e1d9e6276be

--- db/schema/openquake.sql
+++ db/schema/openquake.sql
@@ -333,6 +333,10 @@ CREATE TABLE pshai.mfd_evd (
     magnitude_type VARCHAR(2) NOT NULL DEFAULT 'Mw' CONSTRAINT mage_type_val
         CHECK(magnitude_type IN ('Mb', 'Md', 'Ml', 'Ms', 'Mw')),
     min_val float NOT NULL,
+    -- The maximum magnitude value will be derived/calculated for evenly
+    -- discretized magnitude frequency distributions.
+    -- It is initialized with a value that should never occur in practice.
+    max_val float NOT NULL DEFAULT -1.0,
     bin_size float NOT NULL,
     mfd_values float[] NOT NULL,
     total_cumulative_rate float,
