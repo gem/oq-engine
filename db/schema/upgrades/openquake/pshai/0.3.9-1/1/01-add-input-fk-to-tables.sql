30bfa1bf27f590b104fd39fa30f8c620c8db8f38
Muharem Hrnjadovic <mh@foldr3.com>
2011-05-06 08:57:16 +0200

    add schema upgrade patches
    Former-commit-id: 6a8bac262415ba305718e361c049981b1f126d03

---
+++ db/schema/upgrades/openquake/pshai/0.3.9-1/1/01-add-input-fk-to-tables.sql
@@ -0,0 +1,13 @@
+-- pshai.source and pshai.rupture need to be associated with uiapi.input
+-- This patch adds the needed foreign keys.
+
+ALTER TABLE pshai.rupture ADD COLUMN input_id INTEGER;
+ALTER TABLE pshai.source ADD COLUMN input_id INTEGER;
+
+ALTER TABLE pshai.source ADD CONSTRAINT pshai_source_input_fk
+FOREIGN KEY (input_id) REFERENCES uiapi.input(id) ON DELETE RESTRICT;
+
+ALTER TABLE pshai.rupture ADD CONSTRAINT pshai_rupture_input_fk
+FOREIGN KEY (input_id) REFERENCES uiapi.input(id) ON DELETE RESTRICT;
+
+
