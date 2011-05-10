cc85729be8ed6f31d50159784910eb2c63b29c4c
Muharem Hrnjadovic <mh@foldr3.com>
2011-05-10 15:17:47 +0200

    more fixes
    Former-commit-id: e8b9550d2f4b1b42a320707f00e71bae5666451d [formerly 91771c4a2bc5145e4a9b443819dba93a6a79bc39]
    Former-commit-id: df2aabf37415bc10deeb4c74ba21ad5a52707421

--- db/schema/upgrades/openquake/uiapi/0.3.9-1/2/01-upload-status.sql
+++ db/schema/upgrades/openquake/uiapi/0.3.9-1/2/01-upload-status.sql
@@ -21,8 +21,9 @@
 -- Rename the 'status_value' constraint in uiapi.oq_job first.
 ALTER TABLE uiapi.oq_job DROP CONSTRAINT status_value;
 ALTER TABLE uiapi.oq_job ADD CONSTRAINT job_status_value
-        CHECK(status IN ('created', 'in-progress', 'failed', 'succeeded')),
+    CHECK(status IN ('created', 'in-progress', 'failed', 'succeeded'));
 
 -- Now add the status to the uiapi.upload table.
-ALTER TABLE uiapi.upload ADD CONSTRAINT upload_status_value
-        CHECK(status IN ('created', 'in-progress', 'failed', 'succeeded')),
+ALTER TABLE uiapi.upload ADD COLUMN
+    status VARCHAR NOT NULL DEFAULT 'created' CONSTRAINT upload_status_value
+        CHECK(status IN ('created', 'in-progress', 'failed', 'succeeded'));
