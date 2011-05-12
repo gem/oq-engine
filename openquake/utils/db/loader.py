dcb0bdae2540071981df1b27bbcafa8ca372d121
Lars Butler <Lars.Butler@gmail.com>
2011-05-12 14:15:58 +0200

    outline polygons were being plotted incorrectly
    Former-commit-id: d1efb2e25dce8cd536f69e0c52125243de55971c [formerly d1efb2e25dce8cd536f69e0c52125243de55971c [formerly 73f539fc36c778369f25f0ba8686795e8205ecf3]]
    Former-commit-id: 5bc4a6503b620c097b18fe0f500c7bffb4331265
    Former-commit-id: 9d3306084b2fb26d2969d36f456c5d1d3aa17548

--- openquake/utils/db/loader.py
+++ openquake/utils/db/loader.py
@@ -265,11 +265,11 @@ def parse_simple_fault_src(fault):
 
         surface = get_fault_surface(fault)
 
-        location_list = surface.getLocationList()
+        location_list = surface.getSurfacePerimeterLocsList()
 
         # polygon coordinates need to form a closed loop
         # the first point should also be the last point
-        location_list.add(location_list.get(0))
+        # location_list.add(location_list.get(0))
 
         formatter = java.jclass("LocationListFormatter")(location_list)
 
