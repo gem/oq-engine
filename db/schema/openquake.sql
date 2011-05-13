8056607b3cfea33b26bcbe700f6115743523ff56
Marco Milanesi <marcom@openquake.org>
2011-05-13 15:12:00 +0200

    added a global view for the eq catalog
    changed accordingly sql creation scripts and upgrade scripts
    
    
    Former-commit-id: 9952f31ea569636397a562dca55bb3cca8da96bc [formerly 9952f31ea569636397a562dca55bb3cca8da96bc [formerly f4e6f20b9d95885b42223d287c6970d05a869fa4]]
    Former-commit-id: 803c5faaee589a747adfa3af9ad324d10b4e8b73
    Former-commit-id: b9399adebfacc1e8e4838eaa4947a130066c8607

--- db/schema/openquake.sql
+++ db/schema/openquake.sql
@@ -127,6 +127,21 @@ CREATE TABLE eqcat.surface (
         DEFAULT timezone('UTC'::text, now()) NOT NULL
 ) TABLESPACE eqcat_ts;
 
+-- global catalog view, needed for Geonode integration
+CREATE VIEW eqcat.catalog_allfields AS
+SELECT
+    eqcat.catalog.*,
+    eqcat.surface.semi_minor, eqcat.surface.semi_major,
+    eqcat.surface.strike,
+    eqcat.magnitude.mb_val, eqcat.magnitude.mb_val_error,
+    eqcat.magnitude.ml_val, eqcat.magnitude.ml_val_error,
+    eqcat.magnitude.ms_val, eqcat.magnitude.ms_val_error, 
+    eqcat.magnitude.mw_val, eqcat.magnitude.mw_val_error 
+FROM
+    eqcat.catalog
+JOIN eqcat.magnitude ON eqcat.catalog.magnitude_id = eqcat.magnitude.id
+JOIN eqcat.surface ON eqcat.catalog.surface_id = eqcat.surface.id;
+
 
 -- rupture
 CREATE TABLE pshai.rupture (
