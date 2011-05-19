237aaeb205d73f3772c90b9fb9819c82481932cd
Marco Milanesi <marcom@openquake.org>
2011-05-19 16:12:13 +0200

    moved VIEW creation after mfd_evd/mfd_tgr CREATE TABLE
    Former-commit-id: 79c4391249808f6638f8732ac5a78f20233bd943 [formerly 79c4391249808f6638f8732ac5a78f20233bd943 [formerly e60db60566613cec4087e86829242f167de96128]]
    Former-commit-id: 2d6861dd6977a033a2a5d088cc8964453e3151af
    Former-commit-id: dae3869fc486466e7281816e85eaae2f4482d176

--- db/schema/openquake.sql
+++ db/schema/openquake.sql
@@ -247,42 +247,6 @@ CREATE TABLE pshai.simple_fault (
 SELECT AddGeometryColumn('pshai', 'simple_fault', 'edge', 4326, 'LINESTRING', 3);
 ALTER TABLE pshai.simple_fault ALTER COLUMN edge SET NOT NULL;
 SELECT AddGeometryColumn('pshai', 'simple_fault', 'outline', 4326, 'POLYGON', 3);
--- global simple_fault view, needed for Geonode integration
-CREATE VIEW pshai.simple_fault_allfields AS SELECT
-    pshai.simple_fault.id AS fault_id,
-    pshai.simple_fault.owner_id AS fault_owner_id,
-    pshai.simple_fault.gid AS fault_gid,
-    pshai.simple_fault.name AS fault_name,
-    pshai.simple_fault.description, pshai.simple_fault.dip,
-    pshai.simple_fault.upper_depth, pshai.simple_fault.lower_depth,
-    pshai.simple_fault.mfd_tgr_id, pshai.simple_fault.mfd_evd_id,
-    pshai.simple_fault.last_update AS fault_last_update,
-    pshai.simple_fault.edge, pshai.simple_fault.outline,
-    pshai.mfd_evd.id AS pshai_mfd_evd_id,
-    pshai.mfd_evd.owner_id AS pshai_mfd_evd_owner_id,
-    pshai.mfd_evd.magnitude_type AS pshai_mfd_evd_magnitude_type,
-    pshai.mfd_evd.min_val AS pshai_mfd_evd_min_val,
-    pshai.mfd_evd.max_val AS pshai_mfd_evd_max_val,
-    pshai.mfd_evd.bin_size AS pshai_mfd_evd_bin_size,
-    pshai.mfd_evd.mfd_values AS pshai_mfd_evd_pshai_mfd_values,
-    pshai.mfd_evd.total_cumulative_rate AS pshai_mfd_evd_total_cumulative_rate,
-    pshai.mfd_evd.total_moment_rate AS pshai_mfd_evd_total_moment_rate,
-    pshai.mfd_evd.last_update AS pshai_mfd_evd_last_update,
-    pshai.mfd_tgr.id AS pshai_mfd_tgr_id,
-    pshai.mfd_tgr.owner_id AS pshai_mfd_tgr_owner_id,
-    pshai.mfd_tgr.magnitude_type AS pshai_mfd_tgr_magnitude_type,
-    pshai.mfd_tgr.min_val AS pshai_mfd_tgr_min_val,
-    pshai.mfd_tgr.max_val AS pshai_mfd_tgr_max_val,
-    pshai.mfd_tgr.a_val AS pshai_mfd_tgr_a_val,
-    pshai.mfd_tgr.b_val AS pshai_mfd_tgr_b_val,
-    pshai.mfd_tgr.total_cumulative_rate AS pshai_mfd_tgr_total_cumulative_rate,
-    pshai.mfd_tgr.total_moment_rate AS pshai_mfd_tgr_total_moment_rate,
-    pshai.mfd_tgr.last_update AS pshai_mfd_tgr_last_update
-FROM
-     pshai.simple_fault
-LEFT OUTER JOIN pshai.mfd_evd ON pshai.mfd_evd.id = pshai.simple_fault.mfd_evd_id
-LEFT OUTER JOIN pshai.mfd_tgr ON pshai.mfd_tgr.id  = pshai.simple_fault.mfd_tgr_id;
-
 
 -- simple source view, needed for Opengeo server integration
 CREATE VIEW pshai.simple_source (
@@ -424,6 +388,43 @@ CREATE TABLE pshai.mfd_tgr (
     last_update timestamp without time zone
         DEFAULT timezone('UTC'::text, now()) NOT NULL
 ) TABLESPACE pshai_ts;
+
+-- global simple_fault view, needed for Geonode integration
+CREATE VIEW pshai.simple_fault_allfields AS
+SELECT
+    pshai.simple_fault.id AS fault_id,
+    pshai.simple_fault.owner_id AS fault_owner_id,
+    pshai.simple_fault.gid AS fault_gid,
+    pshai.simple_fault.name AS fault_name,
+    pshai.simple_fault.description, pshai.simple_fault.dip,
+    pshai.simple_fault.upper_depth, pshai.simple_fault.lower_depth,
+    pshai.simple_fault.mfd_tgr_id, pshai.simple_fault.mfd_evd_id,
+    pshai.simple_fault.last_update AS fault_last_update,
+    pshai.simple_fault.edge, pshai.simple_fault.outline,
+    pshai.mfd_evd.id AS pshai_mfd_evd_id,
+    pshai.mfd_evd.owner_id AS pshai_mfd_evd_owner_id,
+    pshai.mfd_evd.magnitude_type AS pshai_mfd_evd_magnitude_type,
+    pshai.mfd_evd.min_val AS pshai_mfd_evd_min_val,
+    pshai.mfd_evd.max_val AS pshai_mfd_evd_max_val,
+    pshai.mfd_evd.bin_size AS pshai_mfd_evd_bin_size,
+    pshai.mfd_evd.mfd_values AS pshai_mfd_evd_pshai_mfd_values,
+    pshai.mfd_evd.total_cumulative_rate AS pshai_mfd_evd_total_cumulative_rate,
+    pshai.mfd_evd.total_moment_rate AS pshai_mfd_evd_total_moment_rate,
+    pshai.mfd_evd.last_update AS pshai_mfd_evd_last_update,
+    pshai.mfd_tgr.id AS pshai_mfd_tgr_id,
+    pshai.mfd_tgr.owner_id AS pshai_mfd_tgr_owner_id,
+    pshai.mfd_tgr.magnitude_type AS pshai_mfd_tgr_magnitude_type,
+    pshai.mfd_tgr.min_val AS pshai_mfd_tgr_min_val,
+    pshai.mfd_tgr.max_val AS pshai_mfd_tgr_max_val,
+    pshai.mfd_tgr.a_val AS pshai_mfd_tgr_a_val,
+    pshai.mfd_tgr.b_val AS pshai_mfd_tgr_b_val,
+    pshai.mfd_tgr.total_cumulative_rate AS pshai_mfd_tgr_total_cumulative_rate,
+    pshai.mfd_tgr.total_moment_rate AS pshai_mfd_tgr_total_moment_rate,
+    pshai.mfd_tgr.last_update AS pshai_mfd_tgr_last_update
+FROM
+     pshai.simple_fault
+LEFT OUTER JOIN pshai.mfd_evd ON pshai.mfd_evd.id = pshai.simple_fault.mfd_evd_id
+LEFT OUTER JOIN pshai.mfd_tgr ON pshai.mfd_tgr.id  = pshai.simple_fault.mfd_tgr_id;
 
 
 -- Rupture depth distribution
