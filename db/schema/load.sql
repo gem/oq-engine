a6b56adbab88df4c2ef6f982ced7342091615aaa
Marco Milanesi <marcom@openquake.org>
2011-05-19 15:13:17 +0200

    added a new view for simple_fault that includes mfd_evd, mfd_tgr
    added updates/migration
    
    
    Former-commit-id: 9a146c913e8b0a5a06345e9ea6d4c5133d340678

--- db/schema/load.sql
+++ db/schema/load.sql
@@ -24,5 +24,5 @@ INSERT INTO admin.oq_user(user_name, full_name, organization_id) VALUES('openqua
 
 INSERT INTO admin.revision_info(artefact, revision) VALUES('openquake/admin', '0.3.9-1');
 INSERT INTO admin.revision_info(artefact, revision, step) VALUES('openquake/eqcat', '0.3.9-1', 1);
-INSERT INTO admin.revision_info(artefact, revision, step) VALUES('openquake/pshai', '0.3.9-1', 4);
+INSERT INTO admin.revision_info(artefact, revision, step) VALUES('openquake/pshai', '0.3.9-1', 5);
 INSERT INTO admin.revision_info(artefact, revision, step) VALUES('openquake/uiapi', '0.3.9-1', 10);
