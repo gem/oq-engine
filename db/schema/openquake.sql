37493ed12db1f2d9d2e7c8131c0571fe4737bc08
Marco Milanesi <marcom@openquake.org>
2011-05-22 10:53:37 +0200

    applied muharem's patch
    Former-commit-id: 2242353b3858e45fc6ce02c3a2b2587084db55af [formerly c7c864c8d04225c89c3173090671a08d0920abd8]
    Former-commit-id: fecd1d6b9d3e69564676e6f11356fa1ab5c318fe

--- db/schema/openquake.sql
+++ db/schema/openquake.sql
@@ -392,32 +392,31 @@ CREATE TABLE pshai.mfd_tgr (
 -- global simple_fault view, needed for Geonode integration
 CREATE VIEW pshai.simple_fault_geo_view AS
 SELECT
-    pshai.simple_fault.id AS fault_id,
-    pshai.simple_fault.owner_id AS fault_owner_id,
-    pshai.simple_fault.gid AS fault_gid,
-    pshai.simple_fault.name AS fault_name,
-    pshai.simple_fault.description, pshai.simple_fault.dip,
-    pshai.simple_fault.upper_depth, pshai.simple_fault.lower_depth,
-    pshai.simple_fault.last_update AS fault_last_update,
-    pshai.simple_fault.edge, pshai.simple_fault.outline,
-    pshai.mfd_evd.owner_id AS pshai_mfd_evd_owner_id,
-    pshai.mfd_evd.magnitude_type AS pshai_mfd_evd_magnitude_type,
-    pshai.mfd_evd.min_val AS pshai_mfd_evd_min_val,
-    pshai.mfd_evd.max_val AS pshai_mfd_evd_max_val,
-    pshai.mfd_evd.bin_size AS pshai_mfd_evd_bin_size,
-    pshai.mfd_evd.mfd_values AS pshai_mfd_evd_pshai_mfd_values,
-    pshai.mfd_evd.total_cumulative_rate AS pshai_mfd_evd_total_cumulative_rate,
-    pshai.mfd_evd.total_moment_rate AS pshai_mfd_evd_total_moment_rate,
-    pshai.mfd_evd.last_update AS pshai_mfd_evd_last_update,
-    pshai.mfd_tgr.owner_id AS pshai_mfd_tgr_owner_id,
-    pshai.mfd_tgr.magnitude_type AS pshai_mfd_tgr_magnitude_type,
-    pshai.mfd_tgr.min_val AS pshai_mfd_tgr_min_val,
-    pshai.mfd_tgr.max_val AS pshai_mfd_tgr_max_val,
-    pshai.mfd_tgr.a_val AS pshai_mfd_tgr_a_val,
-    pshai.mfd_tgr.b_val AS pshai_mfd_tgr_b_val,
-    pshai.mfd_tgr.total_cumulative_rate AS pshai_mfd_tgr_total_cumulative_rate,
-    pshai.mfd_tgr.total_moment_rate AS pshai_mfd_tgr_total_moment_rate,
-    pshai.mfd_tgr.last_update AS pshai_mfd_tgr_last_update
+    pshai.simple_fault.id,
+    pshai.simple_fault.gid,
+    pshai.simple_fault.name,
+    pshai.simple_fault.description,
+    pshai.simple_fault.dip,
+    pshai.simple_fault.upper_depth,
+    pshai.simple_fault.lower_depth,
+    pshai.simple_fault.edge,
+    pshai.simple_fault.outline,
+    CASE WHEN mfd_evd_id IS NOT NULL THEN 'evd' ELSE 'tgr' END AS mfd_type,
+    -- Common columns, only one of each will be not NULL.
+    COALESCE(pshai.mfd_evd.magnitude_type, pshai.mfd_tgr.magnitude_type)
+        AS magnitude_type,
+    COALESCE(pshai.mfd_evd.min_val, pshai.mfd_tgr.min_val) AS min_val,
+    COALESCE(pshai.mfd_evd.max_val, pshai.mfd_tgr.max_val) AS max_val,
+    COALESCE(pshai.mfd_evd.total_cumulative_rate,
+             pshai.mfd_tgr.total_cumulative_rate) AS total_cumulative_rate,
+    COALESCE(pshai.mfd_evd.total_moment_rate,
+             pshai.mfd_tgr.total_moment_rate) AS total_moment_rate,
+    -- Columns specific to pshai.mfd_evd
+    pshai.mfd_evd.bin_size AS evd_bin_size,
+    pshai.mfd_evd.mfd_values AS evd_values,
+    -- Columns specific to pshai.mfd_tgr
+    pshai.mfd_tgr.a_val AS tgr_a_val,
+    pshai.mfd_tgr.b_val AS tgr_b_val
 FROM
      pshai.simple_fault
 LEFT OUTER JOIN pshai.mfd_evd ON pshai.mfd_evd.id = pshai.simple_fault.mfd_evd_id
