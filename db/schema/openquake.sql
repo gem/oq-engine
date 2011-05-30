c8ce8c5e10d80be5f23597b8051c31f2afd7cf3e
Muharem Hrnjadovic <mh@foldr3.com>
2011-05-30 11:48:13 +0200

    add hazard/loss map data tables to schema
    Former-commit-id: 074b53d7036d5668f0409abbfbb4f2bd5f12b80f [formerly 3705fb35622e6a1d63812ad5881a6103f43ec4c8]
    Former-commit-id: d49975e93823260dff22898f876b73002edec5e1

--- db/schema/openquake.sql
+++ db/schema/openquake.sql
@@ -655,6 +655,26 @@ CREATE TABLE uiapi.output (
 ) TABLESPACE uiapi_ts;
 
 
+-- Hazard map data.
+CREATE TABLE uiapi.hazard_map_data (
+    id SERIAL PRIMARY KEY,
+    output_id INTEGER NOT NULL,
+    value float NOT NULL
+) TABLESPACE uiapi_ts;
+SELECT AddGeometryColumn('uiapi', 'hazard_map_data', 'location', 4326, 'POINT', 2);
+ALTER TABLE uiapi.hazard_map_data ALTER COLUMN point SET NOT NULL;
+
+
+-- Hazard map data.
+CREATE TABLE uiapi.loss_map_data (
+    id SERIAL PRIMARY KEY,
+    output_id INTEGER NOT NULL,
+    value float NOT NULL
+) TABLESPACE uiapi_ts;
+SELECT AddGeometryColumn('uiapi', 'loss_map_data', 'location', 4326, 'POINT', 2);
+ALTER TABLE uiapi.loss_map_data ALTER COLUMN point SET NOT NULL;
+
+
 ------------------------------------------------------------------------
 -- Constraints (foreign keys etc.) go here
 ------------------------------------------------------------------------
@@ -791,6 +811,14 @@ FOREIGN KEY (oq_job_id) REFERENCES uiapi.oq_job(id) ON DELETE RESTRICT;
 
 ALTER TABLE uiapi.output ADD CONSTRAINT uiapi_output_owner_fk
 FOREIGN KEY (owner_id) REFERENCES admin.oq_user(id) ON DELETE RESTRICT;
+
+ALTER TABLE uiapi.hazard_map_data
+ADD CONSTRAINT uiapi_hazard_map_data_output_fk
+FOREIGN KEY (output_id) REFERENCES uiapi.output(id) ON DELETE CASCADE;
+
+ALTER TABLE uiapi.loss_map_data
+ADD CONSTRAINT uiapi_loss_map_data_output_fk
+FOREIGN KEY (output_id) REFERENCES uiapi.output(id) ON DELETE CASCADE;
 
 CREATE TRIGGER eqcat_magnitude_before_insert_update_trig
 BEFORE INSERT OR UPDATE ON eqcat.magnitude
