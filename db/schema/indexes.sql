eeae3e1492c8530c656a0a947b36059fb34bc768
Muharem Hrnjadovic <mh@foldr3.com>
2011-06-03 04:13:20 +0200

    add uiapi indexes
    Former-commit-id: 2b09efd723b7e6ab9a1e72b9c58876dc1ffa1677 [formerly 46133532e19cc79f295abdc62fc8f31f92dd8448]
    Former-commit-id: d22128768ff424c4cb00c11d5df69d6673b891a9

--- db/schema/indexes.sql
+++ db/schema/indexes.sql
@@ -56,3 +56,13 @@ CREATE INDEX pshai_r_rate_mdl_owner_id_idx on pshai.r_rate_mdl(owner_id);
 CREATE INDEX pshai_rupture_owner_id_idx on pshai.rupture(owner_id);
 CREATE INDEX pshai_simple_fault_owner_id_idx on pshai.simple_fault(owner_id);
 CREATE INDEX pshai_source_owner_id_idx on pshai.source(owner_id);
+
+CREATE INDEX uiapi_input_owner_id_idx on uiapi.input(owner_id);
+CREATE INDEX uiapi_oq_job_owner_id_idx on uiapi.oq_job(owner_id);
+CREATE INDEX uiapi_output_owner_id_idx on uiapi.output(owner_id);
+CREATE INDEX uiapi_upload_owner_id_idx on uiapi.upload(owner_id);
+
+-- uiapi indexes on foreign keys
+CREATE INDEX uiapi_hazard_map_data_output_id_idx on uiapi.hazard_map_data(output_id);
+CREATE INDEX uiapi_oq_params_upload_id_idx on uiapi.oq_params(upload_id);
+CREATE INDEX uiapi_loss_map_data_output_id_idx on uiapi.loss_map_data(output_id);
