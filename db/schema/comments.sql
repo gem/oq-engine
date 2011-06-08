63c2392b2068eb4c7c6c337889766649116bc8d1
Muharem Hrnjadovic <mh@foldr3.com>
2011-06-08 09:36:59 +0200

    added a uiapi.output.display_name
    Former-commit-id: fd9c1b15717c0c7115f37a56870e1de733e66c76 [formerly 4a2018dc750f32c9954018f59e9be645a53c96a7] [formerly 4a2018dc750f32c9954018f59e9be645a53c96a7 [formerly aff9dad5a6f1f64a19c548a474e492663126b6c8]]
    Former-commit-id: 82ff92334904d5422a42c3024e77a28688ec1dbe
    Former-commit-id: 0202f35ed7cd6c9f42c307d35fea0edf683a95f2

--- db/schema/comments.sql
+++ db/schema/comments.sql
@@ -160,15 +160,17 @@ COMMENT ON COLUMN uiapi.oq_params.imt IS 'Intensity measure type, one of:
     - peak ground displacement (pgd)';
 COMMENT ON COLUMN uiapi.oq_params.poes IS 'Probabilities of exceedence';
 
-COMMENT ON TABLE uiapi.output IS 'A single OpenQuake calculation engine output file.';
-COMMENT ON COLUMN uiapi.output.output_type IS 'Output file type, one of:
+COMMENT ON TABLE uiapi.output IS 'A single OpenQuake calculation engine output. The data may reside in a file or in the database.';
+COMMENT ON COLUMN uiapi.output.db_backed IS 'True if the output''s data resides in the database and not in a file.';
+COMMENT ON COLUMN uiapi.output.display_name IS 'The GUI display name to be used for this output.';
+COMMENT ON COLUMN uiapi.output.path IS 'The full path of the output file on the server (optional).';
+COMMENT ON COLUMN uiapi.output.output_type IS 'Output type, one of:
     - unknown
     - hazard_curve
     - hazard_map
     - loss_curve
     - loss_map';
-COMMENT ON COLUMN uiapi.output.shapefile_path IS 'The full path of the shapefile generated for a hazard or loss map.';
-COMMENT ON COLUMN uiapi.output.path IS 'The full path of the output file on the server.';
+COMMENT ON COLUMN uiapi.output.shapefile_path IS 'The full path of the shapefile generated for a hazard or loss map (optional).';
 
 COMMENT ON TABLE uiapi.upload IS 'A batch of OpenQuake input files uploaded by the user';
 COMMENT ON COLUMN uiapi.upload.job_pid IS 'The process id (PID) of the NRML loader process';
