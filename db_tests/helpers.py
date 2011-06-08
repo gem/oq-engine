14b030e632ea8f61dd168e5a4342df17fd1f212f
Mattia Barbon <mattia@develer.com>
2011-06-08 14:32:17 +0200

    Removed duplicated code to compute output filename for hazard writer tests.
    Former-commit-id: ff523b5a755b8d6ed942d7adf2646f0147d89df8 [formerly ff523b5a755b8d6ed942d7adf2646f0147d89df8 [formerly c01f070da1408977b9d68a35039eeedf47ef5367]]
    Former-commit-id: 629c1d51c8253d1aab346b6d48daf32752b648b8
    Former-commit-id: 85be91802ef336a7c51cef4269e64d875882619b

--- db_tests/helpers.py
+++ db_tests/helpers.py
@@ -147,9 +147,7 @@ class DbTestMixin(TestMixin):
         job = job_to_use if job_to_use else self.setup_classic_job()
         output = Output(owner=job.owner, oq_job=job, output_type=output_type,
                         db_backed=db_backed)
-        output.path = self.touch(
-            dir=os.path.join(job.path, "computed_output"), suffix=".xml",
-            prefix="hzrd." if output_type == "hazard_map" else "loss.")
+        output.path = self.generate_output_path(job, output_type)
         session = Session.get()
         session.add(output)
         session.commit()
