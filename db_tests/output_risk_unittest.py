92f778057dd87fa9b494d8419ddaaa3fe1febfb6
Gabriele Favalessa <gabriele@develer.com>
2011-06-28 11:03:56 +0200

    PEP8
    Former-commit-id: db94902906caeac960506f56397ce40cf36cec7e [formerly 5fe49351405ab610aa96303788ee7652f9cb2217]
    Former-commit-id: e3074941c84574607db244ba886e21b843a2fe9b

--- db_tests/output_risk_unittest.py
+++ db_tests/output_risk_unittest.py
@@ -36,20 +36,44 @@ from db_tests import helpers
 # and putting a breakpoint in openquake/output/risk.py:CurveXMLWriter.write()
 RISK_LOSS_CURVE_DATA = [
     (Site(-118.077721, 33.852034),
-     (Curve([(3.18e-06, 1.0),(8.81e-06, 1.0),(1.44e-05, 1.0),(2.00e-05, 1.0)]),
-      {u'assetValue': 5.07, u'assetID': u'a5625', u'listDescription': u'Collection of exposure values for existing building in Los Angeles County, California, US', u'structureCategory': u'RM1L', u'lon': -118.077721, u'assetDescription': u'LA building', u'vulnerabilityFunctionReference': u'HAZUS_RM1L_LC', u'listID': u'LA01', u'assetValueUnit': None, u'lat': 33.852034})),
+     (Curve([(3.18e-06, 1.0), (8.81e-06, 1.0), (1.44e-05, 1.0),
+             (2.00e-05, 1.0)]),
+      {u'assetValue': 5.07, u'assetID': u'a5625',
+       u'listDescription': u'Collection of exposure values for ...',
+       u'structureCategory': u'RM1L', u'lon': -118.077721,
+       u'assetDescription': u'LA building',
+       u'vulnerabilityFunctionReference': u'HAZUS_RM1L_LC',
+       u'listID': u'LA01', u'assetValueUnit': None, u'lat': 33.852034})),
 
     (Site(-118.077721, 33.852034),
-     (Curve([(7.18e-06, 1.0),(1.91e-05, 1.0),(3.12e-05, 1.0),(4.32e-05, 1.0)]),
-     {u'assetValue': 5.63, u'assetID': u'a5629', u'listDescription': u'Collection of exposure values for existing building in Los Angeles County, California, US', u'structureCategory': u'URML', u'lon': -118.077721, u'assetDescription': u'LA building', u'vulnerabilityFunctionReference': u'HAZUS_URML_LC', u'listID': u'LA01', u'assetValueUnit': None, u'lat': 33.852034})),
+     (Curve([(7.18e-06, 1.0), (1.91e-05, 1.0), (3.12e-05, 1.0),
+             (4.32e-05, 1.0)]),
+     {u'assetValue': 5.63, u'assetID': u'a5629',
+      u'listDescription': u'Collection of exposure values for ...',
+      u'structureCategory': u'URML',
+      u'lon': -118.077721, u'assetDescription': u'LA building',
+      u'vulnerabilityFunctionReference': u'HAZUS_URML_LC',
+      u'listID': u'LA01', u'assetValueUnit': None, u'lat': 33.852034})),
 
     (Site(-118.077721, 33.852034),
-     (Curve([(5.48e-06, 1.0),(1.45e-05, 1.0),(2.36e-05, 1.0),(3.27e-05, 1.0)]),
-     {u'assetValue': 11.26, u'assetID': u'a5630', u'listDescription': u'Collection of exposure values for existing building in Los Angeles County, California, US', u'structureCategory': u'URML', u'lon': -118.077721, u'assetDescription': u'LA building', u'vulnerabilityFunctionReference': u'HAZUS_URML_LS', u'listID': u'LA01', u'assetValueUnit': None, u'lat': 33.852034})),
+     (Curve([(5.48e-06, 1.0), (1.45e-05, 1.0), (2.36e-05, 1.0),
+             (3.27e-05, 1.0)]),
+     {u'assetValue': 11.26, u'assetID': u'a5630',
+      u'listDescription': u'Collection of exposure values for ...',
+      u'structureCategory': u'URML', u'lon': -118.077721,
+      u'assetDescription': u'LA building',
+      u'vulnerabilityFunctionReference': u'HAZUS_URML_LS',
+      u'listID': u'LA01', u'assetValueUnit': None, u'lat': 33.852034})),
 
     (Site(-118.077721, 33.852034),
-     (Curve([(9.77e-06, 1.0),(2.64e-05, 1.0),(4.31e-05, 1.0),(5.98e-05, 1.0)]),
-     {u'assetValue': 5.5, u'assetID': u'a5636', u'listDescription': u'Collection of exposure values for existing building in Los Angeles County, California, US', u'structureCategory': u'C3L', u'lon': -118.077721, u'assetDescription': u'LA building', u'vulnerabilityFunctionReference': u'HAZUS_C3L_MC', u'listID': u'LA01', u'assetValueUnit': None, u'lat': 33.852034})),
+     (Curve([(9.77e-06, 1.0), (2.64e-05, 1.0), (4.31e-05, 1.0),
+             (5.98e-05, 1.0)]),
+     {u'assetValue': 5.5, u'assetID': u'a5636',
+      u'listDescription': u'Collection of exposure values for ...',
+      u'structureCategory': u'C3L', u'lon': -118.077721,
+      u'assetDescription': u'LA building',
+      u'vulnerabilityFunctionReference': u'HAZUS_C3L_MC',
+      u'listID': u'LA01', u'assetValueUnit': None, u'lat': 33.852034})),
 ]
 
 
@@ -91,7 +115,8 @@ class LossCurveDBWriterTestCase(unittest.TestCase, helpers.DbTestMixin):
         self.assertEqual("loss_curve", output.output_type)
         self.assertTrue(self.job is output.oq_job)
 
-        # After calling the function under test we see the expected loss asset data.
+        # After calling the function under test we see the expected loss asset
+        # data.
         self.assertEqual(4, len(output.lossassetdata_set))
 
         inserted_data = []
@@ -102,17 +127,18 @@ class LossCurveDBWriterTestCase(unittest.TestCase, helpers.DbTestMixin):
             for curve in lad.losscurvedata_set:
                 data = (Site(pos[0], pos[1]),
                         (Curve(zip(curve.abscissae, curve.poes)),
-                        { u'assetID': lad.asset_id }))
+                        {u'assetID': lad.asset_id}))
 
                 inserted_data.append(data)
 
         def normalize(values):
             result = []
             for value in values:
-                result.append((value[0], (value[1][0], {
-                    'assetID': value[1][1]['assetID']
-                })))
+                result.append((value[0],
+                               (value[1][0],
+                                {'assetID': value[1][1]['assetID']})))
 
             return sorted(result, key=lambda v: v[1][1]['assetID'])
 
-        self.assertEquals(normalize(RISK_LOSS_CURVE_DATA), normalize(inserted_data))
+        self.assertEquals(normalize(RISK_LOSS_CURVE_DATA),
+                          normalize(inserted_data))
