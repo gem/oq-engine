07b35e81579877cedeb92f3b6ef1c4f0cb36417f
Gabriele Favalessa <gabriele@develer.com>
2011-07-01 12:39:12 +0200

    Added FKs, GRANTs, COMMENTs and INDEXes for loss map data
    Former-commit-id: 21b70fd221e091b588ae69d3f8136f89387c28fe [formerly 473149f3e66cdcb8a7b946033776c6006c250f18]
    Former-commit-id: 1dc109cee992a6e25a4c40c167bcd639fbd4ccf7

--- db/schema/indexes.sql
+++ db/schema/indexes.sql
@@ -71,3 +71,5 @@ CREATE INDEX uiapi_oq_params_upload_id_idx on uiapi.oq_params(upload_id);
 CREATE INDEX uiapi_loss_map_data_output_id_idx on uiapi.loss_map_data(output_id);
 CREATE INDEX uiapi_loss_asset_data_output_id_idx on uiapi.loss_asset_data(output_id);
 CREATE INDEX uiapi_loss_curve_data_loss_asset_id_idx on uiapi.loss_curve_data(loss_asset_id);
+CREATE INDEX uiapi_loss_map_node_data_loss_map_data_id_idx on uiapi.loss_map_node_data(loss_map_data_id);
+CREATE INDEX uiapi_loss_map_node_asset_data_loss_map_node_data_id_idx on uiapi.loss_map_node_asset_data(loss_map_node_data_id);
