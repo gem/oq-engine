017cc6ada5419bd3d8ec9715dfcf56456d8b95ff
Gabriele Favalessa <gabriele@develer.com>
2011-07-01 16:35:30 +0200

    Fix to comments badly truncated.
    Former-commit-id: a0c0d1f695c990a7290a2cc5935bce30f0fce28d [formerly a0c0d1f695c990a7290a2cc5935bce30f0fce28d [formerly 92afa5142b90c7426228889079a421c27e8b61b8]]
    Former-commit-id: a03a9c9d127a0266062d99d4767314bcf165be3f
    Former-commit-id: 429fca75ef11e34f4dd79cf614ec152f3ec182a8

--- db/schema/upgrades/openquake/uiapi/0.3.9-1/18/09-loss-curve-output-comments.sql
+++ db/schema/upgrades/openquake/uiapi/0.3.9-1/18/09-loss-curve-output-comments.sql
@@ -16,13 +16,14 @@
     <http://www.gnu.org/licenses/lgpl-3.0.txt> for a copy of the LGPLv3 License.
 */
 
-COMMENT ON TABLE uiapi.loss_asset_data IS 'Holds the asset id and its position for which loss curves were calculated.'
-COMMENT ON COLUMN uiapi.loss_asset_data.output_id IS 'The foreign key to the output record that represents the corresp
+COMMENT ON TABLE uiapi.loss_asset_data IS 'Holds the asset id and its position for which loss curves were calculated.';
+COMMENT ON COLUMN uiapi.loss_asset_data.output_id IS 'The foreign key to the output record that represents the corresponding loss curve.';
 COMMENT ON COLUMN uiapi.loss_asset_data.asset_id IS 'The asset id';
 COMMENT ON COLUMN uiapi.loss_asset_data.pos IS 'The position of the asset';
 
 COMMENT ON TABLE uiapi.loss_curve_data IS 'Holds the probabilities of excedeence for a given loss curve.';
-COMMENT ON COLUMN uiapi.loss_curve.data.loss_asset_id IS 'The foreign key to the asset record to which the loss curve
+COMMENT ON COLUMN uiapi.loss_curve.data.loss_asset_id IS 'The foreign key to the asset record to which the loss curve belongs';
 COMMENT ON COLUMN uiapi.loss_curve.data.end_branch_label IS 'End branch label for this curve';
 COMMENT ON COLUMN uiapi.loss_curve.data.abscissae IS 'The abscissae of the curve';
 COMMENT ON COLUMN uiapi.loss_curve.data.poes IS 'Probabilities of exceedence';
+
