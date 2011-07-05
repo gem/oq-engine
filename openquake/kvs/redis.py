96076993234b6f7a0bf892722b0507260d63e19f
Lars Butler <Lars.Butler@gmail.com>
2011-07-05 16:53:09 +0200

    pep8 cleanup
    Former-commit-id: 51a0448d0ce39187213f2c16892bbb605ca0ff1b

--- openquake/kvs/redis.py
+++ openquake/kvs/redis.py
@@ -43,9 +43,9 @@ class Redis(object):
     """ A Borg-style wrapper for Redis client class. """
     __shared_state = {}
 
-    def __new__(cls, host=settings.KVS_HOST, 
-                     port=settings.KVS_PORT, 
-                     **kwargs): #pylint: disable=W0613
+    def __new__(cls, host=settings.KVS_HOST,
+                     port=settings.KVS_PORT,
+                     **kwargs):  # pylint: disable=W0613
         self = object.__new__(cls)
         self.__dict__ = cls.__shared_state
         return self
