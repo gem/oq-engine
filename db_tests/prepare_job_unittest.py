6405bad9b399ea9093464101bfef3fb1083f8649
Mattia Barbon <mattia@develer.com>
2011-07-06 18:35:56 +0200

    Test all inserted parameter values, not just the region vertices.
    Former-commit-id: 9f465b64168959b1015822e269ea27196b0b3cf6 [formerly 445676a3f76c91582ff045b3adc1abbf06c88b7c]
    Former-commit-id: f2e53fa954ab3796031aa923cb06b87ed83062b0

--- db_tests/prepare_job_unittest.py
+++ db_tests/prepare_job_unittest.py
@@ -41,15 +41,25 @@ def _toCoordList(polygon):
 
 
 class PrepareJobTestCase(unittest.TestCase, helpers.DbTestMixin):
+    maxDiff = None
+
     """
     Unit tests for the prepare_job helper function, which creates a new
     job entry with the associated parameters.
 
     Test data is a trimmed-down version of smoketest config files
+
+    As a side-effect, also tests that the inserted record satisfied
+    the DB constraints.
     """
     def tearDown(self):
         if hasattr(self, "job") and self.job:
             self.teardown_job(self.job)
+
+    def assertFieldsEqual(self, expected, params):
+        got_params = dict((k, getattr(params, k)) for k in expected.keys())
+
+        self.assertEquals(expected, got_params)
 
     def test_prepare_classical_job(self):
         params = {
@@ -86,6 +96,24 @@ class PrepareJobTestCase(unittest.TestCase, helpers.DbTestMixin):
         self.job = prepare_job(params)
         self.assertEquals(params['REGION_VERTEX'],
                           _toCoordList(self.job.oq_params.region))
+        self.assertFieldsEqual(
+            {'job_type': 'classical',
+             'upload': None,
+             'region_grid_spacing': 0.1,
+             'min_magnitude': 5.0,
+             'investigation_time': 50.0,
+             'component': 'gmroti50',
+             'imt': 'pga',
+             'period': None,
+             'truncation_type': 'twosided',
+             'truncation_level': 3.0,
+             'reference_vs30_value': 760.0,
+             'imls': [0.005, 0.007, 0.0098, 0.0137, 0.0192],
+             'poes': [0.01, 0.1],
+             'realizations': 2,
+             'histories': None,
+             'gm_correlated': None,
+             }, self.job.oq_params)
 
     def test_prepare_deterministic_job(self):
         params = {
@@ -94,26 +122,44 @@ class PrepareJobTestCase(unittest.TestCase, helpers.DbTestMixin):
             'GMF_RANDOM_SEED': '3',
             'RUPTURE_SURFACE_DISCRETIZATION': '0.1',
             'INTENSITY_MEASURE_TYPE': 'PGA',
-            'REFERENCE_VS30_VALUE': '760.0',
+            'REFERENCE_VS30_VALUE': '759.0',
             'COMPONENT': 'Average Horizontal (GMRotI50)',
-            'REGION_GRID_SPACING': '0.01',
+            'REGION_GRID_SPACING': '0.02',
             'REGION_VERTEX': '34.07, -118.25, 34.07, -118.22, 34.04, -118.22',
             'PERIOD': '0.0',
             'NUMBER_OF_GROUND_MOTION_FIELDS_CALCULATIONS': '5',
             'TRUNCATION_LEVEL': '3',
-            'GMPE_TRUNCATION_TYPE': '2 Sided',
+            'GMPE_TRUNCATION_TYPE': '1 Sided',
             'GROUND_MOTION_CORRELATION': 'true',
         }
 
         self.job = prepare_job(params)
         self.assertEquals(params['REGION_VERTEX'],
                           _toCoordList(self.job.oq_params.region))
+        self.assertFieldsEqual(
+            {'job_type': 'deterministic',
+             'upload': None,
+             'region_grid_spacing': 0.02,
+             'min_magnitude': None,
+             'investigation_time': None,
+             'component': 'gmroti50',
+             'imt': 'pga',
+             'period': None,
+             'truncation_type': 'onesided',
+             'truncation_level': 3.0,
+             'reference_vs30_value': 759.0,
+             'imls': None,
+             'poes': None,
+             'realizations': None,
+             'histories': None,
+             'gm_correlated': True,
+             }, self.job.oq_params)
 
     def test_prepare_event_based_job(self):
         params = {
             'CALCULATION_MODE': 'Event Based',
             'POES_HAZARD_MAPS': '0.01 0.10',
-            'INTENSITY_MEASURE_TYPE': 'PGA',
+            'INTENSITY_MEASURE_TYPE': 'SA',
             'REGION_VERTEX': '33.88, -118.30, 33.88, -118.06, 33.76, -118.06',
             'INCLUDE_GRID_SOURCES': 'false',
             'INCLUDE_SUBDUCTION_FAULT_SOURCE': 'false',
@@ -126,22 +172,22 @@ class PrepareJobTestCase(unittest.TestCase, helpers.DbTestMixin):
             'TREAT_AREA_SOURCE_AS': 'Point Sources',
             'QUANTILE_LEVELS': '0.25 0.50',
             'INTENSITY_MEASURE_LEVELS': '0.005, 0.007, 0.0098, 0.0137, 0.0192',
-            'GROUND_MOTION_CORRELATION': 'true',
-            'GMPE_TRUNCATION_TYPE': '2 Sided',
+            'GROUND_MOTION_CORRELATION': 'false',
+            'GMPE_TRUNCATION_TYPE': 'None',
             'STANDARD_DEVIATION_TYPE': 'Total',
             'SUBDUCTION_FAULT_RUPTURE_OFFSET': '10.0',
             'RISK_CELL_SIZE': '0.0005',
             'MAXIMUM_DISTANCE': '200.0',
-            'NUMBER_OF_LOGIC_TREE_SAMPLES': '1',
+            'NUMBER_OF_LOGIC_TREE_SAMPLES': '5',
             'REGION_GRID_SPACING': '0.02',
-            'PERIOD': '0.0',
+            'PERIOD': '1.0',
             'AGGREGATE_LOSS_CURVE': 'true',
             'NUMBER_OF_SEISMICITY_HISTORIES': '1',
             'INCLUDE_FAULT_SOURCE': 'true',
             'SUBDUCTION_RUPTURE_ASPECT_RATIO': '1.5',
             'FAULT_SURFACE_DISCRETIZATION': '1.0',
             'REFERENCE_VS30_VALUE': '760.0',
-            'COMPONENT': 'Average Horizontal (GMRotI50)',
+            'COMPONENT': 'Average Horizontal',
             'CONDITIONAL_LOSS_POE': '0.01',
             'TRUNCATION_LEVEL': '3',
             'COMPUTE_MEAN_HAZARD_CURVE': 'true',
@@ -152,3 +198,21 @@ class PrepareJobTestCase(unittest.TestCase, helpers.DbTestMixin):
         self.job = prepare_job(params)
         self.assertEquals(params['REGION_VERTEX'],
                           _toCoordList(self.job.oq_params.region))
+        self.assertFieldsEqual(
+            {'job_type': 'event_based',
+             'upload': None,
+             'region_grid_spacing': 0.02,
+             'min_magnitude': 5.0,
+             'investigation_time': 50.0,
+             'component': 'average',
+             'imt': 'sa',
+             'period': 1.0,
+             'truncation_type': 'none',
+             'truncation_level': 3.0,
+             'reference_vs30_value': 760.0,
+             'imls': None,
+             'poes': None,
+             'realizations': 5,
+             'histories': 1,
+             'gm_correlated': False,
+             }, self.job.oq_params)
