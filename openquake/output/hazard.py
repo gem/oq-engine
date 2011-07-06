1f20375e93a462de9d8d716e2b68cfe759fe2e7b
Mattia Barbon <mattia@develer.com>
2011-07-06 11:35:07 +0200

    Remove log level/handler overrides: this should be done in a config file, not in module code.
    Former-commit-id: 37835f5c8a02be3f2eb6d6ec36cdf931f531451c [formerly 64725d804ed4a6bb43c9e758cd84a614e3804ca9]
    Former-commit-id: 9ffce89c83d789c78e31f0caac1e31d2e15504a9

--- openquake/output/hazard.py
+++ openquake/output/hazard.py
@@ -56,14 +56,6 @@ from openquake.xml import NSMAP, NRML, GML, NSMAP_WITH_QUAKEML
 
 logger = logging.getLogger('hazard-map-serializer')
 logger.setLevel(logging.DEBUG)
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-# create formatter and add it to the handlers
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-ch.setFormatter(formatter)
-# add the handlers to the logger
-logger.addHandler(ch)
 
 
 NRML_GML_ID = 'n1'
