2afeb2a927d759fbc63c3bc414d65d1040c8fd83
Lars Butler <Lars.Butler@gmail.com>
2011-07-08 17:18:42 +0200

    pep8 fixes
    Former-commit-id: bd72a461201e8b326e93ef963445f58dd6184708 [formerly bd72a461201e8b326e93ef963445f58dd6184708 [formerly af0ad4248e3e1b7416e78b292528c7345274f0d5]]
    Former-commit-id: 736bf9321dc2c8e890e8435c6eff17226d085b67
    Former-commit-id: 8427e061147d6634f30a9b58bd3ab8d0dd856b7a

--- openquake/risk/job/deterministic.py
+++ openquake/risk/job/deterministic.py
@@ -327,7 +327,7 @@ def load_assets_for_point(job_id, point):
 
 
 def collect_region_data(block_loss_map_data, region_loss_map_data):
-    """Collect the loss map data for all the region.""" 
+    """Collect the loss map data for all the region."""
     for site, data in block_loss_map_data.iteritems():
         if site in region_loss_map_data:
             region_loss_map_data[site].extend(data)
