17a09c2e02d8a302657a75faf1baa6ff7505faae
Andrea Cerisara <andreacerisara@gmail.com>
2011-07-11 14:51:03 +0200

    Updated API, added doc
    Former-commit-id: e9d005ba4b4329f6e8503110c2baa7f8df8b222e [formerly c3d301d191470900569104d49c2b9d352485f9c9]
    Former-commit-id: 99ad77b08d1d142f785d84d46924b43002598d65

--- tests/validator_unittest.py
+++ tests/validator_unittest.py
@@ -28,88 +28,79 @@ from openquake.job import config
 import unittest
 
 
-class AlwaysTrueValidator(config.Validator):
-
-    def __init__(self):
-        super(AlwaysTrueValidator, self).__init__()
+class AlwaysTrueValidator(object):
 
     def is_valid(self):
-        return True
-
-    def error_message(self):
-        return "Always true error message"
+        return (True, ["Always true error message"])
 
 
-class AlwaysFalseValidator(config.Validator):
+class AlwaysFalseValidator(object):
 
     def __init__(self, error):
         self.error = error
 
     def is_valid(self):
-        return False
-
-    def error_message(self):
-        return self.error
+        return (False, [self.error])
 
 
 class ValidatorSetTestCase(unittest.TestCase):
 
     def test_an_empty_set_is_valid(self):
-        self.assertTrue(config.ValidatorSet().is_valid())
+        self.assertTrue(config.ValidatorSet().is_valid()[0])
 
     def test_an_empty_set_has_no_error_messages(self):
-        self.assertEquals([], config.ValidatorSet().error_message())
+        self.assertEquals([], config.ValidatorSet().is_valid()[1])
 
     def test_with_a_validator_the_result_is_the_validator(self):
         set = config.ValidatorSet()
-        set.append(AlwaysTrueValidator())
+        set.add(AlwaysTrueValidator())
 
-        self.assertTrue(set.is_valid())
+        self.assertTrue(set.is_valid()[0])
 
     def test_a_set_is_valid_when_all_validators_are_valid(self):
         set = config.ValidatorSet()
 
-        set.append(AlwaysTrueValidator())
-        set.append(AlwaysTrueValidator())
-        set.append(AlwaysTrueValidator())
+        set.add(AlwaysTrueValidator())
+        set.add(AlwaysTrueValidator())
+        set.add(AlwaysTrueValidator())
 
-        self.assertTrue(set.is_valid())
-        set.append(AlwaysFalseValidator(""))
+        self.assertTrue(set.is_valid()[0])
+        set.add(AlwaysFalseValidator(""))
 
-        self.assertFalse(set.is_valid())
+        self.assertFalse(set.is_valid()[0])
 
     def test_no_error_messages_when_the_set_is_valid(self):
         set = config.ValidatorSet()
 
-        set.append(AlwaysTrueValidator())
-        set.append(AlwaysTrueValidator())
-        set.append(AlwaysTrueValidator())
+        set.add(AlwaysTrueValidator())
+        set.add(AlwaysTrueValidator())
+        set.add(AlwaysTrueValidator())
 
-        self.assertEquals([], set.error_message())
+        self.assertEquals([], set.is_valid()[1])
 
     def test_the_set_keeps_track_of_the_failed_validators(self):
         set = config.ValidatorSet()
 
-        set.append(AlwaysTrueValidator())
-        set.append(AlwaysFalseValidator("MESSAGE#1"))
-        set.append(AlwaysFalseValidator("MESSAGE#2"))
-        set.append(AlwaysFalseValidator("MESSAGE#3"))
+        set.add(AlwaysTrueValidator())
+        set.add(AlwaysFalseValidator("MESSAGE#1"))
+        set.add(AlwaysFalseValidator("MESSAGE#2"))
+        set.add(AlwaysFalseValidator("MESSAGE#3"))
 
-        self.assertFalse(set.is_valid())
+        self.assertFalse(set.is_valid()[0])
 
         error_messages = ["MESSAGE#1", "MESSAGE#2", "MESSAGE#3"]
-        self.assertEquals(error_messages, set.error_message())
+        self.assertEquals(error_messages, set.is_valid()[1])
 
 class ConfigurationConstraintsTestCase(unittest.TestCase):
     
     def test_with_risk_processing_the_exposure_must_be_specified(self):
-        sections = ["RISK", "HAZARD", "general"]
+        sections = [config.RISK_SECTION, "HAZARD", "general"]
         params = {}
         
         engine = job.Job(params, sections=sections)
-        self.assertFalse(engine.is_valid())
+        self.assertFalse(engine.is_valid()[0])
 
-        params = {"EXPOSURE": "/a/path/to/exposure"}
+        params = {config.EXPOSURE: "/a/path/to/exposure"}
         
         engine = job.Job(params, sections=sections)
-        self.assertTrue(engine.is_valid())
+        self.assertTrue(engine.is_valid()[0])
