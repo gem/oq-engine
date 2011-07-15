b3142350769d67fdd64b8f2486c78ec1e05dab2c
Lars Butler <Lars.Butler@gmail.com>
2011-07-15 16:38:58 +0200

    pep8 and license header cleanup
    Former-commit-id: 09ac1e60dd38c22c06e9e35864af5ad8b0472e15 [formerly 7e8d8b7cec576351f2010c8611a44e91a302102e] [formerly 7e8d8b7cec576351f2010c8611a44e91a302102e [formerly ee53a5e4a357c4e954858faa18204ad96a056962]]
    Former-commit-id: 2fd1e893861d25add3002d45602040a1a60fb530
    Former-commit-id: ead719139a908129157fb22c3cb1c7d838b75c16

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
 # Copyright (c) 2010-2011, GEM Foundation.
 #
 # OpenQuake is free software: you can redistribute it and/or modify
@@ -14,21 +17,6 @@
 # version 3 along with OpenQuake.  If not, see
 # <http://www.gnu.org/licenses/lgpl-3.0.txt> for a copy of the LGPLv3 License.
 
-
-
-# vim: tabstop=4 shiftwidth=4 softtabstop=4
-
-#    Licensed under the Apache License, Version 2.0 (the "License"); you may
-#    not use this file except in compliance with the License. You may obtain
-#    a copy of the License at
-#
-#         http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#    License for the specific language governing permissions and limitations
-#    under the License.
 
 import os
 
@@ -50,7 +38,7 @@ if os.path.exists('dist'):
     for x in os.listdir('dist'):
         if x[-4:] == '.jar':
             dist.append("dist/%s" % x)
-        
+
 with os.popen("which gfortran") as gf:
     if not gf:
         raise EnvironmentError("You need to install gfortran")
@@ -63,9 +51,9 @@ setup(
     author_email='openquake-dev@googlegroups.com',
     url='http://www.openquake.org/',
     packages=[
-        'openquake','openquake.hazard',
-        'openquake.job','openquake.kvs',
-        'openquake.output','openquake.parser',
+        'openquake', 'openquake.hazard',
+        'openquake.job', 'openquake.kvs',
+        'openquake.output', 'openquake.parser',
         'openquake.risk', 'openquake.risk.job'],
     data_files=[
         ('/etc/openquake', ['celeryconfig.py']),
