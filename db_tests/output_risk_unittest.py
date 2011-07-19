11cb45fb3b75ea041cac71627dbb2952e8038a6d
Gabriele Favalessa <gabriele@develer.com>
2011-07-19 09:20:40 +0200

    pep8
    Former-commit-id: a2c7e379bd7b74cfccd0aabed2fcee2a7e2c9ad1

--- db_tests/output_risk_unittest.py
+++ db_tests/output_risk_unittest.py
@@ -189,6 +189,7 @@ SAMPLE_NONDETERMINISTIC_LOSS_MAP_DATA = [
     (SITE_A_NONDETERMINISTIC_LOSS_TWO, SITE_A_ASSET_TWO)]),
     (SITE_B, [(SITE_B_NONDETERMINISTIC_LOSS_ONE, SITE_B_ASSET_ONE)])]
 
+
 class LossMapDBWriterTestCase(unittest.TestCase, helpers.DbTestMixin):
     """
     Unit tests for the LossMapDBWriter class, which serializes
