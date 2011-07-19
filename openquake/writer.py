bba2bce4309cafe62fa4cef02905fce7bf6edb8e
Mattia Barbon <mattia@develer.com>
2011-07-19 15:01:16 +0200

    Removed duplicate code to insert output record.
    Former-commit-id: a75265ea86ca5843ce1b0d93a5fb7b1018aede84

--- openquake/writer.py
+++ openquake/writer.py
@@ -15,11 +15,18 @@
 # <http://www.gnu.org/licenses/lgpl-3.0.txt> for a copy of the LGPLv3 License.
 
 
-
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 """
 Base classes for the output methods of the various codecs.
 """
+
+import logging
+from os.path import basename
+
+from db.alchemy.models import OqJob, Output
+
+LOGGER = logging.getLogger('serializer')
+LOGGER.setLevel(logging.DEBUG)
 
 
 class FileWriter(object):
@@ -90,3 +97,60 @@ class XMLFileWriter(FileWriter):
             self.write(key, val)
         self.write_footer()
         self.close()
+
+
+class DBWriter(object):
+    """
+    Abstact class implementing the "serialize" interface to output an iterable
+    to the database.
+
+    Subclasses must either implement get_output_type() and insert_datum() or
+    override serialize().
+    """
+
+    def __init__(self, session, nrml_path, oq_job_id):
+        self.nrml_path = nrml_path
+        self.oq_job_id = oq_job_id
+        self.session = session
+        self.output = None
+
+    def insert_output(self, output_type):
+        assert self.output is None
+        """Insert an `uiapi.output` record for the job at hand."""
+        LOGGER.info("> insert_output")
+        job = self.session.query(OqJob).filter(
+            OqJob.id == self.oq_job_id).one()
+        self.output = Output(owner=job.owner, oq_job=job,
+                             display_name=basename(self.nrml_path),
+                             output_type=output_type, db_backed=True)
+        self.session.add(self.output)
+        LOGGER.info("output = '%s'" % self.output)
+        LOGGER.info("< insert_output")
+
+    def insert_datum(self, key, values):
+        """
+        Called for each item of the iterable during serialize.
+        """
+        raise NotImplementedError()
+
+    def serialize(self, iterable):
+        """
+        Implementation of the "serialize" interface.
+
+        An Output record with type get_output_type() will be created, then
+        each item of the iterable will be serialized in turn to the database.
+        """
+        LOGGER.info("> serialize")
+        LOGGER.info("serializing %s points" % len(iterable))
+
+        if not self.output:
+            self.insert_output(self.get_output_type())
+        LOGGER.info("output = '%s'" % self.output)
+
+        for key, values in iterable:
+            self.insert_datum(key, values)
+
+        self.session.commit()
+
+        LOGGER.info("serialized %s points" % len(iterable))
+        LOGGER.info("< serialize")
