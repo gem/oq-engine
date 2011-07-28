17e1fb32e6c68dd42e4df3c9ada8399cd88edaba
Muharem Hrnjadovic <mh@foldr3.com>
2011-07-28 14:27:55 +0200

    move 'hazard_curve_node_data' to hzrdo
    Former-commit-id: 5a2f86b0a660226969864e5c23ba6cb01403cdb6

--- db/schema/security.sql
+++ db/schema/security.sql
@@ -58,7 +58,7 @@ GRANT ALL ON SEQUENCE uiapi.loss_curve_id_seq to GROUP openquake;
 GRANT ALL ON SEQUENCE uiapi.loss_curve_data_id_seq to GROUP openquake;
 GRANT ALL ON SEQUENCE hzrdo.hazard_map_data_id_seq to GROUP openquake;
 GRANT ALL ON SEQUENCE hzrdo.hazard_curve_data_id_seq to GROUP openquake;
-GRANT ALL ON SEQUENCE uiapi.hazard_curve_node_data_id_seq to GROUP openquake;
+GRANT ALL ON SEQUENCE hzrdo.hazard_curve_node_data_id_seq to GROUP openquake;
 GRANT ALL ON SEQUENCE uiapi.gmf_data_id_seq to GROUP openquake;
 GRANT ALL ON SEQUENCE uiapi.input_id_seq to GROUP openquake;
 GRANT ALL ON SEQUENCE uiapi.loss_map_id_seq to GROUP openquake;
@@ -178,9 +178,9 @@ GRANT SELECT,INSERT,UPDATE,DELETE ON hzrdo.hazard_map_data TO oq_hzrdo_writer;
 GRANT SELECT ON hzrdo.hazard_curve_data TO GROUP openquake;
 GRANT SELECT,INSERT,UPDATE,DELETE ON hzrdo.hazard_curve_data TO oq_hzrdo_writer;
 
--- uiapi.hazard_curve_node_data
-GRANT SELECT ON uiapi.hazard_curve_node_data TO GROUP openquake;
-GRANT SELECT,INSERT,UPDATE,DELETE ON uiapi.hazard_curve_node_data TO oq_uiapi_writer;
+-- hzrdo.hazard_curve_node_data
+GRANT SELECT ON hzrdo.hazard_curve_node_data TO GROUP openquake;
+GRANT SELECT,INSERT,UPDATE,DELETE ON hzrdo.hazard_curve_node_data TO oq_hzrdo_writer;
 
 -- uiapi.gmf_data
 GRANT SELECT ON uiapi.gmf_data TO GROUP openquake;
