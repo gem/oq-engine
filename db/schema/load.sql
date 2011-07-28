aa658e7402a38471c06c4b58e8aacce9525cbd82
Muharem Hrnjadovic <mh@foldr3.com>
2011-07-28 14:08:26 +0200

    s/pshai/hzrdi/ -- strike 2
    Former-commit-id: 532db0f067d5f2d7e0a792d5343aa7df0c376ce0

--- db/schema/load.sql
+++ db/schema/load.sql
@@ -25,5 +25,5 @@ INSERT INTO admin.oq_user(user_name, full_name, organization_id) VALUES('openqua
 INSERT INTO admin.revision_info(artefact, revision) VALUES('openquake/admin', '0.3.9-1');
 INSERT INTO admin.revision_info(artefact, revision, step) VALUES('openquake/eqcat', '0.3.9-1', 2);
 INSERT INTO admin.revision_info(artefact, revision, step) VALUES('openquake/oqmif', '0.4.2', 1);
-INSERT INTO admin.revision_info(artefact, revision, step) VALUES('openquake/pshai', '0.3.9-1', 6);
+INSERT INTO admin.revision_info(artefact, revision, step) VALUES('openquake/hzrdi', '0.3.9-1', 6);
 INSERT INTO admin.revision_info(artefact, revision, step) VALUES('openquake/uiapi', '0.3.9-1', 22);
