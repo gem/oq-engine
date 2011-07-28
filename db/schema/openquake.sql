349508d1f82af174fd3b6605d4fe85d32bd530a3
Muharem Hrnjadovic <mh@foldr3.com>
2011-07-28 14:21:00 +0200

    move 'hazard_map_data' to hzrdo
    Former-commit-id: c8078922b53c7eaad5b1f1e9350fdf8632b122af

--- db/schema/openquake.sql
+++ db/schema/openquake.sql
@@ -666,13 +666,13 @@ CREATE TABLE uiapi.output (
 
 
 -- Hazard map data.
-CREATE TABLE uiapi.hazard_map_data (
+CREATE TABLE hzrdo.hazard_map_data (
     id SERIAL PRIMARY KEY,
     output_id INTEGER NOT NULL,
     value float NOT NULL
-) TABLESPACE uiapi_ts;
-SELECT AddGeometryColumn('uiapi', 'hazard_map_data', 'location', 4326, 'POINT', 2);
-ALTER TABLE uiapi.hazard_map_data ALTER COLUMN location SET NOT NULL;
+) TABLESPACE hzrdo_ts;
+SELECT AddGeometryColumn('hzrdo', 'hazard_map_data', 'location', 4326, 'POINT', 2);
+ALTER TABLE hzrdo.hazard_map_data ALTER COLUMN location SET NOT NULL;
 
 
 -- Hazard curve data.
@@ -946,8 +946,8 @@ FOREIGN KEY (oq_job_id) REFERENCES uiapi.oq_job(id) ON DELETE RESTRICT;
 ALTER TABLE uiapi.output ADD CONSTRAINT uiapi_output_owner_fk
 FOREIGN KEY (owner_id) REFERENCES admin.oq_user(id) ON DELETE RESTRICT;
 
-ALTER TABLE uiapi.hazard_map_data
-ADD CONSTRAINT uiapi_hazard_map_data_output_fk
+ALTER TABLE hzrdo.hazard_map_data
+ADD CONSTRAINT hzrdo_hazard_map_data_output_fk
 FOREIGN KEY (output_id) REFERENCES uiapi.output(id) ON DELETE CASCADE;
 
 ALTER TABLE uiapi.hazard_curve_data
