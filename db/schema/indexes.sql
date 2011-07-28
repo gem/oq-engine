84be089ddd18fb7c154a5381a7383a810c311faa
Muharem Hrnjadovic <mh@foldr3.com>
2011-07-28 14:38:00 +0200

    rename indexes
    Former-commit-id: 4970c31e3d8e08ff0a6ab1474a1dcd321588b168 [formerly b49a732c6aee57e8168f248a921b9cbcc6eb56e4]
    Former-commit-id: 88b84bdb2ddf2a7a9e33b8b3b32c88af9857e385

--- db/schema/indexes.sql
+++ db/schema/indexes.sql
@@ -63,12 +63,12 @@ CREATE INDEX uiapi_output_owner_id_idx on uiapi.output(owner_id);
 CREATE INDEX uiapi_upload_owner_id_idx on uiapi.upload(owner_id);
 
 -- uiapi indexes on foreign keys
-CREATE INDEX uiapi_hazard_map_data_output_id_idx on uiapi.hazard_map_data(output_id);
-CREATE INDEX uiapi_hazard_curve_data_output_id_idx on uiapi.hazard_curve_data(output_id);
-CREATE INDEX uiapi_hazard_curve_node_data_hazard_curve_data_id_idx on uiapi.hazard_curve_node_data(hazard_curve_data_id);
-CREATE INDEX uiapi_gmf_data_output_id_idx on uiapi.gmf_data(output_id);
+CREATE INDEX hzrdo_hazard_map_data_output_id_idx on hzrdo.hazard_map_data(output_id);
+CREATE INDEX hzrdo_hazard_curve_data_output_id_idx on hzrdo.hazard_curve_data(output_id);
+CREATE INDEX hzrdo_hazard_curve_node_data_hazard_curve_data_id_idx on hzrdo.hazard_curve_node_data(hazard_curve_data_id);
+CREATE INDEX hzrdo_gmf_data_output_id_idx on hzrdo.gmf_data(output_id);
 CREATE INDEX uiapi_oq_params_upload_id_idx on uiapi.oq_params(upload_id);
-CREATE INDEX uiapi_loss_map_output_id_idx on uiapi.loss_map(output_id);
-CREATE INDEX uiapi_loss_map_data_loss_map_id_idx on uiapi.loss_map_data(loss_map_id);
-CREATE INDEX uiapi_loss_curve_output_id_idx on uiapi.loss_curve(output_id);
-CREATE INDEX uiapi_loss_curve_data_loss_curve_id_idx on uiapi.loss_curve_data(loss_curve_id);
+CREATE INDEX risko_loss_map_output_id_idx on risko.loss_map(output_id);
+CREATE INDEX risko_loss_map_data_loss_map_id_idx on risko.loss_map_data(loss_map_id);
+CREATE INDEX risko_loss_curve_output_id_idx on risko.loss_curve(output_id);
+CREATE INDEX risko_loss_curve_data_loss_curve_id_idx on risko.loss_curve_data(loss_curve_id);
