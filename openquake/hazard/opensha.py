124644102c7cba4f301a25af47936abbb0e06cc3
Mattia Barbon <mattia@develer.com>
2011-08-02 11:42:14 +0200

    Re-throw hazard Java exceptions as JavaException exceptions.
    Former-commit-id: 906c6d4b581c8553e5a5000452ba0c6168b99886

--- openquake/hazard/opensha.py
+++ openquake/hazard/opensha.py
@@ -389,6 +389,7 @@ class ClassicalMixin(BasePSHAMixin):
         for maps in quantiles.values():
             map_serializer(maps)
 
+    @java.jexception
     @preload
     def execute(self):
         """
@@ -666,6 +667,7 @@ class EventBasedMixin(BasePSHAMixin):
     Job class, and thus has access to the self.params dict, full of config
     params loaded from the Job configuration file."""
 
+    @java.jexception
     @preload
     def execute(self):
         """Main hazard processing block.
