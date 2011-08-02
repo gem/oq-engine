97720e2dfb2bc6ecd605c9757c476141d27a1c97
Gabriele Favalessa <gabriele@develer.com>
2011-08-02 17:09:33 +0200

    Revert "Removed specialized JSON encoder not really needed"
    This reverts commit ea83dcbd9cbe308f678b7eb1947d5be456f94493.
    
    
    Former-commit-id: 2887caf6353fa9e53bcc9faeb607f70e37524b04 [formerly b94b0504f13a55f5dc6cd1476e52766807c2d8ef]
    Former-commit-id: b316d3a2c2c9ec2ca28abd6c11c2e6b1f82e0e4b

--- openquake/kvs/__init__.py
+++ openquake/kvs/__init__.py
@@ -140,9 +140,18 @@ def get_list_json_decoded(key):
     return [json.loads(x) for x in get_client().lrange(key, 0, -1)]
 
 
+class NumpyAwareJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        import numpy
+
+        if isinstance(obj, numpy.ndarray) and obj.ndim == 1:
+            return [x for x in obj]
+
+        return json.JSONEncoder.default(self, obj)
+
 def set_value_json_encoded(key, value):
     """ Encode value and set in kvs """
-    encoder = json.JSONEncoder()
+    encoder = NumpyAwareJSONEncoder()
 
     try:
         encoded_value = encoder.encode(value)
