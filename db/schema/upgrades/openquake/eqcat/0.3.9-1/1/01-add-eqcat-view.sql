1067c0c1553215c9cbf4fb5b44384fa6570a9e3f
Marco Milanesi <marcom@openquake.org>
2011-08-03 16:40:48 +0200

    changed LGPL with ODBL on non-stored procedures
    Former-commit-id: 288a6b9f24d9551d0f52ddc9b798b3aa22be9f7b

--- db/schema/upgrades/openquake/eqcat/0.3.9-1/1/01-add-eqcat-view.sql
+++ db/schema/upgrades/openquake/eqcat/0.3.9-1/1/01-add-eqcat-view.sql
@@ -1,4 +1,13 @@
+/*
 
+    Copyright (c) 2010-2011, GEM Foundation.
+
+    OpenQuake database is made available under the Open Database License:
+    http://opendatacommons.org/licenses/odbl/1.0/. Any rights in individual
+    contents of the database are licensed under the Database Contents License:
+    http://opendatacommons.org/licenses/dbcl/1.0/
+
+*/
 -- global catalog view, needed for Geonode integration
 CREATE VIEW eqcat.catalog_allfields AS
 SELECT
