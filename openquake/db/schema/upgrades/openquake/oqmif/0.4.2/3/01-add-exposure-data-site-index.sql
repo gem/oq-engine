c24b868bd5510ee6061019105b16f5a7624e3da7
Anton Gritsay <anton@angri.ru>
2011-08-05 14:22:49 +0200

    added index on oqmif.exposure_model.site
    fixes #820324

---
+++ openquake/db/schema/upgrades/openquake/oqmif/0.4.2/3/01-add-exposure-data-site-index.sql
@@ -0,0 +1,13 @@
+/*
+
+    Copyright (c) 2010-2011, GEM Foundation.
+
+    OpenQuake database is made available under the Open Database License:
+    http://opendatacommons.org/licenses/odbl/1.0/. Any rights in individual
+    contents of the database are licensed under the Database Contents License:
+    http://opendatacommons.org/licenses/dbcl/1.0/
+
+*/
+
+
+CREATE INDEX oqmif_exposure_data_site_idx ON oqmif.exposure_data USING gist(site);
