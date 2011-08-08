b01ef6d352fa4aa13416c959f7d3fb81d81fb694
Mattia Barbon <mattia@develer.com>
2011-08-08 13:14:21 +0200

    Test that configuration properties are set correctly in the appender.

--- java_tests/org/gem/log/DummyAppender.java
+++ java_tests/org/gem/log/DummyAppender.java
@@ -2,11 +2,24 @@ package org.gem.log;
 
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 
 import org.junit.Ignore;
 
 @Ignore
 public class DummyAppender extends AMQPAppender {
+    public static DummyAppender lastAppender;
+
+    public DummyAppender() {
+        super();
+
+        lastAppender = this;
+    }
+
+    public ConnectionFactory getFactory() {
+        return factory;
+    }
+
     @Override
     protected Channel getChannel() {
         if (channel == null)
