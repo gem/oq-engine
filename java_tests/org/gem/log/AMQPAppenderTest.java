17449ebde702c82594b0d592047ae7c684f14fc9
Mattia Barbon <mattia@develer.com>
2011-08-08 12:32:32 +0200

    Test that .info() and .warn() set the correct event type.

--- java_tests/org/gem/log/AMQPAppenderTest.java
+++ java_tests/org/gem/log/AMQPAppenderTest.java
@@ -36,17 +36,25 @@ public class AMQPAppenderTest {
     public void basicLogging() {
         setUpDummyAppender();
 
-        logger.info("Test");
+        logger.info("Test1");
+        logger.warn("Test2");
 
         dummyChannel = (DummyChannel) dummyAppender.getChannel();
 
-        assertThat(dummyChannel.entries.size(), is(equalTo(1)));
+        assertThat(dummyChannel.entries.size(), is(equalTo(2)));
 
-        DummyChannel.Entry entry = dummyChannel.entries.get(0);
+        DummyChannel.Entry entry1 = dummyChannel.entries.get(0);
 
-        assertThat(entry.exchange, is(equalTo("")));
-        assertThat(entry.routingKey, is(equalTo("")));
-        assertThat(entry.properties.getType(), is(equalTo("INFO")));
-        assertThat(entry.body, is(equalTo("Test\n")));
+        assertThat(entry1.exchange, is(equalTo("")));
+        assertThat(entry1.routingKey, is(equalTo("")));
+        assertThat(entry1.properties.getType(), is(equalTo("INFO")));
+        assertThat(entry1.body, is(equalTo("Test1\n")));
+
+        DummyChannel.Entry entry2 = dummyChannel.entries.get(1);
+
+        assertThat(entry2.exchange, is(equalTo("")));
+        assertThat(entry2.routingKey, is(equalTo("")));
+        assertThat(entry2.properties.getType(), is(equalTo("WARN")));
+        assertThat(entry2.body, is(equalTo("Test2\n")));
     }
 }
