eda960fd4e1497bf2cbf742d552b51d5daabaf4a
Mattia Barbon <mattia@develer.com>
2011-08-08 13:14:21 +0200

    Test that configuration properties are set correctly in the appender.
    Former-commit-id: dad16490fb7ec23185b3f38c7158c20efc7b0584 [formerly dad16490fb7ec23185b3f38c7158c20efc7b0584 [formerly b01ef6d352fa4aa13416c959f7d3fb81d81fb694]]
    Former-commit-id: 21fe4708e061e120ce548eb5c172fdd4a76277ef
    Former-commit-id: 5493c3f4fb6d2586707fc94d1a63f6ed4c533b1a

--- java_tests/org/gem/log/AMQPAppenderTest.java
+++ java_tests/org/gem/log/AMQPAppenderTest.java
@@ -1,8 +1,14 @@
 package org.gem.log;
 
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.util.Properties;
+
 import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Layout;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PatternLayout;
+import org.apache.log4j.PropertyConfigurator;
 
 import org.junit.Test;
 
@@ -20,6 +26,7 @@ public class AMQPAppenderTest {
     void tearDown() {
         dummyChannel = null;
         dummyAppender = null;
+        DummyAppender.lastAppender = null;
 
         // this closes the RabbitMQ connections
         BasicConfigurator.resetConfiguration();
@@ -100,5 +107,72 @@ public class AMQPAppenderTest {
         DummyChannel.Entry entry2 = dummyChannel.entries.get(1);
 
         assertThat(entry2.routingKey, is(equalTo("log.WARN")));
+    }
+
+    @Test
+    public void log4jSetup() {
+        Properties props = new Properties();
+
+        props.setProperty("log4j.rootLogger", "DEBUG, rabbit");
+
+        props.setProperty("log4j.appender.rabbit",
+                          "org.gem.log.DummyAppender");
+
+        // connection parameters
+        props.setProperty("log4j.appender.rabbit.host",
+                          "amqp.openquake.org");
+        props.setProperty("log4j.appender.rabbit.port",
+                          "4004");
+        props.setProperty("log4j.appender.rabbit.username",
+                          "looser");
+        props.setProperty("log4j.appender.rabbit.password",
+                          "s3krit");
+        props.setProperty("log4j.appender.rabbit.virtualHost",
+                          "job.oq.org/test");
+
+        // message parameters
+        props.setProperty("log4j.appender.rabbit.routingKeyPattern",
+                          "log.%p");
+        props.setProperty("log4j.appender.rabbit.exchange",
+                          "amq.topic");
+
+        // layout
+        props.setProperty("log4j.appender.rabbit.layout",
+                          "org.apache.log4j.PatternLayout");
+        props.setProperty("log4j.appender.rabbit.layout.ConversionPattern",
+                          "%d %-5p [%c] - %m%n");
+
+        PropertyConfigurator.configure(props);
+
+        logger.info("Test1");
+
+        dummyAppender = DummyAppender.lastAppender;
+        dummyChannel = (DummyChannel) dummyAppender.getChannel();
+
+        // check layout
+        Layout layout = dummyAppender.getLayout();
+
+        assertThat(layout, is(instanceOf(PatternLayout.class)));
+
+        PatternLayout patternLayout = (PatternLayout) layout;
+
+        assertThat(patternLayout.getConversionPattern(),
+                   is(equalTo("%d %-5p [%c] - %m%n")));
+
+        // check message properties
+        assertThat(dummyChannel.entries.size(), is(equalTo(1)));
+
+        DummyChannel.Entry entry1 = dummyChannel.entries.get(0);
+
+        assertThat(entry1.exchange, is(equalTo("amq.topic")));
+
+        // check factory configuration
+        ConnectionFactory factory = dummyAppender.getFactory();
+
+        assertThat(factory.getHost(), is(equalTo("amqp.openquake.org")));
+        assertThat(factory.getPort(), is(equalTo(4004)));
+        assertThat(factory.getUsername(), is(equalTo("looser")));
+        assertThat(factory.getPassword(), is(equalTo("s3krit")));
+        assertThat(factory.getVirtualHost(), is(equalTo("job.oq.org/test")));
     }
 }
