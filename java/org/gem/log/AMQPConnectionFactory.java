2e9936feff82f9d050fb57fe6a7f578e55c01b4e
Mattia Barbon <mattia@develer.com>
2011-08-09 12:30:52 +0200

    Typo fixes.
    Former-commit-id: 1940c132bdad25ff5bcd6464384c08ef13c597c9 [formerly edc0213a22808c78a77037dc75918af2f4e0d118]
    Former-commit-id: 9c431cec7c682de33e1a82a8b3e6582158269c1d

--- java/org/gem/log/AMQPConnectionFactory.java
+++ java/org/gem/log/AMQPConnectionFactory.java
@@ -3,7 +3,7 @@ package org.gem.log;
 import java.util.Date;
 
 /**
- * Abstract AMQP connection factory interfaca
+ * Abstract AMQP connection factory interface
  */
 public interface AMQPConnectionFactory {
     /// Create a new AMQP connection
