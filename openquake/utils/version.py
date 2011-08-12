74ea4144f7d815f7880bafc63b447165826df709
Lars Butler <Lars.Butler@gmail.com>
2011-08-12 15:45:54 +0200

    added 'UTC' to timestamp
    Former-commit-id: 6c3dc657bc815519b75d9ce1fb3a20e2b987be21 [formerly 511bbca656939d4c2afc3e26950fb1752137fc25]
    Former-commit-id: 2adc2b5f57b189ab1600bb0454081ffdd17bcd8c

--- openquake/utils/version.py
+++ openquake/utils/version.py
@@ -64,6 +64,6 @@ def info(version_data):
     if end > seconds_since_epoch > start:
         release_date = datetime.utcfromtimestamp(
             seconds_since_epoch).isoformat()
-        result += ", released %s" % release_date
+        result += ", released %s UTC" % release_date
 
     return result
