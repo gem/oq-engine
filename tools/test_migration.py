a676aa41492e71f6e8b8bb5b9bf9638719230673
Mattia Barbon <mattia@develer.com>
2011-08-12 18:58:54 +0200

    First version of the database upgrade test script.
    Former-commit-id: 4681a3352b5547735df1b50d3b92c14b07629445 [formerly 34c5670989598136d3ec155982323907de7606e3] [formerly 34c5670989598136d3ec155982323907de7606e3 [formerly 2439bdd520e8dae79f51999c433154db1dc4724c]]
    Former-commit-id: 12787efa8ae193a767bd49b9853b32b1604d21a2
    Former-commit-id: 3493a7a3cecde9fb905576e098b1db5903e8f112

---
+++ tools/test_migration.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+import re
+import subprocess
+
+db_admin_user = 'postgres'
+original_db = 'original'
+
+def psql(*args):
+    subprocess.check_call(['psql', '-U', db_admin_user] + list(args))
+
+def load(path):
+    subprocess.check_call('zcat %s | psql -U %s -d %s' % (
+            path, db_admin_user, original_db), shell=True)
+
+def pg_dump(to_file):
+    pg = subprocess.Popen(['pg_dump', '--schema-only', '-U', db_admin_user,
+                           original_db], stdout=subprocess.PIPE)
+
+    comments = []
+    columns = []
+    in_table = False
+
+    with open(to_file, 'w') as out:
+        for line in pg.stdout:
+            # skip SQL comments and blank lines
+            if line.startswith('--') or line == '\n':
+                continue
+
+            # normalize conditions
+            if line.startswith('    CONSTRAINT'):
+                line = re.sub(r"\('(\w+)'::character varying\)::text",
+                              r"'\1'::character varying", line)
+                line = re.sub(r"ANY \(\(ARRAY\[(.*?)\]\)::text\[\]\)",
+                              r"ANY (ARRAY[\1])", line)
+
+            # reorder table columns
+            if in_table:
+                if line == ');\n':
+                    # table finished
+                    in_table = False
+                else:
+                    # accumulate table lines
+                    line = re.sub(r",\n", r"\n", line)
+                    columns.append(line)
+                    continue
+
+                # write table lines sorted
+                for column in sorted(columns):
+                    out.write(column)
+
+            # move comments to the end
+            if line.startswith('COMMENT '):
+                comments.append(line)
+                continue
+
+            if line.startswith('CREATE TABLE'):
+                in_table = True;
+                columns = []
+
+            out.write(line)
+
+        # write out comments sorted
+        for comment in sorted(comments):
+            out.write(comment)
+
+
+# DB from create_oq_schema
+psql('-c', "DROP DATABASE IF EXISTS %s" % original_db)
+psql('-c', "CREATE DATABASE %s" % original_db)
+
+subprocess.check_call(['bin/create_oq_schema', '--yes',
+                       '--db-name=%s' % original_db,
+                       '--db-user=%s' % db_admin_user,
+                       '--schema-path=%s' % 'openquake/db/schema'])
+
+pg_dump('/tmp/fresh.sql')
+
+# DB from dbmaint.py
+psql('-c', "DROP DATABASE IF EXISTS %s" % original_db)
+psql('-c', "CREATE DATABASE %s" % original_db)
+load('tools/test_migration_base.sql.gz')
+
+subprocess.check_call(['tools/dbmaint.py', '--db', original_db,
+                       '-U', db_admin_user])
+
+pg_dump('/tmp/after.sql')
+
+subprocess.call('diff -u /tmp/fresh.sql /tmp/after.sql | less -F', shell=True)
