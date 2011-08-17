c4a926add76e682ad6657fca78b82d5638e8c439
Muharem Hrnjadovic <mh@foldr3.com>
2011-08-17 19:39:49 +0200

    fixed more tests, work in progress
    Former-commit-id: 49c320cf2137fc0b798a299136bb9f6a139052b3

--- openquake/logs.py
+++ openquake/logs.py
@@ -71,7 +71,7 @@ LOG4J_AMQP_SETTINGS = {
 
     'log4j.appender.amqp': 'org.gem.log.AMQPAppender',
     'log4j.appender.amqp.host': config.get("amqp", "host"),
-    'log4j.appender.amqp.port': int(config.get("amqp", "port")),
+    'log4j.appender.amqp.port': config.get("amqp", "port"),
     'log4j.appender.amqp.username': config.get("amqp", "user"),
     'log4j.appender.amqp.password': config.get("amqp", "password"),
     'log4j.appender.amqp.virtualHost': config.get("amqp", "vhost"),
