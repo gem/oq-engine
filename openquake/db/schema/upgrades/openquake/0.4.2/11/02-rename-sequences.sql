197155bc778e5e9205875bf557aa62c1f781882d
Mattia Barbon <mattia@develer.com>
2011-08-17 16:08:13 +0200

    Also rename primary keys and constraints.
    Former-commit-id: 4a753907d87ede8546750043bcb82b501d3dcfe6 [formerly c4ba26e6e7a6a845747df0fad5a0af967cab8b48]
    Former-commit-id: 53c5e8184518d3cd57699ec0d6ed9167ce2e0bdf

--- openquake/db/schema/upgrades/openquake/0.4.2/11/02-rename-sequences.sql
+++ openquake/db/schema/upgrades/openquake/0.4.2/11/02-rename-sequences.sql
@@ -13,3 +13,11 @@
 -- Rename sequences
 ALTER SEQUENCE hzrdr.hazard_curve_data_id_seq RENAME TO hazard_curve_id_seq;
 ALTER SEQUENCE hzrdr.hazard_curve_node_data_id_seq RENAME TO hazard_curve_data_id_seq;
+
+-- Rename primary keys
+ALTER TABLE hzrdr.hazard_curve_data_pkey RENAME TO hazard_curve_pkey;
+ALTER TABLE hzrdr.hazard_curve_node_data_pkey RENAME TO hazard_curve_data_pkey;
+
+-- Rename constraints
+ALTER TABLE riski.vulnerability_data_vulnerability_model_id_key RENAME TO
+    vulnerability_function_vulnerability_model_id_key;
