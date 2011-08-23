4190b05a8957cfbad004640dd50e553c00149977
Mattia Barbon <mattia@develer.com>
2011-08-23 12:35:52 +0200

    Added comments on BCR distribution columns.
    Former-commit-id: 512f6a757ca22a9a88b084bd1d0967e6d04902e6 [formerly 512f6a757ca22a9a88b084bd1d0967e6d04902e6 [formerly 16e907ad4c01c4965e836803923cebfbe1f753f4]]
    Former-commit-id: a1a88a7da212a1956aca5d173ad9f1ec025e615f
    Former-commit-id: f338d0e01b6128051138d57825bef2df5e6579b9

--- openquake/db/schema/upgrades/openquake/0.4.2/14/01-add-bcr-distribution-table.sql
+++ openquake/db/schema/upgrades/openquake/0.4.2/14/01-add-bcr-distribution-table.sql
@@ -45,3 +45,14 @@ FOREIGN KEY (bcr_distribution_id) REFERENCES riskr.bcr_distribution(id) ON DELET
 -- indices
 CREATE INDEX riskr_bcr_distribution_output_id_idx on riskr.bcr_distribution(output_id);
 CREATE INDEX riskr_bcr_distribution_data_bcr_distribution_id_idx on riskr.bcr_distribution_data(bcr_distribution_id);
+
+
+-- comments
+COMMENT ON TABLE riskr.bcr_distribution IS 'Holds metadata for the benefit-cost ratio distribution';
+COMMENT ON COLUMN riskr.bcr_distribution.output_id IS 'The foreign key to the output record that represents the corresponding BCR distribution.';
+COMMENT ON COLUMN riskr.bcr_distribution.exposure_model_id IS 'The foreign key to the exposure model for this BCR distribution.';
+
+COMMENT ON TABLE riskr.bcr_distribution_data IS 'Holds the actual data for the BCR distribution';
+COMMENT ON COLUMN riskr.bcr_distribution_data.bcr_distribution_id IS 'The foreign key to the record to which the BCR distribution data belongs';
+COMMENT ON COLUMN riskr.bcr_distribution_data.asset_ref IS 'The asset id';
+COMMENT ON COLUMN riskr.bcr_distribution_data.bcr IS 'The actual benefit-cost ratio';
