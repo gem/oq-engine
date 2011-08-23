f15ea8c831ec78dc9f7d1d969b461aaf68ea4ba3
Mattia Barbon <mattia@develer.com>
2011-08-23 11:57:45 +0200

    Added table structure for BCR distribution.
    Former-commit-id: 7229dbda682eb4612bb199e3c2a6312c5fd8f030

--- openquake/db/schema/openquake.sql
+++ openquake/db/schema/openquake.sql
@@ -831,6 +831,24 @@ SELECT AddGeometryColumn('riskr', 'collapse_map_data', 'location', 4326, 'POINT'
 ALTER TABLE riskr.collapse_map_data ALTER COLUMN location SET NOT NULL;
 
 
+-- Benefit-cost ratio distribution
+CREATE TABLE riskr.bcr_distribution (
+    id SERIAL PRIMARY KEY,
+    output_id INTEGER NOT NULL, -- FK to output.id
+    exposure_model_id INTEGER NOT NULL -- FK to exposure_model.id
+) TABLESPACE riskr_ts;
+
+CREATE TABLE riskr.bcr_distribution_data (
+    id SERIAL PRIMARY KEY,
+    bcr_distribution_id INTEGER NOT NULL, -- FK to bcr_distribution.id
+    asset_ref VARCHAR NOT NULL,
+    bcr float NOT NULL CONSTRAINT bcr_value
+        CHECK (bcr >= 0.0)
+) TABLESPACE riskr_ts;
+SELECT AddGeometryColumn('riskr', 'bcr_distribution_data', 'location', 4326, 'POINT', 2);
+ALTER TABLE riskr.bcr_distribution_data ALTER COLUMN location SET NOT NULL;
+
+
 -- Exposure model
 CREATE TABLE oqmif.exposure_model (
     id SERIAL PRIMARY KEY,
