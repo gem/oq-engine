cc7e2348ac9d16a58680297356c53a210a9351f3
Muharem Hrnjadovic <mh@foldr3.com>
2011-08-24 15:26:30 +0200

    fix trailing whitespace
    Former-commit-id: 077315f2fb2c3f6e41c2af76ac998374f7aad8d8 [formerly 077315f2fb2c3f6e41c2af76ac998374f7aad8d8 [formerly 79442762b992816a79a2fef83f8cc28d9ece924c]]
    Former-commit-id: db5b272221895fcbca72dcbc4d97bd055572c977
    Former-commit-id: 44b8255998edf9ca975d3253b3e49de873b6853e

--- openquake/db/schema/upgrades/openquake/0.4.2/15/01-user-permission-changes.sql
+++ openquake/db/schema/upgrades/openquake/0.4.2/15/01-user-permission-changes.sql
@@ -11,56 +11,56 @@
 
 REVOKE ALL PRIVILEGES ON hzrdi.complex_fault FROM oq_hzrdi_writer;
 GRANT SELECT,INSERT,UPDATE,DELETE ON hzrdi.complex_fault TO oq_job_init;
- 
+
 REVOKE ALL PRIVILEGES ON hzrdi.fault_edge FROM oq_hzrdi_writer;
 GRANT SELECT,INSERT,UPDATE,DELETE ON hzrdi.fault_edge TO oq_job_init;
- 
+
 REVOKE ALL PRIVILEGES ON hzrdi.focal_mechanism FROM oq_hzrdi_writer;
 GRANT SELECT,INSERT,UPDATE,DELETE ON hzrdi.focal_mechanism TO oq_job_init;
- 
+
 REVOKE ALL PRIVILEGES ON hzrdi.mfd_evd FROM oq_hzrdi_writer;
 GRANT SELECT,INSERT,UPDATE,DELETE ON hzrdi.mfd_evd TO oq_job_init;
- 
+
 REVOKE ALL PRIVILEGES ON hzrdi.mfd_tgr FROM oq_hzrdi_writer;
 GRANT SELECT,INSERT,UPDATE,DELETE ON hzrdi.mfd_tgr TO oq_job_init;
- 
+
 REVOKE ALL PRIVILEGES ON hzrdi.r_depth_distr FROM oq_hzrdi_writer;
 GRANT SELECT,INSERT,UPDATE,DELETE ON hzrdi.r_depth_distr TO oq_job_init;
- 
+
 REVOKE ALL PRIVILEGES ON hzrdi.r_rate_mdl FROM oq_hzrdi_writer;
 GRANT SELECT,INSERT,UPDATE,DELETE ON hzrdi.r_rate_mdl TO oq_job_init;
- 
+
 REVOKE ALL PRIVILEGES ON hzrdi.rupture FROM oq_hzrdi_writer;
 GRANT SELECT,INSERT,UPDATE,DELETE ON hzrdi.rupture TO oq_job_init;
- 
+
 REVOKE ALL PRIVILEGES ON hzrdi.simple_fault FROM oq_hzrdi_writer;
 GRANT SELECT,INSERT,UPDATE,DELETE ON hzrdi.simple_fault TO oq_job_init;
- 
+
 REVOKE ALL PRIVILEGES ON hzrdi.source FROM oq_hzrdi_writer;
 GRANT SELECT,INSERT,UPDATE,DELETE ON hzrdi.source TO oq_job_init;
- 
+
 REVOKE ALL PRIVILEGES ON riski.vulnerability_function FROM oq_riski_writer;
 GRANT SELECT,INSERT,UPDATE,DELETE ON riski.vulnerability_function TO oq_job_init;
- 
+
 REVOKE ALL PRIVILEGES ON riski.vulnerability_model FROM oq_riski_writer;
 GRANT SELECT,INSERT,UPDATE,DELETE ON riski.vulnerability_model TO oq_job_init;
- 
+
 REVOKE ALL PRIVILEGES ON uiapi.input FROM oq_uiapi_writer;
 GRANT SELECT,INSERT,UPDATE,DELETE ON uiapi.input TO oq_job_init;
- 
+
 REVOKE ALL PRIVILEGES ON uiapi.oq_job FROM oq_uiapi_writer;
 REVOKE ALL PRIVILEGES ON uiapi.oq_job FROM oq_reslt_writer;
 GRANT SELECT,INSERT,UPDATE ON uiapi.oq_job TO oq_job_init;
- 
+
 REVOKE ALL PRIVILEGES ON uiapi.oq_params FROM oq_uiapi_writer;
 REVOKE ALL PRIVILEGES ON uiapi.oq_params FROM oq_reslt_writer;
 GRANT SELECT,INSERT,UPDATE ON uiapi.oq_params TO oq_job_init;
- 
+
 REVOKE ALL PRIVILEGES ON uiapi.output FROM oq_uiapi_writer;
- 
+
 REVOKE ALL PRIVILEGES ON uiapi.upload FROM oq_uiapi_writer;
 GRANT SELECT,INSERT,UPDATE,DELETE ON uiapi.upload TO oq_job_init;
- 
+
 REVOKE ALL PRIVILEGES ON uiapi.error_msg FROM oq_uiapi_reader;
 REVOKE ALL PRIVILEGES ON uiapi.error_msg FROM oq_uiapi_writer;
 GRANT SELECT ON uiapi.error_msg TO openquake;
