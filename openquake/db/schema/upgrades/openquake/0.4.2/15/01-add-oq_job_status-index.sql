88f5eb5e55461adf3a7e3680527382a357403c0f
Anton Gritsay <anton@angri.ru>
2011-08-24 18:59:29 +0200

    added index on status column to oq_job
    Former-commit-id: 4597caf13973a49923448284446120b1a89ae989

---
+++ openquake/db/schema/upgrades/openquake/0.4.2/15/01-add-oq_job_status-index.sql
@@ -0,0 +1,15 @@
+/*
+
+    Copyright (c) 2010-2011, GEM Foundation.
+
+    OpenQuake database is made available under the Open Database License:
+    http://opendatacommons.org/licenses/odbl/1.0/. Any rights in individual
+    contents of the database are licensed under the Database Contents License:
+    http://opendatacommons.org/licenses/dbcl/1.0/
+
+*/
+
+
+CREATE INDEX uiapi_oq_job_status_running
+    ON uiapi.oq_job(status)
+    WHERE status = 'running';
