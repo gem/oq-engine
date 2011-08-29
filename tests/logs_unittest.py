0807024439a526db3b74f32f7e45e5e8e5044bb8
Anton Gritsay <anton@angri.ru>
2011-08-29 16:41:11 +0200

    added real java logging unittest

--- tests/logs_unittest.py
+++ tests/logs_unittest.py
@@ -14,37 +14,59 @@
 # version 3 along with OpenQuake.  If not, see
 # <http://www.gnu.org/licenses/lgpl-3.0.txt> for a copy of the LGPLv3 License.
 
-
-from amqplib import client_0_8 as amqp
-import logging
-import os
-import multiprocessing
-import sys
+import logging.handlers
 import unittest
 
-import jpype
-
-from openquake import flags
 from openquake import java
-from openquake import logs
-from openquake import job
-from openquake.utils import config
-
-from tests.utils.helpers import cleanup_loggers
 
 
-class LogsTestCase(unittest.TestCase):
+class JavaLogsTestCase(unittest.TestCase):
     def setUp(self):
-        java.jvm()
+        self.jvm = java.jvm()
+        self.handler = logging.handlers.BufferingHandler(capacity=float('inf'))
+        self.logger = logging.getLogger('java')
+        self.logger.addHandler(self.handler)
+        self.logger.setLevel(logging.DEBUG)
+
+    def tearDown(self):
+        self.logger.removeHandler(self.handler)
+        self.logger.setLevel(logging.NOTSET)
 
     def test_java_logging(self):
-        msg = 'This is a test java log entry'
-        root_logger = jpype.JClass("org.apache.log4j.Logger").getRootLogger()
-        other_logger = jpype.JClass("org.apache.log4j.Logger").getLogger('grr')
+        jlogger_class = self.jvm.JClass("org.apache.log4j.Logger")
+        root_logger = jlogger_class.getRootLogger()
+        other_logger = jlogger_class.getLogger('other_logger')
 
-        root_logger.error(msg)
+        root_logger.error('java error msg')
         other_logger.warn('warning message')
         other_logger.debug('this is verbose debug info')
-        root_logger.fatal('something bad has happend')
+        root_logger.fatal('something bad has happened')
         root_logger.info('information message')
-        1/0
+
+        records = self.handler.buffer
+        self.assertEqual(records[0].levelno, logging.ERROR)
+        self.assertEqual(records[0].levelname, 'ERROR')
+        self.assertEqual(records[0].name, 'java')
+        self.assertEqual(records[0].msg, 'java error msg')
+        self.assertEqual(records[0].threadName, 'main')
+        self.assertEqual(records[0].processName, 'java')
+
+        self.assertEqual(records[1].levelno, logging.WARNING)
+        self.assertEqual(records[1].levelname, 'WARNING')
+        self.assertEqual(records[1].name, 'java.other_logger')
+        self.assertEqual(records[1].msg, 'warning message')
+
+        self.assertEqual(records[2].levelno, logging.DEBUG)
+        self.assertEqual(records[2].levelname, 'DEBUG')
+        self.assertEqual(records[2].name, 'java.other_logger')
+        self.assertEqual(records[2].msg, 'this is verbose debug info')
+
+        self.assertEqual(records[3].levelno, logging.CRITICAL)
+        self.assertEqual(records[3].levelname, 'CRITICAL')
+        self.assertEqual(records[3].name, 'java')
+        self.assertEqual(records[3].msg, 'something bad has happened')
+
+        self.assertEqual(records[4].levelno, logging.INFO)
+        self.assertEqual(records[4].levelname, 'INFO')
+        self.assertEqual(records[4].name, 'java')
+        self.assertEqual(records[4].msg, 'information message')
