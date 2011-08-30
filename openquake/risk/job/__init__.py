541c2dd89cfde2a16749b6c068ab5c24701caf96
Mattia Barbon <mattia@develer.com>
2011-08-30 17:52:02 +0200

    Added missing docstrings.
    Former-commit-id: f230fd2c5e3e04c2a71dc3ddc2664738371fff65 [formerly 07f12350368b4695a95850b195febb9932aac27f]
    Former-commit-id: 9a01c6bea50b0f052e284e000572f07b90339c6c

--- openquake/risk/job/__init__.py
+++ openquake/risk/job/__init__.py
@@ -15,3 +15,5 @@
 # You should have received a copy of the GNU Lesser General Public License
 # version 3 along with OpenQuake.  If not, see
 # <http://www.gnu.org/licenses/lgpl-3.0.txt> for a copy of the LGPLv3 License.
+
+""" Mixins for risk jobs. """
