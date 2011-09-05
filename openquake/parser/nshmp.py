ede800bdae83e028f63277c7cfb796edc7e7f071
Mattia Barbon <mattia@develer.com>
2011-09-05 10:37:35 +0200

    Use os.path.join for path concatenation.
    Former-commit-id: 035ac51990a9ae6d0476b2f9665bc5aabb2c94ac [formerly 3bc238ae4b5424054eedee7b8625329d7513f174]
    Former-commit-id: 27295d5169d6483813d7c2a32d93f729e0abc9f2

--- openquake/parser/nshmp.py
+++ openquake/parser/nshmp.py
@@ -31,7 +31,7 @@ JAVA_CLASS = JAVA_NAMESPACE+ "NshmpUsData"
 BOUNDING_BOX = (24.6, 50.0, -125.0, -65.5)
 # BOUNDING_BOX = (35.0, 35.0, -120, -120.0)
 
-JAVA_MODELS = [(JAVA_NAMESPACE +"NshmpWusFaultData", "WUS/WUSfaults"), 
+JAVA_MODELS = [(JAVA_NAMESPACE +"NshmpWusFaultData", "WUS/WUSfaults"),
                 (JAVA_NAMESPACE +"NshmpCaliforniaFaultData", "CA/CA_faults"),
                 (JAVA_NAMESPACE +"NshmpCascadiaSubductionData", "Cascadia"),
                 (JAVA_NAMESPACE +"NshmpCeusFaultData", "CEUS/CEUS.faults"),
@@ -44,8 +44,8 @@ def init_paths(base_path, jpype):
     """ Init the java paths """
 
     for class_name, subdir in JAVA_MODELS:
-        path = os.path.abspath("%s/%s" % (base_path, subdir))
+        path = os.path.abspath(os.path.join(base_path, subdir))
         java_class = jpype.JClass(class_name)
         LOG.debug("Setting inDir on %s to %s", class_name, path+"/")
         java_class.inDir = path+"/"
-    
+
