c78f9d05b4dd48884ae09d9ecd2aab4e4771e450
Anton Gritsay <anton@openquake.org>
2011-09-23 15:43:30 +0200

    first draft of python logic tree parser/validator/processor
    Former-commit-id: 8e01b7646f40dde167ee770a00bbd079e72f588d [formerly 272d5815ffb98a49b30e06858b5d69577e6a6c5f]
    Former-commit-id: cc704625a5efeb5b96b82130eb05ddc5554b70f9

---
+++ openquake/input/logictree.py
@@ -0,0 +1,172 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2010-2011, GEM Foundation.
+#
+# OpenQuake is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3
+# only, as published by the Free Software Foundation.
+#
+# OpenQuake is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License version 3 for more details
+# (a copy is included in the LICENSE file that accompanied this code).
+#
+# You should have received a copy of the GNU Lesser General Public License
+# version 3 along with OpenQuake.  If not, see
+# <http://www.gnu.org/licenses/lgpl-3.0.txt> for a copy of the LGPLv3 License.
+
+"""Logic tree parser, verifier and processor."""
+
+import os
+from decimal import Decimal
+
+from lxml import etree
+
+
+class LogicTreeError(Exception):
+    pass
+
+class ParsingError(LogicTreeError):
+    pass
+
+class ValidationError(LogicTreeError):
+    def __init__(self, node, msg):
+        self.lineno = node.sourceline
+        self.orig_msg = msg
+        super(ValidationError, self).__init__('Error in line %d: %s' %
+                                              (self.lineno, msg))
+
+
+class Branch(object):
+    def __init__(self, branch_id, weight, value):
+        self.branch_id = branch_id
+        self.weight = weight
+        self.value = value
+        self.child_branchset = None
+
+    def __str__(self):
+        ret = '%s: %s' % (self.weight, self.value)
+        if self.child_branchset:
+            ret = '%s\n%s' % (ret, self.child_branchset)
+        return ret
+
+
+class BranchSet(object):
+    def __init__(self, branches, uncertainty_type, filters):
+        self.branches = branches
+        self.uncertainty_type = uncertainty_type
+        self.filters = filters
+
+    def __str__(self):
+        if self.filters:
+            filters = ' %s' % ' '.join('%s="%s"' % item
+                                       for item in self.filters.items())
+        else:
+            filters = ''
+        ret = ['%s%s:' % (self.uncertainty_type, filters)]
+        for branch in self.branches:
+            lines = iter(str(branch).split('\n'))
+            ret.append('  - %s' % next(lines))
+            ret.extend('    %s' % line for line in lines)
+        return '\n'.join(ret)
+
+
+class LogicTree(object):
+    SCHEMA_PATH = os.path.join(os.path.dirname(__file__),
+                               '..', 'nrml', 'schema', 'nrml.xsd')
+    NRML = 'http://openquake.org/xmlns/nrml/0.2'
+
+    FILTERS = ('applyToTectonicRegionType',
+               'applyToSource',
+               'applyToSources',
+               'applyToSourceType')
+
+    _schema = None
+
+    @classmethod
+    def get_parser(cls):
+        if not cls._schema:
+            cls._schema = etree.XMLSchema(file=cls.SCHEMA_PATH)
+        parser = etree.XMLParser(schema=cls._schema)
+        return parser
+
+    def __init__(self, filename):
+        try:
+            tree = etree.parse(filename, parser=self.get_parser())
+        except etree.XMLSyntaxError as exc:
+            raise ParsingError(str(exc))
+        [tree] = tree.getroot()
+        self.branches = {}
+        self.open_ends = set()
+        self.parse(tree)
+
+    def parse(self, tree):
+        branchinglevels = iter(tree)
+        [root_branchset] = next(branchinglevels)
+        self.root = self.parse_branchset(root_branchset)
+        if self.root.uncertainty_type != 'sourceModel':
+            raise ValidationError(
+                root_branchset,
+                'first branchset must define an uncertainty ' \
+                'of type "sourceModel"'
+            )
+        for branchinglevel in branchinglevels:
+            for branchset_node in branchinglevel:
+                self.parse_branchset(branchset_node)
+
+    def parse_branchset(self, branchset_node):
+        branches = []
+        weight_sum = 0
+        for branchnode in branchset_node:
+            weight = branchnode.find('{%s}uncertaintyWeight' % self.NRML).text
+            weight = Decimal(weight)
+            weight_sum += weight
+            value = branchnode.find('{%s}uncertaintyModel' % self.NRML).text
+            branch_id = branchnode.get('branchID')
+            branch = Branch(branch_id, weight, value)
+            self.branches[branch_id] = branch
+            branches.append(branch)
+        if weight_sum != 1.0:
+            raise ValidationError(
+                branchnode.find('{%s}uncertaintyWeight' % self.NRML),
+                "branchset weights don't sum up to 1.0"
+            )
+        uncert_type = branchset_node.get('uncertaintyType')
+        filters = dict((filtername, branchset_node.get(filtername))
+                       for filtername in self.FILTERS
+                       if filtername in branchset_node.attrib)
+        branchset = BranchSet(branches, uncert_type, filters)
+        apply_to_branches = branchset_node.get('applyToBranches')
+        if apply_to_branches:
+            apply_to_branches = apply_to_branches.split()
+            for branch_id in apply_to_branches:
+                if not branch_id in self.branches:
+                    raise ValidationError(
+                        branchset_node,
+                        'branch %s is not yet defined' % branch_id
+                    )
+                branch = self.branches[branch_id]
+                if branch.child_branchset is not None:
+                    raise ValidationError(
+                        branchset_node,
+                        'branch %s already has child branchset' % branch_id
+                    )
+                branch.child_branchset = branchset
+                self.open_ends.remove(branch)
+        else:
+            for branch in self.open_ends:
+                branch.child_branchset = branchset
+            self.open_ends.clear()
+
+        self.open_ends.update(branches)
+        return branchset
+
+
+if __name__ == '__main__':
+    import sys
+    try:
+        lt = LogicTree(sys.argv[1])
+        print lt.root
+    except LogicTreeError as exc:
+        sys.exit(str(exc))
