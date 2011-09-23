6281b3a3a4641a8407f1bbd81ee6201b445275fb
Andrea Cerisara <andreacerisara@gmail.com>
2011-09-23 14:37:27 +0200

    Added function to read expected results

--- tests/quality_assurance_unittest.py
+++ tests/quality_assurance_unittest.py
@@ -17,14 +17,51 @@
 # version 3 along with OpenQuake.  If not, see
 # <http://www.gnu.org/licenses/lgpl-3.0.txt> for a copy of the LGPLv3 License.
 
+import os
 import unittest
 
+from openquake import shapes
 from tests.utils import helpers
+
+TEST_NAME = "PeerTestSet1Case2"
 
 
 class ClassicalPSHACalculatorAssuranceTestCase(unittest.TestCase):
 
     def test_peerTestSet1Case2(self):
-        config_file = helpers.smoketest_file("hazard_map_simple/config.gem")
+        expected_results = self._load_results()
+        self._run_job(helpers.smoketest_file(TEST_NAME + "/config.gem"))
+        # - query database
+        # - check results
+        # - clean database
+
+    def _run_job(self, config_file):
         job = helpers.job_from_file(config_file)
         job.launch()
+
+    def _load_results(self):
+        # simply split the x string and get
+        # the value at index y casted to float
+        get = lambda x, y: float(x.split(",")[y])
+
+        results_dir = helpers.smoketest_file(TEST_NAME + "/expected_results")
+        results_files = os.listdir(results_dir)
+
+        results = {}
+
+        for file in results_files:
+            path = os.path.join(results_dir, file)
+
+            with open(path, "rb") as hazard_curve:
+                lines = hazard_curve.readlines()[0].split()
+
+                coords = lines.pop(0)
+                site = shapes.Site(get(coords, 0), get(coords, 1))
+
+                results[site] = []
+
+                while len(lines):
+                    pair = lines.pop(0)
+                    results[site].append((get(pair, 0), get(pair, 1)))
+
+        return results
