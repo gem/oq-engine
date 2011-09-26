8422d98bf4a3c59bc7ce50dd2cd925935d139e41
Marco Milanesi <marcom@openquake.org>
2011-09-26 11:21:53 +0200

    timespan added to db, changes to xsd, serialization happens also to db

--- openquake/db/models.py
+++ openquake/db/models.py
@@ -739,6 +739,7 @@ class LossMap(models.Model):
     end_branch_label = models.TextField(null=True)
     category = models.TextField(null=True)
     unit = models.TextField(null=True)
+    timespan = models.IntegerField(null=True)
     poe = models.FloatField(null=True)
 
     class Meta:  # pylint: disable=C0111,W0232
