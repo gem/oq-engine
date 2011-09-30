eabcbf3c4877b41ca036a99a52d7a4fa3de70477
Lars Butler <Lars.Butler@gmail.com>
2011-09-30 14:11:17 +0200

    re-organized param definitions to make them easier to read
    Former-commit-id: 231c6d5f58de51ca43644dbeace8f005ed53c473 [formerly 74c201d0aed561da29134322d56f146945a27688]
    Former-commit-id: 0e98b1d54cb03c34a39a9c54198fae79c5545edf

--- openquake/job/params.py
+++ openquake/job/params.py
@@ -146,7 +146,16 @@ def define_param(name, column, modes=None, default=None, to_db=None):
         PARAMS[name] = Param(column=column, type=column_type,
                              default=default, modes=modes, to_db=to_db)
 
+# general params
 define_param('CALCULATION_MODE', None)
+define_param('SITES', 'sites')
+define_param('REGION_GRID_SPACING', 'region_grid_spacing')
+define_param('REGION_VERTEX', 'region')
+define_param('OUTPUT_DIR', None)
+define_param('BASE_PATH', None)
+
+
+# input files
 define_param('VULNERABILITY', None)
 define_param('SINGLE_RUPTURE_MODEL', None, modes=('deterministic'))
 define_param('EXPOSURE', None)
@@ -154,64 +163,17 @@ define_param('GMPE_LOGIC_TREE_FILE', None,
              modes=('classical', 'event_based', 'disaggregation'))
 define_param('SOURCE_MODEL_LOGIC_TREE_FILE', None,
              modes=('classical', 'event_based', 'disaggregation'))
-define_param('OUTPUT_DIR', None)
-define_param('BASE_PATH', None)
 
-define_param('SITES', 'sites')
-define_param('REGION_GRID_SPACING', 'region_grid_spacing')
-define_param('REGION_VERTEX', 'region')
-define_param('COMPONENT', 'component', to_db=map_enum)
-define_param('INTENSITY_MEASURE_TYPE', 'imt', to_db=map_enum)
-define_param('GMPE_TRUNCATION_TYPE', 'truncation_type', to_db=map_enum)
-define_param('GMPE_MODEL_NAME', 'gmpe_model_name')
-define_param('TRUNCATION_LEVEL', 'truncation_level')
-define_param('REFERENCE_VS30_VALUE', 'reference_vs30_value')
-define_param('MAXIMUM_DISTANCE', 'maximum_distance',
-             modes=('classical', 'disaggregation'))
-define_param('QUANTILE_LEVELS', 'quantile_levels', modes='classical')
-define_param('PERIOD', 'period', default=0.0)
-define_param('DAMPING', 'damping', default=0.0)
-define_param('INTENSITY_MEASURE_LEVELS', 'imls',
-             modes=('classical', 'event_based', 'disaggregation'))
-define_param('POES', 'poes', modes=('classical', 'disaggregation'))
-define_param('GROUND_MOTION_CORRELATION', 'gm_correlated',
-             modes=('deterministic', 'event_based'))
-define_param('INVESTIGATION_TIME', 'investigation_time', default=0.0,
-             modes=('classical', 'event_based', 'disaggregation'))
-define_param('MINIMUM_MAGNITUDE', 'min_magnitude', default=0.0,
-             modes=('classical', 'event_based', 'disaggregation'))
-define_param('NUMBER_OF_LOGIC_TREE_SAMPLES', 'realizations',
-             modes=('classical', 'event_based', 'disaggregation'))
-define_param('NUMBER_OF_GROUND_MOTION_FIELDS_CALCULATIONS',
-             'gmf_calculation_number', modes='deterministic')
-define_param('RUPTURE_SURFACE_DISCRETIZATION',
-             'rupture_surface_discretization', modes='deterministic')
-define_param('NUMBER_OF_SEISMICITY_HISTORIES', 'histories',
-             modes='event_based')
-define_param('REFERENCE_DEPTH_TO_2PT5KM_PER_SEC_PARAM',
-             'reference_depth_to_2pt5km_per_sec_param')
-define_param('GMF_OUTPUT', None,
-             modes=('event_based', 'deterministic'))
-define_param('COMPUTE_HAZARD_AT_ASSETS_LOCATIONS', None,
-             modes=('event_based', 'deterministic', 'classical'))
-define_param('GMF_RANDOM_SEED', 'gmf_random_seed',
-             modes=('event_based', 'deterministic'))
-define_param('SADIGH_SITE_TYPE', 'sadigh_site_type', to_db=map_enum)
-define_param('AGGREGATE_LOSS_CURVE', 'aggregate_loss_curve')
-define_param('LOSS_CURVES_OUTPUT_PREFIX', 'loss_curves_output_prefix')
-define_param('STANDARD_DEVIATION_TYPE', 'standard_deviation_type',
-             modes=('classical', 'event_based', 'disaggregation'),
-             to_db=map_enum)
-define_param('RISK_CELL_SIZE', 'risk_cell_size')
-define_param('WIDTH_OF_MFD_BIN', 'width_of_mfd_bin',
-             modes=('classical', 'event_based', 'disaggregation'))
-define_param('SOURCE_MODEL_LT_RANDOM_SEED', 'source_model_lt_random_seed',
-             modes=('classical', 'event_based', 'disaggregation'))
-define_param('GMPE_LT_RANDOM_SEED', 'gmpe_lt_random_seed',
-             modes=('classical', 'event_based', 'disaggregation'))
-define_param('CONDITIONAL_LOSS_POE', 'conditional_loss_poe')
-define_param('COMPUTE_MEAN_HAZARD_CURVE', 'compute_mean_hazard_curve',
-             modes='classical')
+# Disaggregation parameters:
+define_param('DISAGGREGATION_RESULTS', 'disagg_results',
+             modes='disaggregation', to_db=sequence_map_enum)
+define_param('LATITUDE_BIN_LIMITS', 'lat_bin_limits', modes='disaggregation')
+define_param('LONGITUDE_BIN_LIMITS', 'lon_bin_limits', modes='disaggregation')
+define_param('MAGNITUDE_BIN_LIMITS', 'mag_bin_limits', modes='disaggregation')
+define_param('EPSILON_BIN_LIMITS', 'epsilon_bin_limits',
+             modes='disaggregation')
+define_param('DISTANCE_BIN_LIMITS', 'distance_bin_limits',
+             modes='disaggregation')
 
 # area sources
 define_param('INCLUDE_AREA_SOURCES', 'include_area_sources',
@@ -277,13 +239,56 @@ define_param('SUBDUCTION_RUPTURE_FLOATING_TYPE',
              modes=('classical', 'event_based', 'disaggregation'),
              to_db=map_enum)
 
-# Disaggregation parameters:
-define_param('DISAGGREGATION_RESULTS', 'disagg_results',
-             modes='disaggregation', to_db=sequence_map_enum)
-define_param('LATITUDE_BIN_LIMITS', 'lat_bin_limits', modes='disaggregation')
-define_param('LONGITUDE_BIN_LIMITS', 'lon_bin_limits', modes='disaggregation')
-define_param('MAGNITUDE_BIN_LIMITS', 'mag_bin_limits', modes='disaggregation')
-define_param('EPSILON_BIN_LIMITS', 'epsilon_bin_limits',
-             modes='disaggregation')
-define_param('DISTANCE_BIN_LIMITS', 'distance_bin_limits',
-             modes='disaggregation')
+# Everything else; please maintain alphabetical ordering.
+define_param('AGGREGATE_LOSS_CURVE', 'aggregate_loss_curve')
+define_param('COMPONENT', 'component', to_db=map_enum)
+define_param('COMPUTE_HAZARD_AT_ASSETS_LOCATIONS', None,
+             modes=('event_based', 'deterministic', 'classical'))
+define_param('COMPUTE_MEAN_HAZARD_CURVE', 'compute_mean_hazard_curve',
+             modes='classical')
+define_param('CONDITIONAL_LOSS_POE', 'conditional_loss_poe')
+define_param('DAMPING', 'damping', default=0.0)
+define_param('GMF_OUTPUT', None,
+             modes=('event_based', 'deterministic'))
+define_param('GMF_RANDOM_SEED', 'gmf_random_seed',
+             modes=('event_based', 'deterministic'))
+define_param('GMPE_LT_RANDOM_SEED', 'gmpe_lt_random_seed',
+             modes=('classical', 'event_based', 'disaggregation'))
+define_param('GMPE_MODEL_NAME', 'gmpe_model_name')
+define_param('GMPE_TRUNCATION_TYPE', 'truncation_type', to_db=map_enum)
+define_param('GROUND_MOTION_CORRELATION', 'gm_correlated',
+             modes=('deterministic', 'event_based'))
+define_param('INTENSITY_MEASURE_LEVELS', 'imls',
+             modes=('classical', 'event_based', 'disaggregation'))
+define_param('INTENSITY_MEASURE_TYPE', 'imt', to_db=map_enum)
+define_param('INVESTIGATION_TIME', 'investigation_time', default=0.0,
+             modes=('classical', 'event_based', 'disaggregation'))
+define_param('LOSS_CURVES_OUTPUT_PREFIX', 'loss_curves_output_prefix')
+define_param('MAXIMUM_DISTANCE', 'maximum_distance',
+             modes=('classical', 'disaggregation'))
+define_param('MINIMUM_MAGNITUDE', 'min_magnitude', default=0.0,
+             modes=('classical', 'event_based', 'disaggregation'))
+define_param('NUMBER_OF_GROUND_MOTION_FIELDS_CALCULATIONS',
+             'gmf_calculation_number', modes='deterministic')
+define_param('NUMBER_OF_LOGIC_TREE_SAMPLES', 'realizations',
+             modes=('classical', 'event_based', 'disaggregation'))
+define_param('NUMBER_OF_SEISMICITY_HISTORIES', 'histories',
+             modes='event_based')
+define_param('PERIOD', 'period', default=0.0)
+define_param('POES', 'poes', modes=('classical', 'disaggregation'))
+define_param('QUANTILE_LEVELS', 'quantile_levels', modes='classical')
+define_param('REFERENCE_DEPTH_TO_2PT5KM_PER_SEC_PARAM',
+             'reference_depth_to_2pt5km_per_sec_param')
+define_param('REFERENCE_VS30_VALUE', 'reference_vs30_value')
+define_param('RISK_CELL_SIZE', 'risk_cell_size')
+define_param('RUPTURE_SURFACE_DISCRETIZATION',
+             'rupture_surface_discretization', modes='deterministic')
+define_param('SADIGH_SITE_TYPE', 'sadigh_site_type', to_db=map_enum)
+define_param('SOURCE_MODEL_LT_RANDOM_SEED', 'source_model_lt_random_seed',
+             modes=('classical', 'event_based', 'disaggregation'))
+define_param('STANDARD_DEVIATION_TYPE', 'standard_deviation_type',
+             modes=('classical', 'event_based', 'disaggregation'),
+             to_db=map_enum)
+define_param('TRUNCATION_LEVEL', 'truncation_level')
+define_param('WIDTH_OF_MFD_BIN', 'width_of_mfd_bin',
+             modes=('classical', 'event_based', 'disaggregation'))
