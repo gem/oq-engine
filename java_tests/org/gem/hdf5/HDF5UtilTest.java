657553ceab14335f634199f4cb8190582ecbdabc
Lars Butler <Lars.Butler@gmail.com>
2011-10-05 10:58:05 +0200

    moved all of the hdf5 stuff into a single util class
    cleaned up tests
    
    
    Former-commit-id: 2d01dda31bc0ee13f8a76830d7a07ccdf84174a7 [formerly 2d01dda31bc0ee13f8a76830d7a07ccdf84174a7 [formerly 42570780310ede9ef773052d782f4bd5074f8263]]
    Former-commit-id: eb0ec7ed1eb6b0347d952cb485002eb8a309e5ab
    Former-commit-id: ee9418d636b87fd9f24880f6c9c925891900dee0

--- java_tests/org/gem/hdf5/HDF5UtilTest.java
+++ java_tests/org/gem/hdf5/HDF5UtilTest.java
@@ -2,14 +2,27 @@ package org.gem.hdf5;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
 import java.util.Arrays;
 
+import ncsa.hdf.hdf5lib.exceptions.HDF5LibraryException;
+
 import org.junit.Test;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 
 import static org.gem.hdf5.HDF5Util.reshape;
+import static org.gem.hdf5.HDF5Util.readMatrix;
+import static org.gem.hdf5.HDF5Util.writeMatrix;
 
-public class HDF5UtilTest {
-	
+public class HDF5UtilTest 
+{
+
+	public static final String H5_FILE = "test_5d_array.h5";
+	public static final String H5_FILE_COMPRESSED =
+			"test_5d_array_compressed.h5";
+	public static final String INVALID_HDF5_FILE =
+			"java_tests/data/invalid_hdf5_file.h5";
 	public static final long[] SAMPLE_5D_ARRAY_SHAPE = {2, 2, 2, 2, 2};
 	public static final double[][][][][] SAMPLE_5D_ARRAY = 
 		{
@@ -34,6 +47,22 @@ public class HDF5UtilTest {
 				}
 			}
 		};
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception
+	{
+		// If any test files exist from a previous test run,
+		// clear them out so we can write new files in the tests:
+		new File(H5_FILE).delete();
+		new File(H5_FILE_COMPRESSED).delete();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception
+	{
+		new File(H5_FILE).delete();
+		new File(H5_FILE_COMPRESSED).delete();
+	}
 
 	@Test(expected=HDF5Exception.class)
 	public void testReshapeTooFewDims() throws HDF5Exception
@@ -41,14 +70,14 @@ public class HDF5UtilTest {
 		long[] tooFew = {1, 2, 3, 4};
 		reshape(null, tooFew);
 	}
-	
+
 	@Test(expected=HDF5Exception.class)
 	public void testReshapeTooManyDims() throws HDF5Exception
 	{
 		long[] tooMany = {1, 2, 3, 4, 5, 6};
 		reshape(null, tooMany);
 	}
-	
+
 	@Test(expected=HDF5Exception.class)
 	public void testReshapeLengthDoesNotMatchShape() throws HDF5Exception
 	{
@@ -58,7 +87,7 @@ public class HDF5UtilTest {
 		// data should have a length == 120
 		reshape(data, shape);
 	}
-	
+
 	@Test
 	public void testReshapeWithGoodInput() throws HDF5Exception
 	{
@@ -74,7 +103,41 @@ public class HDF5UtilTest {
 
 		assertTrue(Arrays.deepEquals(expected, reshape(testInput, shape)));
 	}
-	
-	
 
+	/**
+	 * Write a matrix to a file, read it from file, and compare the matrix to
+	 * the original.
+	 */
+	@Test
+	public void testWriteAndReadMatrix() throws Exception
+	{
+		writeMatrix(H5_FILE, "test description",
+			    SAMPLE_5D_ARRAY_SHAPE, SAMPLE_5D_ARRAY, 0);
+		double[][][][][] expected = SAMPLE_5D_ARRAY;
+		
+		assertTrue(Arrays.deepEquals(expected, readMatrix(H5_FILE)));
+	}
+	
+	/**
+	 * Write a matrix to a file (compressed), read it from file, and compare the
+	 * matrix to the original.
+	 */
+	@Test
+	public void testWriteAndReadMatrixCompressed() throws Exception
+	{
+		writeMatrix(H5_FILE_COMPRESSED, "test description",
+			    SAMPLE_5D_ARRAY_SHAPE, SAMPLE_5D_ARRAY, 0);
+		double[][][][][] expected = SAMPLE_5D_ARRAY;
+		
+		assertTrue(Arrays.deepEquals(expected, readMatrix(H5_FILE_COMPRESSED)));
+	}
+
+	/**
+	 * Try to read a text file as HDF5 and watch it crash and burn.
+	 */
+	@Test(expected=HDF5LibraryException.class)
+	public void testReadMatrixInvalidHDF5File() throws Exception
+	{
+		readMatrix(INVALID_HDF5_FILE);
+	}
 }
