5784ba682d4949defa6dd793128ebd6a2db4b0f1
Lars Butler <Lars.Butler@gmail.com>
2011-10-10 17:50:48 +0200

    most of the disagg calculator is done
    still need to write a few more tests and get final matrix calculation to work as expected

--- java/org/gem/calc/DisaggregationCalculator.java
+++ java/org/gem/calc/DisaggregationCalculator.java
@@ -7,6 +7,9 @@ import java.util.Map;
 import org.apache.commons.collections.Closure;
 import org.opensha.commons.data.Site;
 import org.opensha.commons.data.function.DiscretizedFuncAPI;
+import org.opensha.commons.geo.Location;
+import org.opensha.commons.geo.LocationList;
+import org.opensha.commons.geo.LocationUtils;
 import org.opensha.sha.earthquake.EqkRupForecastAPI;
 import org.opensha.sha.earthquake.ProbEqkRupture;
 import org.opensha.sha.earthquake.ProbEqkSource;
@@ -110,7 +113,7 @@ public class DisaggregationCalculator {
 						  [lonBinLims.length - 1]
 						  [magBinLims.length - 1]
 						  [epsilonBinLims.length - 1]
-						  [tectonicRegionTypes.length - 1];
+						  [tectonicRegionTypes.length];
 		
 		// value by which to normalize the final matrix
 		double totalAnnualRate = 0.0;
@@ -136,12 +139,21 @@ public class DisaggregationCalculator {
 				ProbEqkRupture rupture = source.getRupture(rupCnt);
 				imr.setEqkRupture(rupture);
 
+				Location location = closestLocation(rupture.getRuptureSurface().getLocationList(), site.getLocation());
+
 				double lat, lon, mag, epsilon;
-				lat = 0.0;  // TODO:
-				lon = 0.0;
-				mag = 0.0;
-				epsilon = 0.0;
-				// TODO: check if it's in range
+				lat = location.getLatitude();
+				lon = location.getLongitude();
+				mag = rupture.getMag();
+				epsilon = imr.getEpsilon();
+
+				if (!allInRange(lat, lon, mag, epsilon))
+				{
+					// one or more of the parameters is out of range;
+					// skip this rupture
+					continue;
+				}
+
 				int[] binIndex = getBinIndices(lat, lon, mag, epsilon, trt);
 
 				double annualRate = totRate
@@ -158,12 +170,102 @@ public class DisaggregationCalculator {
 
 	}
 
-	public int [] getBinIndices(
+	public boolean allInRange(
+			double lat, double lon, double mag, double epsilon)
+	{
+		
+		return inRange(this.latBinLims, lat)
+				&& inRange(this.lonBinLims, lon)
+				&& inRange(this.magBinLims, mag)
+				&& inRange(this.epsilonBinLims, epsilon);
+	}
+
+	public static boolean inRange(Double[] bins, Double value)
+	{
+		return value >= bins[0] && value < bins[bins.length - 1];
+	}
+
+	/**
+	 * Figure out which bins each input parameter fits into. The returned array
+	 * of indices represent the 5 dimensional coordinates in the disaggregation
+	 * matrix.
+	 * @param lat
+	 * @param lon
+	 * @param mag
+	 * @param epsilon
+	 * @param trt
+	 */
+	public int[] getBinIndices(
 			double lat, double lon, double mag,
 			double epsilon, TectonicRegionType trt)
 	{
-		
-		
-		return null;
+		int[] result = new int[5];
+		result[0] = digitize(this.latBinLims, lat);
+		result[1] = digitize(this.lonBinLims, lon);
+		result[2] = digitize(this.magBinLims, mag);
+		result[3] = digitize(this.epsilonBinLims, epsilon);
+		result[4] = Arrays.asList(TectonicRegionType.values()).indexOf(trt);
+
+		return result;
+	}
+
+	public static int digitize(Double[] bins, Double value)
+	{
+		for (int i = 0; i < bins.length - 1; i++)
+		{
+			if (value >= bins[i] && value < bins[i + 1])
+			{
+				return i;
+			}
+		}
+		throw new IllegalArgumentException(
+				"Value '" + value + "' is outside the expected range");
+	}
+
+	/**
+	 * Given a LocationList and a Location target, get the Location in the
+	 * LocationList which is closest to the target Location.
+	 * @param list
+	 * @param target
+	 * @return closest Location (in the input ListLocation) to the target
+	 */
+	public static Location closestLocation(LocationList list, Location target)
+	{
+		Location closest = null;
+
+		double minDistance = Double.MAX_VALUE;
+
+		for (Location loc : list)
+		{
+			double horzDist = LocationUtils.horzDistance(loc, target);
+			double vertDist = LocationUtils.vertDistance(loc, target);
+			double distance = Math.sqrt(Math.pow(horzDist, 2) + Math.pow(vertDist, 2));
+			if (distance < minDistance)
+			{
+				minDistance = distance;
+				closest = loc;
+			}
+		}
+		return closest;
+	}
+
+	public static void normalize(double[][][][][] matrix, double normFactor)
+	{
+		for (int i = 0; i < matrix.length; i++)
+		{
+			for (int j = 0; j < matrix[i].length; j++)
+			{
+				for (int k = 0; k < matrix[i][j].length; k++)
+				{
+					for (int l = 0; l < matrix[i][j][k].length; l++)
+					{
+						for (int m = 0; m < matrix[i][j][k][l].length; m++)
+						{
+							matrix[i][j][k][l][m] /= normFactor;
+						}
+					}
+				}
+			}
+		}
 	}
 }
--- java_tests/org/gem/calc/DisaggregationCalculatorTest.java
+++ java_tests/org/gem/calc/DisaggregationCalculatorTest.java
@@ -5,8 +5,14 @@ import static org.junit.Assert.*;
 import java.util.Arrays;
 
 import org.junit.Test;
+import org.opensha.commons.geo.Location;
+import org.opensha.commons.geo.LocationList;
+import org.opensha.sha.util.TectonicRegionType;
 
 import static org.gem.calc.DisaggregationTestHelper.*;
+import static org.gem.calc.DisaggregationCalculator.digitize;
+import static org.gem.calc.DisaggregationCalculator.closestLocation;
+import static org.gem.calc.DisaggregationCalculator.inRange;
 
 public class DisaggregationCalculatorTest
 {
@@ -86,12 +92,98 @@ public class DisaggregationCalculatorTest
 		double[][][][][] result = disCalc.computeMatrix(
 				SITE, ERF, IMR_MAP, POE, HAZARD_CURVE);
 				
-		
 		assertTrue(Arrays.deepEquals(EXPECTED, result));
+		
+	}
+
+	@Test
+	public void testDigitize()
+	{
+		int expected = 3;
+
+		int actual = digitize(MAG_BIN_LIMS, 8.9);
+
+		assertEquals(expected, actual);
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void testDigitizeOutOfRange()
+	{
+		digitize(MAG_BIN_LIMS, 4.9);
 	}
 
 	@Test
 	public void testGetBinIndices()
+	{
+		DisaggregationCalculator disCalc = new DisaggregationCalculator(
+				LAT_BIN_LIMS, LON_BIN_LIMS, MAG_BIN_LIMS,
+				EPS_BIN_LIMS, DIST_BIN_LIMS);
+
+		int[] expected = {0, 2, 1, 6, 3};
+		double lat, lon, mag, epsilon;
+		lat = -0.6;
+		lon = 0.0;
+		mag = 6.5;
+		epsilon = 3.49;
+		TectonicRegionType trt = TectonicRegionType.SUBDUCTION_SLAB;
+
+		int[] actual = disCalc.getBinIndices(lat, lon, mag, epsilon, trt);
+
+		assertTrue(Arrays.equals(expected, actual));
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void testGetBinIndicesOutOfRange()
+	{
+		DisaggregationCalculator disCalc = new DisaggregationCalculator(
+				LAT_BIN_LIMS, LON_BIN_LIMS, MAG_BIN_LIMS,
+				EPS_BIN_LIMS, DIST_BIN_LIMS);
+
+		double lat, lon, mag, epsilon;
+		lat = -0.6;
+		lon = 0.0;
+		mag = 6.5;
+		epsilon = 3.5;  // out of range
+		TectonicRegionType trt = TectonicRegionType.SUBDUCTION_SLAB;
+
+		disCalc.getBinIndices(lat, lon, mag, epsilon, trt);
+	}
+
+	@Test
+	public void testClosestLocation()
+	{
+		Location target = new Location(90.0, 180.0);
+
+		LocationList locList = new LocationList();
+		Location loc1, loc2, loc3;
+		loc1 = new Location(0.0, 0.0);
+		loc2 = new Location(90.0, 179.9);
+		loc3 = new Location(90.0, -180.0);
+		locList.add(loc1);
+		locList.add(loc2);
+		locList.add(loc3);
+
+		assertEquals(loc3, closestLocation(locList, target));
+	}
+
+	@Test
+	public void testInRange()
+	{
+		Double[] bins = {10.0, 20.0, 30.0};
+
+		// boundaries:
+		assertTrue(inRange(bins, 10.0));
+		assertFalse(inRange(bins, 30.0));
+
+		// in range:
+		assertTrue(inRange(bins, 29.9));
+
+		// out of range
+		assertFalse(inRange(bins, 31.0));
+	}
+
+	@Test
+	public void testNormalize()
 	{
 		
 	}
