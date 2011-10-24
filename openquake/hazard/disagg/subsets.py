7540f623be2361f002a99dd0bddfe4b11a42ddcc
Anton Gritsay <anton@openquake.org>
2011-10-24 18:25:55 +0200

    disagg/subsets: tested all extractors that do not use distance
    Former-commit-id: 4c3d4459f2677be52e452b1b3ad7607bf0364a84

--- openquake/hazard/disagg/subsets.py
+++ openquake/hazard/disagg/subsets.py
@@ -38,108 +38,135 @@ def pmf(func):
     return func
 
 @pmf
-def magpmf(full_matrix, target_file, nlat, nlon, nmag, neps, ntrt):
+def magpmf(site, full_matrix, distance_bin_edges,
+           nlat, nlon, nmag, neps, ntrt,
+           target_file):
     ds_name = 'magpmf'
     shape = [nmag - 1]
-    ds = target_file.create_dataset(ds_name, shape, DATA_TYPE, fillvalue=0)
+    ds = numpy.ndarray(shape, DATA_TYPE)
     for i in xrange(nmag - 1):
         ds[i] = sum(full_matrix[j][k][i][l][m]
                     for j in xrange(nlat - 1)
-                    for k in xrange(nlot - 1)
+                    for k in xrange(nlon - 1)
                     for l in xrange(neps - 1)
                     for m in xrange(ntrt))
+    target_file.create_dataset(ds_name, data=ds)
 
 @pmf
-def distpmf(full_matrix, target_file, nlat, nlon, nmag, neps, ntrt):
+def distpmf(site, full_matrix, distance_bin_edges,
+            nlat, nlon, nmag, neps, ntrt,
+            target_file):
     # TODO: implement
     pass
 
 @pmf
-def trtpmf(full_matrix, target_file, nlat, nlon, nmag, neps, ntrt):
+def trtpmf(site, full_matrix, distance_bin_edges,
+           nlat, nlon, nmag, neps, ntrt,
+           target_file):
     ds_name = 'trtpmf'
     shape = [ntrt]
-    ds = target_file.create_dataset(ds_name, shape, DATA_TYPE, fillvalue=0)
+    ds = numpy.ndarray(shape, DATA_TYPE)
     for i in xrange(ntrt):
         ds[i] = sum(full_matrix[j][k][l][m][i]
                     for j in xrange(nlat - 1)
-                    for k in xrange(nlot - 1)
+                    for k in xrange(nlon - 1)
                     for l in xrange(nmag - 1)
                     for m in xrange(neps - 1))
+    target_file.create_dataset(ds_name, data=ds)
 
 @pmf
-def magdistpmf(full_matrix, target_file, nlat, nlon, nmag, neps, ntrt):
+def magdistpmf(site, full_matrix, distance_bin_edges,
+               nlat, nlon, nmag, neps, ntrt,
+               target_file):
     # TODO: implement
     pass
 
 @pmf
-def magdistepspmf(full_matrix, target_file, nlat, nlon, nmag, neps, ntrt):
+def magdistepspmf(site, full_matrix, distance_bin_edges,
+                  nlat, nlon, nmag, neps, ntrt,
+                  target_file):
     # TODO: implement
     pass
 
 @pmf
-def latlonpmf(full_matrix, target_file, nlat, nlon, nmag, neps, ntrt):
+def latlonpmf(site, full_matrix, distance_bin_edges,
+              nlat, nlon, nmag, neps, ntrt,
+              target_file):
     ds_name = 'latlonpmf'
     shape = [nlat - 1, nlon - 1]
-    ds = target_file.create_dataset(ds_name, shape, DATA_TYPE, fillvalue=0)
+    ds = numpy.ndarray(shape, DATA_TYPE)
     for i in xrange(nlat - 1):
         for j in xrange(nlon - 1):
             ds[i][j] = sum(full_matrix[i][j][k][l][m]
                            for k in xrange(nmag - 1)
                            for l in xrange(neps - 1)
                            for m in xrange(ntrt))
+    target_file.create_dataset(ds_name, data=ds)
 
 @pmf
-def latlonmagpmf(full_matrix, target_file, nlat, nlon, nmag, neps, ntrt):
+def latlonmagpmf(site, full_matrix, distance_bin_edges,
+                 nlat, nlon, nmag, neps, ntrt,
+                 target_file):
     ds_name = 'latlonmagpmf'
     shape = [nlat - 1, nlon - 1, nmag - 1]
-    ds = target_file.create_dataset(ds_name, shape, DATA_TYPE, fillvalue=0)
+    ds = numpy.ndarray(shape, DATA_TYPE)
     for i in xrange(nlat - 1):
         for j in xrange(nlon - 1):
             for k in xrange(nmag - 1):
                 ds[i][j][k] = sum(full_matrix[i][j][k][l][m]
                                   for l in xrange(neps - 1)
                                   for m in xrange(ntrt))
+    target_file.create_dataset(ds_name, data=ds)
 
 @pmf
-def latlonmagepspmf(full_matrix, target_file, nlat, nlon, nmag, neps, ntrt):
+def latlonmagepspmf(site, full_matrix, distance_bin_edges,
+                    nlat, nlon, nmag, neps, ntrt,
+                    target_file):
     ds_name = 'latlonmagepspmf'
     shape = [nlat - 1, nlon - 1, nmag - 1, neps - 1]
-    ds = target_file.create_dataset(ds_name, shape, DATA_TYPE, fillvalue=0)
+    ds = numpy.ndarray(shape, DATA_TYPE)
     for i in xrange(nlat - 1):
         for j in xrange(nlon - 1):
             for k in xrange(nmag - 1):
                 for l in xrange(neps - 1):
                     ds[i][j][k][l] = sum(full_matrix[i][j][k][l][m]
                                          for m in xrange(ntrt))
+    target_file.create_dataset(ds_name, data=ds)
 
 @pmf
-def magtrtpmf(full_matrix, target_file, nlat, nlon, nmag, neps, ntrt):
+def magtrtpmf(site, full_matrix, distance_bin_edges,
+              nlat, nlon, nmag, neps, ntrt,
+              target_file):
     ds_name = 'magtrtpmf'
     shape = [nmag - 1, ntrt]
-    ds = target_file.create_dataset(ds_name, shape, DATA_TYPE, fillvalue=0)
+    ds = numpy.ndarray(shape, DATA_TYPE)
     for i in xrange(nmag - 1):
         for j in xrange(ntrt):
             ds[i][j] = sum(full_matrix[k][l][i][m][j]
                            for k in xrange(nlat - 1)
                            for l in xrange(nlon - 1)
                            for m in xrange(neps - 1))
+    target_file.create_dataset(ds_name, data=ds)
 
 @pmf
-def latlontrtpmf(full_matrix, target_file, nlat, nlon, nmag, neps, ntrt):
+def latlontrtpmf(site, full_matrix, distance_bin_edges,
+                 nlat, nlon, nmag, neps, ntrt,
+                 target_file):
     ds_name = 'latlontrtpmf'
     shape = [nlat - 1, nlon - 1, ntrt]
-    ds = target_file.create_dataset(ds_name, shape, DATA_TYPE, fillvalue=0)
+    ds = numpy.ndarray(shape, DATA_TYPE)
     for i in xrange(nlat - 1):
         for j in xrange(nlon - 1):
             for k in xrange(ntrt):
                 ds[i][j][k] = sum(full_matrix[i][j][l][m][k]
                                   for l in xrange(nmag - 1)
                                   for m in xrange(neps - 1))
-
+    target_file.create_dataset(ds_name, data=ds)
 
 
 @task
-def extract_subsets(full_matrix_path, target_path, dims, subsets):
+def extract_subsets(site, full_matrix_path, dims, distance_bin_edges,
+                    target_path, subsets):
     assert len(dims) == 5
     subsets = set(subsets)
     assert subsets
@@ -148,7 +175,8 @@ def extract_subsets(full_matrix_path, target_path, dims, subsets):
         shutil.copyfile(full_matrix_path, target_path)
     with h5py.File(full_matrix_path, 'r') as source:
         full_matrix = source[FULL_MATRIX_DS_NAME]
-        with h5py.File(target_path, 'w+') as target:
+        with h5py.File(target_path, 'a') as target:
             for subset_type in subsets:
                 extractor = SUBSET_EXTRACTORS[subset_type]
-                extractor(full_matrix, target, *dims)
+                extractor(site, full_matrix, distance_bin_edges,
+                          *dims, target_file=target)
--- tests/disagg_subsets_unittest.py
+++ tests/disagg_subsets_unittest.py
@@ -30,15 +30,18 @@ from openquake.hazard.disagg import subsets as disagg_subsets
 class SubsetExtractionTestCase(unittest.TestCase):
     FULL_MATRIX_DATA = \
         'latitudeLongitudeMagnitudeEpsilonTectonicRegionTypePMF.dat'
-    #                   lat lon mag eps trt
-    FULL_MATRIX_SHAPE = (5,  5,  4,  4,  5)
+    #                        lat lon mag eps trt
+    FULL_MATRIX_SHAPE      = (6,  6,  5,  5,  5)
+    NLAT, NLON, NMAG, NEPS, NTRT = FULL_MATRIX_SHAPE
 
     @classmethod
     def setUpClass(cls):
         cls.tempdir = tempfile.mkdtemp()
-        full_matrix_path = os.path.join(cls.tempdir, 'full-matrix.hdf5')
-        full_matrix = h5py.File(full_matrix_path, 'w')
-        ds = numpy.ndarray(cls.FULL_MATRIX_SHAPE, disagg_subsets.DATA_TYPE)
+        cls.full_matrix_path = os.path.join(cls.tempdir, 'full-matrix.hdf5')
+        full_matrix = h5py.File(cls.full_matrix_path, 'w')
+        ds = numpy.ndarray([cls.NLAT - 1, cls.NLON - 1, cls.NMAG - 1,
+                            cls.NEPS - 1, cls.NTRT],
+                           disagg_subsets.DATA_TYPE)
         cls.read_data_file(cls.FULL_MATRIX_DATA, ds)
         full_matrix.create_dataset(disagg_subsets.FULL_MATRIX_DS_NAME, data=ds)
 
@@ -50,6 +53,10 @@ class SubsetExtractionTestCase(unittest.TestCase):
     def read_data_file(cls, data_filename, target_dataset):
         data = open(helpers.get_data_path('DisaggSubsets/%s' % data_filename))
         numbers = (float(line.split()[-1]) for line in data)
+        if len(target_dataset.shape) == 1:
+            for i, number in enumerate(numbers):
+                target_dataset[i] = number
+            return
         stack = [iter(target_dataset)]
         while stack:
             try:
@@ -64,5 +71,56 @@ class SubsetExtractionTestCase(unittest.TestCase):
                 stack.append(iter(arr))
         assert len(list(numbers)) == 0
 
-    def test(self):
+    def _test_pmf(self, name, datafile, result_shape,
+                  site=None, distance_bin_edges=None):
+        target_path = os.path.join(self.tempdir, '%s.hdf5' % name)
+        disagg_subsets.extract_subsets(
+            site, self.full_matrix_path, self.FULL_MATRIX_SHAPE,
+            distance_bin_edges, target_path, [name]
+        )
+        expected_result = numpy.ndarray(result_shape)
+        self.read_data_file(datafile, expected_result)
+        result = h5py.File(target_path, 'r')[name].value
+        helpers.assertDeepAlmostEqual(self, expected_result, result)
+
+    def test_magpmf(self):
+        self._test_pmf('magpmf', 'magnitudePMF.dat', [self.NMAG - 1])
+
+    def test_distpmf(self):
+        #self._test_pmf('distpmf', 'distancePMF.dat', [?])
         pass
+
+    def test_trtpmf(self):
+        self._test_pmf('trtpmf', 'tectonicRegionTypePMF.dat', [self.NTRT])
+
+    def test_magdistpmf(self):
+        #self._test_pmf('magdistpmf', 'magnitudeDistancePMF.dat', [?])
+        pass
+
+    def test_magdistepspmf(self):
+        #self._test_pmf('magdistepspmf', 'magnitudeDistanceEpsilonPMF.dat',
+        #               [?])
+        pass
+
+    def test_latlonpmf(self):
+        self._test_pmf('latlonpmf', 'latitudeLongitudePMF.dat',
+                       [self.NLAT - 1, self.NLON - 1])
+
+    def test_latlonmagpmf(self):
+        self._test_pmf('latlonmagpmf', 'latitudeLongitudeMagnitudePMF.dat',
+                       [self.NLAT - 1, self.NLON - 1, self.NMAG - 1])
+
+    def test_latlonmagepspmf(self):
+        self._test_pmf('latlonmagepspmf',
+                       'latitudeLongitudeMagnitudeEpsilonPMF.dat',
+                       [self.NLAT - 1, self.NLON - 1,
+                        self.NMAG - 1, self.NEPS - 1])
+
+    def test_magtrtpmf(self):
+        self._test_pmf('magtrtpmf', 'magnitudeTectonicRegionTypePMF.dat',
+                       [self.NMAG - 1, self.NTRT])
+
+    def test_latlontrtpmf(self):
+        self._test_pmf('latlontrtpmf',
+                       'latitudeLongitudeTectonicRegionTypePMF.dat',
+                       [self.NLAT - 1, self.NLON - 1, self.NTRT])
