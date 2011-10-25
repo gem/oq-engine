f62507cedf12d53af5aabf05fe2a3d654faf49df
Lars Butler <Lars.Butler@gmail.com>
2011-10-25 15:32:07 +0200

    disagg task core function is nearly done. I need to adjust the test input data for a better blackbox test
    Former-commit-id: 894e6240afae862991cda92f209fae0a2097eaae [formerly 894e6240afae862991cda92f209fae0a2097eaae [formerly dfa2e85a0074ef524a77f24fe8903c6d712f5ba7]]
    Former-commit-id: e4114ab73483381604246acae813300fc0a76b9b
    Former-commit-id: 55b51a77442f539be8d7f66d60de6501ed82dad0

--- openquake/hazard/disaggregation.py
+++ openquake/hazard/disaggregation.py
@@ -16,16 +16,20 @@
 # <http://www.gnu.org/licenses/lgpl-3.0.txt> for a copy of the LGPLv3 License.
 
 
+from math import log
+
 from openquake import java
 from openquake import job
 
 from openquake.job import config as job_cfg
 from openquake.job import config_text_to_list
 from openquake.utils import config
-from openquake.hazard.opensha import generate_erf, generate_gmpe_map
+from openquake.hazard.general import (
+    generate_erf, generate_gmpe_map, set_gmpe_params)
 
 
-def compute_disagg_matrix(job_id, site, realization, poe):
+@java.jexception
+def compute_disagg_matrix(job_id, site, realization, poe, result_dir):
     """ Compute a complete 5D Disaggregation matrix. This task leans heavily
     on the DisaggregationCalculator (in the OpenQuake Java lib) to handle this
     computation.
@@ -38,21 +42,41 @@ def compute_disagg_matrix(job_id, site, realization, poe):
     :type realization: `int`
     :param poe: Probability of Exceedence
     :type poe: `float`
+    :param result_dir: location for the Java code to write the matrix in an
+        HDF5 file (in a distributed environment, this should be the path of a
+        mounted NFS)
 
-    :returns: ???
+    :returns: 2-tuple of (ground_motion_value, path_to_h5_matrix_file)
     """
+    def make_disagg_calc(a_job):
+        """Inner function to encapsulate DisaggregationCalculator creation.
+
+        :param a_job: a job
+        :type a_job: :class:`openquake.job.Job` instance
+        :returns: jpype instance of `org.gem.calc.DisaggregationCalculator`
+        """
+
+        lat_bin_lims = config_text_to_list(
+            a_job[job_cfg.LAT_BIN_LIMITS], float)
+        lon_bin_lims = config_text_to_list(
+            a_job[job_cfg.LON_BIN_LIMITS], float)
+        mag_bin_lims = config_text_to_list(
+            a_job[job_cfg.MAG_BIN_LIMITS], float)
+        eps_bin_lims = config_text_to_list(
+            a_job[job_cfg.EPS_BIN_LIMITS], float)
+
+        jd = list_to_jdouble_array
+
+        disagg_calc = java.jclass('DisaggregationCalculator')(
+            jd(lat_bin_lims), jd(lon_bin_lims),
+            jd(mag_bin_lims), jd(eps_bin_lims))
+
+        return disagg_calc
+
+
     the_job = job.Job.from_kvs(job_id)
 
-    lat_bin_lims = config_text_to_list(the_job[job_cfg.LAT_BIN_LIMITS], float)
-    lon_bin_lims = config_text_to_list(the_job[job_cfg.LON_BIN_LIMITS], float)
-    mag_bin_lims = config_text_to_list(the_job[job_cfg.MAG_BIN_LIMITS], float)
-    eps_bin_lims = config_text_to_list(the_job[job_cfg.EPS_BIN_LIMITS], float)
-
-    jd = list_to_jdouble_array
-
-    disagg_calc = java.jclass('DisaggregationCalculator')(
-        jd(lat_bin_lims), jd(lon_bin_lims),
-        jd(mag_bin_lims), jd(eps_bin_lims))
+    disagg_calc = make_disagg_calc(the_job)
 
     cache = java.jclass('KVS')(
         config.get('kvs', 'host'),
@@ -60,14 +84,23 @@ def compute_disagg_matrix(job_id, site, realization, poe):
 
     erf = generate_erf(job_id, cache)
     gmpe_map = generate_gmpe_map(job_id, cache)
-    imls = job.config_text_to_list(the_job['INTENSITY_MEASURE_LEVELS'], float)
+    set_gmpe_params(gmpe_map, the_job.params)
+
+    iml_arraylist = java.jclass('ArrayList')()
+    iml_vals = job.config_text_to_list(
+        the_job['INTENSITY_MEASURE_LEVELS'], float)
+    # Map `log` (natural log) to each IML value before passing to the
+    # calculator.
+    iml_vals = [log(x) for x in iml_vals]
+    iml_arraylist.addAll(iml_vals)
     vs30_value = float(the_job['REFERENCE_VS30_VALUE'])
     depth_to_2pt5 = float(the_job['REFERENCE_DEPTH_TO_2PT5KM_PER_SEC_PARAM'])
 
-    matrix_5d = disagg_calc.computeMatrix(
-        site.latitude, site.longitude, erf, gmpe_map, poe, imls, vs30_value,
-        depth_to_2pt5)
-    return matrix_5d  # TODO: no, don't actually return this
+    matrix_result = disagg_calc.computeAndWriteMatrix(
+        site.latitude, site.longitude, erf, gmpe_map, poe, iml_arraylist,
+        vs30_value, depth_to_2pt5, result_dir)
+
+    return (matrix_result.getGMV(), matrix_result.getMatrixPath())
 
 def list_to_jdouble_array(float_list):
     """Convert a 1D list of floats to a 1D Java Double[] (as a jpype object).
