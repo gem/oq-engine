065106738c36edc2f23305b4b17b0999e3a879de
Anton Gritsay <anton@openquake.org>
2011-10-25 16:18:04 +0200

    disagg: making pylint and pep8 happy

--- openquake/hazard/disagg/__init__.py
+++ openquake/hazard/disagg/__init__.py
@@ -13,3 +13,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # version 3 along with OpenQuake.  If not, see
 # <http://www.gnu.org/licenses/lgpl-3.0.txt> for a copy of the LGPLv3 License.
+
+"""
+Disaggregation hazard calculator.
+"""
