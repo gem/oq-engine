22f75e4300d602d4aad785bce2e8c78fd55ae7fa
Muharem Hrnjadovic <mh@foldr3.com>
2011-11-16 11:31:36 +0100

    Added 1st test

---
+++ tests/utils_stats_unittest.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright (c) 2010-2011, GEM Foundation.
+#
+# OpenQuake is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3
+# only, as published by the Free Software Foundation.
+#
+# OpenQuake is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License version 3 for more details
+# (a copy is included in the LICENSE file that accompanied this code).
+#
+# You should have received a copy of the GNU Lesser General Public License
+# version 3 along with OpenQuake.  If not, see
+# <http://www.gnu.org/licenses/lgpl-3.0.txt> for a copy of the LGPLv3 License.
+
+
+"""
+Unit tests for the utils.stats module.
+"""
+
+import redis
+import unittest
+
+from openquake.utils import config
+from openquake.utils import stats
+
+class ProgressIndicatorTestCase(unittest.TestCase):
+    """Tests the behaviour of utils.stats.progress_indicator()."""
+
+    def __init__(self, *args, **kwargs):
+        super(ProgressIndicatorTestCase, self).__init__(*args, **kwargs)
+        host = config.get("kvs", "host")
+        port = config.get("kvs", "port")
+        port = int(port) if port else 6379
+        stats_db = config.get("kvs", "stats_db")
+        stats_db = int(stats_db) if stats_db else 15
+        args = {"host": host, "port": port, "db": stats_db}
+        self.redis = redis.Redis(**args)
+
+    def test_success_stats(self):
+        """
+        The success counter is incremented when the wrapped function
+        terminates without raising an exception.
+        """
+
+        @stats.progress_indicator
+        def no_exception(job_id):
+            pass
+
+        previous_value = self.redis.get(stats.key_name(11, no_exception))
+        previous_value = int(previous_value) if previous_value else 0
+
+        no_exception(11)
+
+        value = int(self.redis.get(stats.key_name(11, no_exception)))
+        self.assertEqual(1, (value - previous_value))
