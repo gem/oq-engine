2232a54a4efe45c1f6472d0c5a9e26c45187e182
Lars Butler <Lars.Butler@gmail.com>
2011-11-24 11:39:13 +0100

    the core calculator is coded; I still need to validate input (+ tests) and add more documentation
    Former-commit-id: 60f41f29e3d3b55efe293fea36d1f2b6e20c2c07

--- java/org/gem/calc/DisaggregationCalculator.java
+++ java/org/gem/calc/DisaggregationCalculator.java
@@ -28,6 +28,7 @@ import org.opensha.sha.imr.param.SiteParams.Vs30_TypeParam;
 import org.opensha.sha.imr.param.SiteParams.Vs30_TypeParam.Vs30Type;
 import org.opensha.sha.util.TectonicRegionType;
 import static org.gem.Utils.digitize;
+import static org.gem.calc.CalcUtils.getGMV;
 import static org.apache.commons.collections.CollectionUtils.forAllDo;
 
 import org.gem.calc.DisaggregationResult;
@@ -348,42 +349,6 @@ public class DisaggregationCalculator {
             }
         }
         return closest;
-    }
-
-    /**
-     * Extract a GMV (Ground Motion Value) for a given curve and PoE
-     * (Probability of Exceedance) value.
-     *
-     * IML (Intensity Measure Level) values make up the X-axis of the curve.
-     * IMLs are arranged in ascending order. The lower the IML value, the
-     * higher the PoE value (Y value) on the curve. Thus, it is assumed that
-     * hazard curves will always have a negative slope.
-     *
-     * If the input poe value is > the max Y value in the curve, extrapolate
-     * and return the X value corresponding to the max Y value (the first Y
-     * value).
-     * If the input poe value is < the min Y value in the curve, extrapolate
-     * and return the X value corresponding to the min Y value (the last Y
-     * value).
-     * Otherwise, interpolate an X value in the curve given the input PoE.
-     * @param hazardCurve
-     * @param poe Probability of Exceedance value
-     * @return GMV corresponding to the input poe
-     */
-    public static Double getGMV(DiscretizedFuncAPI hazardCurve, double poe)
-    {
-        if (poe > hazardCurve.getY(0))
-        {
-            return hazardCurve.getX(0);
-        }
-        else if (poe < hazardCurve.getY(hazardCurve.getNum() - 1))
-        {
-            return hazardCurve.getX(hazardCurve.getNum() - 1);
-        }
-        else
-        {
-            return hazardCurve.getFirstInterpolatedX(poe);
-        }
     }
 
     /**
