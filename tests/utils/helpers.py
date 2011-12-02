d0b5eb2f9674b58a1dc64edcfe1e74fd3f4bfc49
Lars Butler <Lars.Butler@gmail.com>
2011-12-02 15:48:52 +0100

    Fixed schema path references.
    Former-commit-id: 4cdc39d57170acf4dd6f6ab3281a8dbcedf2b79a

--- tests/utils/helpers.py
+++ tests/utils/helpers.py
@@ -61,7 +61,7 @@ OUTPUT_DIR = os.path.abspath(os.path.join(
     os.path.dirname(__file__), '../data/output'))
 
 SCHEMA_DIR = os.path.abspath(os.path.join(
-    os.path.dirname(__file__), '../../openquake/nrml/schema'))
+    os.path.dirname(__file__), '../../openquake/nrml/schema/0.2'))
 
 SCHEMA_EXAMPLES_DIR = os.path.abspath(os.path.join(
     SCHEMA_DIR, 'examples'))
