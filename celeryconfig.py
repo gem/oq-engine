eaa085d66f97b838a996c250f1a38947b3881aef
Lars Butler <Lars.Butler@gmail.com>
2012-01-05 11:47:19 +0100

    Fixed celeryconfig imports
    Former-commit-id: 5371707f45db1c4d6e41df72c7f7c3f2c50a5c1b

--- celeryconfig.py
+++ celeryconfig.py
@@ -46,6 +46,8 @@ CELERY_RESULT_BACKEND = "amqp"
 
 
 CELERY_IMPORTS = (
-    "openquake.risk.job", "openquake.hazard.tasks", "tests.utils.tasks")
+    "openquake.hazard.disagg.core", "openquake.hazard.disagg.subsets",
+    "openquake.hazard.opensha", "openquake.hazard.uhs.core",
+    "openquake.risk.job.general", "tests.utils.tasks")
 
 os.environ["DJANGO_SETTINGS_MODULE"] = "openquake.settings"
