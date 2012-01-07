990da73dba3eaab2d01efd03f4b21d33df3269d9
Muharem Hrnjadovic <mh@foldr3.com>
2012-01-07 19:24:14 +0100

    even more pylint fixes
    Former-commit-id: cad50439f924dbd813389eae0193dc74fb1ce3cc [formerly 27b1a7d5951d394193ccdccc9f94d9b76fd7de36]
    Former-commit-id: 5ca6bd27737fc0387f40d389535f37d4e267dad3

--- openquake/output/__init__.py
+++ openquake/output/__init__.py
@@ -13,3 +13,6 @@
 # You should have received a copy of the GNU Lesser General Public License
 # version 3 along with OpenQuake.  If not, see
 # <http://www.gnu.org/licenses/lgpl-3.0.txt> for a copy of the LGPLv3 License.
+
+
+"""This is needed for imports to work."""
