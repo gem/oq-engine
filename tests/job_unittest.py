d122b769e5fb8a5d78d80d0523095c2a7424c737
Lars Butler <Lars.Butler@gmail.com>
2012-01-11 16:13:37 +0100

    pylint/pep8/pyflakes
    Former-commit-id: 83c8557849342d41f327c92fe1774bca25c99085

--- tests/job_unittest.py
+++ tests/job_unittest.py
@@ -881,7 +881,7 @@ class RunJobTestCase(unittest.TestCase):
             job.read_sites_from_exposure(test_job))
 
     def test_supervisor_is_spawned(self):
-        with patch('openquake.engine._job_from_file') as from_file:
+        with patch('openquake.engine._job_from_file'):
 
             before_launch = engine._launch_calculation
             try:
