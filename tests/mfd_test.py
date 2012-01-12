967d5c2a1139daed5c2df397bebf4f82998da698
Anton Gritsay <anton@angri.ru>
2012-01-12 15:46:12 +0100

    tests/mfd: tested truncated_gr

--- tests/mfd_test.py
+++ tests/mfd_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from nhe.mfd import EvenlyDiscretized, MFDError
+from nhe.mfd import EvenlyDiscretized, MFDError, TruncatedGR
 
 
 class BaseConstraintsTestCase(unittest.TestCase):
@@ -45,3 +45,76 @@ class EvenlyDiscretizedMFDGetRatesTestCase(unittest.TestCase):
         )
         self.assertEqual(evenly_discretized.get_annual_occurrence_rates(),
                          [(0.2, 2.1), (0.5, 2.4), (0.8, 5.3)])
+
+
+class TruncatedGRConstraintsTestCase(BaseConstraintsTestCase):
+    def test_negative_min_mag(self):
+        self.assert_constraint_violation(
+            TruncatedGR,
+            min_mag=-1, max_mag=2, bin_width=0.4, a_val=1, b_val=2
+        )
+
+    def test_min_mag_higher_than_max_mag(self):
+        self.assert_constraint_violation(
+            TruncatedGR,
+            min_mag=2.4, max_mag=2, bin_width=0.4, a_val=1, b_val=2
+        )
+
+    def test_negative_bin_width(self):
+        self.assert_constraint_violation(
+            TruncatedGR,
+            min_mag=1, max_mag=2, bin_width=-0.4, a_val=1, b_val=2
+        )
+
+    def test_non_positive_b_val(self):
+        self.assert_constraint_violation(
+            TruncatedGR,
+            min_mag=1, max_mag=2, bin_width=0.4, a_val=1, b_val=-2
+        )
+        self.assert_constraint_violation(
+            TruncatedGR,
+            min_mag=1, max_mag=2, bin_width=0.4, a_val=1, b_val=0
+        )
+
+
+class TruncatedGRMFDGetRatesTestCase(unittest.TestCase):
+    def _test(self, expected_rates, rate_tolerance, **kwargs):
+        mfd = TruncatedGR(**kwargs)
+        actual_rates = mfd.get_annual_occurrence_rates()
+        self.assertEqual(len(actual_rates), len(expected_rates))
+        for i, (mag, rate) in enumerate(actual_rates):
+            expected_mag, expected_rate = expected_rates[i]
+            self.assertAlmostEqual(mag, expected_mag, delta=1e-14)
+            self.assertAlmostEqual(rate, expected_rate, delta=rate_tolerance)
+
+    def test_1_different_min_mag_and_max_mag(self):
+        expected_rates = [
+            (5.5, 2.846049894e-5),
+            (6.5, 2.846049894e-6),
+            (7.5, 2.846049894e-7),
+        ]
+        self._test(expected_rates=expected_rates, rate_tolerance=1e-14,
+                   min_mag=5.0, max_mag=8.0, bin_width=1.0,
+                   a_val=0.5, b_val=1.0)
+
+    def test_2_different_min_mag_and_max_mag(self):
+        expected_rates = [
+            (5.5, 2.846049894e-5),
+            (6.5, 2.846049894e-6),
+            (7.5, 2.846049894e-7),
+        ]
+        self._test(expected_rates=expected_rates, rate_tolerance=1e-14,
+                   min_mag=5.1, max_mag=7.9, bin_width=1.0,
+                   a_val=0.5, b_val=1.0)
+
+    def test_3_equal_min_mag_and_max_mag(self):
+        expected_rates = [(6.5, 2.307675159e-7)]
+        self._test(expected_rates=expected_rates, rate_tolerance=1e-14,
+                   min_mag=6.5, max_mag=6.5, bin_width=0.1,
+                   a_val=0.5, b_val=1.0)
+
+    def test_4_equal_min_mag_and_max_mag(self):
+        expected_rates = [(6.5, 2.307675159e-7)]
+        self._test(expected_rates=expected_rates, rate_tolerance=1e-14,
+                   min_mag=6.49, max_mag=6.51, bin_width=0.1,
+                   a_val=0.5, b_val=1.0)
