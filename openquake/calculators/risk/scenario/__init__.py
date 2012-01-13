7833ed792545b95d8b9cd00143dcd2dec7fbb0c9
Lars Butler <Lars.Butler@gmail.com>
2012-01-13 16:21:00 +0100

    Moved scenario risk calculator to new location and folded the aggregate loss curve code into the event_based core risk module
    Former-commit-id: 9e30611d7a63d737707346810e29a9c7bc684db6

--- openquake/calculators/risk/scenario/__init__.py
+++ openquake/calculators/risk/scenario/__init__.py
@@ -14,19 +14,4 @@
 # version 3 along with OpenQuake.  If not, see
 # <http://www.gnu.org/licenses/lgpl-3.0.txt> for a copy of the LGPLv3 License.
 
-
-"""Information about the calculators available for the Risk engine."""
-
-
-from openquake.calculators.risk.classical.core import ClassicalPSHABasedMixin
-from openquake.calculators.risk.event_based.core import ProbabilisticEventMixin
-from openquake.calculators.risk.scenario.core import ScenarioEventBasedMixin
-
-
-CALCULATORS = {
-    'classical': ClassicalPSHABasedMixin,
-    'classical_bcr': ClassicalPSHABasedMixin,
-    'event_based': ProbabilisticEventMixin,
-    'event_based_bcr': ProbabilisticEventMixin,
-    'scenario': ScenarioEventBasedMixin,
-}
+"""Scenario Risk calculator."""
