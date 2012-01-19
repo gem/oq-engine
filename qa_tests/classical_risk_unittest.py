43e35ce1e8f194d1c390961d077b4319b4fda9ed
Lars Butler <Lars.Butler@gmail.com>
2012-01-19 18:24:56 +0100

    Solid draft of the classical risk QA test; still debugging result discrepancies

--- qa_tests/classical_risk_unittest.py
+++ qa_tests/classical_risk_unittest.py
@@ -18,6 +18,7 @@
 import os
 import unittest
 
+from openquake.db.models import LossCurveData
 from openquake.db.models import OqCalculation
 
 from tests.utils import helpers
@@ -30,9 +31,49 @@ class ClassicalRiskQATestCase(unittest.TestCase):
         """Run the full hazard+risk job, serialize all results to the db,
         and verify them against expected values."""
 
-        expected_results = None
+        self.maxDiff =None
+
+        expected_lc_poes = [
+            0.03881,
+            0.03879,
+            0.03793,
+            0.03485,
+            0.03059,
+            0.02644,
+            0.02282,
+            0.01975,
+            0.01717,
+            0.01501,
+            0.01323,
+            0.01054,
+            0.00862,
+            0.00713,
+            0.00591,
+            0.00491,
+            0.00353,
+            0.00274,
+            0.00219,
+            0.00168,
+            0.00119,
+            0.00051,
+            0.00026,
+            0.00019,
+            0.00015,
+            0.00009,
+            0.00005,
+            0.00003,
+            0.00001,
+            0.00001,
+            0.00000, 
+        ]
 
         helpers.run_job(helpers.demo_file(
-            os.path.join('classical_psha_based_risk', 'config.gem')))
+            os.path.join('classical_psha_based_risk', 'config.gem')),
+            debug='debug')
 
-        calc_id = OqCalculation.objects.latest('id')
+        calculation = OqCalculation.objects.latest('id')
+
+        loss_curve = LossCurveData.objects.get(
+            loss_curve__output__oq_calculation=calculation.id)
+
+        self.assertEqual(expected_lc_poes, loss_curve.poes)
