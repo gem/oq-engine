715f87caf9efcf12d18420acb823e85e0ce3be11
Anton Gritsay <anton@angri.ru>
2012-01-19 15:30:18 +0100

    const: added doctests

--- nhe/const.py
+++ nhe/const.py
@@ -6,10 +6,17 @@ Module :mod:`nhe.const` defines various constants.
 class ConstantContainer(object):
     """
     Class that doesn't support instantiation.
+
+    >>> ConstantContainer()
+    Traceback (most recent call last):
+        ...
+    AssertionError: do not create objects ConstantContainer, \
+use class properties instead
     """
     def __init__(self):
         raise AssertionError('do not create objects %s, '
-                             'use class properties instead' % type(self))
+                             'use class properties instead'
+                             % type(self).__name__)
 
 
 class TRT(ConstantContainer):
@@ -22,3 +29,19 @@ class TRT(ConstantContainer):
     SUBDUCTION_INTERFACE = 'Subduction Interface'
     SUBDUCTION_INTRASLAB = 'Subduction IntraSlab'
     VOLCANIC = 'Volcanic'
+
+    ALL = set((ACTIVE_SHALLOW_CRUST, STABLE_CONTINENTAL,
+               SUBDUCTION_INTERFACE, SUBDUCTION_INTRASLAB,
+               VOLCANIC))
+
+    @classmethod
+    def is_valid(cls, value):
+        """
+        Return ``True`` if ``value`` references a correct tectonic region type.
+
+        >>> TRT.is_valid('Active Shallow Crust')
+        True
+        >>> TRT.is_valid('Active Shallo Crust')
+        False
+        """
+        return value in cls.ALL
