df65e46807cb914cc237b158fb75a72c3d5dfb74
Anton Gritsay <anton@angri.ru>
2012-01-23 18:35:58 +0100

    source/point: started documenting the code

--- nhe/source/point.py
+++ nhe/source/point.py
@@ -59,6 +59,27 @@ class PointSource(SeismicSource):
                     yield rupture
 
     def _get_rupture_dimensions(self, mag, nodal_plane):
+        """
+        Calculate and return the rupture length and width
+        for given magnitude ``mag`` and nodal plane.
+
+        :param nodal_plane:
+            Instance of :class:`nhe.common.nodalplane.NodalPlane`.
+        :returns:
+            Tuple of two items: rupture length in width in km.
+
+        The rupture area is calculated using method
+        :meth:`~nhe.msr.base.BaseMSR.get_median_area` of source's
+        magnitude-scaling relationship. In any case the returned
+        dimensions multiplication is equal to that value. Than
+        the area is decomposed to length and width with respect
+        to source's rupture aspect ratio.
+
+        If calculated rupture width being inclined by nodal plane's
+        dip angle would not fit in between upper and lower seismogenic
+        depth, the rupture width is shrunken to a maximum possible
+        and rupture length is extended to preserve the same area.
+        """
         area = self.magnitude_scaling_relationship.get_median_area(
             mag, nodal_plane.rake
         )
@@ -75,41 +96,89 @@ class PointSource(SeismicSource):
         return rup_length, rup_width
 
     def get_rupture_surface(self, mag, nodal_plane, hypocenter):
+        """
+        Create and return rupture surface object with given properties.
+
+        :param mag:
+            Magnitude value, used to calculate rupture dimensions,
+            see :meth:`_get_rupture_dimensions`.
+        :param nodal_plane:
+            Instance of :class:`nhe.common.nodalplane.NodalPlane`
+            describing the rupture orientation.
+        :param hypocenter:
+            Point representing rupture's hypocenter.
+        :returns:
+            Instance of :class:`nhe.source.rupture.EvenlyGriddedSurface`.
+        """
+        assert self.upper_seismogenic_depth <= hypocenter.depth \
+               and self.lower_seismogenic_depth >= hypocenter.depth
         rdip = math.radians(nodal_plane.dip)
 
+        # precalculated azimuth values for horizontal-only and vertical-only
+        # moves from one point to another on the plane defined by strike
+        # and dip:
         azimuth_right = nodal_plane.strike
         azimuth_down = (azimuth_right + 90) % 360
         azimuth_left = (azimuth_down + 90) % 360
         azimuth_up = (azimuth_left + 90) % 360
 
         rup_length, rup_width = self._get_rupture_dimensions(mag, nodal_plane)
+        # calculate the height of the rupture being projected
+        # on the vertical plane:
         rup_proj_height = rup_width * math.sin(rdip)
+        # and it's width being projected on the horizontal one:
         rup_proj_width = rup_width * math.cos(rdip)
 
+        # half height of the vertical component of rupture width
+        # is the vertical distance between the rupture geometrical
+        # center and it's upper and lower borders:
         hheight = rup_proj_height / 2
+        # calculate how much shallower the upper border of the rupture
+        # is than the upper seismogenic depth:
         vshift = self.upper_seismogenic_depth - hypocenter.depth + hheight
+        # if it is shallower (vshift > 0) than we need to move the rupture
+        # by that value vertically.
         if vshift < 0:
+            # the top edge is below upper seismogenic depth. now we need
+            # to check that we do not cross the lower border.
             vshift = self.lower_seismogenic_depth - hypocenter.depth - hheight
             if vshift > 0:
+                # the bottom edge of the rupture is above the lower sesmogenic
+                # depth. that means that we don't need to move the rupture
+                # as it fits inside seismogenic layer.
                 vshift = 0
-        rupture_center = hypocenter
+            # if vshift < 0 than we need to move the rupture up by that value.
 
+        # now we need to find the position of rupture's geometrical center.
+        # in any case the hypocenter point must lie on the surface, however
+        # the rupture center might be off (below or above) along the dip.
+        rupture_center = hypocenter
         if vshift != 0:
+            # we need to move the rupture center to make the rupture fit
+            # inside the seismogenic layer.
             hshift = abs(vshift / math.tan(rdip))
             rupture_center = rupture_center.point_at(
                 horizontal_distance=hshift, vertical_increment=vshift,
                 azimuth=(azimuth_up if vshift < 0 else azimuth_down)
             )
 
+        # now we can find four corner points of the rupture rectangle.
+        # we find the point that is on the same depth than the rupture
+        # center but lies on the right border of its surface:
         right_center = rupture_center.point_at(
             horizontal_distance=rup_length / 2.0,
             vertical_increment=0, azimuth=azimuth_right
         )
+        # than we get the right bottom corner:
         right_bottom = right_center.point_at(
             horizontal_distance=rup_proj_width / 2.0,
             vertical_increment=rup_proj_height / 2.0,
             azimuth=azimuth_down
         )
+        # and other three points can be easily found by stepping from
+        # already known points horizontally only by rupture length
+        # (to get to left point from right one) or horizontally and
+        # vertically (to get to top edge from bottom).
         right_top = right_bottom.point_at(horizontal_distance=rup_proj_width,
                                           vertical_increment=-rup_proj_height,
                                           azimuth=azimuth_up)
