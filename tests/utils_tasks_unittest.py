812b26731b8d15b53a6fcc5a722bd881cd04ac17
Muharem Hrnjadovic <mh@foldr3.com>
2012-01-24 17:08:12 +0100

    code refactored, a part of the unit tests passes
    Former-commit-id: f1dbac54de91b250bbb46858b74f3b738f8c7ef1 [formerly 2f21e06824eeebb454ba96b1e516fdbe3bd37932]
    Former-commit-id: 55b49506ecd4747f0aed5b3a7574ad19d116789f

--- tests/utils_tasks_unittest.py
+++ tests/utils_tasks_unittest.py
@@ -37,8 +37,8 @@ from tests.utils.helpers import patch
 from tests.utils.helpers import TestStore
 from tests.utils.tasks import (
     failing_task, ignore_result, just_say_hello, reflect_args,
-    reflect_data_to_be_processed, single_arg_called_a,
-    reflect_data_with_task_index)
+    reflect_data_to_be_processed, single_arg_called_a)
+
 
 # The keyword args below are injected by the celery framework.
 celery_injected_kwargs = set((
@@ -46,30 +46,13 @@ celery_injected_kwargs = set((
     "task_name", "task_retries"))
 
 
-def actual_kwargs(kwargs):
-    """Filter the keyword arguments injected by celery.
-
-    :param dict kwargs: The keyword arguments thrown back by a celery task.
-    :returns: The keyword arguments that were actually passed.
-    :rtype: dict
-    """
-    filtered_args = []
-    for k, v in kwargs.iteritems():
-        if k not in celery_injected_kwargs:
-            filtered_args.append((k, v))
-    return dict(filtered_args)
-
-
 class DistributeTestCase(unittest.TestCase):
     """Tests the behaviour of utils.tasks.distribute()."""
 
-    def __init__(self, *args, **kwargs):
-        super(DistributeTestCase, self).__init__(*args, **kwargs)
-
     def test_distribute_uses_the_specified_number_of_subtasks(self):
-        """The specified number of subtasks is actually spawned."""
+        """One subtasks per data item is actually spawned."""
         expected = ["hello"] * 5
-        result = tasks.distribute(5, just_say_hello, ("data", range(5)))
+        result = tasks.distribute(just_say_hello, ("data", range(5)))
         self.assertEqual(expected, result)
 
     def test_distribute_with_no_other_args(self):
@@ -77,15 +60,10 @@ class DistributeTestCase(unittest.TestCase):
         # We expect the subtasks to see no positional arguments. The
         # data to be processed is passed in the keyword arguments.
         expected = [
-            ((), {"data_to_process": [100]}),
-            ((), {"data_to_process": [101]})]
-        actual = []
+            ((), {"data_to_process": 100}), ((), {"data_to_process": 101})]
         result = tasks.distribute(
-            2, reflect_args, ("data_to_process", [100, 101]))
-        # Remove celery-injected keyword arguments.
-        for args, kwargs in result:
-            actual.append((args, actual_kwargs(kwargs)))
-        self.assertEqual(expected, actual)
+            reflect_args, ("data_to_process", [100, 101]))
+        self.assertEqual(expected, result)
 
     def test_distribute_with_other_args(self):
         """
@@ -99,51 +77,14 @@ class DistributeTestCase(unittest.TestCase):
         # We expect the subtasks to see the following positional and keyword
         # arguments respectively.
         expected = [
-            ((), {"data_to_process": [88], "1+1": 2, "2/1": 1}),
-            ((), {"data_to_process": [99], "1+1": 2, "2/1": 1})]
-        actual = []
+            ((), {"data_to_process": 88, "1+1": 2, "2/1": 1}),
+            ((), {"data_to_process": 99, "1+1": 2, "2/1": 1})]
 
         # Two subtasks will be spawned and just return the arguments they
         # received.
-        result = tasks.distribute(
-            2, reflect_args, ("data_to_process", [88, 99]),
-            other_args=other_args)
-        # Remove celery-injected keyword arguments.
-        for args, kwargs in result:
-            actual.append((args, actual_kwargs(kwargs)))
-        self.assertEqual(expected, actual)
-
-    def test_distribute_with_non_empty_data_and_cardinality_one(self):
-        """A single subtask will receive all the data to be processed."""
-        expected = ((), {"data_to_process": range(5)})
-        [(args, kwargs)] = tasks.distribute(
-            1, reflect_args, ("data_to_process", range(5)))
-        self.assertEqual(expected, (args, actual_kwargs(kwargs)))
-
-    def test_distribute_with_even_data_and_cardinality_above_one(self):
-        """The data set divides evenly among the subtasks in the task set."""
-        expected = (
-            (), {"data_to_process": range(2)},
-            (), {"data_to_process": range(2, 4)})
-        [(args1, kwargs1), (args2, kwargs2)] = tasks.distribute(
-            2, reflect_args, ("data_to_process", range(4)))
-        self.assertEqual(
-            expected,
-            (args1, actual_kwargs(kwargs1), args2, actual_kwargs(kwargs2)))
-
-    def test_distribute_with_noneven_data_and_cardinality_above_one(self):
-        """
-        The data set does *not* divide evenly among the subtasks in the task
-        set. The last subtask gets all the remaining data.
-        """
-        expected = (
-            (), {"data_to_process": range(2)},
-            (), {"data_to_process": range(2, 5)})
-        [(args1, kwargs1), (args2, kwargs2)] = tasks.distribute(
-            2, reflect_args, ("data_to_process", range(5)))
-        self.assertEqual(
-            expected,
-            (args1, actual_kwargs(kwargs1), args2, actual_kwargs(kwargs2)))
+        result = tasks.distribute(reflect_args, ("data_to_process", [88, 99]),
+                                  other_args=other_args)
+        self.assertEqual(expected, result)
 
     def test_distribute_with_keyword_argument_not_expected_by_task(self):
         """
@@ -151,12 +92,11 @@ class DistributeTestCase(unittest.TestCase):
         `TypeError` exception.
         """
         try:
-            tasks.distribute(2, single_arg_called_a, ("data", range(5)))
+            tasks.distribute(single_arg_called_a, ("data", range(5)))
         except Exception, exc:
             self.assertEqual(
                 "single_arg_called_a() got an unexpected keyword argument "
-                "'data'",
-                exc.args[0])
+                "'data'", exc.args[0])
         else:
             raise Exception("Exception not raised.")
 
@@ -170,7 +110,7 @@ class DistributeTestCase(unittest.TestCase):
             with patch('celery.task.sets.TaskSet.apply_async') as m2:
                 m2.return_value = mock.Mock(spec=TaskSetResult)
                 m2.return_value.join_native.side_effect = TypeError
-                tasks.distribute(2, single_arg_called_a, ("a", range(5)))
+                tasks.distribute(single_arg_called_a, ("a", range(5)))
         except Exception, exc:
             self.assertEqual((), exc.args)
         else:
@@ -179,150 +119,19 @@ class DistributeTestCase(unittest.TestCase):
     def test_distribute_with_failing_subtask(self):
         """At least one subtask failed, a `TaskFailed` exception is raised."""
         try:
-            tasks.distribute(1, failing_task, ("data", range(5)))
+            tasks.distribute(failing_task, ("data", range(5)))
         except Exception, exc:
-            self.assertEqual(range(5), exc.args[0])
+            # The exception is raised by the first task.
+            self.assertEqual(0, exc.args[0])
         else:
             raise Exception("Exception not raised.")
 
-    def test_distribute_with_too_little_data(self):
-        """
-        When the data to be processed is a list of N items and the specified
-        cardinality was M where (N < M), only N subtasks are invoked.
-        """
-        expected = ["hello"] * 3
-        result = tasks.distribute(5, just_say_hello, ("data", range(3)))
-        self.assertEqual(expected, result)
-
-    def test_distribute_returns_results_in_the_right_order(self):
+    def test_distribute_returns_results_in_right_order(self):
         """Results are returned in the right order."""
-        expected = [[0, 1], [2, 3], [4, 5, 6]]
-        result = tasks.distribute(
-            3, reflect_data_to_be_processed, ("data", range(7)))
-        self.assertEqual(expected, result)
-
-    def test_distribute_returns_flattened_results_in_right_order(self):
-        """Flattened results are returned in the right order."""
         expected = range(7)
         result = tasks.distribute(
-            3, reflect_data_to_be_processed, ("data", range(7)),
-            flatten_results=True)
+            reflect_data_to_be_processed, ("data", range(7)))
         self.assertEqual(expected, result)
-
-
-class ParallelizeTestCase(unittest.TestCase):
-    """Tests the behaviour of utils.tasks.parallelize()."""
-
-    def __init__(self, *args, **kwargs):
-        super(ParallelizeTestCase, self).__init__(*args, **kwargs)
-
-    def test_parallelize_uses_the_specified_number_of_subtasks(self):
-        """The specified number of subtasks is actually spawned."""
-        expected = ["hello"] * 5
-        result = tasks.parallelize(
-            5, just_say_hello, dict(), index_tasks=False)
-        self.assertEqual(expected, result)
-
-    def test_parallelize_with_params(self):
-        """All subtasks are invoked with the same parameters."""
-        # The keyword arguments below will be passed to *all* celery subtasks.
-        args = {"1+1": 2, "2/1": 1}
-
-        # We expect the subtasks to see the following positional and keyword
-        # arguments respectively.
-        expected = [
-            ((), {"1+1": 2, "2/1": 1}), ((), {"1+1": 2, "2/1": 1})]
-
-        # Two subtasks will be spawned and just return the arguments they
-        # received.
-        result = tasks.parallelize(2, reflect_args, args, index_tasks=False)
-        # Remove celery-injected keyword arguments.
-        actual = []
-        for args, kwargs in result:
-            actual.append((args, actual_kwargs(kwargs)))
-        self.assertEqual(expected, actual)
-
-    def test_parallelize_with_argument_not_expected_by_task(self):
-        """
-        An unexpected argument is passed to the subtask triggering a
-        `TypeError` exception.
-        """
-        try:
-            tasks.parallelize(2, single_arg_called_a, dict(data=range(5)),
-            index_tasks=False)
-        except Exception, exc:
-            self.assertEqual(
-                "single_arg_called_a() got an unexpected keyword argument "
-                "'data'",
-                exc.args[0])
-        else:
-            raise Exception("Exception not raised.")
-
-    def test_parallelize_with_failing_subtask(self):
-        """At least one subtask failed, a `TaskFailed` exception is raised."""
-        try:
-            tasks.parallelize(1, failing_task, dict(data=range(5)),
-            index_tasks=False)
-        except Exception, exc:
-            self.assertEqual(range(5), exc.args[0])
-        else:
-            raise Exception("Exception not raised.")
-
-    def test_parallelize_returns_correct_results(self):
-        """Correct results are returned."""
-        expected = [range(3)] * 3
-        result = tasks.parallelize(
-            3, reflect_data_to_be_processed, dict(data=range(3)),
-            index_tasks=False)
-        self.assertEqual(expected, result)
-
-    def test_parallelize_returns_flattened_and_correct_results(self):
-        """Flattened and correct results are returned."""
-        expected = range(3) * 3
-        result = tasks.parallelize(
-            3, reflect_data_to_be_processed, dict(data=range(3)),
-            flatten_results=True, index_tasks=False)
-        self.assertEqual(expected, result)
-
-    def test_parallelize_with_params_and_task_index(self):
-        """
-        All subtasks are invoked with the same parameters but will receive a
-        task index parameter.
-        """
-        # The keyword arguments below will be passed to *all* celery subtasks.
-        args = {"1+1": 2, "2/1": 1}
-
-        # We expect the subtasks to see the following positional and keyword
-        # arguments respectively.
-        expected = [
-            ((), {"1+1": 2, "2/1": 1, "task_index": 0}),
-            ((), {"1+1": 2, "2/1": 1, "task_index": 1})]
-
-        # Two subtasks will be spawned and just return the arguments they
-        # received.
-        result = tasks.parallelize(2, reflect_args, args)
-        # Remove celery-injected keyword arguments.
-        actual = []
-        for args, kwargs in result:
-            actual.append((args, actual_kwargs(kwargs)))
-        self.assertEqual(expected, actual)
-
-    def test_parallelize_returns_results_in_the_right_order(self):
-        """Results are returned in the right order."""
-        expected = [[5, 6, 0], [5, 6, 1], [5, 6, 2]]
-        result = tasks.parallelize(
-            3, reflect_data_with_task_index, dict(data=range(5, 7)))
-        self.assertEqual(expected, result)
-
-    def test_parallelize_with_params_not_passed_in_dict(self):
-        """The task parameters must be passed in a dictionary."""
-        try:
-            tasks.parallelize(3, reflect_data_with_task_index, range(5, 7))
-        except AssertionError, exc:
-            self.assertEqual(
-                "Parameters must be passed in a dict.", exc.args[0])
-        else:
-            raise Exception("Exception not raised.")
 
 
 class GetRunningCalculationTestCase(unittest.TestCase):
@@ -418,7 +227,7 @@ class IgnoreResultsTestCase(unittest.TestCase):
         values = [value(uid) for uid in keys]
         data = zip(keys, values)
 
-        result = tasks.distribute(5, ignore_result, ("data", data))
+        result = tasks.distribute(ignore_result, ("data", data))
         # An empty list is returned for tasks with ignore_result=True
         # and no asynchronous task handler function.
         self.assertEqual(False, bool(result))
@@ -462,7 +271,7 @@ class IgnoreResultsTestCase(unittest.TestCase):
         values = [value(uid) for uid in keys]
         data = zip(keys, values)
 
-        result = tasks.distribute(5, ignore_result, ("data", data), ath=ath)
+        result = tasks.distribute(ignore_result, ("data", data), ath=ath)
         self.assertEqual(sorted(keys), sorted(result))
 
         for key, value in data:
