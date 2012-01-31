ebd2d8be316cb4309f3079e8fda254c0092123f1
Marco Milanesi <marcom@openquake.org>
2012-01-31 16:29:39 +0100

    improved commentary

--- tests/risk_unittest.py
+++ tests/risk_unittest.py
@@ -1331,12 +1331,13 @@ class RiskCommonTestCase(unittest.TestCase):
 
     def test_compute_beta_dist(self):
 
-        # expected beta distributions provided by Vitor
 
+        # lrems provided by Vitor
         lrems = [0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1,
             0.12, 0.14, 0.16, 0.18, 0.2, 0.24, 0.28, 0.32, 0.36, 0.4, 0.48,
             0.56, 0.64, 0.72, 0.8, 0.84, 0.86, 0.88, 0.9, 1]
 
+        # expected beta distributions provided by Vitor
         expected_beta_distributions = [
             1.0, 1.0, 1.0 ,1.0, 1.0,
             0.99, 1.0, 1.0, 1.0, 1.0,
