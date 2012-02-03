67f76788ee4228dea61ae29d7357bfa6957a083f
Anton Gritsay <anton@angri.ru>
2012-02-03 11:45:54 +0100

    common/geo: factored common for line and polygon code to priv. functions

--- nhe/common/geo.py
+++ nhe/common/geo.py
@@ -227,14 +227,14 @@ class Line(object):
     """
 
     def __init__(self, points):
-        self.points = points
-
-        self._remove_duplicates()
+        self.points = _clean_points(points)
 
         if len(self.points) < 1:
             raise RuntimeError("One point needed to create a line!")
 
-        if self._intersect_itself():
+        lats = [point.latitude for point in self.points]
+        lons = [point.longitude for point in self.points]
+        if _line_intersects_itself(lons, lats):
             raise RuntimeError("Line intersects itself!")
 
     def __eq__(self, other):
@@ -245,42 +245,6 @@ class Line(object):
 
     def __len__(self):
         return len(self.points)
-
-    def _remove_duplicates(self):
-        """
-        Remove adjacent duplicates.
-        """
-
-        last = None
-        points = []
-
-        for point in self.points:
-            if point != last:
-                last = point
-                points.append(point)
-
-        self.points = points
-
-    def _intersect_itself(self):
-        """
-        Check if this line intersects itself.
-
-        :returns:
-            True if this line intersects itself, false otherwise.
-        :rtype:
-            boolean
-        """
-        if len(self.points) < 2:
-            return False
-
-        lats = numpy.array([point.latitude for point in self.points])
-        lons = numpy.array([point.longitude for point in self.points])
-
-        proj = pyproj.Proj(proj='stere', lat_0=lats[0], lon_0=lons[0])
-        xx, yy = proj(lons, lats)
-
-        linestring = shapely.geometry.LineString(zip(xx, yy))
-        return not linestring.is_simple
 
     def resample(self, section_length):
         """
@@ -343,7 +307,58 @@ class Line(object):
         return Line(resampled_points)
 
 
-def get_longitudinal_extent(lon1, lon2):
+def _clean_points(points):
+    """
+    Given a list of :class:`Point` objects, return a new list with adjacent
+    duplicate points removed.
+    """
+    if not points:
+        return points
+
+    result = [points[0]]
+    for point in points:
+        if point != result[-1]:
+            result.append(point)
+    return result
+
+
+def _line_intersects_itself(lons, lats, closed_shape=False):
+    """
+    Return ``True`` if line of points intersects itself.
+    Line with the last point repeating the first one considered
+    intersecting itself.
+
+    The line is defined by lists (or numpy arrays) of points'
+    longitudes and latitudes (depth is not taken into account).
+
+    :param closed_shape:
+        If ``True`` the line will be checked for twice: first time
+        with its original shape and second time with the points sequence
+        being shifted by one point (the last point becomes first, the
+        first turns second and so on). This is useful for checking
+        that the sequence of points defines a valid :class:`Polygon`.
+    """
+    assert len(lons) == len(lats)
+
+    if len(lons) < 2:
+        return False
+
+    west, east, north, south = _get_spherical_bounding_box(lons, lats)
+    proj = _get_stereographic_projection(west, east, north, south)
+
+    xx, yy = proj(lons, lats)
+    if not shapely.geometry.LineString(zip(xx, yy)).is_simple:
+        return True
+
+    if closed_shape:
+        xx, yy = proj(numpy.roll(lons, 1), numpy.roll(lats, 1))
+        if not shapely.geometry.LineString(zip(xx, yy)).is_simple:
+            return True
+
+    return False
+
+
+def _get_longitudinal_extent(lon1, lon2):
     """
     Return the distance between two longitude values as an angular measure.
     Parameters represent two longitude values in degrees.
@@ -354,21 +369,21 @@ def get_longitudinal_extent(lon1, lon2):
         otherwise. Absolute value of the result doesn't exceed 180 for
         valid parameters values.
 
-    >>> get_longitudinal_extent(10, 20)
+    >>> _get_longitudinal_extent(10, 20)
     10
-    >>> get_longitudinal_extent(20, 10)
+    >>> _get_longitudinal_extent(20, 10)
     -10
-    >>> get_longitudinal_extent(-10, -15)
+    >>> _get_longitudinal_extent(-10, -15)
     -5
-    >>> get_longitudinal_extent(-120, 30)
+    >>> _get_longitudinal_extent(-120, 30)
     150
-    >>> get_longitudinal_extent(-178.3, 177.7)
+    >>> _get_longitudinal_extent(-178.3, 177.7)
     -4.0
-    >>> get_longitudinal_extent(178.3, -177.7)
+    >>> _get_longitudinal_extent(178.3, -177.7)
     4.0
-    >>> get_longitudinal_extent(95, -180 + 94)
+    >>> _get_longitudinal_extent(95, -180 + 94)
     179
-    >>> get_longitudinal_extent(95, -180 + 96)
+    >>> _get_longitudinal_extent(95, -180 + 96)
     -179
     """
     extent = lon2 - lon1
@@ -377,6 +392,43 @@ def get_longitudinal_extent(lon1, lon2):
     elif extent < -180:
         extent = 360 + extent
     return extent
+
+
+def _get_spherical_bounding_box(lons, lats):
+    """
+    >>> _get_spherical_bounding_box([10, -10], [50, 60])
+    (-10, 10, 60, 50)
+    >>> _get_spherical_bounding_box([20], [-40])
+    (20, 20, -40, -40)
+    >>> _get_spherical_bounding_box([-20, 180, 179, 178], [-1, -2, 1, 2])
+    (178, -20, 2, -2)
+    """
+    north, south = numpy.max(lats), numpy.min(lats)
+    west, east = numpy.min(lons), numpy.max(lons)
+    if _get_longitudinal_extent(west, east) < 0:
+        # points are lying on both sides of the international date line
+        # (meridian 180). the actual west longitude is the lowest positive
+        # longitude and east one is the highest negative.
+        west = min(lon for lon in lons if lon > 0)
+        east = max(lon for lon in lons if lon < 0)
+    return west, east, north, south
+
+
+def _get_stereographic_projection(west, east, north, south):
+    """
+    >>> t = lambda *co: sorted(_get_stereographic_projection(*co).srs.split())
+    >>> t(10, 16, -20, 30)
+    ['+lat_0=5.0', '+lon_0=13.0', '+proj=stere', '+units=m']
+    >>> t(-20, 40, 55, 56)
+    ['+lat_0=55.5', '+lon_0=10.0', '+proj=stere', '+units=m']
+    >>> t(177.6, -175.8, -10, 10)
+    ['+lat_0=0.0', '+lon_0=-179.1', '+proj=stere', '+units=m']
+    """
+    middle_lat = (north + south) / 2.0
+    middle_lon = west + _get_longitudinal_extent(west, east) / 2.0
+    if middle_lon > 180:
+        middle_lon = middle_lon - 360
+    return pyproj.Proj(proj='stere', lat_0=middle_lat, lon_0=middle_lon)
 
 
 class Polygon(object):
@@ -392,47 +444,47 @@ class Polygon(object):
     LONGITUDAL_DISCRETIZATION = 1
 
     def __init__(self, points):
-        # TODO: unittest this
-        if not len(points) >= 3:
-            raise RuntimeError('polygon must have at least 3 points')
-        # verify that points define a correct line which doesn't
-        # intersect itself (and also get the list of points that
-        # is freed of duplicates)
-        points = Line(points).points
-        # verify that the polygon doesn't intersect itself after
-        # being closed
-        Line(points[1:] + [points[0]])
+        points = _clean_points(points)
 
-        self.lons = numpy.array([point.longitude for point in points])
-        self.lats = numpy.array([point.latitude for point in points])
+        if not len(points) >= 3:
+            raise RuntimeError('polygon must have at least 3 unique vertices')
+
+        self.lons = numpy.array([float(point.longitude) for point in points])
+        self.lats = numpy.array([float(point.latitude) for point in points])
         self.num_points = len(points)
 
-    def discretize(self, mesh_spacing):
-        """
-        Get a generator of uniformly spaced points inside the polygon area
-        with distance of ``mesh_spacing`` km between.
-        """
-        # cast from km to m
-        mesh_spacing *= 1e3
+        if _line_intersects_itself(self.lons, self.lats, closed_shape=True):
+            raise RuntimeError('polygon perimeter intersects itself')
 
-        # in order to find the higher and lower latitudes that the
-        # polygon touches we need to connect vertices by great circle
-        # arcs. if two points lie on the same parallel, the points
-        # in between that are forming the great circle arc deviate
-        # in latitude and are closer to pole than parallel corner
-        # points lie on (except the special case of equator). we need
-        # to break all longitudinally extended line to smaller pieces
-        # so we would not miss the area in between the great circle
-        # arc that connects two points and a parallel they share.
-        # we don't need to resample latitudinally-extended lines
-        # because all meridians are great circles.
+    def _get_resampled_coordinates(self):
+        """
+        Resample segments the polygon consists of and return new vertices
+        coordinates.
+
+        :return:
+            A tuple of two numpy arrays: longitudes and latitudes
+            of resampled vertices.
+
+        In order to find the higher and lower latitudes that the polygon
+        touches we need to connect vertices by great circle arcs. If two
+        points lie on the same parallel, the points in between that are
+        forming the great circle arc deviate in latitude and are closer
+        to pole than parallel corner points lie on (except the special
+        case of equator). We need to break all longitudinally extended
+        line to smaller pieces so we would not miss the area in between
+        the great circle arc that connects two points and a parallel
+        they share.
+
+        We don't need to resample latitudinally-extended lines because
+        all meridians are great circles.
+        """
         resampled_lons = [self.lons[0]]
         resampled_lats = [self.lats[0]]
         for i in xrange(self.num_points):
             next_point = (i + 1) % self.num_points
             lon1, lat1 = self.lons[i], self.lats[i]
             lon2, lat2 = self.lons[next_point], self.lats[next_point]
-            longitudinal_extent = get_longitudinal_extent(lon1, lon2)
+            longitudinal_extent = _get_longitudinal_extent(lon1, lon2)
             num_segments = longitudinal_extent / self.LONGITUDAL_DISCRETIZATION
             if num_segments <= 1:
                 resampled_lons.append(lon2)
@@ -442,35 +494,35 @@ class Polygon(object):
                                           num_segments):
                     resampled_lons.append(lon)
                     resampled_lats.append(lat)
-        resampled_lons = numpy.array(resampled_lons)
-        resampled_lats = numpy.array(resampled_lats)
+        return numpy.array(resampled_lons), numpy.array(resampled_lats)
 
-        # find the bounding box of a polygon in a spherical coordinates
-        left_lon = numpy.min(resampled_lons)
-        right_lon = numpy.max(resampled_lons)
-        if get_longitudinal_extent(left_lon, right_lon) < 0:
-            # the polygon crosses the international date line (meridian 180).
-            # the actual left longitude is the lowest positive longitude
-            # and right one is the highest negative.
-            left_lon = min(lon for lon in resampled_lons if lon > 0)
-            right_lon = max(lon for lon in resampled_lons if lon < 0)
-        bottom_lat = numpy.min(resampled_lats)
-        top_lat = numpy.max(resampled_lats)
+    def discretize(self, mesh_spacing):
+        """
+        Get a generator of uniformly spaced points inside the polygon area
+        with distance of ``mesh_spacing`` km between.
+        """
+        # cast from km to m
+        mesh_spacing *= 1e3
+
+        # resample longitudinally-extended lines:
+        lons, lats = self._get_resampled_coordinates()
+
+        # find the bounding box of a polygon in a spherical coordinates:
+        west, east, north, south = _get_spherical_bounding_box(lons, lats)
 
         # create a projection that is attached to a polygon. here we use
         # stereographic projection because it is defined for any center
         # latitude and longitude. it is prone to distortions, but we don't
         # care since we don't measure distances, areas or angles on the
         # projection plane.
-        proj = pyproj.Proj(proj='stere', lat_0=resampled_lats[0],
-                           lon_0=resampled_lons[0])
+        proj = _get_stereographic_projection(west, east, north, south)
 
         # project polygon vertices to the Cartesian space and create
         # a shapely polygon object
-        xx, yy = proj(resampled_lons, resampled_lats)
+        xx, yy = proj(lons, lats)
         polygon2d = shapely.geometry.Polygon(zip(xx, yy))
 
-        del xx, yy, resampled_lats, resampled_lons
+        del xx, yy, lats, lons
 
         # we cover the bounding box (in spherical coordinates) from highest
         # to lowest latitude and from left to right by longitude. we step
@@ -478,10 +530,10 @@ class Polygon(object):
         # if it is inside the polygon and yield the point object, if so.
         # this way we produce an uniformly-spaced mesh regardless of the
         # latitude.
-        latitude = top_lat
-        while latitude > bottom_lat:
-            longitude = left_lon
-            while get_longitudinal_extent(longitude, right_lon) > 0:
+        latitude = north
+        while latitude > south:
+            longitude = west
+            while _get_longitudinal_extent(longitude, east) > 0:
                 # we use Cartesian space just for checking if a point
                 # is inside of the polygon.
                 x, y = proj(longitude, latitude)
@@ -492,4 +544,4 @@ class Polygon(object):
                 longitude, _, _ = GEOD.fwd(longitude, latitude,
                                            90, mesh_spacing)
             # ... and by the same distance along meridian in outer one
-            _, latitude, _ = GEOD.fwd(left_lon, latitude, 180, mesh_spacing)
+            _, latitude, _ = GEOD.fwd(west, latitude, 180, mesh_spacing)
