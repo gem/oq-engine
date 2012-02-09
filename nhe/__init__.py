db4bdf9f3e241cc4caef12b6f0e3c01d83d8a9dd
Anton Gritsay <anton@angri.ru>
2012-02-09 15:39:12 +0100

    __init__, source/__init__: import needed stuff to package's namespace

--- nhe/__init__.py
+++ nhe/__init__.py
@@ -1,3 +1,4 @@
 """
 NHE stands for New Hazard Engine.
 """
+from nhe import geo, mfd, msr, source, const, pmf, tom
--- nhe/source/__init__.py
+++ nhe/source/__init__.py
@@ -1,3 +1,7 @@
 """
 Package :mod:`nhe.source` deals with various types of seismic sources.
 """
+from nhe.source.rupture import Rupture, ProbabilisticRupture
+from nhe.source.nodalplane import NodalPlane
+from nhe.source.point import PointSource
+from nhe.source.area import AreaSource
