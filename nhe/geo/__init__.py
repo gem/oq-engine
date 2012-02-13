7e10e4b18456225986b75d5be3fb5cf48c755a32
Anton Gritsay <anton@angri.ru>
2012-02-13 16:07:26 +0100

    geo/__init__ [docs]: better package docstring

--- nhe/geo/__init__.py
+++ nhe/geo/__init__.py
@@ -1,7 +1,9 @@
 """
 Package :mod:`nhe.geo` contains implementations of different geographical
 primitives, such as :class:`~nhe.geo.point.Point`, :class:`~nhe.geo.line.Line`
-:class:`~nhe.geo.polygon.Polygon` and :class:`~nhe.geo.mesh.Mesh`.
+:class:`~nhe.geo.polygon.Polygon` and :class:`~nhe.geo.mesh.Mesh`, as well
+as different :mod:`surface <nhe.geo.surface>` implementations and utility
+class :class:`~nhe.geo.nodalplane.NodalPlane`.
 """
 from nhe.geo.point import Point
 from nhe.geo.line import Line
