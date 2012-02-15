c19d91a898fc6a626dbc43ffe738c242232d0c22
Lars Butler <Lars.Butler@gmail.com>
2012-02-15 11:53:29 +0100

    moved uhs ath code to a separate module and reorganized tests a bit
    Former-commit-id: ae793a235f0012466e9787b0e7d3ddecc71e830b [formerly ae793a235f0012466e9787b0e7d3ddecc71e830b [formerly 7a6b4e66ceaa943bfa457d071ecea4b6420ad43d]]
    Former-commit-id: d6996760d1ab2a871d2af8a195ab634b5a5ab987
    Former-commit-id: ccb8def3e777c5732442525fdb404ecc56d4bbd7

--- tests/calculators/hazard/uhs/core_unittest.py
+++ tests/calculators/hazard/uhs/core_unittest.py
@@ -24,10 +24,8 @@ import unittest
 from openquake import engine
 from openquake import java
 from openquake.calculators.hazard.uhs.core import UHSCalculator
-from openquake.calculators.hazard.uhs.core import completed_task_count
 from openquake.calculators.hazard.uhs.core import compute_uhs
 from openquake.calculators.hazard.uhs.core import compute_uhs_task
-from openquake.calculators.hazard.uhs.core import remaining_tasks_in_block
 from openquake.calculators.hazard.uhs.core import touch_result_file
 from openquake.calculators.hazard.uhs.core import write_uh_spectra
 from openquake.calculators.hazard.uhs.core import write_uhs_spectrum_data
@@ -311,73 +309,4 @@ class UHSCalculatorTestCase(UHSBaseTestCase):
             self.assertEqual(1, write_mock.call_count)
 
 
-class UHSTaskHandlerTestCase(UHSBaseTestCase):
-    """Tests for functionality realted to the asynchronous task handler code,
-    which is used by the mini-framework
-    :function:`openquake.utils.tasks.distribute`.
-    """
 
-    def test_completed_task_count_no_stats(self):
-        # Test `complete_task_count` with no counters set;
-        # it should just return 0.
-        self.assertEqual(0, completed_task_count(self.job_id))
-
-    def test_complete_task_count_success(self):
-        stats.incr_counter(self.job_id, 'h', 'compute_uhs_task')
-        self.assertEqual(1, completed_task_count(self.job_id))
-
-    def test_complete_task_count_success(self):
-        stats.incr_counter(self.job_id, 'h', 'compute_uhs_task-failures')
-        self.assertEqual(1, completed_task_count(self.job_id))
-
-    def test_complete_task_count_success_and_fail(self):
-        # Test `complete_task_count` with success and fail counters:
-        stats.incr_counter(self.job_id, 'h', 'compute_uhs_task')
-        stats.incr_counter(self.job_id, 'h', 'compute_uhs_task-failures')
-        self.assertEqual(2, completed_task_count(self.job_id))
-
-    def test_remaining_tasks_in_block(self):
-        # Tasks should be submitted to works for one block (of sites) at a
-        # time. For each block, we want to look at Redis counters to determine
-        # when the block is finished calculating.
-        # `remaining_tasks_in_block` is a generator that yields the remaining
-        # number of tasks in a block. When there are no more tasks left in the
-        # block, a `StopIteration` is raised.
-        gen = remaining_tasks_in_block(self.job_id, 4, 0)
-
-        incr_count = lambda: stats.incr_counter(
-            self.job_id, 'h', 'compute_uhs_task')
-
-        self.assertEqual(4, gen.next())
-        incr_count()
-        self.assertEqual(3, gen.next())
-        incr_count()
-        incr_count()
-        self.assertEqual(1, gen.next())
-        incr_count()
-        self.assertRaises(StopIteration, gen.next)
-
-    def test_remaining_tasks_in_block_nonzero_start_count(self):
-        # Same as the above test, except test with the start_count
-        # set to something > 0 (to simulate a mid-calculation block).
-
-        incr_count = lambda: stats.incr_counter(
-            self.job_id, 'h', 'compute_uhs_task')
-
-        # Just for variety, set 5 successful and 5 failed task counters:
-        for _ in xrange(5):
-            stats.incr_counter(self.job_id, 'h', 'compute_uhs_task')
-        for _ in xrange(5):
-            stats.incr_counter(self.job_id, 'h', 'compute_uhs_task-failures')
-
-        # count starts at 10
-        gen = remaining_tasks_in_block(self.job_id, 4, 10)
-
-        self.assertEqual(4, gen.next())
-        incr_count()
-        self.assertEqual(3, gen.next())
-        incr_count()
-        incr_count()
-        self.assertEqual(1, gen.next())
-        incr_count()
-        self.assertRaises(StopIteration, gen.next)
