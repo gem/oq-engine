6805ee974ca8c9b9c54c6cc8fd9151deb301ffd4
Lars Butler <Lars.Butler@gmail.com>
2012-02-20 10:17:00 +0100

    Added a QA tests that ensures the oq_user record associated with the current shell user is the owner of oq_calculation, oq_job_profile, and output records.
    Former-commit-id: 11db4d92cf32a10f3bb510157a4224764e84a9a6 [formerly 9ea6f1ac4d35c58d87df17d2dc27941e11f0875b] [formerly 9ea6f1ac4d35c58d87df17d2dc27941e11f0875b [formerly c594cd68878791e1aff0d068f05c7ecf0e7a5a0a]]
    Former-commit-id: c6586f8fbff1aa3067b4d2e317e1934660124848
    Former-commit-id: ccfd0c3481e434fe152f867a6650ff20f7046651

--- qa_tests/export_unittest.py
+++ qa_tests/export_unittest.py
@@ -1,8 +1,29 @@
+# Copyright (c) 2010-2012, GEM Foundation.
+#
+# OpenQuake is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3
+# only, as published by the Free Software Foundation.
+#
+# OpenQuake is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License version 3 for more details
+# (a copy is included in the LICENSE file that accompanied this code).
+#
+# You should have received a copy of the GNU Lesser General Public License
+# version 3 along with OpenQuake.  If not, see
+# <http://www.gnu.org/licenses/lgpl-3.0.txt> for a copy of the LGPLv3 License.
+
+
 import ConfigParser
+import getpass
 import os
 import unittest
 
 from openquake.db.models import OqCalculation
+from openquake.db.models import OqJobProfile
+from openquake.db.models import OqUser
+from openquake.db.models import Output
 
 from tests.utils.helpers import demo_file
 from tests.utils.helpers import run_job
@@ -37,3 +58,33 @@ class CalculationDescriptionTestCase(unittest.TestCase):
 
         # Clean up the temporary config file:
         os.unlink(mod_cfg_path)
+
+
+class CalculationUserAssociation(unittest.TestCase):
+    """Tests related to association of the current (shell) user with
+    calculation and result artifacts."""
+
+    def test_full_calc_user_assoc(self):
+        # Run a full calculation in the same as other QA tests (using
+        # `subprocess` to invoke bin/openquake) and check the following:
+        # 1. There is an OqUser record for the current user.
+        # 2. This user is the owner of all OqJobProfile, OqCalculation,
+        #    and Ouput records.
+        cfg_path = demo_file('uhs/config.gem')
+
+        run_job(cfg_path)
+
+        # Get the OqUser for the current user
+        user = OqUser.objects.get(user_name=getpass.getuser())
+
+        calculation = OqCalculation.objects.latest('id')
+        job_profile = calculation.oq_job_profile
+
+        self.assertEqual(user, calculation.owner)
+        self.assertEqual(user, job_profile.owner)
+
+        outputs = Output.objects.filter(oq_calculation=calculation.id)
+        # We need at least 1 output record, otherwise this test is useless:
+        self.assertTrue(len(outputs) > 0)
+        for output in outputs:
+            self.assertEqual(user, output.owner)
