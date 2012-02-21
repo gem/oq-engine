321e7ff5f2a4f804400480a942b39a417b352b5b
Lars Butler <Lars.Butler@gmail.com>
2012-02-21 10:06:17 +0100

    Added more tests to cover more get_calculations() cases (none complete, none whatsoever)
    Former-commit-id: b70d1dc2f365181a3c39e05c25dfa8f26eff6ebc [formerly b70d1dc2f365181a3c39e05c25dfa8f26eff6ebc [formerly dec17aa6cf92c03df35ae81c543daef5e963af6c]]
    Former-commit-id: 5843bf4f3138451380b2a01895feaab44a22df79
    Former-commit-id: 63d3896b70f4223009759446eec4026fe55a7e59

--- tests/export_unittest.py
+++ tests/export_unittest.py
@@ -29,45 +29,86 @@ from tests.utils import helpers
 class ExportAPITestCase(unittest.TestCase):
     """Tests for the export API defined in :module:`openquake.export`."""
 
+    #: UHS job profile
+    uhs_jp = None
+    #: UHS calculation
+    uhs_calc = None
+    #: UHS pending calculation
+    uhs_pending_calc = None
+
+    #: classical psha (haz+risk) job profile
+    cpsha_jp = None
+    #: classical psha (haz+risk) calculation
+    cpsha_calc_fail = None
+    #: classical psha (haz+risk) running calculation
+
+    def tearDown(self):
+        self.uhs_jp = None
+        self.uhs_calc = None
+        self.uhs_pending_calc = None
+
+        self.cpsha_jp = None
+        self.cpsha_calc_fail = None
+        self.cpsha_running_calc = None
+
     @classmethod
-    def _set_up_calculations(cls):
-        cls.user_name = str(uuid.uuid4())
+    def _create_job_profiles(cls, user_name):
         uhs_cfg = helpers.demo_file('uhs/config.gem')
-        uhs_jp, _, _ = import_job_profile(uhs_cfg, user_name=cls.user_name)
-        cls.uhs_calc = models.OqCalculation(
-            owner=uhs_jp.owner, description=uhs_jp.description,
-            oq_job_profile=uhs_jp, status='succeeded')
-        cls.uhs_calc.save()
+        cls.uhs_jp, _, _ = import_job_profile(uhs_cfg, user_name=user_name)
 
         cpsha_cfg = helpers.demo_file('classical_psha_based_risk/config.gem')
-        cpsha_jp, _, _ = import_job_profile(cpsha_cfg, user_name=cls.user_name)
-        cls.cpsha_calc = models.OqCalculation(
-            owner=cpsha_jp.owner, description=cpsha_jp.description,
-            oq_job_profile=cpsha_jp, status='failed')
-        cls.cpsha_calc.save()
+        cls.cpsha_jp, _, _ = import_job_profile(cpsha_cfg, user_name=user_name)
 
-        # We'll some other calculations (1 pending and 1 running) to make sure
-        # that the API function under test only retrieves 'completed'
-        # calculations.
+    @classmethod
+    def _set_up_complete_calcs(cls):
+        cls.uhs_calc = models.OqCalculation(
+            owner=cls.uhs_jp.owner, description=cls.uhs_jp.description,
+            oq_job_profile=cls.uhs_jp, status='succeeded')
+        cls.uhs_calc.save()
+
+        cls.cpsha_calc_fail = models.OqCalculation(
+            owner=cls.cpsha_jp.owner, description=cls.cpsha_jp.description,
+            oq_job_profile=cls.cpsha_jp, status='failed')
+        cls.cpsha_calc_fail.save()
+
+    @classmethod
+    def _set_up_incomplete_calcs(cls):
         cls.uhs_pending_calc = models.OqCalculation(
-            owner=uhs_jp.owner, description=uhs_jp.description,
-            oq_job_profile=uhs_jp, status='pending')
+            owner=cls.uhs_jp.owner, description=cls.uhs_jp.description,
+            oq_job_profile=cls.uhs_jp, status='pending')
         cls.uhs_pending_calc.save()
 
-        cls.uhs_running_calc = models.OqCalculation(
-            owner=uhs_jp.owner, description=uhs_jp.description,
-            oq_job_profile=uhs_jp, status='running')
-        cls.uhs_running_calc.save()
+        cls.cpsha_running_calc = models.OqCalculation(
+            owner=cls.cpsha_jp.owner, description=cls.cpsha_jp.description,
+            oq_job_profile=cls.cpsha_jp, status='running')
+        cls.cpsha_running_calc.save()
 
     def test_get_calculations(self):
         # Test that :function:`openquake.export.get_calculations` retrieves
         # only _completed_ calculations for the given user, in reverse chrono
         # order.
-        self._set_up_calculations()
+        user_name = str(uuid.uuid4())
+        self._create_job_profiles(user_name)
+        self._set_up_complete_calcs()
+        self._set_up_incomplete_calcs()
 
         # expeced values, sorted in reverse chronological order:
-        expected = sorted([self.uhs_calc, self.cpsha_calc],
+        expected = sorted([self.uhs_calc, self.cpsha_calc_fail],
                           key=lambda x: x.last_update)[::-1]
-        actual = export.get_calculations(self.user_name)
+        actual = export.get_calculations(user_name)
 
         helpers.assertDeepAlmostEqual(self, expected, actual)
+
+    def test_get_calculations_no_completed_calcs(self):
+        # No completed calculations for this user.
+        user_name = str(uuid.uuid4())
+        self._create_job_profiles(user_name)
+        self._set_up_incomplete_calcs()
+
+        self.assertTrue(len(export.get_calculations(user_name)) == 0)
+
+
+    def test_get_calculations_no_results_for_user(self):
+        # No calculation records at all for this user.
+        user_name = str(uuid.uuid4())
+        self.assertTrue(len(export.get_calculations(user_name)) == 0)
