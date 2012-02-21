98d5c4b22b1baa24121c6569cdf8facae0ea1c6c
Lars Butler <Lars.Butler@gmail.com>
2012-02-21 16:59:25 +0100

    Basic export framework API is implemented. No specific export functions are yet written, though.
    Next, I need to write the command line interface portion.
    
    
    Former-commit-id: 20b12cae4b2f5dc36f20a9cda36b18a899c5c59e [formerly a391f9aaba99a1e609f7151758b21ed08f1bcd74]
    Former-commit-id: 5ae817c7036e06da28fec92e3a01d966bd9aa51b

--- openquake/export/core.py
+++ openquake/export/core.py
@@ -17,6 +17,21 @@
 
 from openquake.db import models
 
+def _export_fn_map():
+    """Creates a mapping from output type to export function.
+
+    :rtype: `dict`
+    """
+    # TODO: No export functions have yet been written.
+    fn_map = {}
+    return fn_map
+
+def _export_fn_not_implemented(output, _target_dir):
+    """This gets called if an export is attempted on an unsupported output
+    type. See :data:`_EXPORT_FN_MAP`."""
+    raise NotImplementedError('Cannot export output of type: %s'
+                              % output.output_type)
+
 
 def get_calculations(user_name):
     """Get the completed calculations (successful and failed) for the given
@@ -52,4 +67,24 @@ def get_outputs(calculation_id):
     return models.Output.objects.filter(oq_calculation=calculation_id)
 
 def export(output_id, target_dir):
-    """ """
+    """Export the given calculation output from the database to the specified
+    directory.
+
+    :param int output_id:
+        ID of a :class:`openquake.db.models.Output`.
+    :param str target_dir:
+        Directory where output artifacts should be written.
+    :returns:
+        List of file names (including the full directory path) containing the
+        exported results.
+
+        The quantity and type of the result files depends on
+        the type of output, as well as calculation parameters. (See the
+        `output_type` attribute of :class:`openquake.db.models.Output`.)
+    """
+    output = models.Output.objects.get(id=output_id)
+
+    export_fn = _export_fn_map().get(
+        output.output_type, _export_fn_not_implemented)
+
+    return export_fn(output, target_dir)
