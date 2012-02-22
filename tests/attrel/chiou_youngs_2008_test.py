a4f9a1443645acd53266bad99f515320224320bb
Anton Gritsay <anton@angri.ru>
2012-02-22 11:24:14 +0100

    tests/attrel/chiou_youngs_2008: added one more test

--- tests/attrel/chiou_youngs_2008_test.py
+++ tests/attrel/chiou_youngs_2008_test.py
@@ -6,10 +6,14 @@ from tests.attrel.base import BaseAttRelTestCase
 class ChiouYoungs2008TestCase(BaseAttRelTestCase):
     ATTREL_CLASS = ChiouYoungs2008
 
-    def test_total_stddev_hanging_wall_strike_slip(self):
+    def test_total_stddev_hanging_wall_strike_slip_measured(self):
         self.check('CY08/CY08_SIGMEAS_MS_HW_SS.csv',
                     max_discrep_percentage=0.015)
 
-    def test_mean_hangin_wall_normal_slip(self):
+    def test_total_stddev_hanging_wall_strike_slip_inferred(self):
+        self.check('CY08/CY08_SIGINFER_MS_HW_SS.csv',
+                   max_discrep_percentage=0.015)
+
+    def test_mean_hanging_wall_normal_slip(self):
         self.check('CY08/CY08_MEDIAN_MS_HW_NM.csv',
-                   max_discrep_percentage=0.5)
+                   max_discrep_percentage=0.05)
