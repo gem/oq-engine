43182ab42b3f08a0b5e58ccbcd1f348e474576c4
Lars Butler <Lars.Butler@gmail.com>
2012-02-23 14:30:40 +0100

    Fully implemented and tested touch_result_hdf5_file() and _point_to_ds_name()
    Former-commit-id: 21b3bd0456f466a2c016954374cce365994da386 [formerly 21b3bd0456f466a2c016954374cce365994da386 [formerly b108956650010a5235a1726205de009eb8608ba3]]
    Former-commit-id: 350a2556848a1c799dd922b74c100a9c309d86ab
    Former-commit-id: 7a5739579438bdfdc2fc3bc42b4bcd72b807cab7

--- openquake/export/uhs.py
+++ openquake/export/uhs.py
@@ -15,22 +15,113 @@
 # <http://www.gnu.org/licenses/lgpl-3.0.txt> for a copy of the LGPLv3 License.
 
 
+import h5py
+import numpy
+import os
+
 from openquake.db import models
+from openquake.utils import round_float
+
+#: Format string for HDF5 dataset names
+_DS_NAME_FMT = 'lon:%s-lat:%s'
+_HDF5_FILE_NAME_FMT = 'uhs_poe:%s.hdf5'
+
+
+def _point_to_ds_name(point):
+    """Generate a dataset name from a
+    :class:`django.contrib.gis.geos.point.Point`. This dataset name is meant to
+    be used in UHS HDF5 result files.
+
+    :param point:
+        :class:`django.contrib.gis.geos.point.Point` object.
+    :returns:
+        A dataset name generated from the point's lat/lon values. Example::
+            "lon:-179.45-lat:-20.75"
+
+    A simple example:
+
+    >>> from django.contrib.gis.geos.point import Point
+    >>> pt = Point(-179.45, -20.75)
+    >>> _point_to_ds_name(pt)
+    'lon:-179.45-lat:-20.75'
+
+    This function uses :function:`openquake.utils.round_float` to round
+    coordinate values. Thus, any coordinate value with more than 7 digits after
+    the decimal will be rounded to 7 digits:
+
+    >>> pt = Point(-179.12345675, 79.12345674)
+    >>> _point_to_ds_name(pt)
+    'lon:-179.1234568-lat:79.1234567'
+    """
+    return _DS_NAME_FMT % (round_float(point.x), round_float(point.y))
+
 
 def export_uhs(output, target_dir):
     """ """
 
-    uh_spectra = models.UhSpectra.objects.get(output=output.id)
+    # TODO: mkdir -p (if the target dir doesn't exist?)
 
-    calculation = output.oq_calculation
-    job_profile = oq_calculation.oq_job_profile
+    file_names = []
+
+    uh_spectra = models.UhSpectra.objects.get(output=output.id)
 
     uh_spectrums = models.UhSpectrum.objects.filter(uh_spectra=uh_spectra.id)
 
     for spectrum in uh_spectrums:
         # create a file for each spectrum/poe
-        spectrum.poe
+        uhs_data = models.UhSpectrumData.objects.filter(
+            uh_spectrum=spectrum.id)
+
+        # If there are multiple LT samples/realizations, we'll have multiple
+        # records for each site. However, there should only be a 1 dataset per
+        # site so we need to 'uniquify'.
+        ds_names = list(set([_point_to_ds_name(datum.location)
+                             for datum in uhs_data]))
+
+        file_name = touch_result_hdf5_file(
+            target_dir, spectrum.poe, ds_names, len(uh_spectra.realizations),
+            len(uh_spectra.periods))
+        # TODO: now write the actual data
+        file_names.append(file_name)
+
+    return file_names
 
 
-def touch_result_hdf5_file():
-    pass
+def touch_result_hdf5_file(target_dir, poe, ds_names, n_realizations,
+                           n_periods):
+    """Create an empty HDF5 file with appropriately sized datasets. The quanity
+    of datasets created is equal to the length of ``ds_names``. Each dataset
+    will be a 2D matrix with a number of rows == ``n_realizations`` and number
+    of columns == ``n_periods``. Each dataset will be created 'empty'
+    (all values == 0.0).
+
+    The datatype for each value is `numpy.float64`.
+
+    :param str target_dir:
+        Location to place the new file.
+    :param float poe:
+        Probability of Exceedance associated with this file. The PoE will be
+        used to generate the resulting file name.
+    :param list ds_names:
+        List strings representing dataset names. 1 dataset will be created for
+        each name.
+
+        Note: Each dataset name should be unique.
+    :param int n_realizations:
+        Number of rows in each dataset.
+    :param int n_periods:
+        Number of columns in each dataset.
+
+    :returns:
+        The full path of the created file.
+    """
+    file_name = _HDF5_FILE_NAME_FMT % poe
+    full_path = os.path.join(target_dir, file_name)
+
+    ds_shape = (n_realizations, n_periods)
+
+    with h5py.File(full_path, 'w') as h5_file:
+        for name in ds_names:
+            h5_file.create_dataset(name, dtype=numpy.float64, shape=ds_shape)
+
+    return full_path
