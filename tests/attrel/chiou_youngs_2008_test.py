9a212d53f4e318978cb88bc975b54aa49466f87a
Anton Gritsay <anton@angri.ru>
2012-02-24 11:10:17 +0100

    tests/attrel/chiou_youngs_2008: more correct info on data files source

--- tests/attrel/chiou_youngs_2008_test.py
+++ tests/attrel/chiou_youngs_2008_test.py
@@ -6,28 +6,27 @@ from tests.attrel.base import BaseAttRelTestCase
 class ChiouYoungs2008TestCase(BaseAttRelTestCase):
     ATTREL_CLASS = ChiouYoungs2008
 
+    # First five tests use data ported from Kenneth Campbell
+    # tables for verifying NGA models, see
+    # http://opensha.usc.edu/docs/opensha/NGA/Campbell_NGA_tests.zip
+
     def test_total_stddev_hanging_wall_strike_slip_measured(self):
-        # data ported from opensha test data
         self.check('CY08/CY08_SIGMEAS_MS_HW_SS.csv',
                     max_discrep_percentage=0.015)
 
     def test_total_stddev_hanging_wall_strike_slip_inferred(self):
-        # data ported from opensha test data
         self.check('CY08/CY08_SIGINFER_MS_HW_SS.csv',
                    max_discrep_percentage=0.015)
 
     def test_mean_hanging_wall_normal_slip(self):
-        # data ported from opensha test data
         self.check('CY08/CY08_MEDIAN_MS_HW_NM.csv',
                    max_discrep_percentage=0.05)
 
     def test_mean_hanging_wall_reversed_slip(self):
-        # data ported from opensha test data
         self.check('CY08/CY08_MEDIAN_MS_HW_RV.csv',
                    max_discrep_percentage=0.05)
 
     def test_mean_hanging_wall_strike_slip(self):
-        # data ported from opensha test data
         self.check('CY08/CY08_MEDIAN_MS_HW_SS.csv',
                    max_discrep_percentage=0.05)
 
