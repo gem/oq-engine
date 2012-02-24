92fbdba2c3078766ba129eb77df3671f88da0697
Anton Gritsay <anton@angri.ru>
2012-02-24 10:38:07 +0100

    tests/attrel/chiou_youngs_2008: added a note about test data source

--- tests/attrel/chiou_youngs_2008_test.py
+++ tests/attrel/chiou_youngs_2008_test.py
@@ -7,29 +7,36 @@ class ChiouYoungs2008TestCase(BaseAttRelTestCase):
     ATTREL_CLASS = ChiouYoungs2008
 
     def test_total_stddev_hanging_wall_strike_slip_measured(self):
+        # data ported from opensha test data
         self.check('CY08/CY08_SIGMEAS_MS_HW_SS.csv',
                     max_discrep_percentage=0.015)
 
     def test_total_stddev_hanging_wall_strike_slip_inferred(self):
+        # data ported from opensha test data
         self.check('CY08/CY08_SIGINFER_MS_HW_SS.csv',
                    max_discrep_percentage=0.015)
 
     def test_mean_hanging_wall_normal_slip(self):
+        # data ported from opensha test data
         self.check('CY08/CY08_MEDIAN_MS_HW_NM.csv',
                    max_discrep_percentage=0.05)
 
     def test_mean_hanging_wall_reversed_slip(self):
+        # data ported from opensha test data
         self.check('CY08/CY08_MEDIAN_MS_HW_RV.csv',
                    max_discrep_percentage=0.05)
 
     def test_mean_hanging_wall_strike_slip(self):
+        # data ported from opensha test data
         self.check('CY08/CY08_MEDIAN_MS_HW_SS.csv',
                    max_discrep_percentage=0.05)
 
     def test_inter_event_stddev(self):
+        # data generated from opensha
         self.check('CY08/CY08_INTER_EVENT_SIGMA.csv',
                    max_discrep_percentage=0.002)
 
     def test_intra_event_stddev(self):
+        # data generated from opensha
         self.check('CY08/CY08_INTRA_EVENT_SIGMA.csv',
                    max_discrep_percentage=0.001)
