2b5776114db237f7e9fa6b1a59db283bc86e1fa2
Lars Butler <Lars.Butler@gmail.com>
2012-03-06 12:07:38 +0100

    hooked in the uhs exporter when outpt-type=xml
    Former-commit-id: c5539981475acf651b17de1375984ba6ffca2ae4 [formerly c5539981475acf651b17de1375984ba6ffca2ae4 [formerly b34e89ad03d9378f3e1da01c22aa92bc47f738a0]]
    Former-commit-id: 597cd3718c7c4cb2b073bae5fbb42ea97bb31dbf
    Former-commit-id: e6b16e24082de507a1a4980901ac4902c3bdd157

--- openquake/calculators/hazard/uhs/core.py
+++ openquake/calculators/hazard/uhs/core.py
@@ -18,6 +18,7 @@
 """Core functionality of the Uniform Hazard Spectra calculator."""
 
 
+import os
 import random
 
 from celery.task import task
@@ -265,7 +266,19 @@ class UHSCalculator(Calculator):
                     ath=uhs_task_handler, ath_args=ath_args)
 
     def post_execute(self):
-        """Clean up stats counters."""
+        """Clean up stats counters and create XML output artifacts (if
+        requested).
+        """
         # TODO: export these counters to the database before deleting them
         # See bug https://bugs.launchpad.net/openquake/+bug/925946.
         stats.delete_job_counters(self.calc_proxy.job_id)
+
+        if 'xml' in self.calc_proxy.serialize_results_to:
+            [uhs_output] = models.Output.objects.filter(
+                oq_calculation=self.calc_proxy.oq_calculation.id,
+                output_type='uh_spectra')
+
+            target_dir = os.path.join(self.calc_proxy.params.get('BASE_PATH'),
+                                      self.calc_proxy.params.get('OUTPUT_DIR'))
+
+            export_uhs(uhs_output, target_dir)
