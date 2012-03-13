1f4a966d56708f0d9b0d13b40846e29d7ae5f89e
Andrea Cerisara <andreacerisara@gmail.com>
2012-03-13 13:29:00 +0100

    Added QA test
    Former-commit-id: 393e0554f85fd7067d5f9574e2da846c3fa38366 [formerly 328eaef382e6c5dfdb0ac75e71e34deab4f70fbe] [formerly 328eaef382e6c5dfdb0ac75e71e34deab4f70fbe [formerly d19bb224b64d3695aa03d52a64b3253e7f654051]]
    Former-commit-id: c5241f6bea08f94c3d5f57f21ad14234432a9bb0
    Former-commit-id: 9a69c376cc12d869fdbf516f566df2624486904f

---
+++ qa_tests/probabilistic_risk_unittest.py
@@ -0,0 +1,41 @@
+# Copyright (c) 2010-2012, GEM Foundation.
+#
+# OpenQuake is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# OpenQuake is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import unittest
+
+from nose.plugins.attrib import attr
+
+from openquake.db.models import OqJob
+
+from tests.utils import helpers
+
+
+class ProbabilisticEventBasedRiskQATest(unittest.TestCase):
+    """QA tests for the Probabilistic Event Based Risk calculator."""
+
+    @attr('qa')
+    def test_probabilistic_risk(self):
+        # The rudimentary beginnings of a QA test for the probabilistic
+        # calculator. For now, just run it end-to-end
+        # to make sure it doesn't blow up.
+        cfg = helpers.demo_file(
+            'probabilistic_event_based_risk/config_stest.gem')
+
+        ret_code = helpers.run_job(scen_cfg, ['--output-type=xml'])
+        self.assertEqual(0, ret_code)
+
+        job = OqJob.objects.latest('id')
+        self.assertEqual('succeeded', job.status)
