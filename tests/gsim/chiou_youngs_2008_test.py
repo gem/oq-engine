f1f0049db9a00bf428f5c2e3b1a20726501f9a50
Anton Gritsay <anton@angri.ru>
2012-03-16 12:50:13 +0100

    LICENSE: NGA test data files are distributed under Apache License 2.0

--- tests/gsim/chiou_youngs_2008_test.py
+++ tests/gsim/chiou_youngs_2008_test.py
@@ -22,35 +22,37 @@ class ChiouYoungs2008TestCase(BaseGSIMTestCase):
     GSIM_CLASS = ChiouYoungs2008
 
     # First five tests use data ported from Kenneth Campbell
-    # tables for verifying NGA models, see
+    # tables for verifying NGA models, available from OpenSHA, see
     # http://opensha.usc.edu/docs/opensha/NGA/Campbell_NGA_tests.zip
+    # This data is distributed under different license, see LICENSE.txt
+    # in tests/gsim/data/NGA
 
     def test_total_stddev_hanging_wall_strike_slip_measured(self):
-        self.check('CY08/CY08_SIGMEAS_MS_HW_SS.csv',
+        self.check('NGA/CY08/CY08_SIGMEAS_MS_HW_SS.csv',
                     max_discrep_percentage=0.015)
 
     def test_total_stddev_hanging_wall_strike_slip_inferred(self):
-        self.check('CY08/CY08_SIGINFER_MS_HW_SS.csv',
+        self.check('NGA/CY08/CY08_SIGINFER_MS_HW_SS.csv',
                    max_discrep_percentage=0.015)
 
     def test_mean_hanging_wall_normal_slip(self):
-        self.check('CY08/CY08_MEDIAN_MS_HW_NM.csv',
+        self.check('NGA/CY08/CY08_MEDIAN_MS_HW_NM.csv',
                    max_discrep_percentage=0.05)
 
     def test_mean_hanging_wall_reversed_slip(self):
-        self.check('CY08/CY08_MEDIAN_MS_HW_RV.csv',
+        self.check('NGA/CY08/CY08_MEDIAN_MS_HW_RV.csv',
                    max_discrep_percentage=0.05)
 
     def test_mean_hanging_wall_strike_slip(self):
-        self.check('CY08/CY08_MEDIAN_MS_HW_SS.csv',
+        self.check('NGA/CY08/CY08_MEDIAN_MS_HW_SS.csv',
                    max_discrep_percentage=0.05)
 
     def test_inter_event_stddev(self):
         # data generated from opensha
-        self.check('CY08/CY08_INTER_EVENT_SIGMA.csv',
+        self.check('NGA/CY08/CY08_INTER_EVENT_SIGMA.csv',
                    max_discrep_percentage=0.002)
 
     def test_intra_event_stddev(self):
         # data generated from opensha
-        self.check('CY08/CY08_INTRA_EVENT_SIGMA.csv',
+        self.check('NGA/CY08/CY08_INTRA_EVENT_SIGMA.csv',
                    max_discrep_percentage=0.001)
