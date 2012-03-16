0b68a4676e638c240095bd894a92ad845c3b6667
Andrea Cerisara <andreacerisara@gmail.com>
2012-03-16 16:19:45 +0100

    Added calculator structure
    Former-commit-id: db0319692e1ea71414fa29775956cf0660d2b40c [formerly 3c7971db7e9cb980be80bb9e3bb0f1b6cb3a03bb]
    Former-commit-id: b1222c723514d4fae3861d593ed7e6d1629ae3cf

--- openquake/calculators/risk/__init__.py
+++ openquake/calculators/risk/__init__.py
@@ -13,7 +13,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
 
-
 """Information about the calculators available for the Risk engine."""
 
 
@@ -21,7 +20,8 @@ from openquake.calculators.risk.classical.core import ClassicalRiskCalculator
 from openquake.calculators.risk.event_based.core import (
     EventBasedRiskCalculator)
 from openquake.calculators.risk.scenario.core import ScenarioRiskCalculator
-
+from openquake.calculators.risk.scenario_damage.core import (
+    ScenarioDamageRiskCalculator)
 
 CALCULATORS = {
     'classical': ClassicalRiskCalculator,
@@ -29,4 +29,5 @@ CALCULATORS = {
     'event_based': EventBasedRiskCalculator,
     'event_based_bcr': EventBasedRiskCalculator,
     'scenario': ScenarioRiskCalculator,
+    'scenario_damage': ScenarioDamageRiskCalculator,
 }
