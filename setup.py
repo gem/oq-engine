c61dc3a20ca16e544b29dd2dd7b92003763a39e6
Anton Gritsay <anton@angri.ru>
2012-03-16 16:36:44 +0100

    setup: added license header and copyright info

--- setup.py
+++ setup.py
@@ -1,3 +1,18 @@
+# nhlib: A New Hazard Library
+# Copyright (C) 2012 GEM Foundation
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 nhlib includes modules for modeling seismic sources (point, area and fault),
 earthquake ruptures, temporal (e.g. Poissonian) and magnitude occurrence
@@ -11,6 +26,8 @@ analysis. The GEM Foundation (http://www.globalquakemodel.org/) supports
 the development of the  library by adding the most recent methodologies
 adopted by the seismological/seismic hazard communities. Comments,
 suggestions and criticisms from the community are always very welcome.
+
+Copyright (C) 2012 GEM Foundation.
 """
 from setuptools import setup, find_packages
 
