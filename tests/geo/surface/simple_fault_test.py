55c2b09a357fc865e25661141aec78b90ab0c06f
Anton Gritsay <anton@angri.ru>
2012-03-22 14:47:16 +0100

    geo/surface/simple_fault: constructor takes only mesh object (+)
    simple fault surfaces are used in two ways: one is to represent the
    whole fault source surface and another for a slice of thereof. in second
    case the mesh is always a slice of the first case's mesh
    
    it's also important that simple fault surfaces of at least 2 by 2 points
    are supported

--- tests/geo/surface/simple_fault_test.py
+++ tests/geo/surface/simple_fault_test.py
@@ -22,62 +22,9 @@ from tests.geo.surface import _utils as utils
 
 
 class SimpleFaultSurfaceTestCase(utils.SurfaceTestCase):
-
     def setUp(self):
         self.fault_trace = Line(
             [Point(0.0, 0.0), Point(1.0, 1.0)])
-
-    def test_dip_inside_range(self):
-        self.assertRaises(ValueError, SimpleFaultSurface,
-                self.fault_trace, 0.0, 1.0, 0.0, 1.0)
-
-        self.assertRaises(ValueError, SimpleFaultSurface,
-                self.fault_trace, 0.0, 1.0, -0.1, 1.0)
-
-        self.assertRaises(ValueError, SimpleFaultSurface,
-                self.fault_trace, 0.0, 1.0, 90.1, 1.0)
-
-        SimpleFaultSurface(self.fault_trace, 0.0, 1.0, 0.1, 1.0)
-        SimpleFaultSurface(self.fault_trace, 0.0, 1.0, 90.0, 1.0)
-
-    def test_upper_seismo_depth_range(self):
-        self.assertRaises(ValueError, SimpleFaultSurface,
-                self.fault_trace, -0.1, None, 90.0, 1.0)
-
-        SimpleFaultSurface(self.fault_trace, 0.0, 1.0, 90.0, 1.0)
-        SimpleFaultSurface(self.fault_trace, 1.0, 1.1, 90.0, 1.0)
-
-    def test_upper_lower_seismo_values(self):
-        self.assertRaises(ValueError, SimpleFaultSurface,
-                self.fault_trace, 1.0, 1.0, 90.0, 1.0)
-
-        self.assertRaises(ValueError, SimpleFaultSurface,
-                self.fault_trace, 1.0, 0.9, 90.0, 1.0)
-
-    def test_fault_trace_on_surface(self):
-        fault_trace = Line(
-            [Point(0.0, 0.0, 1.0), Point(1.0, 1.0, 0.0)])
-
-        self.assertRaises(ValueError, SimpleFaultSurface,
-                fault_trace, 0.0, 1.0, 90.0, 1.0)
-
-    def test_mesh_spacing_range(self):
-        SimpleFaultSurface(self.fault_trace, 0.0, 1.0, 90.0, 1.0)
-
-        self.assertRaises(ValueError, SimpleFaultSurface,
-                self.fault_trace, 0.0, 1.0, 90.0, 0.0)
-
-        self.assertRaises(ValueError, SimpleFaultSurface,
-                self.fault_trace, 0.0, 1.0, 90.0, -1.0)
-
-    def test_fault_trace_points(self):
-        """
-        The fault trace must have at least two points.
-        """
-        fault_trace = Line([Point(0.0, 0.0)])
-
-        self.assertRaises(ValueError, SimpleFaultSurface,
-                fault_trace, 0.0, 1.0, 90.0, 1.0)
 
     def test_get_mesh_1(self):
         p1 = Point(0.0, 0.0, 0.0)
@@ -85,7 +32,7 @@ class SimpleFaultSurfaceTestCase(utils.SurfaceTestCase):
         p3 = Point(0.0190775080917, 0.0550503815182, 0.0)
         p4 = Point(0.03974514139, 0.0723925718856, 0.0)
 
-        fault = SimpleFaultSurface(
+        fault = SimpleFaultSurface.from_fault_data(
             Line([p1, p2, p3, p4]), 0.0, 4.2426406871192848, 45.0, 1.0)
 
         self.assert_mesh_is(fault, test_data.TEST_1_MESH)
@@ -96,7 +43,7 @@ class SimpleFaultSurfaceTestCase(utils.SurfaceTestCase):
         p3 = Point(0.0190775080917, 0.0550503815182, 0.0)
         p4 = Point(0.03974514139, 0.0723925718856, 0.0)
 
-        fault = SimpleFaultSurface(Line([p1, p2, p3, p4]),
+        fault = SimpleFaultSurface.from_fault_data(Line([p1, p2, p3, p4]),
                 2.12132034356, 4.2426406871192848, 45.0, 1.0)
 
         self.assert_mesh_is(fault, test_data.TEST_2_MESH)
@@ -107,7 +54,7 @@ class SimpleFaultSurfaceTestCase(utils.SurfaceTestCase):
         p3 = Point(0.0190775080917, 0.0550503815182, 0.0)
         p4 = Point(0.03974514139, 0.0723925718856, 0.0)
 
-        fault = SimpleFaultSurface(Line([p1, p2, p3, p4]),
+        fault = SimpleFaultSurface.from_fault_data(Line([p1, p2, p3, p4]),
                 0.0, 4.0, 90.0, 1.0)
 
         self.assert_mesh_is(fault, test_data.TEST_4_MESH)
@@ -117,7 +64,7 @@ class SimpleFaultSurfaceTestCase(utils.SurfaceTestCase):
         p2 = Point(180.0, 0.0)
         p3 = Point(-179.9, 0.0)
 
-        fault = SimpleFaultSurface(Line([p1, p2, p3]),
+        fault = SimpleFaultSurface.from_fault_data(Line([p1, p2, p3]),
                 1.0, 6.0, 90.0, 1.0)
 
         self.assert_mesh_is(fault, test_data.TEST_5_MESH)
@@ -126,32 +73,32 @@ class SimpleFaultSurfaceTestCase(utils.SurfaceTestCase):
         p1 = Point(0.0, 0.0)
         p2 = Point(0.0635916966572, 0.0635916574897)
 
-        surface = SimpleFaultSurface(Line([p1, p2]),
+        surface = SimpleFaultSurface.from_fault_data(Line([p1, p2]),
                 1.0, 6.0, 90.0, 1.0)
 
-        self.assertAlmostEquals(45.0, surface.get_strike())
+        self.assertAlmostEquals(45.0, surface.get_strike(), delta=1e-4)
 
     def test_get_strike_2(self):
         p1 = Point(0.0, 0.0)
         p2 = Point(0.0635916966572, 0.0635916574897)
         p3 = Point(0.0860747816618, 0.102533437776)
 
-        surface = SimpleFaultSurface(Line([p1, p2, p3]),
+        surface = SimpleFaultSurface.from_fault_data(Line([p1, p2, p3]),
                 1.0, 6.0, 90.0, 1.0)
 
         self.assertAlmostEquals(40.0, surface.get_strike(), delta=0.02)
 
     def test_get_strike_along_meridian(self):
         line = Line([Point(0, 0), Point(1e-5, 1e-3), Point(0, 2e-3)])
-        surface = SimpleFaultSurface(line, 1.0, 6.0, 90.0, 1.0)
-        self.assertAlmostEquals(0, surface.get_strike())
+        surface = SimpleFaultSurface.from_fault_data(line, 1.0, 6.0, 90.0, 0.1)
+        self.assertAlmostEquals(0, surface.get_strike(), delta=6e-2)
 
     def test_get_dip_1(self):
         p1 = Point(0.0, 0.0)
         p2 = Point(0.0635916966572, 0.0635916574897)
         p3 = Point(0.0860747816618, 0.102533437776)
 
-        surface = SimpleFaultSurface(Line([p1, p2, p3]),
+        surface = SimpleFaultSurface.from_fault_data(Line([p1, p2, p3]),
                 1.0, 6.0, 90.0, 1.0)
 
         self.assertAlmostEquals(90.0, surface.get_dip())
@@ -160,30 +107,10 @@ class SimpleFaultSurfaceTestCase(utils.SurfaceTestCase):
         p1 = Point(0.0, 0.0)
         p2 = Point(0.0635916966572, 0.0635916574897)
 
-        surface = SimpleFaultSurface(Line([p1, p2]),
+        surface = SimpleFaultSurface.from_fault_data(Line([p1, p2]),
                 1.0, 6.0, 30.0, 1.0)
 
         self.assertAlmostEquals(30.0, surface.get_dip(), 1)
-
-    def test_get_dip_3(self):
-        p1 = Point(0.0, 0.0)
-        p2 = Point(0.0635916966572, 0.0635916574897)
-        p3 = Point(0.0635916966572, -1.30558137286e-08)
-
-        surface = SimpleFaultSurface(Line([p1, p2, p3]),
-                1.0, 6.0, 45.0, 20.0)
-
-        self.assertAlmostEquals(45.0, surface.get_dip())
-
-    def test_get_dip_4(self):
-        p1 = Point(0.0, 0.0)
-        p2 = Point(0.0635916966572, 0.0635916574897)
-        p3 = Point(0.0635916966572, -1.30558137286e-08)
-
-        surface = SimpleFaultSurface(Line([p1, p2, p3]),
-                1.0, 40.0, 45.0, 30.0)
-
-        self.assertAlmostEquals(45.0, surface.get_dip())
 
     def test_get_dip_5(self):
         p1 = Point(0.0, 0.0)
@@ -191,7 +118,7 @@ class SimpleFaultSurfaceTestCase(utils.SurfaceTestCase):
         p3 = Point(0.0899323137217, 0.0899320921571)
         p4 = Point(0.0899323137217, -1.10782376538e-07)
 
-        surface = SimpleFaultSurface(Line([p1, p2, p3, p4]),
+        surface = SimpleFaultSurface.from_fault_data(Line([p1, p2, p3, p4]),
                 0.0, 10.0, 45.0, 10.0)
 
         self.assertAlmostEquals(75.0, surface.get_dip(), 1)
