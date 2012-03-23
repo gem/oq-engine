139f0f1717ee25dcdce8e4b86510b7ba017025f5
Andrea Cerisara <andreacerisara@gmail.com>
2012-03-23 16:38:25 +0100

    Implementation of the scientific stuff

--- tests/calculators/risk/scenario_damage/core_unittest.py
+++ tests/calculators/risk/scenario_damage/core_unittest.py
@@ -16,30 +16,39 @@
 import unittest
 import json
 
-from openquake import kvs
+from django.contrib.gis import geos
 
-from openquake.shapes import Site, RegionConstraint, GridPoint
+from openquake import kvs
+from openquake.db import models
+from openquake.shapes import Site, GridPoint
 from openquake.engine import JobContext
 from openquake.kvs.tokens import ground_motion_values_key
 from openquake.calculators.risk.general import Block
-from openquake.calculators.risk.scenario_damage.core import ScenarioDamageRiskCalculator
+from openquake.calculators.risk.scenario_damage.core import (
+    ScenarioDamageRiskCalculator)
 
-JOB_ID = 1
+from tests.utils import helpers
+
 BLOCK_ID = 1
 
-class ScenarioDamageRiskCalculatorTestCase(unittest.TestCase):
+class ScenarioDamageRiskCalculatorTestCase(
+    unittest.TestCase, helpers.DbTestCase):
+    
+    job = None
     
     def setUp(self):
-        kvs.mark_job_as_current(JOB_ID)
-        kvs.cache_gc(JOB_ID)
-    
-    def test_pre_execute(self):
-        pass
+        kvs.mark_job_as_current(self.job.id)
+        kvs.cache_gc(self.job.id)
+
+        self.site = Site(1.0, 1.0)
+
+    @classmethod
+    def setUpClass(cls):
+        inputs = [("fragility", ""), ("exposure", "")]
+        cls.job = cls.setup_classic_job(inputs=inputs)
 
     def test_compute_risk(self):
-        site = Site(1.0, 1.0)
-
-        block = Block(JOB_ID, BLOCK_ID, [site])
+        block = Block(self.job.id, BLOCK_ID, [self.site])
         block.to_kvs()
 
         # this region contains a single site, that is exactly
@@ -47,19 +56,56 @@ class ScenarioDamageRiskCalculatorTestCase(unittest.TestCase):
         params = {"REGION_VERTEX": "1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0",
                 "REGION_GRID_SPACING": "0.5"}
 
-        job_ctxt = JobContext(params, JOB_ID)
+        job_ctxt = JobContext(params, self.job.id)
 
         # now storing in kvs the ground motion values
         self._store_gmvs([0.40, 0.30, 0.45, 0.35, 0.40])
+        
+        self._store_em()
 
         calculator = ScenarioDamageRiskCalculator(job_ctxt)
-        calculator.compute_risk(BLOCK_ID)
+        calculator.compute_risk(BLOCK_ID, fmodel=self._fmodel())
 
     def _store_gmvs(self, gmvs):
         client = kvs.get_client()
         encoder = json.JSONEncoder()
 
-        key = ground_motion_values_key(JOB_ID, GridPoint(None, 0, 0))
+        key = ground_motion_values_key(self.job.id, GridPoint(None, 0, 0))
 
         for gmv in gmvs:
             client.rpush(key, encoder.encode({"mag": gmv}))
+
+    def _fmodel(self):
+        [ism] = models.inputs4job(self.job.id, input_type="fragility")
+
+        fmodel = models.FragilityModel(
+            owner=ism.owner, input=ism,
+            lss=["LS1", "LS2"], format="continuous")
+
+        fmodel.save()
+
+        models.Ffc(
+            fragility_model=fmodel, taxonomy="RC",
+            ls="LS1", mean="0.20", stddev="0.05").save()
+
+        models.Ffc(
+            fragility_model=fmodel, taxonomy="RC",
+            ls="LS2", mean="0.35", stddev="0.10").save()
+
+        return fmodel
+
+    def _store_em(self):
+        [ism] = models.inputs4job(self.job.id, input_type="exposure")
+
+        em = models.ExposureModel(
+            owner=ism.owner, input=ism,
+            name="AAA", category="single_asset",
+            reco_type="aggregated", reco_unit="USD",
+            stco_type="aggregated", stco_unit="USD")
+
+        em.save()
+
+        models.ExposureData(
+            exposure_model=em, taxonomy="RC",
+            asset_ref="A", stco=100,
+            site=geos.GEOSGeometry(self.site.point.to_wkt()), reco=1).save()
