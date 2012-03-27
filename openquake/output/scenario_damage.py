92a35de5966202443722442fa32e3dd0de9e48c2
Andrea Cerisara <andreacerisara@gmail.com>
2012-03-27 16:41:43 +0200

    Implementation
    Former-commit-id: c06333f79925bbdad4ebf1785e278dff0ff4dad1 [formerly c06333f79925bbdad4ebf1785e278dff0ff4dad1 [formerly 967de6eefd9de2e36fc48b51344c7568683aca1d]]
    Former-commit-id: d214d19a4cebbbc4b4cbce9222e6807824ab1ef6
    Former-commit-id: e774078bcf19785859e33b07809a9e0f7aefdbf7

--- openquake/output/scenario_damage.py
+++ openquake/output/scenario_damage.py
@@ -13,7 +13,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
 
-
 from lxml import etree
 
 from openquake import xml
@@ -26,37 +25,63 @@ class DmgDistPerAssetXMLWriter(object):
         self.damage_states = damage_states
         self.end_branch_label = end_branch_label
 
+        # < /nrml> element
+        self.root = None
+
+        # < /dmgDistPerAsset> element
+        self.dmg_dist_el = None
+
     def serialize(self, assets_data):
+        # contains the set of </ DDNode> elements indexed per site
         dd_nodes = {}
-        
+
+        # contains the set of </ asset> elements indexed per asset ref
+        asset_nodes = {}
+
         with open(self.path, "w") as fh:
-
-            root = etree.Element("nrml", nsmap=xml.NSMAP)
-            root.set("%sid" % xml.GML, "n1")
-
-            dmg_dist_el = etree.SubElement(root, "dmgDistPerAsset")
-            dmg_dist_el.set("endBranchLabel", self.end_branch_label)
-
-            dmg_states = etree.SubElement(dmg_dist_el, "damageStates")
-            dmg_states.text = " ".join(self.damage_states)
+            self._create_root_elems()
 
             for asset_data in assets_data:
                 site = asset_data.exposure_data.site
+                asset_ref = asset_data.exposure_data.asset_ref
 
-                dd_node_el = nodes.get(site, None)
+                dd_node_el = dd_nodes.get(site.wkt, None)
 
                 if dd_node_el is None:
-                    dd_node_el = etree.SubElement(dmg_dist_el, "DDNode")
+                    dd_node_el = etree.SubElement(self.dmg_dist_el, "DDNode")
                     site_el = etree.SubElement(dd_node_el, xml.RISK_SITE_TAG)
 
                     point_el = etree.SubElement(site_el, xml.GML_POINT_TAG)
                     point_el.set(xml.GML_SRS_ATTR_NAME, xml.GML_SRS_EPSG_4326)
 
                     pos_el = etree.SubElement(point_el, xml.GML_POS_TAG)
-                    pos_el.text = "%s %s" % (site.longitude, site.latitude)
+                    pos_el.text = "%s %s" % (site.x, site.y)
 
-                    nodes[site] = dd_node_el
+                    dd_nodes[site.wkt] = dd_node_el
+
+                asset_node_el = asset_nodes.get(asset_ref, None)
+
+                if asset_node_el is None:
+                    asset_node_el = etree.SubElement(dd_node_el, "asset")
+                    asset_node_el.set("assetRef", asset_ref)
+
+                    asset_nodes[asset_ref] = asset_node_el
+
+                ds_node = etree.SubElement(asset_node_el, "damage")
+                ds_node.set("ds", asset_data.dmg_state)
+                ds_node.set("mean", str(asset_data.mean))
+                ds_node.set("stddev", str(asset_data.stddev))
 
             fh.write(etree.tostring(
-                root, pretty_print=True, xml_declaration=True,
+                self.root, pretty_print=True, xml_declaration=True,
                 encoding="UTF-8"))
+
+    def _create_root_elems(self):
+        self.root = etree.Element("nrml", nsmap=xml.NSMAP)
+        self.root.set("%sid" % xml.GML, "n1")
+
+        self.dmg_dist_el = etree.SubElement(self.root, "dmgDistPerAsset")
+        self.dmg_dist_el.set("endBranchLabel", self.end_branch_label)
+
+        dmg_states = etree.SubElement(self.dmg_dist_el, "damageStates")
+        dmg_states.text = " ".join(self.damage_states)
