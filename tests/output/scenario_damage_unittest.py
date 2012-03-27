967de6eefd9de2e36fc48b51344c7568683aca1d
Andrea Cerisara <andreacerisara@gmail.com>
2012-03-27 16:41:43 +0200

    Implementation

--- tests/output/scenario_damage_unittest.py
+++ tests/output/scenario_damage_unittest.py
@@ -18,18 +18,123 @@ import os
 import tempfile
 import unittest
 
+from django.contrib.gis import geos
+from openquake import shapes
+from openquake.db import models
 from openquake.output.scenario_damage import DmgDistPerAssetXMLWriter
 
 from tests.utils import helpers
 
 
-class UHSXMLWriterTestCase(unittest.TestCase):
+class DmgDistPerAssetXMLWriterTestCase(unittest.TestCase, helpers.DbTestCase):
+
+    job = None
+
+    @classmethod
+    def setUpClass(cls):
+        inputs = [("exposure", "")]
+        cls.job = cls.setup_classic_job(inputs=inputs)
+
+    def setUp(self):
+        self.data = []
+        self.dda = None
 
     def test_serialize(self):
-        # expected_file = helpers.get_data_path('expected-uhs-export.xml')
-        # expected_text = open(expected_file, 'r').readlines()
+# TODO: 1. map nrml/schema/examples/dmg-dist-per-asset.xml DONE
+#       2. check results
+#       3. refactoring
+#       4. validation
+#       5. serialization with no data
+        damage_states = [
+            "no_damage",
+            "slight",
+            "moderate",
+            "extensive",
+            "complete"]
 
-        damage_states = ["no_damage", "slight", "moderate", "extensive"]
-        writer = DmgDistPerAssetXMLWriter("", "end_branch_label", damage_states)
+        writer = DmgDistPerAssetXMLWriter("p.txt",
+                "ebl1", damage_states)
 
-        writer.serialize(None)
+        output = models.Output(
+            owner=self.job.owner,
+            oq_job=self.job,
+            display_name="",
+            db_backed=True,
+            output_type="dmg_dist_per_asset")
+
+        output.save()
+
+        self.dda = models.DmgDistPerAsset(
+            output=output,
+            dmg_states=damage_states)
+
+        self.dda.save()
+
+        asset_1, asset_2, asset_3 = self.make_assets()
+
+        self.make_data(asset_1, "no_damage", 1.0, 1.6)
+        self.make_data(asset_1, "slight", 34.8, 18.3)
+        self.make_data(asset_1, "moderate", 64.2, 19.8)
+        self.make_data(asset_1, "extensive", 64.3, 19.7)
+        self.make_data(asset_1, "complete", 64.3, 19.7)
+
+        self.make_data(asset_2, "no_damage", 1.0, 1.6)
+        self.make_data(asset_2, "slight", 34.8, 18.3)
+        self.make_data(asset_2, "moderate", 64.2, 19.8)
+        self.make_data(asset_2, "extensive", 64.3, 19.7)
+        self.make_data(asset_2, "complete", 64.3, 19.7)
+
+        self.make_data(asset_3, "no_damage", 1.1, 1.7)
+        self.make_data(asset_3, "slight", 34.9, 18.4)
+        self.make_data(asset_3, "moderate", 64.3, 19.7)
+        self.make_data(asset_3, "extensive", 64.3, 19.7)
+        self.make_data(asset_3, "complete", 64.3, 19.7)
+
+        writer.serialize(self.data)
+
+    def make_assets(self):
+        [ism] = models.inputs4job(self.job.id, input_type="exposure")
+
+        em = models.ExposureModel(
+            owner=ism.owner, input=ism,
+            name="AAA", category="single_asset",
+            reco_type="aggregated", reco_unit="USD",
+            stco_type="aggregated", stco_unit="USD")
+
+        em.save()
+
+        site_1 = shapes.Site(-116.0, 41.0)
+        site_2 = shapes.Site(-117.0, 42.0)
+
+        asset_1 = models.ExposureData(
+            exposure_model=em, taxonomy="RC",
+            asset_ref="asset_1", number_of_units=100, stco=1,
+            site=geos.GEOSGeometry(site_1.point.to_wkt()), reco=1)
+
+        asset_2 = models.ExposureData(
+            exposure_model=em, taxonomy="RM",
+            asset_ref="asset_2", number_of_units=40, stco=1,
+            site=geos.GEOSGeometry(site_2.point.to_wkt()), reco=1)
+
+        asset_3 = models.ExposureData(
+            exposure_model=em, taxonomy="RM",
+            asset_ref="asset_3", number_of_units=40, stco=1,
+            site=geos.GEOSGeometry(site_2.point.to_wkt()), reco=1)
+
+        asset_1.save()
+        asset_2.save()
+        asset_3.save()
+
+        return asset_1, asset_2, asset_3
+
+    def make_data(self, asset, dmg_state, mean, stddev):
+        data = models.DmgDistPerAssetData(
+            dmg_dist_per_asset = self.dda,
+            exposure_data = asset,
+            dmg_state = dmg_state,
+            mean = mean,
+            stddev = stddev,
+            location = asset.site)
+
+        data.save()
+        self.data.append(data)
