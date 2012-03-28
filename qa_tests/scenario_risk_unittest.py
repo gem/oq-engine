ca11a465b6e2398041bb9b56e42a90f2f2532d98
Lars Butler <Lars.Butler@gmail.com>
2012-03-28 19:55:12 +0200

    More thorough QA test for scenario risk

--- qa_tests/scenario_risk_unittest.py
+++ qa_tests/scenario_risk_unittest.py
@@ -13,11 +13,14 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
 import unittest
 
+from lxml import etree
 from nose.plugins.attrib import attr
 
 from openquake.db.models import OqJob
+from openquake import xml
 
 from tests.utils import helpers
 
@@ -25,14 +28,86 @@ from tests.utils import helpers
 class ScenarioRiskQATest(unittest.TestCase):
     """QA test for the Scenario Risk calculator."""
 
+    #: decimal places
+    TOTAL_LOSS_PRECISION = 2
+    #: decimal places
+    LOSSMAP_PRECISION = 4
+
+    def _verify_loss_map(self, path, expected_data):
+        namespaces = dict(nrml=xml.NRML_NS, gml=xml.GML_NS)
+        root = etree.parse(path)
+
+        actual_lm_data = []
+        lm_nodes = root.xpath('.//nrml:LMNode', namespaces=namespaces)
+
+        # sanity check: there should be only 3 loss map nodes:
+        self.assertEqual(3, len(lm_nodes))
+
+        for node in lm_nodes:
+            node_data = dict()
+
+            [pos] = node.xpath('.//gml:pos', namespaces=namespaces)
+            node_data['pos'] = pos.text
+
+            [loss] = node.xpath('./nrml:loss', namespaces=namespaces)
+            node_data['asset'] = loss.get('assetRef')
+
+            [mean] = loss.xpath('./nrml:mean', namespaces=namespaces)
+            [stddev] = loss.xpath('./nrml:stdDev', namespaces=namespaces)
+            node_data['mean'] = float(mean.text)
+            node_data['stddev'] = float(stddev.text)
+
+            actual_lm_data.append(node_data)
+
+        helpers.assertDeepAlmostEqual(
+            self, expected_data, actual_lm_data, places=self.LOSSMAP_PRECISION)
+
     @attr('qa')
     def test_scenario_risk(self):
         # The rudimentary beginnings of a QA test for the scenario calc.
         # For now, just run it end-to-end to make sure it doesn't blow up.
         scen_cfg = helpers.demo_file('scenario_risk/config.gem')
 
-        ret_code = helpers.run_job(scen_cfg, ['--output-type=xml'])
-        self.assertEqual(0, ret_code)
+        expected_loss_map = [
+            dict(asset='a3', pos='15.48 38.25', mean=217.510673644,
+                 stddev=86.3215466446),
+            dict(asset='a2', pos='15.56 38.17', mean=469.375607933,
+                 stddev=271.423557166),
+            dict(asset='a1', pos='15.48 38.09', mean=585.814597,
+                 stddev=270.632803227),
+        ]
+
+        expected_mean = 1272.70
+        expected_stddev = 455.83
+
+        result = helpers.run_job(scen_cfg, ['--output-type=xml'],
+                                 check_output=True)
 
         job = OqJob.objects.latest('id')
         self.assertEqual('succeeded', job.status)
+
+        expected_loss_map_file = helpers.demo_file(
+            'scenario_risk/computed_output/loss-map-%s.xml' % job.id)
+
+        self.assertTrue(os.path.exists(expected_loss_map_file))
+
+        self._verify_loss_map(expected_loss_map_file, expected_loss_map)
+
+        # We expected the shell output to look something like the following
+        # two lines:
+        # Mean region loss value: 1272.70087858
+        # Standard deviation region loss value: 455.834734995
+
+        # split on newline and filter out empty lines
+        result = [line for line in result.split('\n') if len(line) > 0]
+
+        # we expect 2 lines; 1 for mean, 1 for stddev
+        self.assertEqual(2, len(result))
+
+        actual_mean = float(result[0].split()[-1])
+        actual_stddev = float(result[1].split()[-1])
+
+        self.assertAlmostEqual(
+            expected_mean, actual_mean, places=self.TOTAL_LOSS_PRECISION)
+        self.assertAlmostEqual(
+            expected_stddev, actual_stddev, places=self.TOTAL_LOSS_PRECISION)
