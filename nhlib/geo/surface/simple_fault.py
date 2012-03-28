c723a41ba39e627a271eac4a0404f55d9351250e
Anton Gritsay <anton@angri.ru>
2012-03-28 17:28:03 +0200

    geo/surface/simple_fault [docs]: fixed module's docstring

--- nhlib/geo/surface/simple_fault.py
+++ nhlib/geo/surface/simple_fault.py
@@ -14,7 +14,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-Module :mod:`nhlib.surface.simple_fault` contains :class:`SimpleFaultSurface`.
+Module :mod:`nhlib.geo.surface.simple_fault` defines
+:class:`SimpleFaultSurface`.
 """
 import math
 import numpy
