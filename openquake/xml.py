6293c6014c27da304dfbe170e43712a4d6eeee29
Lars Butler <Lars.Butler@gmail.com>
2012-05-15 11:00:24 +0200

    Fixed a bunch of imports point to the nrml submod.

--- openquake/xml.py
+++ openquake/xml.py
@@ -23,8 +23,8 @@ including namespaces, and namespace maps.
 
 from lxml import etree
 
-from openquake.nrml import utils
 from openquake import shapes
+from openquake.nrml import utils as nrml_utils
 
 
 NRML_NS = 'http://openquake.org/xmlns/nrml/0.3'
@@ -124,8 +124,8 @@ class XMLMismatchError(Exception):
         return self.message
 
 
-def validates_against_xml_schema(xml_instance_path,
-        schema_path=utils.nrml_schema_file()):
+def validates_against_xml_schema(
+    xml_instance_path, schema_path=nrml_utils.nrml_schema_file()):
     """
     Checks whether an XML file validates against an XML Schema
 
