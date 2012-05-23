a50a58af3252349cbcf20e01e4f5af78caf90ab6
Giuseppe Vallarelli <giuseppevallarelli@gmail.com>
2012-05-23 10:10:26 +0200

    updated check noDamageLimit can be zero
    Former-commit-id: 5183a8bbce141737b6dc5e982bc84844d3be5c04 [formerly 11cb9f9c5d227a9401b2719237038db54afac7ff]
    Former-commit-id: 510f8e41e7818e36a4c5f4e9c3adcce8b7a62e2d

--- openquake/db/schema/functions.sql
+++ openquake/db/schema/functions.sql
@@ -465,7 +465,8 @@ AS $$
         assert imt in imts, "invalid IMT (%s)" % imt
         if no_damage_limit:
             assert no_damage_limit < imls[0], "No Damage Limit must be less than IML values"
-            assert no_damage_limit > 0, "No Damage Limit must be a positive value"
+            assert no_damage_limit >= 0, "No Damage Limit must be a positive
+            value"
         
     else:
         assert imls is None, "IMLs defined for continuous fragility model"
