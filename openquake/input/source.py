d0efb6acaca0b1f64448cd95838dc2cdb0124d25
Lars Butler <Lars.Butler@gmail.com>
2012-06-08 14:06:40 +0200

    Clean up (docstrings, etc.)

--- openquake/input/source.py
+++ openquake/input/source.py
@@ -45,12 +45,14 @@ def nrml_to_nhlib(src, mesh_spacing, bin_width, area_src_disc):
 
     See :mod:`nrml.models` and :mod:`nhlib.source`.
 
-    :param mesh_spacing:
+    :param src:
+        :mod:`nrml.models` seismic source instance.
+    :param float mesh_spacing:
         Rupture mesh spacing, in km.
-    :param bin_width:
+    :param float bin_width:
         Truncated Gutenberg-Richter MFD (Magnitude Frequency Distribution) bin
         width.
-    :param area_src_disc:
+    :param float area_src_disc:
         Area source discretization, in km. Applies only to area sources.
         If the input source is known to be a type other than an area source,
         you can specify `area_src_disc=None`.
@@ -77,9 +79,9 @@ def _point_to_nhlib(src, mesh_spacing, bin_width):
 
     :param src:
         :class:`nrml.models.PointSource` instance.
-    :param mesh_spacing:
+    :param float mesh_spacing:
         Rupture mesh spacing, in km.
-    :param bin_width:
+    :param float bin_width:
         Truncated Gutenberg-Richter MFD (Magnitude Frequency Distribution) bin
         width.
     :returns:
@@ -124,12 +126,12 @@ def _area_to_nhlib(src, mesh_spacing, bin_width, area_src_disc):
 
     :param src:
         :class:`nrml.models.PointSource` instance.
-    :param mesh_spacing:
+    :param float mesh_spacing:
         Rupture mesh spacing, in km.
-    :param bin_width:
+    :param float bin_width:
         Truncated Gutenberg-Richter MFD (Magnitude Frequency Distribution) bin
         width.
-    :param area_src_disc:
+    :param float area_src_disc:
         Area source discretization, in km. Applies only to area sources.
     :returns:
         The NHLib representation of the input source.
@@ -178,9 +180,9 @@ def _simple_to_nhlib(src, mesh_spacing, bin_width):
 
     :param src:
         :class:`nrml.models.PointSource` instance.
-    :param mesh_spacing:
+    :param float mesh_spacing:
         Rupture mesh spacing, in km.
-    :param bin_width:
+    :param float bin_width:
         Truncated Gutenberg-Richter MFD (Magnitude Frequency Distribution) bin
         width.
     :returns:
@@ -216,9 +218,9 @@ def _complex_to_nhlib(src, mesh_spacing, bin_width):
 
     :param src:
         :class:`nrml.models.PointSource` instance.
-    :param mesh_spacing:
+    :param float mesh_spacing:
         Rupture mesh spacing, in km.
-    :param bin_width:
+    :param float bin_width:
         Truncated Gutenberg-Richter MFD (Magnitude Frequency Distribution) bin
         width.
     :returns:
@@ -254,9 +256,15 @@ def _complex_to_nhlib(src, mesh_spacing, bin_width):
 
 
 def _mfd_to_nhlib(src_mfd, bin_width):
-    """
+    """Convert a NRML MFD to an NHLib MFD.
+
+    :param src_mfd:
+        :class:`nrml.models.IncrementalMFD` or :class:`nrml.models.TGRMFD`
+        instance.
     :param float bin_width:
         Optional. Required only for Truncated Gutenberg-Richter MFDs.
+    :returns:
+        The NHLib representation of the MFD. See :mod:`nhlib.mfd`.
     """
     if isinstance(src_mfd, nrml_models.TGRMFD):
         assert bin_width is not None
@@ -275,52 +283,75 @@ def _source_type(src_model):
     """Given of the source types defined in :mod:`nrml.models`, get the
     `source_type` for a :class:`~openquake.db.models.ParsedSource`.
     """
-    if isinstance(nrml_models.AreaSource):
+    if isinstance(src_model, nrml_models.AreaSource):
         return 'area'
-    elif isinstance(nrml_models.PointSource):
+    elif isinstance(src_model, nrml_models.PointSource):
         return 'point'
-    elif isinstance(nrml_models.ComplexFaultSource):
+    elif isinstance(src_model, nrml_models.ComplexFaultSource):
         return 'complex'
-    elif isinstance(nrml_models.SimpleFaultSource):
+    elif isinstance(src_model, nrml_models.SimpleFaultSource):
         return 'simple'
 
 
 class SourceDBWriter(object):
-    """
+    """Takes a sequence of seismic source objects and saves them to the
+    `hzrdi.parsed_source` table in the database.
+
+    The source object data will be stored in the database in pickled blob form.
+    The `hzrdi.parsed_source.polygon` field will contain the "rupture
+    enclosing" polygon. We use NHLib to generate this polygon. (See
+    :method:`nhlib.source.base.SeismicSource.get_rupture_enclosing_polygon`.)
+
     :param inp:
         :class:`~openquake.db.models.Input` object, the top-level container for
-        the sources written to the database. Should have an input_type of
+        the sources written to the database. Should have an `input_type` of
         'source'.
     :param source_model:
         :class:`nrml.models.SourceModel` object, which is an Iterable of NRML
         source model objects (parsed from NRML XML). This also includes the
         name of the source model.
+    :param float mesh_spacing:
+        Rupture mesh spacing, in km.
+    :param float bin_width:
+        Truncated Gutenberg-Richter MFD (Magnitude Frequency Distribution) bin
+        width.
+    :param float area_src_disc:
+        Area source discretization, in km. Applies only to area sources.
+        If the input source is known to be a type other than an area source,
+        you can specify `area_src_disc=None`.
     """
 
-    def __init__(self, inp, source_model, area_source_disc, mesh_spacing,
-                 bin_width):
+    def __init__(self, inp, source_model, mesh_spacing, bin_width,
+                 area_src_disc):
         self.inp = inp
         self.source_model = source_model
 
-        self.area_source_disc = area_source_disc
         self.mesh_spacing = mesh_spacing
         self.bin_width = bin_width
+        self.area_src_disc = area_src_disc
 
     @transaction.commit_on_success(router.db_for_write(models.ParsedSource))
     def serialize(self):
 
+        assert self.inp.input_type == 'source', (
+            "`Input` object has the wrong `input_type`. Expected: 'source'."
+            "Got: '%s'."
+        ) % self.inp.input_type
         # First, set the input name to the source model name
         self.inp.name = self.source_model.name
         self.inp.save()
 
         for source in self.source_model:
             blob = pickle.dumps(source, pickle.HIGHEST_PROTOCOL)
-            nhlib_src = nrml_to_nhlib(source)
+            nhlib_src = nrml_to_nhlib(
+                source, self.mesh_spacing, self.bin_width, self.area_src_disc
+            )
             geom = nhlib_src.get_rupture_enclosing_polygon()
+            # Resample and initialize the `shapely` polygon
             geom._init_polygon2d()
-            geom_wkt = geom._polygon2d.wkt
 
             ps = models.ParsedSource(
                 input=self.inp, source_type=_source_type(source), blob=blob,
-                geom=geom_wkt
+                polygon=geom._polygon2d.wkt
             )
+            ps.save()
