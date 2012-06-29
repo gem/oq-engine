bc13cdc74bac73dcba78403f70f8f6a6a88b39e2
Lars Butler <Lars.Butler@gmail.com>
2012-06-29 10:34:31 +0200

    Finished `initialize_realizations`
    This new functionality populates `lt_realization` and `source_progress`
    tables during the `pre_execute` phase of the classical hazard calculator.
    
    The only remaining table in the `htemp` schema that we need to populate
    is `hazard_curve_progress`.
    
    
    Former-commit-id: db06aca252a61e22a7a4657694f7084cf556e48c

--- openquake/calculators/hazard/classical/core_next.py
+++ openquake/calculators/hazard/classical/core_next.py
@@ -15,16 +15,21 @@
 
 
 import os
+import random
 import StringIO
 
+from django.db import transaction
 from nrml import parsers as nrml_parsers
 
 from openquake import engine2
+from openquake import writer
 from openquake.calculators import base
 from openquake.calculators.hazard import general
 from openquake.db import models
 from openquake.input import logictree
 from openquake.input import source
+from openquake.job.validation import MIN_SINT_32
+from openquake.job.validation import MAX_SINT_32
 
 
 class ClassicalHazardCalculator(base.CalculatorNext):
@@ -71,7 +76,6 @@ class ClassicalHazardCalculator(base.CalculatorNext):
         [gsimlt] = models.inputs4hcalc(hc.id, input_type='lt_gsim')
         source_paths = logictree.read_logic_trees(
             hc.base_path, smlt.path, gsimlt.path)
-        print "source paths are: %s" % source_paths
 
         src_inputs = []
         for src_path in source_paths:
@@ -99,11 +103,66 @@ class ClassicalHazardCalculator(base.CalculatorNext):
                 hc.width_of_mfd_bin, hc.area_source_discretization)
             src_db_writer.serialize()
 
+    @transaction.commit_on_success(using='reslt_writer')
+    def initialize_realizations(self):
+        hc = self.job.hazard_calculation
+
+        # Each realization will have two seeds:
+        # One for source model logic tree, one for GSIM logic tree.
+        rnd = random.Random()
+        seed = hc.random_seed
+        rnd.seed(seed)
+
+        [smlt] = models.inputs4hcalc(hc.id, input_type='lt_source')
+
+        ltp = logictree.LogicTreeProcessor(hc.id)
+
+        # The first realization gets the seed we specified in the config file.
+        for i in xrange(hc.number_of_logic_tree_samples):
+            lt_rlz = models.LtRealization(hazard_calculation=hc)
+            lt_rlz.ordinal = i
+            lt_rlz.seed = seed
+
+            # Sample source model logic tree branch paths:
+            sm_name, _, sm_lt_branch_ids = ltp.sample_source_model_logictree(
+                rnd.randint(MIN_SINT_32, MAX_SINT_32))
+            lt_rlz.sm_lt_path = sm_lt_branch_ids
+
+            # Sample GSIM logic tree branch paths:
+            _, gsim_branch_ids = ltp.sample_gmpe_logictree(
+                rnd.randint(MIN_SINT_32, MAX_SINT_32))
+            lt_rlz.gsim_lt_path = gsim_branch_ids
+
+            # Get the source model for this sample:
+            hzrd_src = models.Src2ltsrc.objects.get(
+                lt_src=smlt.id, filename=sm_name).hzrd_src
+            parsed_sources = models.ParsedSource.objects.filter(input=hzrd_src)
+
+            lt_rlz.total_sources = len(parsed_sources)
+            lt_rlz.save()
+
+            # Create source_progress for this realization
+            # A bulk insert is more efficient because there could be lots of
+            # of individual sources.
+            sp_inserter = writer.BulkInserter(models.SourceProgress)
+            for ps in parsed_sources:
+                sp_inserter.add_entry(
+                    lt_realization_id=lt_rlz.id, parsed_source_id=ps.id)
+            sp_inserter.flush()
+
+            # update the seed for the next realization
+            seed = rnd.randint(MIN_SINT_32, MAX_SINT_32)
 
     def pre_execute(self):
 
         # Parse logic trees and create source Inputs.
         self.initialize_sources()
+
         # Deal with the site model and compute site data for the calculation
         # (if a site model was specified, that is).
         self.initialize_site_model()
+
+        # Now bootstrap the logic tree realizations and related data.
+        # This defines for us the "work" that needs to be done when we reach
+        # the `execute` phase.
+        self.initialize_realizations()
