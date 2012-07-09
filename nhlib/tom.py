964b16d40c5bdb1418a080f4a022297df7947544
Anton Gritsay <anton@angri.ru>
2012-07-09 17:31:48 +0200

    tom: added get_probability_one_occurrence()

--- nhlib/tom.py
+++ nhlib/tom.py
@@ -20,6 +20,7 @@ density functions for earthquake temporal occurrence modeling.
 import math
 
 import numpy
+import scipy.stats
 
 
 class PoissonTOM(object):
@@ -50,6 +51,14 @@ class PoissonTOM(object):
             Float value between 0 and 1 inclusive.
         """
         return 1 - math.exp(- occurrence_rate * self.time_span)
+
+    def get_probability_one_occurrence(self, occurrence_rate):
+        """
+        Calculate and return the probability of event to occur once
+        within the time range defined by the constructor's ``time_span``
+        parameter value.
+        """
+        return scipy.stats.poisson(occurrence_rate * self.time_span).pmf(1)
 
     def sample_number_of_occurrences(self, occurrence_rate):
         """
