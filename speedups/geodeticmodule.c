27f6e4a3b79cbf79bcd8613e1f7f9cf7b5c6c5ab
Anton Gritsay <anton@angri.ru>
2012-08-10 12:26:45 +0200

    speedups/geodeticmodule: brushed up the code a bit (+)
    added docstrings, removed "register" variable declarations, rearranged
    some lines here and there

--- speedups/geodeticmodule.c
+++ speedups/geodeticmodule.c
@@ -6,6 +6,29 @@
 #define EARTH_RADIUS 6371.0
 
 
+/*
+ * Calculate the distance between two points along the geodetic.
+ * Parameters are two pairs of spherical coordinates in radians.
+ */
+static inline double
+geodetic__geodetic_distance(double lon1, double lat1, double lon2, double lat2)
+{
+    return asin(sqrt(
+        pow(sin((lat1 - lat2) / 2.0), 2.0)
+        + cos(lat1) * cos(lat2) * pow(sin((lon1 - lon2) / 2.0), 2.0)
+    )) * 2 * EARTH_RADIUS;
+}
+
+
+static const char geodetic_geodetic_distance__doc__[] = "\n\
+    Calculate the geodetic distance between two collections of points,\n\
+    following the numpy broadcasting rules.\n\
+    \n\
+    geodetic_distance(lons1, lats1, lons2, lats2) -> dists\n\
+    \n\
+    Parameters must be numpy arrays of double, representing spherical\n\
+    coordinates in radians.\n\
+";
 static PyObject *
 geodetic_geodetic_distance(
         PyObject *self,
@@ -21,30 +44,23 @@ geodetic_geodetic_distance(
                 &PyArray_Type, &lons2, &PyArray_Type, &lats2))
         return NULL;
 
-    PyArrayObject *op[5];
-    npy_uint32 flags;
+    PyArrayObject *op[5] = {lons1, lats1, lons2, lats2, NULL /* distance */};
+    npy_uint32 flags = 0;
     npy_uint32 op_flags[5];
     NpyIter_IterNextFunc *iternext;
     PyArray_Descr *double_dtype = PyArray_DescrFromType(NPY_DOUBLE);
-    PyArray_Descr *op_dtypes[] = {
-            double_dtype, double_dtype,
-            double_dtype, double_dtype,
-            double_dtype};
+    PyArray_Descr *op_dtypes[] = {double_dtype, double_dtype,
+                                  double_dtype, double_dtype,
+                                  double_dtype};
     char **dataptrarray;
 
-    flags = 0;
-    op[0] = lons1;
-    op[1] = lats1;
-    op[2] = lons2;
-    op[3] = lats2;
-    op[4] = NULL;
     op_flags[0] = op_flags[1] = op_flags[2] = op_flags[3] = NPY_ITER_READONLY;
     op_flags[4] = NPY_ITER_WRITEONLY | NPY_ITER_ALLOCATE;
+
     NpyIter *iter = NpyIter_MultiNew(
             5, op, flags, NPY_KEEPORDER, NPY_NO_CASTING,
             op_flags, op_dtypes);
     Py_DECREF(double_dtype);
-
     if (iter == NULL)
         return NULL;
 
@@ -52,16 +68,12 @@ geodetic_geodetic_distance(
     dataptrarray = NpyIter_GetDataPtrArray(iter);
     do
     {
-        double register lon1 = *(double *) dataptrarray[0];
-        double register lat1 = *(double *) dataptrarray[1];
-        double register lon2 = *(double *) dataptrarray[2];
-        double register lat2 = *(double *) dataptrarray[3];
-        double *res = (double *) dataptrarray[4];
-
-        *res = 2 * EARTH_RADIUS * asin(sqrt(
-            pow(sin((lat1 - lat2) / 2.0), 2.0)
-            + cos(lat1) * cos(lat2) * pow(sin((lon1 - lon2) / 2.0), 2.0)
-        ));
+        double lon1 = *(double *) dataptrarray[0];
+        double lat1 = *(double *) dataptrarray[1];
+        double lon2 = *(double *) dataptrarray[2];
+        double lat2 = *(double *) dataptrarray[3];
+        *(double *) dataptrarray[4] = geodetic__geodetic_distance(
+                lon1, lat1, lon2, lat2);
     } while (iternext(iter));
 
     PyArrayObject *result = NpyIter_GetOperandArray(iter)[4];
@@ -75,6 +87,23 @@ geodetic_geodetic_distance(
 }
 
 
+static const char geodetic_min_distance__doc__[] = "\n\
+    Calculate the minimum distance between two collections of points.\n\
+    \n\
+    min_distance(mlons, mlats, mdepths, slons, \\\n\
+            slats, sdepths, indices) -> min distances or indices of such\n\
+    \n\
+    Parameters mlons, mlats and mdepths represent coordinates of the first\n\
+    collection of points and slons, slats, sdepths are for the second one.\n\
+    All the coordinates must be numpy arrays of double. Longitudes and \n\
+    latitudes are in radians, depths are in km.\n\
+    \n\
+    Boolean parameter \"indices\" determines whether actual minimum\n\
+    distances should be returned or integer indices of closest points\n\
+    from first collection. Thus, result is numpy array of either distances\n\
+    (array of double, when indices=False) or indices (array of integers,\n\
+    when indices=True).\n\
+";
 static PyObject *
 geodetic_min_distance(
         PyObject *self,
@@ -88,7 +117,7 @@ geodetic_min_distance(
     PyArrayObject *mlons, *mlats, *mdepths, *slons, *slats, *sdepths;
     unsigned char indices = 0;
 
-    if (!PyArg_ParseTupleAndKeywords(args, keywds, "O!O!O!O!O!O!|b", kwlist,
+    if (!PyArg_ParseTupleAndKeywords(args, keywds, "O!O!O!O!O!O!b", kwlist,
                 &PyArray_Type, &mlons, &PyArray_Type, &mlats,
                 &PyArray_Type, &mdepths,
                 &PyArray_Type, &slons, &PyArray_Type, &slats,
@@ -99,8 +128,9 @@ geodetic_min_distance(
     PyArray_Descr *double_dtype = PyArray_DescrFromType(NPY_DOUBLE);
     PyArray_Descr *int_dtype = PyArray_DescrFromType(NPY_INT);
 
-    PyArrayObject *op_s[4], *op_m[3];
-    npy_uint32 flags_s, flags_m;
+    PyArrayObject *op_s[4] = {slons, slats, sdepths, NULL /* min distance */};
+    PyArrayObject *op_m[3] = {mlons, mlats, mdepths};
+    npy_uint32 flags_s = 0, flags_m = 0;
     npy_uint32 op_flags_s[4], op_flags_m[3];
     NpyIter_IterNextFunc *iternext_s, *iternext_m;
     PyArray_Descr *op_dtypes_s[] = {double_dtype, double_dtype, double_dtype,
@@ -109,63 +139,53 @@ geodetic_min_distance(
     PyArray_Descr *op_dtypes_m[] = {double_dtype, double_dtype, double_dtype};
     char **dataptrarray_s, **dataptrarray_m;
 
-    flags_s = 0;
-    op_s[0] = slons;
-    op_s[1] = slats;
-    op_s[2] = sdepths;
-    op_s[3] = NULL;
     op_flags_s[0] = op_flags_s[1] = op_flags_s[2] = NPY_ITER_READONLY;
     op_flags_s[3] = NPY_ITER_WRITEONLY | NPY_ITER_ALLOCATE;
+
     NpyIter *iter_s = NpyIter_MultiNew(
             4, op_s, flags_s, NPY_KEEPORDER, NPY_NO_CASTING,
             op_flags_s, op_dtypes_s);
+    Py_DECREF(int_dtype);
     if (iter_s == NULL) {
         Py_DECREF(double_dtype);
-        Py_DECREF(int_dtype);
         return NULL;
     }
     iternext_s = NpyIter_GetIterNext(iter_s, NULL);
     dataptrarray_s = NpyIter_GetDataPtrArray(iter_s);
 
-    flags_m = 0;
-    op_m[0] = mlons;
-    op_m[1] = mlats;
-    op_m[2] = mdepths;
     op_flags_m[0] = op_flags_m[1] = op_flags_m[2] = NPY_ITER_READONLY;
+
     NpyIter *iter_m = NpyIter_MultiNew(
             3, op_m, flags_m, NPY_KEEPORDER, NPY_NO_CASTING,
             op_flags_m, op_dtypes_m);
     Py_DECREF(double_dtype);
-    Py_DECREF(int_dtype);
     if (iter_m == NULL) {
         NpyIter_Deallocate(iter_s);
         return NULL;
     }
+
     iternext_m = NpyIter_GetIterNext(iter_m, NULL);
     dataptrarray_m = NpyIter_GetDataPtrArray(iter_m);
 
     do
     {
-        double register slon = *(double *) dataptrarray_s[0];
-        double register slat = *(double *) dataptrarray_s[1];
-        double register sdepth = *(double *) dataptrarray_s[2];
-        double register min_dist = INFINITY;
+        double slon = *(double *) dataptrarray_s[0];
+        double slat = *(double *) dataptrarray_s[1];
+        double sdepth = *(double *) dataptrarray_s[2];
+        double min_dist = INFINITY;
         int min_dist_idx = -1;
 
         do
         {
-            // TODO: precompute cos(mlat)
-            double register mlon = *(double *) dataptrarray_m[0];
-            double register mlat = *(double *) dataptrarray_m[1];
-            double register mdepth = *(double *) dataptrarray_m[2];
+            double mlon = *(double *) dataptrarray_m[0];
+            double mlat = *(double *) dataptrarray_m[1];
+            double mdepth = *(double *) dataptrarray_m[2];
 
-            double register geodetic_dist = asin(sqrt(
-                pow(sin((mlat - slat) / 2.0), 2.0)
-                + cos(mlat) * cos(slat) * pow(sin((mlon - slon) / 2.0), 2.0)
-            )) * 2 * EARTH_RADIUS;
+            double geodetic_dist = geodetic__geodetic_distance(
+                mlon, mlat, slon, slat);
 
-            double register vertical_dist = sdepth - mdepth;
-            double register dist;
+            double vertical_dist = sdepth - mdepth;
+            double dist;
             if (vertical_dist == 0)
                 dist = geodetic_dist;
             else
@@ -199,6 +219,22 @@ geodetic_min_distance(
 }
 
 
+static const char geodetic_convex_to_point_distance__doc__[] = "\n\
+    For each point of the collection calculate the distance to the convex\n\
+    polygon, treating points lying inside the polygon as having zero\n\
+    distance.\n\
+    \n\
+    Parameters cxx and cyy represent coordinates of convex polygon vertices\n\
+    in either clockwise or counterclockwise order. The last point must\n\
+    repeat the first one.\n\
+    \n\
+    Parameters pxx, pyy represent coordinates of the point collection.\n\
+    Both pairs of coordinates must be numpy arrays of double. They are \n\
+    treated as the ones in 2d Cartesian space.\n\
+    \n\
+    Result is numpy array of doubles -- distance in units of coordinate\n\
+    system.\n\
+";
 static PyObject *
 geodetic_convex_to_point_distance(
         PyObject *self,
@@ -218,13 +254,12 @@ geodetic_convex_to_point_distance(
 
     PyArrayObject *op_c[5];
     npy_uint32 op_flags_c[5];
-    npy_uint32 flags_c;
+    npy_uint32 flags_c = 0;
     NpyIter_IterNextFunc *iternext_c;
     PyArray_Descr *op_dtypes_c[] = {double_dtype, double_dtype, double_dtype,
                                     double_dtype, double_dtype};
     char **dataptrarray_c;
 
-    flags_c = 0;
     op_c[0] = cxx;
     op_c[1] = cyy;
     op_c[2] = NULL; // length
@@ -244,7 +279,7 @@ geodetic_convex_to_point_distance(
     iternext_c = NpyIter_GetIterNext(iter_c, NULL);
     dataptrarray_c = NpyIter_GetDataPtrArray(iter_c);
 
-    double register cx, cy, prev_cx, prev_cy, length, cos_theta, sin_theta;
+    double cx, cy, prev_cx, prev_cy, length, cos_theta, sin_theta;
     prev_cx = *(double *) dataptrarray_c[0];
     prev_cy = *(double *) dataptrarray_c[1];
 
@@ -252,8 +287,8 @@ geodetic_convex_to_point_distance(
     {
         cx = *(double *) dataptrarray_c[0];
         cy = *(double *) dataptrarray_c[1];
-        double register vx = prev_cx - cx;
-        double register vy = prev_cy - cy;
+        double vx = prev_cx - cx;
+        double vy = prev_cy - cy;
         length = sqrt(vx * vx + vy * vy);
         cos_theta = vx / length;
         sin_theta = vy / length;
@@ -266,23 +301,20 @@ geodetic_convex_to_point_distance(
         prev_cy = cy;
     };
 
-    PyArrayObject *op_p[3];
+    PyArrayObject *op_p[3] = {pxx, pyy, NULL /* distance */};
     npy_uint32 op_flags_p[3];
-    npy_uint32 flags_p;
+    npy_uint32 flags_p = 0;
     NpyIter_IterNextFunc *iternext_p;
     PyArray_Descr *op_dtypes_p[] = {double_dtype, double_dtype, double_dtype};
     char **dataptrarray_p;
 
-    flags_p = 0;
-    op_p[0] = pxx;
-    op_p[1] = pyy;
-    op_p[2] = NULL; // distance
-
     op_flags_p[0] = op_flags_p[1] = NPY_ITER_READONLY;
     op_flags_p[2] = NPY_ITER_WRITEONLY | NPY_ITER_ALLOCATE;
+
     NpyIter *iter_p = NpyIter_MultiNew(
             3, op_p, flags_p, NPY_KEEPORDER, NPY_NO_CASTING,
             op_flags_p, op_dtypes_p);
+
     Py_DECREF(double_dtype);
     if (iter_p == NULL) {
         NpyIter_Deallocate(iter_c);
@@ -293,12 +325,12 @@ geodetic_convex_to_point_distance(
 
     do
     {
-        char register has_lefts = 0;
-        char register has_rights = 0;
-        double register min_distance = INFINITY;
+        char has_lefts = 0;
+        char has_rights = 0;
+        double min_distance = INFINITY;
 
-        double register px = *(double *) dataptrarray_p[0];
-        double register py = *(double *) dataptrarray_p[1];
+        double px = *(double *) dataptrarray_p[0];
+        double py = *(double *) dataptrarray_p[1];
 
         NpyIter_Reset(iter_c, NULL);
         while (iternext_c(iter_c))
@@ -309,11 +341,11 @@ geodetic_convex_to_point_distance(
             cos_theta = *(double *) dataptrarray_c[3];
             sin_theta = *(double *) dataptrarray_c[4];
 
-            double register px2 = px - cx;
-            double register py2 = py - cy;
-            double register dist_x = px2 * cos_theta + py2 * sin_theta;
-            double register dist_y = - px2 * sin_theta + py2 * cos_theta;
-            double register dist;
+            double px2 = px - cx;
+            double py2 = py - cy;
+            double dist_x = px2 * cos_theta + py2 * sin_theta;
+            double dist_y = - px2 * sin_theta + py2 * cos_theta;
+            double dist;
 
             has_lefts |= dist_y < 0;
             has_rights |= dist_y > 0;
@@ -349,23 +381,23 @@ geodetic_convex_to_point_distance(
 }
 
 
-
 static PyMethodDef GeodeticSpeedupsMethods[] = {
     {"geodetic_distance",
-        (PyCFunction)geodetic_geodetic_distance,
-        METH_VARARGS | METH_KEYWORDS,
-        "TBD"},
+            (PyCFunction)geodetic_geodetic_distance,
+            METH_VARARGS | METH_KEYWORDS,
+            geodetic_geodetic_distance__doc__},
     {"min_distance",
-        (PyCFunction)geodetic_min_distance,
-        METH_VARARGS | METH_KEYWORDS,
-        "TBD"},
+            (PyCFunction)geodetic_min_distance,
+            METH_VARARGS | METH_KEYWORDS,
+            geodetic_min_distance__doc__},
     {"convex_to_point_distance",
-        (PyCFunction)geodetic_convex_to_point_distance,
-        METH_VARARGS | METH_KEYWORDS,
-        "TBD"},
+            (PyCFunction)geodetic_convex_to_point_distance,
+            METH_VARARGS | METH_KEYWORDS,
+            geodetic_convex_to_point_distance__doc__},
 
-    {NULL, NULL, 0, NULL}        /* Sentinel */
+    {NULL, NULL, 0, NULL} /* Sentinel */
 };
+
 
 PyMODINIT_FUNC
 init_geodetic_speedups(void)
