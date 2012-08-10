cd8ab5b76608678d4677385af46695a711b70181
Giuseppe Vallarelli <giuseppevallarelli@gmail.com>
2012-08-10 10:09:08 +0200

    Added tests (fake data) insured loss feature probabilistic calculator

---
+++ qa_tests/data/probabilistic_event_based_risk/ins_loss_data.py
@@ -0,0 +1,36 @@
+# Fake data should be replaced.
+
+#: decimal places
+TOTAL_LOSS_PRECISION = 2
+#: decimal places
+LOSSMAP_PRECISION = 4
+
+# Insured Loss output mean based approach
+
+MEAN_INS_LOSS_MB = 799.102578
+
+STDDEV_INS_LOSS_MB = 382.148808
+
+LOSS_MAP_MB = [
+    dict(asset='a3', pos='15.48 38.25', mean=156.750910806,
+        stddev=100.422061776),
+    dict(asset='a2', pos='15.56 38.17', mean=314.859579324,
+        stddev=293.976254984),
+    dict(asset='a1', pos='15.48 38.09', mean=327.492087529,
+        stddev=288.47906994),
+    ]
+
+# Insured Loss output sample based approach
+
+MEAN_INS_LOSS_SB = 799.102578
+
+STDDEV_INS_LOSS_SB = 382.148808
+
+LOSS_MAP_SB = [
+    dict(asset='a3', pos='15.48 38.25', mean=156.750910806,
+        stddev=100.422061776),
+    dict(asset='a2', pos='15.56 38.17', mean=314.859579324,
+        stddev=293.976254984),
+    dict(asset='a1', pos='15.48 38.09', mean=327.492087529,
+        stddev=288.47906994),
+    ]
