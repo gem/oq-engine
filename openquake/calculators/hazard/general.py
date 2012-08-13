69d92565f79e34276049cd3272debe7ca3e69e08
Lars Butler <Lars.Butler@gmail.com>
2012-08-13 12:53:17 +0200

    calculators/hazard/classical/core:
    Refactored the task arg generation. For the event based calculator,
    we'll need to generate the same task args as classical, in addition
    to a random seed. These seeds will be generated in sequence using
    the calculation `random_seed`.
    
    We will need to apply this random seed (at the beginning of each event
    based task) to the global numpy random seed state. Once set, we can
    generate SESs and ruptures (the process for which includes a random
    element) in a consistent manner (so that the calculation will give the
    same results every time).
    
    
    Former-commit-id: 8189f6146627b566899a8bd320402973cedc1022 [formerly 8189f6146627b566899a8bd320402973cedc1022 [formerly 65edf0c2827980ea65f3f65617d702e6ffe1ed4d]]
    Former-commit-id: 1804632edc84a27b8980623c8a60adbaaf94db66
    Former-commit-id: abdab1ae816c7f1de588c8324f8c61c489c8db7b

--- openquake/calculators/hazard/general.py
+++ openquake/calculators/hazard/general.py
@@ -615,6 +615,8 @@ class BaseHazardCalculatorNext(base.CalculatorNext):
 
     #: In subclasses, this would be a reference to the task function
     core_calc_task = None
+    #: Generator function for creating the arguments for each task
+    task_arg_gen = None
 
     def initialize_sources(self):
         """
@@ -896,30 +898,7 @@ class BaseHazardCalculatorNext(base.CalculatorNext):
         # When `progress['compute']` becomes equal to `progress['total']`,
         # `execute` can conclude.
 
-        def task_arg_gen():
-            """
-            Loop through realizations and sources to generate a sequence of
-            task arg tuples. Each tuple of args applies to a single task.
-
-            Yielded results are triples of (job_id, realization_id,
-            source_id_list).
-            """
-            realizations = models.LtRealization.objects.filter(
-                    hazard_calculation=hc, is_complete=False)
-
-            for lt_rlz in realizations:
-                source_progress = models.SourceProgress.objects.filter(
-                        is_complete=False, lt_realization=lt_rlz)
-                source_ids = source_progress.values_list('parsed_source_id',
-                                                         flat=True)
-                progress['total'] += len(source_ids)
-
-                for offset in xrange(0, len(source_ids), sources_per_task):
-                    task_args = (job.id, lt_rlz.id,
-                                 source_ids[offset:offset + sources_per_task])
-                    yield task_args
-
-        task_gen = task_arg_gen()
+        task_gen = self.task_arg_gen(hc, job, sources_per_task, progress)
 
         def task_complete_callback(body, message):
             """
