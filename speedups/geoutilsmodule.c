42b78a13dca6c7b421b8b4531a0f01bdbb846544
Anton Gritsay <anton@angri.ru>
2012-08-13 14:10:42 +0200

    speedups/geoutils: documented the code better

--- speedups/geoutilsmodule.c
+++ speedups/geoutilsmodule.c
@@ -44,17 +44,23 @@ geoutils_convex_to_point_distance(
         PyObject *args,
         PyObject *keywds)
 {
-    static char *kwlist[] = {"cxx", "cyy", "pxx", "pyy", NULL};
+    static char *kwlist[] = {"cxx", "cyy", /* convex coords */
+                             "pxx", "pyy", /* points coords */
+                             NULL}; /* sentinel */
 
     PyArrayObject *cxx, *cyy, *pxx, *pyy;
 
     if (!PyArg_ParseTupleAndKeywords(args, keywds, "O!O!O!O!", kwlist,
+                // convex coords
                 &PyArray_Type, &cxx, &PyArray_Type, &cyy,
+                // points coords
                 &PyArray_Type, &pxx, &PyArray_Type, &pyy))
         return NULL;
 
     PyArray_Descr *double_dtype = PyArray_DescrFromType(NPY_DOUBLE);
 
+    // we use the first iterator ("*_c", for "convex") for iterating over
+    // edges of the convex.
     PyArrayObject *op_c[5];
     npy_uint32 op_flags_c[5];
     npy_uint32 flags_c = 0;
@@ -65,9 +71,10 @@ geoutils_convex_to_point_distance(
 
     op_c[0] = cxx;
     op_c[1] = cyy;
-    op_c[2] = NULL; // length
-    op_c[3] = NULL; // cos_theta
-    op_c[4] = NULL; // sin_theta
+    op_c[2] = NULL; /* length of the edge */
+    op_c[3] = NULL; /* cos theta (angle between x-axis and the
+                       edge, counterclockwise) */
+    op_c[4] = NULL; /* sin theta */
 
     op_flags_c[0] = op_flags_c[1] = NPY_ITER_READONLY;
     op_flags_c[2] = op_flags_c[3] = op_flags_c[4] \
@@ -83,16 +90,25 @@ geoutils_convex_to_point_distance(
     dataptrarray_c = NpyIter_GetDataPtrArray(iter_c);
 
     double cx, cy, prev_cx, prev_cy, length, cos_theta, sin_theta;
+    // remember the "end point" of the first segment
     prev_cx = *(double *) dataptrarray_c[0];
     prev_cy = *(double *) dataptrarray_c[1];
 
+    // here we ignore the first iteration, this is intensional: we need
+    // to iterate over edges, not points
     while (iternext_c(iter_c))
     {
+        // cx and cy are coordinates of the "base point" of the current edge
+        // vector, prev_cx and prev_cy are ones of the "end point" of it
         cx = *(double *) dataptrarray_c[0];
         cy = *(double *) dataptrarray_c[1];
+        // get the free vector coordinates from the bound one
         double vx = prev_cx - cx;
         double vy = prev_cy - cy;
+        // calculate the length of the edge
         length = sqrt(vx * vx + vy * vy);
+        // calculate cosine and sine of the angle between x-axis
+        // and the free vector, measured counterclockwise
         cos_theta = vx / length;
         sin_theta = vy / length;
 
@@ -100,10 +116,14 @@ geoutils_convex_to_point_distance(
         *(double *) dataptrarray_c[3] = cos_theta;
         *(double *) dataptrarray_c[4] = sin_theta;
 
+        // the "base point" of the current vector is in turn the "end point"
+        // of the next one
         prev_cx = cx;
         prev_cy = cy;
     };
 
+    // second iterator is over the target points.
+    // we will collect distance in it as well.
     PyArrayObject *op_p[3] = {pxx, pyy, NULL /* distance */};
     npy_uint32 op_flags_p[3];
     npy_uint32 flags_p = 0;
@@ -128,8 +148,13 @@ geoutils_convex_to_point_distance(
 
     do
     {
+        // loop over points
+
+        // point lies on left hand side from some edges of the polygon
         char has_lefts = 0;
+        // same for right hand side
         char has_rights = 0;
+        // minimum distance found
         double min_distance = INFINITY;
 
         double px = *(double *) dataptrarray_p[0];
@@ -138,34 +163,63 @@ geoutils_convex_to_point_distance(
         NpyIter_Reset(iter_c, NULL);
         while (iternext_c(iter_c))
         {
+            // loop over edges: again ignore the first iteration
             cx = *(double *) dataptrarray_c[0];
             cy = *(double *) dataptrarray_c[1];
             length = *(double *) dataptrarray_c[2];
             cos_theta = *(double *) dataptrarray_c[3];
             sin_theta = *(double *) dataptrarray_c[4];
 
+            // now move the point to the coordinate space of the edge (where
+            // "base point" of the edge has coordinates (0, 0) and the edge
+            // itself goes along x axis) using affine transformations
+
+            // first translate the coordinates
             double px2 = px - cx;
             double py2 = py - cy;
+            // then rotate them
             double dist_x = px2 * cos_theta + py2 * sin_theta;
             double dist_y = - px2 * sin_theta + py2 * cos_theta;
+            // now dist_y is the perpendicular distance between the point
+            // and the line, containing the edge. it is negative in case
+            // when point lies on the right hand side of the edge vector.
+            // dist_x is the 1d coordinate of the projection of the point
+            // on that line. sign is negative if the projection is on the
+            // opposite side from the "end point" of the vector with respect
+            // to its "base point"
+
             double dist;
 
-            has_lefts |= dist_y < 0;
-            has_rights |= dist_y > 0;
+            // if point lies on the left hand side of the vector,
+            // set has_lefts to true
+            has_lefts |= dist_y > 0;
+            // same for right hand side and has_rights
+            has_rights |= dist_y < 0;
 
             if ((0 <= dist_x) && (dist_x <= length)) {
+                // if point projection falls inside the vector itself,
+                // the actual shortest distance to the vector is dist_y
                 dist = fabs(dist_y);
             } else {
+                // otherwise we need to consider both dist_x and dist_y,
+                // combining them in Pythagorean formula
                 if (dist_x > length)
+                    // point lies above the "end point" of the vector
+                    // along the line, so closest x-distance to the vector
+                    // is the distance to the end point
                     dist_x -= length;
                 dist = sqrt(dist_x * dist_x + dist_y * dist_y);
             }
 
             if (dist < min_distance)
+                // update the "minimum distance so far" variable if needed
                 min_distance = dist;
         }
 
         if ((has_lefts == 0) || (has_rights == 0))
+            // point lies inside the polygon if it is either on the right
+            // hand side or on the left hand side of all its edges. note
+            // that it is only true for convex polygons.
             min_distance = 0;
 
         *(double *) dataptrarray_p[2] = min_distance;
@@ -184,6 +238,9 @@ geoutils_convex_to_point_distance(
 }
 
 
+/*
+ * Module method reference table
+ */
 static PyMethodDef GeoutilsSpeedupsMethods[] = {
     {"convex_to_point_distance",
             (PyCFunction)geoutils_convex_to_point_distance,
@@ -194,6 +251,9 @@ static PyMethodDef GeoutilsSpeedupsMethods[] = {
 };
 
 
+/*
+ * Module initialization function
+ */
 PyMODINIT_FUNC
 init_utils_speedups(void)
 {
