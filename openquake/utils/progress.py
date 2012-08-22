7fef952377dc048b53c8f644e71f394c451dbaa9
Muharem Hrnjadovic <arbeit@star.io>
2012-08-22 10:54:15 +0200

    add progress reporting module

---
+++ openquake/utils/progress.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright (c) 2010-2012, GEM Foundation.
+#
+# OpenQuake is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# OpenQuake is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
+
+
+"""
+Utility functions related to reporting the progress of a calculation.
+"""
+
+import logging
+
+
+# Place the new level between info and warning
+logging.STATUS = 25
+logging.addLevelName(logging.STATUS, "STATUS")
+
+
+LOG = logging.getLogger(__name__)
+
+
+def report_status(msg, *args, **kwargs):
+    LOG._log(logging.STATUS, msg, *args, **kwargs)
