83cb23057ae0a924d9cc0989633970b104bdd79a
Muharem Hrnjadovic <arbeit@star.io>
2012-08-23 10:28:08 +0200

    more test fixes
    Former-commit-id: 1498f653ae57c90a6b36ddd3cb87a897ee994bf7 [formerly 5520734526235d59821a19b2956b1d42051a0ed6]
    Former-commit-id: a27e7371998d4a25172fdc691fb11fb57610a349

--- tests/logs_test.py
+++ tests/logs_test.py
@@ -425,5 +425,5 @@ class LogPercentCompleteTestCase(unittest.TestCase):
             rv = logs.log_percent_complete(job_id, "hazard")
             self.assertEqual(20, rv)
             self.assertEqual(1, lpm.call_count)
-            self.assertEqual("hazard calculation  20% complete",
+            self.assertEqual("hazard  20% complete",
                              lpm.call_args_list[0][0][0])
