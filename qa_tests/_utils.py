448df9ee2b295167ad1ba40c1fc2f87577b05066
Lars Butler <Lars.Butler@gmail.com>
2012-09-11 15:29:25 +0200

    qa_tests:
    Added base test case class functionality for executing QA tests.
    
    
    Former-commit-id: 0b87da2fabf9092b3e9753bd6d760ec47fbd7b5f [formerly 39d6b2208790e1a10001c7d35749e309e75e4db6]
    Former-commit-id: aae9116539744f131e7b63429ad1c6680ed8e1c9

--- qa_tests/_utils.py
+++ qa_tests/_utils.py
@@ -13,20 +13,38 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
 
+import unittest
 
 from openquake import engine2
-
 from tests.utils import helpers
 
 
-def run_hazard(cfg, exports=None):
+class BaseQATestCase(unittest.TestCase):
     """
-    :param str cfg:
-        Path to a job config file.
+    Base QA test case class with some general functionality built in for
+    running QA tests.
     """
-    if exports is None:
-        exports = []
 
-    job = helpers.get_hazard_job(cfg)
-    completed_job = engine2._do_run_hazard(job, exports)
-    return completed_job
+    def run_hazard(self, cfg, exports=None):
+        """
+        Given the path to job config file, run the job and assert that it was
+        successful. If this assertion passes, return the completed job.
+
+        :param str cfg:
+            Path to a job config file.
+        :param list exports:
+            A list of export format types. Currently only 'xml' is supported.
+        :returns:
+            The completed :class:`~openquake.db.models.OqJob`.
+        :raises:
+            :exc:`AssertionError` if the job was not successfully run.
+        """
+        if exports is None:
+            exports = []
+
+        job = helpers.get_hazard_job(cfg)
+        completed_job = engine2._do_run_hazard(job, exports)
+
+        self.assertFalse(completed_job.is_running)
+        self.assertEqual('complete', completed_job.status)
+        return completed_job
