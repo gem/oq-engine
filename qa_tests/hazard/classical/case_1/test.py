ab94dbc3552022cd8c8985a8ab7708ad81a3ff61
Lars Butler <Lars.Butler@gmail.com>
2012-09-11 15:53:51 +0200

    qa_tests/hazard/classical/case_1/test:
    Added a test for the hazard curve export as well.

--- qa_tests/hazard/classical/case_1/test.py
+++ qa_tests/hazard/classical/case_1/test.py
@@ -13,26 +13,56 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
 
+import StringIO
 import numpy
 import os
+import shutil
+import tempfile
 
 from nose.plugins.attrib import attr
 from openquake.db import models
+from openquake.export import hazard as hazard_export
 from qa_tests import _utils as qa_utils
 
 
 class ClassicalHazardCase1TestCase(qa_utils.BaseQATestCase):
 
+    EXPECTED_XML = """<?xml version='1.0' encoding='UTF-8'?>
+<nrml xmlns:gml="http://www.opengis.net/gml" xmlns="http://openquake.org/xmlns/nrml/0.4">
+  <hazardCurves IMT="PGA" investigationTime="1.0" sourceModelTreePath="b1" gsimTreePath="b1">
+    <IMLs>0.1 0.4 0.6</IMLs>
+    <hazardCurve>
+      <gml:Point>
+        <gml:pos>0.0 0.0</gml:pos>
+      </gml:Point>
+      <poEs>0.45701348633 0.0586267877384 0.00686616439666</poEs>
+    </hazardCurve>
+  </hazardCurves>
+</nrml>
+"""
+
     @attr('qa')
     def test(self):
-        cfg = os.path.join(os.path.dirname(__file__), 'job.ini')
-        expected_curve_poes = [0.4570, 0.0587, 0.0069]
+        result_dir = tempfile.mkdtemp()
 
-        job = self.run_hazard(cfg)
+        try:
+            cfg = os.path.join(os.path.dirname(__file__), 'job.ini')
+            expected_curve_poes = [0.4570, 0.0587, 0.0069]
+            expected_text = StringIO.StringIO(self.EXPECTED_XML).readlines()
 
-        # One curve only:
-        [actual_curve] = models.HazardCurveData.objects.filter(
-            hazard_curve__output__oq_job=job.id)
+            job = self.run_hazard(cfg)
 
-        numpy.testing.assert_array_almost_equal(
-            expected_curve_poes, actual_curve.poes, decimal=4)
+            # Test the poe values of the single curve:
+            [actual_curve] = models.HazardCurveData.objects.filter(
+                hazard_curve__output__oq_job=job.id)
+
+            numpy.testing.assert_array_almost_equal(
+                expected_curve_poes, actual_curve.poes, decimal=4)
+
+            # Test the export as well:
+            [exported_file] = hazard_export.export(
+                actual_curve.hazard_curve.output.id, result_dir)
+            actual_text = open(exported_file, 'r').readlines()
+            self.assertEqual(expected_text, actual_text)
+        finally:
+            shutil.rmtree(result_dir)
