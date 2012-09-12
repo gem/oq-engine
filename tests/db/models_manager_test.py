d326267ada2e93644d86bd4768e1807ad7b75d5c
Luigi Panzeri <defmacro@gmail.com>
2012-09-12 13:32:25 +0200

    models manager test fixes
    Former-commit-id: 3a1326f39535921a55e6930c0287219cbdecbfcb

--- tests/db/models_manager_test.py
+++ tests/db/models_manager_test.py
@@ -25,7 +25,7 @@ Test Django custom model managers
 
 import random
 import unittest
-from openquake.db import models as openquake
+from openquake.db import models
 from tests.utils import helpers
 
 
@@ -36,6 +36,11 @@ class TestCaseWithAJob(unittest.TestCase):
     def setUp(self):
         cfg = helpers.demo_file('simple_fault_demo_hazard/job.ini')
         self.job = helpers.get_hazard_job(cfg, username="test_user")
+        for i in range(0, random.randint(0, 10)):
+            models.LtRealization(
+                hazard_calculation=self.job.hazard_calculation,
+                ordinal=i, seed=None, weight=1 / (i + 1), sm_lt_path=[i],
+                gsim_lt_path=[i], total_sources=0, completed_sources=0).save()
 
 
 class OutputManagerTestCase(TestCaseWithAJob):
@@ -45,7 +50,7 @@ class OutputManagerTestCase(TestCaseWithAJob):
 
     def setUp(self):
         super(OutputManagerTestCase, self).setUp()
-        self.manager = openquake.Output.objects
+        self.manager = models.Output.objects
 
     def test_create_output(self):
         output = self.manager.create_output(self.job,
@@ -64,8 +69,8 @@ class HazardCurveManagerTestCase(TestCaseWithAJob):
     """
     def setUp(self):
         super(HazardCurveManagerTestCase, self).setUp()
-        self.manager = openquake.HazardCurve.objects
-        self.output = openquake.Output.objects.create_output(self.job,
+        self.manager = models.HazardCurve.objects
+        self.output = models.Output.objects.create_output(self.job,
                                                              "fake output")
 
     def test_create_aggregate_curve(self):
@@ -92,22 +97,22 @@ class HazardCurveDataManagerTestCase(TestCaseWithAJob):
     """
     def setUp(self):
         super(HazardCurveDataManagerTestCase, self).setUp()
-        self.manager = openquake.HazardCurveData.objects
+        self.manager = models.HazardCurveData.objects
         self.manager.current_job = self.job
 
         # Setup some data
         # Requires a working version of Django models
-        output = openquake.Output.objects.create_output(self.job,
+        output = models.Output.objects.create_output(self.job,
                                                         "fake output")
-        realization = openquake.LtRealization.objects.all()[0]
-        curve = openquake.HazardCurve.objects.create(
+        realization = models.LtRealization.objects.all()[0]
+        curve = models.HazardCurve.objects.create(
             output=output,
             lt_realization=realization,
             investigation_time=10,
             imt="PGA", imls=[1, 2, 3])
 
         self.a_location = helpers.random_location_generator(max_x=50, max_y=50)
-        openquake.HazardCurveData.objects.create(
+        models.HazardCurveData.objects.create(
             hazard_curve=curve,
             location=self.a_location.wkt,
             poes=[random.random()])
@@ -115,7 +120,7 @@ class HazardCurveDataManagerTestCase(TestCaseWithAJob):
         self.a_bigger_location = helpers.random_location_generator(
             min_x=50,
             min_y=50)
-        openquake.HazardCurveData.objects.create(
+        models.HazardCurveData.objects.create(
             hazard_curve=curve,
             location=self.a_bigger_location.wkt,
             poes=[random.random()])
@@ -126,13 +131,15 @@ class HazardCurveDataManagerTestCase(TestCaseWithAJob):
         """
         # use a fake imt
         self.assertEqual(0,
-                         len(self.manager.individual_curves("fake imt")))
+                         len(self.manager.individual_curves(
+                             self.job, "fake imt")))
 
         self.assertEqual(2,
-                         len(self.manager.individual_curves("PGA")))
+                         len(self.manager.individual_curves(
+                             self.job, "PGA")))
 
         self.assertEqual(2,
-                         len(self.manager.individual_curves()))
+                         len(self.manager.individual_curves(self.job)))
 
     def test_individual_curves_nr(self):
         """
@@ -140,16 +147,17 @@ class HazardCurveDataManagerTestCase(TestCaseWithAJob):
         """
         # use a fake imt
         self.assertEqual(0,
-                         self.manager.individual_curves_nr("fake imt"))
+                         self.manager.individual_curves_nr(
+                             self.job, "fake imt"))
 
         self.assertEqual(2,
-                         self.manager.individual_curves_nr())
+                         self.manager.individual_curves_nr(self.job))
 
     def test_individual_curves_ordered(self):
         """
         Test getting individual curves ordered by location
         """
-        curves = self.manager.individual_curves_ordered()
+        curves = self.manager.individual_curves_ordered(self.job)
 
         self.assertEqual(2, len(curves))
         self.assertTrue(curves[0].location < curves[1].location)
@@ -159,7 +167,8 @@ class HazardCurveDataManagerTestCase(TestCaseWithAJob):
         Test getting individual curves in chunks
         """
         block_size = 1
-        chunks = self.manager.individual_curves_chunks(block_size=block_size)
+        chunks = self.manager.individual_curves_chunks(self.job,
+                                                       block_size=block_size)
 
         chunk_getter = chunks.next()
         chunk = chunk_getter('wkb')
