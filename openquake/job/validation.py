920f5161f79af6613c27da3a01db0dd85886d0f8
Lars Butler <Lars.Butler@gmail.com>
2012-09-17 15:54:09 +0200

    job/validation:
    Added contextual validation for `hazard_curves_from_gmfs` parameter,
    plus tests to cover cases when related (required) parameters are missing
    or invalid.
    
    
    Former-commit-id: f1f6c95ae1bd354cb364530dd2987321429743c2 [formerly f5e5ccd6959c43709109025389cc655c8b450bca]
    Former-commit-id: 024d78c2e0abbf664904ffc3796d0324e1420d41

--- openquake/job/validation.py
+++ openquake/job/validation.py
@@ -128,16 +128,16 @@ class BaseOQModelForm(ModelForm):
         all_valid = super_valid
 
         # HazardCalculation
-        hjp = self.instance
+        hc = self.instance
 
         # First, check the calculation mode:
-        valid, errs = calculation_mode_is_valid(hjp, self.calc_mode)
+        valid, errs = calculation_mode_is_valid(hc, self.calc_mode)
         all_valid &= valid
         self._add_error('calculation_mode', errs)
 
         # Exclude special fields that require contextual validation.
         for field in sorted(set(self.fields) - set(self.special_fields)):
-            valid, errs = eval('%s_is_valid' % field)(hjp)
+            valid, errs = eval('%s_is_valid' % field)(hc)
             all_valid &= valid
 
             self._add_error(field, errs)
@@ -145,21 +145,21 @@ class BaseOQModelForm(ModelForm):
         # Now do checks which require more context.
 
         # Cannot specify region AND sites
-        if (hjp.region is not None
-            and hjp.sites is not None):
+        if (hc.region is not None
+            and hc.sites is not None):
             all_valid = False
             err = 'Cannot specify `region` and `sites`. Choose one.'
             self._add_error('region', err)
         # At least one must be specified (region OR sites)
-        elif not (hjp.region is not None or hjp.sites is not None):
+        elif not (hc.region is not None or hc.sites is not None):
             all_valid = False
             err = 'Must specify either `region` or `sites`.'
             self._add_error('region', err)
             self._add_error('sites', err)
         # Only region is specified
-        elif hjp.region is not None:
-            if hjp.region_grid_spacing is not None:
-                valid, errs = region_grid_spacing_is_valid(hjp)
+        elif hc.region is not None:
+            if hc.region_grid_spacing is not None:
+                valid, errs = region_grid_spacing_is_valid(hc)
                 all_valid &= valid
 
                 self._add_error('region_grid_spacing', errs)
@@ -169,12 +169,12 @@ class BaseOQModelForm(ModelForm):
                 self._add_error('region', err)
 
             # validate the region
-            valid, errs = region_is_valid(hjp)
+            valid, errs = region_is_valid(hc)
             all_valid &= valid
             self._add_error('region', errs)
         # Only sites was specified
         else:
-            valid, errs = sites_is_valid(hjp)
+            valid, errs = sites_is_valid(hc)
             all_valid &= valid
             self._add_error('sites', errs)
 
@@ -187,14 +187,14 @@ class BaseOQModelForm(ModelForm):
                 'reference_depth_to_2pt5km_per_sec',
                 'reference_depth_to_1pt0km_per_sec',
             ):
-                valid, errs = eval('%s_is_valid' % field)(hjp)
+                valid, errs = eval('%s_is_valid' % field)(hc)
                 all_valid &= valid
                 self._add_error(field, errs)
 
         if self.exports:
             # The user has requested that exports be performed after the
             # calculation i.e. an 'export_dir' parameter must be present.
-            if not hjp.export_dir:
+            if not hc.export_dir:
                 all_valid = False
                 err = ('--export specified on the command line but the '
                        '"export_dir" parameter is missing in the .ini file')
@@ -258,6 +258,7 @@ class EventBasedHazardCalculationForm(BaseOQModelForm):
             'truncation_level',
             'maximum_distance',
             'intensity_measure_types',
+            'intensity_measure_types_and_levels',
             'ses_per_logic_tree_path',
             'ground_motion_correlation_model',
             'ground_motion_correlation_params',
@@ -265,7 +266,46 @@ class EventBasedHazardCalculationForm(BaseOQModelForm):
             'complete_logic_tree_gmf',
             'ground_motion_fields',
             'export_dir',
+            'hazard_curves_from_gmfs',
         )
+
+    def is_valid(self):
+        super_valid = super(EventBasedHazardCalculationForm, self).is_valid()
+        all_valid = super_valid
+
+        hc = self.instance
+
+        # contextual validation
+        # for the case where the user has requested to post-process GMFs into
+        # hazard curves
+        if hc.hazard_curves_from_gmfs is True:
+            # 1) We need to make sure `intensity_measure_types_and_levels` is
+            #    defined (and valid)
+            if hc.intensity_measure_types_and_levels is None:
+                # Not defined
+                msg = '`%s` requires `%s`'
+                msg %= ('hazard_curve_from_gmfs',
+                        'intensity_measure_types_and_levels')
+
+                self._add_error('intensity_measure_types_and_levels', msg)
+                all_valid = False
+            else:
+                # Defined, but is it valid?
+                valid, errs = intensity_measure_types_and_levels_is_valid(hc)
+                all_valid &= valid
+                self._add_error('hazard_curves_from_gmfs', errs)
+
+                # 2) The IMT keys in `intensity_measure_types_and_levels` need
+                #    to be a subset of `intensity_measure_types`.
+                if (not set(hc.intensity_measure_types_and_levels.keys())\
+                    .issubset(set(hc.intensity_measure_types))):
+                    msg = 'The IMTs in `%s` must be a subset of `%s`'
+                    msg %= ('intensity_measure_types_and_levels',
+                            'intensity_measure_types')
+                    self._add_error('intensity_measure_types_and_levels', msg)
+                    all_valid = False
+
+        return all_valid
 
 #: Maps calculation_mode to the appropriate validator class
 VALIDATOR_MAP = {
@@ -449,6 +489,10 @@ def intensity_measure_types_and_levels_is_valid(mdl):
     valid = True
     errors = []
 
+    if mdl.calculation_mode == 'event_based' and im is None:
+        # For event-based hazard calculations, this parameter is optional
+        return valid, errors
+
     for im_type, imls in im.iteritems():
         # validate IMT:
         valid_imt, imt_errors = _validate_imt(im_type)
@@ -563,6 +607,12 @@ def complete_logic_tree_gmf_is_valid(_mdl):
 
 
 def ground_motion_fields_is_valid(_mdl):
+    # This parameter is a simple True or False;
+    # field normalization should cover all of validation necessary.
+    return True, []
+
+
+def hazard_curves_from_gmfs_is_valid(_mdl):
     # This parameter is a simple True or False;
     # field normalization should cover all of validation necessary.
     return True, []
