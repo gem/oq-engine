3124f5a1cf422a1683ce9e06f9b9fa92d4e9ef55
Andrea Cerisara <andreacerisara@gmail.com>
2012-09-19 10:35:26 +0200

    Moved code to RiskLib

--- openquake/calculators/risk/scenario_damage/core.py
+++ openquake/calculators/risk/scenario_damage/core.py
@@ -21,14 +21,12 @@ damage assessment approach.
 """
 
 import os
-import math
 import numpy
-import scipy
 
 from openquake import logs
 from openquake.calculators.risk import general
 from openquake.db.models import Output, FragilityModel
-from openquake.db.models import DmgDistPerAsset, Ffc, Ffd
+from openquake.db.models import DmgDistPerAsset, Ffc
 from openquake.db.models import DmgDistPerAssetData, DmgDistPerTaxonomy
 from openquake.db.models import (DmgDistPerTaxonomyData,
 DmgDistTotal, DmgDistTotalData, ExposureModel, CollapseMap, CollapseMapData)
@@ -37,7 +35,7 @@ from openquake.utils.tasks import distribute
 from openquake.export.risk import export_dmg_dist_per_asset
 from openquake.export.risk import export_dmg_dist_per_taxonomy
 from openquake.export.risk import export_dmg_dist_total, export_collapse_map
-from openquake.calculators.risk.risk_lib.scenario_damage import models as rl
+from risklib.scenario_damage import models, functions
 
 LOGGER = logs.LOG
 
@@ -235,7 +233,7 @@ class ScenarioDamageRiskCalculator(general.BaseRiskCalculator):
                         "with taxonomy %s of asset %s.") % (
                         asset.taxonomy, asset.asset_ref)
 
-                fractions = compute_gmf_fractions(
+                fractions = functions.compute_gmf_fractions(
                     gmf, funcs) * asset.number_of_units
 
                 current_fractions = ddt_fractions.get(
@@ -394,98 +392,6 @@ class ScenarioDamageRiskCalculator(general.BaseRiskCalculator):
             export_collapse_map(output, target_dir)
 
 
-def compute_gmf_fractions(gmf, funcs):
-    """
-    Compute the fractions of each damage state for
-    each ground motion value given.
-
-    :param gmf: ground motion values computed in the grid
-        point where the asset is located.
-    :type gmf: list of floats
-    :param funcs: list of fragility functions describing
-        the distribution for each limit state. The functions
-        must be in order from the one with the lowest
-        limit state to the one with the highest limit state.
-    :type funcs: list of
-        :py:class:`openquake.db.models.Ffc` instances
-    :returns: the fractions for each damage state.
-    :rtype: 2d `numpy.array`. Each column represents
-        a damage state (in order from the lowest
-        to the highest). Each row represents the
-        values for that damage state for a particular
-        ground motion value.
-    """
-
-    # we always have a number of damage states
-    # which is len(limit states) + 1
-    fractions = numpy.zeros((len(gmf), len(funcs) + 1))
-
-    for x, gmv in enumerate(gmf):
-        fractions[x] += compute_gmv_fractions(funcs, gmv)
-
-    return fractions
-
-
-def compute_gmv_fractions(funcs, gmv):
-    """
-    Compute the fractions of each damage state for
-    the ground motion value given.
-
-    :param gmv: ground motion value that defines the Intensity
-        Measure Level used to interpolate the fragility functions.
-    :type gmv: float
-    :param funcs: list of fragility functions describing
-        the distribution for each limit state. The functions
-        must be in order from the one with the lowest
-        limit state to the one with the highest limit state.
-    :type funcs: list of
-        :py:class:`openquake.db.models.Ffc` instances
-    :returns: the fraction of buildings of each damage state
-        computed for the given ground motion value.
-    :rtype: 1d `numpy.array`. Each value represents
-        the fraction of a damage state (in order from the lowest
-        to the highest)
-    """
-
-
-
-    # we always have a number of damage states
-    # which is len(limit states) + 1
-    damage_states = numpy.zeros(len(funcs) + 1)
-
-    fm = funcs[0].fragility_model
-
-    # when we have a discrete fragility model and
-    # the ground motion value is below the lowest
-    # intensity measure level defined in the model
-    # we simply use 100% no_damage and 0% for the
-    # remaining limit states
-    if fm.no_damage(gmv):
-        damage_states[0] = 1.0
-        return numpy.array(damage_states)
-
-    first_poe = funcs[0].poe(gmv)
-
-    # first damage state is always 1 - the probability
-    # of exceedance of first limit state
-    damage_states[0] = 1 - first_poe
-
-    last_poe = first_poe
-
-    # starting from one, the first damage state
-    # is already computed...
-    for x in xrange(1, len(funcs)):
-        poe = funcs[x].poe(gmv)
-        damage_states[x] = last_poe - poe
-        last_poe = poe
-
-    # last damage state is equal to the probabily
-    # of exceedance of the last limit state
-    damage_states[len(funcs)] = funcs[len(funcs) - 1].poe(gmv)
-
-    return numpy.array(damage_states)
-
-
 def _damage_states(limit_states):
     """
     Return the damage states from the given limit states.
@@ -514,14 +420,16 @@ def _fm(oq_job):
 
 def _to_rl_frag_function(func):
     fm = func.fragility_model
-    fm_rl = rl.FragilityModel(fm.format,
-        fm.no_damage_limit, fm.imls)
+    fm_rl = models.FragilityModel(fm.format, fm.imls,
+        fm.no_damage_limit)
+
     if isinstance(func, Ffc):
-        func_rl = rl.FragilityFunctionContinuous(
+        func_rl = models.FragilityFunctionContinuous(
                 fm_rl, func.mean, func.stddev)
     else:
-        func_rl = rl.FragilityFunctionDiscrete(
+        func_rl = models.FragilityFunctionDiscrete(
             fm_rl, func.poes)
+
     return func_rl
 
 
