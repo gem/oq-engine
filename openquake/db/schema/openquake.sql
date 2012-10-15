b6e23acc16515478e9c1fd01c486726d1b8e4257
Muharem Hrnjadovic <arbeit@star.io>
2012-10-15 13:11:13 +0200

    merged branch with cnode stats table

--- openquake/db/schema/openquake.sql
+++ openquake/db/schema/openquake.sql
@@ -353,6 +353,18 @@ CREATE TABLE uiapi.input2hcalc (
 ) TABLESPACE uiapi_ts;
 
 
+CREATE TABLE uiapi.cnode_stats (
+    id SERIAL PRIMARY KEY,
+    oq_job_id INTEGER NOT NULL,
+    node VARCHAR NOT NULL,
+    current_status VARCHAR NOT NULL CONSTRAINT current_status_value
+        CHECK(current_status IN ('up', 'down')),
+    current_ts timestamp without time zone NOT NULL,
+    previous_ts timestamp without time zone,
+    failures INTEGER NOT NULL DEFAULT 0
+) TABLESPACE uiapi_ts;
+
+
 -- The parameters needed for an OpenQuake engine run
 CREATE TABLE uiapi.oq_job_profile (
     id SERIAL PRIMARY KEY,
@@ -1681,6 +1693,12 @@ ALTER TABLE uiapi.oq_job_profile ADD CONSTRAINT uiapi_oq_job_profile_owner_fk
 FOREIGN KEY (owner_id) REFERENCES admin.oq_user(id) ON DELETE RESTRICT;
 
 ALTER TABLE uiapi.job_stats ADD CONSTRAINT  uiapi_job_stats_oq_job_fk
+FOREIGN KEY (oq_job_id) REFERENCES uiapi.oq_job(id) ON DELETE CASCADE;
+
+ALTER TABLE uiapi.job_phase_stats ADD CONSTRAINT  uiapi_job_phase_stats_oq_job_fk
+FOREIGN KEY (oq_job_id) REFERENCES uiapi.oq_job(id) ON DELETE CASCADE;
+
+ALTER TABLE uiapi.cnode_stats ADD CONSTRAINT  uiapi_cnode_stats_oq_job_fk
 FOREIGN KEY (oq_job_id) REFERENCES uiapi.oq_job(id) ON DELETE CASCADE;
 
 ALTER TABLE uiapi.input2job ADD CONSTRAINT  uiapi_input2job_input_fk
