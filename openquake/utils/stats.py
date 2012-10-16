7fcad10f4e3b0b3a043d7ecbb5c9c955c6323b46
Muharem Hrnjadovic <arbeit@star.io>
2012-10-16 09:56:36 +0200

    minor fix

--- openquake/utils/stats.py
+++ openquake/utils/stats.py
@@ -388,4 +388,4 @@ def get_progress_timing_data(job):
     # take the more recent of the two time stamps
     lvr_ts = jps_ts if jps_ts > lvr_ts else lvr_ts
 
-    return (tstamp - lvr_ts, models.profile4job(job.id).no_progress_timeout)
+    return (tstamp - lvr_ts, job.hazard_calculation.no_progress_timeout)
