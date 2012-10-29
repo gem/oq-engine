2221cc2e4a592f44e7cb2b2dc3b1217baefb958d
Lars Butler <Lars.Butler@gmail.com>
2012-10-29 13:24:36 +0100

    Stabilized some GmfSet iter tests
    The tests would randomly fail, about 1 time in 10. Adding this sorting
    should stabilize the ordering of the results and prevent these types
    of failures.
    
    
    Former-commit-id: 77084c2c07cf33f10c4632c0fed8294deedc9a04 [formerly 29efce1e0a5e0febd9714704f3e549f5a023afda]
    Former-commit-id: 4a80b1658b5429ef1cac9c8b982aa9e9a425994e

--- tests/db/models_test.py
+++ tests/db/models_test.py
@@ -534,7 +534,7 @@ class GmfSetIterTestCase(unittest.TestCase):
         [act_gmf_set] = models.GmfSet.objects\
             .filter(gmf_collection__output__oq_job=job.id,
                     gmf_collection__lt_realization__isnull=True)\
-            .order_by('gmf_collection', 'ses_ordinal')
+            .order_by('id')
 
         self.assertEqual(exp_gmf_set.complete_logic_tree_gmf,
                          act_gmf_set.complete_logic_tree_gmf)
