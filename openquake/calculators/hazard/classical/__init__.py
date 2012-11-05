87ba2d791c9854e99b432e8c5b34617258f387a3
Lars Butler <Lars.Butler@gmail.com>
2012-11-05 17:13:08 +0100

    calcs/hazard/classical/__init__:
    Added `Outputs` and `Hazard Curves` documentation to the
    `classical` package.
    
    
    Former-commit-id: 875a0f4ef968fa8e4965632521e02be5aab18fe1 [formerly f9fa862915eedeffe75e5de69182ff4dddd954b8]
    Former-commit-id: 7158464bedf17ca6d4673d081d3d7374f229c417

--- openquake/calculators/hazard/classical/__init__.py
+++ openquake/calculators/hazard/classical/__init__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright (c) 2010-2012, GEM Foundation.
 #
 # OpenQuake is free software: you can redistribute it and/or modify it
@@ -13,4 +14,70 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Classical PSHA hazard calculator."""
+"""
+The Classical Probabilistic Seismic Hazard Analysis (cPSHA) approach
+allows calculation of hazard curves and hazard maps following the
+classical integration procedure (**Cornell [1968]**, **McGuire [1976]**)
+as formulated by **Field et al. [2003]**.
+
+Sources:
+
+* | Cornell, C. A. (1968).
+  | Engineering seismic risk analysis.
+  | Bulletin of the Seismological Society of America, 58:1583–1606.
+* | Field, E. H., Jordan, T. H., and Cornell, C. A. (2003).
+  | OpenSHA - A developing Community-Modeling
+  | Environment for Seismic Hazard Analysis. Seism. Res. Lett., 74:406–419.
+* | McGuire, K. K. (1976).
+  | Fortran computer program for seismic risk analysis. Open-File report 76-67,
+  | United States Department of the Interior, Geological Survey. 102 pages.
+
+
+*******
+Outputs
+*******
+
+* Hazard Curves
+* Hazard Maps
+
+Hazard Curves
+=============
+
+Hazard Curves are discrete functions which describe probability of ground
+motion exceedance in a given time frame. Hazard curves are composed of several
+key elements:
+
+* **Intensity Measure Levels (IMLs)** - IMLs define the x-axis values (or
+  "ordinates") of the curve. IMLs are defined with an Intensity Measure Type
+  (IMT) unit. IMLs are a `strictly monotonically increasing sequence`.
+* **Probabilitites of Exceedance (PoEs)** - PoEs define the y-axis values, (or
+  "abscissae") of the curve. For each node in the curve, the PoE denotes the
+  probability that ground motion will exceedence a given level in a given time
+  span.
+* **Intensity Measure Type (IMT)** - The unit of measurement for the defined
+  IMLs.
+* **Investigation time** - The period of time (in years) for an earthquake
+  hazard study. It is important to consider the investigation time when
+  analyzing hazard curve results, because one can logically conclude that, the
+  longer the time span, there is greater probability of ground motion exceeding
+  the given values.
+* **Spectral Acceleration (SA) Period** - Optional; used only if the IMT is SA.
+* **Spectral Acceleration (SA) Damping** - Optional; used only if the IMT is
+  SA.
+
+For a given calculation, hazard curves are computed for each IMT/IML definition
+and each geographical point of interest. (In other words: If a calculation
+specifies a geometry with 10 points of interest and 3 IMT/IML definitions, 30
+curves will be computed.
+
+Another way to put it is:
+
+``T = P * I``
+
+where
+
+* ``T`` is the total number of curves
+* ``P`` is the number of geographical points of interest
+* ``I`` is the number of IMT/IML definitions
+
+"""
