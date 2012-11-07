f16588834f4e91a07a29657ff6fd2c275afe6ec5
Andrea Cerisara <andreacerisara@gmail.com>
2012-11-07 19:19:25 +0100

    First implementation

--- nrml/risk/writers.py
+++ nrml/risk/writers.py
@@ -25,17 +25,68 @@ import nrml
 
 class LossCurveXMLWriter(object):
 
-    def __init__(self, path, model_id="1"):
+    def __init__(self, path, investigation_time, source_model_tree_path=None,
+                 gsim_tree_path=None, unit=None):
+
+        self._unit = unit
         self._path = path
-        self._model_id = model_id
+        self._gsim_tree_path = gsim_tree_path
+        self._investigation_time = investigation_time
+        self._source_model_tree_path = source_model_tree_path
+
+        self._loss_curves = None
 
     def serialize(self, data):
+
         with open(self._path, "w") as output:
             root = etree.Element("nrml", nsmap=nrml.SERIALIZE_NS_MAP)
 
-            for _ in data:
-                pass
+            for d in data:
+                if self._loss_curves is None:
+                    self._create_loss_curves_container(root)
+
+                loss_curve = etree.SubElement(self._loss_curves, "lossCurve")
+
+                _append_location(loss_curve, d.location)
+                loss_curve.set("assetRef", d.asset_ref)
+
+                poes = etree.SubElement(loss_curve, "poEs")
+                poes.text = " ".join([str(p) for p in d.poes])
+
+                losses = etree.SubElement(loss_curve, "losses")
+                losses.text = " ".join([str(p) for p in d.losses])
+
+                if len(d.loss_ratios):
+                    loss_ratios = etree.SubElement(loss_curve, "lossRatios")
+
+                    loss_ratios.text = " ".join(
+                        [str(p) for p in d.loss_ratios])
 
             output.write(etree.tostring(
                 root, pretty_print=True, xml_declaration=True,
                 encoding="UTF-8"))
+
+    def _create_loss_curves_container(self, root):
+        self._loss_curves = etree.SubElement(root, "lossCurves")
+
+        self._loss_curves.set("investigationTime",
+            str(self._investigation_time))
+
+        if self._source_model_tree_path is not None:
+            self._loss_curves.set("sourceModelTreePath",
+                str(self._source_model_tree_path))
+
+        if self._gsim_tree_path is not None:
+            self._loss_curves.set("gsimTreePath",
+                str(self._gsim_tree_path))
+
+        if self._unit is not None:
+            self._loss_curves.set("unit", str(self._unit))
+
+
+def _append_location(element, location):
+    gml_ns = nrml.SERIALIZE_NS_MAP["gml"]
+
+    gml_point = etree.SubElement(element, "{%s}Point" % gml_ns)
+    gml_pos = etree.SubElement(gml_point, "{%s}pos" % gml_ns)
+    gml_pos.text = "%s %s" % (location.x, location.y)
--- tests/risk/writers_test.py
+++ tests/risk/writers_test.py
@@ -14,29 +14,111 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
 
-import shutil
+import os
 import unittest
-import tempfile
 import StringIO
+import collections
 
 from nrml.risk import writers
 
 
+POINT = collections.namedtuple("Point", "x y")
+LOSS_CURVE = collections.namedtuple(
+    "LossCurve", "poes losses location asset_ref loss_ratios")
+
+
 class LossCurveXMLWriterTestCase(unittest.TestCase):
 
+    filename = "loss_curves.xml"
+
+    def remove_file(self):
+        try:
+            os.remove(self.filename)
+        except: pass
+
     def setUp(self):
-        _, self.path = tempfile.mkstemp()
+        self.remove_file()
 
     def tearDown(self):
-        shutil.rmtree(self.path, ignore_errors=True)
+        self.remove_file()
 
     def test_serialize_an_empty_model(self):
         expected = StringIO.StringIO("""<?xml version='1.0' encoding='UTF-8'?>
 <nrml xmlns:gml="http://www.opengis.net/gml" xmlns="http://openquake.org/xmlns/nrml/0.4"/>
 """)
 
-        writer = writers.LossCurveXMLWriter(self.path)
+        writer = writers.LossCurveXMLWriter(self.filename,
+            investigation_time=10.0)
+
         writer.serialize([])
 
         self.assertEqual(expected.readlines(),
-            open(self.path, 'r').readlines())
+            open(self.filename, "r").readlines())
+
+    def test_serialize_a_model(self):
+        expected = StringIO.StringIO("""\
+<?xml version='1.0' encoding='UTF-8'?>
+<nrml xmlns:gml="http://www.opengis.net/gml" xmlns="http://openquake.org/xmlns/nrml/0.4">
+  <lossCurves investigationTime="10.0">
+    <lossCurve assetRef="asset_1">
+      <gml:Point>
+        <gml:pos>1.0 1.5</gml:pos>
+      </gml:Point>
+      <poEs>1.0 0.5 0.1</poEs>
+      <losses>10.0 20.0 30.0</losses>
+    </lossCurve>
+    <lossCurve assetRef="asset_2">
+      <gml:Point>
+        <gml:pos>2.0 2.5</gml:pos>
+      </gml:Point>
+      <poEs>1.0 0.3 0.2</poEs>
+      <losses>20.0 30.0 40.0</losses>
+    </lossCurve>
+  </lossCurves>
+</nrml>
+""")
+
+        writer = writers.LossCurveXMLWriter(self.filename,
+            investigation_time=10.0)
+
+        data = [
+            LOSS_CURVE(asset_ref="asset_1", location=POINT(1.0, 1.5),
+            poes=[1.0, 0.5, 0.1], losses=[10.0, 20.0, 30.0], loss_ratios=[]),
+            LOSS_CURVE(asset_ref="asset_2", location=POINT(2.0, 2.5),
+            poes=[1.0, 0.3, 0.2], losses=[20.0, 30.0, 40.0], loss_ratios=[]),
+        ]
+
+        writer.serialize(data)
+
+        self.assertEqual(expected.readlines(),
+            open(self.filename, "r").readlines())
+
+    def test_serialize_optional_attributes(self):
+        expected = StringIO.StringIO("""\
+<?xml version='1.0' encoding='UTF-8'?>
+<nrml xmlns:gml="http://www.opengis.net/gml" xmlns="http://openquake.org/xmlns/nrml/0.4">
+  <lossCurves investigationTime="10.0" sourceModelTreePath="b1_b2_b3" gsimTreePath="b1_b2" unit="USD">
+    <lossCurve assetRef="asset_1">
+      <gml:Point>
+        <gml:pos>1.0 1.5</gml:pos>
+      </gml:Point>
+      <poEs>1.0 0.5 0.1</poEs>
+      <losses>10.0 20.0 30.0</losses>
+      <lossRatios>0.4 0.6 0.8</lossRatios>
+    </lossCurve>
+  </lossCurves>
+</nrml>
+""")
+
+        writer = writers.LossCurveXMLWriter(self.filename,
+            investigation_time=10.0, source_model_tree_path="b1_b2_b3",
+            gsim_tree_path="b1_b2", unit="USD")
+
+        data = [LOSS_CURVE(asset_ref="asset_1", location=POINT(1.0, 1.5),
+            poes=[1.0, 0.5, 0.1], losses=[10.0, 20.0, 30.0],
+            loss_ratios=[0.4, 0.6, 0.8])]
+
+        writer.serialize(data)
+
+        self.assertEqual(expected.readlines(),
+            open(self.filename, "r").readlines())
