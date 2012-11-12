ba8eae78e82610b59e536b977611bc531e2e1952
Luigi Panzeri <defmacro@gmail.com>
2012-11-12 14:56:14 +0100

    schema fix
    Former-commit-id: 1a1c5db31abcd412b4cc39629299fb752acad34f

--- openquake/db/schema/openquake.sql
+++ openquake/db/schema/openquake.sql
@@ -1346,9 +1346,8 @@ CREATE TABLE riskr.loss_curve_data (
     asset_ref VARCHAR NOT NULL,
     losses float[] NOT NULL CONSTRAINT non_negative_losses
         CHECK (0 <= ALL(losses)),
-    loss_ratios float[] NOT NULL
-        CONSTRAINT check_loss_ratios
-        CHECK (0 <= ALL(loss_ratios)) AND (ALL(loss_ratios) <= 1)
+    loss_ratios float[] NOT NULL CONSTRAINT check_loss_ratios
+        CHECK (0.0 <= ALL(loss_ratios) AND 1.0 >= ALL(loss_ratios)),
     -- Probabilities of exceedence
     poes float[] NOT NULL
 ) TABLESPACE riskr_ts;
