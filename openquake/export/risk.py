c95ebdf5c7a49d1653d3458c3c0e078281d63672
Lars Butler <Lars.Butler@gmail.com>
2012-11-15 13:23:14 +0100

    More risk code deletion

--- openquake/export/risk.py
+++ openquake/export/risk.py
@@ -22,33 +22,8 @@ import os
 
 from openquake.db import models
 from openquake.export.core import makedirs
-from openquake.output.risk import AggregateLossCurveXMLWriter
 from openquake.output.scenario_damage import (
 DmgDistPerAssetXMLWriter, DmgDistPerTaxonomyXMLWriter, DmgDistTotalXMLWriter)
-
-
-@makedirs
-def export_agg_loss_curve(output, target_dir):
-    """Export the specified aggregate loss curve ``output`` to the
-    ``target_dir``.
-
-    :param output:
-        :class:`openquake.db.models.Output` associated with the aggregate loss
-        curve results.
-    :param str target_dir:
-        Destination directory location of the exported files.
-    """
-
-    file_name = 'aggregate_loss_curve.xml'
-    file_path = os.path.join(target_dir, file_name)
-
-    agg_loss_curve = models.AggregateLossCurveData.objects.get(
-        loss_curve__output=output.id)
-
-    agg_lc_writer = AggregateLossCurveXMLWriter(file_path)
-    agg_lc_writer.serialize(agg_loss_curve.losses, agg_loss_curve.poes)
-
-    return [file_path]
 
 
 @makedirs
