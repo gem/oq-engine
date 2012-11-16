9a66d037fce9babb381ae4117673810f65008990
Matteo Nastasi <nastasi@openquake.org>
2012-11-16 10:16:12 +0100

    development package option added
    Former-commit-id: 1280665728068e018a4cde4ac048be52308e38c4 [formerly 8a78afc4ec227d93f7fcd3a427dd6abb523e4a41]
    Former-commit-id: 53947ddd32d5b4115453b10776e995c7953aa128

--- packager.sh
+++ packager.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+set -x
 GEM_BUILD_ROOT="build-deb"
 GEM_BUILD_SRC="${GEM_BUILD_ROOT}/python-oq"
 
@@ -25,7 +25,9 @@ usage () {
 
     echo
     echo "USAGE:"
-    echo "    $0 [-B] build debian source package, if -B argument is present binary package is build too"
+    echo "    $0 [-D|--development] [-B|--binaries] build debian source package."
+    echo "       if -B argument is present binary package is build too."
+    echo "       if -D argument is present a package with self-computed version is produced."
     echo
     exit $ret
 }
@@ -34,9 +36,13 @@ usage () {
 #  MAIN
 
 BUILD_BINARIES=0
+BUILD_DEVEL=0
 #  args management
 while [ $# -gt 0 ]; do
     case $1 in
+        -D|--development)
+            BUILD_DEVEL=1
+            ;;
         -B|--binaries)
             BUILD_BINARIES=1
             ;;
@@ -63,15 +69,43 @@ mksafedir "$GEM_BUILD_SRC"
 
 git archive HEAD | (cd "$GEM_BUILD_SRC" ; tar xv)
 
-git submodule init
-git submodule update
+# git submodule init
+# git submodule update
 
 #  "submodule foreach" vars: $name, $path, $sha1 and $toplevel:
-git submodule foreach "git archive HEAD | (cd \"\${toplevel}/${GEM_BUILD_SRC}/\$path\" ; tar xv ) "
+# git submodule foreach "git archive HEAD | (cd \"\${toplevel}/${GEM_BUILD_SRC}/\$path\" ; tar xv ) "
 
 
 cd "$GEM_BUILD_SRC"
 
+if [ $BUILD_DEVEL -eq 1 ]; then
+    dt="$(date +%s)"
+    hash="$(git log --pretty='format:%h' -1)"
+    mv debian/changelog debian/changelog.orig
+    h="$(head -n1 debian/changelog.orig)"
+    pkg_name="$(echo "$h" | cut -d ' ' -f 1)"
+    pkg_vers="$(echo "$h" | cut -d ' ' -f 2 | cut -d '(' -f 2 | cut -d ')' -f 1 )"
+    pkg_rest="$(echo "$h" | cut -d ' ' -f 3-)"
+
+    ( echo "$pkg_name (${pkg_vers}+dev${dt}-${hash}) $pkg_rest"
+      echo
+em openquake/bin/oqpath.py
+      echo "  *  development version from $hash commit"
+      echo
+      echo " -- $DEBFULLNAME <$DEBEMAIL>  $(date -d@$dt -R)"
+      echo
+    )  > debian/changelog
+    cat debian/changelog.orig >> debian/changelog
+    rm debian/changelog.orig
+
+#python-noq (0.2-4) precise; urgency=low
+#
+#  *  fix malformed patch
+#
+# -- Muharem Hrnjadovic <mh@foldr3.com>  Mon, 29 Oct 2012 15:14:15 +0100
+#
+    # $DEBEMAIL and $DEBFULLNAME
+fi
 # mods pre-packaging
 mv LICENSE         openquake
 mv README.txt      openquake/README
@@ -79,8 +113,7 @@ mv celeryconfig.py openquake
 mv logging.cfg     openquake
 mv openquake.cfg   openquake
 mv bin/openquake   bin/noq
-rm openquake/bin/oqpath.py 
-rm -rf $(find demos -mindepth 1 -maxdepth 1 | grep -v 'demos/simple_fault_demo_hazard' | grep -v 'demos/event_based_hazard')
+rm -rf $(find demos -mindepth 1 -maxdepth 1 | egrep -v 'demos/simple_fault_demo_hazard|demos/event_based_hazard|demos/_site_model')
 dpkg-buildpackage $DPBP_FLAG
 cd -
 
