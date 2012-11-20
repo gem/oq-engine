3b391aa6dde0f35b3978ad63beb9844e895c341b
Luigi Panzeri <defmacro@gmail.com>
2012-11-20 14:39:20 +0100

    added core calculator class
    Former-commit-id: 970609cfb491181c0dbc4f08b87a58f1cf76052a

--- openquake/calculators/risk/classical_bcr/core.py
+++ openquake/calculators/risk/classical_bcr/core.py
@@ -17,12 +17,15 @@
 Core functionality for the classical PSHA risk calculator.
 """
 
+from risklib import api
+
 
 from openquake.calculators.risk import general
 from openquake.calculators.risk.classical import core as classical
 from openquake.utils import stats
 from openquake.utils import tasks
 from openquake import logs
+from openquake.db import models
 
 
 @tasks.oqtask
@@ -33,9 +36,50 @@ def classical_bcr(job_id, assets, hazard_getter, hazard_id,
                   lrem_steps_per_interval,
                   asset_life_expectancy,
                   interest_rate):
+    """
+    Celery task for the BCR risk calculator based on the classical
+    calculator.
+
+    Gets the vulnerability models, instantiates risklib calculators
+    and stores results to db in a single transaction.
+
+    :param int job_id:
+      ID of the currently running job
+    :param assets:
+      list of Assets to take into account
+    :param hazard_getter:
+      Strategy used to get the hazard curves
+    :param int hazard_id
+      ID of the Hazard Output the risk calculation is based on
+    :param bcr_distribution_id
+      ID of the `openquake.db.models.BCRDistribution` output container used
+      to store the computed bcr distribution
+    :param int lrem_steps_per_interval
+      Steps per interval used to compute the Loss Ratio Exceedance matrix
+    :param float interest_rate
+      The interest rate used in the Cost Benefit Analysis
+    :param float asset_life_expectancy
+      The life expectancy used for every asset
+    """
     logs.LOG.debug('Implement me')
 
 
 class ClassicalRiskCalculatorWithBCR(classical.ClassicalRiskCalculator):
     celery_task = classical_bcr
 
+    @property
+    def calculation_parameters(self):
+        rc = self.job.risk_calculation
+
+        return {
+            'lrem_steps_per_interval': rc.lrem_steps_per_interval,
+            'interest_rate': rc.interest_rate,
+            'asset_life_expectancy': rc.asset_life_expectancy
+            }
+
+    @property
+    def create_outputs(self):
+        return dict(
+            bcr_distribution_id=models.BCRDistribution.objects.create(
+                output=models.Output.objects.create_output(
+                    self.job, "BCR Distribution", "bcr_distribution")).pk)
