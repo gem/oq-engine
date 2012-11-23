9e5f83fbda81033dba6a37f25c2b3d7ad9890940
Lars Butler <Lars.Butler@gmail.com>
2012-11-23 17:24:17 +0100

    calcs/hazard/disagg/core:
    Disagg task arg gen only gets incomplete LT realizations from the DB.

--- openquake/calculators/hazard/disagg/core.py
+++ openquake/calculators/hazard/disagg/core.py
@@ -200,7 +200,7 @@ class DisaggHazardCalculator(haz_general.BaseHazardCalculatorNext):
             calc tasks.
         """
         realizations = models.LtRealization.objects.filter(
-            hazard_calculation=self.hc)
+            hazard_calculation=self.hc, is_complete=False)
 
         # then distribute tasks for disaggregation histogram computation
         all_points = list(self.hc.points_to_compute())
