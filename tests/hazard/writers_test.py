748f41b56a62852588693e22d335c2e1c86bd92e
Lars Butler <Lars.Butler@gmail.com>
2012-12-05 15:37:21 +0100

    tests/hazard/writers_test:
    Added 2 serialization test cases for DisaggXMLWriter.

--- tests/hazard/writers_test.py
+++ tests/hazard/writers_test.py
@@ -14,6 +14,7 @@
 # along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
 
 
+import numpy
 import os
 import StringIO
 import tempfile
@@ -728,4 +729,301 @@ class HazardMapXMLWriterTestCase(unittest.TestCase):
         writer = writers.HazardMapXMLWriter(self.path, **metadata)
         writer.serialize(self.data)
 
+        utils.assert_xml_equal(expected, self.path)
+
+
+class DisaggXMLWriterTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.expected_xml = """\
+<?xml version='1.0' encoding='UTF-8'?>
+<nrml xmlns:gml="http://www.opengis.net/gml" xmlns="http://openquake.org/xmlns/nrml/0.4">
+  %(metaelem)s
+    <disaggMatrix type="Mag" dims="2" poE="0.021" iml="2.129">
+      <prob index="0" value="0.0"/>
+      <prob index="1" value="0.01"/>
+    </disaggMatrix>
+    <disaggMatrix type="Dist" dims="3" poE="0.022" iml="2.128">
+      <prob index="0" value="0.0"/>
+      <prob index="1" value="0.01"/>
+      <prob index="2" value="0.02"/>
+    </disaggMatrix>
+    <disaggMatrix type="TRT" dims="2" poE="0.023" iml="2.127">
+      <prob index="0" value="0.0"/>
+      <prob index="1" value="0.01"/>
+    </disaggMatrix>
+    <disaggMatrix type="Mag,Dist" dims="2,3" poE="0.024" iml="2.126">
+      <prob index="0,0" value="0.0"/>
+      <prob index="0,1" value="0.01"/>
+      <prob index="0,2" value="0.02"/>
+      <prob index="1,0" value="0.03"/>
+      <prob index="1,1" value="0.04"/>
+      <prob index="1,2" value="0.05"/>
+    </disaggMatrix>
+    <disaggMatrix type="Mag,Dist,Eps" dims="2,3,4" poE="0.025" iml="2.125">
+      <prob index="0,0,0" value="0.0"/>
+      <prob index="0,0,1" value="0.01"/>
+      <prob index="0,0,2" value="0.02"/>
+      <prob index="0,0,3" value="0.03"/>
+      <prob index="0,1,0" value="0.04"/>
+      <prob index="0,1,1" value="0.05"/>
+      <prob index="0,1,2" value="0.06"/>
+      <prob index="0,1,3" value="0.07"/>
+      <prob index="0,2,0" value="0.08"/>
+      <prob index="0,2,1" value="0.09"/>
+      <prob index="0,2,2" value="0.1"/>
+      <prob index="0,2,3" value="0.11"/>
+      <prob index="1,0,0" value="0.12"/>
+      <prob index="1,0,1" value="0.13"/>
+      <prob index="1,0,2" value="0.14"/>
+      <prob index="1,0,3" value="0.15"/>
+      <prob index="1,1,0" value="0.16"/>
+      <prob index="1,1,1" value="0.17"/>
+      <prob index="1,1,2" value="0.18"/>
+      <prob index="1,1,3" value="0.19"/>
+      <prob index="1,2,0" value="0.2"/>
+      <prob index="1,2,1" value="0.21"/>
+      <prob index="1,2,2" value="0.22"/>
+      <prob index="1,2,3" value="0.23"/>
+    </disaggMatrix>
+    <disaggMatrix type="Lon,Lat" dims="5,5" poE="0.026" iml="2.124">
+      <prob index="0,0" value="0.0"/>
+      <prob index="0,1" value="0.01"/>
+      <prob index="0,2" value="0.02"/>
+      <prob index="0,3" value="0.03"/>
+      <prob index="0,4" value="0.04"/>
+      <prob index="1,0" value="0.05"/>
+      <prob index="1,1" value="0.06"/>
+      <prob index="1,2" value="0.07"/>
+      <prob index="1,3" value="0.08"/>
+      <prob index="1,4" value="0.09"/>
+      <prob index="2,0" value="0.1"/>
+      <prob index="2,1" value="0.11"/>
+      <prob index="2,2" value="0.12"/>
+      <prob index="2,3" value="0.13"/>
+      <prob index="2,4" value="0.14"/>
+      <prob index="3,0" value="0.15"/>
+      <prob index="3,1" value="0.16"/>
+      <prob index="3,2" value="0.17"/>
+      <prob index="3,3" value="0.18"/>
+      <prob index="3,4" value="0.19"/>
+      <prob index="4,0" value="0.2"/>
+      <prob index="4,1" value="0.21"/>
+      <prob index="4,2" value="0.22"/>
+      <prob index="4,3" value="0.23"/>
+      <prob index="4,4" value="0.24"/>
+    </disaggMatrix>
+    <disaggMatrix type="Mag,Lon,Lat" dims="2,5,5" poE="0.027" iml="2.123">
+      <prob index="0,0,0" value="0.0"/>
+      <prob index="0,0,1" value="0.01"/>
+      <prob index="0,0,2" value="0.02"/>
+      <prob index="0,0,3" value="0.03"/>
+      <prob index="0,0,4" value="0.04"/>
+      <prob index="0,1,0" value="0.05"/>
+      <prob index="0,1,1" value="0.06"/>
+      <prob index="0,1,2" value="0.07"/>
+      <prob index="0,1,3" value="0.08"/>
+      <prob index="0,1,4" value="0.09"/>
+      <prob index="0,2,0" value="0.1"/>
+      <prob index="0,2,1" value="0.11"/>
+      <prob index="0,2,2" value="0.12"/>
+      <prob index="0,2,3" value="0.13"/>
+      <prob index="0,2,4" value="0.14"/>
+      <prob index="0,3,0" value="0.15"/>
+      <prob index="0,3,1" value="0.16"/>
+      <prob index="0,3,2" value="0.17"/>
+      <prob index="0,3,3" value="0.18"/>
+      <prob index="0,3,4" value="0.19"/>
+      <prob index="0,4,0" value="0.2"/>
+      <prob index="0,4,1" value="0.21"/>
+      <prob index="0,4,2" value="0.22"/>
+      <prob index="0,4,3" value="0.23"/>
+      <prob index="0,4,4" value="0.24"/>
+      <prob index="1,0,0" value="0.25"/>
+      <prob index="1,0,1" value="0.26"/>
+      <prob index="1,0,2" value="0.27"/>
+      <prob index="1,0,3" value="0.28"/>
+      <prob index="1,0,4" value="0.29"/>
+      <prob index="1,1,0" value="0.3"/>
+      <prob index="1,1,1" value="0.31"/>
+      <prob index="1,1,2" value="0.32"/>
+      <prob index="1,1,3" value="0.33"/>
+      <prob index="1,1,4" value="0.34"/>
+      <prob index="1,2,0" value="0.35"/>
+      <prob index="1,2,1" value="0.36"/>
+      <prob index="1,2,2" value="0.37"/>
+      <prob index="1,2,3" value="0.38"/>
+      <prob index="1,2,4" value="0.39"/>
+      <prob index="1,3,0" value="0.4"/>
+      <prob index="1,3,1" value="0.41"/>
+      <prob index="1,3,2" value="0.42"/>
+      <prob index="1,3,3" value="0.43"/>
+      <prob index="1,3,4" value="0.44"/>
+      <prob index="1,4,0" value="0.45"/>
+      <prob index="1,4,1" value="0.46"/>
+      <prob index="1,4,2" value="0.47"/>
+      <prob index="1,4,3" value="0.48"/>
+      <prob index="1,4,4" value="0.49"/>
+    </disaggMatrix>
+    <disaggMatrix type="Lon,Lat,TRT" dims="5,5,2" poE="0.028" iml="2.122">
+      <prob index="0,0,0" value="0.0"/>
+      <prob index="0,0,1" value="0.01"/>
+      <prob index="0,1,0" value="0.02"/>
+      <prob index="0,1,1" value="0.03"/>
+      <prob index="0,2,0" value="0.04"/>
+      <prob index="0,2,1" value="0.05"/>
+      <prob index="0,3,0" value="0.06"/>
+      <prob index="0,3,1" value="0.07"/>
+      <prob index="0,4,0" value="0.08"/>
+      <prob index="0,4,1" value="0.09"/>
+      <prob index="1,0,0" value="0.1"/>
+      <prob index="1,0,1" value="0.11"/>
+      <prob index="1,1,0" value="0.12"/>
+      <prob index="1,1,1" value="0.13"/>
+      <prob index="1,2,0" value="0.14"/>
+      <prob index="1,2,1" value="0.15"/>
+      <prob index="1,3,0" value="0.16"/>
+      <prob index="1,3,1" value="0.17"/>
+      <prob index="1,4,0" value="0.18"/>
+      <prob index="1,4,1" value="0.19"/>
+      <prob index="2,0,0" value="0.2"/>
+      <prob index="2,0,1" value="0.21"/>
+      <prob index="2,1,0" value="0.22"/>
+      <prob index="2,1,1" value="0.23"/>
+      <prob index="2,2,0" value="0.24"/>
+      <prob index="2,2,1" value="0.25"/>
+      <prob index="2,3,0" value="0.26"/>
+      <prob index="2,3,1" value="0.27"/>
+      <prob index="2,4,0" value="0.28"/>
+      <prob index="2,4,1" value="0.29"/>
+      <prob index="3,0,0" value="0.3"/>
+      <prob index="3,0,1" value="0.31"/>
+      <prob index="3,1,0" value="0.32"/>
+      <prob index="3,1,1" value="0.33"/>
+      <prob index="3,2,0" value="0.34"/>
+      <prob index="3,2,1" value="0.35"/>
+      <prob index="3,3,0" value="0.36"/>
+      <prob index="3,3,1" value="0.37"/>
+      <prob index="3,4,0" value="0.38"/>
+      <prob index="3,4,1" value="0.39"/>
+      <prob index="4,0,0" value="0.4"/>
+      <prob index="4,0,1" value="0.41"/>
+      <prob index="4,1,0" value="0.42"/>
+      <prob index="4,1,1" value="0.43"/>
+      <prob index="4,2,0" value="0.44"/>
+      <prob index="4,2,1" value="0.45"/>
+      <prob index="4,3,0" value="0.46"/>
+      <prob index="4,3,1" value="0.47"/>
+      <prob index="4,4,0" value="0.48"/>
+      <prob index="4,4,1" value="0.49"/>
+    </disaggMatrix>
+  </disaggMatrices>
+</nrml>
+"""
+        self.metadata = dict(
+            investigation_time=50.0,
+            imt='SA',
+            lon=8.33,
+            lat=47.22,
+            sa_period=0.1,
+            sa_damping=5.0,
+            mag_bin_edges=[5, 6],
+            dist_bin_edges=[0, 20, 40],
+            lon_bin_edges=[6, 7, 8, 9, 10],
+            lat_bin_edges=[46, 47, 48, 49, 50],
+            eps_bin_edges=[-0.5, 0.5, 1.5, 2.5],
+            tectonic_region_types=['active shallow crust',
+                                   'stable continental'],
+        )
+        _, self.path = tempfile.mkstemp()
+
+        poe = 0.02
+        iml = 2.13
+
+        matrices = [
+            # mag
+            numpy.array([x * 0.01 for x in range(2)]),
+            # dist
+            numpy.array([x * 0.01 for x in range(3)]),
+            # TRT
+            numpy.array([x * 0.01 for x in range(2)]),
+            # mag, dist
+            numpy.array([x * 0.01 for x in range(6)]).reshape((2, 3)),
+            # mag, dist, eps
+            numpy.array([x * 0.01 for x in range(24)]).reshape((2, 3, 4)),
+            # lon, lat
+            numpy.array([x * 0.01 for x in range(25)]).reshape((5, 5)),
+            # mag, lon, lat
+            numpy.array([x * 0.01 for x in range(50)]).reshape((2, 5, 5)),
+            # lon, lat, trt
+            numpy.array([x * 0.01 for x in range(50)]).reshape((5, 5, 2)),
+        ]
+
+        class DissMatrix(object):
+            def __init__(self, matrix, dim_labels, poe, iml):
+                self.matrix = matrix
+                self.dim_labels = dim_labels
+                self.poe = poe
+                self.iml = iml
+
+        self.data = [
+            DissMatrix(matrices[0], ['Mag'],
+                       poe + 0.001, iml - 0.001),
+            DissMatrix(matrices[1], ['Dist'],
+                       poe + 0.002, iml - 0.002),
+            DissMatrix(matrices[2], ['TRT'],
+                       poe + 0.003, iml - 0.003),
+            DissMatrix(matrices[3], ['Mag', 'Dist'],
+                       poe + 0.004, iml - 0.004),
+            DissMatrix(matrices[4], ['Mag', 'Dist', 'Eps'],
+                       poe + 0.005, iml - 0.005),
+            DissMatrix(matrices[5], ['Lon', 'Lat'],
+                       poe + 0.006, iml - 0.006),
+            DissMatrix(matrices[6], ['Mag', 'Lon', 'Lat'],
+                       poe + 0.007, iml - 0.007),
+            DissMatrix(matrices[7], ['Lon', 'Lat', 'TRT'],
+                       poe + 0.008, iml - 0.008),
+        ]
+
+    def tearDown(self):
+        os.unlink(self.path)
+
+    def test_serialize(self):
+        self.metadata['smlt_path'] = 'b1_b2_b3'
+        self.metadata['gsimlt_path'] = 'b1_b7_b15'
+        metaelem = (
+            '<disaggMatrices sourceModelTreePath="b1_b2_b3" '
+            'gsimTreePath="b1_b7_b15" IMT="SA" investigationTime="50.0" '
+            'saPeriod="0.1" saDamping="5.0" lon="8.33" lat="47.22" '
+            'magBinEdges="5, 6" distBinEdges="0, 20, 40" '
+            'lonBinEdges="6, 7, 8, 9, 10" latBinEdges="46, 47, 48, 49, 50" '
+            'epsBinEdges="-0.5, 0.5, 1.5, 2.5" '
+            'tectonicRegionTypes="active shallow crust, stable continental">'
+        )
+        self.expected_xml %= dict(metaelem=metaelem)
+
+        writer = writers.DisaggXMLWriter(self.path, **self.metadata)
+        writer.serialize(self.data)
+
+        expected = StringIO.StringIO(self.expected_xml)
+        utils.assert_xml_equal(expected, self.path)
+
+    def test_serialize_quantile(self):
+        self.metadata['statistics'] = 'quantile'
+        self.metadata['quantile_value'] = 0.15
+        metaelem = (
+            '<disaggMatrices statistics="quantile" quantileValue="0.15" '
+            'IMT="SA" investigationTime="50.0" saPeriod="0.1" saDamping="5.0" '
+            'lon="8.33" lat="47.22" magBinEdges="5, 6" '
+            'distBinEdges="0, 20, 40" lonBinEdges="6, 7, 8, 9, 10" '
+            'latBinEdges="46, 47, 48, 49, 50" '
+            'epsBinEdges="-0.5, 0.5, 1.5, 2.5" '
+            'tectonicRegionTypes="active shallow crust, stable continental">'
+        )
+        self.expected_xml %= dict(metaelem=metaelem)
+
+        writer = writers.DisaggXMLWriter(self.path, **self.metadata)
+        writer.serialize(self.data)
+
+        expected = StringIO.StringIO(self.expected_xml)
         utils.assert_xml_equal(expected, self.path)
