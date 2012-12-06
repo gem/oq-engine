21e693e0876c9dc756156e42ed000270de59d582
Marco Pagani <marco.pgani@globalquakemodel.org>
2012-12-06 15:50:07 +0100

    Added tests/seismicity

---
+++ tests/seismicity/declusterer/dec_afteran_test.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+"""
+"""
+
+import unittest
+import os
+import numpy as np
+
+from hmtk.seismicity.declusterer.dec_afteran import Afteran
+from hmtk.seismicity.declusterer.distance_time_windows import GardnerKnopoffWindow
+from hmtk.parsers.catalogue import CsvCatalogueParser
+
+class AfteranTestCase(unittest.TestCase):
+    """ 
+    Unit tests for the Afteran declustering algorithm class.
+    """
+    
+    BASE_DATA_PATH = os.path.join(os.path.dirname(__file__), 'data')
+    
+    def setUp(self):
+        """
+        Read the sample catalogue 
+        """
+        flnme = 'gardner_knopoff_test_catalogue.csv'
+        filename = os.path.join(self.BASE_DATA_PATH, flnme)
+        parser = CsvCatalogueParser(filename)
+        self.cat = parser.read_file()
+        
+    def test_dec_gardner_knopoff(self):
+        """
+        Testing the Afteran algorithm 
+        """
+        config = {'time_distance_window' : GardnerKnopoffWindow(),
+                  'time_window' : 60.,
+                  'fs_time_prop' : 1.0}
+        # Instantiate the declusterer and process the sample catalogue 
+        dec = Afteran()
+        vcl, flagvector = dec.decluster(self.cat, config)
+        print 'vcl:',vcl
+        print 'flagvector:',flagvector, self.cat.data['flag']
+        self.assertIs(np.allclose(flagvector,self.cat.data['flag']),True)
---
+++ tests/seismicity/declusterer/distance_time_windows_test.py
@@ -0,0 +1,45 @@
+import unittest
+import os
+import numpy as np
+
+from hmtk.seismicity.declusterer.distance_time_windows import (
+    GardnerKnopoffWindow, GruenthalWindow, UhrhammerWindow)
+
+class CsvCatalogueParserTestCase(unittest.TestCase):
+    """ Unit tests for the csv Catalogue Parser Class"""
+    BASE_DATA_PATH = os.path.join(os.path.dirname(__file__), 'data')
+    
+    def setUp(self):
+        self.gardner_knopoff_window = GardnerKnopoffWindow()
+        self.gruenthal_window = GruenthalWindow()
+        self.uhrhammer_window = UhrhammerWindow()
+
+    def test_gardner_knopoff_window(self):
+        """
+        Test the Gardner and Knopoff Distance-Time window
+        """
+        mag = np.array([5.0,6.6])
+        sw_space, sw_time = self.gardner_knopoff_window.calc(mag)
+        self.assertAlmostEqual(sw_space[0], 39.99447, places=5)
+        self.assertAlmostEqual(sw_space[1], 63.10736, places=5)
+        self.assertAlmostEqual(sw_time[0], 143.71430/364.75, places=5)
+        self.assertAlmostEqual(sw_time[1], 891.45618/364.75, places=5)
+    
+    def test_gruenthal_window(self):
+        """
+        Test the Gruenthal Distance-Time window
+        """
+        mag = np.array([5.0,6.6])
+        sw_space, sw_time = self.gruenthal_window.calc(mag)
+        #self.assertAlmostEqual(sw_space[0], 39.99447, places=5)
+        #self.assertAlmostEqual(sw_time[0], 143.71430/364.75, places=5)
+        #self.assertAlmostEqual(sw_space[1], 63.10736, places=5)
+        #self.assertAlmostEqual(sw_time[1], 891.45618/364.75, places=5)
+    
+    def test_uhrhammer_window(self):
+        mag = np.array([5.0,6.6])
+        sw_space, sw_time = self.uhrhammer_window.calc(mag)
+        #self.assertAlmostEqual(sw_space[0], 39.99447, places=5)
+        #self.assertAlmostEqual(sw_time[0], 143.71430/364.75, places=5)
+        #self.assertAlmostEqual(sw_space[1], 63.10736, places=5)
+        #self.assertAlmostEqual(sw_time[1], 891.45618/364.75, places=5)
