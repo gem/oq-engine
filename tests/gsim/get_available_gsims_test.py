fbc67a80bb780f87a0c13eb5b5b5c22448f2021f
Michele Simionato <michele.simionato@gmail.com>
2012-12-10 13:15:44 +0100

    Changed get_available_gsims to return and ordered list

--- tests/gsim/get_available_gsims_test.py
+++ tests/gsim/get_available_gsims_test.py
@@ -1,25 +1,36 @@
 import mock
+import unittest
 from nose.tools import assert_equal
 from nhlib.gsim import get_available_gsims
 from nhlib.gsim.base import GMPE
+
 
 class FakeModule(object):
     'An object faking a module containing only one subclass of GMPE'
     class AtkinsonBoore2006(GMPE):
         pass
+
+    class BooreAtkinson2008(GMPE):
+        pass
+
     class IamNotAGMPE():
         pass
+
 
 def fake_import(modname):
     if modname == 'nhlib.gsim.atkinson_boore_2006':
         return FakeModule
     return type('EmptyFakeModule', (), {})
 
-def test_get_available_gsims():
-    with mock.patch('os.listdir') as mock_listdir:
-        # returns some file names
-        mock_listdir.return_value = [
-            '__init__.py', 'base.py', 'atkinson_boore_2006.py', 'zhao_2006.py',
-            'README.txt']
-        with mock.patch('importlib.import_module', fake_import):
-            assert_equal(get_available_gsims(), ['AtkinsonBoore2006'])
+
+class AvailableGSIMTestCase(unittest.TestCase):
+
+    def test_get_available_gsims(self):
+        with mock.patch('os.listdir') as mock_listdir:
+            # returns some file names
+            mock_listdir.return_value = [
+                '__init__.py', 'base.py', 'atkinson_boore_2006.py',
+                'zhao_2006.py', 'README.txt']
+            with mock.patch('importlib.import_module', fake_import):
+                assert_equal(get_available_gsims(),
+                             ['AtkinsonBoore2006', 'BooreAtkinson2008'])
