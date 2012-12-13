f802e3275761ac707a48102a7c6c64621a696a38
Michele Simionato <michele.simionato@gmail.com>
2012-12-13 10:39:50 +0100

    Solved the off-by-one error (saving the Input twice)
    Former-commit-id: d47a266a1a7dafad757f2f8d89cf1a60aefc4f59 [formerly a465ec011ff10881fa7c7223b061e722cb310a01] [formerly a465ec011ff10881fa7c7223b061e722cb310a01 [formerly 0cdbe460fcc20ddfa932efd9538cac331474b6ba]]
    Former-commit-id: 2127353516f9eaf1447e82bd14b9c2a623ec86c9
    Former-commit-id: 563b6a521ac03289c90b0c2ab72ce2957929482e

--- openquake/calculators/hazard/scenario/core.py
+++ openquake/calculators/hazard/scenario/core.py
@@ -65,21 +65,19 @@ def gmfs(job_id, rupture_ids, task_seed, task_no):
 # pylint: disable=R0914
 def compute_gmfs(job_id, rupture_ids, task_seed, task_no):
     print job_id, rupture_ids, task_seed, task_no, '***********************'
-    
+
     hc = models.HazardCalculation.objects.get(oqjob=job_id)
     rupture_mdl = source.nrml_to_nhlib(
-                    models.ParsedRupture.objects.get(id=rupture_ids[0]).nrml,
-                    hc.rupture_mesh_spacing, None, None)
+        models.ParsedRupture.objects.get(id=rupture_ids[0]).nrml,
+        hc.rupture_mesh_spacing, None, None)
     sites = haz_general.get_site_collection(hc)
     imts = [haz_general.imt_to_nhlib(x) for x in hc.intensity_measure_types]
     GSIM = AVAILABLE_GSIMS[hc.gsim]
-    
-    print '############################'
-    print ground_motion_fields(rupture_mdl, sites, imts, GSIM(),
-                    hc.truncation_level, realizations=1,
-                    correlation_model=GM_CORRELATION_MODEL_MAP['JB2009'](True))
-    print '############################'
-    
+
+    print ground_motion_fields(
+        rupture_mdl, sites, imts, GSIM(),
+        hc.truncation_level, realizations=1,
+        correlation_model=GM_CORRELATION_MODEL_MAP['JB2009'](True))
 
 
 class ScenarioHazardCalculator(haz_general.BaseHazardCalculatorNext):
@@ -91,19 +89,14 @@ class ScenarioHazardCalculator(haz_general.BaseHazardCalculatorNext):
         """
         logs.log_progress("initializing sources", 2)
 
-        [rupture_mdl] = models.inputs4hcalc(
-            self.hc.id, input_type='rupture_model')
-        rupture_path = os.path.join(self.hc.base_path, rupture_mdl.path)
-
-        # Get or reuse the 'source' Input:
-        inp = engine2.get_input(
-            rupture_path, 'rupture_model', self.hc.owner, self.hc.force_inputs)
+		# Get the rupture model in input
+        [inp] = models.inputs4hcalc(self.hc.id, input_type='rupture_model')
 
         # Associate the source input to the calculation:
         models.Input2hcalc.objects.get_or_create(
             input=inp, hazard_calculation=self.hc)
 
-        # Now store the ParsedRupture record
+        # Store the ParsedRupture record
         src_content = StringIO(inp.model_content.raw_content)
         rupt_parser = RuptureModelParser(src_content)
         src_db_writer = source.RuptureDBWriter(inp, rupt_parser.parse())
@@ -142,7 +135,7 @@ class ScenarioHazardCalculator(haz_general.BaseHazardCalculatorNext):
         rnd.seed(self.hc.random_seed)
 
         inp = models.inputs4hcalc(self.hc.id, 'rupture_model')[0]
-        ruptures = models.ParsedRupture.objects.filter(input__id=inp.id - 1)
+        ruptures = models.ParsedRupture.objects.filter(input__id=inp.id)
         rupture_ids = [rupture.id for rupture in ruptures]
         for task_no in range(self.hc.number_of_ground_motion_fields):
             task_seed = rnd.randint(0, MAX_SINT_32)
