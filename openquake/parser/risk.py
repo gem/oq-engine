8594f6e0d441cecfa67892e832067e1226cfd0d2
Andrea Cerisara <andreacerisara@gmail.com>
2012-12-19 13:54:43 +0100

    Removed obsolete code
    Former-commit-id: bb89f468170c8d3d028fab61a4f86be4f9c7b01a

--- openquake/parser/risk.py
+++ openquake/parser/risk.py
@@ -17,9 +17,6 @@
 # along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
 
 
-# TODO(lp) move parsers into nrml schemas
-
-
 """
 This module contains a class that parses NRML instance document files
 for the output of risk computations. At the moment, loss and loss curve data
@@ -39,12 +36,9 @@ A DOM version of the vulnerability model parser,
 that takes into account the really small size of this input file.
 """
 
-from collections import namedtuple
-
 from lxml import etree
 
 from openquake import logs
-from openquake import shapes
 import nrml
 
 
@@ -53,56 +47,8 @@ GML_NS = 'http://www.opengis.net/gml'
 
 NRML = "{%s}" % NRML_NS
 GML = "{%s}" % GML_NS
-
 NSMAP = {None: NRML_NS, "gml": GML_NS}
-
-NRML_ROOT_TAG = "%snrml" % NRML
-NRML_CONFIG_TAG = "%sconfig" % NRML
-
-GML_POINT_TAG = "%sPoint" % GML
 GML_POS_TAG = "%spos" % GML
-GML_ID_ATTR_NAME = "%sid" % GML
-
-GML_SRS_ATTR_NAME = 'srsName'
-GML_SRS_EPSG_4326 = 'epsg:4326'
-
-# common risk tag names
-RISK_RESULT_TAG = "%sriskResult" % NRML
-RISK_ASSET_TAG = "%sasset" % NRML
-RISK_SITE_TAG = "%ssite" % NRML
-RISK_LMNODE_TAG = "%sLMNode" % NRML
-RISK_BCR_NODE_TAG = "%sBCRNode" % NRML
-RISK_POE_TAG = "%spoE" % NRML
-RISK_END_BRANCH_ATTR_NAME = 'endBranchLabel'
-
-RISK_CURVE_ORDINATE_PROPERTY = 'Probability of Exceedance'
-
-RISK_LOSS_CONTAINER_TAG = "%slossCurveList" % NRML
-RISK_LOSS_CURVES_TAG = "%slossCurves" % NRML
-RISK_LOSS_CURVE_TAG = "%slossCurve" % NRML
-RISK_LOSS_ABSCISSA_TAG = "%sloss" % NRML
-RISK_LOSS_ABSCISSA_PROPERTY = 'Loss'
-
-RISK_LOSS_RATIO_CONTAINER_TAG = "%slossRatioCurveList" % NRML
-RISK_LOSS_RATIO_CURVES_TAG = "%slossRatioCurves" % NRML
-RISK_LOSS_RATIO_CURVE_TAG = "%slossRatioCurve" % NRML
-RISK_LOSS_RATIO_ABSCISSA_TAG = "%slossRatio" % NRML
-RISK_LOSS_RATIO_ABSCISSA_PROPERTY = 'Loss Ratio'
-
-RISK_LOSS_MAP_CONTAINER_TAG = "%slossMap" % NRML
-RISK_LOSS_MAP_LOSS_CONTAINER_TAG = "%sloss" % NRML
-RISK_LOSS_MAP_MEAN_LOSS_TAG = "%smean" % NRML
-RISK_LOSS_MAP_STANDARD_DEVIATION_TAG = "%sstdDev" % NRML
-RISK_LOSS_MAP_VALUE = "%svalue" % NRML
-RISK_LOSS_MAP_LOSS_CATEGORY_ATTR = "lossCategory"
-RISK_LOSS_MAP_UNIT_ATTR = "unit"
-RISK_LOSS_MAP_ASSET_REF_ATTR = "assetRef"
-
-RISK_BCR_MAP_CONTAINER_TAG = "%sbenefitCostRatioMap" % NRML
-RISK_BCR_MAP_BCR_CONTAINER_TAG = "%sbenefitCostRatioValue" % NRML
-RISK_BCR_MAP_BCR_VALUE = "%sbenefitCostRatio" % NRML
-RISK_BCR_MAP_EAL_ORIGINAL_VALUE = "%sexpectedAnnualLossOriginal" % NRML
-RISK_BCR_MAP_EAL_RETROFITTED_VALUE = "%sexpectedAnnualLossRetrofitted" % NRML
 
 
 class AttributeConstraint(object):
@@ -262,195 +208,6 @@ class XMLMismatchError(Exception):
 
     def __str__(self):
         return self.message
-
-
-def validates_against_xml_schema(xml_instance_path,
-                                 schema_path=nrml.nrml_schema_file()):
-    """
-    Checks whether an XML file validates against an XML Schema
-
-    :param xml_instance_path: XML document path
-    :param schema_path: XML schema path
-    :returns: boolean success value
-    """
-    xml_doc = etree.parse(xml_instance_path)
-    xmlschema = etree.XMLSchema(etree.parse(schema_path))
-    return xmlschema.validate(xml_doc)
-
-
-def element_equal_to_site(element, site):
-    """Check whether a given XML element (containing a gml:pos) has the same
-    coordinates as a shapes.Site.
-    Note: doesn't check whether the spatial reference system is the same.
-    """
-    (element_lon, element_lat) = lon_lat_from_site(element)
-    if site == shapes.Site(element_lon, element_lat):
-        return True
-    else:
-        return False
-
-
-def lon_lat_from_site(element):
-    """Extract (lon, lat) pair from gml:pos sub-element of element."""
-    pos_el = element.findall(".//%s" % GML_POS_TAG)
-    if len(pos_el) > 1:
-        raise ValueError(
-            "site element %s has more than one gml:pos elements" %
-            '||'.join(element.attrib.values()))
-    if len(pos_el) < 1:
-        raise ValueError("site element %s has zero gml:pos elements" %
-        '||'.join(element.attrib.values()))
-    return lon_lat_from_gml_pos(pos_el[0])
-
-
-def lon_lat_from_gml_pos(pos_el):
-    """Return (lon, lat) coordinate pair from gml:pos element."""
-    coord = pos_el.text.strip().split()
-    return (float(coord[0]), float(coord[1]))
-
-
-def strip_namespace_from_tag(full_tag, namespace):
-    """Remove namespace alias from a tag"""
-    return full_tag[len(namespace):]
-
-
-class RiskXMLReader(FileProducer):
-    """ This class parses a NRML loss/loss ratio curve file.
-    The class is implemented as a generator.
-    For each curve element in the parsed
-    instance document, it yields a pair of objects, of which the
-    first one is a shapes object of type Site (representing a
-    geographical site as WGS84 lon/lat), and the second one
-    is a dictionary with risk-related attribute values for this site.
-
-    The attribute dictionary looks like
-    {'nrml_id': 'nrml',
-     'result_id': 'rr',
-     'list_id': 'list_1',
-     'assetID': 'a100',
-     'poE': [0.2, 0.02, ...],
-     'loss': [0.0, 1280.0, ...], # for loss
-     'lossRatio': [0.0, 0.1, ...], # for loss ratio
-     'endBranchLabel': '1_1',
-     'property': 'Loss' # 'Loss Ratio'
-    }
-    """
-
-    # these tag names and properties have to be redefined in the
-    # derived classes for loss and loss ratio
-    container_tag = None
-    curves_tag = None
-    curve_tag = None
-    abscissa_tag = None
-    abscissa_property = None
-
-    # common names
-    ordinate_property = RISK_CURVE_ORDINATE_PROPERTY
-    ordinate_tag = RISK_POE_TAG
-
-    property_output_key = 'property'
-
-    def __init__(self, path):
-        self._current_site = None
-        self._current_id_meta = None
-        self._current_asset_meta = None
-
-        self.abscissa_output_key = strip_namespace_from_tag(
-            self.abscissa_tag, NRML)
-        self.ordinate_output_key = strip_namespace_from_tag(
-            self.ordinate_tag, NRML)
-        super(RiskXMLReader, self).__init__(path)
-
-    def _parse(self):
-        for event, element in etree.iterparse(
-                self.file, events=('start', 'end')):
-            if event == 'start' and element.tag == NRML_ROOT_TAG:
-                self._to_id_attributes(element)
-
-            # IMPORTANT: we parse RISK_ASSET_TAG at the 'end' event,
-            # because in the 'start' event children are not assumed to be
-            # *there*
-            elif event == 'end' and element.tag == RISK_ASSET_TAG:
-                self._to_asset_attributes(element)
-
-                curve_els = element.findall('.//%s' % self.curve_tag)
-
-                for curve_el in curve_els:
-                    curr_attributes = self._to_curve_attributes(curve_el)
-                    yield (self._current_site, curr_attributes)
-
-    def _to_id_attributes(self, element):
-        """Collect id attributes from root element."""
-
-        self._current_id_meta = {}
-
-        # get nrml id
-        self._current_id_meta['nrml_id'] = \
-            element.attrib[GML_ID_ATTR_NAME]
-
-        # get riskResult id
-        curr_el = element.find('.//%s' % RISK_RESULT_TAG)
-        self._current_id_meta['result_id'] = \
-            curr_el.attrib[GML_ID_ATTR_NAME]
-
-        # get lossCurveList id
-        curr_el = element.find('.//%s' % self.container_tag)
-        self._current_id_meta['list_id'] = \
-            curr_el.attrib[GML_ID_ATTR_NAME]
-
-    def _to_asset_attributes(self, element):
-        """Collect metadata attributes for new asset element."""
-
-        # assetID
-        self._current_asset_meta = {
-            'assetID': element.attrib[GML_ID_ATTR_NAME]}
-
-        # get site
-        lon, lat = lon_lat_from_site(element)
-        self._current_site = shapes.Site(lon, lat)
-
-    def _to_curve_attributes(self, element):
-        """Build an attributes dict from NRML curve element (this can be
-        'lossCurve' or 'lossRatioCurve'.
-        This element contains abscissae (loss/loss ratio) and ordinate
-        (PoE) values of a curve.
-        """
-
-        attributes = {self.property_output_key: self.abscissa_property}
-
-        abscissa_el_txt = element.findtext(self.abscissa_tag)
-        attributes[self.abscissa_output_key] = [float(x) \
-            for x in abscissa_el_txt.strip().split()]
-
-        ordinate_el_txt = element.findtext(self.ordinate_tag)
-        attributes[self.ordinate_output_key] = [float(x) \
-            for x in ordinate_el_txt.strip().split()]
-
-        if RISK_END_BRANCH_ATTR_NAME in element.attrib:
-            attributes[RISK_END_BRANCH_ATTR_NAME] = \
-                element.attrib[RISK_END_BRANCH_ATTR_NAME]
-
-        attributes.update(self._current_id_meta)
-        attributes.update(self._current_asset_meta)
-        return attributes
-
-
-class LossCurveXMLReader(RiskXMLReader):
-    """NRML parser class for loss curves"""
-    container_tag = RISK_LOSS_CONTAINER_TAG
-    curves_tag = RISK_LOSS_CURVES_TAG
-    curve_tag = RISK_LOSS_CURVE_TAG
-    abscissa_tag = RISK_LOSS_ABSCISSA_TAG
-    abscissa_property = RISK_LOSS_ABSCISSA_PROPERTY
-
-
-class LossRatioCurveXMLReader(RiskXMLReader):
-    """NRML parser class for loss ratio curves"""
-    container_tag = RISK_LOSS_RATIO_CONTAINER_TAG
-    curves_tag = RISK_LOSS_RATIO_CURVES_TAG
-    curve_tag = RISK_LOSS_RATIO_CURVE_TAG
-    abscissa_tag = RISK_LOSS_RATIO_ABSCISSA_TAG
-    abscissa_property = RISK_LOSS_RATIO_ABSCISSA_PROPERTY
 
 
 def _parse_set_attributes(vulnerability_set):
