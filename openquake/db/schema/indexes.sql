5d5ccc89e3cf2db579d34010128b6265421b2592
Lars Butler <Lars.Butler@gmail.com>
2012-12-20 15:22:24 +0100

    db/schema/indexes.sql:
    Added index to hzrdr.lt_realization.hazard_calculation_id.
    
    
    Former-commit-id: 0272e42cb855280dc70479ce0a3c6e43c2de6394 [formerly 0272e42cb855280dc70479ce0a3c6e43c2de6394 [formerly af57ef19bd8fcfa877c4c4d9c4a2e4db723bba00]]
    Former-commit-id: 46305a9a77dd2fa15c31c1e5600eca80df3d6245
    Former-commit-id: a2474a4e069d708a5f06d04d9c5b1ac82e70fe4e

--- openquake/db/schema/indexes.sql
+++ openquake/db/schema/indexes.sql
@@ -59,6 +59,8 @@ CREATE INDEX hzrdr_ses_ses_collection_id_idx on hzrdr.ses(ses_collection_id);
 CREATE INDEX hzrdr_ses_rupture_ses_id_idx on hzrdr.ses_rupture(ses_id);
 -- disagg_result
 CREATE INDEX hzrdr_disagg_result_location_idx on hzrdr.disagg_result using gist(location);
+-- lt_realization
+CREATE INDEX hzrdr_lt_realization_hazard_calculation_id_idx on hzrdr.lt_realization(hazard_calculation_id);
 
 -- riskr indexes
 CREATE INDEX riskr_loss_map_output_id_idx on riskr.loss_map(output_id);
