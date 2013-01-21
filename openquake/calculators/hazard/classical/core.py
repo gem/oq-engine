6197f9e9ca87eca9c673085a0fcdcdbfed209977
Lars Butler <Lars.Butler@gmail.com>
2013-01-21 18:05:45 +0100

    calcs/hazard/classical/core:
    Added mean curve, quantile curve, and weighted quantile curve post-processing
    functions.
    
    
    Former-commit-id: bc5aac657ece0b95b9ffc30133562daa83fb53ac

--- openquake/calculators/hazard/classical/core.py
+++ openquake/calculators/hazard/classical/core.py
@@ -308,6 +308,106 @@ class ClassicalHazardCalculator(haz_general.BaseHazardCalculatorNext):
         logs.LOG.debug('< done with post processing')
 
 
+def compute_mean_curve(curves, weights=None):
+    """
+    Compute the mean or weighted average of a set of curves.
+
+    :param curves:
+        2D array-like collection of hazard curve PoE values. Each element
+        should be a sequence of PoE `float` values. Example::
+
+            [[0.5, 0.4, 0.3], [0.6, 0.59, 0.1]]
+
+        .. note::
+            This data represents the curves for all realizations for a given
+            site and IMT.
+
+    :param weights:
+        List or numpy array of weights, 1 weight value for each of the input
+        ``curves``. This is only used for weighted averages.
+
+    :returns:
+        A curve representing the mean/average (or weighted average, in case
+        ``weights`` are specified) of all the input ``curves``.
+    """
+    # Weights
+    if weights is not None:
+        # If all of the weights are None, don't compute a weighted average
+        if set(weights) == set([None]):
+            weights = None
+        elif any([x is None for x in weights]):
+            # a subset of the weights are None
+            # this is invalid
+            raise ValueError('`None` value found in weights: %s' % weights)
+
+    return numpy.average(curves, weights=weights, axis=0)
+
+
+def compute_weighted_quantile_curve(curves, weights, quantile):
+    """
+    Compute the weighted quantile aggregate of a set of curves. This method is
+    used in the case where hazard curves are computed using the logic tree
+    end-branch enumeration approach. In this case, the weights are explicit.
+
+    :param curves:
+        2D array-like of curve PoEs. Each row represents the PoEs for a single
+        curve
+    :param weights:
+        Array-like of weights, 1 for each input curve.
+    :param quantile:
+        Quantile value to calculate. Should in the range [0.0, 1.0].
+
+    :returns:
+        A numpy array representing the quantile aggregate of the input
+        ``curves`` and ``quantile``, weighting each curve with the specified
+        ``weights``.
+    """
+    # Each curve needs to be associated with a weight:
+    assert len(weights) == len(curves)
+    # NOTE(LB): Weights might be passed as a list of `decimal.Decimal`
+    # types, and numpy.interp can't handle this (it throws TypeErrors).
+    # So we explicitly cast to floats here before doing interpolation.
+    weights = numpy.array(weights, dtype=numpy.float64)
+
+    result_curve = []
+
+    np_curves = numpy.array(curves)
+    np_weights = numpy.array(weights)
+
+    for poes in np_curves.transpose():
+        sorted_poe_idxs = numpy.argsort(poes)
+        sorted_weights = np_weights[sorted_poe_idxs]
+        sorted_poes = poes[sorted_poe_idxs]
+
+        # cumulative sum of weights:
+        cum_weights = numpy.cumsum(sorted_weights)
+
+        result_curve.append(numpy.interp(quantile, cum_weights, sorted_poes))
+
+    return numpy.array(result_curve)
+
+
+def compute_quantile_curve(curves, quantile):
+    """
+    Compute the quantile aggregate of a set of curves. This method is used in
+    the case where hazard curves are computed using the Monte-Carlo logic tree
+    sampling approach. In this case, the weights are implicit.
+
+    :param curves:
+        2D array-like collection of hazard curve PoE values. Each element
+        should be a sequence of PoE `float` values. Example::
+
+            [[0.5, 0.4, 0.3], [0.6, 0.59, 0.1]]
+    :param float quantile:
+        The quantile value. We expected a value in the range [0.0, 1.0].
+
+    :returns:
+        A numpy array representing the quantile aggregate of the input
+        ``curves`` and ``quantile``.
+    """
+    return numpy.array(mstats.mquantiles(curves, prob=quantile, axis=0))[0]
+
+
 def update_result_matrix(current, new):
     """
     Use the following formula to combine multiple iterations of results:
