3311cac905cc1776171aa5a9a4b740933c696fe7
Giuseppe Vallarelli <giuseppevallarelli@gmail.com>
2013-02-04 18:14:17 +0100

    pep8 fixes, added missing docstrings
    Former-commit-id: d790fc6105075dd24c405e9b2f6a60ee338d80d2

--- openquake/calculators/risk/scenario/core.py
+++ openquake/calculators/risk/scenario/core.py
@@ -22,7 +22,7 @@ from django import db
 import risklib
 
 from openquake.calculators import base
-from openquake.calculators.risk import general 
+from openquake.calculators.risk import general
 from openquake.utils import tasks, stats
 from openquake.db import models
 
@@ -32,46 +32,79 @@ from openquake.db import models
 def scenario(job_id, assets, hazard_getter_name, hazard,
         seed, vulnerability_function, output_containers,
         imt, asset_correlation):
+    """
+    Celery task for the scenario damage risk calculator.
 
-   calc = risklib.api.Scenario(vulnerability_function, seed,
-           asset_correlation)
+    :param job_id: the id of the current :class:`openquake.db.models.OqJob`
+    :param assets: the list of :class:`risklib.scientific.Asset`
+        instances considered
+    :param hazard_getter: the name of an hazard getter to be used
+    :param hazard: the hazard output dictionary
+    :param seed: the seed used to initialize the rng
+    :param output_containers: a dictionary {hazard_id: output_id}
+        of output_type "dmg_dist_per_asset"
+    :param imt: the Intensity Measure Type of the ground motion field
+    :param asset_correlation: asset correlation coefficient
+    """
 
-   hazard_getter = general.hazard_getter(hazard_getter_name,
+    calc = risklib.api.Scenario(vulnerability_function,
+            seed, asset_correlation)
+
+    hazard_getter = general.hazard_getter(hazard_getter_name,
                     hazard.keys()[0], imt)
 
-   outputs = calc(assets, [hazard_getter(a.site) for a in assets])
+    outputs = calc(assets, [hazard_getter(a.site) for a in assets])
 
-   outputs_id = output_containers.values()[0][0]
+    outputs_id = output_containers.values()[0][0]
 
+    with db.transaction.commit_on_success(using='reslt_writer'):
+        for i, output in enumerate(outputs):
+            general.write_loss_map_data(outputs_id, assets[i].asset_ref,
+                value=output.mean, std_dev=output.standard_deviation,
+                location=assets[i].site)
 
-   with db.transaction.commit_on_success(using='reslt_writer'):
-       for i, output in enumerate(outputs):
-           general.write_loss_map_data(outputs_id, assets[i].asset_ref,
-                   value=output.mean, std_dev=output.standard_deviation,
-                   location=assets[i].site)
+    base.signal_task_complete(job_id=job_id, num_items=len(assets))
 
-   base.signal_task_complete(job_id=job_id, num_items=len(assets))
-   
-       
 
 class ScenarioRiskCalculator(general.BaseRiskCalculator):
-    
+    """
+    Scenario Risk Calculator. Computes a Loss Map,
+    for a given set of assets.
+    """
+
     hazard_getter = "GroundMotionScenarioGetter"
 
     core_calc_task = scenario
 
     def hazard_output(self, output):
+        """
+        Returns hazard output id
+        """
+
         return output.id
 
     @property
     def calculator_parameters(self):
+        """
+        Provides calculator specific params
+        in a list format where first value
+        represents the intensity measure
+        type and the second one the asset
+        correlation value.
+        """
+
         if self.rc.asset_correlation is None:
             return [self.imt, 0]
         else:
             return [self.imt, self.rc.asset_correlation]
-    
+
     def create_outputs(self, hazard_output):
-        return [models.LossMap.objects.create(output=models.Output.objects.create_output( 
+        """
+        Create the the output of a ScenarioRisk calculator
+        which is a LossMap.
+        """
+
+        return [models.LossMap.objects.create(
+                output=models.Output.objects.create_output(
                                 self.job, "Loss Map", "loss_map"),
                                 hazard_output=hazard_output).id]
-
