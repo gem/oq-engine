05f0501447a5853e776f2b410581ff8c8f6d9377
Lars Butler <Lars.Butler@gmail.com>
2013-02-21 11:49:03 +0100

    db/models:
    HazardCurveProgress.result_matrix field gets a default value of `None` so that
    there are no spurious exceptions if a HazardCurveProgress object is created
    with default field values.

--- openquake/engine/db/models.py
+++ openquake/engine/db/models.py
@@ -2655,7 +2655,7 @@ class HazardCurveProgress(djm.Model):
     # array is 2d: sites x IMLs
     # each row indicates a site,
     # each column holds the PoE vaue for the IML at that index
-    result_matrix = fields.NumpyListField()
+    result_matrix = fields.NumpyListField(default=None)
 
     class Meta:
         db_table = 'htemp\".\"hazard_curve_progress'
