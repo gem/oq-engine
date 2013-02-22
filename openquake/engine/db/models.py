444444a731cb8a021177c3add6eec12dd6e6d2c1
Luigi Panzeri <defmacro@gmail.com>
2013-02-22 11:24:29 +0100

    refactored code to get hazard metadata
    Former-commit-id: e6fa23bf3613f89d775a53194931b684d2f8f850 [formerly e6fa23bf3613f89d775a53194931b684d2f8f850 [formerly ae1ac1460a6921be9f0714349cf3a304316a6059]]
    Former-commit-id: a3628e898be1e90ddc75417236732b717c0a0936
    Former-commit-id: 81714d5934e5339811d698498e147762b38da515

--- openquake/engine/db/models.py
+++ openquake/engine/db/models.py
@@ -1121,31 +1121,6 @@ class RiskCalculation(djm.Model):
         return self.hazard_maximum_distance or DEFAULT_HAZARD_MAXIMUM_DISTANCE
 
     @property
-    def hazard_statistics(self):
-        """
-        The hazard statistics value (mean/quantile and quantile value
-        if applicable) associated with the hazard output used by this
-        risk calculation
-        """
-        if self.hazard_output.is_hazard_curve():
-            return (self.hazard_output.hazardcurve.statistics,
-                    self.hazard_output.hazardcurve.quantile)
-        else:
-            return None, None  # no mean/quantile for gmf
-
-    @property
-    def hazard_logic_tree_paths(self):
-        """
-        The logic tree paths associated with the hazard output used by
-        this risk calculation
-        """
-        if self.hazard_output.is_hazard_curve():
-            lt = self.hazard_output.hazardcurve.lt_realization
-        else:
-            lt = self.hazard_output.gmfcollection.lt_realization
-        return lt.sm_lt_path, lt.gsim_lt_path
-
-    @property
     def is_bcr(self):
         return self.calculation_mode in ['classical_bcr', 'event_based_bcr']
 
@@ -1479,6 +1454,52 @@ class Output(djm.Model):
 
     def is_hazard_curve(self):
         return self.output_type == 'hazard_curve'
+
+    @property
+    def hazard_metadata(self):
+        """
+        Given an Output produced by a risk calculation it returns a
+        tuple with hazard metadata associated with the hazard output
+        used to compute the output ``self``.
+
+        :returns: a tuple with:
+           1) the hazard investigation time (float)
+           2) the kind of hazard statistics (None, "mean" or "quantile")
+           3) quantile value (if applicable) associated with the statistics
+           4) a list representing the source model path (if applicable)
+           5) a list representing the gsim path (if applicable)
+        """
+
+        rc = self.oq_job.risk_calculation
+        hc = rc.get_hazard_calculation()
+
+        investigation_time = hc.investigation_time
+
+        # in scenario calculation we do not have neither statistics
+        # neither logic tree realizations
+
+        # if ``hazard_output`` is None, then the risk output is
+        # computed over multiple hazard outputs (related to different
+        # logic tree realizations). Then, We do not have to collect
+        # metadata regarding statistics or logic tree
+        if self.calculation_mode != u'scenario' or rc.hazard_output is None:
+            ho = rc.hazard_output
+
+            if ho.is_hazard_curve():
+                lt = self.hazard_output.hazardcurve.lt_realization
+                statistics = ho.hazardcurve.statistics
+                quantile = ho.hazardcurve.quantile
+            else:
+                statistics, quantile = None, None  # no mean/quantile for gmf
+                lt = ho.gmfcollection.lt_realization
+            source_model_path, gsim_path = lt.sm_lt_path, lt.gsim_lt_path
+        else:
+            statistics, quantile, source_model_path, gsim_path = (
+                None, None, None, None)
+
+        return (investigation_time,
+                statistics, quantile,
+                source_model_path, gsim_path)
 
 
 class ErrorMsg(djm.Model):
