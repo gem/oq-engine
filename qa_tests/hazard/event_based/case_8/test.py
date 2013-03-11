4280f4fb0f23e15a503fbab49cf877b5d92d0bb9
Lars Butler <Lars.Butler@gmail.com>
2013-03-11 11:42:31 +0100

    qa_tests/hazard/event_based:
    Fixed imports.

--- qa_tests/hazard/event_based/case_8/test.py
+++ qa_tests/hazard/event_based/case_8/test.py
@@ -17,7 +17,7 @@ import numpy
 import os
 
 from nose.plugins.attrib import attr
-from openquake.db import models
+from openquake.engine.db import models
 from qa_tests import _utils as qa_utils
 
 
