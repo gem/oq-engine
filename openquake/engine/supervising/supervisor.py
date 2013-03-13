d99d77efd8835c4ccb202dbea7ef09ae03caf8da
Michele Simionato <michele.simionato@gmail.com>
2013-03-13 16:17:41 +0100

    When bin/openquake is called as a subprocess, use --log-level=error to avoid pollution of stderr from the progress messages
    Former-commit-id: fb2b2771d5e2a75ff6d927993bc723f6217d6509 [formerly f5fe277276637a9c046e6474de1e054a4cdc98ad]
    Former-commit-id: 1f1f85da959fe27a41bdc47d1bab1aff266f0ed2

--- openquake/engine/supervising/supervisor.py
+++ openquake/engine/supervising/supervisor.py
@@ -29,7 +29,6 @@ supervise() will:
 
 import logging
 import os
-import sys
 import signal
 
 import openquake.engine
@@ -161,7 +160,7 @@ class SupervisorLogStreamHandler(logging.StreamHandler):
     """
 
     def __init__(self, job_id):
-        super(SupervisorLogStreamHandler, self).__init__(sys.stdout)
+        super(SupervisorLogStreamHandler, self).__init__()
         self.setFormatter(logging.Formatter(LOG_FORMAT))
         self.job_id = job_id
 
