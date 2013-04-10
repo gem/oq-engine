8cb630806c18e0fcda3deac84b15972e04270b58
Luigi Panzeri <defmacro@gmail.com>
2013-04-10 15:18:34 +0200

    updated the hazard getter
    Former-commit-id: 0d44df73a6d5087847c7327da6fbe7a04512b9e6 [formerly f2abfce5762c5d16dcee55abcba8738eca25aba3]
    Former-commit-id: a5427beff8cb2641f5d8cfc0d8d47a1f80207602

--- openquake/engine/calculators/risk/hazard_getters.py
+++ openquake/engine/calculators/risk/hazard_getters.py
@@ -140,8 +140,20 @@ class HazardCurveGetterPerAsset(HazardGetter):
     def __init__(self, hazard_id, imt, assets, max_distance):
         super(HazardCurveGetterPerAsset, self).__init__(
             hazard_id, imt, assets, max_distance)
-        self.imls = models.HazardCurve.objects.get(
-            pk=self.hazard_id).imls
+        hc = models.HazardCurve.objects.get(pk=self.hazard_id)
+
+        if hc.output.output_type == 'hazard_curve':
+            self.imls = hc.imls
+            self.actual_hazard_id = hc.id
+        elif hc.output.output_type == 'hazard_curve_multi':
+            hc = models.HazardCurve.objects.filter(
+                output__oq_job=hc.output.oq_job,
+                output__output_type='hazard_curve',
+                imt=self._imt,
+                sa_period=self._sa_period,
+                sa_damping=self._sa_damping)
+            self.imls = hc.imls
+            self.actual_hazard_id = hc.id
 
     def get_data(self):
         """
@@ -175,11 +187,12 @@ class HazardCurveGetterPerAsset(HazardGetter):
                 AS min_distance
         FROM hzrdr.hazard_curve_data
         WHERE hazard_curve_id = %s
+        AND imt = %s
         GROUP BY id
         ORDER BY min_distance
         LIMIT 1;"""
 
-        args = (site.wkt, self.hazard_id)
+        args = (site.wkt, self.actual_hazard_id)
 
         cursor.execute(query, args)
         poes, distance = cursor.fetchone()
