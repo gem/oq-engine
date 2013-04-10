5d074912d47ebe84e3b4838392ddde143ad3bd17
Luigi Panzeri <defmacro@gmail.com>
2013-04-10 16:49:57 +0200

    removed a bunch of old stuff
    Former-commit-id: 32740702248ea3668d14fcaa41afa89bd3bfe96d [formerly 32740702248ea3668d14fcaa41afa89bd3bfe96d [formerly e27bc929825bd734041a49bf3f581b7c25797acb]]
    Former-commit-id: 6c59182de22dcc8f358d869aea99d6f54018982c
    Former-commit-id: e5cea23afd71ea8b44e67d88c37c68c92140144c

--- tests/utils/helpers.py
+++ tests/utils/helpers.py
@@ -111,10 +111,6 @@ def get_data_path(file_name):
     return os.path.join(DATA_DIR, file_name)
 
 
-def get_output_path(file_name):
-    return os.path.join(OUTPUT_DIR, file_name)
-
-
 def demo_file(file_name):
     """
     Take a file name and return the full path to the file in the demos
@@ -122,15 +118,6 @@ def demo_file(file_name):
     """
     return os.path.join(
         os.path.dirname(__file__), "../../demos", file_name)
-
-
-def testdata_path(file_name):
-    """
-    Take a file name and return the full path to the file in the
-    tests/data/demos directory
-    """
-    return os.path.normpath(os.path.join(
-        os.path.dirname(__file__), "../data/demos", file_name))
 
 
 def run_hazard_job(cfg, exports=None):
@@ -637,13 +624,6 @@ class DbTestCase(object):
         except ValueError:
             # no job profile for this job
             pass
-
-    def generate_output_path(self, job, output_type="hazard_map"):
-        """Return a random output path for the given job."""
-        path = touch(
-            dir=os.path.join(job.path, "computed_output"), suffix=".xml",
-            prefix="hzrd." if output_type == "hazard_map" else "loss.")
-        return path
 
     def teardown_output(self, output, teardown_job=True, filesystem_only=True):
         """
