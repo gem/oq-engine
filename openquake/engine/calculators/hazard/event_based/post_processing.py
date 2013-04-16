379d704a6ebd9beb99739aa604612775f4838404
Michele Simionato <michele.simionato@gmail.com>
2013-04-16 16:54:13 +0200

    Removed a comment
    Former-commit-id: 60885cfa5fe059c495fdd92c86f8b6cbc48411e8

--- openquake/engine/calculators/hazard/event_based/post_processing.py
+++ openquake/engine/calculators/hazard/event_based/post_processing.py
@@ -163,12 +163,6 @@ def gmf_to_hazard_curve_task(job_id, point, lt_rlz_id, imt, imls, hc_coll_id,
         Spectral Acceleration damping. Used only with ``imt`` of 'SA'.
     """
     lt_rlz = models.LtRealization.objects.get(id=lt_rlz_id)
-    #gmfs = models.GmfAgg.objects.filter(
-    #    gmf_collection__lt_realization=lt_rlz_id,
-    #    imt=imt,
-    #    sa_period=sa_period,
-    #    sa_damping=sa_damping).extra(where=[
-    #        "location::geometry ~= 'SRID=4326;%s'::geometry" % point.wkt2d])
     gmfs = models.Gmf.objects.filter(
         gmf_set__gmf_collection__lt_realization=lt_rlz_id,
         imt=imt,
