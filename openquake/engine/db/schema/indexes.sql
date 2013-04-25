0ab2bbd9ae633c749c0d9ea5c0cfe51cb9e4381e
Lars Butler <Lars.Butler@gmail.com>
2013-04-25 14:39:06 +0200

    db/schema:
    Cleaned up and updated copyright headers in DB schema files.
    
    
    Former-commit-id: 2a25effdd1949086984a9484d8a0128711ea6d64 [formerly a53e234b2450e70a3226fa9795c5d910d1423a43]
    Former-commit-id: 9778f9869972bde2ab810d5340f03e64dae53079

--- openquake/engine/db/schema/indexes.sql
+++ openquake/engine/db/schema/indexes.sql
@@ -1,14 +1,20 @@
 /*
-  Indexes for the OpenQuake database.
+  Copyright (c) 2010-2013, GEM Foundation.
 
-    Copyright (c) 2010-2012, GEM Foundation.
+  OpenQuake is free software: you can redistribute it and/or modify it
+  under the terms of the GNU Affero General Public License as published
+  by the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-    OpenQuake database is made available under the Open Database License:
-    http://opendatacommons.org/licenses/odbl/1.0/. Any rights in individual
-    contents of the database are licensed under the Database Contents License:
-    http://opendatacommons.org/licenses/dbcl/1.0/
+  OpenQuake is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
+  You should have received a copy of the GNU Affero General Public License
+  along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 
 -- admin.oq_user
 CREATE UNIQUE INDEX admin_oq_user_user_name_uniq_idx ON admin.oq_user(user_name);
