bb5ed6a252057c49350e1d72f2623b7f4f9d2cd8
Matteo Nastasi <nastasi@openquake.org>
2013-05-08 15:57:04 +0200

    packager.sh: lxc-sandbox improved

--- packager.sh
+++ packager.sh
@@ -13,6 +13,9 @@ GEM_BUILD_SRC="${GEM_BUILD_ROOT}/${GEM_DEB_PACKAGE}"
 
 GEM_ALWAYS_YES=false
 
+if [ "$GEM_EPHEM_CMD" = "" ]; then
+    GEM_EPHEM_CMD="lxc-start-ephemeral"
+fi
 GEM_EPHEM_NAME="ubuntu-lxc-eph"
 
 NL="
@@ -28,6 +31,9 @@ sig_hand () {
         set +e
         echo "Destroying [$lxc_name] lxc"
         upper="$(mount | grep "${lxc_name}.*upperdir" | sed 's@.*upperdir=@@g;s@,.*@@g')"
+        if [ -f "${upper}.dsk" ]; then
+            loop_dev="$(sudo losetup -a | grep "(${upper}.dsk)$" | cut -d ':' -f1)"
+        fi
         sudo lxc-stop -n $lxc_name
         sudo umount /var/lib/lxc/$lxc_name/rootfs
         sudo umount /var/lib/lxc/$lxc_name/ephemeralbind
@@ -35,8 +41,17 @@ sig_hand () {
         if [ $? -eq 0 ]; then
             sudo umount "$upper"
             sudo rm -r "$upper"
+            if [ "$loop_dev" != "" ]; then
+                sudo losetup -d "$loop_dev"
+                if [ -f "${upper}.dsk" ]; then
+                    sudo rm -f "${upper}.dsk"
+                fi
+            fi
         fi
         sudo lxc-destroy -n $lxc_name
+    fi
+    if [ -f /tmp/packager.eph.$$.log ]; then
+        rm /tmp/packager.eph.$$.log
     fi
 }
 
@@ -194,7 +209,7 @@ devtest_run () {
     local branch_id="$1"
 
     sudo echo
-    sudo lxc-start-ephemeral -o $GEM_EPHEM_NAME -d 2>&1 | tee /tmp/packager.eph.$$.log &
+    sudo ${GEM_EPHEM_CMD} -o $GEM_EPHEM_NAME -d 2>&1 | tee /tmp/packager.eph.$$.log &
     _lxc_name_and_ip_get /tmp/packager.eph.$$.log
 
     _wait_ssh $lxc_ip
@@ -255,7 +270,7 @@ EOF
     cd -
 
     sudo echo
-    sudo lxc-start-ephemeral -o $GEM_EPHEM_NAME -d 2>&1 | tee /tmp/packager.eph.$$.log &
+    sudo ${GEM_EPHEM_CMD} -o $GEM_EPHEM_NAME -d 2>&1 | tee /tmp/packager.eph.$$.log &
     _lxc_name_and_ip_get /tmp/packager.eph.$$.log
 
     _wait_ssh $lxc_ip
