63590f10aebf92ec6b4ad91a2b63cdb2936fdf21
Matteo Nastasi <nastasi@openquake.org>
2013-05-08 11:52:34 +0200

    packager.sh: typo fixed
    Former-commit-id: c1ea193bf0a84b966c01fa3fd7ffe81fdf15f731

--- packager.sh
+++ packager.sh
@@ -146,7 +146,7 @@ _devtest_innervm_run () {
     ssh $lxc_ip "echo \"local   all             \$USER          trust\" | sudo tee -a /etc/postgresql/9.1/main/pg_hba.conf"
     ssh $lxc_ip "
         for dbu in oq_reslt_writer oq_job_superv oq_job_init oq_admin; do
-            sudo sed "1ilocal   openquake   \$dbu                   md5\" /etc/postgresql/9.1/main/pg_hba.conf
+            sudo sed \"1ilocal   openquake   \$dbu                   md5\" /etc/postgresql/9.1/main/pg_hba.conf
         done"
 
     ssh $lxc_ip "sudo sed -i 's/#standard_conforming_strings = on/standard_conforming_strings = off/g' /etc/postgresql/9.1/main/postgresql.conf"
