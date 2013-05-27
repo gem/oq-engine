65ff819486604f58505693ba2759e27be1bc4f55
Michele Simionato <michele.simionato@gmail.com>
2013-05-27 16:33:03 +0200

    inserted the GMFs with the CacheInserter instead of the BulkInserter

--- openquake/engine/writer.py
+++ openquake/engine/writer.py
@@ -26,6 +26,7 @@ from django.db import transaction
 from django.db import connections
 from django.db import router
 from django.contrib.gis.db.models.fields import GeometryField
+from django.contrib.gis.geos.point import Point
 
 LOGGER = logging.getLogger('serializer')
 
@@ -114,8 +115,16 @@ class CacheInserter(object):
     Bulk insert bunches of Django objects by converting them in strings
     and by using COPY FROM.
     """
-    def __init__(self, max_cache_size):
+    def __init__(self, dj_model, max_cache_size):
+        self.table = dj_model
         self.max_cache_size = max_cache_size
+        self.alias = router.db_for_write(dj_model)
+        meta = dj_model._meta
+        self.tname = '"%s"' % meta.db_table
+        meta._fill_fields_cache()
+        self.fields = [f.attname for f in meta._field_name_cache[1:]]
+        # skip the first field, the id
+
         self.values = []
 
     def add(self, obj):
@@ -125,6 +134,7 @@ class CacheInserter(object):
         Append an object to the list of objects to save. If the list exceeds
         the max_cache_size, flush it on the database.
         """
+        assert isinstance(obj, self.table)
         self.values.append(obj)
         if len(self.values) >= self.max_cache_size:
             self.flush()
@@ -137,41 +147,50 @@ class CacheInserter(object):
             return
 
         # perform some introspection
-        objects, last = self.values[:-1], self.values[-1]
-        alias = router.db_for_write(last.__class__)
-        meta = last.__class__._meta
-        tname = '"%s"' % meta.db_table
-        fields = []
-        for f in meta._field_name_cache[1:]:  # skip the first field, the id
-            col = getattr(last, f.name)
-            fname = f.name + '_id' if hasattr(col, 'id') else f.name
-            fields.append(fname)
+        objects = self.values
 
         # generate a big string with the objects and save it with COPY FROM
-        text = '\n'.join(self.to_line(obj, fields) for obj in objects)
-        curs = connections[alias].cursor()
-        curs.copy_from(StringIO(text), tname, columns=fields)
-        last.save()  # this "commits" the operation
+        text = '\n'.join(self.to_line(obj) for obj in objects)
+        with transaction.commit_on_success(using=self.alias):
+            curs = connections[self.alias].cursor()
+            curs.copy_from(StringIO(text), self.tname, columns=self.fields)
 
-        LOGGER.debug('saved %d rows in uiapi.performance', len(self.values))
+        ## TODO: should we add an assert that the number of rows stored
+        ## in the db is the expected one? I (MS) have seen a case where
+        ## this fails silently (it was for True/False not converted in t/f)
+
+        LOGGER.debug('saved %d rows in %s', len(self.values), self.tname)
         self.values = []
 
-    @staticmethod
-    def to_line(obj, fields):
+    def to_line(self, obj):
         """
         Convert the fields of a Django object into a line string suitable
         for import via COPY FROM. The encoding is UTF8.
         """
         cols = []
-        for f in fields:
+        for f in self.fields:
             col = getattr(obj, f)
             if col is None:
                 col = r'\N'
+            elif isinstance(col, bool):
+                col = 't' if col else 'f'
+            elif isinstance(col, Point):
+                col = 'SRID=4326;' + col.wkt
             elif isinstance(col, GeometryField):
                 col = col.wkt()
-            elif isinstance(col, list):  # for arrays; this is fragile
-                col = '{%s}' % str(col)[1:-1]
+            elif isinstance(col, list):  # for numeric arrays; this is fragile
+                col = self.array_to_pgstring(col)
             else:
                 col = unicode(col).encode('utf8')
             cols.append(col)
         return '\t'.join(cols)
+
+    @staticmethod
+    def array_to_pgstring(a):
+        ls = []
+        for n in a:
+            s = str(n)
+            if s.endswith('L'):  # strip the trailing "L"
+                s = s[:-1]
+            ls.append(s)
+        return '{%s}' % ','.join(ls)
