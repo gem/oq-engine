23405b06e401b172c0e8db80eec6fa4ec1033731
Luigi Panzeri <defmacro@gmail.com>
2013-05-30 15:36:28 +0200

    added skeleton for peb qa test
    Former-commit-id: 1f22084790854c403d612ee96abf79a779b8bb80 [formerly c6c59a19e45743493ce3a46c02aa30321af9992d]
    Former-commit-id: 795e7b9f603998cc27e058adcb306f8141251df5

---
+++ qa_tests/risk/event_based/qatest_1/test.py
@@ -0,0 +1,38 @@
+# Copyright (c) 2013, GEM Foundation.
+#
+# OpenQuake is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# OpenQuake is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+
+
+from nose.plugins.attrib import attr as noseattr
+
+from qa_tests import risk
+
+from openquake.engine.db import models
+
+
+class EventBaseQATestCase1(risk.LogicTreeBasedTestCase):
+    hazard_cfg = os.path.join(os.path.dirname(__file__), 'job_haz.ini')
+    risk_cfg = os.path.join(os.path.dirname(__file__), 'job_risk.ini')
+
+    @noseattr('qa', 'risk', 'event_based', 'e2e')
+    def test(self):
+        self._run_test()
+
+    def actual_data(self):
+        return [0]
+
+    def expected_data(self):
+        return [0]
