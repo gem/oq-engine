063ac86097924ebff9a6250b554e8b71e2e93bbc
Luigi Panzeri <defmacro@gmail.com>
2013-05-31 11:12:39 +0200

    avoid duplicate imt to be used in hazard calculators when imt are got from risk models
    Former-commit-id: ff389ccfd72fee06905cd39694872b24fbb462d1

--- openquake/engine/calculators/hazard/general.py
+++ openquake/engine/calculators/hazard/general.py
@@ -687,17 +687,30 @@ class BaseHazardCalculator(base.Calculator):
                 intensity_measure_types_and_levels = dict([
                     (record['IMT'], record['IML'])
                     for record in parsers.VulnerabilityModelParser(content)])
-                intensity_measure_types = list(set(
-                    record['IMT']
-                    for record in parsers.VulnerabilityModelParser(content)))
+                intensity_measure_types = (
+                    intensity_measure_types_and_levels.keys())
 
-                hc.intensity_measure_types_and_levels.update(
-                    intensity_measure_types_and_levels)
+                for imt, levels in intensity_measure_types_and_levels:
+                    if (imt in hc.intensity_measure_types_and_levels and
+                        (set(hc.intensity_measure_types_and_levels) -
+                         set(levels))):
+                        logs.LOG.warning("The same IMT %s is associated with "
+                                         "different levels" % imt)
+                    else:
+                        hc.intensity_measure_types_and_levels[imt] = levels
+
                 hc.intensity_measure_types.extend(intensity_measure_types)
+
+            if hc.intensity_measure_types is not None:
+                hc.intensity_measure_types = list(set(
+                    hc.intensity_measure_types))
             hc.save()
 
         queryset = self.hc.inputs.filter(input_type='fragility')
         if queryset.exists():
+            hc.intensity_measure_types_and_levels = dict()
+            hc.intensity_measure_types = list()
+
             parser = iter(parsers.FragilityModelParser(
                 StringIO.StringIO(
                     queryset.all()[0].model_content.raw_content_ascii)))
@@ -714,9 +727,8 @@ class BaseHazardCalculator(base.Calculator):
             hc.intensity_measure_types_and_levels = dict(
                 (iml['IMT'], iml['imls'])
                 for _taxonomy, iml, _params, _no_damage_limit in parser)
-            hc.intensity_measure_types = (
-                hc.intensity_measure_types_and_levels.keys())
-            hc.save()
+            hc.intensity_measure_types = list(set(
+                hc.intensity_measure_types_and_levels.keys()))
 
         queryset = self.hc.inputs.filter(input_type='exposure')
         if queryset.exists():
