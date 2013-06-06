9d98169358d48c7c4e0366978e5e9f603cdb57cd
Michele Simionato <michele.simionato@gmail.com>
2013-06-06 05:28:21 +0200

    Fixed the instrumentation of the hazard getter
    Former-commit-id: dcd445eae485f7bb8ad3d10b6d87903faefe26f5

--- openquake/engine/calculators/risk/event_based/core.py
+++ openquake/engine/calculators/risk/event_based/core.py
@@ -501,7 +501,6 @@ class EventBasedRiskCalculator(base.RiskCalculator):
         risk_model = self.risk_models[taxonomy][loss_type]
 
         time_span, tses = self.hazard_times()
-        monitor = EnginePerformanceMonitor('hazard getter', self.job.id)
 
         return [base.CalculationUnit(
             api.ProbabilisticEventBased(
@@ -515,7 +514,7 @@ class EventBasedRiskCalculator(base.RiskCalculator):
                 ho,
                 assets,
                 self.rc.best_maximum_distance,
-                risk_model.imt, monitor))
+                risk_model.imt))
                 for ho in self.rc.hazard_outputs()]
 
     def hazard_times(self):
