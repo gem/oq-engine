3687cd0e20c53f98a107d1466dc4a67dda12caa0
Lars Butler <Lars.Butler@gmail.com>
2013-07-10 16:00:52 +0200

    Add `engine` app with south migrations

--- openquakeserver/settings.py
+++ openquakeserver/settings.py
@@ -120,6 +120,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'south',
+    'engine',
 )
 
 # A sample logging configuration. The only tangible logging
