4dea74ac98805d810a2bdaea77de555790415cc0
Lars Butler <Lars.Butler@gmail.com>
2013-07-15 16:14:17 +0200

    urls, engine/calc_urls:
    Added first url mappings, including the admin stuff.

--- openquakeserver/urls.py
+++ openquakeserver/urls.py
@@ -1,17 +1,19 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.defaults import patterns
+from django.conf.urls.defaults import include
+from django.conf.urls.defaults import url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'openquakeserver.views.home', name='home'),
     # url(r'^openquakeserver/', include('openquakeserver.foo.urls')),
+    url(r'^calc/', include('openquakeserver.engine.calc_urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
