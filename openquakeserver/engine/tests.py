f2509449d3ba54b75d62370bd770dc8e44fcc4c2
Lars Butler <Lars.Butler@gmail.com>
2013-07-15 17:12:16 +0200

    engine/tests:
    Added a basic test for `calc_risk`.

--- openquakeserver/engine/tests.py
+++ openquakeserver/engine/tests.py
@@ -46,3 +46,37 @@ class CalcHazardTestCase(unittest.TestCase):
 
             self.assertEqual(200, response.status_code)
             self.assertEqual(expected_content, response.content)
+
+
+class CalcRiskTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_get(self):
+        expected_content = json.dumps([
+            {u'url': u'http://www.openquake.org/calc/risk/1',
+             u'status': u'executing',
+             u'description': u'description 1',
+             u'id': 1},
+            {u'url': u'http://www.openquake.org/calc/risk/2',
+             u'status': u'pre_executing',
+             u'description': u'description 2',
+             u'id': 2},
+            {u'url': u'http://www.openquake.org/calc/risk/3',
+             u'status': u'complete',
+             u'description': u'description e',
+             u'id': 3},
+        ])
+        with mock.patch('engine.views._get_risk_calcs') as grc:
+            grc.return_value = [
+                (1, 'executing', 'description 1'),
+                (2, 'pre_executing', 'description 2'),
+                (3, 'complete', 'description e'),
+            ]
+            request = self.factory.get('/calc/risk/')
+            request.META['HTTP_HOST'] = 'www.openquake.org'
+            response = views.calc_risk(request)
+
+            self.assertEqual(200, response.status_code)
+            self.assertEqual(expected_content, response.content)
