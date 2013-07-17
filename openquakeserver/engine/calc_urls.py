4a383a55d5ebd9249a986d4d188488af69463f27
Lars Butler <Lars.Butler@gmail.com>
2013-07-17 16:31:37 +0200

    engine/calc_urls:
    Don't require a trailing slash for any of the API endpoints.

--- openquakeserver/engine/calc_urls.py
+++ openquakeserver/engine/calc_urls.py
@@ -4,9 +4,9 @@ from django.conf.urls.defaults import url
 # each url is prefixed with /calc/
 urlpatterns = patterns(
     'openquakeserver.engine.views',
-    url(r'^hazard/$', 'calc_hazard'),
-    url(r'^hazard/(\d+)/$', 'calc_hazard_info'),
+    url(r'^hazard$', 'calc_hazard'),
+    url(r'^hazard/(\d+)$', 'calc_hazard_info'),
 
-    url(r'^risk/$', 'calc_risk'),
-    url(r'^risk/(\d+)/$', 'calc_risk_info'),
+    url(r'^risk$', 'calc_risk'),
+    url(r'^risk/(\d+)$', 'calc_risk_info'),
 )
