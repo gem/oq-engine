aad693547b707e9b75c0b5ceae0e5f6f4d8ec27f
Lars Butler <Lars.Butler@gmail.com>
2013-07-26 16:32:22 +0200

    engine/v1/calc_urls:
    Minor correct to url mapping regexes.

--- openquakeserver/engine/v1/calc_urls.py
+++ openquakeserver/engine/v1/calc_urls.py
@@ -6,9 +6,9 @@ urlpatterns = patterns(
     'openquakeserver.engine.views',
     url(r'^hazard$', 'calc_hazard'),
     url(r'^hazard/(\d+)$', 'calc_hazard_info'),
-    url(r'^hazard/(\d+)/results', 'calc_hazard_results'),
+    url(r'^hazard/(\d+)/results$', 'calc_hazard_results'),
 
     url(r'^risk$', 'calc_risk'),
     url(r'^risk/(\d+)$', 'calc_risk_info'),
-    url(r'^risk/(\d+)/results', 'calc_risk_results'),
+    url(r'^risk/(\d+)/results$', 'calc_risk_results'),
 )
