7c19c1cb4b07416751a58344de973432b29dfb83
Lars Butler <Lars.Butler@gmail.com>
2013-07-26 13:06:22 +0200

    tests/utils/helpers:
    Added a description and example usage docs to MultiMock.
    
    
    Former-commit-id: 0db8923f99e951b8d2b4e0f561bd764e7a2e0def

--- tests/utils/helpers.py
+++ tests/utils/helpers.py
@@ -913,6 +913,40 @@ def random_location_generator(min_x=-180, max_x=180, min_y=-90, max_y=90):
 
 
 class MultiMock(object):
+    """
+    Context-managed multi-object. This is useful if you need to mock multiple
+    things at once. So instead of creating individual patch+mock objects for
+    each, you can define them basically as a dictionary. You can also use the
+    mock context managers without having to nest `with` statements.
+
+    Example usage:
+
+    .. code-block:: python
+
+        # First, define your mock targets as a dictionary.
+        # The value of each item is the path to the function/method you wish to
+        # mock. The key is basically a shortcut to the mock.
+        mocks = {
+            'touch': 'openquake.engine.engine.touch_log_file',
+            'job': 'openquake.engine.engine.haz_job_from_file',
+        }
+        multi_mock = MultiMock(**mocks)
+
+        # To start mocking, start the context manager using `with`:
+        # with multi_mock:
+        with multi_mock:
+            # You can mock return values, for example, just as you would with
+            # any other Mock object:
+            multi_mock['job'].return_value = 'foo'
+
+            # call the function under test which will calls the mocked
+            # functions
+            engine.run_hazard('job.ini', 'debug', 'oq.log', ['geojson'])
+
+            # To test the mocks, you can simply access each mock from
+            # `multi_mock` like a dict:
+            assert multi_mock['touch'].call_count == 1
+    """
 
     def __init__(self, **mocks):
         # dict of mock names -> mock paths
