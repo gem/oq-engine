efd9211bfe4de2352bbeb392c3faa88a401623d6
Lars Butler <Lars.Butler@gmail.com>
2013-07-29 17:25:56 +0200

    engine/views:
    Added views for exporting risk and hazard results.

--- openquakeserver/engine/views.py
+++ openquakeserver/engine/views.py
@@ -1,3 +1,4 @@
+import StringIO
 import json
 import logging
 import urlparse
@@ -6,13 +7,21 @@ from django.http import HttpResponse
 
 from openquake.engine.db import models as oqe_models
 from openquake.engine import engine
+from openquake.engine.export import hazard as hazard_export
+from openquake.engine.export import risk as risk_export
 
 METHOD_NOT_ALLOWED = 405
+NOT_IMPLEMENTED = 501
 JSON = 'application/json'
 
 IGNORE_FIELDS = ('base_path', 'export_dir', 'owner')
 GEOM_FIELDS = ('region', 'sites', 'region_constraint', 'sites_disagg')
 RISK_INPUTS = ('hazard_calculation', 'hazard_output')
+
+#: For exporting calculation outputs, the client can request a specific format
+#: (xml, geojson, csv, etc.). If the client does not specify give them (NRML)
+#: XML by default.
+DEFAULT_EXPORT_TYPE = 'xml'
 
 LOGGER = logging.getLogger('openquakeserver')
 
@@ -156,6 +165,10 @@ def calc_hazard_results(request, calc_id):
 
 
 @allowed_methods(('GET', ))
+def get_hazard_result(request, result_id):
+    return _get_result(request, result_id, hazard_export.export)
+
+
 def calc_risk(request):
     """
     The following request types are supported:
@@ -226,3 +239,31 @@ def calc_risk_results(request, calc_id):
         response_data.append(datum)
 
     return HttpResponse(content=json.dumps(response_data))
+
+
+@allowed_methods(('GET', ))
+def get_risk_result(request, result_id):
+    return _get_result(request, result_id, risk_export.export)
+
+
+def _get_result(request, result_id, export_fn):
+    export_type = request.GET.get('export_type', DEFAULT_EXPORT_TYPE)
+
+    content = StringIO.StringIO()
+    try:
+        content = export_fn(result_id, content, export_type=export_type)
+    except NotImplementedError, err:
+        # Throw back a 501 if the exact export parameters are not supported
+        return HttpResponse(err.message, status=NOT_IMPLEMENTED)
+
+    # Just in case the original StringIO object was closed:
+    resp_content = StringIO.StringIO()
+    resp_content.writelines(content.buflist)
+    del content
+    # rewind and prepare to throw the content back to the client
+    resp_content.seek(0)
+
+    # TODO(LB): A possible necessary optimization--in the future--would be to
+    # iteratively stream large files.
+    return HttpResponse(content=resp_content.getvalue())
+
