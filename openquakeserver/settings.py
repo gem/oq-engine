c29ca5496d39f5c1af2823d49a18b4418c232258
Lars Butler <Lars.Butler@gmail.com>
2013-08-07 13:03:31 +0200

    settings:
    Settings can now find form templates in the `templates/` dir.

--- openquakeserver/settings.py
+++ openquakeserver/settings.py
@@ -1,3 +1,5 @@
+import os
+
 from openquake.engine import settings as oqe_settings
 
 DEBUG = True
@@ -110,6 +112,9 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+
+    # Include `templates` from the current package/dir:
+    os.path.join(os.path.dirname(__file__), 'templates'),
 )
 
 INSTALLED_APPS = (
