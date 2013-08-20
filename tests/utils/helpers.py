151ec5276fe89cf88bec506c68460f498c302192
Luigi Panzeri <defmacro@gmail.com>
2013-08-20 19:31:24 +0200

    reload riskcalculation to make all the attributes be converted to the proper python type

--- tests/utils/helpers.py
+++ tests/utils/helpers.py
@@ -870,6 +870,7 @@ def get_fake_risk_job(risk_cfg, hazard_cfg, output_type="curve",
     job.risk_calculation = risk_calc
     job.save()
     error_message = validate(job, 'risk', params, files, [])
+    job.risk_calculation = models.RiskCalculation.objects.get(id=risk_calc.id)
     if error_message:
         raise RuntimeError(error_message)
     return job, files
