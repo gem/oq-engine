1f5ed4be87551fa64479889450a55d78f84d0354
Damiano Monelli <damiano.monelli@globalquakemodel.org>
2013-08-23 12:16:27 +0200

    added test for Non-Cratonic Mean

--- tests/gsim/somerville_2009_test.py
+++ tests/gsim/somerville_2009_test.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from openquake.hazardlib.gsim.somerville_2009 import \
-    SomervilleEtAl2009YilgarnCraton
+    SomervilleEtAl2009YilgarnCraton, SomervilleEtAl2009NonCratonic
 
 from tests.gsim.utils import BaseGSIMTestCase
 
@@ -33,3 +33,12 @@ class SomervilleEtAl2009YilgarnCratonTestCase(BaseGSIMTestCase):
     def test_std_total(self):
         self.check('S09/SOMMERVILLE_YILGARN2009_STD_TOTAL.csv',
                    max_discrep_percentage=0.1)
+
+
+class SomervilleEtAl2009NonCratonicTestCase(BaseGSIMTestCase):
+    GSIM_CLASS = SomervilleEtAl2009NonCratonic
+
+    def test_mean(self):
+        self.check('S09/SOMMERVILLE_Non_Cratonic2009_MEAN.csv',
+                   max_discrep_percentage=0.4)
+
