7ff32cbe7ae03591baf4c5aac09bd29ca6379e01
Lars Butler <Lars.Butler@gmail.com>
2013-08-28 15:10:34 +0200

    engine/forms:
    Added optional parameters for performing a post-calc migration of
    results.

--- openquakeserver/engine/forms.py
+++ openquakeserver/engine/forms.py
@@ -1,12 +1,25 @@
 from django import forms
 
 
-class HazardForm(forms.Form):
+class BaseForm(forms.Form):
+    # Optional callback url to hit to start automatic migration of calculation
+    # results once a calculation concludes
     job_config = forms.FileField(label='Job configuration (.ini file)')
+    migration_callback_url = forms.URLField(
+        required=False,
+        label='URL to notify of calc completion',
+    )
+    owner_user = forms.CharField(
+        required=False,
+        label='Owner username of calculation results',
+    )
 
 
-class RiskForm(forms.Form):
-    job_config = forms.FileField(label='Job configuration (.ini file)')
+class HazardForm(BaseForm):
+    pass
+
+
+class RiskForm(BaseForm):
     hazard_calc = forms.IntegerField(
         required=False,
         label='Hazard calculation ID',
