3ea0919cc61367a8ade532b74a41a2ff633a2114
g-weatherill <gaweatherill82@yahoo.co.uk>
2013-09-12 11:43:52 +0200

    Adds Arbitrary class to docs

--- doc/gsim/campbell_bozorgnia_2008.rst
+++ doc/gsim/campbell_bozorgnia_2008.rst
@@ -1,8 +1,14 @@
 =======================
-Campbell Bozorgnia 2008
+Campbell and Bozorgnia 2008
 =======================
 
 .. module:: openquake.hazardlib.gsim.campbell_bozorgnia_2008
 
+
 .. autoclass:: CampbellBozorgnia2008
     :members:
+
+
+.. autoclass:: CampbellBozorgnia2008Arbitrary
+    :members:
+
--- openquake/hazardlib/gsim/campbell_bozorgnia_2008.py
+++ openquake/hazardlib/gsim/campbell_bozorgnia_2008.py
@@ -14,7 +14,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-Module exports :class:`CampbellBozorgnia2008`.
+Module exports :class:`CampbellBozorgnia2008`, and 
+:class:'CampbellBozorgnia2008Arbitrary'
 """
 from __future__ import division
 
