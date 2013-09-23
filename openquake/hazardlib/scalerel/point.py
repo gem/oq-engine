6114e427d739ac52b452c450bd207300ae975de7
Damiano Monelli <damiano.monelli@globalquakemodel.org>
2013-09-23 11:40:03 +0200

    added note to doc

--- openquake/hazardlib/scalerel/point.py
+++ openquake/hazardlib/scalerel/point.py
@@ -25,6 +25,12 @@ class PointMSR(BaseMSR):
     Independently of the magnitude value, this scaling relationship returns
     always a very small value (1e-4 squared km, corresponding to a 10 by 10 m
     square) for the median area.
+
+    NOTE: This scaling-relationship is meant to be used in area and point
+    sources to mimic point ruptures. Is not intended to be used in fault
+    sources, it would require a fault surface discretization step to small
+    (less than 10 m, using an aspect ratio equal to 1) which is unfeasible for
+    realistic applications.
     """
     def get_median_area(self, mag, rake):
         """
