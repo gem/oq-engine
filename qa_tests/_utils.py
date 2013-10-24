64856fa8357cecc4515f71d014bb253b3cb42794
Michele Simionato <michele.simionato@gmail.com>
2013-10-24 15:54:17 +0200

    Fixed the tolerance and got the right files from Damiano
    Former-commit-id: a610de32f9552ccbd5050eb36a33336cabf65c05 [formerly 51f6e5f9869fd9555364ad75fd7cf7f7c57c3c5c] [formerly 51f6e5f9869fd9555364ad75fd7cf7f7c57c3c5c [formerly d6a8cec31e053f89057bd32029893d2655a044e9]]
    Former-commit-id: aff77d5819dce7bc122a0fd569867368c9cdc323
    Former-commit-id: f0d37e6306731e70a7fbf3f6a3b9d0b5091b86ae

--- qa_tests/_utils.py
+++ qa_tests/_utils.py
@@ -112,7 +112,7 @@ def count(gmf_value, gmfs_site_one, gmfs_site_two,
 
 def compare_hazard_curve_with_csv(
         hc, sm_lt_path, gsim_lt_path, imt, sa_period, sa_damping,
-        csv_name, csv_delimiter=','):
+        csv_name, csv_delimiter=',', rtol=0.2):
     """
     This is useful in tests that compares the hazard curves in the db with
     the expected values in the csv. The csv is expected to have the form
@@ -142,7 +142,7 @@ def compare_hazard_curve_with_csv(
         reader = csv.reader(f, delimiter=csv_delimiter)
         expected_data = [map(float, row) for row in reader]
 
-    numpy.testing.assert_array_almost_equal(expected_data, data, decimal=3)
+    numpy.testing.assert_allclose(expected_data, data, rtol=rtol)
     # to debug the test, in case it breaks, comment the assert and
     # uncomment the following, lines then compare the expected file with
     # the file generated from the computed data and stored in /tmp:
