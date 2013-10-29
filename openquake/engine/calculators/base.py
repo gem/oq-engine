018ac5ba2413414645a4a91114366a59904968bd
Michele Simionato <michele.simionato@gmail.com>
2013-10-29 04:44:01 +0100

    Restore a log message removed by mistake
    Former-commit-id: 8d5c2b3ee5b6167a22effb2ff3d78164813d0294

--- openquake/engine/calculators/base.py
+++ openquake/engine/calculators/base.py
@@ -335,6 +335,7 @@ class Calculator(object):
                                 self.job.calculation.export_dir,
                                 export_type
                             )
+                            logs.LOG.info('exported %s', fname)
                             exported_files.append(fname)
 
         return exported_files
