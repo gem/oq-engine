d2d24d403390f9a4b1f1e66e87122c22f0fbdaa0
Damiano Monelli <damiano.monelli@globalquakemodel.org>
2013-10-30 17:27:25 +0100

    replicated Peer tests for 'hazard_curves' method

--- tests/acceptance/peer_test.py
+++ tests/acceptance/peer_test.py
@@ -31,7 +31,7 @@ from openquake.hazardlib.pmf import PMF
 from openquake.hazardlib.geo import NodalPlane
 from openquake.hazardlib.scalerel import PeerMSR, PointMSR
 from openquake.hazardlib.gsim.sadigh_1997 import SadighEtAl1997
-from openquake.hazardlib.calc import hazard_curves_poissonian as hazard_curves
+from openquake.hazardlib.calc import hazard_curves_poissonian, hazard_curves
 from openquake.hazardlib.tom import PoissonTOM
 
 from tests.acceptance import _peer_test_data as test_data
@@ -43,9 +43,10 @@ def assert_hazard_curve_is(testcase, actual, expected, atol, rtol):
                         "%s != %s" % (actual, expected))
 
 class Set1TestCase(unittest.TestCase):
-    def test_case_10(self):
+    def setUp(self):
+        # input data for case 10
         hypocenter_pmf = PMF([(1, test_data.SET1_CASE10_HYPOCENTER_DEPTH)])
-        sources = [AreaSource(source_id='area', name='area',
+        self.sources_case10 = [AreaSource(source_id='area', name='area',
             tectonic_region_type=const.TRT.ACTIVE_SHALLOW_CRUST,
             mfd=test_data.SET1_CASE10_MFD,
             nodal_plane_distribution=PMF([(1, NodalPlane(0.0, 90.0, 0.0))]),
@@ -59,29 +60,16 @@ class Set1TestCase(unittest.TestCase):
             rupture_mesh_spacing=10.0,
             temporal_occurrence_model=PoissonTOM(1.)
         )]
-        sites = SiteCollection([
+        self.sites_case10 = SiteCollection([
             test_data.SET1_CASE10_SITE1, test_data.SET1_CASE10_SITE2,
             test_data.SET1_CASE10_SITE3, test_data.SET1_CASE10_SITE4
         ])
-        gsims = {const.TRT.ACTIVE_SHALLOW_CRUST: SadighEtAl1997()}
-        truncation_level = 0
-        time_span = 1.0
-        imts = {test_data.IMT: test_data.SET1_CASE10_IMLS}
+        self.gsims = {const.TRT.ACTIVE_SHALLOW_CRUST: SadighEtAl1997()}
+        self.truncation_level = 0
+        self.time_span = 1.0
+        self.imts_case10 = {test_data.IMT: test_data.SET1_CASE10_IMLS}
 
-        curves = hazard_curves(sources, sites, imts, time_span,
-                               gsims, truncation_level)
-        s1hc, s2hc, s3hc, s4hc = curves[test_data.IMT]
-
-        assert_hazard_curve_is(self, s1hc, test_data.SET1_CASE10_SITE1_POES,
-                               atol=1e-4, rtol=1e-1)
-        assert_hazard_curve_is(self, s2hc, test_data.SET1_CASE10_SITE2_POES,
-                               atol=1e-4, rtol=1e-1)
-        assert_hazard_curve_is(self, s3hc, test_data.SET1_CASE10_SITE3_POES,
-                               atol=1e-4, rtol=1e-1)
-        assert_hazard_curve_is(self, s4hc, test_data.SET1_CASE10_SITE4_POES,
-                               atol=1e-4, rtol=1e-1)
-
-    def test_case_11(self):
+        # input data for case 11
         hypocenter_probability = (
             Decimal(1) / len(test_data.SET1_CASE11_HYPOCENTERS)
         )
@@ -90,7 +78,7 @@ class Set1TestCase(unittest.TestCase):
             for hypocenter in test_data.SET1_CASE11_HYPOCENTERS
         ])
         # apart from hypocenter pmf repeats case 10
-        sources = [AreaSource(source_id='area', name='area',
+        self.sources_case11 = [AreaSource(source_id='area', name='area',
             tectonic_region_type=const.TRT.ACTIVE_SHALLOW_CRUST,
             mfd=test_data.SET1_CASE11_MFD,
             nodal_plane_distribution=PMF([(1, NodalPlane(0.0, 90.0, 0.0))]),
@@ -104,30 +92,14 @@ class Set1TestCase(unittest.TestCase):
             rupture_mesh_spacing=10.0,
             temporal_occurrence_model=PoissonTOM(1.)
         )]
-        sites = SiteCollection([
+        self.sites_case11 = SiteCollection([
             test_data.SET1_CASE11_SITE1, test_data.SET1_CASE11_SITE2,
             test_data.SET1_CASE11_SITE3, test_data.SET1_CASE11_SITE4
         ])
-        gsims = {const.TRT.ACTIVE_SHALLOW_CRUST: SadighEtAl1997()}
-        truncation_level = 0
-        time_span = 1.0
-        imts = {test_data.IMT: test_data.SET1_CASE11_IMLS}
+        self.imts_case11 = {test_data.IMT: test_data.SET1_CASE11_IMLS}
 
-        curves = hazard_curves(sources, sites, imts, time_span,
-                               gsims, truncation_level)
-        s1hc, s2hc, s3hc, s4hc = curves[test_data.IMT]
-
-        assert_hazard_curve_is(self, s1hc, test_data.SET1_CASE11_SITE1_POES,
-                               atol=1e-4, rtol=1e-1)
-        assert_hazard_curve_is(self, s2hc, test_data.SET1_CASE11_SITE2_POES,
-                               atol=1e-4, rtol=1e-1)
-        assert_hazard_curve_is(self, s3hc, test_data.SET1_CASE11_SITE3_POES,
-                               atol=1e-4, rtol=1e-1)
-        assert_hazard_curve_is(self, s4hc, test_data.SET1_CASE11_SITE4_POES,
-                               atol=1e-4, rtol=1e-1)
-
-    def test_case_2(self):
-        sources = [SimpleFaultSource(source_id='fault1', name='fault1',
+        # input data for case 2
+        self.sources_case2 = [SimpleFaultSource(source_id='fault1', name='fault1',
             tectonic_region_type=const.TRT.ACTIVE_SHALLOW_CRUST,
             mfd=test_data.SET1_CASE2_MFD,
             rupture_mesh_spacing=1.0,
@@ -140,19 +112,91 @@ class Set1TestCase(unittest.TestCase):
             rake=test_data.SET1_CASE1TO9_RAKE,
             temporal_occurrence_model=PoissonTOM(1.)
         )]
-        sites = SiteCollection([
+        self.sites_case2and5 = SiteCollection([
             test_data.SET1_CASE1TO9_SITE1, test_data.SET1_CASE1TO9_SITE2,
             test_data.SET1_CASE1TO9_SITE3, test_data.SET1_CASE1TO9_SITE4,
             test_data.SET1_CASE1TO9_SITE5, test_data.SET1_CASE1TO9_SITE6,
             test_data.SET1_CASE1TO9_SITE7
         ])
-        gsims = {const.TRT.ACTIVE_SHALLOW_CRUST: SadighEtAl1997()}
-        truncation_level = 0
-        time_span = 1.0
-        imts = {test_data.IMT: test_data.SET1_CASE2_IMLS}
+        self.imts_case2 = {test_data.IMT: test_data.SET1_CASE2_IMLS}
 
-        curves = hazard_curves(sources, sites, imts, time_span,
-                               gsims, truncation_level)
+        # input data for case 5
+        self.sources_case5 = [SimpleFaultSource(source_id='fault1', name='fault1',
+            tectonic_region_type=const.TRT.ACTIVE_SHALLOW_CRUST,
+            mfd=test_data.SET1_CASE5_MFD,
+            rupture_mesh_spacing=1.0,
+            magnitude_scaling_relationship=PeerMSR(),
+            rupture_aspect_ratio=test_data.SET1_RUPTURE_ASPECT_RATIO,
+            upper_seismogenic_depth=test_data.SET1_CASE1TO9_UPPER_SEISMOGENIC_DEPTH,
+            lower_seismogenic_depth=test_data.SET1_CASE1TO9_LOWER_SEISMOGENIC_DEPTH,
+            fault_trace=test_data.SET1_CASE1TO9_FAULT_TRACE,
+            dip=test_data.SET1_CASE1TO9_DIP,
+            rake=test_data.SET1_CASE1TO9_RAKE,
+            temporal_occurrence_model=PoissonTOM(1.)
+        )]
+        self.imts_case5 = {test_data.IMT: test_data.SET1_CASE5_IMLS}
+
+    def test_case_10_poissonian(self):
+        curves = hazard_curves_poissonian(self.sources_case10,
+            self.sites_case10, self.imts_case10, self.time_span,
+            self.gsims, self.truncation_level)
+        s1hc, s2hc, s3hc, s4hc = curves[test_data.IMT]
+
+        assert_hazard_curve_is(self, s1hc, test_data.SET1_CASE10_SITE1_POES,
+                               atol=1e-4, rtol=1e-1)
+        assert_hazard_curve_is(self, s2hc, test_data.SET1_CASE10_SITE2_POES,
+                               atol=1e-4, rtol=1e-1)
+        assert_hazard_curve_is(self, s3hc, test_data.SET1_CASE10_SITE3_POES,
+                               atol=1e-4, rtol=1e-1)
+        assert_hazard_curve_is(self, s4hc, test_data.SET1_CASE10_SITE4_POES,
+                               atol=1e-4, rtol=1e-1)
+
+    def test_case_10(self):
+        curves = hazard_curves(self.sources_case10, self.sites_case10,
+            self.imts_case10, self.gsims, self.truncation_level)
+        s1hc, s2hc, s3hc, s4hc = curves[test_data.IMT]
+
+        assert_hazard_curve_is(self, s1hc, test_data.SET1_CASE10_SITE1_POES,
+                               atol=1e-4, rtol=1e-1)
+        assert_hazard_curve_is(self, s2hc, test_data.SET1_CASE10_SITE2_POES,
+                               atol=1e-4, rtol=1e-1)
+        assert_hazard_curve_is(self, s3hc, test_data.SET1_CASE10_SITE3_POES,
+                               atol=1e-4, rtol=1e-1)
+        assert_hazard_curve_is(self, s4hc, test_data.SET1_CASE10_SITE4_POES,
+                               atol=1e-4, rtol=1e-1)
+
+    def test_case_11_poissonian(self):
+        curves = hazard_curves_poissonian(self.sources_case11,
+            self.sites_case11, self.imts_case11, self.time_span, self.gsims,
+            self.truncation_level)
+        s1hc, s2hc, s3hc, s4hc = curves[test_data.IMT]
+
+        assert_hazard_curve_is(self, s1hc, test_data.SET1_CASE11_SITE1_POES,
+                               atol=1e-4, rtol=1e-1)
+        assert_hazard_curve_is(self, s2hc, test_data.SET1_CASE11_SITE2_POES,
+                               atol=1e-4, rtol=1e-1)
+        assert_hazard_curve_is(self, s3hc, test_data.SET1_CASE11_SITE3_POES,
+                               atol=1e-4, rtol=1e-1)
+        assert_hazard_curve_is(self, s4hc, test_data.SET1_CASE11_SITE4_POES,
+                               atol=1e-4, rtol=1e-1)
+
+    def test_case_11(self):
+        curves = hazard_curves(self.sources_case11, self.sites_case11,
+            self.imts_case11, self.gsims, self.truncation_level)
+        s1hc, s2hc, s3hc, s4hc = curves[test_data.IMT]
+
+        assert_hazard_curve_is(self, s1hc, test_data.SET1_CASE11_SITE1_POES,
+                               atol=1e-4, rtol=1e-1)
+        assert_hazard_curve_is(self, s2hc, test_data.SET1_CASE11_SITE2_POES,
+                               atol=1e-4, rtol=1e-1)
+        assert_hazard_curve_is(self, s3hc, test_data.SET1_CASE11_SITE3_POES,
+                               atol=1e-4, rtol=1e-1)
+        assert_hazard_curve_is(self, s4hc, test_data.SET1_CASE11_SITE4_POES,
+                               atol=1e-4, rtol=1e-1)
+
+    def test_case_2_poissonian(self):
+        curves = hazard_curves_poissonian(self.sources_case2, self.sites_case2and5,
+            self.imts_case2, self.time_span, self.gsims, self.truncation_level)
         s1hc, s2hc, s3hc, s4hc, s5hc, s6hc, s7hc = curves[test_data.IMT]
 
         assert_hazard_curve_is(self, s1hc, test_data.SET1_CASE2_SITE1_POES,
@@ -170,34 +214,50 @@ class Set1TestCase(unittest.TestCase):
         assert_hazard_curve_is(self, s7hc, test_data.SET1_CASE2_SITE7_POES,
                                atol=2e-5, rtol=1e-5)
 
-    def test_case_5(self):
-        # only mfd differs from case 2
-        sources = [SimpleFaultSource(source_id='fault1', name='fault1',
-            tectonic_region_type=const.TRT.ACTIVE_SHALLOW_CRUST,
-            mfd=test_data.SET1_CASE5_MFD,
-            rupture_mesh_spacing=1.0,
-            magnitude_scaling_relationship=PeerMSR(),
-            rupture_aspect_ratio=test_data.SET1_RUPTURE_ASPECT_RATIO,
-            upper_seismogenic_depth=test_data.SET1_CASE1TO9_UPPER_SEISMOGENIC_DEPTH,
-            lower_seismogenic_depth=test_data.SET1_CASE1TO9_LOWER_SEISMOGENIC_DEPTH,
-            fault_trace=test_data.SET1_CASE1TO9_FAULT_TRACE,
-            dip=test_data.SET1_CASE1TO9_DIP,
-            rake=test_data.SET1_CASE1TO9_RAKE,
-            temporal_occurrence_model=PoissonTOM(1.)
-        )]
-        sites = SiteCollection([
-            test_data.SET1_CASE1TO9_SITE1, test_data.SET1_CASE1TO9_SITE2,
-            test_data.SET1_CASE1TO9_SITE3, test_data.SET1_CASE1TO9_SITE4,
-            test_data.SET1_CASE1TO9_SITE5, test_data.SET1_CASE1TO9_SITE6,
-            test_data.SET1_CASE1TO9_SITE7
-        ])
-        gsims = {const.TRT.ACTIVE_SHALLOW_CRUST: SadighEtAl1997()}
-        truncation_level = 0
-        time_span = 1.0
-        imts = {test_data.IMT: test_data.SET1_CASE5_IMLS}
+    def test_case_2(self):
+        curves = hazard_curves(self.sources_case2, self.sites_case2and5,
+            self.imts_case2, self.gsims, self.truncation_level)
+        s1hc, s2hc, s3hc, s4hc, s5hc, s6hc, s7hc = curves[test_data.IMT]
 
-        curves = hazard_curves(sources, sites, imts, time_span,
-                               gsims, truncation_level)
+        assert_hazard_curve_is(self, s1hc, test_data.SET1_CASE2_SITE1_POES,
+                               atol=3e-3, rtol=1e-5)
+        assert_hazard_curve_is(self, s2hc, test_data.SET1_CASE2_SITE2_POES,
+                               atol=2e-5, rtol=1e-5)
+        assert_hazard_curve_is(self, s3hc, test_data.SET1_CASE2_SITE3_POES,
+                               atol=2e-5, rtol=1e-5)
+        assert_hazard_curve_is(self, s4hc, test_data.SET1_CASE2_SITE4_POES,
+                               atol=1e-3, rtol=1e-5)
+        assert_hazard_curve_is(self, s5hc, test_data.SET1_CASE2_SITE5_POES,
+                               atol=1e-3, rtol=1e-5)
+        assert_hazard_curve_is(self, s6hc, test_data.SET1_CASE2_SITE6_POES,
+                               atol=1e-3, rtol=1e-5)
+        assert_hazard_curve_is(self, s7hc, test_data.SET1_CASE2_SITE7_POES,
+                               atol=2e-5, rtol=1e-5)
+
+    def test_case_5_poissonian(self):
+        curves = hazard_curves_poissonian(self.sources_case5,
+            self.sites_case2and5, self.imts_case5, self.time_span, self.gsims,
+            self.truncation_level)
+        s1hc, s2hc, s3hc, s4hc, s5hc, s6hc, s7hc = curves[test_data.IMT]
+
+        assert_hazard_curve_is(self, s1hc, test_data.SET1_CASE5_SITE1_POES,
+                               atol=1e-3, rtol=1e-5)
+        assert_hazard_curve_is(self, s2hc, test_data.SET1_CASE5_SITE2_POES,
+                               atol=1e-3, rtol=1e-5)
+        assert_hazard_curve_is(self, s3hc, test_data.SET1_CASE5_SITE3_POES,
+                               atol=1e-3, rtol=1e-5)
+        assert_hazard_curve_is(self, s4hc, test_data.SET1_CASE5_SITE4_POES,
+                               atol=1e-3, rtol=1e-5)
+        assert_hazard_curve_is(self, s5hc, test_data.SET1_CASE5_SITE5_POES,
+                               atol=1e-3, rtol=1e-5)
+        assert_hazard_curve_is(self, s6hc, test_data.SET1_CASE5_SITE6_POES,
+                               atol=1e-3, rtol=1e-5)
+        assert_hazard_curve_is(self, s7hc, test_data.SET1_CASE5_SITE7_POES,
+                               atol=1e-3, rtol=1e-5)
+
+    def test_case_5(self):
+        curves = hazard_curves(self.sources_case5, self.sites_case2and5,
+            self.imts_case5, self.gsims, self.truncation_level)
         s1hc, s2hc, s3hc, s4hc, s5hc, s6hc, s7hc = curves[test_data.IMT]
 
         assert_hazard_curve_is(self, s1hc, test_data.SET1_CASE5_SITE1_POES,
