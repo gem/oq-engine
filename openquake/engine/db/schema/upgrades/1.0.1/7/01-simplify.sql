bf55a83ae10965c91b9dfb96166d3c59bdbdf628
Luigi Panzeri <defmacro@gmail.com>
2013-11-02 18:02:59 +0100

    fix migration script
    Former-commit-id: 02e1b8174268512f3edeece046fc2ae8d867d057 [formerly 428902c2e5992c7230f759be5d13fef8ccc561b9] [formerly 428902c2e5992c7230f759be5d13fef8ccc561b9 [formerly a2d7bb2876e0f95d7aee5d0ca71a081448e4be37]]
    Former-commit-id: 30f7b7bfd1761b1769f5e40842d2475c71159fd6
    Former-commit-id: cc0330c7f8acacac0cc3c344a6d1622d87bc3c0d

--- openquake/engine/db/schema/upgrades/1.0.1/7/01-simplify.sql
+++ openquake/engine/db/schema/upgrades/1.0.1/7/01-simplify.sql
@@ -1,5 +1,6 @@
--- OQ_USER
+--- Drop oq_user. ADD user_name to oq_job
 ALTER TABLE uiapi.oq_job ADD user_name VARCHAR;
+CREATE INDEX uiapi_oq_job_user_name_idx ON uiapi.oq_job(user_name);
 UPDATE uiapi.oq_job SET user_name = (SELECT user_name FROM admin.oq_user WHERE id=uiapi.oq_job.owner_id);
 ALTER TABLE uiapi.oq_job ALTER user_name SET NOT NULL;
 ALTER TABLE uiapi.risk_calculation DROP owner_id;
@@ -10,79 +11,86 @@ ALTER TABLE uiapi.output DROP owner_id;
 DROP TRIGGER admin_oq_user_refresh_last_update_trig ON admin.oq_user;
 DROP TABLE admin.oq_user;
 
--- OQ_ORGANIZATION
+--- Drop organization
 DROP TRIGGER admin_organization_refresh_last_update_trig ON admin.organization;
 DROP TABLE admin.organization;
 
 
--- OQ_INPUT
+--- Drop oq_input. ADD job_id in each table an input_id is referenced
+
+-- site_model
 ALTER TABLE hzrdi.site_model ADD job_id INTEGER NULL;
+CREATE INDEX hzrdi_site_model_job_id_idx ON hzrdi.site_model(job_id);
 UPDATE hzrdi.site_model SET job_id=(
        SELECT job.id FROM uiapi.oq_job AS job
        JOIN uiapi.hazard_calculation AS hc ON hc.id = job.hazard_calculation_id
        JOIN uiapi.input2hcalc AS i2h ON i2h.hazard_calculation_id = hc.id
-       WHERE i2h.input_id = input_id) WHERE job_id IS NULL;
+       WHERE i2h.input_id = hzrdi.site_model.input_id) WHERE job_id IS NULL;
+ALTER TABLE hzrdi.site_model ADD CONSTRAINT hzrdi_site_model_job_fk
+FOREIGN KEY (job_id) REFERENCES uiapi.oq_job(id) ON DELETE RESTRICT;
 ALTER TABLE hzrdi.site_model DROP input_id;
 
-ALTER TABLE hzrdi.site_model ADD CONSTRAINT hzrdi_site_model_job_fk
-FOREIGN KEY (job_id) REFERENCES uiapi.job(id) ON DELETE RESTRICT;
-
+-- parsed_source
 ALTER TABLE hzrdi.parsed_source ADD job_id INTEGER NULL;
 UPDATE hzrdi.parsed_source SET job_id=(
        SELECT job.id FROM uiapi.oq_job AS job
        JOIN uiapi.hazard_calculation AS hc ON hc.id = job.hazard_calculation_id
        JOIN uiapi.input2hcalc AS i2h ON i2h.hazard_calculation_id = hc.id
-       WHERE i2h.input_id = input_id) WHERE job_id IS NULL;
+       WHERE i2h.input_id = hzrdi.parsed_source.input_id) WHERE job_id IS NULL;
+ALTER TABLE hzrdi.parsed_source ADD CONSTRAINT hzrdi_parsed_source_job_fk
+FOREIGN KEY (job_id) REFERENCES uiapi.oq_job(id) ON DELETE RESTRICT;
 ALTER TABLE hzrdi.parsed_source DROP input_id;
 
-ALTER TABLE hzrdi.parsed_source ADD CONSTRAINT hzrdi_parsed_source_job_fk
-FOREIGN KEY (job_id) REFERENCES uiapi.job(id) ON DELETE RESTRICT;
-
-
+-- parsed_rupture_model
 ALTER TABLE hzrdi.parsed_rupture_model ADD job_id INTEGER NULL;
+CREATE INDEX hzrdi_parsed_rupture_model_job_id_idx ON hzrdi.parsed_rupture_model(job_id);
 UPDATE hzrdi.parsed_rupture_model SET job_id=(
        SELECT job.id FROM uiapi.oq_job AS job
        JOIN uiapi.hazard_calculation AS hc ON hc.id = job.hazard_calculation_id
        JOIN uiapi.input2hcalc AS i2h ON i2h.hazard_calculation_id = hc.id
-       WHERE i2h.input_id = input_id) WHERE job_id IS NULL;
+       WHERE i2h.input_id = hzrdi.parsed_rupture_model.input_id)
+WHERE job_id IS NULL;
+ALTER TABLE hzrdi.parsed_rupture_model ADD CONSTRAINT hzrdi_parsed_rupture_model_job_fk
+FOREIGN KEY (job_id) REFERENCES uiapi.oq_job(id) ON DELETE RESTRICT;
 ALTER TABLE hzrdi.parsed_rupture_model DROP input_id;
 
-ALTER TABLE hzrdi.parsed_rupture_model ADD CONSTRAINT hzrdi_parsed_rupture_model_job_fk
-FOREIGN KEY (job_id) REFERENCES uiapi.job(id) ON DELETE RESTRICT;
-
-
+-- risk_calculation. Here we replace exposure_input_id with
+-- preloaded_exposure_model_id
 ALTER TABLE uiapi.risk_calculation ADD preloaded_exposure_model_id INTEGER;
 UPDATE uiapi.risk_calculation SET preloaded_exposure_model_id=(
        SELECT em.id FROM riski.exposure_model AS em
        WHERE em.input_id = exposure_input_id)
 WHERE exposure_input_id IS NOT NULL;
+
 ALTER TABLE uiapi.risk_calculation DROP exposure_input_id;
 
 ALTER TABLE uiapi.risk_calculation ADD CONSTRAINT uiapi_risk_calculation_exposure_model_fk
 FOREIGN KEY (preloaded_exposure_model_id) REFERENCES riski.exposure_model(id) ON DELETE RESTRICT;
 
-
+-- exposure_model
 ALTER TABLE riski.exposure_model ADD job_id INTEGER NULL;
 UPDATE riski.exposure_model SET job_id=(
        SELECT job.id FROM uiapi.oq_job AS job
        JOIN uiapi.risk_calculation AS rc ON rc.id = job.risk_calculation_id
        JOIN uiapi.input2rcalc AS i2r ON i2r.risk_calculation_id = rc.id
-       WHERE i2r.input_id = input_id) WHERE job_id IS NULL;
-ALTER TABLE riski.exposure_model DROP input_id;
-
+       WHERE i2r.input_id = riski.exposure_model.input_id) WHERE job_id IS NULL;
 ALTER TABLE riski.exposure_model ADD CONSTRAINT riski_exposure_model_job_fk
 FOREIGN KEY (job_id) REFERENCES uiapi.oq_job(id) ON DELETE RESTRICT;
+ALTER TABLE riski.exposure_model DROP input_id;
 
-ALTER TABLE uiapi.hazard_calculation ADD inputs BYTEA;
-ALTER TABLE uiapi.risk_calculation ADD inputs BYTEA;
-
-DROP TABLE uiapi.model_content;
 DROP TABLE uiapi.src2ltsrc;
 DROP TABLE uiapi.input2job;
 DROP TABLE uiapi.input2hcalc;
 DROP TABLE uiapi.input2rcalc;
-
 DROP TABLE uiapi.input;
 
-
+--- Drop error_msg
 DROP TABLE uiapi.error_msg;
+
+
+--- Drop model_content
+
+--- Add inputs to hazard and risk calculation tables
+ALTER TABLE uiapi.hazard_calculation ADD inputs BYTEA;
+ALTER TABLE uiapi.risk_calculation ADD inputs BYTEA;
+
