0f8f0a816e7c6f704a2fc855d62aab3210c52d39
Luigi Panzeri <defmacro@gmail.com>
2013-11-03 21:04:43 +0100

    added new qa test and migration script
    Former-commit-id: 332a31f9b5e6dad4c7a66074bd9a1588d2cf899e [formerly 2f128d5f4aa725230acb808cf1ce93174a54ca0d] [formerly 2f128d5f4aa725230acb808cf1ce93174a54ca0d [formerly 050dac12e06257ed5eeb250bb3057b1c510a23a6]]
    Former-commit-id: 4791b42460c3cdb08b92f5ec0ba5bfaa62863605
    Former-commit-id: 90a74af90cfc0fcbf5b96c77bad3eb2daf2f7459

---
+++ qa_tests/risk/event_based/run_from_dump_restore/test.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2013, GEM Foundation.
+#
+# OpenQuake is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# OpenQuake is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from nose.plugins.attrib import attr as noseattr
+from qa_tests import risk
+
+
+class EventBaseDumpRestoreSESTestCase(
+        risk.CompleteTestCase, risk.FixtureBasedQATestCase):
+    hazard_calculation_fixture = ("QA (regression) test for Risk Event "
+                                  "Based from Stochastic Event Set")
+    dump_restore = True
+
+    @noseattr('qa', 'risk', 'event_based')
+    def test(self):
+        expected_losses = [
+            69.0103214562, 28.6348740356, 7.03106365373, 6.13903150308,
+            5.46296324231, 2.62305127604, 2.27520725638, 1.97496927237,
+            1.07155984657, 0.684801646576, 0.6614696242, 0.413749931498,
+            0.111114467633, 0.0817185722631, 0.0657068428625, 0.0229156239494]
+
+        outputs = self._run_test().output_set
+        losses = outputs.get(output_type="event_loss").event_loss
+
+        for event_loss, expected in zip(losses, expected_losses):
+            self.assertAlmostEqual(
+                expected, event_loss.aggregate_loss,
+                msg="loss for rupture %r is %s (expected %s)" % (
+                    event_loss.rupture.tag, event_loss.aggregate_loss,
+                    expected))
