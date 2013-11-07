2d706a465647ab727550e064b150bd366a119cb6
Michele Simionato <michele@openquake.org>
2013-11-07 15:20:22 +0100

    Now the engine records the disk space occupation along with the job_stats
    Former-commit-id: 26e371a1ad70de495f37ec97db5362e90f95fd38 [formerly ba89b332d7f04de8d770ee5be6d1229df3d14685] [formerly ba89b332d7f04de8d770ee5be6d1229df3d14685 [formerly 3adcaf228eeb881f0778b7aaad91365a8defbb5c]]
    Former-commit-id: 3f853b5fb01a696052534c0604d899f545010499
    Former-commit-id: 7a440422c10b536d01d64a978a7d9377ce19b393

--- openquake/engine/calculators/hazard/event_based/core.py
+++ openquake/engine/calculators/hazard/event_based/core.py
@@ -539,8 +539,6 @@ class EventBasedHazardCalculator(haz_general.BaseHazardCalculator):
         if self.job.hazard_calculation.complete_logic_tree_ses:
             self.initialize_complete_lt_ses_db_records()
 
-        self.record_init_stats()
-
         num_sources = models.SourceProgress.objects.filter(
             is_complete=False,
             lt_realization__hazard_calculation=self.hc).count()
