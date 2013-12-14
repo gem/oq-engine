46fda8f146dfd41b4a84a3a687deb851c3057c00
Michele Simionato <michele.simionato@gmail.com>
2013-12-14 09:34:22 +0100

    Save the num_sources info inside job_stats

--- openquake/engine/db/schema/upgrades/1.0.1/11/01-change-ses_collection.sql
+++ openquake/engine/db/schema/upgrades/1.0.1/11/01-change-ses_collection.sql
@@ -3,3 +3,5 @@ ALTER TABLE hzrdr.ses ALTER COLUMN ordinal SET NOT NULL;
 ALTER TABLE hzrdr.lt_realization DROP COLUMN is_complete;
 ALTER TABLE hzrdr.lt_realization DROP COLUMN total_items;
 ALTER TABLE hzrdr.lt_realization DROP COLUMN completed_items;
+
+ALTER TABLE uiapi.job_stats ADD COLUMN num_sources INTEGER[];
