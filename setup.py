de073bf6f66340ad2cab4c3328382cee76b53818
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2014-01-27 16:51:14 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,70 @@
+# input_tool: The Input Tool
+# Copyright (C) 2014 GEM Foundation
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import os
+import re
+import sys
+from setuptools import setup, find_packages
+
+
+def get_version():
+    version_re = r"^__version__\s+=\s+['\"]([^'\"]*)['\"]"
+    version = None
+
+    package_init = 'openquake/__init__.py'
+    for line in open(package_init, 'r'):
+        version_match = re.search(version_re, line, re.M)
+        if version_match:
+            version = version_match.group(1)
+            break
+    else:
+        sys.exit('__version__ variable not found in %s' % package_init)
+
+    return version
+version = get_version()
+
+url = "http://github.com/gem/oq-common"
+cd = os.path.dirname(os.path.join(__file__))
+
+setup(
+    name='oq-common',
+    version=version,
+    description="oq-common",
+    long_description=__doc__,
+    url=url,
+    packages=find_packages(exclude=[]),
+    install_requires=[
+        'openquake.risklib',
+        'openquake.nrmllib',
+    ],
+    maintainer='GEM',
+    maintainer_email='info@openquake.org',
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Scientific/Engineering',
+    ),
+    keywords="seismic risk",
+    license="GNU AGPL v3",
+    platforms=["any"],
+    scripts=[],
+    namespace_packages=[],
+
+    zip_safe=False,
+)
