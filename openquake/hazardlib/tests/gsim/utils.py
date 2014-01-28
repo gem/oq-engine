366a7d2fb8ab682087314a42b608b7c5ce27bb66
Damiano Monelli <damiano.monelli@globalquakemodel.org>
2014-01-28 14:47:57 +0100

    BaseGSIMTestCase now checks that required parameters as defined in tables match exactly parameters defined in GSIM object

--- openquake/hazardlib/tests/gsim/utils.py
+++ openquake/hazardlib/tests/gsim/utils.py
@@ -15,6 +15,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import unittest
 import os
+import inspect
 
 from openquake.hazardlib.tests.gsim.check_gsim import check_gsim
 
@@ -23,11 +24,27 @@ class BaseGSIMTestCase(unittest.TestCase):
     BASE_DATA_PATH = os.path.join(os.path.dirname(__file__), 'data')
     GSIM_CLASS = None
 
+    def get_context_attributes(self, ctx):
+        att = inspect.getmembers(ctx, lambda a: not(inspect.isroutine(a)))
+        att = [
+            k for k, v in att if not (k.startswith('__') and k.endswith('__'))
+        ]
+
+        return set(att)
+
     def check(self, filename, max_discrep_percentage):
         assert self.GSIM_CLASS is not None
         filename = os.path.join(self.BASE_DATA_PATH, filename)
-        errors, stats = check_gsim(self.GSIM_CLASS, open(filename),
-                                   max_discrep_percentage)
+        errors, stats, sctx, rctx, dctx = check_gsim(
+            self.GSIM_CLASS, open(filename),
+            max_discrep_percentage
+        )
+        s_att = self.get_context_attributes(sctx)
+        r_att = self.get_context_attributes(rctx)
+        d_att = self.get_context_attributes(dctx)
+        self.assertEqual(self.GSIM_CLASS.REQUIRES_SITES_PARAMETERS, s_att)
+        self.assertEqual(self.GSIM_CLASS.REQUIRES_RUPTURE_PARAMETERS, r_att)
+        self.assertEqual(self.GSIM_CLASS.REQUIRES_DISTANCES, d_att)
         if errors:
             raise AssertionError(stats)
         print
