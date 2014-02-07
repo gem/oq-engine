aa8e5addd6cb0329d8b49029d214e90778e20503
Michele Simionato <michele.simionato@gmail.com>
2014-02-07 16:25:39 +0100

    Fixed the migration script
    Former-commit-id: d021f1fc3be506bdcc4126312d235656a5bf2080 [formerly d021f1fc3be506bdcc4126312d235656a5bf2080 [formerly dc0d3ae2f894653ef6936f066cded0251a4d95ca]]
    Former-commit-id: 383ef23d33db1c4074cc9b969e6f633486e966db
    Former-commit-id: ba99181b07bdc42930d07d9946aa6ebd593e76c4

--- openquake/engine/db/schema/upgrades/1.0.1/14/01-migrate-ses-collections.sql
+++ openquake/engine/db/schema/upgrades/1.0.1/14/01-migrate-ses-collections.sql
@@ -1,10 +1,11 @@
-ALTER TABLE hzrdr.ses_collection ADD COLUMN lt_realizations INTEGER[];
+ALTER TABLE hzrdr.ses_collection ADD COLUMN lt_realization_ids INTEGER[];
 ALTER TABLE hzrdr.ses_collection ADD COLUMN ordinal INTEGER;
 
 UPDATE hzrdr.ses_collection
 SET lt_realization_ids=ARRAY[lt_realization_id], ordinal=lt_realization_id;
 
-ALTER TABLE hzrdr.ses_collection ALTER COLUMN lt_realization_ids NOT NULL;
-ALTER TABLE hzrdr.ses_collection ALTER COLUMN ordinal NOT NULL;
+ALTER TABLE hzrdr.ses_collection ALTER COLUMN lt_realization_ids SET NOT NULL;
+
+ALTER TABLE hzrdr.ses_collection ALTER COLUMN ordinal SET NOT NULL;
 
 ALTER TABLE hzrdr.ses_collection DROP COLUMN lt_realization_id;
