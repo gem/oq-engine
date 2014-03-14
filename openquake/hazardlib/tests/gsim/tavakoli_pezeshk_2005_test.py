eeebb63a1886ffc9e1e93d4fd9d0cc204cda8a95
Damiano Monelli <damiano.monelli@globalquakemodel.org>
2014-03-14 15:32:12 +0100

    Tavakoli an dPezeshk 2005 with Johnston 1996 conversion equation and with Mw

--- openquake/hazardlib/tests/gsim/tavakoli_pezeshk_2005_test.py
+++ openquake/hazardlib/tests/gsim/tavakoli_pezeshk_2005_test.py
@@ -13,19 +13,38 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-from openquake.hazardlib.gsim.tavakoli_pezeshk_2005 import \
-    TavakoliPezeshk2005NSHMP2008
+from openquake.hazardlib.gsim.tavakoli_pezeshk_2005 import (
+    TavakoliPezeshk2005MblgAB1987NSHMP2008,
+    TavakoliPezeshk2005MblgJ1996NSHMP2008,
+    TavakoliPezeshk2005MwNSHMP2008
+)
 
 from openquake.hazardlib.tests.gsim.utils import BaseGSIMTestCase
 
 
-class TavakoliPezeshk2005NSHMP20088TestCase(BaseGSIMTestCase):
-    GSIM_CLASS = TavakoliPezeshk2005NSHMP2008
+class TavakoliPezeshk2005MblgAB1987NSHMP2008TestCase(BaseGSIMTestCase):
+    GSIM_CLASS = TavakoliPezeshk2005MblgAB1987NSHMP2008
 
-    def test_mean_normal(self):
-        self.check('TP05/TP05usgs_MEAN.csv',
-                   max_discrep_percentage=1.1)
+    def test_mean(self):
+        self.check('TP05/TP05MblgAB1987NSHMP2008_MEAN.csv',
+                   max_discrep_percentage=0.4)
 
     def test_std_total(self):
-        self.check('TP05/TP05usgs_STD_TOT.csv',
-                   max_discrep_percentage=0.5)
+        self.check('TP05/TP05MblgAB1987NSHMP2008_STD_TOTAL.csv',
+                   max_discrep_percentage=0.4)
+
+
+class TavakoliPezeshk2005MblgJ1996NSHMP2008TestCase(BaseGSIMTestCase):
+    GSIM_CLASS = TavakoliPezeshk2005MblgJ1996NSHMP2008
+
+    def test_mean(self):
+        self.check('TP05/TP05MblgJ1996NSHMP2008_MEAN.csv',
+                   max_discrep_percentage=0.4)
+
+
+class TavakoliPezeshk2005MwNSHMP2008TestCase(BaseGSIMTestCase):
+    GSIM_CLASS = TavakoliPezeshk2005MwNSHMP2008
+
+    def test_mean(self):
+        self.check('TP05/TP05MwNSHMP2008_MEAN.csv',
+                   max_discrep_percentage=0.4)
