b2aa4be6dc2e32df30870836a96d1ecd057f6840
Michele Simionato <michele.simionato@gmail.com>
2014-03-26 09:49:11 +0100

    Added a comment explaining the need for the DELETE duplicates query
    Former-commit-id: dddfd62162ea049b8fb1cdb96d39f27f303c54e4 [formerly 17a1de692402924beffd2b96b1221135806c423d]
    Former-commit-id: e3b3c065f16cefb1fa682c71dbbac78a2080b832

--- openquake/engine/db/schema/upgrades/1.0.1/19/01-ses_rupture_unique.sql
+++ openquake/engine/db/schema/upgrades/1.0.1/19/01-ses_rupture_unique.sql
@@ -1,8 +1,21 @@
--- remove duplicates, if any
+/*
+A bug was introduced in march 2014 with the refactoring to remove
+the ruptures as pickled objects. This is why duplicated rupture
+tags may appear in the ses_rupture table. If this upgrade script
+fails, just remove the duplicates with the following query:
+
 DELETE FROM hzrdr.ses_rupture AS x USING (
    SELECT ses_id, tag FROM hzrdr.ses_rupture
    GROUP BY ses_id, tag HAVING count(*) > 1) AS y
 WHERE x.ses_id=y.ses_id AND x.tag=y.tag;
+
+This is the only viable approach, since if you have duplicated tags
+in the database the risk results are not reliable, depending on the
+random order the ruptures are extracted from the database.
+
+Since we not want to delete data from the user database without
+warning, the query is commented and must be executed manually.
+*/
 
 CREATE UNIQUE INDEX hzrdr_ses_rupture_tag_uniq_idx
 ON hzrdr.ses_rupture(ses_id, tag);
