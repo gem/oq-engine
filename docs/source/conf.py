f250a0151b0ceb962f06ec29e3a8cae687909d5a
Daniele Vigano <daniele@vigano.me>
2014-03-27 16:56:08 +0100

    Updates Copyright to 2014 in more files
    Former-commit-id: 68353d5001cf62f29a1d214e7dd5593303e0c614

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -71,7 +71,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'OpenQuake Engine'
-copyright = u'2013, GEM Foundation'
+copyright = u'2014, GEM Foundation'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
