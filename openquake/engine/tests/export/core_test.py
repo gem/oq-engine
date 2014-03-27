68353d5001cf62f29a1d214e7dd5593303e0c614
Daniele Vigano <daniele@vigano.me>
2014-03-27 16:56:08 +0100

    Updates Copyright to 2014 in more files

--- openquake/engine/tests/export/core_test.py
+++ openquake/engine/tests/export/core_test.py
@@ -1,5 +1,5 @@
 
-# Copyright (c) 2010-2012, GEM Foundation.
+# Copyright (c) 2010-2014, GEM Foundation.
 #
 # OpenQuake is free software: you can redistribute it and/or modify it
 # under the terms of the GNU Affero General Public License as published
