d4d3040a31ca81a6d18120d40efd59e3acf6f1bb
Michele Simionato <michele.simionato@gmail.com>
2014-04-07 06:26:42 +0200

    Added more logging
    Former-commit-id: 342e908bdd2b393535a1e1c2acb436a5106f05c0 [formerly 0e9150c83b111de73c3f0a8f1fe8119deeb92113]
    Former-commit-id: 096d222a3357a715f745b6bdb920e2a4a741acba

--- openquake/engine/calculators/risk/base.py
+++ openquake/engine/calculators/risk/base.py
@@ -60,13 +60,13 @@ class RiskCalculator(base.Calculator):
             3. Initialize progress counters
             4. Validate exposure and risk models
         """
-        with logs.tracing('get exposure'):
+        with self.monitor('get exposure'):
             self.taxonomies_asset_count = \
                 (self.rc.preloaded_exposure_model or loaders.exposure(
                     self.job, self.rc.inputs['exposure'])
                  ).taxonomies_in(self.rc.region_constraint)
 
-        with logs.tracing('parse risk models'):
+        with self.monitor('parse risk models'):
             self.risk_models = self.get_risk_models()
 
             # consider only the taxonomies in the risk models if
@@ -77,6 +77,12 @@ class RiskCalculator(base.Calculator):
                     (t, count)
                     for t, count in self.taxonomies_asset_count.items()
                     if t in self.risk_models)
+
+        n_assets = sum(self.taxonomies_asset_count.itervalues())
+        logs.LOG.info('Considering %d assets of %d distinct taxonomies',
+                      n_assets, len(self.taxonomies_asset_count))
+        for taxonomy, counts in self.taxonomies_asset_count.iteritems():
+            logs.LOG.info('taxonomy=%s, assets=%d', taxonomy, counts)
 
         for validator_class in self.validators:
             validator = validator_class(self)
