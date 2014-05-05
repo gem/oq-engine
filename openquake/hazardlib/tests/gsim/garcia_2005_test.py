872b6371d1f3be67f9f6706a208a0b7b7f2987fe
jgarcia <julio.garcia@globalquakemodel.org>
2014-05-05 15:21:38 +0200

    fixed error in garcia_2005_test.py and renamed names of verification tables for vertical component case

--- openquake/hazardlib/tests/gsim/garcia_2005_test.py
+++ openquake/hazardlib/tests/gsim/garcia_2005_test.py
@@ -25,41 +25,35 @@ class GarciaEtAl2005SSlabTestCase(BaseGSIMTestCase):
     GSIM_CLASS = GarciaEtAl2005SSlab
 
     # Test data generated from Fortran implementation
-    # provided by Daniel Garcia 
+    # provided by Daniel Garcia
 
     def test_mean(self):
-        self.check('GA05/GA05SSLab_MEAN.csv',
-                    max_discrep_percentage=0.2)
+        self.check('GA05/GA05SSLab_MEAN.csv', max_discrep_percentage=0.2)
 
     def test_std_total(self):
-        self.check('GA05/GA05SSLab_STD_TOTAL.csv',
-                    max_discrep_percentage=0.1)
+        self.check('GA05/GA05SSLab_STD_TOTAL.csv', max_discrep_percentage=0.1)
+
     def test_std_intra(self):
-        self.check('GA05/GA05SSLab_STD_INTRA.csv,
-                  max_discrep_percentage=0.1)
+        self.check('GA05/GA05SSLab_STD_INTRA.csv', max_discrep_percentage=0.1)
 
     def test_std_inter(self):
-        self.check('GA05/GA05SSLab_STD_INTER.csv',
-                  max_discrep_percentage=0.1)
+        self.check('GA05/GA05SSLab_STD_INTER.csv', max_discrep_percentage=0.1)
 
 
 class GarciaEtAl2005SSlabVertTestCase(BaseGSIMTestCase):
     GSIM_CLASS = GarciaEtAl2005SSlabVert
 
     # Test data generated from Fortran implementation
-    # provided by Daniel Garcia 
+    # provided by Daniel Garcia
 
     def test_mean(self):
-        self.check('GA05/GA05SSLab_Vert_MEAN.csv',
-                    max_discrep_percentage=0.1)
+        self.check('GA05/GA05SSLabV_MEAN.csv', max_discrep_percentage=0.2)
 
     def test_std_total(self):
-        self.check('GA05/GA05SSLab_Vert_STD_TOTAL.csv',
-                    max_discrep_percentage=0.1)
+        self.check('GA05/GA05SSLabV_STD_TOTAL.csv', max_discrep_percentage=0.1)
+
     def test_std_intra(self):
-        self.check('GA05/GA05SSLab_Vert_STD_INTRA.csv,
-                  max_discrep_percentage=0.1)
+        self.check('GA05/GA05SSLabV_STD_INTRA.csv', max_discrep_percentage=0.1)
 
     def test_std_inter(self):
-        self.check('GA05/GA05SSLab_Vert_STD_INTER.csv',
-                  max_discrep_percentage=0.1)
+        self.check('GA05/GA05SSLabV_STD_INTER.csv', max_discrep_percentage=0.1)
