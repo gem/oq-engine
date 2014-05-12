e409cfc9a1c9ba8aaacce98d895ed181a5d82d23
Michele Simionato <michele.simionato@gmail.com>
2014-05-12 15:00:17 +0200

    Added a test on complex logic trees
    Former-commit-id: ebdd1de12d63541c9b16901a032bc409be542a29 [formerly 5949c4b9dcfd17cc74e4c88e77f6403a7ecdf04c] [formerly 5949c4b9dcfd17cc74e4c88e77f6403a7ecdf04c [formerly 621dc42a0b1f0398548f7ad60711c76da5112b6d]]
    Former-commit-id: abd70f7e2aaefec6e119a432d5954f3180fdd0f4
    Former-commit-id: 749808d9b3e8fa4dbdec7d07725ea93aab1a06f0

--- qa_tests/_utils.py
+++ qa_tests/_utils.py
@@ -149,14 +149,17 @@ def compare_hazard_curve_with_csv(
         reader = csv.reader(f, delimiter=csv_delimiter)
         expected_data = [map(float, row) for row in reader]
 
-    numpy.testing.assert_allclose(expected_data, data, rtol=rtol)
-    # to debug the test, in case it breaks, comment the assert and
-    # uncomment the following, lines then compare the expected file with
-    # the file generated from the computed data and stored in /tmp:
-    # import os
-    # tmp = os.path.join('/tmp', os.path.basename(csv_name))
-    # print 'saving', tmp
-    # print >> open(tmp, 'w'), '\n'.join(' '.join(map(str, r)) for r in data)
+    try:
+        numpy.testing.assert_allclose(expected_data, data, rtol=rtol)
+    except:
+        # to debug the test, in case it breaks, comment the assert and
+        # uncomment the following, lines then compare the expected file with
+        # the file generated from the computed data and stored in /tmp:
+        #import os
+        #tmp = os.path.join('/tmp', os.path.basename(csv_name))
+        #print 'saving', tmp
+        #print >>open(tmp, 'w'), '\n'.join(' '.join(map(str, r)) for r in data)
+        raise
 
 aac = lambda a, b: numpy.testing.assert_allclose(a, b, atol=1e-5)
 
