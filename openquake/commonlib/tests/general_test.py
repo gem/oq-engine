4a68142a217533a2fede59d0eeac938c53f99a38
Michele Simionato <michele.simionato@gmail.com>
2014-05-14 14:01:17 +0000

    Moved utils/general.py from the engine

---
+++ openquake/commonlib/tests/general_test.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright (c) 2010-2014, GEM Foundation.
+#
+# OpenQuake is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# OpenQuake is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Test related to code in openquake/utils/general.py
+"""
+
+
+import unittest
+
+from openquake.commonlib.general import \
+    block_splitter, SequenceSplitter
+
+class SequenceSplitterTestCase(unittest.TestCase):
+    """Tests for :function:`openquake.engine.utils.general.block_splitter`."""
+
+    DATA = range(10)
+
+    def test_block_splitter(self):
+        expected = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [9],
+        ]
+        actual = [x for x in block_splitter(self.DATA, 3)]
+        self.assertEqual(expected, actual)
+
+    def test_block_splitter_block_size_eq_data_len(self):
+        expected = [self.DATA]
+        actual = [x for x in block_splitter(self.DATA, 10)]
+        self.assertEqual(expected, actual)
+
+    def test_block_splitter_block_size_gt_data_len(self):
+        expected = [self.DATA]
+        actual = [x for x in block_splitter(self.DATA, 11)]
+        self.assertEqual(expected, actual)
+
+    def test_block_splitter_zero_block_size(self):
+        gen = block_splitter(self.DATA, 0)
+        self.assertRaises(ValueError, gen.next)
+
+    def test_block_splitter_block_size_lt_zero(self):
+        gen = block_splitter(self.DATA, -1)
+        self.assertRaises(ValueError, gen.next)
+
+    def test_block_splitter_with_generator(self):
+        # Test the block with a data set of unknown length
+        # (such as a generator)
+        data = xrange(10)
+        expected = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [9],
+        ]
+        actual = [x for x in block_splitter(data, 3)]
+        self.assertEqual(expected, actual)
+
+    def test_block_splitter_with_iter(self):
+        # Test the block with a data set of unknown length
+        data = iter(range(10))
+        expected = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [9],
+        ]
+        actual = [x for x in block_splitter(data, 3)]
+        self.assertEqual(expected, actual)
+
+    def test_weighted_block_splitter(self):
+        # try to generate 4 blocks
+        split_weight = SequenceSplitter(4).split_on_max_weight
+        blocks = split_weight(
+            [('a', 11), ('b', 10), ('c', 100), ('d', 15), ('e', 20),
+             ('f', 5), ('g', 30), ('h', 17), ('i', 25)])
+        self.assertEqual(repr(blocks), "[<WeightedSequence ['a', 'b'], weight=21>, <WeightedSequence ['c'], weight=100>, <WeightedSequence ['d', 'e', 'f'], weight=40>, <WeightedSequence ['g', 'h'], weight=47>, <WeightedSequence ['i'], weight=25>]")
