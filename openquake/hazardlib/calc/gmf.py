688b02fb0fb33c8679b843529f528a2fd62c1089
Michele Simionato <michele.simionato@gmail.com>
2014-05-22 05:05:33 +0200

    Introduced a GmfComputer class

--- openquake/hazardlib/calc/gmf.py
+++ openquake/hazardlib/calc/gmf.py
@@ -24,9 +24,133 @@ from openquake.hazardlib.const import StdDev
 from openquake.hazardlib.calc import filters
 
 
+class GmfComputer(object):
+    """
+    Given an earthquake rupture, the ground motion field computer computes
+    ground shaking over a set of sites, by randomly sampling a ground
+    shaking intensity model.
+
+    :param openquake.hazardlib.source.rupture.Rupture rupture:
+        Rupture to calculate ground motion fields radiated from.
+    :param :class:`openquake.hazardlib.site.SiteCollection` sites:
+        Sites of interest to calculate GMFs.
+    :param imts:
+        List of intensity measure type objects (see
+        :mod:`openquake.hazardlib.imt`).
+    :param gsim:
+        Ground-shaking intensity model, instance of subclass of either
+        :class:`~openquake.hazardlib.gsim.base.GMPE` or
+        :class:`~openquake.hazardlib.gsim.base.IPE`.
+    :param truncation_level:
+        Float, number of standard deviations for truncation of the intensity
+        distribution, or ``None``.
+    :param correlation_model:
+        Instance of correlation model object. See
+        :mod:`openquake.hazardlib.correlation`. Can be ``None``, in which
+        case non-correlated ground motion fields are calculated.
+        Correlation model is not used if ``truncation_level`` is zero.
+    """
+    def __init__(self, rupture, sites, imts, gsim, truncation_level,
+                 correlation_model=None):
+        self.rupture = rupture
+        self.sites = sites
+        self.imts = imts
+        self.gsim = gsim
+        self.truncation_level = truncation_level
+        self.correlation_model = correlation_model
+        self.sctx, self.rctx, self.dctx = gsim.make_contexts(sites, rupture)
+
+    def _compute(self, seed, realizations):
+        # the method doing the real stuff; use compute instead
+        if seed is not None:
+            numpy.random.seed(seed)
+        result = {}
+
+        if self.truncation_level == 0:
+            assert self.correlation_model is None
+            for imt in self.imts:
+                mean, _stddevs = self.gsim.get_mean_and_stddevs(
+                    self.sctx, self.rctx, self.dctx, imt, stddev_types=[])
+                mean = self.gsim.to_imt_unit_values(mean)
+                mean.shape += (1, )
+                mean = mean.repeat(realizations, axis=1)
+                result[imt] = mean
+            return result
+        elif self.truncation_level is None:
+            distribution = scipy.stats.norm()
+        else:
+            assert self.truncation_level > 0
+            distribution = scipy.stats.truncnorm(
+                - self.truncation_level, self.truncation_level)
+
+        for imt in self.imts:
+            if self.gsim.DEFINED_FOR_STANDARD_DEVIATION_TYPES == \
+               set([StdDev.TOTAL]):
+                # If the GSIM provides only total standard deviation, we need
+                # to compute only mean and total standard deviation at the sites
+                # of interest.
+                # In this case, we also assume no correlation model is used.
+                assert self.correlation_model is None
+
+                mean, [stddev_total] = self.gsim.get_mean_and_stddevs(
+                    self.sctx, self.rctx, self.dctx, imt, [StdDev.TOTAL]
+                )
+                stddev_total = stddev_total.reshape(stddev_total.shape + (1, ))
+                mean = mean.reshape(mean.shape + (1, ))
+
+                total_residual = stddev_total * distribution.rvs(
+                    size=(len(self.sites), realizations)
+                )
+                gmf = self.gsim.to_imt_unit_values(mean + total_residual)
+            else:
+                mean, [stddev_inter, stddev_intra] = \
+                self.gsim.get_mean_and_stddevs(
+                    self.sctx, self.rctx, self.dctx, imt,
+                    [StdDev.INTER_EVENT, StdDev.INTRA_EVENT]
+                )
+                stddev_intra = stddev_intra.reshape(stddev_intra.shape + (1, ))
+                stddev_inter = stddev_inter.reshape(stddev_inter.shape + (1, ))
+                mean = mean.reshape(mean.shape + (1, ))
+
+                intra_residual = stddev_intra * distribution.rvs(
+                    size=(len(self.sites), realizations)
+                )
+
+                if self.correlation_model is not None:
+                    intra_residual = self.correlation_model.apply_correlation(
+                        self.sites, imt, intra_residual
+                    )
+
+                inter_residual = stddev_inter * distribution.rvs(
+                    size=realizations)
+
+                gmf = self.gsim.to_imt_unit_values(
+                    mean + intra_residual + inter_residual)
+
+            result[imt] = gmf
+
+        return result
+
+    def compute(self, seed):
+        """
+        Compute the ground motion field for the given sites.
+
+        :param seed:
+            the seed for the numpy random number generator
+        :returns:
+            Dictionary mapping intensity measure type objects (same as in
+            parameter ``imts``) to numpy arrays of floats, representing
+            ground shaking intensity for all sites in the collection.
+        """
+        # consider 1 realization, i.e. return column 0-th of the GMF arrays
+        return dict((imt, gmf[:, 0]) for imt, gmf in self._compute(
+            seed, realizations=1).iteritems())
+
+
 def ground_motion_fields(rupture, sites, imts, gsim, truncation_level,
                          realizations, correlation_model=None,
-                         rupture_site_filter=filters.rupture_site_noop_filter):
+                         rupture_site_filter=filters.rupture_site_noop_filter,
+                         seed=None):
     """
     Given an earthquake rupture, the ground motion field calculator computes
     ground shaking over a set of sites, by randomly sampling a ground shaking
@@ -65,7 +189,8 @@ def ground_motion_fields(rupture, sites, imts, gsim, truncation_level,
     :param rupture_site_filter:
         Optional rupture-site filter function. See
         :mod:`openquake.hazardlib.calc.filters`.
-
+    :param int seed:
+        The seed used in the numpy random number generator
     :returns:
         Dictionary mapping intensity measure type objects (same
         as in parameter ``imts``) to 2d numpy arrays of floats,
@@ -77,72 +202,13 @@ def ground_motion_fields(rupture, sites, imts, gsim, truncation_level,
     if not ruptures_sites:
         return dict((imt, numpy.zeros((len(sites), realizations)))
                     for imt in imts)
-    no_filter = rupture_site_filter is filters.rupture_site_noop_filter
     [(rupture, sites)] = ruptures_sites
 
-    sctx, rctx, dctx = gsim.make_contexts(sites, rupture)
-    result = {}
-
-    if truncation_level == 0:
-        assert correlation_model is None
-        for imt in imts:
-            mean, _stddevs = gsim.get_mean_and_stddevs(sctx, rctx, dctx, imt,
-                                                       stddev_types=[])
-            mean = gsim.to_imt_unit_values(mean)
-            mean.shape += (1, )
-            mean = mean.repeat(realizations, axis=1)
-            result[imt] = mean if no_filter else sites.expand(
-                mean, placeholder=0)
-        return result
-
-    if truncation_level is None:
-        distribution = scipy.stats.norm()
-    else:
-        assert truncation_level > 0
-        distribution = scipy.stats.truncnorm(- truncation_level,
-                                             truncation_level)
-
-    for imt in imts:
-
-        if gsim.DEFINED_FOR_STANDARD_DEVIATION_TYPES == set([StdDev.TOTAL]):
-            # If the GSIM provides only total standard deviation, then we need
-            # to compute only mean and total standard deviation at the sites
-            # of interest.
-            # In this case, we also assume that no correlation model is used.
-            assert correlation_model is None
-
-            mean, [stddev_total] = gsim.get_mean_and_stddevs(
-                sctx, rctx, dctx, imt, [StdDev.TOTAL]
-            )
-            stddev_total = stddev_total.reshape(stddev_total.shape + (1, ))
-            mean = mean.reshape(mean.shape + (1, ))
-
-            total_residual = stddev_total * distribution.rvs(
-                size=(len(sites), realizations)
-            )
-            gmf = gsim.to_imt_unit_values(mean + total_residual)
-        else:
-            mean, [stddev_inter, stddev_intra] = gsim.get_mean_and_stddevs(
-                sctx, rctx, dctx, imt, [StdDev.INTER_EVENT, StdDev.INTRA_EVENT]
-            )
-            stddev_intra = stddev_intra.reshape(stddev_intra.shape + (1, ))
-            stddev_inter = stddev_inter.reshape(stddev_inter.shape + (1, ))
-            mean = mean.reshape(mean.shape + (1, ))
-
-            intra_residual = stddev_intra * distribution.rvs(
-                size=(len(sites), realizations)
-            )
-
-            if correlation_model is not None:
-                intra_residual = correlation_model.apply_correlation(
-                    sites, imt, intra_residual
-                )
-
-            inter_residual = stddev_inter * distribution.rvs(size=realizations)
-
-            gmf = gsim.to_imt_unit_values(
-                mean + intra_residual + inter_residual)
-
-        result[imt] = gmf if no_filter else sites.expand(gmf, placeholder=0)
+    result = GmfComputer(rupture, sites, imts, gsim, truncation_level,
+                         correlation_model)._compute(seed, realizations)
+    for imt, gmf in result.iteritems():
+        # makes sure the lenght of the arrays in output is the same as sites
+        if rupture_site_filter is not filters.rupture_site_noop_filter:
+            result[imt] = sites.expand(gmf, placeholder=0)
 
     return result
