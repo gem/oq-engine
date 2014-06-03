2111156d63f286a06a27f33146fbec4a21cb2abe
Michele Simionato <michele.simionato@gmail.com>
2014-06-03 16:16:47 +0200

    Raise source_max_weight

--- openquake/engine/calculators/hazard/general.py
+++ openquake/engine/calculators/hazard/general.py
@@ -162,7 +162,7 @@ class BaseHazardCalculator(base.Calculator):
                 yield self.job.id, sitecol, block, trt_model.id, gsims, task_no
                 num_blocks += 1
                 num_sources += len(block)
-                logs.LOG.progress('Filtered %d sources out of %d' %
+                logs.LOG.progress('Processing %d sources out of %d' %
                                   sc.filtered_sources)
 
             task_no += num_blocks
