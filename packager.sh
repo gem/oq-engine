f01511aa55f04b85b6bc0ecee7616d7f65196325
Daniele Viganò <daniele@vigano.me>
2014-06-06 13:41:04 +0200

    Adds a debug flag to enable set -x in packager.sh

--- packager.sh
+++ packager.sh
@@ -7,7 +7,10 @@
 #
 
 # export PS4='+${BASH_SOURCE}:${LINENO}:${FUNCNAME[0]}: '
-set -x
+
+if [ $GEM_SET_DEBUG ]; then
+    set -x
+fi
 set -e
 GEM_GIT_REPO="git://github.com/gem"
 GEM_GIT_PACKAGE="oq-commonlib"
