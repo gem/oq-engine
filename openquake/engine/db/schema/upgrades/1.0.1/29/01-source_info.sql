65a5cb0afe6e3f5160facdec48e3338cdef46028
Michele Simionato <michele.simionato@gmail.com>
2014-06-26 06:44:08 +0200

    Added a source_progress view
    Former-commit-id: bc02825b34c8892af7af667734b05c6fe74eaa9b [formerly 2cd6dd1932c01bd297d9f94313c57cbf147c0086]
    Former-commit-id: 102c0ccb92e172a24fef48c971d547bdcb2b2b61

--- openquake/engine/db/schema/upgrades/1.0.1/29/01-source_info.sql
+++ openquake/engine/db/schema/upgrades/1.0.1/29/01-source_info.sql
@@ -17,3 +17,17 @@ ALTER TABLE hzrdr.source_info ADD CONSTRAINT hzrdr_source_info_trt_model_fk
 FOREIGN KEY (trt_model_id) REFERENCES hzrdr.trt_model(id)
 ON DELETE CASCADE;
 
+
+CREATE VIEW hzrdr.source_progress AS
+SELECT a.hazard_calculation_id, sources_done, sources_todo FROM
+   (SELECT y.hazard_calculation_id, count(x.id) AS sources_done
+   FROM hzrdr.source_info AS x, hzrdr.lt_source_model AS y, hzrdr.trt_model AS z
+   WHERE x.trt_model_id = z.id AND z.lt_model_id=y.id
+   GROUP BY y.hazard_calculation_id) AS a,
+   (SELECT y.hazard_calculation_id, sum(num_sources) AS sources_todo
+   FROM hzrdr.lt_source_model AS y, hzrdr.trt_model AS z
+   WHERE z.lt_model_id=y.id
+   GROUP by y.hazard_calculation_id) AS b
+WHERE a.hazard_calculation_id=b.hazard_calculation_id;
+
+ALTER VIEW hzrdr.source_progress OWNER TO oq_admin;
