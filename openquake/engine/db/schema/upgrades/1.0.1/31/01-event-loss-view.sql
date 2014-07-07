b785a3e719d60c79dd8ff42f5449dfaeab43cb60
Michele Simionato <michele.simionato@gmail.com>
2014-07-07 09:20:21 +0200

    Fixed an upgrade script

--- openquake/engine/db/schema/upgrades/1.0.1/31/01-event-loss-view.sql
+++ openquake/engine/db/schema/upgrades/1.0.1/31/01-event-loss-view.sql
@@ -10,8 +10,8 @@ ALTER VIEW riskr.event_loss_view OWNER TO oq_admin;
    
 -- fix a missing DELETE CASCADE
 ALTER TABLE hzrdr.probabilistic_rupture
-   DROP CONSTRAINT hzrdr_probabilistic_rupture_ses_collection_fk;
+   DROP CONSTRAINT hzrdr_probabilistic_rupture_ses_collection_trt_model_fk;
 ALTER TABLE hzrdr.probabilistic_rupture
-   ADD CONSTRAINT hzrdr_probabilistic_rupture_trt_model_fk
-   FOREIGN KEY (trt_model_id) REFERENCES hzrdr.trt_model(id)
-   ON DELETE CASCADE;
+ADD CONSTRAINT hzrdr_probabilistic_rupture_ses_collection_fk
+FOREIGN KEY (ses_collection_id) REFERENCES hzrdr.ses_collection(id)
+ON DELETE CASCADE;
