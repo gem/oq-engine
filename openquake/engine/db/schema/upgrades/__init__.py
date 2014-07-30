4ccff69d408cf18dab15c2f19d001a11603caa20
Michele Simionato <michele.simionato@gmail.com>
2014-07-30 10:57:48 +0200

    First version of the migration procedure, using sqlplain

---
+++ openquake/engine/db/schema/upgrades/__init__.py
@@ -0,0 +1,9 @@
+import os
+from sqlplain import upgrade_manager
+
+version_pattern = r'\d\d\d\d'
+version_table = 'revision_info'
+directory = os.path.abspath(os.path.dirname(__file__))
+
+upgrade = upgrade_manager.UpgradeManager(
+    version_pattern, directory, version_table)
