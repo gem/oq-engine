1f6172d4229aa880391d9b14d7705182079c6aab
Michele Simionato <michele.simionato@gmail.com>
2014-07-30 14:25:16 +0200

    Removed the need for one PYTHONPATH
    Former-commit-id: 959c8a66bfa5898ce9e7b76025edfddbcc943c8b [formerly b09706fff3164868a27e55d630472173f9f6afee]
    Former-commit-id: d2bb4520037d9c24eda68316e14d5531234bf3c0

--- openquake/engine/db/schema/upgrades/__init__.py
+++ openquake/engine/db/schema/upgrades/__init__.py
@@ -5,5 +5,5 @@ version_pattern = r'\d\d\d\d'
 version_table = 'revision_info'
 directory = os.path.abspath(os.path.dirname(__file__))
 
-upgrade = upgrade_manager.UpgradeManager(
+upgrader = upgrade_manager.UpgradeManager(
     version_pattern, directory, version_table, echo=True)
