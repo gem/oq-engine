f9b477cf2160d3b84deb211b0a6ebb599ded22cd
Michele Simionato <michele.simionato@gmail.com>
2014-07-31 16:52:12 +0200

    Remove a "\" in $USER to make sure the passed user is ubuntu, not postgres
    Former-commit-id: 194e8308e8c250ae3628cd05f0002d20a41cefaf [formerly 2eda5dedadb739bd6f7c37ec0ec09d7cf32f1a7a] [formerly 2eda5dedadb739bd6f7c37ec0ec09d7cf32f1a7a [formerly 99a445524004043628ae11c534dfa3c4df4fcafc]]
    Former-commit-id: f88c3c0cf2f529d68634f0c4a956a824d8a185e2
    Former-commit-id: 07df689465ff86698ea9d75997acd19d11dc2274

--- packager.sh
+++ packager.sh
@@ -278,9 +278,9 @@ _devtest_innervm_run () {
     ssh $lxc_ip "sudo sed -i 's/#standard_conforming_strings = on/standard_conforming_strings = off/g' /etc/postgresql/9.1/main/postgresql.conf"
 
     ssh $lxc_ip "sudo service postgresql restart"
-    ssh $lxc_ip "sudo -u postgres  createuser -d -e -i -l -s -w \$USER"
+    ssh $lxc_ip "sudo -u postgres createuser -d -e -i -l -s -w \$USER"
 
-    ssh $lxc_ip "sudo su postgres -c \"cd oq-engine ; openquake/engine/bin/oq_create_db --yes --db-user=\\\$USER --db-name=openquake --schema-path=\\\$(pwd)/openquake/engine/db/schema\""
+    ssh $lxc_ip "sudo su postgres -c \"cd oq-engine ; openquake/engine/bin/oq_create_db --yes --db-user=\$USER --db-name=openquake --schema-path=\\\$(pwd)/openquake/engine/db/schema\""
 
     for dbu in oq_admin oq_job_init; do
         ssh $lxc_ip "sudo su postgres -c \"psql -c \\\"ALTER ROLE $dbu WITH PASSWORD 'openquake'\\\"\""
