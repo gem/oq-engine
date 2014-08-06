aa7368d014add9280cd385426b177f9caebe7409
Michele Simionato <michele.simionato@gmail.com>
2014-08-06 15:36:09 +0200

    Extracted process_sources from task_arg_gen
    Former-commit-id: ea8a747c45c21b1266eb8ef638f145d9ea241b88 [formerly ea8a747c45c21b1266eb8ef638f145d9ea241b88 [formerly ebfd2bd71db49042de220d3e52df6955835b19b9]]
    Former-commit-id: c668b21dd8b7809387812fea2b0990857fd966ef
    Former-commit-id: 6cf6ad441e332ba8836cb385e72fe0786c5d349d

--- openquake/engine/tests/calculators/hazard/general_test.py
+++ openquake/engine/tests/calculators/hazard/general_test.py
@@ -176,8 +176,7 @@ class InitializeSourcesTestCase(unittest.TestCase):
         self.assertEqual(
             [m1.get_num_sources(), m2.get_num_sources(), m3.get_num_sources()],
             [2, 2, 2])
-        for args in self.calc.task_arg_gen():
-            pass  # now filtering the sources
+        self.calc.process_sources()
         self.assertEqual(
             [m1.get_num_sources(), m2.get_num_sources(), m3.get_num_sources()],
             [1, 1, 1])
