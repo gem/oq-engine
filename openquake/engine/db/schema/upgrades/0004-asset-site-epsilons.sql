96b7a8825b2af985647a113e05d2eb47e03197e4
Michele Simionato <michele.simionato@gmail.com>
2014-08-27 11:34:35 +0200

    Added and populated the tables AssetSite and Epsilon
    Former-commit-id: 00c380f0a1abb3f2d767ae9c64fa280330b836b7 [formerly 00c380f0a1abb3f2d767ae9c64fa280330b836b7 [formerly b1fcd1cb2e2d8653fd97cddc47b1e22d3ca3209c]]
    Former-commit-id: 21d9009226df79c871996953630f34415e3b30b5
    Former-commit-id: e138d1647fa8ccc00da55ec37a13809e8bfbd1f8

---
+++ openquake/engine/db/schema/upgrades/0004-asset-site-epsilons.sql
@@ -0,0 +1,32 @@
+CREATE TABLE riskr.asset_site (
+id SERIAL PRIMARY KEY,
+risk_job_id INTEGER NOT NULL REFERENCES uiapi.oq_job (id),
+asset_id INTEGER NOT NULL REFERENCES riski.exposure_data (id),
+site_id INTEGER NOT NULL REFERENCES hzrdi.hazard_site (id)
+) TABLESPACE riskr_ts;
+
+GRANT SELECT,INSERT ON riskr.asset_site TO oq_job_init;
+GRANT USAGE ON riskr.asset_site_id_seq TO oq_job_init;
+
+COMMENT ON TABLE riskr.asset_site IS 'Association between assets and hazard sites';
+COMMENT ON COLUMN riskr.asset_site.risk_job_id IS 'The risk job performing the association';
+COMMENT ON COLUMN riskr.asset_site.asset_id IS 'The asset ID (exposure_data_id)';
+COMMENT ON COLUMN riskr.asset_site.site_id IS 'The hazard site ID';
+
+
+CREATE TABLE riskr.epsilon (
+id SERIAL PRIMARY KEY,
+asset_site_id INTEGER NOT NULL REFERENCES riskr.asset_site (id),
+ses_collection_id INTEGER NOT NULL REFERENCES hzrdr.ses_collection (id),
+epsilons FLOAT[] NOT NULL
+) TABLESPACE riskr_ts;
+
+GRANT SELECT,INSERT ON riskr.epsilon TO oq_job_init;
+GRANT USAGE ON riskr.epsilon_id_seq TO oq_job_init;
+
+COMMENT ON TABLE riskr.epsilon IS 'The epsilons for each asset and SES collection used in an event_based or scenario risk calculation';
+
+COMMENT ON COLUMN riskr.epsilon.asset_site_id IS 'The asset and site associated to the given epsilons';
+
+COMMENT ON COLUMN riskr.epsilon.ses_collection_id IS 'The SES collection corresponding to the given epsilons'
+COMMENT ON COLUMN riskr.epsilon.epsilons IS 'The array of epsilons'
