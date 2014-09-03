c04f87998ad72fdd3285b5654d314f4355847508
ldanciul <laurentiudanciu@gmail.com>
2014-09-03 18:08:18 +0200

    CY2008Swiss test and tables clean -up

--- openquake/hazardlib/tests/gsim/chiou_youngs_2008_swiss_test.py
+++ openquake/hazardlib/tests/gsim/chiou_youngs_2008_swiss_test.py
@@ -14,131 +14,106 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from openquake.hazardlib.gsim.chiou_youngs_2008    import (ChiouYoungs2008SWISS01,ChiouYoungs2008SWISS06,ChiouYoungs2008SWISS04)
-from openquake.hazardlib.gsim.chiou_youngs_2008    import (ChiouYoungs2008SWISS01T,ChiouYoungs2008SWISS06T,ChiouYoungs2008SWISS04T)
-
 from openquake.hazardlib.tests.gsim.utils import BaseGSIMTestCase
-
+from openquake.hazardlib.gsim.chiou_youngs_2008 import (ChiouYoungs2008SWISS01,
+    ChiouYoungs2008SWISS06,
+    ChiouYoungs2008SWISS04,
+    ChiouYoungs2008SWISS01T,
+    ChiouYoungs2008SWISS06T,
+    ChiouYoungs2008SWISS04T)
 
 class ChiouYoungs2008SWISS01TestCase(BaseGSIMTestCase):
     GSIM_CLASS = ChiouYoungs2008SWISS01
 
-    # First five tests use data ported from Kenneth Campbell
-    # tables for verifying NGA models, available from OpenSHA, see
-    # http://opensha.usc.edu/docs/opensha/NGA/Campbell_NGA_tests.zip
-    # This data is distributed under different license, see LICENSE.txt
-    # in tests/gsim/data/NGA
     def test_std_total(self):
         self.check('CY08Swiss/CY08_STD_TOTAL_SigmaSS_TMR.csv',
                     max_discrep_percentage=0.60)
-                   #~ 
     def test_mean_hanging_wall_normal_slip(self):
-        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_NM_VsK-1.csv', max_discrep_percentage=0.60) 
-
+        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_NM_VsK-1.csv', 
+                    max_discrep_percentage=0.60) 
     def test_mean_hanging_wall_reversed_slip(self):
-        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_RV_VsK-1.csv', max_discrep_percentage=0.60)
-
+        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_RV_VsK-1.csv', 
+                    max_discrep_percentage=0.60)
     def test_mean_hanging_wall_strike_slip(self):
-        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_SS_VsK-1.csv', max_discrep_percentage=0.60)
+        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_SS_VsK-1.csv', 
+                    max_discrep_percentage=0.60)
 
 class ChiouYoungs2008SWISS06TestCase(BaseGSIMTestCase):
     GSIM_CLASS = ChiouYoungs2008SWISS06
 
-    # First five tests use data ported from Kenneth Campbell
-    # tables for verifying NGA models, available from OpenSHA, see
-    # http://opensha.usc.edu/docs/opensha/NGA/Campbell_NGA_tests.zip
-    # This data is distributed under different license, see LICENSE.txt
-    # in tests/gsim/data/NGA
     def test_std_total(self):
         self.check('CY08Swiss/CY08_STD_TOTAL_SigmaSS_TMR.csv',
                     max_discrep_percentage=0.10)
-
     def test_mean_hanging_wall_normal_slip(self):
-        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_NM_VsK-6.csv', max_discrep_percentage=0.55) 
-
+        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_NM_VsK-6.csv', 
+                    max_discrep_percentage=0.55) 
     def test_mean_hanging_wall_reversed_slip(self):
-        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_RV_VsK-6.csv', max_discrep_percentage=0.55)
-
+        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_RV_VsK-6.csv', 
+                    max_discrep_percentage=0.55)
     def test_mean_hanging_wall_strike_slip(self):
-        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_SS_VsK-6.csv', max_discrep_percentage=0.55)
+        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_SS_VsK-6.csv', 
+                    max_discrep_percentage=0.55)
 
 class ChiouYoungs2008SWISS04TestCase(BaseGSIMTestCase):
     GSIM_CLASS = ChiouYoungs2008SWISS04
 
-    # First five tests use data ported from Kenneth Campbell
-    # tables for verifying NGA models, available from OpenSHA, see
-    # http://opensha.usc.edu/docs/opensha/NGA/Campbell_NGA_tests.zip
-    # This data is distributed under different license, see LICENSE.txt
-    # in tests/gsim/data/NGA
-
     def test_std_total(self):
         self.check('CY08Swiss/CY08_STD_TOTAL_SigmaSS_TMR.csv',
                     max_discrep_percentage=0.10)
-
     def test_mean_hanging_wall_normal_slip(self):
-        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_NM_VsK-4_corr.csv', max_discrep_percentage=0.55) 
-
+        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_NM_VsK-4_corr.csv', 
+                    max_discrep_percentage=0.55) 
     def test_mean_hanging_wall_reversed_slip(self):
-        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_RV_VsK-4_corr.csv', max_discrep_percentage=0.55)
-
+        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_RV_VsK-4_corr.csv', 
+                    max_discrep_percentage=0.55)
     def test_mean_hanging_wall_strike_slip(self):
-        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_SS_VsK-4_corr.csv', max_discrep_percentage=0.55)
-
+        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_SS_VsK-4_corr.csv', 
+                    max_discrep_percentage=0.55)
 
 class ChiouYoungs2008SWISS01TTestCase(BaseGSIMTestCase):
     GSIM_CLASS = ChiouYoungs2008SWISS01T
 
-    # First five tests use data ported from Kenneth Campbell
-    # tables for verifying NGA models, available from OpenSHA, see
-    # http://opensha.usc.edu/docs/opensha/NGA/Campbell_NGA_tests.zip
-    # This data is distributed under different license, see LICENSE.txt
-    # in tests/gsim/data/NGA
     def test_std_total(self):
-        self.check('CY08Swiss/CY08_STD_TOTAL_SigmaSS_T.csv', max_discrep_percentage=0.10)
-        
+        self.check('CY08Swiss/CY08_STD_TOTAL_SigmaSS_T.csv', 
+                    max_discrep_percentage=0.10)
     def test_mean_hanging_wall_normal_slip(self):
-        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_NM_VsK-1.csv', max_discrep_percentage=0.60) 
-
+        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_NM_VsK-1.csv', 
+                    max_discrep_percentage=0.60) 
     def test_mean_hanging_wall_reversed_slip(self):
-        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_RV_VsK-1.csv', max_discrep_percentage=0.60)
-
+        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_RV_VsK-1.csv', 
+                    max_discrep_percentage=0.60)
     def test_mean_hanging_wall_strike_slip(self):
-        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_SS_VsK-1.csv', max_discrep_percentage=0.60)        
+        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_SS_VsK-1.csv', 
+                    max_discrep_percentage=0.60)        
 
 class ChiouYoungs2008SWISS06TTestCase(BaseGSIMTestCase):
     GSIM_CLASS = ChiouYoungs2008SWISS06T
 
-    # First five tests use data ported from Kenneth Campbell
-    # tables for verifying NGA models, available from OpenSHA, see
-    # http://opensha.usc.edu/docs/opensha/NGA/Campbell_NGA_tests.zip
-    # This data is distributed under different license, see LICENSE.txt
-    # in tests/gsim/data/NGA
     def test_std_total(self):
-        self.check('CY08Swiss/CY08_STD_TOTAL_SigmaSS_T.csv', max_discrep_percentage=0.10)
+        self.check('CY08Swiss/CY08_STD_TOTAL_SigmaSS_T.csv', 
+                    max_discrep_percentage=0.10)
     def test_mean_hanging_wall_normal_slip(self):
-        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_NM_VsK-6.csv', max_discrep_percentage=0.55) 
-
+        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_NM_VsK-6.csv', 
+                    max_discrep_percentage=0.55) 
     def test_mean_hanging_wall_reversed_slip(self):
-        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_RV_VsK-6.csv', max_discrep_percentage=0.55)
-
+        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_RV_VsK-6.csv', 
+                    max_discrep_percentage=0.55)
     def test_mean_hanging_wall_strike_slip(self):
-        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_SS_VsK-6.csv', max_discrep_percentage=0.55)
+        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_SS_VsK-6.csv', 
+                    max_discrep_percentage=0.55)
 
 class ChiouYoungs2008SWISS04TTestCase(BaseGSIMTestCase):
     GSIM_CLASS = ChiouYoungs2008SWISS04T
 
-    # First five tests use data ported from Kenneth Campbell
-    # tables for verifying NGA models, available from OpenSHA, see
-    # http://opensha.usc.edu/docs/opensha/NGA/Campbell_NGA_tests.zip
-    # This data is distributed under different license, see LICENSE.txt
-    # in tests/gsim/data/NGA
     def test_std_total(self):
-        self.check('CY08Swiss/CY08_STD_TOTAL_SigmaSS_T.csv', max_discrep_percentage=0.10)
+        self.check('CY08Swiss/CY08_STD_TOTAL_SigmaSS_T.csv', 
+                    max_discrep_percentage=0.10)
     def test_mean_hanging_wall_normal_slip(self):
-        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_NM_VsK-4_corr.csv', max_discrep_percentage=0.55) 
-
+        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_NM_VsK-4_corr.csv', 
+                    max_discrep_percentage=0.55) 
     def test_mean_hanging_wall_reversed_slip(self):
-        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_RV_VsK-4_corr.csv', max_discrep_percentage=0.55)
-
+        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_RV_VsK-4_corr.csv', 
+                    max_discrep_percentage=0.55)
     def test_mean_hanging_wall_strike_slip(self):
-        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_SS_VsK-4_corr.csv', max_discrep_percentage=0.55)
+        self.check('CY08Swiss/CY08_MEDIAN_MS_HW_SS_VsK-4_corr.csv', 
+                    max_discrep_percentage=0.55)
