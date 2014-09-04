7c66a0f079f8e277eb3e49b47227d8af8aff043d
Michele Simionato <michele.simionato@gmail.com>
2014-09-04 07:22:22 +0200

    Fixed openquake/engine/tests/calculators/hazard/post_processing_test.py
    Former-commit-id: acd7a2584f153e477cf680e66c24be74b40757f9

--- openquake/engine/tests/calculators/hazard/post_processing_test.py
+++ openquake/engine/tests/calculators/hazard/post_processing_test.py
@@ -43,7 +43,7 @@ aaae = numpy.testing.assert_array_almost_equal
 
 
 # package prefix used for mock.patching
-MOCK_PREFIX = "openquake.engine.calculators.hazard.classical.post_processing"
+MOCK_PREFIX = "openquake.engine.calculators.hazard.post_processing"
 
 
 class HazardMapsTestCase(unittest.TestCase):
