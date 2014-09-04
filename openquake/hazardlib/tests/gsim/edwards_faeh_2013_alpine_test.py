3ec53d2863dc9bfb891c1d6e4f506ea80b6f3950
ldanciul <laurentiudanciu@gmail.com>
2014-09-04 17:55:57 +0200

    EdwardsFah Alpine 2013 reviewed, tests and tables corrected

--- openquake/hazardlib/tests/gsim/edwards_faeh_2013_alpine_test.py
+++ openquake/hazardlib/tests/gsim/edwards_faeh_2013_alpine_test.py
@@ -15,27 +15,23 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from openquake.hazardlib.gsim.edwards_fah_2013a import (
-                                                    EdwardsFah2013Alpine10MPa,   EdwardsFah2013Alpine20MPa,
-                                                    EdwardsFah2013Alpine30MPa,   EdwardsFah2013Alpine50MPa,
-                                                    EdwardsFah2013Alpine60MPa,   EdwardsFah2013Alpine75MPa,
-                                                    EdwardsFah2013Alpine90MPa,   EdwardsFah2013Alpine120MPa
-                                                    )
+            EdwardsFah2013Alpine10MPa,   EdwardsFah2013Alpine20MPa,
+            EdwardsFah2013Alpine30MPa,   EdwardsFah2013Alpine50MPa,
+            EdwardsFah2013Alpine60MPa,   EdwardsFah2013Alpine75MPa,
+            EdwardsFah2013Alpine90MPa,   EdwardsFah2013Alpine120MPa
+            )
 from openquake.hazardlib.gsim.edwards_fah_2013a import (
 
-                                                    EdwardsFah2013Alpine10MPaMR, EdwardsFah2013Alpine20MPaMR,
-                                                    EdwardsFah2013Alpine30MPaMR, EdwardsFah2013Alpine50MPaMR,
-                                                    EdwardsFah2013Alpine60MPaMR, EdwardsFah2013Alpine75MPaMR,
-                                                    EdwardsFah2013Alpine90MPaMR, EdwardsFah2013Alpine120MPaMR
-                                                    )
-                                                    
+             EdwardsFah2013Alpine10MPaMR, EdwardsFah2013Alpine20MPaMR,
+             EdwardsFah2013Alpine30MPaMR, EdwardsFah2013Alpine50MPaMR,
+             EdwardsFah2013Alpine60MPaMR, EdwardsFah2013Alpine75MPaMR,
+             EdwardsFah2013Alpine90MPaMR, EdwardsFah2013Alpine120MPaMR
+             )
 from openquake.hazardlib.tests.gsim.utils import BaseGSIMTestCase
 
 
 class EdwardsFah2013Alpine10MPaTestCase(BaseGSIMTestCase):
     GSIM_CLASS = EdwardsFah2013Alpine10MPa
- 
-   # Test data were obtained from a tool given by the authors
-   # The data of the values of the mean PGA and SA are in cm/s2.
 
     def test_mean(self):
         self.check('EF13a/alp_sd10_table.csv', max_discrep_percentage=0.55)
@@ -46,91 +42,75 @@ class EdwardsFah2013Alpine10MPaTestCase(BaseGSIMTestCase):
 class EdwardsFah2013Alpine20MPaTestCase(BaseGSIMTestCase):
     GSIM_CLASS = EdwardsFah2013Alpine20MPa
 
-    # Test data were obtained from a tool given by the authors
-    # The data of the values of the mean PGA and SA are in cm/s2.
 
     def test_mean(self):
         self.check('EF13a/alp_sd20_table.csv', max_discrep_percentage=0.600)
     def test_std_total(self):
-        self.check('EF13a/EF13_STD_TOTAL_SigmaSS_T.csv', max_discrep_percentage=0.60)
+        self.check('EF13a/EF13_STD_TOTAL_SigmaSS_T.csv', 
+        max_discrep_percentage=0.60)
                     
 class EdwardsFah2013Alpine30MPaTestCase(BaseGSIMTestCase):
     GSIM_CLASS = EdwardsFah2013Alpine30MPa
 
-    # Test data were obtained from a tool given by the authors
-    # The data of the values of the mean PGA and SA are in cm/s2.
-
     def test_mean(self):
         self.check('EF13a/alp_sd30_table.csv', max_discrep_percentage=0.600)
     def test_std_total(self):
-        self.check('EF13a/EF13_STD_TOTAL_SigmaSS_T.csv', max_discrep_percentage=0.60)
+        self.check('EF13a/EF13_STD_TOTAL_SigmaSS_T.csv', 
+        max_discrep_percentage=0.60)
                             
 class EdwardsFah2013Alpine50MPaTestCase(BaseGSIMTestCase):
     GSIM_CLASS = EdwardsFah2013Alpine50MPa
 
-    # Test data were obtained from a tool given by the authors
-    # The data of the values of the mean PGA and SA are in cm/s2.
-
     def test_mean(self):
         self.check('EF13a/alp_sd50_table.csv', max_discrep_percentage=0.600)
     def test_std_total(self):
-        self.check('EF13a/EF13_STD_TOTAL_SigmaSS_T.csv', max_discrep_percentage=0.60)
+        self.check('EF13a/EF13_STD_TOTAL_SigmaSS_T.csv', 
+        max_discrep_percentage=0.60)
 
 class EdwardsFah2013Alpine60MPaTestCase(BaseGSIMTestCase):
     GSIM_CLASS = EdwardsFah2013Alpine60MPa
- 
-    # Test data were obtained from a tool given by the authors
-    # The data of the values of the mean PGA and SA are in cm/s2.
     def test_mean(self):
         self.check('EF13a/alp_sd60_table.csv', max_discrep_percentage=0.55)
     def test_std_total(self):
-        self.check('EF13a/EF13_STD_TOTAL_SigmaSS_T.csv', max_discrep_percentage=0.60)  
+        self.check('EF13a/EF13_STD_TOTAL_SigmaSS_T.csv', 
+        max_discrep_percentage=0.60)  
         
 class EdwardsFah2013Alpine75MPaTestCase(BaseGSIMTestCase):
     GSIM_CLASS = EdwardsFah2013Alpine75MPa
 
-    # Test data were obtained from a tool given by the authors
-    # The data of the values of the mean PGA and SA are in cm/s2.
-
     def test_mean(self):
         self.check('EF13a/alp_sd75_table.csv', max_discrep_percentage=0.55)
     def test_std_total(self):
-        self.check('EF13a/EF13_STD_TOTAL_SigmaSS_T.csv', max_discrep_percentage=0.60)
+        self.check('EF13a/EF13_STD_TOTAL_SigmaSS_T.csv', 
+        max_discrep_percentage=0.60)
                     
 class EdwardsFah2013Alpine90MPaTestCase(BaseGSIMTestCase):
     GSIM_CLASS = EdwardsFah2013Alpine90MPa
 
-    # Test data were obtained from a tool given by the authors
-    # The data of the values of the mean PGA and SA are in cm/s2.
-
     def test_mean(self):
         self.check('EF13a/alp_sd90_table.csv', max_discrep_percentage=0.55)  
     def test_std_total(self):
-        self.check('EF13a/EF13_STD_TOTAL_SigmaSS_T.csv', max_discrep_percentage=0.60)
+        self.check('EF13a/EF13_STD_TOTAL_SigmaSS_T.csv', 
+        max_discrep_percentage=0.60)
         
 class EdwardsFah2013Alpine120MPaTestCase(BaseGSIMTestCase):
     GSIM_CLASS = EdwardsFah2013Alpine120MPa
 
-    # Test data were obtained from a tool given by the authors
-    # The data of the values of the mean PGA and SA are in cm/s2.
-
     def test_mean(self):
         self.check('EF13a/alp_sd120_table.csv', max_discrep_percentage=0.55)
     def test_std_total(self):
-        self.check('EF13a/EF13_STD_TOTAL_SigmaSS_T.csv', max_discrep_percentage=0.60)
+        self.check('EF13a/EF13_STD_TOTAL_SigmaSS_T.csv', 
+        max_discrep_percentage=0.60)
         
 class EdwardsFah2013Alpine60MPaMRTestCase(BaseGSIMTestCase):
     GSIM_CLASS = EdwardsFah2013Alpine60MPaMR
  
-   # Test data were obtained from a tool given by the authors
-   # The data of the values of the mean PGA and SA are in cm/s2.
-
     def test_mean(self):
-        self.check('EF13a/alp_sd60_table.csv', max_discrep_percentage=0.55)
+        self.check('EF13a/alp_sd60_table.csv', 
+        max_discrep_percentage=0.55)
     def test_std_total(self):
         self.check('EF13a/EF13_STD_TOTAL_SigmaSS_TMR.csv',
                     max_discrep_percentage=0.61)   
-
 
 """
 Testing the single station GMPEs magnitude-distance dependent 
@@ -138,9 +118,6 @@ Testing the single station GMPEs magnitude-distance dependent
 
 class EdwardsFah2013Alpine10MPaMRTestCase(BaseGSIMTestCase):
     GSIM_CLASS = EdwardsFah2013Alpine10MPaMR
- 
-   # Test data were obtained from a tool given by the authors
-   # The data of the values of the mean PGA and SA are in cm/s2.
 
     def test_mean(self):
         self.check('EF13a/alp_sd10_table.csv', max_discrep_percentage=0.55)
@@ -150,9 +127,7 @@ class EdwardsFah2013Alpine10MPaMRTestCase(BaseGSIMTestCase):
 
 class EdwardsFah2013Alpine20MPaMRTestCase(BaseGSIMTestCase):
     GSIM_CLASS = EdwardsFah2013Alpine20MPaMR
- 
-   # Test data were obtained from a tool given by the authors
-   # The data of the values of the mean PGA and SA are in cm/s2.
+
 
     def test_mean(self):
         self.check('EF13a/alp_sd20_table.csv', max_discrep_percentage=0.55)
@@ -162,9 +137,6 @@ class EdwardsFah2013Alpine20MPaMRTestCase(BaseGSIMTestCase):
 
 class EdwardsFah2013Alpine30MPaMRTestCase(BaseGSIMTestCase):
     GSIM_CLASS = EdwardsFah2013Alpine30MPaMR
- 
-   # Test data were obtained from a tool given by the authors
-   # The data of the values of the mean PGA and SA are in cm/s2.
 
     def test_mean(self):
         self.check('EF13a/alp_sd30_table.csv', max_discrep_percentage=0.55)
@@ -174,9 +146,6 @@ class EdwardsFah2013Alpine30MPaMRTestCase(BaseGSIMTestCase):
                       
 class EdwardsFah2013Alpine50MPaMRTestCase(BaseGSIMTestCase):
     GSIM_CLASS = EdwardsFah2013Alpine50MPaMR
- 
-   # Test data were obtained from a tool given by the authors
-   # The data of the values of the mean PGA and SA are in cm/s2.
 
     def test_mean(self):
         self.check('EF13a/alp_sd50_table.csv', max_discrep_percentage=0.55)
@@ -186,9 +155,6 @@ class EdwardsFah2013Alpine50MPaMRTestCase(BaseGSIMTestCase):
 
 class EdwardsFah2013Alpine75MPaMRTestCase(BaseGSIMTestCase):
     GSIM_CLASS = EdwardsFah2013Alpine75MPaMR
- 
-   # Test data were obtained from a tool given by the authors
-   # The data of the values of the mean PGA and SA are in cm/s2.
 
     def test_mean(self):
         self.check('EF13a/alp_sd75_table.csv', max_discrep_percentage=0.55)
@@ -198,9 +164,6 @@ class EdwardsFah2013Alpine75MPaMRTestCase(BaseGSIMTestCase):
 
 class EdwardsFah2013Alpine90MPaMRTestCase(BaseGSIMTestCase):
     GSIM_CLASS = EdwardsFah2013Alpine90MPaMR
- 
-   # Test data were obtained from a tool given by the authors
-   # The data of the values of the mean PGA and SA are in cm/s2.
 
     def test_mean(self):
         self.check('EF13a/alp_sd90_table.csv', max_discrep_percentage=0.55)
@@ -211,9 +174,6 @@ class EdwardsFah2013Alpine90MPaMRTestCase(BaseGSIMTestCase):
 class EdwardsFah2013Alpine120MPaMRTestCase(BaseGSIMTestCase):
     GSIM_CLASS = EdwardsFah2013Alpine120MPaMR
  
-   # Test data were obtained from a tool given by the authors
-   # The data of the values of the mean PGA and SA are in cm/s2.
-
     def test_mean(self):
         self.check('EF13a/alp_sd120_table.csv', max_discrep_percentage=0.55)
     def test_std_total(self):
