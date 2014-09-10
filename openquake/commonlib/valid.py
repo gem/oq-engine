113466f8485ba7d150f9cf74a2c6fab3b5009599
Michele Simionato <michele.simionato@gmail.com>
2014-09-10 05:29:12 +0200

    Renamed val->validator

--- openquake/commonlib/valid.py
+++ openquake/commonlib/valid.py
@@ -37,8 +37,8 @@ def compose(*validators):
     """
     def composed_validator(value):
         out = value
-        for val in reversed(validators):
-            out = val(out)
+        for validator in reversed(validators):
+            out = validator(out)
         return out
     composed_validator.__name__ = 'compose(%s)' % ','.join(
         val.__name__ for val in validators)
