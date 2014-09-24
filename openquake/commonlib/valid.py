7c5028e5b06204fdf02c487160abbef0d7f3d4af
Michele Simionato <michele.simionato@gmail.com>
2014-09-24 14:25:23 +0200

    Fixed the check on the levels

--- openquake/commonlib/valid.py
+++ openquake/commonlib/valid.py
@@ -371,8 +371,11 @@ def IML(value, IMT, minIML=None, maxIML=None, imlUnit=None):
     into ("MMI", [7., 8., 9., 10., 11.], None, None)
     """
     imt_str = str(imt.from_string(IMT))
-    imls = positivefloats(value)
-    check_levels(imls, imt_str) if value else None
+    if value:
+        imls = positivefloats(value)
+        check_levels(imls, imt_str)
+    else:
+        imls = None
     min_iml = positivefloat(minIML) if minIML else None
     max_iml = positivefloat(maxIML) if maxIML else None
     return (imt_str, imls, min_iml, max_iml)
