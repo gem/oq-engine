5a403a345be30a9ee6d13179c30e3594ab49a5eb
Michele Simionato <michele.simionato@gmail.com>
2014-10-01 12:05:59 +0200

    Fixed the upgrade script

--- openquake/engine/db/schema/upgrades/0008-lt_model-not-null.py
+++ openquake/engine/db/schema/upgrades/0008-lt_model-not-null.py
@@ -6,7 +6,7 @@ Then add a NOT NULL constraint on lt_model_id.
 
 
 GET_SESCOLL_AND_JOBS = """\
-SELECT a.id AS ses_coll_id, b.id AS oq_job_id
+SELECT a.id AS ses_coll_id, b.oq_job_id
 FROM hzrdr.ses_collection AS a, uiapi.output as b
 WHERE output_id=b.id AND lt_model_id IS NULL;
 """
@@ -31,7 +31,7 @@ ALTER TABLE hzrdr.ses_collection ALTER COLUMN lt_model_id SET NOT NULL;
 def upgrade(conn):
     curs = conn.cursor()
     curs.execute(GET_SESCOLL_AND_JOBS)
-    for ses_coll_id, oq_job_id in curs:
+    for ses_coll_id, oq_job_id in curs.fetchall():
         curs.execute(CREATE_LT_MODEL, (oq_job_id, []))
         [[lt_model_id]] = curs.fetchall()
         curs.execute(UPDATE_SES_COLL, (lt_model_id, ses_coll_id))
