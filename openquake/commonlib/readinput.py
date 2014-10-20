a699a5206f052e0027688e4d55c4b464f8f067a9
Michele Simionato <michele.simionato@gmail.com>
2014-10-20 16:16:39 +0200

    Added get_specific_assets

--- openquake/commonlib/readinput.py
+++ openquake/commonlib/readinput.py
@@ -207,6 +207,19 @@ def get_exposure(oqparam):
                     values, deductibles, insurance_limits, retrofitting_values)
 
 
+def get_specific_assets(oqparam):
+    """
+    Get the assets from the parameters specific_assets or specific_assets_csv
+
+    :param oqparam:
+        an :class:`openquake.commonlib.oqvalidation.OqParam` instance
+    """
+    try:
+        return oqparam.specific_assets.split()
+    except AttributeError:
+        return open(oqparam.inputs['specific_assets']).read().split()
+
+
 def get_sitecol_assets(oqparam):
     """
     Returns two sequences of the same length: a list with the assets
