49553de5d1135e40a3aca4e009f813cf34ed2d6b
Michele Simionato <michele.simionato@gmail.com>
2014-10-21 15:30:25 +0200

    Added a comment

--- openquake/commonlib/readini.py
+++ openquake/commonlib/readini.py
@@ -58,6 +58,15 @@ def parse_config(source, hazard_calculation_id=None, hazard_output_id=None):
 
     for sect in cp.sections():
         for key, value in cp.items(sect):
+            # we parse the .ini file and we build a dictionary key -> value
+            # with all the parameters defined there; the parameters with key
+            # ending in `_file`, `_csv` (i.e. the input types) are treated
+            # specially; they are assumed to correspond to file names; the
+            # names are stored in the sub dictionary 'inputs', by stripping
+            # the extension; for instance, if the .ini file has a line
+            # sites_csv = file_with_the_sites.csv then the dictionary
+            # input with have the form
+            # params['inputs'] = {'sites': 'BASEPATH/file_with_the_sites.csv'}
             if key.endswith(('_file', '_csv')):
                 input_type, _ext = key.rsplit('_', 1)
                 path = value if os.path.isabs(value) else os.path.join(
