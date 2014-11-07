fb1592c65dcd5b86d23c333ac0284158e8613d4a
Michele Simionato <michele.simionato@gmail.com>
2014-11-07 08:43:25 +0100

    Removed parallelize and fixed the scenario calculator
    Former-commit-id: c903f81c5aae158e00f075de2afb837583e605e4 [formerly 0dc7d9085f56f8e0a1c34b9e418c69b1c24b437d] [formerly 0dc7d9085f56f8e0a1c34b9e418c69b1c24b437d [formerly cbf80ff66384da9fd21fa7c1832f7a8d0d34e387]]
    Former-commit-id: 7f6c0bf5eded4a2019c66a931b967b8362313270
    Former-commit-id: d475475f972eafd0c69a2063339958944576687c

--- openquake/engine/calculators/hazard/scenario/core.py
+++ openquake/engine/calculators/hazard/scenario/core.py
@@ -26,7 +26,6 @@ from openquake.hazardlib.calc.gmf import GmfComputer
 from openquake.hazardlib.imt import from_string
 import openquake.hazardlib.gsim
 
-from openquake.commonlib.general import split_in_blocks
 from openquake.commonlib.readinput import get_rupture
 
 from openquake.engine.calculators.hazard import general as haz_general
@@ -40,8 +39,6 @@ from django.db import transaction
 
 AVAILABLE_GSIMS = openquake.hazardlib.gsim.get_available_gsims()
 
-## XXX: use the oq-lite calculator
-
 
 @tasks.oqtask
 def gmfs(job_id, ses_ruptures, sitecol, imts, gmf_id):
@@ -50,7 +47,7 @@ def gmfs(job_id, ses_ruptures, sitecol, imts, gmf_id):
     :param ses_ruptures: a set of `SESRupture` instances
     :param sitecol: a `SiteCollection` instance
     :param imts: a list of hazardlib IMT instances
-    :param int gmf_id: the ID of a `Gmf` instance
+    :param int gmf_id: the ID of the `Gmf` instance
     """
     hc = models.oqparam(job_id)
     gsim = AVAILABLE_GSIMS[hc.gsim]()  # instantiate the GSIM class
@@ -200,15 +197,10 @@ class ScenarioHazardCalculator(haz_general.BaseHazardCalculator):
             create_db_ruptures(self.rupture, self.ses_coll, tags,
                                self.hc.random_seed)
 
-    def task_arg_gen(self):
-        """
-        Yield a tuple of the form (job_id, sitecol, rupture_id, gmf_id,
-        task_seed, num_realizations). `task_seed` will be used to seed
-        numpy for temporal occurence sampling. Only a single task
-        will be generated which is fine since the computation is fast
-        anyway.
-        """
+    def execute(self):
         ses_ruptures = models.SESRupture.objects.filter(
             rupture__ses_collection=self.ses_coll.id)
-        for ruptures in split_in_blocks(ses_ruptures, self.concurrent_tasks):
-            yield self.job.id, ruptures, self.sites, self.imts, self.gmf.id
+        self.acc = tasks.apply_reduce(
+            self.core_calc_task,
+            (self.job.id, ses_ruptures, self.site_collection,
+             self.imts, self.gmf.id), lambda a, x: None)
