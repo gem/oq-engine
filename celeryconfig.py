92b5dd28ca42081132d39062d7fb2145d926fef1
Michele Simionato <michele.simionato@gmail.com>
2014-11-07 12:55:31 +0100

    Removed filter_and_split_sources
    Former-commit-id: 21c6eec0a4fff271886211735924605b2a109349 [formerly e46dba63761d86a5eaeeb2c3512aea9503722162]
    Former-commit-id: 07d119ccd6073cb9459fb9d3ddc4c0f159c656e1

--- celeryconfig.py
+++ celeryconfig.py
@@ -70,7 +70,6 @@ CELERYD_PREFETCH_MULTIPLIER = 1
 CELERY_MAX_CACHED_RESULTS = 1
 
 CELERY_IMPORTS = get_core_modules(engine) + [
-    "openquake.engine.calculators.hazard.general",
     "openquake.engine.tests.utils.tasks"]
 
 os.environ["DJANGO_SETTINGS_MODULE"] = "openquake.engine.settings"
