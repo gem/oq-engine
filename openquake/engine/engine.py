e25df70f45575eca7b1c8bdd4c24e4079ef49a46
Michele Simionato <michele.simionato@gmail.com>
2014-11-16 08:25:44 +0100

    Fixed engine_test.py
    Former-commit-id: edfad5637b184105d61318b19dadf4939fb6b45a [formerly a6131834bb730b35f8ff1d392a78ff8a46fd8633]
    Former-commit-id: c14d6a9a56292c8a1e4fe42a6098c9abae1bea5d

--- openquake/engine/engine.py
+++ openquake/engine/engine.py
@@ -321,8 +321,7 @@ def print_outputs_summary(outputs, full=True):
                     o.id, o.get_output_type_display(), o.display_name)
         if truncated:
             print ('Some outputs where not shown. You can see the full list '
-                   'with the commands\n`oq-engine --list-hazard-outputs` or '
-                   '`oq-engine --list-risk-outputs`')
+                   'with the command\n`oq-engine --list-outputs`')
 
 
 def run_job(cfg_file, log_level, log_file, exports=(), hazard_output_id=None,
