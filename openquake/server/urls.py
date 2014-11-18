49c8b910acca93134f50289d85a3fa6e03f4e685
Michele Simionato <michele.simionato@gmail.com>
2014-11-18 12:03:46 +0100

    Added an API to retrieve the engine version
    Former-commit-id: 6b84df861c655762c155bac72c96d7c76ec7fb41

--- openquake/server/urls.py
+++ openquake/server/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls.defaults import url
 
 urlpatterns = patterns(
     '',
+    url(r'^engine_version$', 'openquake.server.views.get_engine_version'),
     url(r'^v1/calc/', include('openquake.server.v1.calc_urls')),
 )
