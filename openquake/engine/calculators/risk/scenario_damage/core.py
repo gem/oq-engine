5e9426ca85c58e21796080181ca3493d1b235cb6
Michele Simionato <michele.simionato@gmail.com>
2014-11-19 16:09:03 +0100

    Simplified the monitoring
    Former-commit-id: c5fc37f78bdb62c2b76490e0b9b473f70980d5c8 [formerly 49487d0425dc62ee4095b5924f7ff70f39e358c3]
    Former-commit-id: 567ff9b129aabb890400a135cedebb7e0f441f84

--- openquake/engine/calculators/risk/scenario_damage/core.py
+++ openquake/engine/calculators/risk/scenario_damage/core.py
@@ -54,13 +54,13 @@ def scenario_damage(workflow, getter, outputdict, params, monitor):
 
     # and no output containers
     assert len(outputdict) == 0, outputdict
-    with monitor.copy('computing risk'):
+    with monitor('computing risk'):
         assets, fractions = workflow(
             'damage', getter.assets, getter.get_data(), None)
         aggfractions = sum(fractions[i] * asset.number_of_units
                            for i, asset in enumerate(assets))
 
-    with monitor.copy('saving damage per assets'):
+    with monitor('saving damage per assets'):
         writers.damage_distribution(
             getter.assets, fractions, params.damage_state_ids)
 
