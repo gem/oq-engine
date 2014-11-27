5ee6fbc0b1a12b4959d882ea1c559139d44c759c
Michele Simionato <michele.simionato@gmail.com>
2014-11-27 13:08:47 +0100

    Removed wrong classical_test

--- openquake/commonlib/tests/calculators/classical_test.py
+++ openquake/commonlib/tests/calculators/classical_test.py
@@ -1,16 +1,10 @@
-import numpy
-from numpy.testing import assert_almost_equal as aae
 from nose.plugins.attrib import attr
-
-from openquake.qa_tests_data.scenario import (
-    case_1, case_2, case_3, case_4, case_5, case_6, case_7, case_8)
-
 from openquake.commonlib.tests.calculators import CalculatorTestCase
 
 
+# skeleton of the test: to be filled with something useful
 class ClassicalTestCase(CalculatorTestCase):
 
     @attr('qa', 'hazard', 'classical')
     def test_case_1(self):
-        out = self.run_calc(case_1.__file__, 'job.ini')
-        self.assertEqualFiles('expected.xml', out['gmf_xml'])
+        pass
