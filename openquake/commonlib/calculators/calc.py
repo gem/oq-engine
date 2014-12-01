ab0cf55a56c23f179761faffe530165a2af76e0c
Michele Simionato <michele.simionato@gmail.com>
2014-12-01 16:49:11 +0100

    Added a docstring

--- openquake/commonlib/calculators/calc.py
+++ openquake/commonlib/calculators/calc.py
@@ -208,6 +208,11 @@ def compute_hazard_maps(curves, imls, poes):
 def compute_hazard_maps_by_imt(curves_by_imt, imtls, poes):
     """
     Compute the hazard maps for all the IMTs.
+
+    :param curves_by_imt: dictionary IMT -> curves
+    :param imtls: dictionary IMT -> intensity measure levels
+    :param poes: list of PoEs for which to compute the maps
+    :returns: an AccumDict IMT -> hazard_maps per site and poes
     """
     return AccumDict({imt: compute_hazard_maps(curves_by_imt[imt], imls, poes)
                       for imt, imls in imtls.iteritems()})
