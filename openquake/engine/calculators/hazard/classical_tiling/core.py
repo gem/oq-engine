701bd423013adfa6452e9d312570adb75fb5c731
Michele Simionato <michele.simionato@gmail.com>
2014-12-02 07:06:06 +0100

    Fixed the tiling calculator

--- openquake/engine/calculators/hazard/classical_tiling/core.py
+++ openquake/engine/calculators/hazard/classical_tiling/core.py
@@ -17,46 +17,49 @@
 Core functionality for the classical tilint PSHA hazard calculator.
 """
 import math
-from openquake.baselib import split_in_blocks
+from openquake.baselib.general import split_in_blocks
 from openquake.hazardlib.site import SiteCollection
 
 from openquake.engine.calculators import base, calculators
+from openquake.engine.calculators.hazard.general import BaseHazardCalculator
 from openquake.engine.logs import LOG
 
-MAX_TILE_WEIGHT = 1000
+MAX_TILE_WEIGHT = 100000
 
 
 @calculators.add('classical_tiling')
-class ClassicalTilingHazardCalculator(base.Calculator):
+class ClassicalTilingHazardCalculator(BaseHazardCalculator):
     """
     Classical tiling PSHA hazard calculator.
     """
 
-    def run_tile(self, tile):
-        classical = calculators['Classical'](self.job)
+    def run_tile(self, i, tile):
+        classical = calculators['classical'](self.job)
+        classical.tilepath = ('tile%d' % i,)
         classical.site_collection = SiteCollection(tile)
         classical.initialize_sources()
         classical.init_zeros_ones()
         classical.execute()
         classical.post_execute()
-        classical.post_processing()
+        classical.post_process()
 
     def pre_execute(self):
-        self.oqparam = self.job.get_params()
+        self.oqparam = self.job.get_oqparam()
         self.parse_risk_model()
         self.initialize_site_collection()
         imtls = self.oqparam.intensity_measure_types_and_levels
         num_levels = sum(map(len, imtls.values()))
-        weight = len(self.site_collection) * num_levels
-        self.tiles = split_in_blocks(self.site_collection,
-                                     math.ceil(weight, MAX_TILE_WEIGHT))
+        weight = float(len(self.site_collection) * num_levels)
+        self.tiles = list(
+            split_in_blocks(
+                self.site_collection, math.ceil(weight / MAX_TILE_WEIGHT)))
         self.num_tiles = len(self.tiles)
 
     def execute(self):
         for i, tile in enumerate(self.tiles, 1):
             LOG.progress('Running tile %d of %d, %s sites',
                          i, self.num_tiles, len(tile))
-            self.run_tile(tile)
+            self.run_tile(i, tile)
 
     def post_execute(self):
         pass
