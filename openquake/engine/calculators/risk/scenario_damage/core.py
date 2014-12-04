9a1ac348f519e7fd06a7d3041efb2162af8dd964
Michele Simionato <michele.simionato@gmail.com>
2014-12-04 07:10:14 +0100

    Making more use of oq-lite in the risk calculators
    Former-commit-id: b75a7a274d2fb6bc4a730be4bfe26ba5c7e0e7ec [formerly f0c1d4c3a0f03554a82909b2733ce914348400d1]
    Former-commit-id: bc731b15846cd444ae604f916b76fb3130dd404c

--- openquake/engine/calculators/risk/scenario_damage/core.py
+++ openquake/engine/calculators/risk/scenario_damage/core.py
@@ -94,7 +94,6 @@ class ScenarioDamageRiskCalculator(base.RiskCalculator):
     def __init__(self, job):
         super(ScenarioDamageRiskCalculator, self).__init__(job)
         self.acc = {}  # taxonomy -> fractions
-        self.damage_state_ids = None
 
     @EnginePerformanceMonitor.monitor
     def agg_result(self, acc, task_result):
@@ -118,6 +117,7 @@ class ScenarioDamageRiskCalculator(base.RiskCalculator):
         """
         Save the damage distributions by taxonomy and total on the db.
         """
+        damage_state_ids = self.oqparam.damage_state_ids
 
         models.Output.objects.create_output(
             self.job, "Damage Distribution per Asset",
@@ -135,7 +135,7 @@ class ScenarioDamageRiskCalculator(base.RiskCalculator):
         tot = None
         for taxonomy, fractions in self.acc.iteritems():
             writers.damage_distribution_per_taxonomy(
-                fractions, self.damage_state_ids, taxonomy)
+                fractions, damage_state_ids, taxonomy)
             if tot is None:  # only the first time
                 tot = numpy.zeros(fractions.shape)
             tot += fractions
@@ -144,31 +144,21 @@ class ScenarioDamageRiskCalculator(base.RiskCalculator):
             models.Output.objects.create_output(
                 self.job, "Damage Distribution Total",
                 "dmg_dist_total")
-            writers.total_damage_distribution(tot, self.damage_state_ids)
+            writers.total_damage_distribution(tot, damage_state_ids)
 
     def get_risk_model(self):
         """
         Load fragility model and store damage states
         """
-        risk_model = get_risk_model(models.oqparam(self.job.id))
+        risk_model = get_risk_model(self.oqparam)
 
         for lsi, dstate in enumerate(risk_model.damage_states):
             models.DmgState.objects.get_or_create(
                 risk_calculation=self.job, dmg_state=dstate, lsi=lsi)
 
-        self.damage_state_ids = [d.id for d in models.DmgState.objects.filter(
+        damage_state_ids = [d.id for d in models.DmgState.objects.filter(
             risk_calculation=self.job).order_by('lsi')]
+        self.oqparam.damage_state_ids = damage_state_ids
 
         self.loss_types.add('damage')  # single loss_type
         return risk_model
-
-    @property
-    def calculator_parameters(self):
-        """
-        The specific calculation parameters passed as args to the
-        celery task function. A calculator must override this to
-        provide custom arguments to its celery task
-        """
-        oqparam = self.job.get_oqparam()
-        oqparam.damage_state_ids = self.damage_state_ids
-        return oqparam
