418aad3ae57ac6318b440611ff8a479490aff46b
Michele Simionato <michele.simionato@gmail.com>
2014-12-15 15:08:58 +0100

    Assets without a cost must be imported anyway, otherwise fatalities cannot be computed

--- openquake/engine/calculators/risk/scenario_risk/core.py
+++ openquake/engine/calculators/risk/scenario_risk/core.py
@@ -21,7 +21,6 @@ import itertools
 import numpy
 from django import db
 
-from openquake.risklib import workflows
 from openquake.engine.calculators.risk import (
     base, hazard_getters, validation, writers)
 from openquake.engine.db import models
