4f6a672c682d11c6eccabb30b5ec8e0c8318fa0d
Michele Simionato <michele.simionato@gmail.com>
2014-12-17 15:28:00 +0100

    Changed an import

--- openquake/hazardlib/__init__.py
+++ openquake/hazardlib/__init__.py
@@ -17,12 +17,11 @@
 hazardlib stands for Hazard Library.
 """
 
-from openquake.baselib.general import git_suffix
 from openquake.hazardlib import (
     calc, geo, gsim, mfd, scalerel, source, const, correlation, imt, pmf, site,
-    tom
+    tom, general
 )
 
 # the version is managed by packager.sh with a sed
 __version__ = '0.12.1'
-__version__ += git_suffix(__file__)
+__version__ += general.git_suffix(__file__)
