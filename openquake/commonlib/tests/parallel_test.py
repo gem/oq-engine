47d348ed66a0c325daf8883fc9e866212a45d0d3
Michele Simionato <michele.simionato@gmail.com>
2015-01-18 11:15:24 +0100

    Changed the QA test case_17 to use two source models and fixed a bug in parallel.apply_reduce

--- openquake/commonlib/tests/parallel_test.py
+++ openquake/commonlib/tests/parallel_test.py
@@ -13,6 +13,15 @@ class TaskManagerTestCase(unittest.TestCase):
         self.assertEqual(res, {'n': 10})
         self.assertEqual(map(len, parallel.apply_reduce._chunks), [4, 4, 2])
 
+    # this case is non-trivial since there is a key, so two groups are
+    # generated even if everything is run in a single core
+    def test_apply_reduce_no_tasks(self):
+        res = parallel.apply_reduce(
+            get_length, ('aaabb',), concurrent_tasks=0, key=lambda char: char)
+        self.assertEqual(res, {'n': 5})
+        self.assertEqual(parallel.apply_reduce._chunks,
+                         [['a', 'a', 'a'], ['b', 'b']])
+
     def test_spawn(self):
         all_data = [
             ('a', range(10)), ('b', range(20)), ('c', range(15))]
