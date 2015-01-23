04363da1fa88626adf1b721774bcc29a5bc6228c
Daniele Viganò <daniele@vigano.me>
2015-01-23 10:20:48 +0100

    Fix pep8

--- openquake/hazardlib/general.py
+++ openquake/hazardlib/general.py
@@ -26,8 +26,9 @@ def git_suffix(fname):
     :returns: `<short git hash>` if Git repository found
     """
     try:
-        gh = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'],
-             stderr=open(os.devnull, 'w'), cwd=os.path.dirname(fname)).strip()
+        gh = subprocess.check_output(
+            ['git', 'rev-parse', '--short', 'HEAD'],
+            stderr=open(os.devnull, 'w'), cwd=os.path.dirname(fname)).strip()
         gh = "-git" + gh if gh else ''
         return gh
     except:
