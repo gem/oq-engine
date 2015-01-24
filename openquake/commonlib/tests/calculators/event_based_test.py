53491292c997e54cc665f8b001976967967efa1e
Michele Simionato <michele.simionato@gmail.com>
2015-01-24 08:24:04 +0100

    Ported the spatial correlation tests

--- openquake/commonlib/tests/calculators/event_based_test.py
+++ openquake/commonlib/tests/calculators/event_based_test.py
@@ -1,12 +1,95 @@
+import csv
+import math
 from nose.plugins.attrib import attr
 
+import numpy.testing
+
+from openquake.baselib.general import AccumDict
 from openquake.commonlib.tests.calculators import CalculatorTestCase
 from openquake.qa_tests_data.event_based import (
     blocksize, case_1, case_2, case_4, case_5, case_6, case_12, case_13,
     case_17)
+from openquake.qa_tests_data.event_based.spatial_correlation import (
+    case_1 as sc1, case_2 as sc2, case_3 as sc3)
+
+
+def joint_prob_of_occurrence(gmvs_site_1, gmvs_site_2, gmv, time_span,
+                             num_ses, delta_gmv=0.1):
+    """
+    Compute the Poissonian probability of a ground shaking value to be in the
+    range [``gmv`` - ``delta_gmv`` / 2, ``gmv`` + ``delta_gmv`` / 2] at two
+    different locations within a given ``time_span``.
+
+    :param gmvs_site_1, gmvs_site_2:
+        Lists of ground motion values (as floats) for two different sites.
+    :param gmv:
+        Reference value for computing joint probability.
+    :param time_span:
+        `investigation_time` parameter from the calculation which produced
+        these ground motion values.
+    :param num_ses:
+        `ses_per_logic_tree_path` parameter from the calculation which produced
+        these ground motion values. In other words, the total number of
+        stochastic event sets.
+    """
+    assert len(gmvs_site_1) == len(gmvs_site_2)
+
+    half_delta = float(delta_gmv) / 2
+    gmv_close = lambda v: (gmv - half_delta <= v <= gmv + half_delta)
+    count = 0
+    for gmv_site_1, gmv_site_2 in zip(gmvs_site_1, gmvs_site_2):
+        if gmv_close(gmv_site_1) and gmv_close(gmv_site_2):
+            count += 1
+
+    prob = 1 - math.exp(- (float(count) / (time_span * num_ses)) * time_span)
+    return prob
+
+
+def get_gmfs_by_imt(fname, imts):
+    """
+    Return a list of dictionaries with a ground motion field per IMT,
+    one dictionary per rupture.
+
+    :param fname: path to the CSV file
+    :param imts: the IMTs corresponding to the columns in the CSV file
+    """
+    dicts = []
+    with open(fname) as f:
+        for row in csv.reader(f):
+            gmf_by_imt = [map(float, col.split()) for col in row[1:]]
+            dic = AccumDict(zip(imts, gmf_by_imt))
+            dic.tag = row[0]
+            dicts.append(dic)
+    return sorted(dicts, key=lambda dic: dic.tag)
 
 
 class EventBasedTestCase(CalculatorTestCase):
+
+    @attr('qa', 'hazard', 'event_based')
+    def test_spatial_correlation(self):
+        expected = {sc1: [0.99, 0.41],
+                    sc2: [0.99, 0.64],
+                    sc3: [0.99, 0.22]}
+
+        for case in expected:
+            out = self.run_calc(case.__file__, 'job.ini')
+            oq = self.calc.oqparam
+            self.assertEqual(list(oq.imtls), ['PGA'])
+
+            gmfs = get_gmfs_by_imt(out['0-BooreAtkinson2008.csv'], oq.imtls)
+            gmvs_site_1 = [gmf['PGA'][0] for gmf in gmfs]
+            gmvs_site_2 = [gmf['PGA'][1] for gmf in gmfs]
+
+            joint_prob_0_5 = joint_prob_of_occurrence(
+                gmvs_site_1, gmvs_site_2, 0.5, oq.investigation_time,
+                oq.ses_per_logic_tree_path)
+            joint_prob_1_0 = joint_prob_of_occurrence(
+                gmvs_site_1, gmvs_site_2, 1.0, oq.investigation_time,
+                oq.ses_per_logic_tree_path)
+
+            p05, p10 = expected[case]
+            numpy.testing.assert_almost_equal(joint_prob_0_5, p05, decimal=1)
+            numpy.testing.assert_almost_equal(joint_prob_1_0, p10, decimal=1)
 
     @attr('qa', 'hazard', 'event_based')
     def test_blocksize(self):
