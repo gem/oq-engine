41e13111f8eec3041a9a0f09a0b0f2bc0575043a
Michele Simionato <michele.simionato@gmail.com>
2015-01-26 09:46:27 +0100

    Ported the classical test case_1

--- openquake/commonlib/tests/calculators/classical_test.py
+++ openquake/commonlib/tests/calculators/classical_test.py
@@ -1,20 +1,26 @@
 from nose.plugins.attrib import attr
 from openquake.commonlib.tests.calculators import CalculatorTestCase
-from openquake.qa_tests_data.classical import case_17
+from openquake.qa_tests_data.classical import case_1, case_17
 
 
 class ClassicalTestCase(CalculatorTestCase):
-    expected = [
-        'hazard_curve-smltp_b2-gsimltp_b1-ltr_0.csv',
-        'hazard_curve-smltp_b2-gsimltp_b1-ltr_1.csv',
-        'hazard_curve-smltp_b1-gsimltp_b1-ltr_2.csv',
-        'hazard_curve-smltp_b2-gsimltp_b1-ltr_3.csv',
-        'hazard_curve-smltp_b2-gsimltp_b1-ltr_4.csv',
-        'hazard_curve-mean.csv',
-    ]
+    @attr('qa', 'hazard', 'classical')
+    def test_case_1(self):
+        out = self.run_calc(case_1.__file__, 'job.ini', exports='csv')
+        self.assertEqualFiles(
+            'expected/hazard_curve-smltp_b1-gsimltp_b1-ltr_0.csv',
+            out['hazard_curve-smltp_b1-gsimltp_b1-ltr_0.csv'])
 
     @attr('qa', 'hazard', 'classical')
     def test_case_17(self):
+        expected = [
+            'hazard_curve-smltp_b2-gsimltp_b1-ltr_0.csv',
+            'hazard_curve-smltp_b2-gsimltp_b1-ltr_1.csv',
+            'hazard_curve-smltp_b1-gsimltp_b1-ltr_2.csv',
+            'hazard_curve-smltp_b2-gsimltp_b1-ltr_3.csv',
+            'hazard_curve-smltp_b2-gsimltp_b1-ltr_4.csv',
+            'hazard_curve-mean.csv',
+        ]
         out = self.run_calc(case_17.__file__, 'job.ini', exports='csv')
-        for fname in self.expected:
+        for fname in expected:
             self.assertEqualFiles('expected/%s' % fname, out[fname])
