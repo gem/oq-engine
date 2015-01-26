548b066bc35915df1e735b9806c0f38cdfede8e1
Michele Simionato <michele.simionato@gmail.com>
2015-01-26 09:55:53 +0100

    Ported the classical test case_3

--- openquake/commonlib/tests/calculators/classical_test.py
+++ openquake/commonlib/tests/calculators/classical_test.py
@@ -1,6 +1,7 @@
 from nose.plugins.attrib import attr
 from openquake.commonlib.tests.calculators import CalculatorTestCase
-from openquake.qa_tests_data.classical import case_1, case_2, case_17
+from openquake.qa_tests_data.classical import \
+    case_1, case_2, case_3, case_17
 
 
 class ClassicalTestCase(CalculatorTestCase):
@@ -14,6 +15,13 @@ class ClassicalTestCase(CalculatorTestCase):
     @attr('qa', 'hazard', 'classical')
     def test_case_2(self):
         out = self.run_calc(case_2.__file__, 'job.ini', exports='csv')
+        self.assertEqualFiles(
+            'expected/hazard_curve-smltp_b1-gsimltp_b1-ltr_0.csv',
+            out['hazard_curve-smltp_b1-gsimltp_b1-ltr_0.csv'])
+
+    @attr('qa', 'hazard', 'classical')
+    def test_case_3(self):
+        out = self.run_calc(case_3.__file__, 'job.ini', exports='csv')
         self.assertEqualFiles(
             'expected/hazard_curve-smltp_b1-gsimltp_b1-ltr_0.csv',
             out['hazard_curve-smltp_b1-gsimltp_b1-ltr_0.csv'])
