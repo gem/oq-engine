b2b949c914026837500b7c4c096dad56836eec17
Michele Simionato <michele.simionato@gmail.com>
2015-01-28 14:47:07 +0100

    Running the demos in risklib
    Former-commit-id: 55195b3e2ed4d052fdf357ffe49c91ec5b8345bd [formerly a68faadfacc37debedbeacd247eb2dddb79aae19]
    Former-commit-id: c78d70ff1fcb1bf6fcaf772fa9477fa2f8494fd5

--- packager.sh
+++ packager.sh
@@ -442,7 +442,7 @@ _pkgtest_innervm_run () {
     if [ -z "$GEM_PKGTEST_SKIP_DEMOS" ]; then
         # run all of the hazard and risk demos
         ssh $lxc_ip "set -e ; cd demos
-        for ini in \$(find ./hazard -name job.ini | sort); do
+        for ini in \$(find /usr/share/doc/python-oq-risklib/examples/demos -name job.ini | sort); do
             echo \"Running \$ini\"
             for loop in \$(seq 1 $GEM_MAXLOOP); do
                 set +e
@@ -461,14 +461,15 @@ _pkgtest_innervm_run () {
             fi
         done
 
-        for demo_dir in \$(find /usr/share/doc/python-oq-risklib/examples/demos
-                -mindepth 1 -maxdepth 1 -type d | sort); do
+        for demo_dir in \$(find /usr/share/doc/python-oq-risklib/examples/demos -mindepth 1 -maxdepth 1 -type d | sort); do
             cd \$demo_dir
+            if [ -f \$demo_dir/job_hazard.ini ]; then
             echo \"Running \$demo_dir/job_hazard.ini\"
             oq-engine --run-hazard job_hazard.ini -l info
             job_id=\$(oq-engine --list-hazard-calculations | tail -1 | awk '{print \$1}')
             echo \"Running \$demo_dir/job_risk.ini\"
             oq-engine --run-risk job_risk.ini --exports xml,csv --hazard-calculation-id \$job_id -l info
+            fi
             cd -
         done"
     fi
