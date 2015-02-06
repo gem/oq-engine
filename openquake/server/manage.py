758de2bc7c1cf4a0fce1d8d0dd650ae8fff256e2
Michele Simionato <michele.simionato@gmail.com>
2015-02-06 14:41:30 +0100

    Fixed manage.py for django 1.6
    Former-commit-id: 5dd3639dc26d154b56d6a7276732ba00be5d133b [formerly 5dd3639dc26d154b56d6a7276732ba00be5d133b [formerly 340a0d1fc22f1c70a573e2f2fe3e50bf33cfebe0]]
    Former-commit-id: a6059f48cf0e40e2e85e054a256b7e1d47232bf9
    Former-commit-id: b3d504f828d60218a28b3c981bf52681619138e9

--- openquake/server/manage.py
+++ openquake/server/manage.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 import os
 import sys
-import subprocess
 import celery
+import subprocess
 from django.core.management import execute_from_command_line
-import openquake.server.settings as settings
 from openquake.server import executor
 
 if celery.__version__ < '3.0.0':
@@ -15,14 +14,16 @@ else:
               '-l', 'INFO', '--purge', '--logfile', '/tmp/celery.log']
 
 
-# the code here is run in development mode
+# the code here is run in development mode; for instance
+# $ python manage.py runserver 0.0.0.0:8080 celery
 if __name__ == "__main__":
     os.environ.setdefault(
         "DJANGO_SETTINGS_MODULE", "openquake.server.settings")
     # the django autoreloader sets the variable RUN_MAIN; at the beginning
     # it is None, and only at that moment celery must be run
-    run_celery = settings.RUN_CELERY and os.environ.get("RUN_MAIN") is None
+    run_celery = 'celery' in sys.argv and os.environ.get("RUN_MAIN") is None
     if run_celery:
+        sys.argv.remove('celery')
         cel = subprocess.Popen(CELERY)
         print 'Starting celery, logging on /tmp/celery.log'
     try:
