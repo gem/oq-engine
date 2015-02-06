fec815949b577c6adf2608015e797645b0b41cb4
Michele Simionato <michele.simionato@gmail.com>
2015-02-06 09:03:18 +0100

    Supported both old and new celery in manage.py

--- openquake/server/manage.py
+++ openquake/server/manage.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python
 import os
 import sys
-import imp
 import subprocess
-from django.core.management import execute_manager
-try:
-    imp.find_module('settings')  # Assumed to be in the same directory.
-except ImportError:
-    sys.exit("""Error: Can't find the file 'settings.py' in the directory
-        containing %r. It appears you've customized things.\nYou'll have to
-        run django-admin.py, passing it your settings module.\n""" % __file__)
-import settings
+import celery
+from django.core.management import execute_from_command_line
+import openquake.server.settings as settings
 from openquake.server import executor
 
-CELERYD = [sys.executable, '-m', 'celery.bin.celeryd', '-l', 'INFO', '--purge',
-           '--logfile', '/tmp/celery.log']
+if celery.__version__ < '3.0.0':
+    CELERY = [sys.executable, '-m', 'celery.bin.celeryd', '-l', 'INFO',
+              '--purge', '--logfile', '/tmp/celery.log']
+else:
+    CELERY = [sys.executable, '-m', 'celery.bin.celery', 'worker',
+              '-l', 'INFO', '--purge', '--logfile', '/tmp/celery.log']
 
 
 # the code here is run in development mode
 if __name__ == "__main__":
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE", "openquake.server.settings")
     # the django autoreloader sets the variable RUN_MAIN; at the beginning
     # it is None, and only at that moment celery must be run
     run_celery = settings.RUN_CELERY and os.environ.get("RUN_MAIN") is None
     if run_celery:
-        cel = subprocess.Popen(CELERYD)
+        cel = subprocess.Popen(CELERY)
         print 'Starting celery, logging on /tmp/celery.log'
     try:
         with executor:
-            execute_manager(settings)
+            execute_from_command_line(sys.argv)
     finally:
         if run_celery:
             print '\nKilling celery'
