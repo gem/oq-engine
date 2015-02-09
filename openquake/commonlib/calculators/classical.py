dafffa2226b00a25b656dc4b91bfcfbe652a60d7
Michele Simionato <michele.simionato@gmail.com>
2015-02-09 11:37:06 +0100

    Added forgotten files

---
+++ openquake/commonlib/calculators/classical.py
@@ -0,0 +1,215 @@
+#  -*- coding: utf-8 -*-
+#  vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+#  Copyright (c) 2014, GEM Foundation
+
+#  OpenQuake is free software: you can redistribute it and/or modify it
+#  under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+
+#  OpenQuake is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
+#  You should have received a copy of the GNU Affero General Public License
+#  along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
+
+import operator
+import collections
+
+from openquake.hazardlib.site import SiteCollection
+from openquake.hazardlib.calc.hazard_curve import calc_hazard_curves
+from openquake.hazardlib.calc.filters import source_site_distance_filter, \
+    rupture_site_distance_filter
+from openquake.risklib import scientific
+from openquake.commonlib import parallel
+from openquake.commonlib.export import export
+from openquake.baselib.general import AccumDict, split_in_blocks
+
+from openquake.commonlib.calculators import base, calc
+
+
+HazardCurve = collections.namedtuple('HazardCurve', 'location poes')
+
+
+def classical(sources, sitecol, gsims_assoc, monitor):
+    """
+    :param sources:
+        a non-empty sequence of sources of homogeneous tectonic region type
+    :param sitecol:
+        a SiteCollection
+    :param gsims_assoc:
+        associations trt_model_id -> gsims
+    :param monitor:
+        a Monitor instance
+    :returns:
+        an AccumDict rlz -> curves
+    """
+    max_dist = monitor.oqparam.maximum_distance
+    truncation_level = monitor.oqparam.truncation_level
+    imtls = monitor.oqparam.imtls
+    trt_model_id = sources[0].trt_model_id
+    trt = sources[0].tectonic_region_type
+    gsims = gsims_assoc[trt_model_id]
+    result = AccumDict()
+    for gsim in gsims:
+        curves = calc_hazard_curves(
+            sources, sitecol, imtls, {trt: gsim}, truncation_level,
+            source_site_filter=source_site_distance_filter(max_dist),
+            rupture_site_filter=rupture_site_distance_filter(max_dist))
+        # notice that the rupture filter may remove everything
+        if sum(v.sum() for v in curves.itervalues()):
+            result[trt_model_id, str(gsim)] = AccumDict(curves)
+    return result
+
+
+@base.calculators.add('classical')
+class ClassicalCalculator(base.HazardCalculator):
+    """
+    Classical PSHA calculator
+    """
+    core_func = classical
+
+    def execute(self):
+        """
+        Run in parallel `core_func(sources, sitecol, monitor)`, by
+        parallelizing on the sources according to their weight and
+        tectonic region type.
+        """
+        monitor = self.monitor(self.core_func.__name__)
+        monitor.oqparam = self.oqparam
+        sources = list(self.composite_source_model.sources)
+        zero = AccumDict((key, AccumDict())
+                         for key in self.rlzs_assoc)
+        gsims_assoc = sum((sm.get_gsims_by_trt_id()
+                          for sm in self.composite_source_model), {})
+        return parallel.apply_reduce(
+            self.core_func.__func__,
+            (sources, self.sitecol, gsims_assoc, monitor),
+            agg=calc.agg_prob, acc=zero,
+            concurrent_tasks=self.oqparam.concurrent_tasks,
+            weight=operator.attrgetter('weight'),
+            key=operator.attrgetter('trt_model_id'))
+
+    def post_execute(self, result):
+        """
+        Collect the hazard curves by realization and export them.
+
+        :param result:
+            a dictionary of hazard curves dictionaries
+        """
+        curves_by_rlz = self.rlzs_assoc.combine(result)
+        rlzs = self.rlzs_assoc.realizations
+        oq = self.oqparam
+        nsites = len(self.sitecol)
+
+        # export curves
+        saved = AccumDict()
+        exports = oq.exports.split(',')
+        for rlz in rlzs:
+            smlt_path = '_'.join(rlz.sm_lt_path)
+            gsimlt_path = '_'.join(rlz.gsim_uid)
+            for fmt in exports:
+                fname = 'hazard_curve-smltp_%s-gsimltp_%s-ltr_%d.%s' % (
+                    smlt_path, gsimlt_path, rlz.ordinal, fmt)
+                saved += self.export_curves(curves_by_rlz[rlz], fmt, fname)
+        if len(rlzs) == 1:  # cannot compute statistics
+            return saved
+
+        weights = (None if oq.number_of_logic_tree_samples
+                   else [rlz.weight for rlz in rlzs])
+        curves_by_imt = {imt: [curves_by_rlz[rlz][imt] for rlz in rlzs]
+                         for imt in oq.imtls}
+        mean_curves = scientific.mean_curve(
+            [curves_by_rlz[rlz] for rlz in rlzs], weights)
+        quantile = {
+            q: {imt: scientific.quantile_curve(
+                curves_by_imt[imt], q, weights).reshape((nsites, -1))
+                for imt in oq.imtls}
+            for q in getattr(oq, 'quantile_hazard_curves', [])
+        }
+        for fmt in exports:
+            saved += self.export_curves(
+                mean_curves, fmt, 'hazard_curve-mean.%s' % fmt)
+            for q in quantile:
+                saved += self.export_curves(
+                    quantile[q], fmt, 'quantile_curve-%s.%s' % (q, fmt))
+        return saved
+
+    def hazard_maps(self, curves_by_imt):
+        """
+        Compute the hazard maps associated to the curves and returns
+        a dictionary of arrays.
+        """
+        return {imt:
+                calc.compute_hazard_maps(
+                    curves, self.oqparam.imtls[imt], self.oqparam.poes)
+                for imt, curves in curves_by_imt.iteritems()}
+
+    def export_curves(self, curves, fmt, fname):
+        """
+        :param curves: an array of N curves to export
+        :param fmt: the export format ('xml', 'csv', ...)
+        :param fname: the name of the exported file
+        """
+        saved = AccumDict()
+        oq = self.oqparam
+        export_dir = oq.export_dir
+        saved += export(
+            ('hazard_curves', fmt), export_dir, fname, self.sitecol, curves,
+            oq.imtls, oq.investigation_time)
+        if getattr(oq, 'hazard_maps', None):
+            hmaps = self.hazard_maps(curves)
+            saved += export(
+                ('hazard_curves', fmt), export_dir,
+                fname.replace('curve', 'map'), self.sitecol,
+                # hmaps is a dictionary IMT -> matrix(P, N)
+                {k: v.T for k, v in hmaps.iteritems()},
+                oq.imtls, oq.investigation_time)
+            if getattr(oq, 'uniform_hazard_spectra', None):
+                uhs_curves = calc.make_uhs(hmaps)
+                saved += export(
+                    ('uhs', fmt), oq.export_dir,
+                    fname.replace('curve', 'uhs'),
+                    self.sitecol, uhs_curves)
+        return saved
+
+
+@base.calculators.add('classical_tiling')
+class ClassicalTilingCalculator(ClassicalCalculator):
+    """
+    Classical Tiling calculator
+    """
+    prefilter = False
+
+    def execute(self):
+        """
+        Run in parallel `core_func(sources, sitecol, monitor)`, by
+        parallelizing on the sources according to their weight and
+        tectonic region type.
+        """
+        monitor = self.monitor(self.core_func.__name__)
+        monitor.oqparam = self.oqparam
+
+        self.tiles = map(SiteCollection, split_in_blocks(
+            self.sitecol, self.oqparam.concurrent_tasks or 1))
+        self.num_tiles = len(self.tiles)
+        zero = AccumDict((key, AccumDict())
+                         for key in self.rlzs_assoc)
+        all_args = []
+        for smodel in self.composite_source_model:
+            gsims_assoc = smodel.get_gsims_by_trt_id()
+            for trt_model in smodel.trt_models:
+                for tile in self.tiles:
+                    all_args.append(
+                        (trt_model.sources, tile, gsims_assoc, monitor))
+        return parallel.starmap(self.core_func.__func__, all_args).reduce(
+            calc.agg_prob, zero)
+
+    def post_execute(self, result):
+        effective_trts = set(trt_id for trt_id, _gsim in result)
+        self.rlzs_assoc = self.composite_source_model.get_rlzs_assoc(
+            lambda tm: tm.id in effective_trts)
+        ClassicalCalculator.post_execute(self, result)
