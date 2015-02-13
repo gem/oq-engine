58704888d456a21b97cf6b71a727c1198656c3a2
Michele Simionato <michele.simionato@gmail.com>
2015-02-13 19:35:19 +0100

    Added an export facility for the average losses in csv
    Former-commit-id: 48cb1d73b5b0e777de16ca7766546fe0c84e7c2f

--- openquake/engine/export/risk.py
+++ openquake/engine/export/risk.py
@@ -163,6 +163,20 @@ def export_loss_curve_xml(key, output, target):
     return dest
 
 
+@core.export_output.add(('loss_curve', 'csv'))
+def export_avgloss_csv(key, output, target):
+    """
+    Export `output` to `target` in csv format for a given loss type
+    """
+    dest = _get_result_export_dest(target, output)[:-3] + '.csv'
+    data = output.loss_curve.losscurvedata_set.all().order_by('asset_ref')
+    rows = [(c.location.x, c.location.y, c.asset_ref, c.asset_value,
+             c.average_loss, c.stddev_loss) for c in data]
+    # NB: the loss type can be extracted from c.loss_curve.loss_type
+    writers.save_csv(dest, rows)
+    return dest
+
+
 @core.export_output.add(('loss_map', 'xml'), ('loss_map', 'geojson'))
 def export_loss_map(key, output, target):
     """
