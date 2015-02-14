4d921cb7d8a8b6a43bc753f995b2446ae4d5ed33
Michele Simionato <michele.simionato@gmail.com>
2015-02-14 14:40:51 +0100

    Changed the export filenames to include the realization ordinal to avoid oversampling
    Former-commit-id: 0a29f4bf13af735fc940eaca5c67ed69fa6e4407 [formerly 8d40634793dbcbb423667fd44482d62875f9e25f]
    Former-commit-id: 86f9e0e4f1c65fabc4dc6202ad8529e362b69df1

--- openquake/engine/export/hazard.py
+++ openquake/engine/export/hazard.py
@@ -100,55 +100,31 @@ def _get_result_export_dest(calc_id, target, result, file_ext='xml'):
             ltr = result.lt_realization
             sm_ltp = core.LT_PATH_JOIN_TOKEN.join(ltr.sm_lt_path)
             gsim_ltp = core.LT_PATH_JOIN_TOKEN.join(ltr.gsim_lt_path)
-            if ltr.weight is None:
-                # Monte-Carlo logic tree sampling
-                filename = '%s-smltp_%s-gsimltp_%s-ltr_%s.%s' % (
-                    output_type, sm_ltp, gsim_ltp, ltr.ordinal, file_ext
-                )
-            else:
-                # End Branch Enumeration
-                filename = '%s-smltp_%s-gsimltp_%s.%s' % (
-                    output_type, sm_ltp, gsim_ltp, file_ext
-                )
+            filename = '%s-smltp_%s-gsimltp_%s-ltr_%s.%s' % (
+                output_type, sm_ltp, gsim_ltp, ltr.ordinal, file_ext)
+
     elif output_type == 'gmf':
         # only logic trees, no stats
         ltr = result.lt_realization
         sm_ltp = core.LT_PATH_JOIN_TOKEN.join(ltr.sm_lt_path)
         gsim_ltp = core.LT_PATH_JOIN_TOKEN.join(ltr.gsim_lt_path)
-        if ltr.weight is None:
-            # Monte-Carlo logic tree sampling
-            filename = '%s-smltp_%s-gsimltp_%s-ltr_%s.%s' % (
-                output_type, sm_ltp, gsim_ltp, ltr.ordinal, file_ext
-            )
-        else:
-            # End Branch Enumeration
-            filename = '%s-smltp_%s-gsimltp_%s.%s' % (
-                output_type, sm_ltp, gsim_ltp, file_ext
-            )
+        filename = '%s-smltp_%s-gsimltp_%s-ltr_%s.%s' % (
+            output_type, sm_ltp, gsim_ltp, ltr.ordinal, file_ext)
+
     elif output_type == 'ses':
         sm_ltp = core.LT_PATH_JOIN_TOKEN.join(result.sm_lt_path)
         filename = '%s-%s-smltp_%s.%s' % (
-            output_type, result.ordinal, sm_ltp, file_ext
-        )
+            output_type, result.ordinal, sm_ltp, file_ext)
+
     elif output_type == 'disagg_matrix':
         # only logic trees, no stats
-
         out = '%s(%s)' % (output_type, result.poe)
         location = 'lon_%s-lat_%s' % (result.location.x, result.location.y)
-
         ltr = result.lt_realization
         sm_ltp = core.LT_PATH_JOIN_TOKEN.join(ltr.sm_lt_path)
         gsim_ltp = core.LT_PATH_JOIN_TOKEN.join(ltr.gsim_lt_path)
-        if ltr.weight is None:
-            # Monte-Carlo logic tree sampling
-            filename = '%s-%s-smltp_%s-gsimltp_%s-ltr_%s.%s' % (
-                out, location, sm_ltp, gsim_ltp, ltr.ordinal, file_ext
-            )
-        else:
-            # End Branch Enumeration
-            filename = '%s-%s-smltp_%s-gsimltp_%s.%s' % (
-                out, location, sm_ltp, gsim_ltp, file_ext
-            )
+        filename = '%s-%s-smltp_%s-gsimltp_%s-ltr_%s.%s' % (
+            out, location, sm_ltp, gsim_ltp, ltr.ordinal, file_ext)
     else:
         filename = '%s.%s' % (output_type, file_ext)
 
