bbed48ae26213ae66e7a9b20f3ba3f74921850d9
Michele Simionato <michele.simionato@gmail.com>
2015-02-24 14:53:02 +0100

    Fixed the input weight

--- openquake/commonlib/readinput.py
+++ openquake/commonlib/readinput.py
@@ -498,7 +498,8 @@ def get_job_info(oqparam, source_models, sitecol):
     # The input weight is given by the number of ruptures generated
     # by the sources; for point sources however a corrective factor
     # given by the parameter `point_source_weight` is applied
-    input_weight = sum(src.weight or 0 for src_model in source_models
+    input_weight = sum((src.weight or 0) * src_model.samples
+                       for src_model in source_models
                        for trt_model in src_model.trt_models
                        for src in trt_model)
 
