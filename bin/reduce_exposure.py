f427cf5e2b945ff7ddedac792a1325c565c420d1
Michele Simionato <michele.simionato@gmail.com>
2015-02-26 10:41:59 +0100

    Changed the name of the reduced model

--- bin/reduce_exposure.py
+++ bin/reduce_exposure.py
@@ -19,6 +19,14 @@ def random_filter(objects, reduction_factor, seed=42):
     return out
 
 
+def reduced_fname(fname, reduction_factor):
+    """
+    Build the name of the reduced model
+    """
+    assert fname.endswith('.xml'), fname
+    return fname[:-4] + '_reduced_%s.xml' % reduction_factor
+
+
 def main(exposure_fname, reduction_factor):
     """
     Produce a new exposure from `exposure_fname` by sampling the assets.
@@ -27,10 +35,11 @@ def main(exposure_fname, reduction_factor):
     all_assets = len(exposure.assets)
     exposure.assets.nodes = random_filter(
         exposure.assets, float(reduction_factor))
-    with open(exposure_fname + '~', 'w') as f:
+    fname = reduced_fname(exposure_fname, reduction_factor)
+    with open(fname, 'w') as f:
         nrml.write([exposure], f)
     print 'Extracted %d assets out of %d: %s' % (
-        len(exposure.assets), all_assets, exposure_fname + '~')
+        len(exposure.assets), all_assets, fname)
 
 if __name__ == '__main__':
     parser = sap.Parser(main)
