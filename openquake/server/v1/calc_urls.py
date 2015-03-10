a24b3f49526b9e0289c9e2ba84713d5ab3e79e9f
Michele Simionato <michele.simionato@gmail.com>
2015-03-10 06:45:07 +0100

    Implemented a log API in the engine server and a simple log viewer
    Former-commit-id: 5eb62e6049c32bf1e8af1c25466a812d07ea8a42 [formerly fc871b009ac845c939dc422ae47933af1bee2489] [formerly fc871b009ac845c939dc422ae47933af1bee2489 [formerly bcee256459fb1fc6a7ea88531c96ceb42c036fd6]]
    Former-commit-id: a44ac040a53158ce396b1641c9054ce2f39f3616
    Former-commit-id: ef58be1b0bd0aa69621c5ce7769430d919d77c29

--- openquake/server/v1/calc_urls.py
+++ openquake/server/v1/calc_urls.py
@@ -6,6 +6,9 @@ urlpatterns = patterns(
     url(r'^(hazard|risk)$', 'calc'),
     url(r'^(\d+)$', 'calc_info'),
     url(r'^(\d+)/results$', 'calc_results'),
+    url(r'^(\d+)/log/size$', 'get_log_size'),
+    url(r'^(\d+)/log/(\d+):(\d*)$', 'get_log_slice'),
+    url(r'^(\d+)/log$', 'get_log'),
     url(r'^result/(\d+)$', 'get_result'),
     url(r'^run$', 'run_calc'),
 )
