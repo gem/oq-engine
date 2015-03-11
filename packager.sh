464c29838e1e41497910bac860f24f241a4a5caa
Daniele Viganò <daniele@vigano.me>
2015-03-11 10:38:16 +0100

    Check dnsmasq leases on all the interfaces

--- packager.sh
+++ packager.sh
@@ -361,8 +361,8 @@ _lxc_name_and_ip_get()
             lxc_name="$(grep "sudo lxc-console -n $GEM_EPHEM_NAME" /tmp/packager.eph.$$.log | sed "s/.*sudo lxc-console -n \($GEM_EPHEM_NAME\)/\1/g")"
             for e in $(seq 1 40); do
                 sleep 2
-                if grep -q "$lxc_name" /var/lib/misc/dnsmasq.leases ; then
-                    lxc_ip="$(grep " $lxc_name " /var/lib/misc/dnsmasq.leases | cut -d ' ' -f 3)"
+                if grep -q "$lxc_name" /var/lib/misc/dnsmasq*.leases ; then
+                    lxc_ip="$(grep " $lxc_name " /var/lib/misc/dnsmasq*.leases | cut -d ' ' -f 3)"
                     break
                 fi
             done
