188ae4e52c7f6ede6b4bbb83839c8c2f186e5766
Michele Simionato <michele.simionato@gmail.com>
2015-03-16 12:02:17 +0100

    Declassed an assert to a warning
    Former-commit-id: 7a8fc19ff96fa13d0e8bedcd36770026c0af8a96 [formerly 5370ae21ed4843260f35d5bf4f9e04c000471437] [formerly 5370ae21ed4843260f35d5bf4f9e04c000471437 [formerly 6a11b6f9f136e31d827b5ddc7336d98c1d435293]]
    Former-commit-id: 03ca3f01621c13db1511e676b148466147b3c7d1
    Former-commit-id: bb0a6765ae8501fa6336763a69cdbee7de788c59

--- openquake/engine/logs.py
+++ openquake/engine/logs.py
@@ -181,12 +181,12 @@ def handle(job, log_level='info', log_file=None):
     try:
         yield
     finally:
+        # sanity check to make sure that the logging on file is working
+        if log_file and os.path.getsize(log_file) == 0:
+            logging.root.warn('The log file %s is empty!?' % log_file)
         for handler in handlers:
             logging.root.removeHandler(handler)
-        # sanity check to make sure that the logging on file is working
-        if log_file:
-            assert os.path.getsize(log_file) > 0
-
+            
 
 class tracing(object):
     """
