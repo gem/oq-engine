b6254034935fd7685c4ddb250e39778e4f5c9e50
Michele Simionato <michele.simionato@gmail.com>
2015-03-20 10:14:24 +0100

    Added a test with 2 realizations

--- openquake/commonlib/tests/calculators/classical_risk_test.py
+++ openquake/commonlib/tests/calculators/classical_risk_test.py
@@ -2,7 +2,7 @@ import unittest
 from nose.plugins.attrib import attr
 
 from openquake.qa_tests_data.classical_risk import (
-    case_1, case_2, case_3)
+    case_1, case_2, case_3, case_4)
 from openquake.commonlib.tests.calculators import CalculatorTestCase
 
 
@@ -22,3 +22,12 @@ class ClassicalRiskTestCase(CalculatorTestCase):
                             exports='csv')
         self.assertEqualFiles(
             'expected/rlz-000-avg_loss.csv', out['rlz-000-avg_loss'])
+
+    @attr('qa', 'risk', 'classical_risk')
+    def test_case_4(self):
+        out = self.run_calc(case_4.__file__, 'job_haz.ini,job_risk.ini',
+                            exports='csv')
+        self.assertEqualFiles(
+            'expected/rlz-000-avg_loss.csv', out['rlz-000-avg_loss'])
+        self.assertEqualFiles(
+            'expected/rlz-001-avg_loss.csv', out['rlz-001-avg_loss'])
