f7ae308901bca087388c050459228abf481e0abf
Michele Simionato <michele.simionato@gmail.com>
2015-03-26 15:28:57 +0100

    Restored event_loss tests

--- openquake/commonlib/tests/calculators/event_loss_test.py
+++ openquake/commonlib/tests/calculators/event_loss_test.py
@@ -1,4 +1,3 @@
-import unittest
 from nose.plugins.attrib import attr
 
 from openquake.commonlib.tests.calculators import CalculatorTestCase
@@ -8,8 +7,6 @@ from openquake.qa_tests_data.event_based_risk import case_2
 class EventLossTestCase(CalculatorTestCase):
     @attr('qa', 'risk', 'event_loss')
     def test_case_2(self):
-        raise unittest.SkipTest  # until the test is fixed properly
-
         out = self.run_calc(case_2.__file__, 'job_haz.ini,job_risk.ini',
                             calculation_mode='event_loss', exports='csv')
 
