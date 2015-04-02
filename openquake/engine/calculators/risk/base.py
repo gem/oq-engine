f9ab1ea7f4cefe6b71af7b16337fbb2977f553b5
Michele Simionato <michele.simionato@gmail.com>
2015-04-02 05:18:17 +0200

    Fixed an incorrect asset_hazard_distance in the computation of best_maximum_distance
    Former-commit-id: befe5c5bf32961905ca9a4e5c91158ec6a996ea5 [formerly acaa75c7bd9c7b63857f0966c03afe45492e07aa]
    Former-commit-id: 4ffa1cfb5882acc25a3910f047ed7e0cee7be6ff

--- openquake/engine/calculators/risk/base.py
+++ openquake/engine/calculators/risk/base.py
@@ -157,7 +157,7 @@ class RiskCalculator(base.Calculator):
         self.oqparam.hazard_output = models.Output.objects.get(
             pk=self.oqparam.hazard_output_id) \
             if self.oqparam.hazard_output_id else None
-        dist = self.oqparam.asset_hazard_distance
+        dist = self.oqparam.maximum_distance
         grid_spacing = self.oqparam.region_grid_spacing
         if grid_spacing:
             dist = min(dist, grid_spacing * numpy.sqrt(2) / 2)
