7904f5c09867b2740aa8f557330312944ea7e707
Daniele Viganò <daniele@vigano.me>
2015-04-03 15:33:49 +0000

    Add authentication support for the engine server
    - Activate /admin
    - Add a new sqlite database for storing auth data
    - Add a new database router for auth_db
    - Add login_required decorator
    - Add django-pam as authentication backend
    
    
    Former-commit-id: c1dbbbe49c6a7a272628f3940c3b4aba7666d645

--- openquake/server/urls.py
+++ openquake/server/urls.py
@@ -1,7 +1,16 @@
 from django.conf.urls import patterns, url, include
+from django.contrib import admin
+
+admin.autodiscover()
 
 urlpatterns = patterns(
     '',
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login',
+        {'template_name': 'admin/login.html'}),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
+
     url(r'^engine_version$', 'openquake.server.views.get_engine_version'),
     url(r'^v1/calc/', include('openquake.server.v1.calc_urls')),
     url(r'^engineweb$', 'openquake.server.views.engineweb', name="index"),
