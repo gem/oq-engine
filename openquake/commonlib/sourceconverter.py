bf0c4e8002c51015ae052d54a1500399268c25f8
Michele Simionato <michele.simionato@gmail.com>
2015-04-08 09:03:17 +0200

    Added a forgotten monitor

--- openquake/commonlib/sourceconverter.py
+++ openquake/commonlib/sourceconverter.py
@@ -640,5 +640,5 @@ def filter_sources(sources, sitecol, maxdist):
             operator.add, [])
     else:
         # few sources and sites, filter sequentially on a single core
-        sources = _filter_sources(sources, sitecol, maxdist)
+        sources = _filter_sources(sources, sitecol, maxdist, mon)
     return sorted(sources, key=operator.attrgetter('source_id'))
