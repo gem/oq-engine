c875baaf3a6796b08c3fbb4f0ea6721ad956d70d
Michele Simionato <michele.simionato@gmail.com>
2015-04-08 16:59:36 +0200

    Fixed validation_test
    Former-commit-id: c92ff23d283030a989cf6f8014b3599b8d4f061f [formerly 3d6ad222b6f6334a632d4ddd47bdb396e5c43d12] [formerly 3d6ad222b6f6334a632d4ddd47bdb396e5c43d12 [formerly f74cfc22e7539119737fd653810e4214010f520c]]
    Former-commit-id: d34520c14cc0846df675d6d50ba25c1422780030
    Former-commit-id: 041e35e49c7ce44a7f32bd1d78b8b0d2cab0980b

--- openquake/engine/tests/calculators/risk/validation_test.py
+++ openquake/engine/tests/calculators/risk/validation_test.py
@@ -37,11 +37,11 @@ class HazardIMTTestCase(unittest.TestCase):
         calc.risk_model = {
             ('PGA', 'tax1'): workflow,
             ('PGV', 'tax2'): workflow}
-        calc.oqparam.imtls = {'PGA': None, 'PGV': None}
+        calc.oqparam.hazard_imtls = {'PGA': None, 'PGV': None}
         val = validation.HazardIMT(calc)
 
         self.assertIsNone(val.get_error())
-        calc.oqparam.imtls = {'PGA': None}
+        calc.oqparam.hazard_imtls = {'PGA': None}
         self.assertEqual(("There is no hazard output for: PGV. "
                           "The available IMTs are: PGA."), val.get_error())
 
