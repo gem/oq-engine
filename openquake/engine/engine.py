db6d3d3faba4f2ba872dc4eaffea764a9533f930
Michele Simionato <michele.simionato@gmail.com>
2015-04-08 16:56:13 +0200

    Fixed a demo
    Former-commit-id: c26780feb973ef5674eaa028931d86d62834c080 [formerly c26780feb973ef5674eaa028931d86d62834c080 [formerly 1a3af3361d1954b55bc27e701078f931f810b32b]]
    Former-commit-id: ec643fd2c2ab4a67e1e6537f1138242cba82801e
    Former-commit-id: e390440ada7ae05074b30bdbc6bf0e8b712cf48c

--- openquake/engine/engine.py
+++ openquake/engine/engine.py
@@ -486,6 +486,7 @@ def job_from_file(cfg_file_path, username, log_level='info', exports='',
             if name not in params:
                 params[name] = value
         params['hazard_investigation_time'] = hc.investigation_time
+        params['hazard_imtls'] = dict(hc.imtls)
         cfd = hc.continuous_fragility_discretization
         if cfd and cfd != oqparam.continuous_fragility_discretization:
             raise RuntimeError(
