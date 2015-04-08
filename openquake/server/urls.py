2dbf1726a9b64620fa7103f370200163c8851291
Matteo Nastasi <nastasi@openquake.org>
2015-04-08 12:45:41 +0000

    webui: prefixed with 'web_' web related views
    Former-commit-id: 81a4617a73d277950c62bd36cbfb1483d0735c1f

--- openquake/server/urls.py
+++ openquake/server/urls.py
@@ -4,9 +4,9 @@ urlpatterns = patterns(
     '',
     url(r'^engine_version$', 'openquake.server.views.get_engine_version'),
     url(r'^v1/calc/', include('openquake.server.v1.calc_urls')),
-    url(r'^engine$', 'openquake.server.views.engine', name="index"),
+    url(r'^engine$', 'openquake.server.views.web_engine', name="index"),
     url(r'^engine/(\d+)/outputs$',
-        'openquake.server.views.engine', name="outputs"),
+        'openquake.server.views.web_engine_get_outputs', name="outputs"),
     url(r'^engine/license$', 'openquake.server.views.license',
         name="license"),
 )
