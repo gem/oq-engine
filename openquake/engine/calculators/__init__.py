b91dcbb28748fba0c50e7187847ae52c38c4dfb1
Michele Simionato <michele.simionato@gmail.com>
2015-04-09 09:44:21 +0200

    Cleanup
    Former-commit-id: f55269ec202151831d965ebadb3f172a023151fe [formerly 944c195554d4db5896277bed52bcdd30b7f29941]
    Former-commit-id: 8e38788ea824ac8f8033063d5dfec6cf52e2ede1

--- openquake/engine/calculators/__init__.py
+++ openquake/engine/calculators/__init__.py
@@ -17,10 +17,8 @@
 """This package contains Hazard and Risk calculator classes."""
 
 from openquake.baselib.general import CallableDict, import_all
-from openquake.commonlib.source import TrtModel
 from openquake.commonlib import parallel
-from openquake.engine.utils import tasks, config
-from openquake.engine.performance import EnginePerformanceMonitor
+from openquake.engine.utils import tasks
 
 # monkey patch the parallel module
 parallel.starmap = tasks.starmap
@@ -30,23 +28,7 @@ parallel.litetask = tasks.oqtask
 # import the oq-lite calculators  *after* the patching
 from openquake.commonlib.calculators import base
 
-
-# monkey patch the base.BaseCalculator class
-def __init__(self, oqparam, monitor=None):
-    self.oqparam = oqparam
-    if not hasattr(self.oqparam, 'concurrent_tasks'):
-        self.oqparam.concurrent_tasks = int(
-            config.get('celery', 'concurrent_tasks'))
-    if not hasattr(self.oqparam, 'usecache'):
-        self.oqparam.usecache = False
-    self.max_input_weight = float(
-        config.get('hazard', 'max_input_weight'))
-    self.max_output_weight = float(
-        config.get('hazard', 'max_output_weight'))
-    TrtModel.POINT_SOURCE_WEIGHT = float(
-        config.get('hazard', 'point_source_weight'))
-
-base.BaseCalculator.__init__ = __init__
+# temporarily patching the BaseCalculator class
 base.BaseCalculator.post_process = lambda self: None
 base.BaseCalculator.export = lambda self, exports='': None
 base.BaseCalculator.clean_up = lambda self: None
