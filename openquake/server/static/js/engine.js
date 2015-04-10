cdfc0d0564f9d385e56d5b55e5d97da353a44909
Matteo Nastasi <nastasi@openquake.org>
2015-04-10 09:29:37 +0000

    webui: fixed source misalignment

--- openquake/server/static/js/engine.js
+++ openquake/server/static/js/engine.js
@@ -31,13 +31,13 @@
                       var pleaseWaitDiv = $('<div class="modal hide" id="pleaseWaitDialog" data-backdrop="static" data-keyboard="false"><div class="modal-header"><h1>Processing...</h1></div><div class="modal-body"><div class="progress progress-striped active"><div class="bar" style="width: 0%;"></div></div></div></div>');
                       return {
                           show: function(msg, progress) {
-                          $('h1', pleaseWaitDiv).text(msg);
-                          if (progress) {
-                          progressHandlingFunction({loaded: 0, total: 1});
-                          } else {
-                          progressHandlingFunction({loaded: 1, total: 1});
-                      }
-                              pleaseWaitDiv.modal();
+                              $('h1', pleaseWaitDiv).text(msg);
+                              if (progress) {
+                                  progressHandlingFunction({loaded: 0, total: 1});
+                              } else {
+                                  progressHandlingFunction({loaded: 1, total: 1});
+                              }
+                              pleaseWaitDiv.modal('show');
                           },
                           hide: function () {
                               pleaseWaitDiv.modal('hide');
