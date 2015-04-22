cbccbc1274994f4e5e0d60a427c1093c694d5ef5
Michele Simionato <michele.simionato@gmail.com>
2015-04-22 09:15:59 +0200

    Removed spurious tests for scenario_risk

--- openquake/commonlib/tests/calculators/scenario_risk_test.py
+++ openquake/commonlib/tests/calculators/scenario_risk_test.py
@@ -22,18 +22,3 @@ class ScenarioRiskTestCase(CalculatorTestCase):
     def test_occupants(self):
         out = self.run_calc(occupants.__file__, 'job_haz.ini,job_risk.ini')
         self.assertEqualFiles('expected/agg_loss.csv', out['agg', 'csv'])
-
-
-class _ScenarioRiskQATestCase(CalculatorTestCase):
-    cases = ['case_3a', 'case_7a']
-
-    @attr('qa', 'risk', 'scenario_damage')
-    def test_all(self):
-        datadir = os.path.join(
-            os.path.dirname(os.path.dirname(case_3.__file__)), 'data')
-        oq = readinput.get_oqparam(os.path.join(datadir, 'case_7a.ini'))
-        ScenarioCalculator(oq).run()  # save cache
-
-        for case in self.cases:
-            out = self.run_calc(datadir, '%s.ini' % case, usecache='0')
-            print out.keys()
