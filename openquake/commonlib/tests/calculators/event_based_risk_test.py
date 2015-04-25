1fbd1b88346f45d975c302995c7f45167a549a48
Michele Simionato <michele.simionato@gmail.com>
2015-04-25 06:35:52 +0200

    Renamed EventBasedRiskTestCase

--- openquake/commonlib/tests/calculators/event_based_risk_test.py
+++ openquake/commonlib/tests/calculators/event_based_risk_test.py
@@ -4,7 +4,7 @@ from openquake.commonlib.tests.calculators import CalculatorTestCase
 from openquake.qa_tests_data.event_based_risk import case_1, case_2, case_3
 
 
-class EventLossTestCase(CalculatorTestCase):
+class EventBasedRiskTestCase(CalculatorTestCase):
     @attr('qa', 'risk', 'event_based_risk')
     def test_case_1(self):
         out = self.run_calc(case_1.__file__, 'job_haz.ini,job_risk.ini',
