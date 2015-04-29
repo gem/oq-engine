155c5123ba811adaa812b7bef56bb91f9990d9e7
Daniele Viganò <daniele@vigano.me>
2015-04-29 07:57:37 +0000

    webui: add a generic helper function to get the authenticated user
    Former-commit-id: 4643e8f015242821d7884b3e1e1431689605b731 [formerly 47b02900549cf5a7d4f856e2588887a273579e55]
    Former-commit-id: 9217ea5f7a56ab15f0d5e3bb0df035dc7284409f

--- openquake/server/utils.py
+++ openquake/server/utils.py
@@ -1,6 +1,19 @@
 from openquake.engine import __version__ as oqversion
 
 
+def getusername(request):
+    """
+    Return the real user is authentication support is enabled and user is
+    authenticated, otherwise it returns "platform" as user for backward
+    compatibility.
+    """
+
+    user_name = (request.user.username if hasattr(request, 'user') and
+                 request.user.is_authenticated() else 'platform')
+
+    return user_name
+
+
 def oq_server_context_processor(request):
     """
     A custom context processor which allows injection of additional
