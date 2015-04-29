e6a5e1d2e20d6c6c8ffb4bd9cdfc3f9737089546
Michele Simionato <michele.simionato@gmail.com>
2015-04-29 15:33:41 +0200

    Added test case_6a

--- openquake/commonlib/tests/calculators/scenario_risk_test.py
+++ openquake/commonlib/tests/calculators/scenario_risk_test.py
@@ -1,7 +1,8 @@
+import os
 from nose.plugins.attrib import attr
 
 from openquake.qa_tests_data.scenario_risk import (
-    case_3, occupants)
+    case_3, occupants, case_6a)
 
 from openquake.commonlib.tests.calculators import CalculatorTestCase
 
@@ -21,3 +22,10 @@ class ScenarioRiskTestCase(CalculatorTestCase):
         out = self.run_calc(occupants.__file__, 'job_haz.ini,job_risk.ini')
         for fname in out['agg', 'csv']:
             self.assertEqualFiles('expected/agg_loss.csv', fname)
+
+    @attr('qa', 'risk', 'scenario_damage')
+    def test_case_6a(self):
+        out = self.run_calc(case_6a.__file__, 'job_haz.ini,job_risk.ini')
+        for fname in out['agg', 'csv']:
+            expected = os.path.join('expected', os.path.basename(fname))
+            self.assertEqualFiles(expected, fname)
