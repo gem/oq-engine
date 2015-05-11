d41d80f6b92eb4ea1845ef07dd7507d4e7e852b6
Matteo Nastasi <nastasi@openquake.org>
2015-05-11 15:14:51 +0200

    packager.sh: enable debug by form boolean entry

--- packager.sh
+++ packager.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# export PS4='+${BASH_SOURCE}:${LINENO}:${FUNCNAME[0]}: '
-if [ $GEM_SET_DEBUG ]; then
+if [ "$GEM_SET_DEBUG" = "true ]; then
+    export PS4='+${BASH_SOURCE}:${LINENO}:${FUNCNAME[0]}: '
     set -x
 fi
 set -e
