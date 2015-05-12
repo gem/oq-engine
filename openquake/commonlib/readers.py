f6276dedc896f54c296763fe280f47f6f9e12aef
Michele Simionato <michele.simionato@gmail.com>
2015-05-12 06:49:07 +0200

    More cleaning up

--- openquake/commonlib/readers.py
+++ openquake/commonlib/readers.py
@@ -41,19 +41,7 @@ def parse_header(header):
     return numpy.dtype(triples)
 
 
-def get_subtypes(dtype):
-    """
-    :param: a composite numpy dtype
-    :returns: the subtypes contained in the dtype
-    """
-    sd = []
-    for f in dtype.fields:
-        dt = dtype.fields[f][0]
-        sd.append((dt.subdtype[0].type if dt.subdtype else dt.type, dt.shape))
-    return sd
-
-
-def _cast_str(col, type, shape, lineno, fname):
+def _cast(col, type, shape, lineno, fname):
     # convert strings into tuples or numbers, used inside read_composite_array
     if shape:
         return tuple(map(type, col.split()))
@@ -75,8 +63,12 @@ def read_composite_array(fname, sep=','):
     with open(fname) as f:
         header = next(f)
         dtype = parse_header(header.split(sep))
-        subtypes = get_subtypes(dtype)
-        col_ids = range(1, len(subtypes) + 1)
+        ts_pairs = []  # [(type, shape), ...]
+        for name in dtype.fields:
+            dt = dtype.fields[name][0]
+            ts_pairs.append((dt.subdtype[0].type if dt.subdtype else dt.type,
+                             dt.shape))
+        col_ids = range(1, len(ts_pairs) + 1)
         num_columns = len(col_ids)
         records = []
         col, col_id = '', 0
@@ -88,8 +80,8 @@ def read_composite_array(fname, sep=','):
                     (num_columns, len(row), fname, i))
             try:
                 record = []
-                for (type, shape), col, col_id in zip(subtypes, row, col_ids):
-                    record.append(_cast_str(col, type, shape, i, fname))
+                for (type, shape), col, col_id in zip(ts_pairs, row, col_ids):
+                    record.append(_cast(col, type, shape, i, fname))
                 records.append(tuple(record))
             except Exception as e:
                 raise InvalidFile(
@@ -99,6 +91,7 @@ def read_composite_array(fname, sep=','):
         return numpy.array(records, dtype)
 
 
+# this is simple and without error checking for the moment
 def read_array(fname, sep=','):
     r"""
     Convert a CSV file without header into a numpy array of floats.
