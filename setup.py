c03c00e3862fc7b3780f612f4a9b043b19409cb1
Daniele Viganò <daniele@vigano.me>
2015-05-28 20:54:28 +0200

    setup.py: remove statement in the README about celeryconfig.py

--- setup.py
+++ setup.py
@@ -26,13 +26,6 @@ README = """
 OpenQuake is an open source application that allows users to
 compute seismic hazard and seismic risk of earthquakes on a global scale.
 
-Please note: the /usr/bin/oq-engine script requires a celeryconfig.py
-file in the PYTHONPATH.  Please make sure this is the case and that your
-celeryconfig.py file works with your python-celery setup.
-
-Feel free to copy /usr/share/openquake/engine/celeryconfig.py and revise it
-as needed.
-
 Copyright (C) 2010-2015, GEM Foundation.
 """
 
