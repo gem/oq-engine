dbf913684b0eb1f756014d6a28db128fd9f97af4
Michele Simionato <michele.simionato@gmail.com>
2015-06-09 06:53:02 +0200

    Added monitoring to hazardlib

---
+++ openquake/baselib/performance.py
@@ -0,0 +1,173 @@
+import os
+import re
+import time
+import operator
+import datetime
+import collections
+
+import numpy
+
+import psutil
+if psutil.__version__ > '2.0.0':  # Ubuntu 14.10
+    def virtual_memory():
+        return psutil.virtual_memory()
+
+    def memory_info(proc):
+        return proc.memory_info()
+
+elif psutil.__version__ >= '1.2.1':  # Ubuntu 14.04
+    def virtual_memory():
+        return psutil.virtual_memory()
+
+    def memory_info(proc):
+        return proc.get_memory_info()
+
+else:  # Ubuntu 12.04
+    def virtual_memory():
+        return psutil.phymem_usage()
+
+    def memory_info(proc):
+        return proc.get_memory_info()
+
+
+# this is not thread-safe
+class Monitor(object):
+    """
+    Measure the resident memory occupied by a list of processes during
+    the execution of a block of code. Should be used as a context manager,
+    as follows::
+
+     with PerformanceMonitor('do_something') as mon:
+         do_something()
+     print mon.mem
+
+    At the end of the block the PerformanceMonitor object will have the
+    following 5 public attributes:
+
+    .start_time: when the monitor started (a datetime object)
+    .duration: time elapsed between start and stop (in seconds)
+    .exc: None unless an exception happened inside the block of code
+    .mem: the memory delta in bytes
+
+    The behaviour of the PerformanceMonitor can be customized by subclassing it
+    and by overriding the method on_exit(), called at end and used to display
+    or store the results of the analysis.
+    """
+    def __init__(self, operation, monitor_dir=None, pid=None,
+                 autoflush=False, measuremem=False):
+        self.operation = operation
+        self.pid = pid
+        self.monitor_dir = monitor_dir
+        self.autoflush = autoflush
+        self.measuremem = measuremem
+        self._proc = None
+        self.mem = 0
+        self.duration = 0
+        self._start_time = time.time()
+
+    def write(self, row):
+        """Write a row in the performance file, if any"""
+        csv = self.monitor_csv
+        if csv:
+            open(csv, 'a').write('\t'.join(row) + '\n')
+
+    def measure_mem(self):
+        """A memory measurement (in bytes)"""
+        try:
+            if self._proc:
+                return memory_info(self._proc).rss
+        except psutil.AccessDenied:
+            # no access to information about this process
+            # don't not try to check it anymore
+            self._proc = None
+
+    @property
+    def start_time(self):
+        """
+        Datetime instance recording when the monitoring started
+        """
+        return datetime.fromtimestamp(self._start_time)
+
+    @property
+    def monitor_csv(self):
+        """
+        The path to the .csv where the monitor will write, or None.
+        """
+        if self.monitor_dir:
+            return os.path.join(
+                self.monitor_dir, 'performance-%d.csv' % self.pid)
+
+    def __enter__(self):
+        if not self.pid:
+            self.pid = os.getpid()
+            self._proc = psutil.Process(self.pid)
+        self.exc = None  # exception
+        self._start_time = time.time()
+        if self.measuremem:
+            self.start_mem = self.measure_mem()
+        return self
+
+    def __exit__(self, etype, exc, tb):
+        self.exc = exc
+        if self.measuremem:
+            self.stop_mem = self.measure_mem()
+            self.mem += self.stop_mem - self.start_mem
+        self.duration += time.time() - self._start_time
+        self.on_exit()
+
+    def on_exit(self):
+        "To be overridden in subclasses"
+        if self.autoflush:
+            self.flush()
+
+    def flush(self):
+        """
+        Save the measurements on the performance file
+        """
+        time_sec = str(self.duration)
+        memory_mb = str(self.mem / 1024. / 1024.) if self.measuremem else '0'
+        self.write([self.operation, time_sec, memory_mb])
+        self.duration = 0
+        self.mem = 0
+
+    def __call__(self, operation, **kw):
+        """
+        Return a copy of the monitor usable for a different operation.
+        """
+        self_vars = vars(self).copy()
+        del self_vars['operation']
+        new = self.__class__(operation)
+        vars(new).update(self_vars)
+        vars(new).update(kw)
+        new.pid = None
+        return new
+
+    def __repr__(self):
+        if self.measuremem:
+            return '<%s duration=%ss, memory=%dM>' % (
+                self.__class__.__name__, self.duration,
+                self.mem / 1024. / 1024.)
+        return '<%s duration=%ss>' % (self.__class__.__name__, self.duration)
+
+    def collect_performance(self):
+        """
+        :returns: a composite array (operation, time, memory, counts)
+        """
+        if self.monitor_dir is None:  # no monitoring info
+            return numpy.array([])
+        data = collections.defaultdict(lambda: numpy.zeros(3))
+        for f in os.listdir(self.monitor_dir):
+            mo = re.match(r'performance-(\d+)\.csv', f)
+            if mo:
+                fname = os.path.join(self.monitor_dir, f)
+                for line in open(fname):
+                    operation, time, memory = line.split('\t')
+                    data[operation] += numpy.array(
+                        [float(time), float(memory), 1])
+        perf_dt = numpy.dtype([('operation', (str, 50)), ('time_sec', float),
+                               ('memory_mb', float), ('counts', int)])
+        rows = []
+        for operation, rec in data.iteritems():
+            rows.append((operation, rec[0], rec[1], rec[2]))
+        rows.sort(key=operator.itemgetter(1), reverse=True)
+        return numpy.array(rows, perf_dt)
