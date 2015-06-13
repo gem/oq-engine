6700519f5530655962989f5bc30bc3fc099e315e
Michele Simionato <michele.simionato@gmail.com>
2015-06-13 05:23:39 +0200

    Fixed the classical_tiling parallel calculator

--- openquake/commonlib/tests/calculators/classical_tiling_test.py
+++ openquake/commonlib/tests/calculators/classical_tiling_test.py
@@ -1,4 +1,3 @@
-import unittest
 from nose.plugins.attrib import attr
 from openquake.commonlib.tests.calculators import CalculatorTestCase
 from openquake.qa_tests_data.classical_tiling import case_1
@@ -7,22 +6,24 @@ from openquake.qa_tests_data.classical_tiling import case_1
 class ClassicalTestCase(CalculatorTestCase):
     @attr('qa', 'hazard', 'classical_tiling')
     def test_case_1(self):
-        raise unittest.SkipTest  # temporarily skipped
         out = self.run_calc(case_1.__file__, 'job.ini', exports='csv')
         expected = [
             'hazard_curve-mean.csv',
-            'hazard_uhs-mean.csv',
             'hazard_curve-smltp_b1-gsimltp_b1.csv',
-            'hazard_uhs-smltp_b1-gsimltp_b1.csv',
             'hazard_curve-smltp_b1-gsimltp_b2.csv',
-            'hazard_uhs-smltp_b1-gsimltp_b2.csv',
-            'hazard_map-mean.csv',
             'quantile_curve-0.1.csv',
+            'hazard_map-mean.csv',
             'hazard_map-smltp_b1-gsimltp_b1.csv',
-            'quantile_map-0.1.csv',
             'hazard_map-smltp_b1-gsimltp_b2.csv',
+            'quantile_map-0.1.csv',
+            'hazard_uhs-mean.csv',
+            'hazard_uhs-smltp_b1-gsimltp_b1.csv',
+            'hazard_uhs-smltp_b1-gsimltp_b2.csv',
             'quantile_uhs-0.1.csv',
         ]
-        out = self.run_calc(case_1.__file__, 'job.ini', exports='csv')
-        for fname in expected:
-            self.assertEqualFiles('expected/%s' % fname, out[fname])
+        got = (out['/hcurves', 'csv'] +
+               out.get(('/hmaps', 'csv'), []) +
+               out.get(('/uhs', 'csv'), []))
+        self.assertEqual(len(expected), len(got))
+        for fname, actual in zip(expected, got):
+            self.assertEqualFiles('expected/%s' % fname, actual, delta=1E-6)
