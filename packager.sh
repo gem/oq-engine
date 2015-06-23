9342f46c29d875a3340a71783d25d6e82cf3cc7f
Michele Simionato <michele.simionato@gmail.com>
2015-06-23 11:17:20 +0200

    Updated the packager

--- packager.sh
+++ packager.sh
@@ -422,7 +422,8 @@ _pkgtest_innervm_run () {
         # run selected risk demos
         ssh $lxc_ip "set -e; cd /usr/share/doc/python-oq-risklib/examples/demos
         echo 'running ClassicalPSHA...'
-        oq-lite run ClassicalPSHA/job_hazard.ini,ClassicalPSHA/job_risk.ini
+        oq-lite run ClassicalPSHA/job_hazard.ini
+        oq-lite run ClassicalPSHA/job_risk.ini --hc -1
         echo 'running ScenarioDamage...'
         oq-lite run ScenarioDamage/job_hazard.ini,ScenarioDamage/job_risk.ini
         echo 'running ProbabilisticEventBased...'
