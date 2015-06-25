bf979c44196fe9c36506b33ca4942a369d45cb04
Daniele Viganò <daniele@vigano.me>
2015-06-25 09:35:13 +0200

    Update some links to the openquake website
    Former-commit-id: b81214319f80fc2c4940202f4ce9eb267a4239e7 [formerly af62d47c943ece3b096ce82ecea286e4485536ab]
    Former-commit-id: b1acbf9c8c5a154fea385092828b54ac9a493f62

--- openquake/engine/__init__.py
+++ openquake/engine/__init__.py
@@ -24,10 +24,10 @@ For more information, please see the website at http://www.globalquakemodel.org
 This software may be downloaded at http://github.com/gem/openquake
 
 The continuous integration server is at
-    http://openquake.globalquakemodel.org
+    https://ci.openquake.org
 
 Up-to-date sphinx documentation is at
-    http://openquake.globalquakemodel.org/docs
+    http://docs.openquake.org
 
 This software is licensed under the AGPL license, for more details
 please see the LICENSE file.
