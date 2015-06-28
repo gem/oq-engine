984193845cac74d4322ce35c40b438363e8012a6
Michele Simionato <michele.simionato@gmail.com>
2015-06-28 06:55:16 +0200

    Refined the test
    Former-commit-id: cd8b997217d2da124a8dc420699921df5d405af4 [formerly cd8b997217d2da124a8dc420699921df5d405af4 [formerly c1d80cf621b94740226d5ba425edf70b5b9a9448]]
    Former-commit-id: fb4c1f294bd0535e9ece9fa18aeb26b142b3cd17
    Former-commit-id: 13487761f95c2c070ca9076a349817c9aa9f4568

--- openquake/engine/tests/engine_test.py
+++ openquake/engine/tests/engine_test.py
@@ -146,18 +146,25 @@ class JobFromFileTestCase(unittest.TestCase):
 
 class RunCalcTestCase(unittest.TestCase):
     """
-    Test engine.run_calc in case of error
+    Test engine.run_calc in case of errors
     """
     def test(self):
         cfg = helpers.get_data_path('event_based_hazard/job.ini')
         job = engine.job_from_file(cfg, 'test_user')
         with tempfile.NamedTemporaryFile() as temp:
             with self.assertRaises(ZeroDivisionError), mock.patch(
-                    'openquake.engine.engine._do_run_calc', lambda *args: 1/0):
+                    'openquake.engine.engine._do_run_calc', lambda *args: 1/0
+            ), mock.patch('openquake.engine.engine.cleanup_after_job',
+                          lambda job: None):
                 engine.run_calc(job, 'info', temp.name, exports=[])
-            # make sure the error has been logged
-            self.assertIn('integer division or modulo by zero',
-                          open(temp.name).read())
+            logged = open(temp.name).read()
+
+            # make sure the real error has been logged
+            self.assertIn('integer division or modulo by zero', logged)
+
+            # also check the spurious cleanup error
+            self.assertIn('TypeError: <lambda>() got an unexpected keyword '
+                          "argument 'terminate'",  logged)
 
 
 class OpenquakeCliTestCase(unittest.TestCase):
