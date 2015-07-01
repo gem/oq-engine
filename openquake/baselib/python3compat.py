7b88543548c0ec26c8dcae1b8ab3f36d90f2af52
Michele Simionato <michele.simionato@gmail.com>
2015-07-01 07:25:08 +0200

    Added a python3-compatibility checker

---
+++ openquake/baselib/python3compat.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+import os
+import sys
+import importlib
+import subprocess
+
+
+def check_syntax(pkg):
+    ok, err = 0, 0
+    for cwd, dirs, files in os.walk(pkg.__path__[0]):
+        for f in files:
+            if f.endswith('.py'):
+                fname = os.path.join(cwd, f)
+                errno = subprocess.call(['python3', '-m', 'py_compile', fname])
+                if errno:
+                    err += 1
+                else:
+                    ok += 1
+    print('Checked %d ok, %d wrong modules' % (ok, err))
+
+
+if __name__ == '__main__':
+    check_syntax(importlib.import_module(sys.argv[1]))
