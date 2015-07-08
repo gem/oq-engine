ffb317788d2a9946514400ea17b7c0c73411c1a9
Michele Simionato <michele.simionato@gmail.com>
2015-07-08 06:00:00 +0200

    Fixed test_assetcol and remove an unused file

--- openquake/risklib/tests/riskinput_test.py
+++ openquake/risklib/tests/riskinput_test.py
@@ -35,12 +35,12 @@ class RiskInputTestCase(unittest.TestCase):
 
     def test_assetcol(self):
         expected = writetmp('''\
-asset_ref:|S20:,site_id:uint32:,taxonomy:uint32:,structural:float64:,deductible~structural:float64:,insurance_limit~structural:float64:
-a0,0,0,3000,25,100
-a1,1,0,2000,0.1,0.2
-a2,2,0,1000,0.02,0.08
-a3,2,0,5000,1000,3000
-a4,3,0,500000,1000,3000
+asset_ref:|S20:,site_id:uint32:,taxonomy:uint32:,fatalities:float64:,structural:float64:,deductible~structural:float64:,insurance_limit~structural:float64:
+a0,0,0,10,3000,25,100
+a1,1,0,20,2000,0.1,0.2
+a2,2,0,30,1000,0.02,0.08
+a3,2,0,0,5000,1000,3000
+a4,3,0,50,500000,1000,3000
 ''')
         assetcol = riskinput.build_asset_collection(self.assets_by_site)
         numpy.testing.assert_equal(
