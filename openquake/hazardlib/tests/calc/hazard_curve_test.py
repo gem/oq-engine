438557b2c2515ade355b9d3de74f95275445ea31
Michele Simionato <michele.simionato@gmail.com>
2015-07-17 16:43:03 +0200

    Fixed FakeSource class

--- openquake/hazardlib/tests/calc/hazard_curve_test.py
+++ openquake/hazardlib/tests/calc/hazard_curve_test.py
@@ -40,6 +40,7 @@ class HazardCurvesTestCase(unittest.TestCase):
             self.time_span = time_span
             self.ruptures = ruptures
             self.tectonic_region_type = trt
+            self.id = 0
 
         def iter_ruptures(self):
             return iter(self.ruptures)
