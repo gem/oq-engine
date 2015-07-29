837b9b9e1d74ff21dca63a0993b0971657ce9091
Michele Simionato <michele.simionato@gmail.com>
2015-07-29 13:38:57 +0200

    Added a custom TestLoader

--- setup.py
+++ setup.py
@@ -114,6 +114,7 @@ setup(
         "README.rst", "LICENSE", "CONTRIBUTORS.txt"]},
     namespace_packages=['openquake'],
     include_package_data=True,
-    test_suite='openquake.hazardlib',
+    test_loader='openquake.baselib.testloader:TestLoader',
+    test_suite='openquake.baselib,openquake.hazardlib',
     zip_safe=False,
 )
