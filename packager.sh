fe9f1eec4cf8195c3d4fd0cd783a250faca4caa5
Michele Simionato <michele.simionato@gmail.com>
2015-07-30 16:31:56 +0200

    Renamed a job.ini

--- packager.sh
+++ packager.sh
@@ -430,7 +430,7 @@ _pkgtest_innervm_run () {
         echo 'running ScenarioDamage...'
         oq-lite run ScenarioDamage/job_hazard.ini,ScenarioDamage/job_risk.ini
         echo 'running ProbabilisticEventBased...'
-        oq-lite run ProbabilisticEventBased/job_hazard.ini,ProbabilisticEventBased/job_risk.ini
+        oq-lite run ProbabilisticEventBased/job_risk.ini
         echo 'running EventLossTableDemo...'        
         oq-lite run ProbabilisticEventBased/job.ini
         oq-lite export -1 /event_loss_table-rlzs csv /tmp
