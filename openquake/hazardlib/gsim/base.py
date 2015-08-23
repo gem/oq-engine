4fe76307ea54238fe20c0dbcf7e41a36a5738133
Michele Simionato <michele.simionato@gmail.com>
2015-08-23 05:11:34 +0200

    Added a base __repr__ for the GSIMs

--- openquake/hazardlib/gsim/base.py
+++ openquake/hazardlib/gsim/base.py
@@ -111,7 +111,7 @@ class MetaGSIM(abc.ABCMeta):
     deprecated = False
     non_verified = False
 
-    def __call__(cls, *args, **kw):
+    def __call__(cls, **kwargs):
         if not cls.instantiable:
             raise NonInstantiableError(
                 '%s cannot be directly instantiated in this context' % cls)
@@ -123,7 +123,9 @@ class MetaGSIM(abc.ABCMeta):
             msg = ('%s is not independently verified - the user is liable '
                    'for their application') % cls.__name__
             warnings.warn(msg, NotVerifiedWarning)
-        return super(MetaGSIM, cls).__call__(*args, **kw)
+        self = super(MetaGSIM, cls).__call__(**kwargs)
+        self.kwargs = kwargs
+        return self
 
     # NB: the idea is to use this context manager inside the oqtask
     # decorator in the engine, so that GSIM classes cannot be directly
@@ -640,7 +642,14 @@ class GroundShakingIntensityModel(with_metaclass(MetaGSIM)):
         """
         To be overridden in subclasses if the GSIM takes parameters.
         """
-        return '%s' % self.__class__.__name__
+        return self.__class__.__name__
+
+    def __repr__(self):
+        """
+        To be overridden in subclasses if the GSIM takes parameters.
+        """
+        kwargs = ', '.join('%s=%r' % kv for kv in sorted(self.kwargs.items()))
+        return "'%s(%s)'" % (self.__class__.__name__, kwargs)
 
 
 def _truncnorm_sf(truncation_level, values):
