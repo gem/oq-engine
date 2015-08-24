8d706300b27e4d5738774acfb0317386b863c467
Michele Simionato <michele.simionato@gmail.com>
2015-08-24 11:15:54 +0200

    Fixed a doctest

--- openquake/commonlib/valid.py
+++ openquake/commonlib/valid.py
@@ -43,7 +43,7 @@ def gsim(value):
     Make sure the given value is the name of an available GSIM class.
 
     >>> gsim('BooreAtkinson2011')  # doctest: +ELLIPSIS
-    <openquake.hazardlib.gsim.boore_atkinson_2011.BooreAtkinson2011 ...>
+    'BooreAtkinson2011'
     """
     if value.endswith(')'):
         gsim_name, argstr = value[:-1].split('(', 1)
