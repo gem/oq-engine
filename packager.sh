8768a525316ddc5ec8c969f9808d6be7394b4fe7
Michele Simionato <michele.simionato@gmail.com>
2015-08-29 11:14:43 +0200

    Tried to run the tests in parallel

--- packager.sh
+++ packager.sh
@@ -191,7 +191,7 @@ _devtest_innervm_run () {
             # skip slow tests
             skip_tests="!slow,"
         fi
-        ssh $lxc_ip "cd $GEM_GIT_PACKAGE ; nosetests -v -a '${skip_tests}' --with-doctest --with-coverage --cover-package=openquake.hazardlib --with-xunit"
+        ssh $lxc_ip "cd $GEM_GIT_PACKAGE ; find . -name \*_test.py | parallel nosetests -v -a '${skip_tests}' --with-doctest --with-coverage --cover-package=openquake.hazardlib --with-xunit"
         scp "$lxc_ip:$GEM_GIT_PACKAGE/nosetests.xml" "out_${BUILD_UBUVER}/"
     else
         if [ -d $HOME/fake-data/$GEM_GIT_PACKAGE ]; then
