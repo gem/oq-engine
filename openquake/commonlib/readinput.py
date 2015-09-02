35d1bf1d426f9a4d9ebb7d30f530c56dfb7dfde6
Michele Simionato <michele.simionato@gmail.com>
2015-09-02 15:14:29 +0200

    Fixed scenario_damage_test

--- openquake/commonlib/readinput.py
+++ openquake/commonlib/readinput.py
@@ -809,8 +809,8 @@ def get_sitecol_assets(oqparam, exposure):
     mesh = geo.Mesh(numpy.array(lons), numpy.array(lats))
     sitecol = get_site_collection(oqparam, mesh)
     assets_by_site = []
-    for s in sitecol:
-        assets = assets_by_loc[s.location.longitude, s.location.latitude]
+    for lon, lat in zip(sitecol.lons, sitecol.lats):
+        assets = assets_by_loc[lon, lat]
         assets_by_site.append(sorted(assets, key=operator.attrgetter('id')))
     return sitecol, numpy.array(assets_by_site)
 
