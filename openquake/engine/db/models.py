26ce7661284b917f7e5ea3c5d9151597c5a56133
Michele Simionato <michele.simionato@gmail.com>
2015-09-21 10:57:17 +0200

    Fixed save_sites in the case of multiple assets on the same point
    Former-commit-id: ef8e0d2b364f1ea980b281209e4c5448b92e6140 [formerly ed5dff6b32f176205969cbecd240e252a0617379] [formerly ed5dff6b32f176205969cbecd240e252a0617379 [formerly f6ee22672180680db90fd356631d3fbcb8870ab3]]
    Former-commit-id: a352b8d653c20f9aa4569b6c77ade7f578c7a331
    Former-commit-id: 42a99ea1b9f1490b3fa83cc48744ddfe1a198d48

--- openquake/engine/db/models.py
+++ openquake/engine/db/models.py
@@ -42,6 +42,7 @@ from django.db import connections
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.gis.db import models as djm
 
+from openquake.baselib.general import groupby
 from openquake.hazardlib.imt import from_string
 from openquake.hazardlib import geo, correlation
 
@@ -484,10 +485,16 @@ def save_sites(job, coords):
     :param coords: a sequence of coordinates
     :returns: the ids of the inserted HazardSite instances
     """
+    groups = groupby(coords, lambda x: x)
     sites = [HazardSite(hazard_calculation=job,
                         lon=point[0], lat=point[1])
-             for point in coords]
-    return writer.CacheInserter.saveall(sites)
+             for point in groups]
+    site_ids = writer.CacheInserter.saveall(sites)
+    sids = []
+    for site_id, point in zip(site_ids, groups):
+        for p in groups[point]:
+            sids.append(site_id)
+    return sids
 
 
 def extract_from(objlist, attr):
