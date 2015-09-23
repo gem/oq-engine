abe3be3f896ae350f413caee2ee5fbd4ad8b951f
Michele Simionato <michele.simionato@gmail.com>
2015-09-23 10:50:13 +0200

    Fixed the engine server tests
    Former-commit-id: 5363fbdf859f53dec735268e360fd831f2f5b9c3 [formerly c209cd720fb5bb080746e02cbed09748b91c4c47] [formerly c209cd720fb5bb080746e02cbed09748b91c4c47 [formerly 6a59792d9445499a00e2dfcffa1265198bd055c7]]
    Former-commit-id: 3b2ecf7ed7c34dd8e326081a0d5b139c9ea5e786
    Former-commit-id: cdfa72739257199fa42116ce81e14ba7f66e21b2

--- openquake/server/views.py
+++ openquake/server/views.py
@@ -403,17 +403,19 @@ def calc_results(request, calc_id):
 
     response_data = []
     for result in results:
-        outtypes = output_types[result.output_type]
-        if outtypes == [u'']:  # for datastore
-            continue
+        try:
+            rtype = result.output_type
+            outtypes = output_types[rtype]
+        except KeyError:
+            try:
+                rtype = result.display_name
+                outtypes = output_types[rtype]
+            except KeyError:
+                continue  # non-exportable outputs should not be shown
         url = urlparse.urljoin(base_url, 'v1/calc/result/%d' % result.id)
         datum = dict(
-            id=result.id,
-            name=result.display_name,
-            type=result.output_type,
-            outtypes=outtypes,
-            url=url,
-        )
+            id=result.id, name=result.display_name, type=rtype,
+            outtypes=outtypes, url=url)
         response_data.append(datum)
 
     return HttpResponse(content=json.dumps(response_data))
@@ -492,8 +494,8 @@ def get_result(request, result_id):
     if exported is None:
         # Throw back a 404 if the exact export parameters are not supported
         return HttpResponseNotFound(
-            'export_type=%s is not supported for output_type=%s' %
-            (export_type, output.output_type))
+            'export_type=%s is not supported for %s' %
+            (export_type, output.display_name))
 
     content_type = EXPORT_CONTENT_TYPE_MAP.get(
         export_type, DEFAULT_CONTENT_TYPE)
