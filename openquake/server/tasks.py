c174ba532e412cb5c8febf1131eb051b90e0b19e
Michele Simionato <michele.simionato@gmail.com>
2015-10-04 14:18:01 +0200

    PEP8 fix
    Former-commit-id: dab414d18151f8ed6bf81b89940d41c2956add67

--- openquake/server/tasks.py
+++ openquake/server/tasks.py
@@ -133,7 +133,7 @@ def _trigger_migration(job, callback_url, foreign_calc_id, dbname="platform"):
         a key to extract database connection settings from settings.DATABASES
         in order to get a cursor from the platform database
     """
-    if not dbname in DATABASES:
+    if dbname not in DATABASES:
         logger.error('Unknow key %s in PLATFORM_DATABASES; '
                      'you forgot to set local_settings.py' % dbname)
         return
