ed0818abdffa702508e56237cc7558b6545c703a
Michele Simionato <michele.simionato@gmail.com>
2015-10-13 15:02:07 +0200

    Fixed version number

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
         'scipy',
         'futures',
         'decorator',
-        'psutil==1.2.1',
+        'psutil >= 0.4.1, <= 1.2.1',
     ],
     author='GEM Foundation',
     author_email='devops@openquake.org',
