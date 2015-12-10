b920c16baf2a3ce746152f6bf1ecfcd93264dc89
Michele Simionato <michele.simionato@gmail.com>
2015-12-10 18:25:49 +0100

    Fixed exports default

--- openquake/commonlib/commands/run.py
+++ openquake/commonlib/commands/run.py
@@ -1,7 +1,7 @@
 #  -*- coding: utf-8 -*-
 #  vim: tabstop=4 shiftwidth=4 softtabstop=4
 
-#  Copyright (c) 2014, GEM Foundation
+#  Copyright (c) 2014-2015, GEM Foundation
 
 #  OpenQuake is free software: you can redistribute it and/or modify it
 #  under the terms of the GNU Affero General Public License as published
@@ -114,7 +114,7 @@ def _run(job_ini, concurrent_tasks, pdb, loglevel, hc, exports):
     return calc
 
 
-def run(job_ini, slowest, concurrent_tasks, hc, exports,
+def run(job_ini, slowest, concurrent_tasks, hc, exports='',
         loglevel='info', pdb=None):
     """
     Run a calculation.
