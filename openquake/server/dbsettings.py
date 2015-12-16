5aed71f49d8164c527be0d57ad7f106fb3bd090f
Michele Simionato <michele.simionato@gmail.com>
2015-12-16 06:11:32 +0100

    Removed icebox support
    Former-commit-id: ab4e3ff774b40e6253202c4eedf22a219b9f9da6

--- openquake/server/dbsettings.py
+++
@@ -1,9 +0,0 @@
-PLATFORM_DATABASES = {
-    'oq-platform': {
-        'HOST': 'localhost',
-        'NAME': "oqplatform",
-        'USER': 'oqplatform',
-        'PASSWORD': 'openquake',
-        'PORT': 5432
-    }
-}
