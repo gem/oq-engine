8f017efbafb31f30a16003b79ba1fcb378ec1430
Michele Simionato <michele.simionato@gmail.com>
2015-12-16 06:11:32 +0100

    Removed icebox support
    Former-commit-id: c4b0557923e398692a20ec898fb1c419147527f3 [formerly 5aed71f49d8164c527be0d57ad7f106fb3bd090f] [formerly 5aed71f49d8164c527be0d57ad7f106fb3bd090f [formerly ab4e3ff774b40e6253202c4eedf22a219b9f9da6]]
    Former-commit-id: 97893de7bb4b22440a8422e243d353a5cd4715e0
    Former-commit-id: 6917fa795c62f207ec6561d5d4ca04cef9b11db8

--- openquake/server/tasks.py
+++ openquake/server/tasks.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 
-# Copyright (c) 2014, GEM Foundation.
+# Copyright (c) 2014-2015, GEM Foundation.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -20,17 +20,13 @@
 import sys
 import logging
 import traceback
-import collections
 import shutil
-import tempfile
-import psycopg2
 import urllib
 import urllib2
 
 from openquake.engine import engine
 from openquake.engine.db import models as oqe_models
 
-from openquake.server.dbsettings import PLATFORM_DATABASES as DATABASES
 from openquake.server.settings import DEBUG
 
 # all the logs are sent to the platform; one would need a different logger
@@ -124,58 +120,6 @@ def run_calc(job_id, calc_dir,
         calc.datastore.close()
     shutil.rmtree(calc_dir)
 
-    # If requested to, signal job completion and trigger a migration of
-    # results.
-    if None not in (callback_url, foreign_calc_id):
-        _trigger_migration(job, callback_url, foreign_calc_id, dbname)
-
-
-def _trigger_migration(job, callback_url, foreign_calc_id, dbname="platform"):
-    """
-    Helper function to initiate a post-calculation migration of results.
-
-    :param OqJob job:
-        The job with which the calculation has been run
-    :param str callback_url:
-        A URL to POST a request to for pulling results out of the
-        oq-engine-server.
-    :param str foreign_calc_id:
-        The id of the foreign calculation
-    :param str dbname:
-        a key to extract database connection settings from settings.DATABASES
-        in order to get a cursor from the platform database
-    """
-    if dbname not in DATABASES:
-        logger.error('Unknow key %s in PLATFORM_DATABASES; '
-                     'you forgot to set local_settings.py' % dbname)
-        return
-    host = DATABASES[dbname]['HOST']
-    if host == 'localhost':
-        msg = ('Using localhost as database host; probably '
-               'you forgot to override PLATFORM_DATABASES in '
-               'local_settings.py ')
-        logger.error(msg)
-        return
-
-    update_calculation(
-        callback_url,
-        description=job.get_param('description'),
-        status="transfering outputs")
-
-    platform_connection = psycopg2.connect(
-        host=host,
-        database=DATABASES[dbname]['NAME'],
-        user=DATABASES[dbname]['USER'],
-        password=DATABASES[dbname]['PASSWORD'],
-        port=DATABASES[dbname]['PORT'])
-
-    try:
-        for output in job.output_set.all():
-            copy_output(platform_connection, output, foreign_calc_id)
-        update_calculation(callback_url, status="creating layers")
-    finally:
-        platform_connection.close()
-
 
 def update_calculation(callback_url=None, **query):
     """
@@ -187,194 +131,3 @@ def update_calculation(callback_url=None, **query):
     # post to an external service
     url = urllib2.urlopen(callback_url, data=urllib.urlencode(query))
     url.close()
-
-
-# Simple structure that holds all the query components needed to
-# transfer calculation outputs from the engine database to the platform
-DbInterface = collections.namedtuple(
-    'DbInterface',
-    'export_query target_table fields import_query')
-
-
-DBINTERFACE = {
-    'hazard_curve': DbInterface(
-        """SELECT ST_AsText(location) as location, imls, poes
-            FROM hzrdr.hazard_curve_data
-            JOIN hzrdr.hazard_curve hc ON hc.id = hazard_curve_id
-            JOIN uiapi.output o ON o.id = hc.output_id
-            WHERE o.id = %(output_id)d""",
-        "icebox_hazardcurve",
-        "location varchar, imls float[], poes float[]",
-        """INSERT INTO
-           icebox_hazardcurve(output_layer_id, location, imls, poes)
-           SELECT %s, ST_GeomFromText(location, 4326), imls, poes
-           FROM temp_icebox_hazardcurve"""),
-    'hazard_map': DbInterface(
-        """SELECT unnest(lons) as longitude, unnest(lats) as latitude,
-                  unnest(imls)
-           FROM hzrdr.hazard_map
-           JOIN uiapi.output o ON o.id = output_id
-            WHERE o.id = %(output_id)d""",
-        "icebox_hazardmap",
-        "longitude float, latitude float, iml float",
-        """INSERT INTO
-           icebox_hazardmap(output_layer_id, iml, location)
-           SELECT %s, iml, ST_SetSRID(ST_MakePoint(longitude, latitude), 4326)
-           FROM temp_icebox_hazardmap"""),
-    # TODO: instead of the region_constraint, we should specify the convex
-    # hull of the exposure
-    'aggregate_loss': DbInterface(
-        """SELECT St_AsText(region_constraint), mean, std_dev
-           FROM riskr.aggregate_loss al
-           JOIN uiapi.output o ON o.id = al.output_id
-           JOIN uiapi.risk_calculation rc ON rc.id = %(calculation_id)d
-           WHERE o.id = %(output_id)d""",
-        "icebox_aggregateloss",
-        "region varchar, mean_loss float, stddev_loss float",
-        """INSERT INTO
-           icebox_aggregateloss(
-               output_layer_id, region, mean_loss, stddev_loss)
-           SELECT %s, St_GeomFromText(region, 4326), mean_loss, stddev_loss
-           FROM temp_icebox_aggregateloss"""),
-    'agg_loss_curve': DbInterface(
-        """SELECT ST_AsText(region_constraint) as region, losses, poes,
-                  average_loss, stddev_loss
-           FROM riskr.aggregate_loss_curve_data
-           JOIN riskr.loss_curve lc ON lc.id = loss_curve_id
-           JOIN uiapi.output o ON o.id = lc.output_id
-           JOIN uiapi.risk_calculation rc ON rc.id = %(calculation_id)d
-           WHERE o.id = %(output_id)d""",
-        "icebox_aggregatelosscurve",
-        ("region varchar, losses float[], poes float[], "
-         "average_loss float, stddev_loss float"),
-        """INSERT INTO icebox_aggregatelosscurve(
-               output_layer_id, region, losses, poes, mean_loss, stddev_loss)
-           SELECT %s, St_GeomFromText(region, 4326),
-                  losses, poes, average_loss, stddev_loss
-           FROM temp_icebox_aggregatelosscurve"""),
-    'loss_curve': DbInterface(
-        """SELECT ST_AsText(location) as location,
-                  loss_ratios, poes, average_loss_ratio, stddev_loss_ratio,
-                  asset_ref
-           FROM riskr.loss_curve_data
-           JOIN riskr.loss_curve lc ON lc.id = loss_curve_id
-           JOIN uiapi.output o ON o.id = lc.output_id
-           WHERE o.id = %(output_id)d""",
-        "icebox_losscurve",
-        ("location varchar, losses float[], poes float[], "
-         "average_loss float, stddev_loss float, asset_ref varchar"),
-        """INSERT INTO icebox_losscurve(
-               output_layer_id, location, losses, poes,
-               average_loss, stddev_loss, asset_ref)
-           SELECT %s, St_GeomFromText(location, 4326),
-                  losses, poes, average_loss, stddev_loss, asset_ref
-           FROM temp_icebox_losscurve"""),
-    'loss_map': DbInterface(
-        """SELECT ST_AsText(location) as location,
-                  value, std_dev, asset_ref
-           FROM riskr.loss_map_data
-           JOIN riskr.loss_map lm ON lm.id = loss_map_id
-           JOIN uiapi.output o ON o.id = lm.output_id
-           WHERE o.id = %(output_id)d""",
-        "icebox_lossmap",
-        "location varchar, loss float, stddev_loss float, asset_ref varchar",
-        """INSERT INTO icebox_lossmap(
-               output_layer_id, location, loss, stddev_loss, asset_ref)
-           SELECT %s, St_GeomFromText(location, 4326),
-                  loss, stddev_loss, asset_ref
-           FROM temp_icebox_lossmap"""),
-    'bcr_distribution': DbInterface(
-        """SELECT ST_AsText(location) as location,
-                  average_annual_loss_original,
-                  average_annual_loss_retrofitted,
-                  bcr,
-                  asset_ref
-           FROM riskr.bcr_distribution_data
-           JOIN riskr.bcr_distribution bd ON bd.id = bcr_distribution_id
-           JOIN uiapi.output o ON o.id = bd.output_id
-           WHERE o.id = %(output_id)d""",
-        "icebox_bcrdistribution",
-        ("location varchar, average_annual_loss_original float, "
-         "average_annual_loss_retrofitted float, bcr float, "
-         "asset_ref varchar"),
-        """INSERT INTO icebox_bcrdistribution(
-               output_layer_id, location,
-               average_annual_loss_original,
-               average_annual_loss_retrofitted, bcr, asset_ref)
-           SELECT %s, St_GeomFromText(location, 4326),
-                  average_annual_loss_original,
-                  average_annual_loss_retrofitted, bcr, asset_ref
-           FROM temp_icebox_bcrdistribution"""),
-}
-
-
-def copy_output(platform_connection, output, foreign_calculation_id):
-    """
-    Copy `output` data from the engine database to the platform one.
-
-    :param platform_connection: a psycopg2 connection handler
-    :param output: a :class:`openquake.engine.db.models.Output` object
-    :param foreign_calculation_id: the id of the foreign (platform) calculation
-    """
-
-    # the workflow is the following:
-    # 1) Insert a pointer to the output into the output_layer table
-    # 2) Create a temporary table on the platform
-    # 3) Copy data from the engine to a temporary file
-    # 4) Copy data to the temporary table from the temporary file
-    # 5) Move data from the temporary table to the persistent one
-    #    by considering foreign key issues
-    engine_cursor = oqe_models.getcursor('admin')
-    platform_cursor = platform_connection.cursor()
-
-    with tempfile.TemporaryFile() as temporary_file:
-        try:
-            platform_cursor.execute(
-                """INSERT INTO
-                   icebox_outputlayer(display_name, calculation_id, engine_id)
-                   VALUES(%s, %s, %s) RETURNING id""",
-                (output.display_name, foreign_calculation_id, output.id))
-
-            [[output_layer_id]] = platform_cursor.fetchall()
-
-            iface = DBINTERFACE.get(output.output_type)
-
-            if iface is None:
-                # FIXME. Implement proper logging
-                print "Output type %s not supported" % output.output_type
-                return
-
-            logger.info("Copying to temporary stream")
-            engine_cursor.copy_expert(
-                """COPY (%s) TO STDOUT
-                   WITH (FORMAT 'csv', HEADER true,
-                         ENCODING 'utf8', DELIMITER '|')""" % (
-                iface.export_query % {
-                    'output_id': output.id,
-                    'calculation_id': output.oq_job.id}),
-                temporary_file)
-
-            temporary_file.seek(0)
-
-            temp_table = "temp_%s" % iface.target_table
-            platform_cursor.execute("DROP TABLE IF EXISTS %s" % temp_table)
-            platform_cursor.execute("CREATE TABLE %s(%s)" % (
-                temp_table, iface.fields))
-
-            import_query = """COPY %s FROM STDIN
-                              WITH (FORMAT 'csv',
-                                    HEADER true,
-                                    ENCODING 'utf8',
-                                    DELIMITER '|')""" % temp_table
-            logger.info("Copying from temporary stream")
-            platform_cursor.copy_expert(import_query, temporary_file)
-
-            platform_cursor.execute(iface.import_query % output_layer_id)
-            platform_cursor.execute("DROP TABLE IF EXISTS %s" % temp_table)
-        except Exception as e:
-            # FIXME. Implement proper logging
-            print str(e)
-            platform_connection.rollback()
-            raise
-        else:
-            platform_connection.commit()
