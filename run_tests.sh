3b66c1a2fc650cdec799587b49dfa1b23dd13159
Michele Simionato <michele.simionato@gmail.com>
2015-12-17 08:52:12 +0100

    Removed all calculators
    Former-commit-id: 73227d58181d928aa6a797ebeb7142ebcc87a781

--- run_tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd openquake/server
-python manage.py test --settings=test_db_settings
-(export DJANGO_SETTINGS_MODULE="openquake.server.settings"; nosetests -v --with-coverage --cover-package=openquake.server)
-cd -
