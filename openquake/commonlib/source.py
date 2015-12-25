7f25946ca8321e39bc2fffa0c699adbcfc22b6cf
Michele Simionato <michele.simionato@gmail.com>
2015-12-25 08:16:49 +0100

    Fixed misprint

--- openquake/commonlib/source.py
+++ openquake/commonlib/source.py
@@ -565,7 +565,7 @@ class CompositeSourceModel(collections.Sequence):
         """
         if sitecol is None:
             for trt_model in self.trt_models:
-                for src in sorted(trt_model, operator.attrgetter('weight')):
+                for src in sorted(trt_model, key=operator.attrgetter('weight')):
                     yield src
             return
         assert maximum_distance
