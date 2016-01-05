76567a9fb146c8066d568d77453ef19718786ce7
Michele Simionato <michele.simionato@gmail.com>
2016-01-05 15:23:01 +0100

    Fixed export_hmaps_xml_json

--- openquake/commonlib/export/hazard.py
+++ openquake/commonlib/export/hazard.py
@@ -525,13 +525,13 @@ def export_hmaps_xml_json(ekey, dstore):
             gsimlt_path = ''
         maps = hmaps[kind]
         for imt in oq.imtls:
-            for i, poe in enumerate(oq.poes):
+            for poe in oq.poes:
                 suffix = '-%s-%s' % (poe, imt)
                 fname = hazard_curve_name(
                     dstore, ekey, kind + suffix, rlzs_assoc,
                     oq.number_of_logic_tree_samples)
-                data = [HazardMap(site[0], site[1], poes[imt][i])
-                        for site, poes in zip(sitemesh, maps)]
+                data = [HazardMap(site[0], site[1], hmap['%s~%s' % (imt, poe)])
+                        for site, hmap in zip(sitemesh, maps)]
                 writer = writercls(
                     fname, investigation_time=oq.investigation_time,
                     imt=imt, poe=poe,
