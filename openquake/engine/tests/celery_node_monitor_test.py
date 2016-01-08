5075c9216897910c079e20aa3f977328d978da8e
Michele Simionato <michele.simionato@gmail.com>
2016-01-08 06:11:40 +0100

    Restored correct usage of celery
    Former-commit-id: 7eb78c7378be9ddb6c173363f0e97df1dad4a23b [formerly c1a4ad7f502ed7ed3645d78701dd109f13d5816f]
    Former-commit-id: 4c4f9d9966a754079fc6617f4520559862496120

--- openquake/engine/tests/celery_node_monitor_test.py
+++ openquake/engine/tests/celery_node_monitor_test.py
@@ -12,7 +12,7 @@ class CeleryNodeMonitorTestCase(unittest.TestCase):
     def test_all_nodes_were_down(self):
         ping = self.inspect().ping
         ping.return_value = {}
-        mon = CeleryNodeMonitor(no_distribute=False, interval=1, celery_on=1)
+        mon = CeleryNodeMonitor(no_distribute=False, interval=1, use_celery=1)
         with self.assertRaises(SystemExit):
             mon.__enter__()
         self.assertEqual(ping.call_count, 1)  # called only once
@@ -20,7 +20,7 @@ class CeleryNodeMonitorTestCase(unittest.TestCase):
     def test_all_nodes_are_up(self):
         ping = self.inspect().ping
         ping.return_value = {'node1': []}
-        mon = CeleryNodeMonitor(no_distribute=False, interval=1, celery_on=1)
+        mon = CeleryNodeMonitor(no_distribute=False, interval=1, use_celery=1)
         with mon:
             time.sleep(1.1)
         # one ping was done in the thread, plus one at the beginning
@@ -29,7 +29,7 @@ class CeleryNodeMonitorTestCase(unittest.TestCase):
     def test_one_node_went_down(self):
         ping = self.inspect().ping
         ping.return_value = {'node1': []}
-        mon = CeleryNodeMonitor(no_distribute=False, interval=1, celery_on=1)
+        mon = CeleryNodeMonitor(no_distribute=False, interval=1, use_celery=1)
         with mon, mock.patch('openquake.engine.logs.LOG') as log:
             time.sleep(1.1)
             ping.return_value = {}
@@ -43,13 +43,13 @@ class CeleryNodeMonitorTestCase(unittest.TestCase):
     def test_AMQPException(self):
         ping = self.inspect().ping
         ping.side_effect = Exception(0, 'fake error', '')
-        mon = CeleryNodeMonitor(no_distribute=False, interval=1, celery_on=1)
+        mon = CeleryNodeMonitor(no_distribute=False, interval=1, use_celery=1)
         with mock.patch('openquake.engine.logs.LOG') as log:
             mon.ping(timeout=0.1)
             self.assertTrue(log.warn.called)
 
     def test_no_distribute(self):
-        with CeleryNodeMonitor(no_distribute=True, interval=0.1, celery_on=1):
+        with CeleryNodeMonitor(no_distribute=True, interval=0.1, use_celery=1):
             time.sleep(0.5)
         self.assertIsNone(self.inspect.call_args)
 
