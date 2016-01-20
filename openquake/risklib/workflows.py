ede2f1f44d1aa9f437d8172b8bf1ce77c1c4b999
Michele Simionato <michele.simionato@gmail.com>
2016-01-20 15:10:58 +0100

    Fixed EBR case

--- openquake/risklib/workflows.py
+++ openquake/risklib/workflows.py
@@ -223,7 +223,8 @@ def out_by_rlz(workflow, assets, hazards, epsilons, tags, loss_type):
     """
     out_by_rlz = List()
     out_by_rlz.imt_taxo = (workflow.imt, workflow.taxonomy)
-    out_by_rlz.curve_resolution = len(workflow.loss_ratios[loss_type])
+    out_by_rlz.curve_resolution = len(
+        workflow.loss_ratios[loss_type]) if workflow.loss_ratios else 0
     out_by_rlz.loss_type = loss_type
     out_by_rlz.assets = assets
     # extract the realizations from the first asset
