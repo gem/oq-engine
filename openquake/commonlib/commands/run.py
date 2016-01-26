350511d9ab01ac0f01dfd5058712bb2928399ed9
Matteo Nastasi <nastasi@openquake.org>
2016-01-26 11:21:14 +0100

    monkey patching to manage parent segfault case

--- openquake/commonlib/commands/run.py
+++ openquake/commonlib/commands/run.py
@@ -24,8 +24,9 @@ import io
 
 from openquake.baselib import performance, general
 from openquake.commonlib import sap, readinput, valid, datastore, oqvalidation
+from openquake.commonlib.concurrent_futures_process_mpatch import (
+    concurrent_futures_process_monkeypatch)
 from openquake.calculators import base, views
-
 CT = oqvalidation.OqParam.concurrent_tasks.default
 
 calc_path = None  # set only when the flag --slowest is given
@@ -136,6 +137,7 @@ def run(job_ini, slowest, hc, concurrent_tasks=CT, exports='',
     :param pdb:
         flag to enable pdb debugging on failing calculations
     """
+    concurrent_futures_process_monkeypatch()
     if slowest:
         prof = cProfile.Profile()
         stmt = '_run(job_ini, concurrent_tasks, pdb, loglevel, hc, exports)'
---
+++ openquake/commonlib/concurrent_futures_process_mpatch.py
@@ -0,0 +1,42 @@
+import sys
+import prctl
+import signal
+from concurrent.futures import process
+from concurrent.futures.process import _ResultItem
+
+
+def _process_worker(call_queue, result_queue):
+    """Evaluates calls from call_queue and places the results in result_queue.
+
+    This worker is run in a separate process.
+
+    Args:
+        call_queue: A multiprocessing.Queue of _CallItems that will be read and
+            evaluated by the worker.
+        result_queue: A multiprocessing.Queue of _ResultItems that will written
+            to by the worker.
+        shutdown: A multiprocessing.Event that will be set as a signal to the
+            worker that it should exit when call_queue is empty.
+    """
+    prctl.set_pdeathsig(signal.SIGKILL)
+    while True:
+        call_item = call_queue.get(block=True)
+        if call_item is None:
+            # Wake up queue management thread
+            result_queue.put(None)
+            return
+        try:
+            r = call_item.fn(*call_item.args, **call_item.kwargs)
+        except BaseException:
+            e = sys.exc_info()[1]
+            result_queue.put(_ResultItem(call_item.work_id,
+                                         exception=e))
+        else:
+            result_queue.put(_ResultItem(call_item.work_id,
+                                         result=r))
+
+
+def concurrent_futures_process_monkeypatch():
+    global process, _process_worker
+
+    process._process_worker = _process_worker
