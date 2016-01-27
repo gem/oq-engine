7d9162beb351285e81bb825352371afeaed1dd16
Matteo Nastasi <nastasi@openquake.org>
2016-01-27 15:31:26 +0100

    add description of bash_tests.sh script

--- openquake/risklib/tests/bin/bash_tests.sh
+++ openquake/risklib/tests/bin/bash_tests.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
+#
+# bash_test.sh  Copyright (c) 2016, GEM Foundation.
+#
+# OpenQuake is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# OpenQuake is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
+
+# DESCRIPTION
+#
+# This script get list of scripts inside its basedir and
+# run all (or one) of them, for each print exit status and
+# run time and building optionally a cumulative xunit report.
+#
+
 # set -x
 declare -a available_tests
 NL='
@@ -58,6 +81,7 @@ USAGE:
 # set PYTHONPATH properly
 # cd git repository base directory
 ./openquake/risklib/tests/bin/bash_tests.sh <all|<name of the test>> [<xunit_fname>]
+./openquake/risklib/tests/bin/bash_tests.sh <-h|--help>
 
 EOF
 
