3e4116719ede1d61ad92cac1521d0453ca70065a
Michele Simionato <michele.simionato@gmail.com>
2016-02-07 11:39:33 +0100

    Improved a logging message

--- openquake/calculators/event_based.py
+++ openquake/calculators/event_based.py
@@ -450,11 +450,12 @@ class EventBasedRuptureCalculator(ClassicalCalculator):
                         'The tag %s is long %d characters, it will be '
                         'truncated to 100 characters in the /tags array',
                         sr.tag, len(sr.tag))
-        logging.info('Saving the SES collection')
         with self.monitor('saving ruptures', autoflush=True):
             self.tags = numpy.array(tags, (bytes, 100))
             for sescol, col in zip(sescollection, cols):
                 nr = len(sescol)
+                logging.info('Saving the SES collection #%d with %d ruptures',
+                             col, nr)
                 key = 'sescollection/trtmod=%s-%s' % tuple(col)
                 self.datastore[key] = sescol
                 self.datastore.set_attrs(key, num_ruptures=nr)
