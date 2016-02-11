d0d1f77242d7aa234a4df5863c1733cfbfb267c0
Daniele Viganò <daniele@vigano.me>
2016-02-11 08:09:15 +0100

    Replace (c) with (C) in copyright

--- openquake/server/tasks.py
+++ openquake/server/tasks.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 
-# Copyright (c) 2014-2016-2015, GEM Foundation.
+# Copyright (C) 2014-2016-2015, GEM Foundation.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
