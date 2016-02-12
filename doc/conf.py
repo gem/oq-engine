d37947526387da5c7b3d3585bec1b59140894d26
Matteo Nastasi <nastasi@openquake.org>
2016-02-12 16:00:41 +0100

    sources headers grand refactoring

--- doc/conf.py
+++ doc/conf.py
@@ -11,7 +11,7 @@
 # OpenQuake is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake. If not, see <http://www.gnu.org/licenses/>.
@@ -27,6 +27,7 @@
 #
 # All configuration values have a default; values that are commented out
 # serve to show the default.
+
 import sys, os
 import openquake.risklib
 
--- openquake/risklib/tests/riskmodels_test.py
+++ openquake/risklib/tests/riskmodels_test.py
@@ -11,10 +11,11 @@
 # OpenQuake is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake. If not, see <http://www.gnu.org/licenses/>.
+
 
 import unittest
 import mock
