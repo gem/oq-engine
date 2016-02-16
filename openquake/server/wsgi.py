d48a6344844cc9763177661f0675fe10fa06140b
Michele Simionato <michele.simionato@gmail.com>
2016-02-16 13:50:58 +0100

    Renamed uiapi.oq_job -> job
    Former-commit-id: a0887678966c4260f6197cae1b04c8321921ffe6 [formerly a0887678966c4260f6197cae1b04c8321921ffe6 [formerly c02add36608afab1b199761f70a0ebeb5bc075de]]
    Former-commit-id: 454904c6d39a9a16d375b5e4b43a950866e7947e
    Former-commit-id: 83a5a7f678e2797417806b43c54c4b7dfe4a00c3

--- openquake/server/wsgi.py
+++ openquake/server/wsgi.py
@@ -24,7 +24,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openquake.server.settings")
 from openquake.server.db import models
 models.getcursor('job_init').execute(
     # cleanup of the flag oq_job.is_running
-    'UPDATE uiapi.oq_job SET is_running=false WHERE is_running')
+    'UPDATE job SET is_running=false WHERE is_running')
 
 # This application object is used by the development server
 # as well as any WSGI server configured to use this file.
