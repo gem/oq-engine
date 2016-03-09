34777069af4522eeeab3534488f3e6f2c79036b3
Michele Simionato <michele.simionato@gmail.com>
2016-03-09 14:39:19 +0100

    Added a DataManager object
    Former-commit-id: d27b3dad2313186b54c38dd3dcab5f6386e10ed1 [formerly d27b3dad2313186b54c38dd3dcab5f6386e10ed1 [formerly 5586806662dfb0ee57fda90deac41bfa06b6cf4c]]
    Former-commit-id: 5640704ffecbd8f9737e4feefe4c1ee959851e81
    Former-commit-id: 198e620602a00150edf8b401be371825260084d6

---
+++ openquake/server/datamanager.py
@@ -0,0 +1,78 @@
+#  -*- coding: utf-8 -*-
+#  vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+#  Copyright (c) 2016, GEM Foundation
+
+#  OpenQuake is free software: you can redistribute it and/or modify it
+#  under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+
+#  OpenQuake is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
+#  You should have received a copy of the GNU Affero General Public License
+#  along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+import threading
+from multiprocessing.connection import Client
+from openquake.baselib.performance import Monitor
+from openquake.baselib.hdf5 import Hdf5Dataset
+from openquake.commonlib.datastore import read
+
+
+class DataManager(object):
+    def __init__(self, port):
+        self.listener = Monitor.make_listener(port)
+        self.thread = threading.Thread(target=self.loop)
+
+    def __enter__(self):
+        self.thread.start()
+        return self
+
+    def __exit__(self, etype, exc, tb):
+        client = Client(Monitor.address, authkey=Monitor.authkey)
+        client.send(sys.exit)
+        client.close()
+        self.thread.join()
+        self.listener.close()
+        for ds in self.dstore.values():
+            ds.close()
+
+    def loop(self):
+        self.dstore = {}
+        while True:
+            conn = self.listener.accept()
+            cmd = conn.recv()
+            conn.close()
+            call = cmd[0]
+            if callable(call):
+                call(*cmd[1:])
+                continue
+            args = cmd[1:-1]
+            calc_id = cmd[-1]
+            try:
+                self.datastore = self.dstore[calc_id]
+            except KeyError:
+                self.datastore = self.dstore[calc_id] = read(calc_id, 'r+')
+            else:  # assume `call` is a method name, like `save` or `extend`
+                getattr(self, call)(*args)
+
+    def save(self, key, array):
+        """
+        :param key: datastore key
+        :param array: an array to save for the given key
+        """
+        self.datastore[key] = array
+        self.datastore.flush()
+
+    def extend(self, key, array):
+        """
+        :param key: datastore key
+        :param array: an array extending the dataset with the given key
+        """
+        Hdf5Dataset(self.datastore.hdf5[key]).extend(array)
+        self.datastore.flush()
