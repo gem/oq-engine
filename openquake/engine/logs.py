36c6ebd95faf11c9db1f60bd71d6e3a891408907
Michele Simionato <michele.simionato@gmail.com>
2016-03-10 14:43:26 +0100

    More work on the CmdServer
    Former-commit-id: 230090803a7342a5886a12c7719139e6e04c86e5 [formerly 230090803a7342a5886a12c7719139e6e04c86e5 [formerly 134659c9581c462bc6dbf02aa02887ffbdf3daa2]]
    Former-commit-id: 6ab19047653b54d3a5ed7d92eeab2c320c8c3a8c
    Former-commit-id: 477b2532462381a7b0512f14e9e4761b38b67b8e

--- openquake/engine/logs.py
+++ openquake/engine/logs.py
@@ -170,7 +170,7 @@ def handle(job, log_level='info', log_file=None):
     :param log_file:
          log file path (if None, logs on stdout only)
     """
-    oq = job.calc.oqparam
+    oq = job.oqparam
     handlers = [LogDatabaseHandler(job)]  # log on db always
     if log_file is None:
         handlers.append(LogStreamHandler(job))
