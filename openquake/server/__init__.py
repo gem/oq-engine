0121597884727dc2fb66d5c4db11d4cae6ae1864
Michele Simionato <michele.simionato@gmail.com>
2016-03-18 11:23:14 +0100

    Moved setting DJANGO_SETTINGS_MODULE in the right place
    Former-commit-id: 7c3fb67a181dbc5fdaef326d84412f8e9cc8aa85

--- openquake/server/__init__.py
+++ openquake/server/__init__.py
@@ -16,8 +16,12 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake. If not, see <http://www.gnu.org/licenses/>.
 
+import os
 from concurrent.futures import ThreadPoolExecutor
 
 # recommended setting for development
 
 executor = ThreadPoolExecutor(max_workers=1)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'openquake.server.settings')
+
