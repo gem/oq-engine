5aa50128df21c796c2855fd618482c00a09f9a8d
Michele Simionato <michele.simionato@gmail.com>
2016-03-18 11:18:13 +0100

    The engine is independent from the server, again

--- openquake/engine/bin/openquake_cli.py
+++ openquake/engine/bin/openquake_cli.py
@@ -40,10 +40,6 @@ if os.environ.get("OQ_ENGINE_USE_SRCDIR") is not None:
         0, join(dirname(dirname(__file__)), "openquake")
     )
 
-from openquake.engine import utils
-from openquake.engine.logs import dbcmd
-
-utils.config.abort_if_no_config_available()
 
 # Please note: the /usr/bin/oq-engine script requires a celeryconfig.py
 # file in the PYTHONPATH; when using binary packages, if a celeryconfig.py
@@ -54,17 +50,11 @@ try:
 except ImportError:
     sys.path.append('/usr/share/openquake/engine')
 
+from openquake.engine.utils import confirm, config
 import openquake.engine
-
-from openquake.engine import __version__
 from openquake.engine import engine, logs
 from openquake.commonlib import datastore
 from openquake.calculators import views
-from openquake.server.db import models, upgrade_manager
-from openquake.server.db.schema.upgrades import upgrader
-from openquake.engine.export import core
-from openquake.engine.tools.make_html_report import make_report
-from django.db import connection as conn
 
 HAZARD_OUTPUT_ARG = "--hazard-output-id"
 HAZARD_CALCULATION_ARG = "--hazard-calculation-id"
@@ -89,7 +79,7 @@ def run_job(cfg_file, log_level, log_file, exports='',
         ID of the previous calculation or None
     """
     job_ini = os.path.abspath(cfg_file)
-    job_id, oqparam = dbcmd(
+    job_id, oqparam = logs.dbcmd(
         'job_from_file', job_ini, getpass.getuser(), hazard_calculation_id)
     calc = engine.run_calc(job_id, oqparam, log_level, log_file, exports,
                            hazard_calculation_id=hazard_calculation_id)
@@ -97,7 +87,7 @@ def run_job(cfg_file, log_level, log_file, exports='',
     calc.monitor.flush()
     print('Calculation %d completed in %d seconds. Results:' % (
         job_id, duration))
-    for line in dbcmd('list_outputs', job_id, False):
+    for line in logs.dbcmd('list_outputs', job_id, False):
         print line
     return job_id
 
@@ -106,11 +96,11 @@ def delete_calculation(job_id, confirmed=False):
     """
     Delete a calculation and all associated outputs.
     """
-    if confirmed or utils.confirm(
+    if confirmed or confirm(
             'Are you sure you want to delete this calculation and all '
             'associated outputs?\nThis action cannot be undone. (y/n): '):
         try:
-            dbcmd('del_calc', job_id)
+            logs.dbcmd('del_calc', job_id)
         except RuntimeError as err:
             print(err)
 
@@ -261,7 +251,7 @@ def main():
     exports = args.exports or 'xml,csv'
 
     if args.version:
-        print __version__
+        print openquake.engine.__version__
         sys.exit(0)
 
     if args.run or args.run_hazard or args.run_risk:
@@ -272,9 +262,9 @@ def main():
         logging.basicConfig(level=logging.INFO)
 
     if args.config_file:
-        os.environ[utils.config.OQ_CONFIG_FILE_VAR] = \
+        os.environ[config.OQ_CONFIG_FILE_VAR] = \
             abspath(expanduser(args.config_file))
-        utils.config.refresh()
+        config.refresh()
 
     if args.no_distribute:
         os.environ[openquake.engine.NO_DISTRIBUTE_VAR] = '1'
@@ -283,36 +273,32 @@ def main():
         # check if the DbServer is up
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
-            err = sock.connect_ex(utils.config.DBS_ADDRESS)
+            err = sock.connect_ex(config.DBS_ADDRESS)
         finally:
             sock.close()
         if err:
             sys.exit('Please start the DbServer: '
                      'sudo -u openquake python -m openquake.server.dbserver &')
 
-    conn.cursor()  # connect to the database, so that conn.connection is valid
-
     if args.upgrade_db:
         logs.set_level('info')
-        msg = upgrade_manager.what_if_I_upgrade(
-            conn.connection, extract_scripts='read_scripts')
-        print msg
+        msg = logs.dbcmd('what_if_I_upgrade', 'read_scripts')
         if msg.startswith('Your database is already updated'):
             pass
-        elif args.yes or utils.confirm('Proceed? (y/n) '):
-            upgrade_manager.upgrade_db(conn.connection)
+        elif args.yes or confirm('Proceed? (y/n) '):
+            logs.dbcmd('upgrade_db')
         sys.exit(0)
 
     if args.version_db:
-        print upgrade_manager.version_db(conn.connection)
+        print logs.dbcmd('version_db')
         sys.exit(0)
 
     if args.what_if_I_upgrade:
-        print upgrade_manager.what_if_I_upgrade(conn.connection)
+        print logs.dbcmd('what_if_I_upgrade', 'extract_upgrade_scripts')
         sys.exit(0)
 
     # check if the db is outdated
-    outdated = dbcmd('check_outdated')
+    outdated = logs.dbcmd('check_outdated')
     if outdated:
         sys.exit(outdated)
 
@@ -320,7 +306,7 @@ def main():
     hc_id = args.hazard_calculation_id
     if hc_id and int(hc_id) < 0:
         # make it possible commands like `oq-engine --run job_risk.ini --hc -1`
-        hc_id = dbcmd('get_hc_id', int(hc_id))
+        hc_id = logs.dbcmd('get_hc_id', int(hc_id))
     if args.run:
         job_inis = map(expanduser, args.run.split(','))
         if len(job_inis) not in (1, 2):
@@ -344,7 +330,7 @@ def main():
                 args.exports, hazard_calculation_id=hc_id)
     # hazard
     elif args.list_hazard_calculations:
-        for line in dbcmd('list_calculations', 'hazard'):
+        for line in logs.dbcmd('list_calculations', 'hazard'):
             print line
     elif args.run_hazard is not None:
         log_file = expanduser(args.log_file) \
@@ -352,10 +338,10 @@ def main():
         run_job(expanduser(args.run_hazard), args.log_level,
                 log_file, args.exports)
     elif args.delete_calculation is not None:
-        dbcmd('delete_calculation', args.delete_calculation, args.yes)
+        logs.dbcmd('delete_calculation', args.delete_calculation, args.yes)
     # risk
     elif args.list_risk_calculations:
-        for line in dbcmd('list_calculations', 'risk'):
+        for line in logs.dbcmd('list_calculations', 'risk'):
             print line
     elif args.run_risk is not None:
         if args.hazard_calculation_id is None:
@@ -369,39 +355,42 @@ def main():
 
     # export
     elif args.make_html_report:
+        from django.db import connection as conn
+        from openquake.engine.tools.make_html_report import make_report
         print 'Written', make_report(conn, args.make_html_report)
         sys.exit(0)
 
     elif args.list_outputs is not None:
-        hc_id = dbcmd('get_hc_id', args.list_outputs)
-        for line in dbcmd('list_outputs', hc_id):
+        hc_id = logs.dbcmd('get_hc_id', args.list_outputs)
+        for line in logs.dbcmd('list_outputs', hc_id):
             print line
     elif args.show_view is not None:
         job_id, view_name = args.show_view
         print views.view(view_name, datastore.read(int(job_id)))
     elif args.show_log is not None:
-        hc_id = dbcmd('get_hc_id', args.show_log[0])
-        for line in dbcmd('get_log', hc_id):
+        hc_id = logs.dbcmd('get_hc_id', args.show_log[0])
+        for line in logs.dbcmd('get_log', hc_id):
             print line
 
     elif args.export_output is not None:
         output_id, target_dir = args.export_output
-        for line in dbcmd('export_output', int(output_id),
-                          expanduser(target_dir), exports):
+        for line in logs.dbcmd('export_output', int(output_id),
+                               expanduser(target_dir), exports):
             print line
 
     elif args.export_outputs is not None:
         job_id, target_dir = args.export_outputs
-        hc_id = dbcmd('get_hc_id', job_id)
-        for line in dbcmd('export_outputs', hc_id, expanduser(target_dir),
-                          exports):
+        hc_id = logs.dbcmd('get_hc_id', job_id)
+        for line in logs.dbcmd('export_outputs', hc_id, expanduser(target_dir),
+                               exports):
             print line
 
     elif args.delete_uncompleted_calculations:
-        dbcmd('delete_uncompleted_calculations')
+        logs.dbcmd('delete_uncompleted_calculations')
     else:
         arg_parser.print_usage()
 
 
 if __name__ == '__main__':
+    config.abort_if_no_config_available()
     main()
