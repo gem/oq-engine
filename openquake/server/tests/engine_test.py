c18889e5680f434cbe90aa25ba28bc901b708232
Michele Simionato <michele.simionato@gmail.com>
2016-04-12 05:44:18 +0200

    Fixed the database user
    Former-commit-id: 92167b4ea7f4e19eb0de8822393cedc85d69a304 [formerly 92167b4ea7f4e19eb0de8822393cedc85d69a304 [formerly d61986da5ed7e538ddc46bfcc1ea7f618479b97f]]
    Former-commit-id: d0f320c24fc15afe66abe50c28e760af72faeb04
    Former-commit-id: 818119657f4c00147663181ab0f2ee046de1803e

--- openquake/server/tests/engine_test.py
+++ openquake/server/tests/engine_test.py
@@ -36,6 +36,7 @@ def setup_module():
     os.close(fh)
     sys.stderr.write('Using the database %s\n' % tmpfile)
     DATABASE['name'] = tmpfile
+    DATABASE['user'] = getpass.getuser()
     connection.cursor()  # connect to the db
     upgrade_manager.upgrade_db(connection.connection)
 
