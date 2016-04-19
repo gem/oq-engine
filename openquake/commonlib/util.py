af13ac198624bdd1f71417fa0829247cd9087073
Michele Simionato <michele.simionato@gmail.com>
2016-04-19 10:22:30 +0200

    Fixed a doctest

--- openquake/commonlib/util.py
+++ openquake/commonlib/util.py
@@ -140,7 +140,7 @@ def get_ses_idx(etag):
 
 def get_serial(etag):
     """
-    >>> get_col_serial("ses=0007~src=1-3~rup=018-01")
+    >>> get_serial("ses=0007~src=1-3~rup=018-01")
     '018'
     """
     ses, src, rup = etag.split('~')
