9c9ea4964429a2a58fed59cdd797250ac9a67c55
Michele Simionato <michele.simionato@gmail.com>
2016-05-09 14:03:05 +0200

    Refined the control on OQ_DISTRIBUTE
    Former-commit-id: 3b1477bd03f095a838451bf4911451a6f8761e1d [formerly 80964441dfd07b560428c52aeb65b681c0f368a0]
    Former-commit-id: d3eff75f9e35a86cd94b797c28b8f3232e7c4939

--- openquake/engine/engine.py
+++ openquake/engine/engine.py
@@ -19,6 +19,7 @@
 """Engine: A collection of fundamental functions for initializing and running
 calculations."""
 
+import os
 import sys
 import signal
 import traceback
@@ -167,7 +168,7 @@ def run_calc(job_id, oqparam, log_level, log_file, exports,
     """
     monitor = Monitor('total runtime', measuremem=True)
     with logs.handle(job_id, log_level, log_file):  # run the job
-        if USE_CELERY:
+        if USE_CELERY and os.environ.get('OQ_DISTRIBUTE') == 'celery':
             set_concurrent_tasks_default()
         calc = base.calculators(oqparam, monitor, calc_id=job_id)
         tb = 'None\n'
