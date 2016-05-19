0aaf6618bf3c7930ed0d7f8ac038a77eac53cfb6
Daniele Viganò <daniele@vigano.me>
2016-05-19 09:36:56 +0200

    Revert "Remove old code which uses pwd"
    This reverts commit b27ad2bc68f701cd4c176abb1874f0c8f0346f8b [formerly 6ed222aea50b3aebb667285a745ec04bf470552c] [formerly 9856be6864ba460149134ed8439b540b46faee75] [formerly df303d4143fbedf98a9ce36087d143df30b8331b [formerly df303d4143fbedf98a9ce36087d143df30b8331b [formerly 7078350372d00e514f16ddc5e5c04799da6877fc]]].
    
    
    Former-commit-id: 80549e93fc4a4f6da7b86e226d72932a96c583d3 [formerly 80549e93fc4a4f6da7b86e226d72932a96c583d3 [formerly 4b2be32ed5abf8f91e596501c230a34b44bd4fa2]]
    Former-commit-id: 72878a62445c1fbd7e99a847ef631cb25c6d8464
    Former-commit-id: 3b0f58f288e5ee20c12eac8b35c4055344e3fb1e

--- openquake/engine/config.py
+++ openquake/engine/config.py
@@ -23,6 +23,7 @@ Various utility functions concerned with configuration.
 
 import ConfigParser
 import os
+import pwd
 import sys
 from contextlib import contextmanager
 
@@ -136,6 +137,16 @@ def abort_if_no_config_available():
         msg = ('Could not find a configuration file in %s. '
                'Probably your are not in the right directory'
                % cfg._get_paths())
+        print msg
+        sys.exit(2)
+    if not cfg.is_readable():
+        msg = (
+            "\nYou are not authorized to read any of the OpenQuake "
+            "configuration files.\n"
+            "Please contact a system administrator or run the following "
+            "command:\n\n"
+            "   sudo gpasswd --add %s openquake"
+            % pwd.getpwuid(os.geteuid()).pw_name)
         print msg
         sys.exit(2)
 
