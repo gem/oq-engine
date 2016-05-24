d097f1fb9d27a301bfb7c93e718cb300d2f4ed36
Michele Simionato <michele.simionato@gmail.com>
2016-05-24 10:24:51 +0200

    Added a comment

--- openquake/hazardlib/calc/hazard_curve.py
+++ openquake/hazardlib/calc/hazard_curve.py
@@ -220,8 +220,10 @@ def poe_map(src, s_sites, imtls, cmaker, trunclevel, bbs,
                     # protecting against possible refactoring errors
                     assert len(bs) == len(jb_dists) == len(closest_points)
                     for bb, dist, p in zip(bs, jb_dists, closest_points):
+                        # FIXME: perhaps this is not needed since there is
+                        # the FarAwayRupture check before
                         if dist < cmaker.maximum_distance:
-                            # rbuptures too far away are ignored
+                            # ruptures too far away are ignored
                             bb.update([dist], [p.longitude], [p.latitude])
     except Exception as err:
         etype, err, tb = sys.exc_info()
