2f03b1cc9db3b5106d5ae5667b7295ae248029a7
Michele Simionato <michele.simionato@gmail.com>
2016-05-27 06:23:00 +0200

    Fixed commonlib.tests

--- openquake/commands/tests/independence_test.py
+++ openquake/commands/tests/independence_test.py
@@ -37,6 +37,7 @@ class IndependenceTestCase(unittest.TestCase):
         assert_independent('openquake.risklib', 'openquake.calculators')
 
     def test_commonlib(self):
+        assert_independent('openquake.commonlib', 'openquake.calculators')
         assert_independent('openquake.commonlib.node', 'openquake.hazardlib')
         # NB: ugly: parallel uses GroundShakingIntensityModel
         # assert_independent('openquake.commonlib.parallel', 'openquake.hazardlib')
