bcf907924f79b1d01f6dcca83b02782bd1b4d66a
Michele Simionato <michele.simionato@gmail.com>
2016-06-08 09:51:23 +0200

    Renamed tm->taskman
    Former-commit-id: f5f56645186c8c7dcc35c948a3f6d41f70be2f2c

--- openquake/calculators/classical.py
+++ openquake/calculators/classical.py
@@ -234,8 +234,8 @@ class ClassicalCalculator(base.HazardCalculator):
         """
         monitor = self.monitor.new(self.core_task.__name__)
         monitor.oqparam = self.oqparam
-        curves_by_trt_id = self.tm.reduce(self.agg_dicts, self.zerodict())
-        self.save_data_transfer(self.tm)
+        curves_by_trt_id = self.taskman.reduce(self.agg_dicts, self.zerodict())
+        self.save_data_transfer(self.taskman)
         with self.monitor('store source_info', autoflush=True):
             self.store_source_info(curves_by_trt_id)
         self.rlzs_assoc = self.csm.info.get_rlzs_assoc(
@@ -245,9 +245,9 @@ class ClassicalCalculator(base.HazardCalculator):
 
     def store_source_info(self, curves_by_trt_id):
         # store the information about received data
-        received = self.tm.received
+        received = self.taskman.received
         if received:
-            tname = self.tm.name
+            tname = self.taskman.name
             self.datastore.save('job_info', {
                 tname + '_max_received_per_task': max(received),
                 tname + '_tot_received': sum(received),
