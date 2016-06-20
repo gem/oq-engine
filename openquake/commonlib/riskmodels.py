3967eb9d3137ccb4d63c635494f49ff6eefed9ff
Michele Simionato <michele.simionato@gmail.com>
2016-06-20 13:44:05 +0200

    Removed the slash-escaping for the taxonomy

--- openquake/commonlib/riskmodels.py
+++ openquake/commonlib/riskmodels.py
@@ -20,7 +20,6 @@
 Reading risk models for risk calculators
 """
 import re
-import urllib
 import collections
 
 import numpy
@@ -134,7 +133,7 @@ def get_risk_models(oqparam, kind=None):
                 # limit states; this may change in the future
                 assert limit_states == fm.limitStates, (
                     limit_states, fm.limitStates)
-                rdict[urllib.quote_plus(taxo)][loss_type] = ffl
+                rdict[taxo][loss_type] = ffl
                 # TODO: see if it is possible to remove the attribute
                 # below, used in classical_damage
                 ffl.steps_per_interval = oqparam.steps_per_interval
@@ -147,6 +146,6 @@ def get_risk_models(oqparam, kind=None):
         # to make sure they are strictly increasing
         for loss_type, rm in rmodels.items():
             for (imt, taxo), rf in rm.items():
-                rdict[urllib.quote_plus(taxo)][loss_type] = (
+                rdict[taxo][loss_type] = (
                     rf.strictly_increasing() if cl_risk else rf)
     return rdict
