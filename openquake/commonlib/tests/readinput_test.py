cb0fb99a6dbcc1a1b88b1ec0396c0676a5c22ed4
Michele Simionato <michele.simionato@gmail.com>
2016-06-28 15:56:46 +0200

    Removed a debug print

--- openquake/commonlib/tests/readinput_test.py
+++ openquake/commonlib/tests/readinput_test.py
@@ -783,7 +783,6 @@ class TestLoadCurvesTestCase(unittest.TestCase):
         self.assertEqual(sorted(oqparam.hazard_imtls.items()),
                          [('PGA', [0.005, 0.007, 0.0137, 0.0337]),
                           ('SA(0.025)', [0.005, 0.007, 0.0137])])
-        print(sitecol.mesh.lons, sitecol.mesh.lats)
         self.assertEqual(str(hcurves), '''\
 [([0.098727, 0.098265, 0.094956], [0.098728, 0.098216, 0.094945, 0.092947])
  ([0.98728, 0.98266, 0.94957], [0.98728, 0.98226, 0.94947, 0.92947])]''')
