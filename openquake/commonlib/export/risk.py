3337e43bf2cda60d81ec29c9e8c61bbb996f67e9
Michele Simionato <michele.simionato@gmail.com>
2016-06-29 14:54:24 +0200

    Fixed a few tests
    Former-commit-id: 05d5a31535dc1d8adcd5655089fb5aeb39eb3389 [formerly fd8d78ceb64cb12adabcae9a30d6c7301e0bad38]
    Former-commit-id: 29316b0f1014ff2b14f0217f448f1225076f20cc

--- openquake/commonlib/export/risk.py
+++ openquake/commonlib/export/risk.py
@@ -183,10 +183,10 @@ def export_agg_losses_ebr(ekey, dstore):
             if data.dtype['loss'].shape == (2,):  # insured losses
                 losses = data['loss'][:, 0]
                 inslosses = data['loss'][:, 1]
-                edata = [('event_tag', 'loss', 'loss_ins')] + zip(
-                    tags, losses, inslosses)
+                edata = [('event_tag', 'loss', 'loss_ins')] + list(
+                    zip(tags, losses, inslosses))
             else:
-                edata = [('event_tag', 'loss')] + zip(tags, data['loss'])
+                edata = [('event_tag', 'loss')] + list(zip(tags, data['loss']))
             writer.save(edata, dest)
     return writer.getsaved()
 
