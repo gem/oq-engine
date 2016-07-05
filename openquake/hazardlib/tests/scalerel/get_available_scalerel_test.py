3c164800e7f45579582e852d84528ad81265ae9d
g-weatherill <gaweatherill82@yahoo.co.uk>
2016-07-05 14:34:26 +0200

    Resolves failing tests in get_available_scalerel

--- openquake/hazardlib/tests/scalerel/get_available_scalerel_test.py
+++ openquake/hazardlib/tests/scalerel/get_available_scalerel_test.py
@@ -11,7 +11,8 @@ class AvailableScaleRelTestCase(unittest.TestCase):
                           'PointMSR': scalerel.point.PointMSR,
                           'CEUS2011': scalerel.ceus2011.CEUS2011, 
                           'StrasserInterface': scalerel.strasser2010.StrasserInterface,
-                          'StrasserIntraslab': scalerel.strasser2010.StrasserIntraslab},
+                          'StrasserIntraslab': scalerel.strasser2010.StrasserIntraslab,
+                          'WC1994_QCSS': scalerel.wc1994_qcss.WC1994_QCSS},
                          dict(scalerel.get_available_scalerel()))
 
     def test_get_available_area_scalerel(self):
@@ -26,7 +27,8 @@ class AvailableScaleRelTestCase(unittest.TestCase):
                           'WC1994': scalerel.wc1994.WC1994,
                           'CEUS2011': scalerel.ceus2011.CEUS2011,
                           'StrasserInterface': scalerel.strasser2010.StrasserInterface,
-                          'StrasserIntraslab': scalerel.strasser2010.StrasserIntraslab},
+                          'StrasserIntraslab': scalerel.strasser2010.StrasserIntraslab,
+                          'WC1994_QCSS': scalerel.wc1994_qcss.WC1994_QCSS},
                          dict(scalerel.get_available_magnitude_scalerel()))
 
     def test_get_available_sigma_area_scalerel(self):
@@ -39,5 +41,6 @@ class AvailableScaleRelTestCase(unittest.TestCase):
         self.assertEqual({'PeerMSR': scalerel.peer.PeerMSR,
                           'WC1994': scalerel.wc1994.WC1994,
                           'StrasserInterface': scalerel.strasser2010.StrasserInterface,
-                          'StrasserIntraslab': scalerel.strasser2010.StrasserIntraslab},
+                          'StrasserIntraslab': scalerel.strasser2010.StrasserIntraslab,
+                          'WC1994_QCSS': scalerel.wc1994_qcss.WC1994_QCSS},
                          dict(scalerel.get_available_sigma_magnitude_scalerel()))
