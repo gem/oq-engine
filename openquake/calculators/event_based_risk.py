abf9bc9b216d782fcc47dbfe83acc05853f12053
Michele Simionato <michele.simionato@gmail.com>
2016-08-16 04:57:14 +0200

    Added a test for the NO-GMFs error
    Former-commit-id: 62c1d6cc40e52fdee59a22579ee1aefd36d38ddd [formerly bf4cd3ad9c580a570dd310b1ad43215d7348ea54]
    Former-commit-id: 1a0081256f5f736000641b6b72f3f69fa740e91c

--- openquake/calculators/event_based_risk.py
+++ openquake/calculators/event_based_risk.py
@@ -358,7 +358,7 @@ class EventBasedRiskCalculator(base.RiskCalculator):
         logging.info('Generated %s of GMFs', humansize(self.gmfbytes))
         self.datastore.save('job_info', {'gmfbytes': self.gmfbytes})
         if self.gmfbytes == 0:
-            raise RuntimeError('No GMFs were generated, perhaps they are '
+            raise RuntimeError('No GMFs were generated, perhaps they were '
                                'all below the minimum_intensity threshold')
 
         if self.oqparam.asset_loss_table:
