6b425b391b79d986f3c11cac6a159cbfda405a7b
Michele Simionato <michele.simionato@gmail.com>
2016-09-06 08:39:47 +0200

    Implemented `oq show name:spec` for the views
    Former-commit-id: e0b3c1bc31f505cddaacb5dcd1c60a0263f52404 [formerly 0b2ba4cd3b29fd99792590bb0c476df97cf91063]
    Former-commit-id: e6cfd9defaaebad597f4231487cd79efa24767ac

--- openquake/commands/show.py
+++ openquake/commands/show.py
@@ -90,14 +90,14 @@ def show(what, calc_id=-1):
         print('Realizations in order of distance from the mean curves')
         for dist, rlz in sorted(dists):
             print('%s: rmsep=%s' % (rlz, dist))
-    elif what in datastore.view:
-        print(datastore.view(what, ds))
-    else:
+    elif what in ds:
         obj = ds[what]
         if hasattr(obj, 'value'):  # an array
             print(write_csv(io.StringIO(), obj.value))
         else:
             print(obj)
+    else:
+        print(datastore.view(what, ds))
 
     ds.close()
 
