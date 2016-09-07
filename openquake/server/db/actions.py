f10c7631bd053c8fbfe69c5bdd6720e278cade6a
Daniele Viganò <daniele@vigano.me>
2016-09-07 08:07:08 +0200

    Fix the SQL query for /v1/calc/list

--- openquake/server/db/actions.py
+++ openquake/server/db/actions.py
@@ -448,7 +448,7 @@ def get_calcs(db, request_get_dict, user_name, user_acl_on=False, id=None):
     if 'relevant' in request_get_dict:
         relevant = request_get_dict.get('relevant')
         filterdict['relevant'] = valid.boolean(relevant)
-    jobs = db('SELECT *, %s FROM job WHERE ?A ORDER BY id DESC' % JOB_TYPE,
+    jobs = db('SELECT *, %s FROM job ORDER BY id DESC' % JOB_TYPE,
               filterdict)
     return [(job.id, job.user_name, job.status, job.job_type,
              job.is_running, job.description) for job in jobs]
