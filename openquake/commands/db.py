dc20fb97112dd3bfd6b89cf7b7124c76b2da5a4b
Michele Simionato <michele.simionato@gmail.com>
2016-09-16 07:16:23 +0200

    Added a command oq db
    Former-commit-id: 5f3c5e806116071f2983fc90ed9df5b0615d2f85

---
+++ openquake/commands/db.py
@@ -0,0 +1,61 @@
+#  -*- coding: utf-8 -*-
+#  vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+#  Copyright (c) 2016, GEM Foundation
+
+#  OpenQuake is free software: you can redistribute it and/or modify it
+#  under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+
+#  OpenQuake is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+
+#  You should have received a copy of the GNU Affero General Public License
+#  along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
+import ast
+import inspect
+from decorator import getfullargspec
+from openquake.baselib import sap
+from openquake.engine import logs
+from openquake.server.db import actions
+
+commands = {}
+for func in vars(actions).values():
+    if inspect.isfunction(func):  # is really a function
+        argspec = getfullargspec(func)
+        if argspec.args[0] == 'db':  # is a db action
+            commands[func.__name__] = argspec.args[1:]
+
+
+def convert(strings):
+    """
+    Convert strings into literal Python objects
+    """
+    for s in strings:
+        try:
+            yield ast.literal_eval(s)
+        except:
+            yield s
+
+
+@sap.Script
+def db(cmd):
+    """
+    Run a database command
+    """
+    cmds = cmd.split()
+    if cmds[0] not in commands:
+        okcmds = '\n'.join('%s(%s)' % (name, ', '.join(args))
+                           for name, args in sorted(commands.items()))
+        print('Invalid command "%s": choose one from\n%s' % (cmds[0], okcmds))
+    elif len(cmds[1:]) != len(commands[cmds[0]]):
+        print('Wrong number of arguments, expected %s, got %s' % (
+            commands[cmds[0]], cmds[1:]))
+    else:
+        print(logs.dbcmd(*convert(cmds)))
+
+
+db.arg('cmd', 'db command')
