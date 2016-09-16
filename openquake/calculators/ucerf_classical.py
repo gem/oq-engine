a2f26374cab29348e46e5fcecd373b0c80558967
Michele Simionato <michele.simionato@gmail.com>
2016-09-16 15:35:48 +0200

    Fixed the ucerf calculators
    Former-commit-id: 0de08749dc1b798fcad9a4b1020515eaf8d2c53f [formerly 79821ee359f985761d04bc0490ce77fb731c7474] [formerly 79821ee359f985761d04bc0490ce77fb731c7474 [formerly 5040858ae3835b583f1f831a7b0e402783cd59bc]]
    Former-commit-id: 267683d81f4ab379dabb251e498948bc4fc389d8
    Former-commit-id: 51198906dda2e8d0b75df4701bec6ccc77ffeaca

--- openquake/calculators/ucerf_classical.py
+++ openquake/calculators/ucerf_classical.py
@@ -250,7 +250,7 @@ def hazard_curves_per_rupture_subset(rupset_idx, ucerf_source, sites, imtls,
                               imtls, cmaker, truncation_level, bbs,
                               ctx_mon, pne_mon, disagg_mon)
         monitor.calc_times.append(
-            (ucerf_source.id, len(sites), time.time() - t0))
+            (ucerf_source.source_id, len(sites), time.time() - t0))
         monitor.eff_ruptures += pne_mon.counts
     return pmap
 
@@ -389,7 +389,6 @@ class UcerfPSHACalculator(classical.PSHACalculator):
         for ordinal, (name, branch) in enumerate(branches):
             sg = copy.copy(self.src_group)
             sg.id = ordinal
-            sg.sources[0].id = ordinal
 
             # Update the event set
             sg.sources[0].idx_set = sg.sources[0].build_idx_set(branch.value)
@@ -400,8 +399,8 @@ class UcerfPSHACalculator(classical.PSHACalculator):
             self.gsim_lt, self.smlt, source_models, set_weight=False)
         self.rlzs_assoc = self.csm.info.get_rlzs_assoc()
         self.rup_data = {}
-        self.infos = []
         self.num_tiles = 1
+        self.infos = {}
 
     def gen_args(self, branches, ucerf_source, monitor):
         """
@@ -409,6 +408,8 @@ class UcerfPSHACalculator(classical.PSHACalculator):
         """
         for grp_id, branch in enumerate(branches):
             gsims = self.rlzs_assoc.gsims_by_grp_id[grp_id]
+            self.infos[grp_id, ucerf_source.source_id] = (
+                source.SourceInfo(ucerf_source))
             yield branch, ucerf_source, grp_id, self.sitecol, gsims, monitor
 
     def execute(self):
@@ -439,6 +440,10 @@ class UcerfPSHACalculator(classical.PSHACalculator):
             gsims = self.rlzs_assoc.gsims_by_grp_id[0]
             [(branch_id, branch)] = self.smlt.branches.items()
             branchname = branch.value
+            ucerf_source.src_group_id = 0
+            ucerf_source.weight = 1
+            self.infos[0, ucerf_source.source_id] = source.SourceInfo(
+                ucerf_source)
 
             logging.info('Getting the background point sources')
             with self.monitor('getting background sources', autoflush=True):
