601f327fff7871b99dc5a10a15680c59348e5c37
Michele Simionato <michele.simionato@gmail.com>
2016-10-08 16:47:22 +0200

    Reduced the splitting of fault sources
    Former-commit-id: a49006d33dc50b55c7d7ee0e6c914d43a7ccf520

--- openquake/calculators/classical.py
+++ openquake/calculators/classical.py
@@ -367,9 +367,11 @@ class PSHACalculator(base.HazardCalculator):
                     self.infos[sg.id, src.source_id] = source.SourceInfo(src)
                     sources = split_filter_source(
                         src, sites, self.ss_filter, self.random_seed)
-                    logging.info(
-                        'Splitting %s "%s" in %d sources',
-                        src.__class__.__name__, src.source_id, len(sources))
+                    if len(sources) > 1:
+                        logging.info(
+                            'Splitting %s "%s" in %d sources',
+                            src.__class__.__name__,
+                            src.source_id, len(sources))
                     for block in block_splitter(
                             sources, maxweight,
                             weight=operator.attrgetter('weight')):
