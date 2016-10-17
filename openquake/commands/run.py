dc05fa115c6993914cb3f656afbc7e0b231cdeba
Michele Simionato <michele.simionato@gmail.com>
2016-10-17 15:12:00 +0200

    Removed concurrent_futures_process_mpatch
    Former-commit-id: ed81a33fe4226aaef8d0f75c7265d8a1440b8480 [formerly 99a6620f6ea1e978229393249e76e2d6bd5b99db] [formerly 99a6620f6ea1e978229393249e76e2d6bd5b99db [formerly dbf0ca6d54ab5f17b2334e334103902306141302]]
    Former-commit-id: a19f92a872a7897945896288c3b7e959bb5799c8
    Former-commit-id: 72b90b69e11e522134f8c4af7ba71fa44a033c72

--- openquake/commands/run.py
+++ openquake/commands/run.py
@@ -26,8 +26,6 @@ import io
 from openquake.baselib import performance, general, sap
 from openquake.risklib import valid
 from openquake.commonlib import readinput, datastore, oqvalidation
-from openquake.commonlib.concurrent_futures_process_mpatch import (
-    concurrent_futures_process_monkeypatch)
 from openquake.calculators import base, views
 CT = oqvalidation.OqParam.concurrent_tasks.default
 
@@ -134,7 +132,6 @@ def run(job_ini, slowest, hc, param, concurrent_tasks=CT, exports='',
     """
     Run a calculation.
     """
-    concurrent_futures_process_monkeypatch()
     params = oqvalidation.OqParam.check(
         dict(p.split('=', 1) for p in param or ()))
     if slowest:
