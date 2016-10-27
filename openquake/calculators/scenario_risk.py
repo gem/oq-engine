47731f86de1ea57c1d40e7b42091d0bf46793927
Michele Simionato <michele.simionato@gmail.com>
2016-10-27 15:58:16 +0200

    Updated scenario_damage too
    Former-commit-id: d4780546ffbcacd0d741926653d726f3a4806379

--- openquake/calculators/scenario_risk.py
+++ openquake/calculators/scenario_risk.py
@@ -93,10 +93,8 @@ class ScenarioRiskCalculator(base.RiskCalculator):
         logging.info('Building the epsilons')
         epsilon_matrix = self.make_eps(
             self.oqparam.number_of_ground_motion_fields)
-        logging.info('Reading the GMFs')
-        with self.monitor('reading gmfs', autoflush=True):
-            self.datastore['etags'], gmfs = calc.get_gmfs(
-                self.datastore, self.precalc)
+        self.datastore['etags'], gmfs = calc.get_gmfs(
+            self.datastore, self.precalc)
         hazard_by_rlz = {rlz: gmfs[rlz.ordinal]
                          for rlz in self.rlzs_assoc.realizations}
         self.riskinputs = self.build_riskinputs(hazard_by_rlz, epsilon_matrix)
