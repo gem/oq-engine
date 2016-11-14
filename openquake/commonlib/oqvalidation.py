f7678d21bcf40c76d2e2ff64b81532ed94c97838
Michele Simionato <michele.simionato@gmail.com>
2016-11-14 14:00:26 +0100

    Added a print
    Former-commit-id: 0b48e9a4982201280c564c4eb4f677abdc039e85 [formerly 0b48e9a4982201280c564c4eb4f677abdc039e85 [formerly 58c6588214e668a3f15aeddfccb97e6ef963a603]]
    Former-commit-id: 8cd1a2dc7bd41c82b7057adb60fb885d9a63beaa
    Former-commit-id: 8a5e482781c51e440be9d32ef5190fb1c382b68c

--- openquake/commonlib/oqvalidation.py
+++ openquake/commonlib/oqvalidation.py
@@ -507,6 +507,8 @@ class OqParam(valid.ParamSet):
             can_write = os.path.exists(pdir) and os.access(pdir, os.W_OK)
             if can_write:
                 os.mkdir(self.export_dir)
+            print('isdir=%s, writeable=%s' %
+                  (os.path.exists(pdir), os.access(pdir, os.W_OK)))
             return can_write
         return os.path.isdir(self.export_dir) and os.access(
             self.export_dir, os.W_OK)
