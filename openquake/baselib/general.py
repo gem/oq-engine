f797abcb6ded79c32f7495b641e32e4a91da0463
Michele Simionato <michele.simionato@gmail.com>
2016-11-15 15:54:35 +0100

    Put the rtree warning in the right place

--- openquake/baselib/general.py
+++ openquake/baselib/general.py
@@ -635,6 +635,7 @@ class DictArray(collections.Mapping):
         return carray, {}
 
     def __fromh5__(self, carray, attrs):
+        import pdb; pdb.set_trace()
         self.array = carray[:].view(F64)
         self.imt_dt = dt = numpy.dtype(
             [(str(imt), F64, len(carray[0][imt]))
