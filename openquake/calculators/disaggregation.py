917a3c52572562cef6de13d138d37e9d7a7249e4
Michele Simionato <michele.simionato@gmail.com>
2016-11-22 08:46:16 +0100

    Small cleanup
    Former-commit-id: 456898ceeb9e7687e8c9e2f07b7f4a5fb5f403d0 [formerly a2d90da78614b7075db9fc2028e11d3dbfae0a02]
    Former-commit-id: 57470c4f39497bd7112170a80d780c2ea4bb092e

--- openquake/calculators/disaggregation.py
+++ openquake/calculators/disaggregation.py
@@ -217,8 +217,7 @@ class DisaggregationCalculator(classical.ClassicalCalculator):
                     if (sm_id, sid) in self.bin_edges:
                         bin_edges[sid] = self.bin_edges[sm_id, sid]
 
-                ss_filter = RtreeFilter(sitecol, oq.maximum_distance,
-                                        use_rtree=False)
+                ss_filter = RtreeFilter(sitecol, oq.maximum_distance)
                 split_sources = []
                 for src in src_group:
                     for split, _sites in ss_filter(
