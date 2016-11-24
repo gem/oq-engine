b2cf765c69fc154529aaaa7c98808ce0a90418d4
Michele Simionato <michele.simionato@gmail.com>
2016-11-24 07:06:27 +0100

    Removed the ebrisk calculator
    Former-commit-id: 02030a725b5c6695012cf5982aa2c7b61f5fc4da

--- openquake/calculators/base.py
+++ openquake/calculators/base.py
@@ -78,8 +78,7 @@ PRECALC_MAP = dict(
     classical_damage=['classical'],
     event_based=['event_based_risk'],
     event_based_risk=['event_based'],
-    ucerf_classical=['ucerf_psha'],
-    ebrisk=['event_based'])
+    ucerf_classical=['ucerf_psha'])
 
 
 def set_array(longarray, shortarray):
