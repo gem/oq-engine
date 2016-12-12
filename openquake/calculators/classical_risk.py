f3cd0e6d582acd4b3c67cdb8ee5963185e3bda89
Michele Simionato <michele.simionato@gmail.com>
2016-12-12 16:16:41 +0100

    Fixed classical_risk tests
    Former-commit-id: 8741ef7c9286013d958af5d8774391790fd43d32

--- openquake/calculators/classical_risk.py
+++ openquake/calculators/classical_risk.py
@@ -83,7 +83,8 @@ def classical_risk(riskinput, riskmodel, monitor):
                     oq.conditional_loss_poes,
                     curve_resolution, insured_losses=oq.insured_losses)
                 stats = statsbuilder.build(outs)
-                stat_curves, stat_maps = statsbuilder._get_curves_maps(stats)
+                stat_curves, stat_maps = statsbuilder._get_curves_maps(
+                    stats, curve_resolution)
                 for i, asset in enumerate(assets):
                     result['stat_curves'].append(
                         (l, asset.ordinal, stat_curves[:, i]))
