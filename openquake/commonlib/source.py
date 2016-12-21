5c1c786ea45866b9c7ab7c56508e7ffd74702729
Michele Simionato <michele.simionato@gmail.com>
2016-12-21 19:57:54 +0100

    Fixed MAXWEIGHT
    Former-commit-id: 2722eb6506fca37cbc0d5ed6efd393c4423cf46a [formerly 2722eb6506fca37cbc0d5ed6efd393c4423cf46a [formerly 1c8f77b1a83a7bb640cdc3a860993a8f80cf7cc9]]
    Former-commit-id: b673b21f01bc9f18bd3be5ac46a175f4e2c9fb8c
    Former-commit-id: f8db2ef9ad7914173d322589183067fa93e5d123

--- openquake/commonlib/source.py
+++ openquake/commonlib/source.py
@@ -33,7 +33,7 @@ from openquake.baselib.general import groupby, group_array
 from openquake.commonlib import logictree, sourceconverter, InvalidFile
 from openquake.commonlib import nrml, node
 
-MAXWEIGHT = 200  # tuned by M. Simionato
+MAXWEIGHT = sourceconverter.MAXWEIGHT
 MAX_INT = 2 ** 31 - 1
 U16 = numpy.uint16
 U32 = numpy.uint32
