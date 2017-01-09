62c884dda6855b5db9af7c022d134813c17d3e7f
Daniele Viganò <daniele@vigano.me>
2017-01-09 15:06:57 +0100

    Make sure we are not taking oq-hazardlib from pypi when running master

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ install_requires = [
     'django >=1.6, <1.10',
     'requests >=2.2, <2.13',
     'pyshp ==1.2',
-    'openquake.hazardlib',
+    'openquake.hazardlib ==0.22',
 ]
 if sys.version < '3':
     install_requires.append(
