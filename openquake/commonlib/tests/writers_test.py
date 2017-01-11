6e5688bb1cfdf4c1d12cb30a632fbb02cede5d15
Michele Simionato <michele.simionato@gmail.com>
2017-01-11 06:38:09 +0100

    Fixed import
    Former-commit-id: 97d5d17b8fc112836bc47e1adb0a43ebc90d39fb [formerly f778335a0f28edafa5fca023057aaf435a1209be]
    Former-commit-id: e4f756a2efc3d7b38b934c39a3d703168df10471

--- openquake/commonlib/tests/writers_test.py
+++ openquake/commonlib/tests/writers_test.py
@@ -21,8 +21,8 @@ import unittest
 import tempfile
 from io import BytesIO
 from openquake.baselib.performance import memory_info
-from openquake.commonlib.writers import tostring, StreamingXMLWriter, write_csv
-from openquake.commonlib.node import Node
+from openquake.commonlib.writers import write_csv
+from openquake.commonlib.node import Node, tostring, StreamingXMLWriter
 from xml.etree import ElementTree as etree
 
 import numpy
