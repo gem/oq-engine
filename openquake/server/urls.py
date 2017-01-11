29ff67e1e6956e531eab8db93803b77ddabf850d
Daniele Viganò <daniele@vigano.me>
2017-01-11 11:28:16 +0100

    Do not rename openquake.server.views in *urls.py
    Former-commit-id: ad0e5fe878cf93812b6174dc3d44f727c8a043ad [formerly 2104e167fda644e22edb875cf39e3bcb8d9f3449] [formerly 2104e167fda644e22edb875cf39e3bcb8d9f3449 [formerly e00631d92158d6fd4444800751c03b4242880e93]]
    Former-commit-id: 6556a355d20e969501b6cf321c9fb5137765f9a9
    Former-commit-id: d3aced3b26fc4d416b019ee0c71ed18919e8e1f0

--- openquake/server/urls.py
+++ openquake/server/urls.py
@@ -21,17 +21,17 @@ from django.conf.urls import url, include
 from django.views.generic.base import RedirectView
 from django.contrib.auth.views import login, logout
 
-from openquake.server import views as oqserver
+from openquake.server import views
 
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url='/engine/', permanent=True)),
-    url(r'^engine_version$', oqserver.get_engine_version),
+    url(r'^engine_version$', views.get_engine_version),
     url(r'^v1/calc/', include('openquake.server.v1.calc_urls')),
-    url(r'^v1/valid/', oqserver.validate_nrml),
-    url(r'^engine/?$', oqserver.web_engine, name="index"),
+    url(r'^v1/valid/', views.validate_nrml),
+    url(r'^engine/?$', views.web_engine, name="index"),
     url(r'^engine/(\d+)/outputs$',
-        oqserver.web_engine_get_outputs, name="outputs"),
-    url(r'^engine/license$', oqserver.license,
+        views.web_engine_get_outputs, name="outputs"),
+    url(r'^engine/license$', views.license,
         name="license"),
 ]
 
@@ -45,6 +45,6 @@ if settings.LOCKDOWN:
             {'template_name': 'account/login.html'}, name="login"),
         url(r'^accounts/logout/$', logout,
             {'template_name': 'account/logout.html'}, name="logout"),
-        url(r'^accounts/ajax_login/$', oqserver.ajax_login),
-        url(r'^accounts/ajax_logout/$', oqserver.ajax_logout),
+        url(r'^accounts/ajax_login/$', views.ajax_login),
+        url(r'^accounts/ajax_logout/$', views.ajax_logout),
     ]
