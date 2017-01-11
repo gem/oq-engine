5e48675aac6a9c4c67bbf93bcf2f5be0f8e541a2
Daniele Viganò <daniele@vigano.me>
2017-01-11 10:36:15 +0100

    Update classifiers in setup.py [skip ci]

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ install_requires = [
 if sys.version < '3':
     install_requires.append(
         'futures >=2.1, <3.1'
-)
+    )
 
 extras_require = {
     'rtree':  ["Rtree==0.8.2"],
@@ -93,15 +93,20 @@ setup(
     author_email='devops@openquake.org',
     maintainer='GEM Foundation',
     maintainer_email='devops@openquake.org',
-    classifiers=(
-        'Development Status :: 4 - Beta',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
         'License :: OSI Approved :: GNU Affero General Public License v3',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
         'Topic :: Scientific/Engineering',
-    ),
+        'Environment :: Console',
+        'Environment :: Web Environment',
+    ],
     keywords="seismic hazard",
     license="AGPL3",
     platforms=["any"],
