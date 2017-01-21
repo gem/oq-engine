ce98e6ffe9c0d67187897722cc28a1e1f9619cda
Michele Simionato <michele.simionato@gmail.com>
2017-01-21 08:25:17 +0100

    Fixed nbytes of hcurves
    Former-commit-id: 06f69a29da3adfd306e5b4d9eee2a7a71b9165a5

--- openquake/calculators/classical.py
+++ openquake/calculators/classical.py
@@ -453,17 +453,23 @@ class ClassicalCalculator(PSHACalculator):
         attrs = dict(
             __pyclass__='openquake.hazardlib.probability_map.ProbabilityMap',
             sids=numpy.arange(N, dtype=numpy.uint32))
+        nbytes = N * L * 4  # bytes per realization (32 bit floats)
+        totbytes = 0
         if oq.individual_curves:
             for rlz in rlzs:
                 self.datastore.create_dset(
                     'hcurves/rlz-%03d' % rlz.ordinal, F32,
                     (N, L, 1),  attrs=attrs)
-        if oq.mean_hazard_curves:
+                totbytes += nbytes
+        if oq.mean_hazard_curves and len(rlzs) > 1:
             self.datastore.create_dset(
                 'hcurves/mean', F32, (N, L, 1), attrs=attrs)
+            totbytes += nbytes
         for q in oq.quantile_hazard_curves:
             self.datastore.create_dset(
                 'hcurves/quantile-%s' % q, F32, (N, L, 1), attrs=attrs)
+            totbytes += nbytes
+        self.datastore.set_attrs('hcurves', nbytes=totbytes)
         self.datastore.flush()
 
         logging.info('Building hazard curves')
