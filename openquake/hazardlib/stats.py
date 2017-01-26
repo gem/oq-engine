87e4aebb312da3879d82e89bac16b51a0638a456
Michele Simionato <michele.simionato@gmail.com>
2017-01-26 10:48:50 +0100

    Refactored the quantile algorithm

--- openquake/hazardlib/stats.py
+++ openquake/hazardlib/stats.py
@@ -34,13 +34,10 @@ def mean_curve(values, weights=None):
 
 def quantile_curve(curves, quantile, weights=None):
     """
-    Compute the weighted quantile aggregate of a set of curves
-    when using the logic tree end-branch enumeration approach, or just the
-    standard quantile when using the sampling approach.
+    Compute the weighted quantile aggregate of a set of curves.
 
     :param curves:
-        2D array-like of curve PoEs. Each row represents the PoEs for a single
-        curve
+        Array of PoEs of shape (R,), (R, N) or (R, N, L)
     :param quantile:
         Quantile value to calculate. Should be in the range [0.0, 1.0].
     :param weights:
@@ -49,12 +46,28 @@ def quantile_curve(curves, quantile, weights=None):
         A numpy array representing the quantile aggregate
     """
     assert len(curves)
+    if weights is not None:
+        weights = numpy.array(weights)
+        assert len(weights) == len(curves)
+    if not isinstance(curves, numpy.ndarray):
+        curves = numpy.array(curves)
+    dim = len(curves.shape)
+    if dim == 1:  # shape R -> 1
+        return _quantile_curve(curves[:, None], quantile, weights)[0]
+    elif dim == 2:  # shape R, N -> N
+        return _quantile_curve(curves, quantile, weights)
+    elif dim == 3:  # shape R, N, L -> N, L
+        return numpy.curves([_quantile_curve(arr, quantile, weights)
+                             for arr in curves.transpose(1, 0, 2)])
+
+
+def _quantile_curve(arr, quantile, weights):
+    # arr is a 2D array of shape (R, N)
     if weights is None:
         # this implementation is an alternative to
         # numpy.array(mstats.mquantiles(curves, prob=quantile, axis=0))[0]
         # more or less copied from the scipy mquantiles function, just special
         # cased for what we need (and a lot faster)
-        arr = numpy.array(curves).reshape(len(curves), -1)
         p = numpy.array(quantile)
         m = 0.4 + p * 0.2
         n = len(arr)
@@ -65,25 +78,14 @@ def quantile_curve(curves, quantile, weights=None):
         qcurve = (1.0 - gamma) * data[:, k - 1] + gamma * data[:, k]
         return qcurve
 
-    # Each curve needs to be associated with a weight
-    assert len(weights) == len(curves)
-    weights = numpy.array(weights)
-
     result_curve = []
-    np_curves = numpy.array(curves).reshape(len(curves), -1)
-    np_weights = numpy.array(weights)
-    for poes in np_curves.transpose():
+    for poes in arr.transpose():
         sorted_poe_idxs = numpy.argsort(poes)
-        sorted_weights = np_weights[sorted_poe_idxs]
+        sorted_weights = weights[sorted_poe_idxs]
         sorted_poes = poes[sorted_poe_idxs]
         cum_weights = numpy.cumsum(sorted_weights)
         result_curve.append(numpy.interp(quantile, cum_weights, sorted_poes))
-
-    shape = getattr(curves[0], 'shape', None)
-    if shape:  # passed a sequence of arrays
-        return numpy.array(result_curve).reshape(shape)
-    else:  # passed a sequence of numbers
-        return result_curve
+    return numpy.array(result_curve)
 
 
 # NB: this is a function linear in the array argument
@@ -100,16 +102,8 @@ def compute_stats(array, quantiles, weights):
     """
     result = numpy.zeros((len(quantiles) + 1,) + array.shape[1:], array.dtype)
     result[0] = mean_curve(array, weights)
-    shp = result[0].shape
     for i, q in enumerate(quantiles, 1):
-        qc = quantile_curve(array, q, weights)
-        # TODO: try to simplify the ugliness below
-        if isinstance(qc, list) and len(qc) == 1:
-            result[i] = qc[0]
-        elif qc.shape != shp:
-            result[i] = qc.reshape(shp)
-        else:
-            result[i] = qc
+        result[i] = quantile_curve(array, q, weights)
     return result
 
 
