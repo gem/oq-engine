eb99f6ed38f08598a18d106bfad59d9aa590c840
Michele Simionato <michele.simionato@gmail.com>
2017-02-07 10:34:51 +0100

    Remove an empty line [skip CI]

--- openquake/hazardlib/valid.py
+++ openquake/hazardlib/valid.py
@@ -415,7 +415,6 @@ def coordinates(value):
     [(1.1, 1.2, 0.0), (2.2, 2.3, 0.0)]
     >>> coordinates('1.1 1.2 -0.4, 2.2 2.3 -0.5')
     [(1.1, 1.2, -0.4), (2.2, 2.3, -0.5)]
-
     >>> coordinates('0 0 0, 0 0 -1')
     Traceback (most recent call last):
     ...
