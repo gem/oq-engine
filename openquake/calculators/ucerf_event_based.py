f76f8cbd5567a13f168a535c232a1bbab86adf25
Michele Simionato <michele.simionato@gmail.com>
2017-02-26 08:33:38 +0100

    Removed unused import [skip CI]
    Former-commit-id: 68686bc3f439a74b84bf8452b3f5c4aa99710dba

--- openquake/calculators/ucerf_event_based.py
+++ openquake/calculators/ucerf_event_based.py
@@ -51,7 +51,6 @@ from openquake.hazardlib.scalerel.wc1994 import WC1994
 from openquake.hazardlib.calc.filters import SourceFilter, FarAwayRupture
 from openquake.hazardlib.mfd import EvenlyDiscretizedMFD
 from openquake.hazardlib.sourceconverter import SourceConverter
-from openquake.hazardlib.gsim.base import ContextMaker
 
 # ######################## rupture calculator ############################ #
 
