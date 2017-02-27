d3218965e7b14eb289f4e0688d057b9d2cf4f176
Michele Simionato <michele.simionato@gmail.com>
2017-02-27 13:06:01 +0100

    Fixed leftover
    Former-commit-id: 7a1af50d11f25de60737df9fadf1237bc836e69a [formerly 7a1af50d11f25de60737df9fadf1237bc836e69a [formerly 6ac185e9ee79a66fe7e211642253ca7627a22f0d]]
    Former-commit-id: 97a2b2768f34772d186d8de87aa41235ca249cdf
    Former-commit-id: 6270d5f007ec7dafa6b6b525adae297d34ab0e92

--- openquake/commonlib/readinput.py
+++ openquake/commonlib/readinput.py
@@ -214,7 +214,7 @@ def get_mesh(oqparam):
     elif 'site_model' in oqparam.inputs:
         coords = [(param.lon, param.lat, param.depth)
                   for param in get_site_model(oqparam)]
-        mesh = geo.Mesh.from_coords(coords, from_site_model=True)
+        mesh = geo.Mesh.from_coords(coords)
         mesh.from_site_model = True
         return mesh
 
