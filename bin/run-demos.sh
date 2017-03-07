0092c96678014a08b6c7a96776f239a723915c2a
Michele Simionato <michele.simionato@gmail.com>
2017-03-07 14:56:37 +0100

    Used TRAVIS_BUILD_DIR
    Former-commit-id: 75d4802b06e0c6e32169e6c080827b28addf139c

--- bin/run-demos.sh
+++ bin/run-demos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-python -m openquake.commands engine --run $HOME/demos/hazard/AreaSourceClassicalPSHA/job.ini
+python -m openquake.commands engine --run $TRAVIS_BUILD_DIR/demos/hazard/AreaSourceClassicalPSHA/job.ini
 MPLBACKEND=Template python -m openquake.commands plot_uhs -1
 # MPLBACKEND=Template python -m openquake.commands plot -1
