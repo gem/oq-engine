242281033dde247dff6b50a2a739553d032f5baa
Michele Simionato <michele.simionato@gmail.com>
2017-03-07 13:32:58 +0100

    Fixed filename
    Former-commit-id: 3eecd17452e740dc12399a818a1f73392b2044c3 [formerly 38332cfbcdfda80db02f67cd829882dbbb3c28c7]
    Former-commit-id: 5ca67824f42a40eb024e79b7dfdbdd308f859b45

--- bin/run-demos.sh
+++ bin/run-demos.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python -m openquake.commands engine --run oq-engine/demos/hazard/AreaSourceClassicalPSHA/job.ini
+python -m openquake.commands engine --run $HOME/oq-engine/demos/hazard/AreaSourceClassicalPSHA/job.ini
 MPLBACKEND=Template python -m openquake.commands plot_uhs -1
 # MPLBACKEND=Template python -m openquake.commands plot -1
