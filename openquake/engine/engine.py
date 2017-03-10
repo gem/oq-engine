0602464c1267f41d688815bb7a49991db1ac5b67
Michele Simionato <michele.simionato@gmail.com>
2017-03-10 14:38:39 +0100

    Replace use_celery with oq_distribute
    Former-commit-id: a04e6876053cef313e802e1b8b9598f770df92b5 [formerly a04e6876053cef313e802e1b8b9598f770df92b5 [formerly 1cc87eb9b77c3f97e3563e3924abdb9c0f45736c]]
    Former-commit-id: 16b7e9fc6081765b9f421db5a6c9253f4e3aa35d
    Former-commit-id: 9aaec22e5f8ffa48e220b4cd68aef6c2b2cf6d61

--- openquake/engine/engine.py
+++ openquake/engine/engine.py
@@ -35,7 +35,7 @@ from openquake.commonlib import logs
 TERMINATE = valid.boolean(
     config.get('celery', 'terminate_workers_on_revoke') or 'false')
 
-USE_CELERY = valid.boolean(config.get('celery', 'use_celery') or 'false')
+USE_CELERY = config.get('distribution', 'oq_distribute') == 'celery'
 
 if USE_CELERY:
     import celery.task.control
