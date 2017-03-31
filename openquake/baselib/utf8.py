1ac9e6ccaae2086079f3db8cc7d061e54b0ecc7f
Daniele Viganò <daniele@vigano.me>
2017-03-31 16:28:53 +0200

    Add an UTF-8 helper which overloads print()

---
+++ openquake/baselib/utf8.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+#
+# Copyright (C) 2017 GEM Foundation
+#
+# OpenQuake is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# OpenQuake is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with OpenQuake. If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import print_function
+import sys
+
+try:
+    import __builtin__
+except ImportError:
+    # Python 3
+    import builtins as __builtin__
+
+
+def print(*args, **kwargs):
+    conv_str = ()
+    for s in args:
+        conv_str = s.encode('utf-8').decode(sys.stdout.encoding, 'ignore')
+    return __builtin__.print(conv_str, **kwargs)
