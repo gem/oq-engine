52d9020db249a3b94d7fb93342f059595b056d73
Michele Simionato <michele.simionato@gmail.com>
2017-04-01 06:29:57 +0200

    Checked the numbers only in the reference platform
    Former-commit-id: 026411a95cc05a3e8b855d1f9661bc8e9f7d7bca [formerly 026411a95cc05a3e8b855d1f9661bc8e9f7d7bca [formerly 4eea6f4fd614df618dba16a1d752cb72b7e24090]]
    Former-commit-id: 14a5595168dbb7a44322150aa981112584f34d73
    Former-commit-id: 9f63013a061c703d686f4b2c585648449647d2d5

--- openquake/calculators/tests/event_based_test.py
+++ openquake/calculators/tests/event_based_test.py
@@ -20,6 +20,7 @@ from __future__ import division
 import os
 import re
 import math
+import platform
 from nose.plugins.attrib import attr
 
 import numpy.testing
@@ -214,9 +215,8 @@ gmf-smltp_b3-gsimltp_@_@_@_b4_1.txt'''.split()
             self.assertEqualFiles('expected/%s' % exp, got, sorted)
 
         [fname] = export(('ruptures', 'csv'), self.calc.datastore)
-        if 'TRAVIS_BRANCH' in os.environ:  # avoid rounding errors
-            return
-        self.assertEqualFiles('expected/ruptures.csv', fname)
+        if 'Ubuntu-16.04' in platform.platform():  # reference platform
+            self.assertEqualFiles('expected/ruptures.csv', fname)
 
     @attr('qa', 'hazard', 'event_based')
     def test_case_6(self):
