46a63f7b2978e51330f86790667d651baf2efb01
Michele Simionato <michele.simionato@gmail.com>
2017-04-08 05:40:58 +0200

    Removed loss_maps and loss_curves from the event based calculator
    Former-commit-id: e96593ba03e16d23e3ee640b846ce478a8411a47 [formerly e96593ba03e16d23e3ee640b846ce478a8411a47 [formerly 069e368b1cfde6e67facf0d9d6c6d860f8d16a6a]]
    Former-commit-id: 77ff76efbbace25f16a523ee826752cdff3a9043
    Former-commit-id: a3b49d400083d9a48ce805eb19855e7353cf222a

--- openquake/calculators/tests/event_based_risk_test.py
+++ openquake/calculators/tests/event_based_risk_test.py
@@ -73,16 +73,7 @@ class EventBasedRiskTestCase(CalculatorTestCase):
     def test_case_1(self):
         self.run_calc(case_1.__file__, 'job.ini',
                       exports='csv', individual_curves='false')
-        ekeys = [
-            ('rcurves-stats', 'xml'),
-            ('rcurves-stats', 'geojson'),
-
-            ('loss_maps-stats', 'csv'),
-            ('loss_maps-stats', 'xml'),
-            ('loss_maps-stats', 'geojson'),
-
-            ('agg_curve-stats', 'xml'),
-        ]
+        ekeys = [('agg_curve-stats', 'xml')]
         for ekey in ekeys:
             for fname in export(ekey, self.calc.datastore):
                 self.assertEqualFiles(
@@ -166,6 +157,7 @@ class EventBasedRiskTestCase(CalculatorTestCase):
     def test_occupants(self):
         out = self.run_calc(occupants.__file__, 'job.ini',
                             exports='xml', individual_curves='true')
+        raise unittest.SkipTest('missing loss_maps')
         fnames = export(('loss_maps-rlzs', 'xml'), self.calc.datastore) + \
                  out['agg_curve-rlzs', 'xml']
         self.assertEqual(len(fnames), 3)  # 2 loss_maps + 1 agg_curve
@@ -183,10 +175,10 @@ class EventBasedRiskTestCase(CalculatorTestCase):
             self.assertEqualFiles('expected/' + strip_calc_id(fname), fname,
                                   delta=1E-5)
 
-        fnames = export(('loss_maps-rlzs', 'csv'), self.calc.datastore)
-        for fname in fnames:
-            self.assertEqualFiles('expected/' + strip_calc_id(fname), fname,
-                                  delta=1E-5)
+        # fnames = export(('loss_maps-rlzs', 'csv'), self.calc.datastore)
+        # for fname in fnames:
+        #     self.assertEqualFiles('expected/' + strip_calc_id(fname), fname,
+        #                           delta=1E-5)
 
         fnames = export(('losses_by_taxon-stats', 'csv'), self.calc.datastore)
         for fname in fnames:
