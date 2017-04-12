afb8f140f62283891171b9f9c5fdcd215ca68e7a
Michele Simionato <michele.simionato@gmail.com>
2017-04-12 15:12:14 +0200

    Fixed the statistics
    Former-commit-id: e636bfb368361b3d3e199cc1138dde78d99e479c [formerly fae4da103b08b983a04b91a23af34963eea5484d] [formerly fae4da103b08b983a04b91a23af34963eea5484d [formerly 79971cf18737288cec8732dd28fc618d872a9756]]
    Former-commit-id: 6f593d4446515a563d8b9ee73d15707869acfaf3
    Former-commit-id: fc5b3a72e85f3caae7ae3c965813a5b4a521a037

--- openquake/risklib/scientific.py
+++ openquake/risklib/scientific.py
@@ -30,6 +30,7 @@ from numpy.testing import assert_equal
 from scipy import interpolate, stats, random
 
 from openquake.baselib.general import CallableDict, group_array
+from openquake.hazardlib.stats import compute_stats2
 from openquake.risklib import utils
 from openquake.baselib.python3compat import with_metaclass
 
@@ -970,7 +971,6 @@ class CurveBuilder(object):
         for i in range(self.I):
             for cb in self.cbs:
                 lt = cb.loss_type + '_ins' * i
-                lst.append((lt, F32))
                 dtlist.append((lt, (F32, len(cb.ratios))))
         self.dt = numpy.dtype(dtlist)
 
@@ -1010,7 +1010,7 @@ class CurveBuilder(object):
                     arr['avg'] = average_loss([losses, poes])
         return curves
 
-    def build_maps(self, assets, getter, rlzs, mon):
+    def build_maps(self, assets, getter, rlzs, quantiles, mon):
         """"
         :param assets:
             a list of assets
@@ -1018,29 +1018,45 @@ class CurveBuilder(object):
             a :class:`openquake.risklib.riskinput.LossRatiosGetter` instance
         :param rlzs:
             a list of realizations
+        :param quantiles:
+            a list of quantiles
         :returns:
-            aids and maps
+            aids, loss_maps, loss_maps_stats
         """
         assert self.clp, 'No conditional_loss_poes in the job.ini!'
         lti = {cb.loss_type: i for i, cb in enumerate(self)}
         for lt, i in sorted(lti.items()):
             lti[lt + '_ins'] = i
-        loss_maps = numpy.zeros((len(assets), len(rlzs)), self.loss_maps_dt)
         with mon('getting loss ratios'):
             aids = [asset.ordinal for asset in assets]
             loss_ratios = getter.get_all(aids)
-        losses = [[asset.value(cb.loss_type) * cb.ratios for cb in self]
-                  for asset in assets]
-        for a, poes in enumerate(self.build_poes(aids, loss_ratios, rlzs)):
-            for lt in poes.dtype.names:
-                alosses = losses[a][lti[lt]]
+        losses = {}
+        for cb in self.cbs:
+            losses[cb.loss_type] = [asset.value(cb.loss_type) * cb.ratios
+                                    for asset in assets]
+            losses[cb.loss_type + '_ins'] = losses[cb.loss_type]
+        all_poes = self.build_all_poes(aids, loss_ratios, rlzs)
+        loss_maps = self._build_maps(losses, all_poes)
+        if len(rlzs) > 1:
+            weights = [rlz.weight for rlz in rlzs]
+            stat_poes = compute_stats2(all_poes, quantiles, weights)
+            loss_maps_stats = self._build_maps(losses, stat_poes)
+        else:
+            loss_maps_stats = None
+        return aids, loss_maps, loss_maps_stats
+
+    def _build_maps(self, losses, all_poes):
+        loss_maps = numpy.zeros(all_poes.shape, self.loss_maps_dt)
+        for a, poes in enumerate(all_poes):
+            for lt in all_poes.dtype.names:
+                alosses = losses[lt][a]
                 for r, the_poes in enumerate(poes[lt]):
                     loss_maps[lt][a, r] = tuple(
                         conditional_loss_ratio(alosses, the_poes, poe)
                         for poe in self.clp)
-        return aids, loss_maps
+        return loss_maps
 
-    def build_poes(self, aids, loss_ratios, rlzs):
+    def build_all_poes(self, aids, loss_ratios, rlzs):
         """"
         :param aids:
             a list of asset IDs
@@ -1049,13 +1065,13 @@ class CurveBuilder(object):
         :param rlzs:
             a list of realizations
         :yields:
-            quartet (lt, a, r, poes)
+            a matrix of shape (A, R) of PoEs
         """
         L = len(self.cbs)
         LI = L * self.I
+        poes = numpy.zeros((len(aids), len(rlzs)), self.dt)
         for a, aid in enumerate(aids):
             dic = group_array(loss_ratios[a], 'rlzi')
-            poes = numpy.zeros(len(rlzs), self.dt)
             for rlz in rlzs:
                 r = rlz.ordinal
                 try:
@@ -1068,8 +1084,8 @@ class CurveBuilder(object):
                         lrs = ratios[:, lt]
                         counts = numpy.array([(lrs >= ratio).sum()
                                               for ratio in cb.ratios], F32)
-                        poes[r][lt] = 1. - numpy.exp(- counts * cb.ses_ratio)
-            yield poes
+                        poes[a, r][lt] = 1. - numpy.exp(-counts * cb.ses_ratio)
+        return poes
 
 
 # should I use the ses_ratio here?
