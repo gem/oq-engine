bb978476e05efc28793577d01f6246956667ae68
Michele Simionato <michele.simionato@gmail.com>
2017-05-12 15:38:01 +0200

    Small fix
    Former-commit-id: 4200098e5af41a5fb90d127862ecded833a571d5 [formerly 4200098e5af41a5fb90d127862ecded833a571d5 [formerly 5ecdbf4092b827efe00f38fff41351cde87ab1ea]]
    Former-commit-id: 94e71be6f92caf4cf8574cf36fdeebe49d7923e5
    Former-commit-id: d8dd2a96af28713f71f84704f4b92510f239af6a

--- openquake/commonlib/readinput.py
+++ openquake/commonlib/readinput.py
@@ -255,7 +255,7 @@ def get_site_collection(oqparam, mesh=None):
                     param.z1pt0, param.z2pt5, param.backarc))
             return site.SiteCollection(sitecol)
         # read the parameters directly from their file
-        site_model_params = geo.geodetic.GeographicObjects(
+        site_model_params = geo.utils.GeographicObjects(
             get_site_model(oqparam))
         for pt in mesh:
             # attach the closest site model params to each site
