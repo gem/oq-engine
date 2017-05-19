6bbdb708149debe20aee96b991d8dda5f3080048
Daniele Viganò <daniele@vigano.me>
2017-05-19 13:58:53 +0200

    Merge build_new.sh and build.sh in doc/manual
    Former-commit-id: bf486efcb00146837e0463b6d43fe8cd1f9a117e [formerly bf486efcb00146837e0463b6d43fe8cd1f9a117e [formerly 9346d54c0a10c8f9c9736462b2aad6c8cb259673]]
    Former-commit-id: 40124117c5eb868203ca057052684c5537d32d96
    Former-commit-id: 0a385aa5b29540c94e7cfd072df57c53d25d9a04

--- doc/manual/build.sh
+++ doc/manual/build.sh
@@ -10,7 +10,6 @@ inkscape -A figures/oq_manual_cover.pdf figures/oq_manual_cover.svg
 (pdflatex -shell-escape -interaction=nonstopmode oq-manual.tex
 bibtex oq-manual
 pdflatex -shell-escape -interaction=nonstopmode oq-manual.tex
-pdflatex -shell-escape -interaction=nonstopmode oq-manual.tex
 makeindex oq-manual.idx
 makeglossaries oq-manual
 pdflatex -shell-escape -interaction=nonstopmode oq-manual.tex) | egrep -i "error|warning|missing"
--- doc/manual/build_new.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pdflatex -shell-escape -interaction=nonstopmode oq-manual.tex
-bibtex oq-manual
-pdflatex -shell-escape -interaction=nonstopmode oq-manual.tex
-makeindex oq-manual.idx
-makeglossaries oq-manual
-pdflatex -shell-escape -interaction=nonstopmode oq-manual.tex
-
