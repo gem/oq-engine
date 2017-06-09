a56f98bbd10f26f449365ef293a7f17681b44d57
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2017-06-09 14:04:11 +0200

    added success function after show modal confirm [skip CI]

--- openquake/server/static/js/engine.js
+++ openquake/server/static/js/engine.js
@@ -219,10 +219,13 @@ var CalculationTable = Backbone.View.extend(
 
             showMenuContent: function(e) {
                 e.preventDefault();
-                var calc_id = $(e.target).attr('data-calc-id');
-                this.conf_show = $('#confirmDialog' + calc_id).show().success();
-                this.back_conf_show = $('.back_confirmDialog' + calc_id).show().success();
-                //return conf_show 
+                var calc_id = $(e.target).attr('data-calc-id');i
+                
+                this.conf_show = function(data, textStatus, jqXHR)
+                                 {
+                                     $('#confirmDialog' + calc_id).show();
+                                     $('.back_confirmDialog' + calc_id).show();
+                                 }
             },
             //confirm_remove: function(e) {
                 //e.preventDefault();
