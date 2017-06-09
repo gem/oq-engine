c011f2d51f9595596c6facf578f206061be2b208
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2017-06-09 14:14:04 +0200

    added event.stopPropagation in function after show modal confirm [skip CI]

--- openquake/server/static/js/engine.js
+++ openquake/server/static/js/engine.js
@@ -222,7 +222,7 @@ var CalculationTable = Backbone.View.extend(
                 var calc_id = $(e.target).attr('data-calc-id');
                 this.conf_show = $('#confirmDialog' + calc_id).show();
                 this.back_conf_show = $('.back_confirmDialog' + calc_id).show();
-                e.preventDefault();
+                e.event.stopPropagation();
             },
             //confirm_remove: function(e) {
                 //e.preventDefault();
