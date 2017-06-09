38274997dd87f69b5a1ca6b834f72dbce9f89c6f
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2017-06-09 13:37:40 +0200

    added function for show modal [skip CI]

--- openquake/server/static/js/engine.js
+++ openquake/server/static/js/engine.js
@@ -220,8 +220,8 @@ var CalculationTable = Backbone.View.extend(
             showMenuContent: function(e) {
                 e.preventDefault();
                 var calc_id = $(e.target).attr('data-calc-id');
-                var conf_show = $('#confirmDialog' + calc_id).show();
-                return conf_show 
+                this.conf_show = $('#confirmDialog' + calc_id).show();
+                //return conf_show 
             },
             //confirm_remove: function(e) {
                 //e.preventDefault();
