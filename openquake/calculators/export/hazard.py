f0360cccc8d755e2410d00a040870bc9e8236616
Michele Simionato <michele.simionato@gmail.com>
2017-06-12 13:56:24 +0200

    Fixed event based

--- openquake/calculators/export/hazard.py
+++ openquake/calculators/export/hazard.py
@@ -813,9 +813,9 @@ def export_gmf_scenario_npz(ekey, dstore):
     elif 'event_based' in oq.calculation_mode:
         dic['sitemesh'] = get_mesh(dstore['sitecol'])
         for grp in sorted(dstore['gmf_data']):
-            for rlzno in sorted(dstore['gmf_data/' + grp]):
-                dic['rlz-' + rlzno] = dstore[
-                    'gmf_data/%s/%s' % (grp, rlzno)].value
+            data_by_rlzi = group_array(dstore['gmf_data/' + grp].value, 'rlzi')
+            for rlzi in data_by_rlzi:
+                dic['rlz-%03d' % rlzi] = data_by_rlzi[rlzi]
     else:  # nothing to export
         return []
     savez(fname, **dic)
