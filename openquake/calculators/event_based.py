34d00165cd0fb287f0e009f50ac312c1fd46d9c6
Michele Simionato <michele.simionato@gmail.com>
2017-06-12 09:45:06 +0200

    Simplified the data storage of the GMFs
    Former-commit-id: c7b938e8e165fac2f4526803a68a08ec97830384 [formerly 593099cc321184b2a53fa6f63b5147269f1afa4c]
    Former-commit-id: ad169adb7a8bc8dddc3b7edb8250fa85ae6ef150

--- openquake/calculators/event_based.py
+++ openquake/calculators/event_based.py
@@ -31,7 +31,6 @@ from openquake.baselib.general import AccumDict, block_splitter, humansize
 from openquake.hazardlib.calc.filters import FarAwayRupture
 from openquake.hazardlib.probability_map import ProbabilityMap
 from openquake.hazardlib.stats import compute_pmap_stats
-from openquake.hazardlib.geo.surface import PlanarSurface
 from openquake.risklib.riskinput import GmfGetter, str2rsi, rsi2str
 from openquake.baselib import parallel
 from openquake.commonlib import calc, util
@@ -331,29 +330,26 @@ def compute_gmfs_and_curves(getter, oq, monitor):
     if oq.hazard_curves_from_gmfs:
         hc_mon = monitor('building hazard curves', measuremem=False)
         duration = oq.investigation_time * oq.ses_per_logic_tree_path
-        for gsim in getter.rlzs_by_gsim:
-            with monitor('building hazard', measuremem=True):
-                gmfcoll[grp_id, gsim] = data = numpy.fromiter(
-                    getter.gen_gmv(gsim), getter.gmf_data_dt)
-                hazard = getter.get_hazard(gsim, data)
-            for r, rlz in enumerate(getter.rlzs_by_gsim[gsim]):
-                hazardr = hazard[r]
-                for sid in getter.sids:
-                    array = hazardr[sid]
-                    if len(array) == 0:  # no data
-                        continue
-                    with hc_mon:
-                        gmvs = array['gmv']
-                        for imti, imt in enumerate(getter.imts):
-                            poes = calc._gmvs_to_haz_curve(
-                                gmvs[:, imti], oq.imtls[imt],
-                                oq.investigation_time, duration)
-                            hcurves[rsi2str(rlz.ordinal, sid, imt)] = poes
+        with monitor('building hazard', measuremem=True):
+            gmfcoll[grp_id] = data = numpy.fromiter(
+                getter.gen_gmv(), getter.gmf_data_dt)
+            hazard = getter.get_hazard(data)
+        for rlzi, hazardr in enumerate(hazard):
+            for sid in getter.sids:
+                array = hazardr[sid]
+                if len(array) == 0:  # no data
+                    continue
+                with hc_mon:
+                    gmvs = array['gmv']
+                    for imti, imt in enumerate(getter.imts):
+                        poes = calc._gmvs_to_haz_curve(
+                            gmvs[:, imti], oq.imtls[imt],
+                            oq.investigation_time, duration)
+                        hcurves[rsi2str(rlzi, sid, imt)] = poes
     else:  # fast lane
-        for gsim in getter.rlzs_by_gsim:
-            with monitor('building hazard', measuremem=True):
-                gmfcoll[grp_id, gsim] = numpy.fromiter(
-                    getter.gen_gmv(gsim), getter.gmf_data_dt)
+        with monitor('building hazard', measuremem=True):
+            gmfcoll[grp_id] = numpy.fromiter(
+                getter.gen_gmv(), getter.gmf_data_dt)
     return dict(gmfcoll=gmfcoll if oq.ground_motion_fields else None,
                 hcurves=hcurves, gmdata=getter.gmdata)
 
@@ -424,9 +420,9 @@ class EventBasedCalculator(ClassicalCalculator):
         self.gmdata += res['gmdata']
         if res['gmfcoll'] is not None:
             with sav_mon:
-                for (grp_id, gsim), array in res['gmfcoll'].items():
+                for grp_id, array in res['gmfcoll'].items():
                     if len(array):
-                        key = 'gmf_data/grp-%02d/%s' % (grp_id, gsim)
+                        key = 'gmf_data/grp-%02d' % grp_id
                         hdf5.extend3(self.datastore.hdf5path, key, array)
         slicedic = self.oqparam.imtls.slicedic
         with agg_mon:
@@ -498,8 +494,7 @@ class EventBasedCalculator(ClassicalCalculator):
         """Save the attribute nbytes in the gmf_data datasets"""
         ds = self.datastore
         for sm_id in ds['gmf_data']:
-            for rlzno in ds['gmf_data/' + sm_id]:
-                ds.set_nbytes('gmf_data/%s/%s' % (sm_id, rlzno))
+            ds.set_nbytes('gmf_data/' + sm_id)
             ds['gmf_data'].attrs['num_sites'] = len(self.sitecol.complete)
             ds['gmf_data'].attrs['num_imts'] = len(self.oqparam.imtls)
             ds.set_nbytes('gmf_data')
