727b7d21012d3c93120a94a863b7ccaa7d681fb3
Michele Simionato <michele.simionato@gmail.com>
2017-06-13 15:52:25 +0200

    Added commands oq dump and oq restore
    Former-commit-id: 4af1dc97f75d8d668b2b9275bd606adb6012de57

---
+++ openquake/commands/restore.py
@@ -0,0 +1,56 @@
+#  -*- coding: utf-8 -*-
+#  vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+#  Copyright (c) 2017, GEM Foundation
+
+#  OpenQuake is free software: you can redistribute it and/or modify it
+#  under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+
+#  OpenQuake is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+
+#  You should have received a copy of the GNU Affero General Public License
+#  along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
+
+import re
+import sys
+import os.path
+import zipfile
+import sqlite3
+from openquake.baselib import sap
+from openquake.server.dbapi import Db
+
+
+@sap.Script
+def restore(archive, oqdata):
+    """
+    Dump the openquake database and all datastores into a archive zip file.
+    In a multiuser installation must be run as administrator.
+    """
+    oqdata = os.path.abspath(oqdata)
+    assert archive.endswith('.zip'), archive
+    if os.path.exists(oqdata):
+        sys.exit('%s exists already' % oqdata)
+    os.mkdir(oqdata)
+    zipfile.ZipFile(archive).extractall(oqdata)
+    dbpath = os.path.join(oqdata, 'db.sqlite3')
+    db = Db(sqlite3.connect, dbpath, isolation_level=None,
+            detect_types=sqlite3.PARSE_DECLTYPES)
+    n = 0
+    for fname in os.listdir(oqdata):
+        mo = re.match('calc_(\d+)\.hdf5', fname)
+        if mo:
+            job_id = int(mo.group(1))
+            fullname = os.path.join(oqdata, fname)[:-5]  # strip .hdf5
+            db("UPDATE job SET ds_calc_dir=?x WHERE id=?x", fullname, job_id)
+            print('Restoring ' + fname)
+            n += 1
+    print('Extracted %d calculations into %s' % (n, oqdata))
+
+
+restore.arg('archive', 'path to a zip file')
+restore.arg('oqdata', 'path to an oqdata directory')
