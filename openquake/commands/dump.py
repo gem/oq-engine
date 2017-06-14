1f9128abdcdd552cd7071839be23fade91f815c5
Michele Simionato <michele.simionato@gmail.com>
2017-06-14 05:03:50 +0200

    Added info about dump and restore time
    Former-commit-id: 27d5cb80680b84969c78930df8fddc1076df9c08 [formerly 27d5cb80680b84969c78930df8fddc1076df9c08 [formerly 51bf8c071b2293462ca102df5d9e24abb72dc7c7]]
    Former-commit-id: c67a32fce6cbc057443fb75fd2e961728ff76b6c
    Former-commit-id: 8af21850e674589474f95c2d1f404c0876edf40a

--- openquake/commands/dump.py
+++ openquake/commands/dump.py
@@ -17,6 +17,7 @@
 #  along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import print_function
+import time
 import os.path
 from openquake.baselib import sap
 from openquake.server.manage import db
@@ -29,11 +30,14 @@ def dump(archive):
     Dump the openquake database and all the complete calculations into a zip
     file. In a multiuser installation must be run as administrator.
     """
+    t0 = time.time()
     assert archive.endswith('.zip'), archive
     query = 'select ds_calc_dir || ".hdf5" from job where status="complete"'
     fnames = [fname for fname, in db(query) if os.path.exists(fname)]
     fnames.append(db.path)
     zipfiles(fnames, archive, print)
-    print('Archived %d files into %s' % (len(fnames), archive))
+    dt = time.time() - t0
+    print('Archived %d files into %s in %d seconds'
+          % (len(fnames), archive, dt))
 
 dump.arg('archive', 'path to a zip file')
