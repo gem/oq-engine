fcbd6bdfad8fbdc6c702f00ecf02459d288f6ed7
Michele Simionato <michele.simionato@gmail.com>
2017-06-14 05:38:23 +0200

    Using demo dir
    Former-commit-id: 94dac9682de6362044202b01b9241273f53bc60d

--- bin/run-demos.sh
+++ bin/run-demos.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 set -e
 # run some risk demos
-for demo_dir in $(find $TRAVIS_BUILD_DIR -type d | sort); do
+for demo_dir in $(find $TRAVIS_BUILD_DIR/demos -type d | sort); do
    if [ -f $demo_dir/job_hazard.ini ]; then
        cd $demo_dir
        python -m openquake.commands engine --run job_hazard.ini
        python -m openquake.commands engine --run job_risk.ini --hc -1
-       cd -
    fi
 done
 # run the other demos
-for ini in $(find $TRAVIS_BUILD_DIR -name job.ini | sort); do
+for ini in $(find $TRAVIS_BUILD_DIR/demos -name job.ini | sort); do
     oq engine --run $ini
 done
-oq export hcurves-rlzs --exports hdf5
+oq export hcurves-rlzs --exports hdf5 -d /tmp
 python -m openquake.commands engine --lhc
 MPLBACKEND=Agg python -m openquake.commands plot -1
 MPLBACKEND=Agg python -m openquake.commands plot_uhs -1
