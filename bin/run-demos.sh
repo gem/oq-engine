3555badf111c640eeff8bc9b2bec0e8c21b35b5f
Michele Simionato <michele.simionato@gmail.com>
2017-06-14 11:43:34 +0200

    Some improvements
    Former-commit-id: 4d35f617bbd099667da379f810ead00bf37111e8

--- bin/run-demos.sh
+++ bin/run-demos.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 set -e
 # run some risk demos
-for demo_dir in $(find $TRAVIS_BUILD_DIR/demos -type d | sort); do
+for demo_dir in $(find $1 -type d | sort); do
    if [ -f $demo_dir/job_hazard.ini ]; then
-       cd $demo_dir
-       python -m openquake.commands engine --run job_hazard.ini
-       python -m openquake.commands engine --run job_risk.ini --hc -1
+       python -m openquake.commands engine --run $demo_dir/job_hazard.ini
+       python -m openquake.commands engine --run $demo_dir/job_risk.ini --hc -1
    fi
 done
 # run the other demos
-for ini in $(find $TRAVIS_BUILD_DIR/demos -name job.ini | sort); do
-    oq engine --run $ini
+for ini in $(find $1 -name job.ini | sort); do
+    python -m openquake.commands engine --run $ini
 done
 
 # do something with the generated data
