7c7aef172210c31a6f462ce12c722a0fdd6ac5ae
Michele Simionato <michele.simionato@gmail.com>
2017-06-14 05:29:45 +0200

    Run all demos in travis
    Former-commit-id: 956bcac1dcdda29fec6d887e8f95062c8913fa74 [formerly 1684672615a80041c63cfc323e6709f9ed8d58d4]
    Former-commit-id: da93af24db267e98c5156b51e7ea3959c846e292

--- bin/run-demos.sh
+++ bin/run-demos.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 set -e
-python -m openquake.commands engine --run $TRAVIS_BUILD_DIR/demos/hazard/AreaSourceClassicalPSHA/job.ini
+# run some risk demos
+for demo_dir in $(find $TRAVIS_BUILD_DIR -type d | sort); do
+   if [ -f $demo_dir/job_hazard.ini ]; then
+       cd $demo_dir
+       python -m openquake.commands engine --run job_hazard.ini
+       python -m openquake.commands engine --run job_risk.ini --hc -1
+       cd -
+   fi
+done
+# run the other demos
+for ini in $(find $TRAVIS_BUILD_DIR -name job.ini | sort); do
+    oq engine --run $ini
+done
 oq export hcurves-rlzs --exports hdf5
 python -m openquake.commands engine --lhc
 MPLBACKEND=Agg python -m openquake.commands plot -1
