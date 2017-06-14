ed7c18631d3591cee0a98e8602d0c530c3a6df92
Michele Simionato <michele.simionato@gmail.com>
2017-06-14 17:07:45 +0200

    Fixed username
    Former-commit-id: 6cadecab2e0fdd9fa91f24a4f75011877a7cf8c7 [formerly 8c0502dbdfdcc536a9308fac7906e556c499f8a8]
    Former-commit-id: 9e641f495bd790f97f7dce0f6c519d933d404031

--- bin/run-demos.sh
+++ bin/run-demos.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -e
-# run some risk demos
+# run some fast risk demos
 for demo_dir in $(find $1 -type d | sort); do
    if [ -f $demo_dir/job_hazard.ini ]; then
        python -m openquake.commands engine --run $demo_dir/job_hazard.ini
        python -m openquake.commands engine --run $demo_dir/job_risk.ini --hc -1
    fi
 done
-# run the other demos
-for ini in $(find $1 -name job.ini | sort); do
-    python -m openquake.commands engine --run $ini
-done
+# run the other demos (slow)
+#for ini in $(find $1 -name job.ini | sort); do
+#    python -m openquake.commands engine --run $ini
+#done
 
 # do something with the generated data
 python -m openquake.commands export hcurves-rlzs 18 --exports hdf5 -d /tmp
