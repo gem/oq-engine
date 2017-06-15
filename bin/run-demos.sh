68be00a7896a627c07ab99efded602162509e32f
Michele Simionato <michele.simionato@gmail.com>
2017-06-15 05:06:16 +0200

    Split the demos between travis and jenkins

--- bin/run-demos.sh
+++ bin/run-demos.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 set -e
-# run some fast risk demos
-for demo_dir in $(find $1 -type d | sort); do
-   if [ -f $demo_dir/job_hazard.ini ]; then
-       python -m openquake.commands engine --run $demo_dir/job_hazard.ini
-       python -m openquake.commands engine --run $demo_dir/job_risk.ini --hc -1
-   fi
-done
-# run the other demos (slow)
+# commented out some demos that are run on Jenkins with celery
+#for demo_dir in $(find $1 -type d | sort); do
+#   if [ -f $demo_dir/job_hazard.ini ]; then
+#       python -m openquake.commands engine --run $demo_dir/job_hazard.ini
+#       python -m openquake.commands engine --run $demo_dir/job_risk.ini --hc -1
+#   fi
+#done
+# run the other demos
 for ini in $(find $1 -name job.ini | sort); do
     python -m openquake.commands engine --run $ini
 done
 
-# do something with the generated data
-python -m openquake.commands export hcurves-rlzs 18 --exports hdf5 -d /tmp
+# do something with the generated data; -3 is LogicTreeCase3ClassicalPSHA
+python -m openquake.commands export hcurves-rlzs -3 --exports hdf5 -d /tmp
 python -m openquake.commands engine --lhc
-MPLBACKEND=Agg python -m openquake.commands plot 18
-MPLBACKEND=Agg python -m openquake.commands plot_uhs 18
+MPLBACKEND=Agg python -m openquake.commands plot -3
+MPLBACKEND=Agg python -m openquake.commands plot_uhs -3
