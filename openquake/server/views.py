7df67baeeee6daa22bc22351ecff19a02f79c903
hascar <armando.scarpati@globalquakemodel.org>
2017-06-20 14:46:26 +0000

    changed ajax request for remove calc and changed handler status in views.py
    Former-commit-id: c09168e43688b9404e2f97e29635064fee2044e9 [formerly 428afd085374d3d0d05bb09a3923ae0c769296a6]
    Former-commit-id: d7f0c9c57247f7d16b9b045567ac2d591029aa19

--- openquake/server/views.py
+++ openquake/server/views.py
@@ -326,15 +326,21 @@ def calc_remove(request, calc_id):
         message = logs.dbcmd('del_calc', calc_id, user)
     except dbapi.NotFound:
         return HttpResponseNotFound()
-    #return HttpResponse(content=json.dumps(message),
-    #                    content_type=JSON, status=200)
-    if isinstance(message, list):  # list of removed files
+    if 'success' in message:
+         return HttpResponse(content=json.dumps(message),
+                             content_type=JSON, status=200)
+    elif 'error' in message:
+        logging.error(message['error'])
         return HttpResponse(content=json.dumps(message),
-                            content_type=JSON, status=200)
-    else:  # FIXME: the error is not passed properly to the javascript
-        logging.error(message)
-        return HttpResponse(content=message,
-                            content_type='text/plain', status=500)
+                            content_type=JSON, status=403)
+    else:
+        # This is an untrapped server error
+         logging.error(message)
+         return HttpResponse(content=message,
+                             content_type='text/plain', status=500)
+ 
+
+
 
 def log_to_json(log):
     """Convert a log record into a list of strings"""
