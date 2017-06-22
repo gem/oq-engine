7f186d481b4c1d1d1c0911987a6b4883fd32d516
Michele Simionato <michele.simionato@gmail.com>
2017-06-22 14:21:11 +0200

    Updated the changelog [skip CI]
    Former-commit-id: 52224900729e4966bca7955d8a87339ab30d1ce3 [formerly 05c8dc04f700e4048ad881158f8b026677a58a40]
    Former-commit-id: d1cbf139fb214af5cd735c3086d40dc180cf76be

--- openquake/commonlib/readinput.py
+++ openquake/commonlib/readinput.py
@@ -840,6 +840,7 @@ def get_gmfs(oqparam):
     if fname.endswith('.csv'):
         array = writers.read_composite_array(fname)
         R = len(numpy.unique(array['rlzi']))
+        # the array has the structure rlzi, sid, eid, gmv_PGA, gmv_...
         dtlist = [(name, array.dtype[name]) for name in array.dtype.names[:3]]
         num_gmv = len(array.dtype.names[3:])
         assert num_gmv == I, (num_gmv, I)
