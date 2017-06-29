e46751d429dfbbe8b2bdec5ecdd24c556db2c380
Michele Simionato <michele.simionato@gmail.com>
2017-06-29 11:15:04 +0200

    Fixed event_based_risk/test_case_3
    Former-commit-id: dcd15be1539d8f174930139695134ecf2bf1cc24 [formerly 1af28b09108151b19c36f59595540f07f76a2b03]
    Former-commit-id: 6a44e0d2bc6c96ceffb7352601565e6a202ab18b

--- openquake/calculators/tests/event_based_risk_test.py
+++ openquake/calculators/tests/event_based_risk_test.py
@@ -162,8 +162,8 @@ class EventBasedRiskTestCase(CalculatorTestCase):
         grp00 = self.calc.datastore.get_attr('ruptures/grp-00', 'nbytes')
         grp02 = self.calc.datastore.get_attr('ruptures/grp-02', 'nbytes')
         grp03 = self.calc.datastore.get_attr('ruptures/grp-03', 'nbytes')
-        self.assertEqual(grp00, 545)
-        self.assertEqual(grp02, 545)
+        self.assertEqual(grp00, 550)
+        self.assertEqual(grp02, 550)
         self.assertEqual(grp03, 218)
 
         hc_id = self.calc.datastore.calc_id
