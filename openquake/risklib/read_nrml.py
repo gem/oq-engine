71e6d33f94dc6cabb56b84d50dad08ae9c931eb6
Michele Simionato <michele.simionato@gmail.com>
2017-07-06 11:09:47 +0200

    Used an exposure to check the multi-tag feature in event_based_risk/case_master
    Former-commit-id: b69f0ac37939efa8f1753b97f11fab31d87ffbbf [formerly cbfa5fb01a0869095f7a202b662f95ba8b55ff18] [formerly cbfa5fb01a0869095f7a202b662f95ba8b55ff18 [formerly 0d44c454881e73a5f66d2a782d5789cb47549a00]]
    Former-commit-id: c1ba9c99b96fa5d95c76e01caf487605509abaae
    Former-commit-id: d6b9c93b4d6b567da8eb06c3fe2195188817523f

--- openquake/risklib/read_nrml.py
+++ openquake/risklib/read_nrml.py
@@ -393,7 +393,7 @@ def update_validators():
         'vulnerabilityFunction.id': valid.utf8,  # taxonomy
         'consequenceFunction.id': valid.utf8,  # taxonomy
         'asset.id': valid.asset_id,
-        'asset.tag': valid.simple_id,
+        'asset.tag': valid.namelist,
         'costType.name': valid.cost_type,
         'costType.type': valid.cost_type_type,
         'cost.type': valid.cost_type,
