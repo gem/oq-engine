a7d2bfeeb50c7ea163f2e7585380d9ba4455e4f3
Michele Simionato <michele.simionato@gmail.com>
2017-07-24 08:05:29 +0200

    Fixed concurrent_tasks.default [demos]

--- openquake/engine/engine.py
+++ openquake/engine/engine.py
@@ -37,7 +37,12 @@ TERMINATE = valid.boolean(
 
 USE_CELERY = config.get('distribution', 'oq_distribute') == 'celery'
 
-if USE_CELERY:
+if parallel.oq_distribute() == 'zmq':
+    num_cores = sum(int(item[-1]) for item in config.get_host_cores())
+    OqParam.concurrent_tasks.default = num_cores
+    logs.LOG.info('Using %d cores', num_cores)
+
+elif USE_CELERY:
     import celery.task.control
 
     def set_concurrent_tasks_default():
