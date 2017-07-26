dc75351aeedb34e36fe52ae72481251f53961dce
Michele Simionato <michele.simionato@gmail.com>
2017-07-26 07:33:27 +0200

    Added .nsites to Ucerf sources in event based too

--- openquake/calculators/classical.py
+++ openquake/calculators/classical.py
@@ -215,8 +215,7 @@ def saving_sources_by_task(iterargs, dstore):
     for i, args in enumerate(iterargs, 1):
         source_ids.append(' ' .join(src.source_id for src in args[0]))
         for src in args[0]:  # collect source data
-            # UCERF sources have no attribute .nsites
-            data.append((i, getattr(src, 'nsites', 0), src.weight))
+            data.append((i, src.nsites, src.weight))
         yield args
     dstore['task_sources'] = numpy.array([encode(s) for s in source_ids])
     dstore.extend('task_info/source_data', numpy.array(data, source_data_dt))
