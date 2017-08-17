d051610be280e6895d433fe3aa055555f257dc9e
Michele Simionato <michele.simionato@gmail.com>
2017-08-17 11:09:17 +0200

    Added a comment [skip CI]
    Former-commit-id: 9ba123bdaf182d410035265a3ff7c3bb47806068

--- openquake/hazardlib/source/base.py
+++ openquake/hazardlib/source/base.py
@@ -39,7 +39,7 @@ class BaseSeismicSource(with_metaclass(abc.ABCMeta)):
     _slots_ = ['source_id', 'name', 'tectonic_region_type',
                'src_group_id', 'num_ruptures', 'seed', 'id']
     RUPTURE_WEIGHT = 1.  # overridden in (Multi)PointSource, AreaSource
-    nsites = 1  # must be overridden as instance attribute
+    nsites = 1  # FIXME: remove this and fix all hazardlib tests
 
     @abc.abstractproperty
     def MODIFICATIONS(self):
