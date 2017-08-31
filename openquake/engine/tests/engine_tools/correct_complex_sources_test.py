b641577278142067d17f8c897f78ca0568fb5af6
Michele Simionato <michele.simionato@gmail.com>
2017-08-31 07:54:16 +0200

    Extended the test
    Former-commit-id: cf7cfe228acc77ad53c25c9377ecf8f07ca66728

--- openquake/engine/tests/engine_tools/correct_complex_sources_test.py
+++ openquake/engine/tests/engine_tools/correct_complex_sources_test.py
@@ -1,6 +1,8 @@
 import os
+import tempfile
 import unittest
 from openquake.hazardlib import nrml, sourceconverter, InvalidFile
+from openquake.engine.tools.correct_complex_sources import fix
 
 
 class CorrectComplexSourceTestCase(unittest.TestCase):
@@ -8,5 +10,10 @@ class CorrectComplexSourceTestCase(unittest.TestCase):
         fname = os.path.join(os.path.dirname(__file__),
                              'faults_backg_source_model.xml')
         converter = sourceconverter.SourceConverter()
+        # check that the input file requires the fix indeed
         with self.assertRaises(InvalidFile):
             nrml.SourceModelParser(converter).parse_groups(fname)
+        fd, tmpname = tempfile.mkstemp(suffix='.xml')
+        os.close(fd)
+        fix(fname, tmpname)  # invoke the fix
+        print('meld %s %s' % (fname, tmpname))
