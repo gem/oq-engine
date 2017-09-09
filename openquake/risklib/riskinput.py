078d7596170e751746b7f90777bf7f6ba5eb4010
Michele Simionato <michele.simionato@gmail.com>
2017-09-09 06:54:59 +0200

    More cleanup [skip hazardlib] [demos]

--- openquake/risklib/riskinput.py
+++ openquake/risklib/riskinput.py
@@ -408,25 +408,21 @@ class CompositeRiskModel(collections.Mapping):
                 if len(curve_resolutions) > 1:  # example in test_case_5
                     logging.info(
                         'Different num_loss_ratios:\n%s', '\n'.join(lines))
-                cb = scientific.LossTypeCurveBuilder(
-                    l, loss_type, max(curve_resolutions), ratios, ses_ratio,
-                    True, oqparam.conditional_loss_poes,
-                    oqparam.insured_losses)
+                cb = scientific.CurveParams(
+                    l, loss_type, max(curve_resolutions), ratios, True)
             elif loss_type in oqparam.loss_ratios:  # loss_ratios provided
                 lratios = numpy.array(oqparam.loss_ratios[loss_type], F32)
-                cb = scientific.LossTypeCurveBuilder(
-                    l, loss_type, oqparam.loss_curve_resolution,
-                    lratios, ses_ratio, True,
-                    oqparam.conditional_loss_poes, oqparam.insured_losses)
+                cb = scientific.CurveParams(
+                    l, loss_type, oqparam.loss_curve_resolution, lratios, True)
             else:  # no loss_ratios provided
-                cb = scientific.LossTypeCurveBuilder(
+                cb = scientific.CurveParams(
                     l, loss_type, oqparam.loss_curve_resolution,
-                    default_loss_ratios, ses_ratio, False,
-                    oqparam.conditional_loss_poes, oqparam.insured_losses)
+                    default_loss_ratios, False)
             cbs.append(cb)
             self.lti[loss_type] = l
         return scientific.CurveBuilder(
-            cbs, oqparam.insured_losses, oqparam.conditional_loss_poes)
+            cbs, ses_ratio, oqparam.insured_losses,
+            oqparam.conditional_loss_poes)
 
     def get_loss_ratios(self):
         """
