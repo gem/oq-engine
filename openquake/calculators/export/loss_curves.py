4bfc5462385a6e34e741946917ee5de6865b39ae
Michele Simionato <michele.simionato@gmail.com>
2017-09-11 15:21:03 +0200

    Fixed small bug [skip hazardlib] [demos]
    Former-commit-id: bcce57b0efece02da8044eaba0d6555962abc165 [formerly b11b8d7cd26a582cf1c6ef519b6a8bb45b86ca6a]
    Former-commit-id: 36b57ccf60e23382da2415da23b9a0bd35eee392

--- openquake/calculators/export/loss_curves.py
+++ openquake/calculators/export/loss_curves.py
@@ -35,7 +35,7 @@ def get_loss_builder(dstore):
         eff_time, num_events.max())
     return scientific.LossesByPeriodBuilder(
         numpy.array(periods), oq.loss_dt(), weights, num_events,
-        eff_time, oq.risk_investigation_time)
+        eff_time, oq.risk_investigation_time or oq.hazard_investigation_time)
 
 
 class LossCurveExporter(object):
@@ -64,7 +64,7 @@ class LossCurveExporter(object):
             self.builder = get_loss_builder(dstore)
         except KeyError:  # no 'agg_loss_table' for non event_based_risk
             pass
-        self.rit = oq.risk_investigation_time
+        self.rit = oq.risk_investigation_time or oq.hazard_investigation_time
         self.assetcol = dstore['assetcol']
         arefs = [decode(aref) for aref in self.dstore['asset_refs']]
         self.str2asset = {arefs[asset.idx]: asset for asset in self.assetcol}
