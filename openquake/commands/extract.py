8bc0b4d423b1c9cc96c31deb93842b379ca8d662
Michele Simionato <michele.simionato@gmail.com>
2017-09-16 08:32:26 +0200

    Added a function extract_hazard
    Former-commit-id: b700c754acebdd3f9e4ba8cf7206b8c9a8864a51

---
+++ openquake/commands/extract.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+#
+# Copyright (C) 2017 GEM Foundation
+#
+# OpenQuake is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# OpenQuake is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with OpenQuake. If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import print_function
+import logging
+
+from openquake.baselib import performance, sap
+from openquake.commonlib import datastore
+from openquake.calculators.extract import extract as extract_
+
+
+# the export is tested in the demos
+@sap.Script
+def extract(what, calc_id=-1):
+    """
+    Extract an output from the datastore and prints it on the terminal.
+    This is meant for debugging.
+    """
+    logging.basicConfig(level=logging.INFO)
+    dstore = datastore.read(calc_id)
+    parent_id = dstore['oqparam'].hazard_calculation_id
+    if parent_id:
+        dstore.parent = datastore.read(parent_id)
+    with performance.Monitor('extract', measuremem=True) as mon, dstore:
+        print(extract_(dstore, what))
+    if mon.duration > 1:
+        print(mon)
+
+
+extract.arg('what', 'string specifying what to export')
+extract.arg('calc_id', 'number of the calculation', type=int)
