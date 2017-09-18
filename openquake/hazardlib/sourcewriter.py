922fdade420c5487125b15e87720674b304a7ebe
Michele Simionato <michele.simionato@gmail.com>
2017-09-18 14:42:49 +0200

    Fixed test
    Former-commit-id: 75dd6b089e4bb3dc55cafca4fe97f0f412efda0b

--- openquake/hazardlib/sourcewriter.py
+++ openquake/hazardlib/sourcewriter.py
@@ -236,8 +236,8 @@ def build_multi_mfd(mfd):
     for name in sorted(mfd.kwargs):
         values = mfd.kwargs[name]
         if name in ('magnitudes', 'occurRates'):
-            if len(values[0]) > 1:  # arbitraryMFD; TODO: add a test
-                values = numpy.concatenate(values)
+            if len(values[0]) > 1:  # tested in multipoint_test.py
+                values = list(numpy.concatenate(values))
             else:
                 values = sum(values, [])
         node.append(Node(name, text=values))
