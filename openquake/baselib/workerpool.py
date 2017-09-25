6537f2358cddff20ad50b314a7277f6d5283a55c
Michele Simionato <michele.simionato@gmail.com>
2017-09-25 07:24:01 +0200

    More work [skip CI]
    Former-commit-id: 946fff12991b3ddd5ca78643ba4d67481ac59a32

--- openquake/baselib/workerpool.py
+++ openquake/baselib/workerpool.py
@@ -1,72 +1,85 @@
 import os
 import sys
 import signal
-import logging
 import multiprocessing
 from openquake.baselib import zeromq as z, parallel as p
 
 
-def starmap(frontend_url, backend_url, func, allargs):
+def starmap(frontend_url, backend_url, func, iterargs):
     """
-    starmap a function over an iterator of arguments by using a zmq socket
+    starmap a function over an iterator of arguments by using a zmq socket.
+
+    :param frontend_url:
+    :param backend_url: url with a range of ports
+    :param func:
+    :param iterargs:
     """
-    backurl, receiver = z.bind_to_random_port(backend_url, z.zmq.PULL)
-    sender = z.connect(frontend_url, z.zmq.PUSH)
-    with sender:
+    with z.Socket(backend_url, z.zmq.PULL) as receiver, \
+         z.Socket(frontend_url, z.zmq.PUSH, 'connect') as sender:
         n = 0
-        for args in allargs:
-            args[-1].backurl = backurl
+        for args in iterargs:
+            args[-1].backurl = receiver.true_end_point
             sender.send_pyobj((func, args))
             n += 1
         yield n
-    with receiver:
         for _ in range(n):
-            yield receiver.recv_pyobj()
+            # receive n responses for the n requests sent
+            yield receiver.zsocket.recv_pyobj()
 
 
 class WorkerPool(object):
     """
+    A pool of workers accepting the command 'stop' and 'kill' and reading
+    tasks to perform from the stream_url.
+
+    :param ctrl_url: zmq address of the control socket
+    :param stream_url: zmq address of the task streamer
+    :param num_workers: a string with the number of workers (or 'default')
     """
-    def __init__(self, backend, num_workers=None):
-        self.backend = backend
-        self.num_workers = num_workers or multiprocessing.cpu_count()
+    def __init__(self, ctrl_url, stream_url, num_workers='default'):
+        self.ctrl_url = ctrl_url
+        self.stream_url = stream_url
+        self.num_workers = (multiprocessing.cpu_count()
+                            if num_workers == 'default' else int(num_workers))
         self.pid = os.getpid()
 
     def worker(self, sock):
         for cmd_ in sock:
             cmd, args = cmd_[0], cmd_[1:]
-            with z.Socket(args[-1].backurl, z.zmq.PUSH) as s:
+            backurl = args[-1].backurl  # attached to the monitor
+            with z.Socket(backurl, z.zmq.PUSH, 'connect') as s:
                 s.push(p.safely_call(cmd, args))
 
     def start(self):
         # start workers
         self.workers = []
         for _ in range(self.num_workers):
-            sock = z.Socket(self.backend, z.zmq.PULL, 'connect')
+            sock = z.Socket(self.stream_url, z.zmq.PULL, 'connect')
             proc = multiprocessing.Process(target=self.worker, args=(sock,))
             proc.start()
             sock.pid = proc.pid
             self.workers.append(sock)
-        logging.warn('WorkerPool started with %s on %s, pid=%d',
-                     sys.executable, self.backend, self.pid)
-        # start control socket
+        # print pid
+        sys.stdout.write('%d\n' % self.pid)
+        sys.stdout.flush()
+        # start control loop accepting the commands stop and kill
+        ctrlsock = z.Socket(self.ctrl_url, z.zmq.REP)
+        for cmd in ctrlsock:
+            assert cmd in ('stop', 'kill'), cmd
+            msg = getattr(self, cmd)()
+            ctrlsock.send(msg)
 
     def stop(self):
         for sock in self.workers:
+            os.kill(sock.pid, signal.SIGINT)
+        return 'WorkerPool %s stopped' % self.ctrl_url
+
+    def kill(self):
+        for sock in self.workers:
             os.kill(sock.pid, signal.SIGTERM)
-        logging.warn('WorkerPool stopped')
+        return 'WorkerPool %s killed' % self.ctrl_url
 
 
 if __name__ == '__main__':
-    kind, frontend, backend, num_workers = sys.argv[1:]
-    if kind == 'master':
-        # start frontend->backend proxy
-        try:
-            z.zmq.proxy(z.bind(frontend, z.zmq.PULL),
-                        z.bind(backend, z.zmq.PUSH))
-        except (KeyboardInterrupt, z.zmq.ZMQError):
-            pass
-    elif kind == 'workerpool':
-        WorkerPool(backend, num_workers).start()
-    else:
-        raise ValueError('Unknown pool kind: %s' % kind)
+    ctrl_url, stream_url, num_workers = sys.argv[1:]
+    WorkerPool(ctrl_url, stream_url, num_workers).start()
