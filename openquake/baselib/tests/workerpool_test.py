a8cc3e7016ad5ba38dee3be2beab86b9a807a0a0
Michele Simionato <michele.simionato@gmail.com>
2017-09-25 15:24:48 +0200

    More work

---
+++ openquake/baselib/tests/workerpool_test.py
@@ -0,0 +1,48 @@
+#  -*- coding: utf-8 -*-
+#  vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+#  Copyright (c) 2017, GEM Foundation
+
+#  OpenQuake is free software: you can redistribute it and/or modify it
+#  under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+
+#  OpenQuake is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+
+#  You should have received a copy of the GNU Affero General Public License
+#  along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+from openquake.baselib.workerpool import MasterWorker
+from openquake.baselib.performance import PerformanceMonitor
+
+
+def double(x, mon):
+    return 2 * x
+
+
+class WorkerPoolTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.master = MasterWorker('ipc://starmap', 'tcp://127.0.0.1:5000-5100')
+        cls.master.start()
+
+    def test1(self):
+        mon = PerformanceMonitor()
+        iterargs = ((i, mon) for i in range(10))
+        res = sum(self.master.starmap(double, iterargs))
+        self.assertEqual(res, 90)  # sum[0, 2, 4, 6, 8, 10, 12, 14, 16, 18]
+
+    def test2(self):
+        mon = PerformanceMonitor()
+        iterargs = ((i, mon) for i in range(5))
+        res = sum(self.master.starmap(double, iterargs))
+        self.assertEqual(res, 20)  # sum[0, 2, 4, 6, 8]
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.master.stop()
