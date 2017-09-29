40420e8804f5e2800cdc59ed5ecfb093c0498630
Michele Simionato <michele.simionato@gmail.com>
2017-09-29 11:31:31 +0200

    Refactored the configuration mechanism [skip CI]
    Former-commit-id: 18c1621fce88ef4a31f22635d1927465ae53d614 [formerly ff400bd5d5d12b9b9f3312fa12b45602a4be22b3]
    Former-commit-id: 555ee227b361f13cc9d6582a04d5bad11f8cd19e

--- openquake/baselib/__init__.py
+++ openquake/baselib/__init__.py
@@ -16,8 +16,36 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake. If not, see <http://www.gnu.org/licenses/>.
 
+import os
+import collections
+from openquake.baselib.python3compat import configparser
 from openquake.baselib.general import git_suffix
 
 # the version is managed by packager.sh with a sed
 __version__ = '2.7.0'
 __version__ += git_suffix(__file__)
+
+PATHS = ('~/openquake.cfg', '/etc/openquake/openquake.cfg')
+
+
+class DotDict(collections.OrderedDict):
+    """A string-valued dictionary that can be accessed with the "." notation"""
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(key)
+
+config = DotDict()  # global configuration
+
+
+def _read(*paths):
+    # load the configuration file by looking at the given paths
+    paths = paths + PATHS
+    parser = configparser.SafeConfigParser()
+    parser.read(os.path.normpath(os.path.expanduser(p)) for p in paths)
+    config.clear()
+    for section in parser.sections():
+        config[section] = DotDict(parser.items(section))
+
+config.read = _read
