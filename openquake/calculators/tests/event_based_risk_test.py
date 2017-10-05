0b559d0726e081b6aa4c74a4c5e3132ee75464dc
Michele Simionato <michele.simionato@gmail.com>
2017-10-05 17:04:38 +0200

    Fixed test [skip hazardlib]

--- openquake/calculators/tests/event_based_risk_test.py
+++ openquake/calculators/tests/event_based_risk_test.py
@@ -236,8 +236,8 @@ class EventBasedRiskTestCase(CalculatorTestCase):
 
         # extract curves by tag
         tags = ['taxonomy=tax1', 'state=01', 'cresta=0.11']
-        array = extract(self.calc.datastore, 'aggcurves/structural', *tags)
-        self.assertEqual(array.shape, (4, 2))  # 4 stats, 2 return periods
+        a = extract(self.calc.datastore, 'aggcurves/structural', *tags)
+        self.assertEqual(a.array.shape, (4, 2))  # 4 stats, 2 return periods
 
         fname = writetmp(view('portfolio_loss', self.calc.datastore))
         self.assertEqualFiles('expected/portfolio_loss.txt', fname, delta=1E-5)
