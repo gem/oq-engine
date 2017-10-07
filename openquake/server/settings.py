a702a8a7b23cf770613b44d03d4793c90de959e9
Daniele Viganò <daniele@vigano.me>
2017-10-07 17:14:54 +0200

    Activate Tools automatically if openquakeplatform module is installed
    Former-commit-id: 3d24d2f0bcc6a47b08f9b3ac5e6fa11ff10d63ed [formerly 3d24d2f0bcc6a47b08f9b3ac5e6fa11ff10d63ed [formerly 136ff16161b0a8fb812bd75dbf5a6a9cd79221bd]]
    Former-commit-id: b373bdfd78b4cd70155fb477f1cad5f5915972d4
    Former-commit-id: 21e2538082323c0501fdd74f6d23dba4e6983417

--- openquake/server/settings.py
+++ openquake/server/settings.py
@@ -21,6 +21,13 @@ import getpass
 
 from openquake.baselib import config
 
+try:
+    import openquakeplatform
+    STANDALONE = True
+except ImportError:
+    STANDALONE = False
+
+
 DB_SECTION = config.dbserver
 
 INSTALLED_APPS = ('openquake.server.db',)
@@ -115,8 +122,6 @@ INSTALLED_APPS += (
     'openquake.server',
 )
 
-STANDALONE_APPS = ()
-
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error.
@@ -161,6 +166,28 @@ LOGGING = {
 }
 
 FILE_UPLOAD_MAX_MEMORY_SIZE = 1
+
+
+# OpenQuake Standalone tools (IPT, Taxtweb, Taxonomy Glossary)
+STANDALONE_APPS = ()
+if STANDALONE:
+    INSTALLED_APPS += (
+        'openquakeplatform',
+    )
+
+    STANDALONE_APPS += (
+        'openquakeplatform_ipt',
+        'openquakeplatform_taxtweb',
+        'openquakeplatform_taxonomy',
+    )
+
+    INSTALLED_APPS += STANDALONE_APPS
+
+    FILE_PATH_FIELD_DIRECTORY = os.path.join(os.path.expanduser('~'), 'oqdata')
+
+    CONTEXT_PROCESSORS = TEMPLATES[0]['OPTIONS']['context_processors']
+    CONTEXT_PROCESSORS.insert(0, 'django.template.context_processors.request')
+    CONTEXT_PROCESSORS.append('openquakeplatform.utils.oq_context_processor')
 
 try:
     # Try to load a local_settings.py from the current folder; this is useful
