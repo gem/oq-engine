c28e223561bb084b8ad8ebd3d3023bdccfa8cb45
Michele Simionato <michele.simionato@gmail.com>
2017-10-10 06:34:39 +0200

    Reduced scenario_risk/case_8
    Former-commit-id: d0deadd03edf3dba4f77660d75728b6ed95a1a14 [formerly 06d997fb075bedb13475ad5df1cbc8e82781f478] [formerly 06d997fb075bedb13475ad5df1cbc8e82781f478 [formerly 5b71d413098c8045979c7665b317ad1c6391e6ee]]
    Former-commit-id: a50f7abcb228f64cfd8cc373ef9384486e93f6a9
    Former-commit-id: 0c49d5ffb22a4158bd020a6ba6327fb2252512f0

--- openquake/calculators/tests/scenario_risk_test.py
+++ openquake/calculators/tests/scenario_risk_test.py
@@ -172,7 +172,7 @@ class ScenarioRiskTestCase(CalculatorTestCase):
         # not in the asset locations
         self.run_calc(case_8.__file__, 'job.ini')
         tot = self.calc.datastore['losses_by_tag-rlzs'].value.sum()
-        self.assertAlmostEqual(tot / 1E6, 17.64311, 5)
+        self.assertAlmostEqual(tot / 1E6, 1.96813145)
 
         # make sure the fullreport can be extracted
         view('fullreport', self.calc.datastore)
