e832793f5f860fbeb47c43bac9f184e49693ac17
Michele Simionato <michele.simionato@gmail.com>
2017-10-11 14:05:19 +0200

    Added attributes to agglosses extracted output [skip hazardlib]
    Former-commit-id: 088e34066dea22253940c880d190a7ca08496113

--- openquake/calculators/tests/event_based_risk_test.py
+++ openquake/calculators/tests/event_based_risk_test.py
@@ -238,7 +238,7 @@ class EventBasedRiskTestCase(CalculatorTestCase):
         # extract curves by tag
         tags = ['taxonomy=tax1', 'state=01', 'cresta=0.11']
         a = extract(self.calc.datastore, 'aggcurves/structural', *tags)
-        self.assertEqual(a.array.shape, (1, 4, 2))  # 4 stats, 2 return periods
+        self.assertEqual(a.array.shape, (4, 2))  # 4 stats, 2 return periods
 
         fname = writetmp(view('portfolio_loss', self.calc.datastore))
         self.assertEqualFiles('expected/portfolio_loss.txt', fname, delta=1E-5)
