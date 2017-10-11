ea7dda69d125d7178bc3e3eaebd35891af4d638f
Michele Simionato <michele.simionato@gmail.com>
2017-10-11 15:57:08 +0200

    Fixed python 3 bug [skip hazardlib]
    Former-commit-id: 67a602ee06b5b94cd926888cbb4860dfc10a5b7f [formerly a30fd4b53e9ec3bd9a05aef6a7af1209aaecfde7] [formerly a30fd4b53e9ec3bd9a05aef6a7af1209aaecfde7 [formerly 52dd923a96d4e325da58df44b09b72a36157be0c]]
    Former-commit-id: 1b7dc93fdaf215e0575163ee2bbd6117d7bc6aa4
    Former-commit-id: bd7c0e5648dfe3ff7f92e8198a5175c16c21cd3c

--- openquake/calculators/tests/scenario_risk_test.py
+++ openquake/calculators/tests/scenario_risk_test.py
@@ -164,8 +164,8 @@ class ScenarioRiskTestCase(CalculatorTestCase):
         # extract agglosses with a * and a selection
         obj = extract(self.calc.datastore, 'agglosses/structural',
                       'state=*', 'cresta=0.11')
-        self.assertEqual(obj.selected, ['state=*', 'cresta=0.11'])
-        self.assertEqual(obj.tags, ['state=01'])
+        self.assertEqual(obj.selected, [b'state=*', b'cresta=0.11'])
+        self.assertEqual(obj.tags, [b'state=01'])
         numpy.testing.assert_almost_equal(
             obj.array, [[1299.3848877, 1561.6965332]])
 
