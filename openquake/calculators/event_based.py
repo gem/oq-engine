3f0c7bab996ba92d76574054be91ae7b60d8fa6e
Michele Simionato <michele.simionato@gmail.com>
2017-10-13 13:20:45 +0200

    Fix [skip hazardlib]
    Former-commit-id: 8d956a230a8e7388620f6fe7156328a01b8921e4

--- openquake/calculators/event_based.py
+++ openquake/calculators/event_based.py
@@ -309,8 +309,8 @@ def compute_gmfs_and_curves(getter, oq, monitor):
         duration = oq.investigation_time * oq.ses_per_logic_tree_path
         with monitor('building hazard', measuremem=True):
             gmfdata = numpy.fromiter(getter.gen_gmv(), getter.gmf_data_dt)
-            hazard = sorted(getter.get_hazard(data=gmfdata).items())
-        for sid, hazardr in hazard:
+            hazard = getter.get_hazard(data=gmfdata)
+        for sid, hazardr in zip(getter.sids, hazard):
             for rlzi, array in hazardr.items():
                 if len(array) == 0:  # no data
                     continue
