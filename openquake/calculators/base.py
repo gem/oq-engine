dd1a2d64fc10609c7a03cc1d6e16889a1e5a3e4e
Michele Simionato <michele.simionato@gmail.com>
2017-10-14 11:22:41 +0200

    Added an utility to plot assets and sites
    Former-commit-id: f71b8d0db0055488ec72ee214aecb4b63038be41 [formerly 5863a2a4d989abcf791672e944a2bc0d1784299b]
    Former-commit-id: de4b35de393a1d05d44b2116eacf71c7f6fe23e3

--- openquake/calculators/base.py
+++ openquake/calculators/base.py
@@ -340,7 +340,7 @@ class HazardCalculator(BaseCalculator):
         if some assets were discarded or if there were missing assets
         for some sites.
         """
-        maximum_distance = self.oqparam.asset_hazard_distance
+        asset_hazard_distance = self.oqparam.asset_hazard_distance
         siteobjects = geo.utils.GeographicObjects(
             Site(sid, lon, lat) for sid, lon, lat in
             zip(sitecol.sids, sitecol.lons, sitecol.lats))
@@ -348,13 +348,13 @@ class HazardCalculator(BaseCalculator):
         for assets in self.assetcol.assets_by_site():
             if len(assets):
                 lon, lat = assets[0].location
-                site, _ = siteobjects.get_closest(lon, lat, maximum_distance)
-                if site:
+                site, min_dist = siteobjects.get_closest(lon, lat)
+                if min_dist <= asset_hazard_distance:
                     assets_by_sid += {site.sid: list(assets)}
         if not assets_by_sid:
             raise AssetSiteAssociationError(
                 'Could not associate any site to any assets within the '
-                'maximum distance of %s km' % maximum_distance)
+                'maximum distance of %s km' % asset_hazard_distance)
         mask = numpy.array([sid in assets_by_sid for sid in sitecol.sids])
         assets_by_site = [assets_by_sid.get(sid, []) for sid in sitecol.sids]
         return sitecol.filter(mask), asset.AssetCollection(
