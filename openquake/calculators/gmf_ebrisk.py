d324bac037d35097eff2ed98dc5d09e295ac5251
Michele Simionato <michele.simionato@gmail.com>
2017-10-26 13:15:25 +0200

    Made number_of_ground_motion_fields optional in scenario_risk too [skip hazardlib] [demos]
    Former-commit-id: b58bcc3c1d56105edb230271743979c4d0c9be32

--- openquake/calculators/gmf_ebrisk.py
+++ openquake/calculators/gmf_ebrisk.py
@@ -48,12 +48,12 @@ class GmfEbRiskCalculator(base.RiskCalculator):
         self.A = len(self.assetcol)
         self.E = oq.number_of_ground_motion_fields
         self.I = oq.insured_losses + 1
+        eids, gmfs = base.get_gmfs(self)  # shape (R, N, E, I)
         if oq.ignore_covs:
             eps = numpy.zeros((self.A, self.E), numpy.float32)
         else:
             logging.info('Building the epsilons')
             eps = self.make_eps(self.E)
-        eids, gmfs = base.get_gmfs(self)  # shape (R, N, E, I)
         self.R = len(gmfs)
         self.riskinputs = self.build_riskinputs('gmf', eps, eids)
         self.param['assetcol'] = self.assetcol
