97d7c11898c0b5aac04cf94f19bf2b5b69f1fc8a
Michele Simionato <michele.simionato@gmail.com>
2017-10-29 06:09:50 +0100

    Simplified gsims_by_grp_id
    Former-commit-id: c442acb58c8199e59bc782c2aadf51badbc0a21a [formerly 2d3e86e5719535a875b1c78f0c058fc4c7bd7a0d]
    Former-commit-id: 45461027ef7d748c2ca62f23393d4683f680c31b

--- openquake/calculators/views.py
+++ openquake/calculators/views.py
@@ -590,10 +590,10 @@ def view_required_params_per_trt(token, dstore):
     """
     Display the parameters needed by each tectonic region type
     """
-    gsims_per_grp_id = sorted(
-        dstore['csm_info'].get_rlzs_assoc().gsims_by_grp_id.items())
+    rlzs_assoc = dstore['csm_info'].get_rlzs_assoc()
     tbl = []
-    for grp_id, gsims in gsims_per_grp_id:
+    for grp_id, trt in sorted(rlzs_assoc.trt_by_grp.items()):
+        gsims = rlzs_assoc.gsim_lt.get_gsims(trt)
         maker = ContextMaker(gsims)
         distances = sorted(maker.REQUIRES_DISTANCES)
         siteparams = sorted(maker.REQUIRES_SITES_PARAMETERS)
