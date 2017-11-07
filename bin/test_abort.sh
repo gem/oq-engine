4d9af263356471c74033f7cac95b0d8091595f54
Michele Simionato <michele.simionato@gmail.com>
2017-11-07 06:33:33 +0100

    Cleanup [skip CI]

--- bin/test_abort.sh
+++ bin/test_abort.sh
@@ -5,12 +5,12 @@ if [ ! -d "$1" ]; then
     exit 1
 fi
 cd $1  # demos directory
-python3 -m openquake.commands engine --run hazard/AreaSourceClassicalPSHA/job.ini &
+oq engine --run hazard/AreaSourceClassicalPSHA/job.ini &
 sleep .25
-python3 -m openquake.commands engine --run hazard/EventBasedPSHA/job.ini &
+oq engine --run hazard/EventBasedPSHA/job.ini &
 sleep .25
-python3 -m openquake.commands engine --run hazard/Disaggregation/job.ini &
+oq engine --run hazard/Disaggregation/job.ini &
 sleep .25
-python3 -m openquake.commands engine --run hazard/ScenarioCase1/job.ini &
+oq engine --run hazard/ScenarioCase1/job.ini &
 sleep 2  # give time to start
-python3 -m openquake.commands abort -2  # the disaggregation
+oq abort -2  # the disaggregation
