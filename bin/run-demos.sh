d334ec4b5086c4efd58fc287071f2370c3289b05
Michele Simionato <michele.simionato@gmail.com>
2017-11-07 16:13:43 +0100

    Restored subprocess.Popen [skip hazardlib] [demos]
    Former-commit-id: a495643682f32bfebf5d8253f52ae3afa7bab7e5 [formerly a495643682f32bfebf5d8253f52ae3afa7bab7e5 [formerly 2e9b21fe10d4cefdb1b79abbab490a9ace37c3c2]]
    Former-commit-id: 6be1e6d87da036f82e2d495a5afe791621751c49
    Former-commit-id: a050453d8772f226f9683894806077974582c3a5

--- bin/run-demos.sh
+++ bin/run-demos.sh
@@ -3,8 +3,8 @@ set -e
 # run demos with job_hazard.ini and job_risk.ini
 for demo_dir in $(find "$1" -type d | sort); do
    if [ -f $demo_dir/job_hazard.ini ]; then
-       python -m openquake.commands engine --run $demo_dir/job_hazard.ini
-       python -m openquake.commands engine --run $demo_dir/job_risk.ini --hc -1
+       oq engine --run $demo_dir/job_hazard.ini
+       oq engine --run $demo_dir/job_risk.ini --hc -1
    fi
 done
 # run the other demos
@@ -14,23 +14,23 @@ if [ ! -d "$1" ]; then
 fi
 
 for ini in $(find $1 -name job.ini | sort); do
-    python -m openquake.commands engine --run $ini
+    oq engine --run $ini
 done
 
 # do something with the generated data; -2 is LogicTreeCase3ClassicalPSHA
-python -m openquake.commands extract -2 hazard/rlzs
-python -m openquake.commands engine --lhc
-MPLBACKEND=Agg python -m openquake.commands plot -2
-MPLBACKEND=Agg python -m openquake.commands plot_uhs -2
-MPLBACKEND=Agg python -m openquake.commands plot_sites -2
+oq extract -2 hazard/rlzs
+oq engine --lhc
+MPLBACKEND=Agg oq plot -2
+MPLBACKEND=Agg oq plot_uhs -2
+MPLBACKEND=Agg oq plot_sites -2
 
 # fake a wrong calculation still in executing status (AreaSource)
-python -m openquake.commands db set_status 26 executing
+oq db set_status 26 executing
 # repeat the failed/executing calculation, which is useful for QGIS
-python -m openquake.commands engine --run $1/hazard/AreaSourceClassicalPSHA/job.ini
+oq engine --run $1/hazard/AreaSourceClassicalPSHA/job.ini
 
 # display the calculations
-python -m openquake.commands db find %
+oq db find %
 
 # build an HTML report
-python -m openquake.commands engine --make-html-report today
+oq engine --make-html-report today
