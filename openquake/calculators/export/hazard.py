8f28d026f5356521508ffcb07b37dcf2f068ce24
Michele Simionato <michele.simionato@gmail.com>
2017-11-08 15:17:56 +0100

    Removed the poe from the disaggregation outputs in case of iml_disagg [skip hazardlib] [demos]
    Former-commit-id: 2e98111cedc6cb69ef25a940cfeebaa3b5f5a93e

--- openquake/calculators/export/hazard.py
+++ openquake/calculators/export/hazard.py
@@ -874,7 +874,7 @@ def export_disagg_xml(ekey, dstore):
         matrix = dstore['disagg/' + key]
         attrs = group[key].attrs
         rlz = rlzs[attrs['rlzi']]
-        poe = attrs['poe']
+        poe = attrs.get('poe')
         iml = attrs['iml']
         imt, sa_period, sa_damping = from_string(attrs['imt'])
         fname = dstore.export_path(key + '.xml')
