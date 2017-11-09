701b384fc6c59791ea46c10d10dedaee2cee6397
Michele Simionato <michele.simionato@gmail.com>
2017-11-09 13:32:15 +0100

    Some refactoring
    Former-commit-id: 6196fec7bea6a0abd53a717c8fe9f5ce1d5f7638 [formerly 7b9a9cda0863e8656d234d907f9db16ce498a2d9]
    Former-commit-id: 0eb71317291d2dc037055d12b25254f55e470a5a

--- openquake/calculators/disaggregation.py
+++ openquake/calculators/disaggregation.py
@@ -142,7 +142,7 @@ producing too small PoEs.'''
     def get_curves(self, sid):
         """
         Get all the relevant hazard curves for the given site ordinal.
-        Returns a dictionary {(rlz_id, imt) -> curve}.
+        Returns a dictionary rlz_id -> curve_by_imt.
         """
         dic = {}
         imtls = self.oqparam.imtls
@@ -165,7 +165,7 @@ producing too small PoEs.'''
                         'skipping site %d, rlz=%d, IMT=%s',
                         sid, rlz.ordinal, imt_str)
                     continue
-                dic[rlz.ordinal, imt_str] = poes[imt_str]
+                dic[rlz.ordinal] = poes
         return dic
 
     def full_disaggregation(self):
@@ -205,8 +205,10 @@ producing too small PoEs.'''
                 sid = sitecol.sids[i]
                 curve = curves[i]
                 # populate max_poe array
-                for (rlzi, imt), poes in curve.items():
-                    max_poe[rlzi][imt] = max(max_poe[rlzi][imt], poes.max())
+                for rlzi, poes in curve.items():
+                    for imt in oq.imtls:
+                        max_poe[rlzi][imt] = max(
+                            max_poe[rlzi][imt], poes[imt].max())
                 if not curve:
                     continue  # skip zero-valued hazard curves
                 bb = bb_dict[sid]
