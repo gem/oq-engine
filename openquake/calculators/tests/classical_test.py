715576ccea095e3f61cbf7e190991ab69dd488f2
Michele Simionato <michele.simionato@gmail.com>
2017-11-11 18:16:56 +0100

    Fixed a test [skip hazardlib]
    Former-commit-id: e298daf7c155966f4e071544f7715dab4cb59969 [formerly 4622ab2dc1fc3934ab1d2e6bd2ead795f72df2b5]
    Former-commit-id: ac3af2ae588b22affbfd50d76390159173ce5752

--- openquake/calculators/tests/classical_test.py
+++ openquake/calculators/tests/classical_test.py
@@ -187,7 +187,7 @@ class ClassicalTestCase(CalculatorTestCase):
                               delta=1E-5)
 
         # test extract; 'hazard/rlzs also works
-        haz = dict(extract(self.calc.datastore, 'hazard/rlz-0'))
+        haz = dict(extract(self.calc.datastore, 'qgis-hazard/rlz-0'))
         self.assertEqual(
             sorted(haz),
             ['checksum32', 'hcurves-rlz-0', 'hmaps-rlz-0', 'oqparam',
