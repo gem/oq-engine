7cbda3320ba7ebaa9d9d5740a0bc3e153046eb90
Michele Simionato <michele.simionato@gmail.com>
2017-11-16 05:02:57 +0100

    Added SourceFilter.get_bounding_boxes
    Former-commit-id: 941cecc484f20a7d2da63f37d39c53b7b862fc7c [formerly 9c3080b9c0811cde7772bc63189eb7bb42974b48]
    Former-commit-id: 7193f40a0dea4bd791e0f6db8600d5f5ed78497a

--- openquake/hazardlib/tests/calc/filters_test.py
+++ openquake/hazardlib/tests/calc/filters_test.py
@@ -18,7 +18,10 @@
 
 import unittest
 from numpy.testing import assert_almost_equal as aae
-from openquake.hazardlib.calc.filters import IntegrationDistance, MAX_DISTANCE
+from openquake.hazardlib.geo.point import Point
+from openquake.hazardlib.site import Site, SiteCollection
+from openquake.hazardlib.calc.filters import (
+    IntegrationDistance, MAX_DISTANCE, SourceFilter)
 
 
 class IntegrationDistanceTestCase(unittest.TestCase):
@@ -37,3 +40,37 @@ class IntegrationDistanceTestCase(unittest.TestCase):
         aae(maxdist('ANY_TRT', mag=6.9), 300)
         bb = maxdist.get_bounding_box(0, 10, 'ANY_TRT', mag=6.9)
         aae(bb, [-2.7395804, 7.30204, 2.7395804, 12.69796])
+
+
+class SourceFilterTestCase(unittest.TestCase):
+    def test_get_bounding_boxes(self):
+        maxdist = IntegrationDistance({'default': [
+            (3, 30), (4, 40), (5, 100), (6, 200), (7, 300), (8, 400)]})
+        sitecol = SiteCollection([
+            Site(location=Point(10, 20, 30),
+                 vs30=1.2, vs30measured=True,
+                 z1pt0=3.4, z2pt5=5.6, backarc=True),
+            Site(location=Point(-1.2, -3.4, -5.6),
+                 vs30=55.4, vs30measured=False,
+                 z1pt0=66.7, z2pt5=88.9, backarc=False)])
+        srcfilter = SourceFilter(sitecol, maxdist)
+        bb1, bb2 = srcfilter.get_bounding_boxes(mag=4.5)
+        # bounding boxes in the form west, east, north, south
+        aae(bb1, (9.0429636, 10.9570364, 20.89932, 19.10068))
+        aae(bb2, (-2.1009058, -0.29909425, -2.50068, -4.29932))
+
+    def test_international_date_line(self):
+        maxdist = IntegrationDistance({'default': [
+            (3, 30), (4, 40), (5, 100), (6, 200), (7, 300), (8, 400)]})
+        sitecol = SiteCollection([
+            Site(location=Point(179, 80),
+                 vs30=1.2, vs30measured=True,
+                 z1pt0=3.4, z2pt5=5.6, backarc=True),
+            Site(location=Point(-179, 80),
+                 vs30=55.4, vs30measured=False,
+                 z1pt0=66.7, z2pt5=88.9, backarc=False)])
+        srcfilter = SourceFilter(sitecol, maxdist)
+        bb1, bb2 = srcfilter.get_bounding_boxes(mag=4.5)
+        # bounding boxes in the form west, east, north, south
+        aae(bb1, [173.8210225, 184.1789775, 80.89932, 79.10068])
+        aae(bb2, [-184.1789775, -173.8210225, 80.89932, 79.10068])
