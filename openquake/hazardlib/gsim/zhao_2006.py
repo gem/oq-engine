3a1350c32f2b2f3d2494ac48c90d273120fc6767
klunk386 <poggi.valerio@gmail.com>
2017-11-21 15:34:47 +0100

    Distance taper for Zhao 2006
    Former-commit-id: 1d47d12752f8486343e406b912370e1fc2d57a41 [formerly 1d47d12752f8486343e406b912370e1fc2d57a41 [formerly c6e844b0bad2f5038d1ef0b03f48f16ab5aac29c]]
    Former-commit-id: d25f6efd4aff4a1741f6a65d761400714934871e
    Former-commit-id: 1b126a877cf0168cfe65fd9f798bd08b0f32065e

--- openquake/hazardlib/gsim/zhao_2006.py
+++ openquake/hazardlib/gsim/zhao_2006.py
@@ -643,3 +643,25 @@ class ZhaoEtAl2006SSlabCascadia(ZhaoEtAl2006SSlab):
                                     num_sites=len(sites.vs30))
 
         return mean, stddevs
+
+
+class ZhaoEtAl2006AscSGS(ZhaoEtAl2006Asc):
+    """
+    This class extends the original base class
+        :class:`openquake.hazardlib.gsim.zhao_2006.ZhaoEtAl2006Asc`
+    by introducing a distance filter for the near field, as implemented
+    by SGS for the national PSHA model for Saudi Arabia.
+    """
+
+    def get_mean_and_stddevs(self, sites, rup, dists, imt, stddev_types):
+        """
+        Using a minimum distance of 5km for the calculation.
+        """
+
+        dists.rrup = np.array([d if d > 5. else 5. for d in dists.rrup])
+
+        base = super(ZhaoEtAl2006AscSGS, self)
+        mean, stddevs = base.get_mean_and_stddevs(sites, rup, dists,
+                                                  imt, stddev_types)
+
+        return mean, stddevs
