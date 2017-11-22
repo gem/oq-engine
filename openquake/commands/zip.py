aa52601ab7c775d872822dd25eedfc3b67693418
Michele Simionato <michele.simionato@gmail.com>
2017-11-22 09:51:35 +0100

    Small fix [skip CI]

--- openquake/commands/zip.py
+++ openquake/commands/zip.py
@@ -41,7 +41,8 @@ def zip(job_ini, archive_zip):
     for key in oq.inputs:
         fname = oq.inputs[key]
         if isinstance(fname, list):
-            files.extend(os.path.normpath(fname))
+            for f in fname:
+                files.append(os.path.normpath(f))
         else:
             files.append(os.path.normpath(fname))
     general.zipfiles(files, archive_zip, log=logging.info)
