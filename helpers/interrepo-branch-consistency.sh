235d2b5bf4b1178aa94da133534b748189f1a6e6
Matteo Nastasi <nastasi@openquake.org>
2017-11-30 09:36:06 +0100

    inter-repository consistency check: loop on sibling repositories
    Former-commit-id: 6d6d2fbaf690e53a8b837d85303799416c4034c6 [formerly 6d6d2fbaf690e53a8b837d85303799416c4034c6 [formerly d6e12987b5a5ff1f2d8b1e7459cd5e3680ae5220]]
    Former-commit-id: fc265f93307cb49779a842737fca335c0fc47ef6
    Former-commit-id: 0999749f1899545a7d7223dfc7c69715011b3f57

--- helpers/interrepo-branch-consistency.sh
+++ helpers/interrepo-branch-consistency.sh
@@ -11,3 +11,16 @@ if [ ! -d ../oq-platform-standalone ]; then
     cd -
 fi
 
+export PYTHONPATH="../oq-platform-standalone"
+si_apps="openquakeplatform_standalone $(python -c "from openquakeplatform.settings import STANDALONE_APPS ; print(' '.join(STANDALONE_APPS))")"
+si_repos="$(echo "$si_apps" | sed 's/openquakeplatform/oq-platform/g;s/_/-/g')"
+for repo in $si_repos; do
+    if [ ! -d "../${repo}" ]; then
+        cd ..
+        read -p "'../${repo}' repository not exists, clone it ? " yn
+        git clone "git@github.com:gem/${repo}.git"
+        cd -
+    fi
+    echo "${repo}"
+done
+echo
