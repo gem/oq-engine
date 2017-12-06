06710fc7641e59d8a03214c780b1759827e729e3
Michele Simionato <michele.simionato@gmail.com>
2017-12-06 14:47:33 +0100

    Removed .gsims weight
    Former-commit-id: 3b26cc38546f7c0610405ad1866a7ac8b2f4bad0 [formerly a03fe2e3cde24b90d1f600ead60d4e64dec81ca6]
    Former-commit-id: 74643c55360e6068aa69066c710de25f78778e13

--- openquake/commonlib/source.py
+++ openquake/commonlib/source.py
@@ -36,7 +36,7 @@ from openquake.commonlib import logictree
 
 
 MINWEIGHT = source.MINWEIGHT
-MAXWEIGHT = 1E7  # heuristic, set by M. Simionato
+MAXWEIGHT = 2E4  # heuristic, set by M. Simionato
 MAX_INT = 2 ** 31 - 1
 TWO16 = 2 ** 16
 U16 = numpy.uint16
