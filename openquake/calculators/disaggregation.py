682f97da7545d3a1df94c20dd4b594ad0f73b414
Michele Simionato <michele.simionato@gmail.com>
2018-01-08 09:29:05 +0100

    Fixed disaggregation [skip hazardlib][demos]

--- openquake/calculators/disaggregation.py
+++ openquake/calculators/disaggregation.py
@@ -140,6 +140,7 @@ producing too small PoEs.'''
         dic = {}
         imtls = self.oqparam.imtls
         pgetter = calc.PmapGetter(self.datastore, sids=numpy.array([sid]))
+        pgetter.init()
         for rlz in self.rlzs_assoc.realizations:
             try:
                 pmap = pgetter.get(rlz.ordinal)
