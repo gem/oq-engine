a70de8275af4018ff3613139615854d27066d806
Michele Simionato <michele.simionato@gmail.com>
2018-01-11 09:30:37 +0100

    Restore realizations output in the database [skip hazardlib][demos]
    Former-commit-id: 97ff0f7fc0714c2cc52d4d181d7f6cbd9d67eed7

--- openquake/engine/engine.py
+++ openquake/engine/engine.py
@@ -110,10 +110,8 @@ def expose_outputs(dstore):
     calcmode = oq.calculation_mode
     dskeys = set(dstore) & exportable  # exportable datastore keys
     dskeys.add('fullreport')
-    try:
-        rlzs = dstore['csm_info'].rlzs
-    except KeyError:
-        rlzs = []
+    rlzs = dstore['csm_info'].rlzs
+    dskeys.add('realizations')
     # expose gmf_data only if < 10 MB
     if oq.ground_motion_fields and calcmode == 'event_based':
         nbytes = dstore['gmf_data'].attrs['nbytes']
