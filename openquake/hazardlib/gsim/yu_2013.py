9051d60ec803009a982e9cb46bcc8697bb6ba798
Marco Pagani <marco.pagani@globalquakemodel.org>
2018-02-07 09:44:15 +0100

    Adding tests for standard deviation
    Former-commit-id: d048f66f57cde14ee07a7aa91e7e231d578ac7b4

--- openquake/hazardlib/gsim/yu_2013.py
+++ openquake/hazardlib/gsim/yu_2013.py
@@ -17,7 +17,9 @@
 # along with OpenQuake. If not, see <http://www.gnu.org/licenses/>.
 
 """
-Module exports :class:`yu2013`.
+Module exports :class:`YuEtAl2013`, :class:`YuEtAl2013Tibet`,
+:class:`YuEtAl2013Eastern`, :class:`YuEtAl2013Stable`
+
 """
 from __future__ import division
 from __future__ import print_function
@@ -30,34 +32,34 @@ from scipy.optimize import minimize
 from openquake.hazardlib.gsim.base import GMPE, CoeffsTable
 from openquake.hazardlib import const
 from openquake.hazardlib.imt import PGA, PGV, SA
-from openquake.hazardlib.geo import geodetic
 
 
-def rbf(ra, C, mag):
-
+def rbf(ra, coeff, mag):
+    """
+    Set the coefficients depending on the magnitude of the modelled rupture
+    """
     if mag > 6.5:
-        a1ca = C['ua']
-        a1cb = C['ub']
-        a1cc = C['uc']
-        a1cd = C['ud']
-        a1ce = C['ue']
-        a2ca = C['ia']
-        a2cb = C['ib']
-        a2cc = C['ic']
-        a2cd = C['id']
-        a2ce = C['ie']
+        a1ca = coeff['ua']
+        a1cb = coeff['ub']
+        a1cc = coeff['uc']
+        a1cd = coeff['ud']
+        a1ce = coeff['ue']
+        a2ca = coeff['ia']
+        a2cb = coeff['ib']
+        a2cc = coeff['ic']
+        a2cd = coeff['id']
+        a2ce = coeff['ie']
     else:
-        a1ca = C['a']
-        a1cb = C['b']
-        a1cc = C['c']
-        a1cd = C['d']
-        a1ce = C['e']
-        a2ca = C['ma']
-        a2cb = C['mb']
-        a2cc = C['mc']
-        a2cd = C['md']
-        a2ce = C['me']
-
+        a1ca = coeff['a']
+        a1cb = coeff['b']
+        a1cc = coeff['c']
+        a1cd = coeff['d']
+        a1ce = coeff['e']
+        a2ca = coeff['ma']
+        a2cb = coeff['mb']
+        a2cc = coeff['mc']
+        a2cd = coeff['md']
+        a2ce = coeff['me']
     term1 = a1ca + a1cb * mag + a1cc * np.log(ra + a1cd*np.exp(a1ce*mag))
     term2 = a2ca + a2cb * mag
     term3 = a2cd*np.exp(a2ce*mag)
@@ -66,14 +68,13 @@ def rbf(ra, C, mag):
 
 def fnc(ra, *args):
     """
+    Find the rupture to site distance
     """
     repi = args[0]
     theta = args[1]
     mag = args[2]
-    imt = args[3]
-    C = args[4]
-
-    rb = rbf(ra, C, mag)
+    coeff = args[3]
+    rb = rbf(ra, coeff, mag)
     t1 = ra**2 * (np.sin(np.radians(theta)))**2
     t2 = rb**2 * (np.cos(np.radians(theta)))**2
     xx = ra * rb / (t1+t2)**0.5
@@ -131,7 +132,8 @@ class YuEtAl2013(GMPE):
             # Check that the requested standard deviation type is available
             assert all(stddev_type in self.DEFINED_FOR_STANDARD_DEVIATION_TYPES
                        for stddev_type in stddev_types)
-            # Get the coefficients
+            #
+            # Set coefficients
             C = self.COEFFS[imt]
             if rup.mag > 6.5:
                 a1ca = C['ua']
@@ -155,36 +157,28 @@ class YuEtAl2013(GMPE):
                 a2cc = C['mc']
                 a2cd = C['md']
                 a2ce = C['me']
-
+            #
             # Compute the mean value (i.e. natural logarithm of ground motion)
-            mag = rup.mag                     
+            mag = rup.mag
             epi = dists.repi
             theta = dists.azimuth
-            #imt = PGA()
             #
-            #
+            # Get correction coefficients
             ras = []
             for epi, theta in zip(dists.repi, dists.azimuth):
-                res = minimize(fnc, epi, args=(epi, theta, mag, imt, C),
+                res = minimize(fnc, epi, args=(epi, theta, mag, C),
                                method='Nelder-Mead', tol=0.1)
                 ras.append(res.x[0])
-            #imt = PGA()
-            #
-            #
             ras = np.array(ras)
             rbs = rbf(ras, C, mag)
-
-            # Convert acceleration from gals into fraction of g
-            #idx = np.nonzero(ras < 15)
-            #mean = np.zeros_like(rbs)
-            #if len(idx):
-            #    mean[idx] = a1ca + a1cb * mag + a1cc * np.log10(15 + a1cd * np.exp(a1ce * mag))
-            #idx = np.nonzero(ras >= 15)
-            #if len(idx):
-            #    mean[idx] = a1ca + a1cb * mag + a1cc * np.log10(ras[idx] + a1cd * np.exp(a1ce * mag))
-
-            mean1 = a1ca + a1cb * mag + a1cc * np.log((ras**2+225)**0.5+ a1cd * np.exp(a1ce * mag))
-            mean2 = a2ca + a2cb * mag + a2cc * np.log((rbs**2+225)**0.5+ a2cd * np.exp(a2ce * mag))
+            #
+            # Compute values of ground motion for the two cases
+            mean1 = (a1ca + a1cb * mag + a1cc *
+                     np.log((ras**2+225)**0.5 + a1cd * np.exp(a1ce * mag)))
+            mean2 = (a2ca + a2cb * mag + a2cc *
+                     np.log((rbs**2+225)**0.5 + a2cd * np.exp(a2ce * mag)))
+            #
+            # Get distances
             x = (mean1 * np.sin(np.radians(dists.azimuth)))**2
             y = (mean2 * np.cos(np.radians(dists.azimuth)))**2
             mean = mean1 * mean2 / np.sqrt(x+y)
@@ -193,6 +187,7 @@ class YuEtAl2013(GMPE):
             #
             # Get the standard deviation
             stddevs = self._compute_std(C, stddev_types, len(dists.repi))
+            #
             # Return results
             return np.log(mean), stddevs
 
@@ -204,8 +199,6 @@ class YuEtAl2013(GMPE):
 IMT a b c d e ua ub uc ud ue ma mb mc md me ia ib ic id ie sigma
 PGA 4.1193 1.656 -2.389 1.772 0.424 7.8269 1.0856 -2.389 1.772 0.424 2.2609 1.6399 -2.118 0.825 0.465 6.003 1.0649 -2.118 0.825 0.465 0.5428
 PGV -1.2581 1.932 -2.181 1.772 0.424 3.013 1.2742 -2.181 1.772 0.424 -3.1073 1.9389 -1.945 0.825 0.465 1.3087 1.2627 -1.945 0.825 0.465 0.6233
-
-
         """)
 
 class YuEtAl2013Tibet(YuEtAl2013):
