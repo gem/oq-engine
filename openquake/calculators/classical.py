03571c7cc7e66453366f3701fcb3e8b5fd583c75
Michele Simionato <michele.simionato@gmail.com>
2018-02-10 08:43:53 +0100

    Fixed the maxweight logic [skip hazardlib] [demos]
    Former-commit-id: 72fb96e1d2698a018bb41f30aed4b36968165b9a

--- openquake/calculators/classical.py
+++ openquake/calculators/classical.py
@@ -164,6 +164,7 @@ class PSHACalculator(base.HazardCalculator):
         else:
             tiles = [self.sitecol]
         param = dict(truncation_level=oq.truncation_level, imtls=oq.imtls)
+        maxweight = None
         for tile_i, tile in enumerate(tiles, 1):
             num_tasks = 0
             num_sources = 0
@@ -171,7 +172,9 @@ class PSHACalculator(base.HazardCalculator):
                 logging.info('Prefiltering tile %d of %d', tile_i, len(tiles))
                 src_filter = SourceFilter(tile, oq.maximum_distance)
                 csm = self.csm.filter(src_filter)
-            if tile_i == 1:  # set it only on the first tile
+                if csm.weight == 0:  # the tile was completely filtered out
+                    continue
+            if maxweight is None:  # set maxweight only once
                 maxweight = csm.get_maxweight(tasks_per_tile)
                 logging.info('Using maxweight=%d', maxweight)
             if csm.has_dupl_sources and not opt:
