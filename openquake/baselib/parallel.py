a60e8996e6e8dc9bac083dbe9f2a6e62a01bb53f
Michele Simionato <michele.simionato@gmail.com>
2018-02-12 11:11:06 +0100

    Fixed celery [demos]
    Former-commit-id: 01db2ddd4ba3d882a76064c20bd492058cd05219

--- openquake/baselib/parallel.py
+++ openquake/baselib/parallel.py
@@ -179,6 +179,7 @@ if OQ_DISTRIBUTE == 'celery':
     from celery.task import task
     from openquake.engine.celeryconfig import BROKER_URL, CELERY_RESULT_BACKEND
     app = Celery('openquake', backend=CELERY_RESULT_BACKEND, broker=BROKER_URL)
+    safetask = task(safely_call, queue='celery')  # has to be global
 
 
 def oq_distribute(task=None):
@@ -563,7 +564,6 @@ class Starmap(object):
             yield res
 
     def _iter_celery(self):
-        safetask = task(safely_call, queue='celery')
         results = []
         for piks in self._genargs():
             results.append(safetask.delay(self.task_func, piks))
