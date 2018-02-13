6f2a54f4c5c66833b1be1962ad32cdced466a3a8
Matteo Nastasi <nastasi@openquake.org>
2018-02-13 20:28:36 +0100

    devtest: run dbserver with oq-python3.5
    Former-commit-id: 1f1f31f64b9df9450c0544d67e1f708d05fed40f

--- packager.sh
+++ packager.sh
@@ -381,9 +381,9 @@ _devtest_innervm_run () {
         fi
 
         ssh $lxc_ip "set -e
-                 export PYTHONPATH=\"\$PWD/oq-engine:$OPT_LIBS_PATH\"
+                 export PYTHONPATH=\"\$PWD/oq-engine\"
                  echo 'Starting DbServer. Log is saved to /tmp/dbserver.log'
-                 cd oq-engine; nohup bin/oq dbserver start &>/tmp/dbserver.log </dev/null &"
+                 cd oq-engine; nohup /opt/openquake/bin/python3 bin/oq dbserver start &>/tmp/dbserver.log </dev/null &"
 
         ssh $lxc_ip "export GEM_SET_DEBUG=$GEM_SET_DEBUG
                  set -e
