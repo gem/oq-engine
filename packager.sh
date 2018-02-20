70ff0df34dad714d1687b50db877b08cce9972c5
Matteo Nastasi <nastasi@openquake.org>
2018-02-20 15:54:52 +0100

    udpdate oq-libs dependency to 1.6.0 and revert devel oq-libs dependency type to 'deb' instead 'cust' type

--- packager.sh
+++ packager.sh
@@ -41,7 +41,7 @@ fi
 set -e
 GEM_GIT_REPO="git://github.com/gem"
 GEM_GIT_PACKAGE="oq-engine"
-GEM_DEPENDS="oq-libs|cust oq-libs-extra|sub"
+GEM_DEPENDS="oq-libs|deb oq-libs-extra|sub"
 GEM_DEB_PACKAGE="python3-${GEM_GIT_PACKAGE}"
 GEM_DEB_SERIE="master"
 if [ -z "$GEM_DEB_REPO" ]; then
