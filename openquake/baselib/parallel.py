6e344194eda747d0476cbc285b47cf5c0611cdd8
Michele Simionato <michele.simionato@gmail.com>
2018-02-24 07:07:48 +0100

    Cleanup [demos]

--- openquake/baselib/parallel.py
+++ openquake/baselib/parallel.py
@@ -625,14 +625,12 @@ class Starmap(object):
 
     def _iter_sequential(self):
         self.progress('Executing "%s" in process', self.name)
-        allargs = []
-        for task_no, args in enumerate(self.task_args):
+        allargs = list(self.task_args)
+        yield len(allargs)
+        for task_no, args in enumerate(allargs):
             if isinstance(args[-1], Monitor):  # not the case for wakeup
                 args[-1].task_no = task_no
-            allargs.append(args)
-        yield len(allargs)
-        for args in allargs:
-            args[-1].backurl = None  # no zmq in sequential
+                args[-1].backurl = None
             yield safely_call(self.task_func, args)
 
     def _iter_processes(self):
