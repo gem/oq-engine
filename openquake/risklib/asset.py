84694585e4bfaf0d70308bbf1eb2c00bffa3dfa8
Michele Simionato <michele.simionato@gmail.com>
2018-02-26 13:43:02 +0100

    More work

--- openquake/risklib/asset.py
+++ openquake/risklib/asset.py
@@ -355,6 +355,7 @@ class AssetCollection(object):
         self.array = self.build_asset_array(
             assets_by_site, tagcol.tagnames, time_event)
         self.asset_refs = [asset_refs[rec['idx']] for rec in self.array]
+        import pdb; pdb.set_trace()
         fields = self.array.dtype.names
         self.loss_types = [f[6:] for f in fields if f.startswith('value-')]
         if 'occupants' in fields:
@@ -366,6 +367,9 @@ class AssetCollection(object):
 
     @property
     def tagnames(self):
+        """
+        :returns: the tagnames
+        """
         return self.tagcol.tagnames
 
     def get_aids_by_tag(self):
