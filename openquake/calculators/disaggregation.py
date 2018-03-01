e1be2ac948d429bbc64d091f99fc9a1b10059f66
Michele Simionato <michele.simionato@gmail.com>
2018-03-01 10:51:29 +0100

    Small fix [skip hazardlib]
    Former-commit-id: a703560087daa2c9e68fff137c418b98ad1f756c [formerly a703560087daa2c9e68fff137c418b98ad1f756c [formerly 90d4783c6c33c43e8a72382e82f446553e8d1d42]]
    Former-commit-id: a1b91857d56b66f4bdb55752d9f46f4b377b7502
    Former-commit-id: 817a72e835ab7fe34541cbfb296ec4e106b5d3ce

--- openquake/calculators/disaggregation.py
+++ openquake/calculators/disaggregation.py
@@ -118,7 +118,6 @@ producing too small PoEs.'''
             # the hazard curves, hence the need to run a PSHACalculator here
             cl = classical.PSHACalculator(oq, self.monitor('classical'),
                                           calc_id=self.datastore.calc_id)
-            cl.split_time = self.split_time
             cl.grp_by_src = oq.disagg_by_src
             cl.run()
             self.csm = cl.csm
