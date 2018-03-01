ac51bc91d0ba96ddbf4b906e2259f0aaead75608
klunk386 <poggi.valerio@gmail.com>
2018-03-01 10:43:53 +0100

    More fixes

--- openquake/hmtk/seismicity/catalogue.py
+++ openquake/hmtk/seismicity/catalogue.py
@@ -125,28 +125,27 @@ class Catalogue(object):
             Optional list of attribute keys to be exported
         """
 
-        outfile = open(output_file, 'wt')
-        writer = csv.DictWriter(outfile, fieldnames=key_list)
-
-        writer.writeheader()
-        for i in range(0, self.get_number_events()):
-            row_dict = {}
-            for key in key_list:
-                if len(self.data[key]) > 0:
-                    data = self.data[key][i]
-                    if key in self.INT_ATTRIBUTE_LIST:
-                        if np.isnan(data):
-                            data = ''
-                        else:
-                            data = int(data)
-                    if key in self.FLOAT_ATTRIBUTE_LIST:
-                        if np.isnan(data):
-                            data = ''
-                        else:
-                            data = float(data)
-                row_dict[key] = data
-            writer.writerow(row_dict)
-        outfile.close()
+        with open(output_file, 'wt') as of:
+            writer = csv.DictWriter(of, fieldnames=key_list)
+            writer.writeheader()
+            for i in range(self.get_number_events()):
+                row_dict = {}
+                for key in key_list:
+                    if len(self.data[key]) > 0:
+                        data = self.data[key][i]
+                        if key in self.INT_ATTRIBUTE_LIST:
+                            if np.isnan(data):
+                                data = ''
+                            else:
+                                data = int(data)
+                        if key in self.FLOAT_ATTRIBUTE_LIST:
+                            if np.isnan(data):
+                                data = ''
+                            else:
+                                data = float(data)
+                    row_dict[key] = data
+                writer.writerow(row_dict)
+            of.close()
 
     def load_to_array(self, keys):
         """
