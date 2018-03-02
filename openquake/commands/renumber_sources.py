32134bdd61c04f23e00a65da63c6aedca57d9abd
Michele Simionato <michele.simionato@gmail.com>
2018-03-02 03:21:01 +0100

    Cleanup [skip hazardlib]
    Former-commit-id: 90077e930183b7ae1893ad2adbfaaf9aee6552e0 [formerly 90077e930183b7ae1893ad2adbfaaf9aee6552e0 [formerly 12f96327ad0ad35a00a05a7d40791019cdb722c6]]
    Former-commit-id: fc31cf6fd1f43a4d7879988ad06fa1183555ef93
    Former-commit-id: 5fbb8f05d215b04fae8357d7cd4cc0254aa49648

--- openquake/commands/renumber_sources.py
+++ openquake/commands/renumber_sources.py
@@ -15,6 +15,7 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake. If not, see <http://www.gnu.org/licenses/>.
+import shutil
 import logging
 from openquake.baselib import sap
 from openquake.commonlib import readinput
@@ -28,21 +29,25 @@ class ObsoleteFormat(Exception):
 @sap.Script
 def renumber_sources(smlt_file):
     """
-    Renumber all the sources in all XML files.
+    Renumber the sources belowing to the same source model, even if split
+    in multiple files, to avoid duplicated source IDs.
     """
     logging.basicConfig(level=logging.INFO)
-    number = 1
-    for path in readinput.get_paths(smlt_file):
-        logging.info('Renumbering %s', path)
-        root = nrml.read(path)
-        if root['xmlns'] == 'http://openquake.org/xmlns/nrml/0.4':
-            raise ObsoleteFormat('Please use oq upgrade_nrml .')
-        [smodel] = root
-        for sgroup in smodel:
-            for src in sgroup:
-                src['id'] = str(number)
-                number += 1
-        with open(path, 'wb') as f:
-            nrml.write(root, f)
+    for paths in readinput.gen_sm_paths(smlt_file):
+        number = 1
+        for path in paths:
+            # make a backup of the original file
+            shutil.copy(path, path + '~')
+            logging.info('Renumbering %s', path)
+            root = nrml.read(path)
+            if root['xmlns'] == 'http://openquake.org/xmlns/nrml/0.4':
+                raise ObsoleteFormat('Please use oq upgrade_nrml .')
+            [smodel] = root
+            for sgroup in smodel:
+                for src in sgroup:
+                    src['id'] = str(number)
+                    number += 1
+            with open(path, 'wb') as f:
+                nrml.write(root, f)
 
 renumber_sources.arg('smlt_file', 'source model logic tree file')
