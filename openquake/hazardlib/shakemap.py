12a3cc07845bf466833793186ac13cd3f7037d7f
Michele Simionato <michele.simionato@gmail.com>
2018-03-13 09:07:20 +0100

    More cleanup [skip CI]
    Former-commit-id: 5701e569e7a6c3b58dc35b38ab2ad67242f15d0b [formerly 4938dd25a27b03dbfbae4cb130f89b89a16f61ae]
    Former-commit-id: 8b9ed66eabf57074e0bf2e4f9f76832b9ffb6a16

--- openquake/hazardlib/shakemap.py
+++ openquake/hazardlib/shakemap.py
@@ -49,6 +49,8 @@ Here is an example of the format
 """
 from urllib.request import urlopen
 import numpy
+from scipy.stats import truncnorm
+from scipy import interpolate
 from openquake.baselib.node import node_from_xml
 from openquake.hazardlib import geo
 
@@ -62,7 +64,7 @@ SHAKEMAP_FIELDS = set(
 
 def get_shakemap_array(grid_file):
     """
-    :param grid_file: an shakemap file
+    :param grid_file: a shakemap file
     :returns: array with fields lon, lat, val, std
     """
     grid_node = node_from_xml(grid_file)
@@ -141,20 +143,19 @@ def to_shaking(shakemap, imts):
     return arr
 
 
-def calculate_spatial_correlation_matrix(
-        dmatrix, imts, correlationType):
+def calculate_spatial_correlation_matrix(dmatrix, imts, correl):
     """
     :returns: array of shape (M, N, N)
     """
     n = len(dmatrix)
     out = []
     for imt in imts:
-        if correlationType == 'no correlation':
+        if correl == 'no correlation':
             corr = numpy.zeros((n, n))
-        if correlationType == 'full correlation':
+        if correl == 'full correlation':
             corr = numpy.eye(n)
-        elif correlationType == 'spatial':
-            b = calculate_spatial_length_scale(imt, 'Vs30clustered')
+        elif correl == 'spatial':
+            b = calculate_spatial_length_scale(imt, 'vs30clustered')
             corr = numpy.eye(n) + numpy.exp(-3 * dmatrix / b)
         out.append(corr)
     return numpy.array(corr)
@@ -162,40 +163,39 @@ def calculate_spatial_correlation_matrix(
 
 def calculate_spatial_covariance_matrices(groundShaking, spatialCorrMatrices):
     # this depends on sPGA, sSa03, sSa10, sSa30
-    noIMT = len(spatialCorrMatrices)
-    n = len(spatialCorrMatrices[0])
-    spatialCovMatrices = []
-    for i in range(noIMT):
-        tempCovMatrix = numpy.zeros((n, n))
-        for j in range(n):
-            for k in range(n):
-                tempCovMatrix[j, k] = spatialCorrMatrices[i, j, k] * \
-                    groundShaking[j, 2 + noIMT + i] * \
-                    groundShaking[k, 2 + noIMT + i]
-        spatialCovMatrices.append(tempCovMatrix)
-
-    return numpy.array(spatialCovMatrices)
+    M = len(spatialCorrMatrices)
+    N = len(spatialCorrMatrices[0])
+    matrices = []
+    for i in range(M):
+        covmatrix = numpy.zeros((N, N))
+        for j in range(N):
+            for k in range(N):
+                covmatrix[j, k] = (spatialCorrMatrices[i, j, k] *
+                                   groundShaking[j, 2 + M + i] *
+                                   groundShaking[k, 2 + M + i])
+        matrices.append(covmatrix)
+    return numpy.array(matrices)
 
 
 def generate_random_fields_ground_motion(
-        IMTs, groundShaking, spatialCovMatrices, crossCorrMatrix,
-        siteEffects, noSigmas, noGMFs):
+        imts, groundShaking, spatialCovMatrices, crossCorrMatrix,
+        site_effects, trunclevel, noGMFs):
     # groundShaking has shape (N, 11) where
     # 11 = lon lat mPGA mSa03 mSa10 mSa30 sPGA sSa03 sSa10 sSa30 Vs30
     n = spatialCovMatrices.shape[1]
-    noIMT = crossCorrMatrix.shape[0]
+    M = crossCorrMatrix.shape[0]
     L = []
     LLT = []
     Z = []
 
-    for i in range(noIMT):
+    for i in range(M):
         L.append(numpy.linalg.cholesky(spatialCovMatrices[i]))
 
     L = numpy.array(L)
 
-    for i in range(noIMT):
+    for i in range(M):
         LLTrow = []
-        for j in range(noIMT):
+        for j in range(M):
             LLTrow.append(
                 numpy.dot(L[i], numpy.transpose(L[j])) * crossCorrMatrix[i, j])
         for irow in range(len(LLTrow[0])):
@@ -208,49 +208,50 @@ def generate_random_fields_ground_motion(
     LLT = numpy.array(LLT)
 
     mu = []
-    for i in range(noIMT):
+    for i in range(M):
         for j in range(n):
             mu.append(numpy.ones(noGMFs) * groundShaking[j][i + 2])
     mu = numpy.array(mu)
     L = (numpy.linalg.cholesky(LLT))
-    Z = truncnorm.rvs(-noSigmas, noSigmas, loc=0, scale=1,
-                      size=(n * noIMT, noGMFs))
+    Z = truncnorm.rvs(-trunclevel, trunclevel, loc=0, scale=1,
+                      size=(n * M, noGMFs))
 
     gmfs = numpy.exp(numpy.dot(L, Z) + mu)
 
-    if siteEffects:  # use vs30 which is the last field
-        gmfs = amplifyGMFs(IMTs, groundShaking[:, -1], gmfs) * 0.8
+    if site_effects:  # use vs30 which is the last field
+        gmfs = amplify_gmfs(imts, groundShaking[:, -1], gmfs) * 0.8
 
     return gmfs
 
 
-def amplifyGMFs(IMTs, Vs30s, gmfs):
-    n = len(Vs30s)
-
+def amplify_gmfs(imts, vs30s, gmfs):
+    """
+    Amplify the ground shaking depending on the vs30s
+    """
+    n = len(vs30s)
     for i in range(4):
-        if IMTs[i] == 'PGA':
+        if imts[i] == 'PGA':
             T = 0.0
-        elif IMTs[0:2] == 'SA':
-            IMT = IMTs[i]
+        elif imts[0:2] == 'SA':
+            IMT = imts[i]
             T = float(IMT.replace("SA(", "").replace(")", ""))
-
         for iloc in range(n):
             gmfs[i * n + iloc] = amplify_ground_shaking(
-                T, Vs30s[iloc], gmfs[i * n + iloc])
+                T, vs30s[iloc], gmfs[i * n + iloc])
 
     return gmfs
 
 
-def calculate_spatial_length_scale(IMT, Vs30Case):
-    if IMT == 'PGA':
+def calculate_spatial_length_scale(imt, vs30case):
+    if imt == 'PGA':
         T = 0.0
-    elif IMT[0:2] == 'SA':
-        T = float(IMT.replace("SA(", "").replace(")", ""))
+    elif imt[0:2] == 'SA':
+        T = float(imt.replace("SA(", "").replace(")", ""))
 
     if T < 1:
-        if Vs30Case != 'Vs30clustered':
+        if vs30case != 'vs30clustered':
             b = 8.5 + 17.2 * T
-        elif Vs30Case == 'Vs30clustered':
+        elif vs30case == 'vs30clustered':
             b = 40.7 - 15.0 * T
     elif T >= 1:
         b = 22.0 + 3.7 * T
@@ -258,45 +259,31 @@ def calculate_spatial_length_scale(IMT, Vs30Case):
     return b
 
 
-def calculate_distance_2points(point1, point2):
-    lon1 = point1[0]
-    lon2 = point2[0]
-    lat1 = point1[1]
-    lat2 = point2[1]
-
-    lon1, lat1, lon2, lat2 = map(math.radians, [lon1, lat1, lon2, lat2])
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
-    a = math.sin(dlat / 2)**2 + math.cos(lat1) * \
-        math.cos(lat2) * math.sin(dlon / 2)**2
-
-    distance = 6367 * 2 * math.asin(math.sqrt(a))
-    return distance
-
-
-def amplify_ground_shaking(T, Vs30, IMLs):
-    ampFactorsShort = [(760 / Vs30)**0.35,
-                       (760 / Vs30)**0.35,
-                       (760 / Vs30)**0.25,
-                       (760 / Vs30)**0.10,
-                       (760 / Vs30)**-0.05,
-                       (760 / Vs30)**-0.05]
-    ampFactorsMid = [(760 / Vs30)**0.65,
-                     (760 / Vs30)**0.65,
-                     (760 / Vs30)**0.60,
-                     (760 / Vs30)**0.53,
-                     (760 / Vs30)**0.45,
-                     (760 / Vs30)**0.45]
-
+def amplify_ground_shaking(T, vs30, imls):
+    """
+    :param T: period
+    :param vs30: velocity
+    :param imls: intensity measure levels
+    """
     if T <= 0.3:
         interpolator = interpolate.interp1d(
             [-1, 0.1, 0.2, 0.3, 0.4, 100],
-            ampFactorsShort, kind='linear')
-    if T > 0.3:
+            [(760 / vs30)**0.35,
+             (760 / vs30)**0.35,
+             (760 / vs30)**0.25,
+             (760 / vs30)**0.10,
+             (760 / vs30)**-0.05,
+             (760 / vs30)**-0.05], kind='linear')
+    else:
         interpolator = interpolate.interp1d(
-            [-1, 0.1, 0.2, 0.3, 0.4, 100], ampFactorsMid, kind='linear')
-
-    return interpolator(IMLs) * IMLs
+            [-1, 0.1, 0.2, 0.3, 0.4, 100],
+            [(760 / vs30)**0.65,
+             (760 / vs30)**0.65,
+             (760 / vs30)**0.60,
+             (760 / vs30)**0.53,
+             (760 / vs30)**0.45,
+             (760 / vs30)**0.45], kind='linear')
+    return interpolator(imls) * imls
 
 """
 here is an example for Tanzania:
