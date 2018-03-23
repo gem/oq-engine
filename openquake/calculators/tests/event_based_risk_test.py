40c94a4af212c465a40c5cb9b12274b21c508cb5
Michele Simionato <michele.simionato@gmail.com>
2018-03-23 05:23:49 +0100

    Automatic gridding of the exposure [skip hazardlib]

--- openquake/calculators/tests/event_based_risk_test.py
+++ openquake/calculators/tests/event_based_risk_test.py
@@ -264,6 +264,13 @@ class EventBasedRiskTestCase(CalculatorTestCase):
             'expected/portfolio_loss.txt', fname, delta=1E-5)
         os.remove(fname)
 
+        # this is a case with exposure and region_grid_spacing
+        self.run_calc(case_miriam.__file__, 'job2.ini', exports='csv')
+        sitecol = self.calc.datastore['sitecol']
+        assetcol = self.calc.datastore['assetcol']
+        self.assertEqual(len(sitecol), 27)
+        self.assertEqual(len(assetcol), 548)
+
     @attr('qa', 'risk', 'event_based_risk')
     def test_case_7a(self):
         # case with  <insuranceLimit isAbsolute="false"/>
