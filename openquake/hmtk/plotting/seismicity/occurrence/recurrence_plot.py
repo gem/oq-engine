bb4aadfdea1de04717f6d7c07753014cb88a68f0
Nick Ackerley <nicholas.ackerley@canada.ca>
2018-03-27 19:18:30 +0000

    Fixed a bug introduced by recent changes to openquake.hmtk.plotting.seismicity.catalogue_plots._save_image(). Added support for directing the plot into an already-existing axes. Cleaned out some lint. Removed hard-coding of font sizes.
    Former-commit-id: e8459a202bc3d15806f44f069464c711c2017f25 [formerly e8459a202bc3d15806f44f069464c711c2017f25 [formerly 67e3dec42e342abe1c865141199616b86eb675f4]]
    Former-commit-id: fe8cadbeafe99360b1029aacc66b25845845f2c1
    Former-commit-id: 4331ace3f863dfe2d1bf7fbd521c0bb1408ad05d

--- openquake/hmtk/plotting/seismicity/occurrence/recurrence_plot.py
+++ openquake/hmtk/plotting/seismicity/occurrence/recurrence_plot.py
@@ -6,8 +6,7 @@ Simple plots for the recurrence model
 
 import numpy as np
 import matplotlib.pyplot as plt
-from openquake.hmtk.plotting.seismicity.catalogue_plots import \
-    (get_completeness_adjusted_table, _save_image, DEFAULT_SIZE)
+from openquake.hmtk.plotting.seismicity.catalogue_plots import _save_image
 from openquake.hmtk.seismicity.occurrence.utils import get_completeness_counts
 from openquake.hazardlib.mfd.truncated_gr import TruncatedGRMFD
 from openquake.hazardlib.mfd.evenly_discretized import EvenlyDiscretizedMFD
@@ -17,7 +16,7 @@ from openquake.hazardlib.mfd.youngs_coppersmith_1985 import\
 
 def _check_recurrence_model_type(input_model):
     """
-
+    Ensure model is a known type
     """
     valid_model = False
     for model in [TruncatedGRMFD, EvenlyDiscretizedMFD,
@@ -60,18 +59,19 @@ def _check_completeness_table(completeness, catalogue):
         raise ValueError('Completeness representation not recognised')
 
 
-def plot_recurrence_model(input_model, catalogue, completeness, dmag,
-                          figure_size=DEFAULT_SIZE, filename=None,
-                          filetype='png',  dpi=300, ax=None):
+def plot_recurrence_model(
+        input_model, catalogue, completeness, dmag, filename=None,
+        figure_size=None, filetype='png', dpi=300, ax=None):
     """
     Plot a calculated recurrence model over an observed catalogue, adjusted for
     time-varying completeness
     """
     if figure_size is None:
-        figure_size = (10, 8)
+        figure_size = (8, 6)
     if dmag is None:
         dmag = 0.1
     annual_rates, cumulative_rates = _get_recurrence_model(input_model)
+
     # Get observed annual recurrence
     if not catalogue.end_year:
         catalogue.update_end_year()
@@ -81,31 +81,31 @@ def plot_recurrence_model(input_model, catalogue, completeness, dmag,
     obs_rates = n_obs / t_per
     cum_obs_rates = np.array([np.sum(obs_rates[i:])
                               for i in range(len(obs_rates))])
-    # Create plot
+
     if ax is None:
-        fig, ax = plt.subplots(figsize=DEFAULT_SIZE)
+        fig, ax = plt.subplots(figsize=(8, 6))
     else:
         fig = ax.get_figure()
 
-    plt.figure(figsize=figure_size)
-    plt.semilogy(cent_mag, obs_rates, 'bo')
-    plt.semilogy(annual_rates[:, 0], annual_rates[:, 1], 'b-')
-    plt.semilogy(cent_mag, cum_obs_rates, 'rs')
-    plt.semilogy(annual_rates[:, 0], cumulative_rates, 'r-')
-    plt.grid(which='both')
-    plt.xlabel('Magnitude', fontsize='16')
-    plt.ylabel('Annual Rate', fontsize='16')
-    plt.legend(['Observed Incremental Rate',
-                'Model Incremental Rate',
-                'Observed Cumulative Rate',
-                'Model Cumulative Rate'], fontsize=14)
-    plt.tick_params(labelsize=12)
+    ax.semilogy(cent_mag, obs_rates, 'bo')
+    ax.semilogy(annual_rates[:, 0], annual_rates[:, 1], 'b-')
+    ax.semilogy(cent_mag, cum_obs_rates, 'rs')
+    ax.semilogy(annual_rates[:, 0], cumulative_rates, 'r-')
+    ax.grid(which='both')
+    ax.set_xlabel('Magnitude')
+    ax.set_ylabel('Annual Rate')
+    ax.legend(['Observed Incremental Rate',
+               'Model Incremental Rate',
+               'Observed Cumulative Rate',
+               'Model Cumulative Rate'])
+    ax.tick_params(labelsize=12)
     _save_image(fig, filename, filetype, dpi)
 
 
-def plot_trunc_gr_model(aval, bval, min_mag, max_mag, dmag, catalogue=None,
-                        completeness=None, figure_size=None, filename=None, filetype='png',
-                        dpi=300):
+def plot_trunc_gr_model(
+        aval, bval, min_mag, max_mag, dmag,
+        catalogue=None, completeness=None, filename=None,
+        figure_size=(8, 6), filetype='png', dpi=300, ax=None):
     """
     Plots a Gutenberg-Richter model
     """
@@ -113,19 +113,21 @@ def plot_trunc_gr_model(aval, bval, min_mag, max_mag, dmag, catalogue=None,
     if not catalogue:
         # Plot only the modelled recurrence
         annual_rates, cumulative_rates = _get_recurrence_model(input_model)
-        plt.semilogy(annual_rates[:, 0], annual_rates[:, 1], 'b-')
-        plt.semilogy(annual_rates[:, 0], cumulative_rates, 'r-')
-        plt.xlabel('Magnitude', fontsize='large')
-        plt.ylabel('Annual Rate', fontsize='large')
-        plt.legend(['Incremental Rate', 'Cumulative Rate'])
-        _save_image(filename, filetype, dpi)
+
+        if ax is None:
+            fig, ax = plt.subplots(figsize=figure_size)
+        else:
+            fig = ax.get_figure()
+
+        ax.semilogy(annual_rates[:, 0], annual_rates[:, 1], 'b-')
+        ax.semilogy(annual_rates[:, 0], cumulative_rates, 'r-')
+        ax.xlabel('Magnitude')
+        ax.set_ylabel('Annual Rate')
+        ax.set_legend(['Incremental Rate', 'Cumulative Rate'])
+        _save_image(fig, filename, filetype, dpi)
+
     else:
         completeness = _check_completeness_table(completeness, catalogue)
-        plot_recurrence_model(input_model,
-                              catalogue,
-                              completeness,
-                              input_model.bin_width,
-                              figure_size,
-                              filename,
-                              filetype,
-                              dpi)
+        plot_recurrence_model(
+            input_model, catalogue, completeness, dmag, filename=filename,
+            figure_size=figure_size, filetype=filetype, dpi=dpi, ax=ax)
