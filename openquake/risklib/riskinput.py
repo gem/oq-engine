b9641556d69dd4153c23cd8d4a8d0dd148c2dfda
Michele Simionato <michele.simionato@gmail.com>
2018-03-27 06:33:36 +0200

    Fixed RiskInput.weight [skip hazardlib]

--- openquake/risklib/riskinput.py
+++ openquake/risklib/riskinput.py
@@ -306,8 +306,6 @@ class RiskInput(object):
         self.aids = numpy.array(aids, numpy.uint32)
         self.taxonomies = sorted(taxonomies_set)
         self.by_site = hazard_getter.__class__.__name__ != 'GmfGetter'
-        self.weight = len(self.aids) if self.by_site else len(
-            hazard_getter.ebruptures)
 
     @property
     def imt_taxonomies(self):
