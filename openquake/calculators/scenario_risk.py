7ea3e650f3d37a2d9cd587da17bb319504db3db2
Michele Simionato <michele.simionato@gmail.com>
2018-04-12 10:49:49 +0200

    Added test
    Former-commit-id: 61f220817002647c7987f8afcd09d5b5d0e25c8d [formerly 61f220817002647c7987f8afcd09d5b5d0e25c8d [formerly 65502f3750472bc80256c554b2882ecde0d20850]]
    Former-commit-id: 775c1259fc0af6e71c9d40d027343cb69808b86e
    Former-commit-id: 0ac4118a80cbf442df2472dd1b680cad010a21e2

--- openquake/calculators/scenario_risk.py
+++ openquake/calculators/scenario_risk.py
@@ -95,7 +95,7 @@ class ScenarioRiskCalculator(base.RiskCalculator):
         if 'gmfs' in self.oqparam.inputs:
             self.pre_calculator = None
         base.RiskCalculator.pre_execute(self)
-        if self.oqparam.shakemap_id:
+        if self.oqparam.shakemap_id or 'shakemap' in self.oqparam.inputs:
             self.read_shakemap()
             self.R = 1
         else:
