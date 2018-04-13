1f8fdef76f32a76ef47d68107e7d51338d84c9ac
Michele Simionato <michele.simionato@gmail.com>
2018-04-13 10:04:02 +0200

    Cleanup [demos]

--- openquake/hazardlib/tests/site_test.py
+++ openquake/hazardlib/tests/site_test.py
@@ -37,6 +37,46 @@ class SiteModelParam(object):
         self.reference_backarc = False
 
 
+class SiteTestCase(unittest.TestCase):
+    def _assert_creation(self, error=None, **kwargs):
+        default_kwargs = {
+            'location': Point(10, 20),
+            'vs30': 10,
+            'vs30measured': False,
+            'z1pt0': 20,
+            'z2pt5': 30,
+            'backarc': True
+        }
+        default_kwargs.update(kwargs)
+        kwargs = default_kwargs
+        if error is not None:
+            with self.assertRaises(ValueError) as ar:
+                Site(**kwargs)
+            self.assertEqual(str(ar.exception), error)
+        else:
+            site = Site(**kwargs)
+            for attr in kwargs:
+                self.assertEqual(getattr(site, attr), kwargs[attr])
+
+    def test_wrong_vs30(self):
+        error = 'vs30 must be positive'
+        self._assert_creation(error=error, vs30=0)
+        self._assert_creation(error=error, vs30=-1)
+
+    def test_wrong_z1pt0(self):
+        error = 'z1pt0 must be positive'
+        self._assert_creation(error=error, z1pt0=0)
+        self._assert_creation(error=error, z1pt0=-1)
+
+    def test_wrong_z2pt5(self):
+        error = 'z2pt5 must be positive'
+        self._assert_creation(error=error, z2pt5=0)
+        self._assert_creation(error=error, z2pt5=-1)
+
+    def test_successful_creation(self):
+        self._assert_creation()
+
+
 class SiteCollectionCreationTestCase(unittest.TestCase):
     def test_from_sites(self):
         s1 = Site(location=Point(10, 20, 30),
