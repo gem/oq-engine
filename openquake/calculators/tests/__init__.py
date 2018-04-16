6acf2f6978cee7a872dad949990678e35a666bb5
Michele Simionato <michele.simionato@gmail.com>
2018-04-16 11:02:33 +0200

    Now the BCR exporter also exports the tags [skip hazardlib]
    Former-commit-id: bec361e2c4ebcb8af46704d2bb8698b45fd922cd

--- openquake/calculators/tests/__init__.py
+++ openquake/calculators/tests/__init__.py
@@ -63,7 +63,7 @@ def columns(line):
 
 
 class CalculatorTestCase(unittest.TestCase):
-    OVERWRITE_EXPECTED = False
+    OVERWRITE_EXPECTED = True
     edir = None  # will be set to a temporary directory
 
     @classmethod
