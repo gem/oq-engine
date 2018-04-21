87aef19601f7df06111eb774d68f00b9e763083b
Michele Simionato <michele.simionato@gmail.com>
2018-04-21 06:04:30 +0200

    Fixed tests [skip hazardlib]
    Former-commit-id: 3eb539c4639a3dee2a7fc6af9a4d2622ec39141e [formerly 52f268daedc3235e472ad2373e10241989c5d505] [formerly 52f268daedc3235e472ad2373e10241989c5d505 [formerly b2317f8637f32bfe1888d997fca9ad991ba756df]]
    Former-commit-id: 32208cc9bdf0e3afc46af59564e2079fe65bff27
    Former-commit-id: 92efe6cdbbdec8346a40cfa82f64dd0e0c4cf469

--- openquake/calculators/scenario_damage.py
+++ openquake/calculators/scenario_damage.py
@@ -118,6 +118,7 @@ class ScenarioDamageCalculator(base.RiskCalculator):
         if 'gmfs' in self.oqparam.inputs:
             self.pre_calculator = None
         base.RiskCalculator.pre_execute(self)
+        _, self.R = base.get_gmfs(self)  # set number_of_ground_motion_fields
         E = self.oqparam.number_of_ground_motion_fields
         self.param['number_of_ground_motion_fields'] = E
         self.param['consequence_models'] = riskmodels.get_risk_models(
