2d622a9a7f7b1a1ab45d2f3947a20af710001829
Michele Simionato <michele.simionato@gmail.com>
2018-04-24 08:15:36 +0200

    Renamed get_gmfs->set_gmfs [skip hazardlib]
    Former-commit-id: f3a7a3ee86f0f31e0306eb0fd636f5181278a371 [formerly 4568c0d68319c0ef60e7075bb68a863d2930cd4f]
    Former-commit-id: 4d28821f21a478f3a88a9edf55c724e545bd11a2

--- openquake/calculators/scenario_risk.py
+++ openquake/calculators/scenario_risk.py
@@ -95,7 +95,7 @@ class ScenarioRiskCalculator(base.RiskCalculator):
         if 'gmfs' in self.oqparam.inputs:
             self.pre_calculator = None
         base.RiskCalculator.pre_execute(self)
-        _, self.R = base.get_gmfs(self)  # set number_of_ground_motion_fields
+        base.set_gmfs(self)  # set number_of_ground_motion_fields, R
         self.assetcol = self.datastore['assetcol']
         A = len(self.assetcol)
         E = self.oqparam.number_of_ground_motion_fields
