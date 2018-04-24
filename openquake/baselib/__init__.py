f7297a466e6496cdb3b50387f52702efe551bd2f
Matteo Nastasi <nastasi@openquake.org>
2018-04-24 10:20:42 +0200

    update changelog and documentation for 3.0.1 release (engine-3.0)

--- openquake/baselib/__init__.py
+++ openquake/baselib/__init__.py
@@ -23,7 +23,7 @@ from openquake.baselib.python3compat import configparser
 from openquake.baselib.general import git_suffix
 
 # the version is managed by packager.sh with a sed
-__version__ = '3.0.0'
+__version__ = '3.0.1'
 __version__ += git_suffix(__file__)
 
 
