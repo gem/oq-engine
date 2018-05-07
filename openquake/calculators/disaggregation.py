b16ff3ece0abe8c4d3b2e815bb5ec841c47e6d03
Michele Simionato <michele.simionato@gmail.com>
2018-05-07 08:53:39 +0200

    Restored prefiltering in disaggregation [skip CI]
    Former-commit-id: 30fefdaa65d28a21da68d396afccdde58cb5ae5f [formerly 8ff1375d863f6f34d5a6199e59f0a58e516cbae0]
    Former-commit-id: c587765ac87ecf25b506851c8bf2501e2889b1da

--- openquake/calculators/disaggregation.py
+++ openquake/calculators/disaggregation.py
@@ -214,7 +214,7 @@ producing too small PoEs.'''
         oq = self.oqparam
         tl = oq.truncation_level
         src_filter = SourceFilter(self.sitecol, oq.maximum_distance,
-                                  prefilter='no')
+                                  prefilter='numpy')
         csm = self.csm.filter(src_filter)  # fine filtering
         if not csm.get_sources():
             raise RuntimeError('All sources were filtered away!')
