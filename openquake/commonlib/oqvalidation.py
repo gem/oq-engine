2716849b4883f4096c19206a975b9d9df07e7c54
Michele Simionato <michele.simionato@gmail.com>
2018-05-14 15:27:02 +0200

    Restored filter_distance=rjb
    Former-commit-id: d3cae13da4082826d7d2f8cc233b7822b55f8172

--- openquake/commonlib/oqvalidation.py
+++ openquake/commonlib/oqvalidation.py
@@ -69,7 +69,7 @@ class OqParam(valid.ParamSet):
     exports = valid.Param(valid.export_formats, ())
     prefilter_sources = valid.Param(valid.Choice('rtree', 'numpy', 'no'),
                                     'rtree')
-    filter_distance = valid.Param(valid.Choice('rjb', 'rrup'), 'rrup')
+    filter_distance = valid.Param(valid.Choice('rjb', 'rrup'), 'rjb')
     ground_motion_correlation_model = valid.Param(
         valid.NoneOr(valid.Choice(*GROUND_MOTION_CORRELATION_MODELS)), None)
     ground_motion_correlation_params = valid.Param(valid.dictionary)
