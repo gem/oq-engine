e7ef81d9c2e8722f2c773f154b493f0092c81ac9
Michele Simionato <michele.simionato@gmail.com>
2018-05-17 19:11:36 +0200

    Removed min_idx_dst function
    Former-commit-id: b2762c37331ada65db9440c8575a31e7ce7e8501

--- openquake/hazardlib/geo/mesh.py
+++ openquake/hazardlib/geo/mesh.py
@@ -261,18 +261,18 @@ class Mesh(object):
         this mesh to each point of the target mesh and returns the lowest found
         for each.
         """
-        # NB: a slower alternative is to use self._min_idx_dst(mesh)[1]
         return cdist(self.xyz, mesh.xyz).min(axis=0)
 
-    def get_closest_points(self, mesh):
+    def get_closest_points(self, sites):
         """
-        Find closest point of this mesh for each one in ``mesh``.
+        Find closest point of this mesh for each site
 
         :returns:
-            :class:`Mesh` object of the same shape as ``mesh`` with closest
+            :class:`Mesh` object of the same shape as `sites` with closest
             points from this one at respective indices.
         """
-        min_idx, min_dst = self._min_idx_dst(mesh)
+        dists = cdist(self.xyz, sites.xyz)  # shape (M, N)
+        min_idx = [d.argmin() for d in dists.T]
         lons = self.lons.take(min_idx)
         lats = self.lats.take(min_idx)
         if self.depths is None:
@@ -280,18 +280,6 @@ class Mesh(object):
         else:
             depths = self.depths.take(min_idx)
         return Mesh(lons, lats, depths)
-
-    def _min_idx_dst(self, mesh):
-        if self.depths is None:
-            depths1 = numpy.zeros_like(self.lons)
-        else:
-            depths1 = self.depths
-        if mesh.depths is None:
-            depths2 = numpy.zeros_like(mesh.lons)
-        else:
-            depths2 = mesh.depths
-        return geodetic.min_idx_dst(
-            self.lons, self.lats, depths1, mesh.lons, mesh.lats, depths2)
 
     def get_distance_matrix(self):
         """
