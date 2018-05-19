0fd6441a4afc6b6d21403b7bc9058b2de8ce1f63
Michele Simionato <michele.simionato@gmail.com>
2018-05-19 08:50:16 +0200

    Simplified pre-job.ini in shakemap calculations
    Former-commit-id: 9525d0646f26589d06e45417f671aaf3fcb7f185 [formerly 66b1692f572799a1c3cd7a67ec1e5f7b599ce082]
    Former-commit-id: 5d80c24524eccc15330a0a34edbc2a7fd9f6ae01

--- openquake/commonlib/oqvalidation.py
+++ openquake/commonlib/oqvalidation.py
@@ -74,7 +74,7 @@ class OqParam(valid.ParamSet):
         valid.NoneOr(valid.Choice(*GROUND_MOTION_CORRELATION_MODELS)), None)
     ground_motion_correlation_params = valid.Param(valid.dictionary)
     ground_motion_fields = valid.Param(valid.boolean, False)
-    gsim = valid.Param(valid.gsim, None)
+    gsim = valid.Param(valid.gsim, valid.FromFile())
     hazard_calculation_id = valid.Param(valid.NoneOr(valid.positiveint), None)
     hazard_curves_from_gmfs = valid.Param(valid.boolean, False)
     hazard_output_id = valid.Param(valid.NoneOr(valid.positiveint))
