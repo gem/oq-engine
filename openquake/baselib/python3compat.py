cfc8d04ed82706a245c77bf3493dd4ae437458c4
Michele Simionato <michele.simionato@gmail.com>
2018-05-21 10:41:39 +0200

    Restored with_metaclass [skip CI]

--- openquake/baselib/python3compat.py
+++ openquake/baselib/python3compat.py
@@ -82,3 +82,21 @@ def raise_(tp, value=None, tb=None):
     if exc.__traceback__ is not tb:
         raise exc.with_traceback(tb)
     raise exc
+
+
+# the following is used in the SMTK
+# copied from http://lucumr.pocoo.org/2013/5/21/porting-to-python-3-redux/
+def with_metaclass(meta, *bases):
+    """
+    Returns an instance of meta inheriting from the given bases.
+    To be used to replace the __metaclass__ syntax.
+    """
+    class metaclass(meta):
+        __call__ = type.__call__
+        __init__ = type.__init__
+
+        def __new__(mcl, name, this_bases, d):
+            if this_bases is None:
+                return type.__new__(mcl, name, (), d)
+            return meta(name, bases, d)
+    return metaclass('temporary_class', None, {})
