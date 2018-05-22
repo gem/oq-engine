8531ffc5cbfd61527a93979a236ca5b4f687ab23
Michele Simionato <michele.simionato@gmail.com>
2018-05-22 14:59:14 +0200

    Updated changelog [skip CI]
    Former-commit-id: 632aae966e9ebebe575e187fba7b1098a9ec8d58

--- openquake/commands/__main__.py
+++ openquake/commands/__main__.py
@@ -27,7 +27,7 @@ from openquake import commands
 
 # check for Python version
 if sys.version < '3.5':
-    sys.exit('Python 3.5 is required, you are using %s', sys.executable)
+    sys.exit('Python 3.5+ is required, you are using %s', sys.executable)
 
 # force cluster users to use `oq engine` so that we have centralized logs
 if os.environ['OQ_DISTRIBUTE'] == 'celery' and 'run' in sys.argv:
