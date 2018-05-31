3623032c59ad67e233d832e79dc47854b5b94d57
Michele Simionato <michele.simionato@gmail.com>
2018-05-31 14:41:45 +0200

    Cleanup [skip CI]
    Former-commit-id: a737e103ffed98fd1a48c02f8bfc4aea434869ca

--- utils/combine_mean_curves.py
+++ utils/combine_mean_curves.py
@@ -17,13 +17,13 @@
 #  along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
 
 from openquake.baselib import sap, hdf5, datastore
+from openquake.calculators.getters import PmapGetter
 
 
 @sap.Script
 def combine_mean_curves(calc1, calc2):
     """
-    Combine the hazard curves coming from two different calculations,
-    only on the matching points
+    Combine the hazard curves coming from two different calculations
     """
     dstore1 = datastore.read(calc1)
     dstore2 = datastore.read(calc2)
@@ -37,16 +37,22 @@ def combine_mean_curves(calc1, calc2):
     if not common:
         raise RuntimeError('There are no common sites between calculation '
                            '%d and %d' % (calc1, calc2))
-    pmap = dstore1['hcurves/mean']
-    pmap2 = dstore2['hcurves/mean']
+    sids1 = [site_id1[lonlat] for lonlat in common]
+    sids2 = [site_id2[lonlat] for lonlat in common]
+    assoc1 = dstore1['csm_info'].get_rlzs_assoc()
+    assoc2 = dstore1['csm_info'].get_rlzs_assoc()
+    pmap = PmapGetter(dstore1, assoc1, sids1).get_mean()
+    pmap2 = PmapGetter(dstore1, assoc2, sids2).get_mean()
     for lonlat in common:
         pmap[site_id1[lonlat]] |= pmap2[site_id2[lonlat]]
-    with hdf5.File('combine_%d_%d.hdf5' % (calc1, calc2), 'w') as h5:
+    out = 'combine_%d_%d.hdf5' % (calc1, calc2)
+    with hdf5.File(out, 'w') as h5:
         h5['hcurves/mean'] = pmap
+    print('Generated %s' % out)
 
 
-combine_mean_curves.arg('calc1', 'first calculation')
-combine_mean_curves.arg('calc2', 'second calculation')
+combine_mean_curves.arg('calc1', 'first calculation', type=int)
+combine_mean_curves.arg('calc2', 'second calculation', type=int)
 
 if __name__ == '__main__':
     combine_mean_curves.callfunc()
