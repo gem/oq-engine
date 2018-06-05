856fd0369f7ae5bfd1a6abfc4a9de3d7c6362e1e
Michele Simionato <michele.simionato@gmail.com>
2018-06-05 14:33:32 +0200

    Fixed tests [skip hazardlib]

--- openquake/calculators/tests/event_based_risk_test.py
+++ openquake/calculators/tests/event_based_risk_test.py
@@ -286,8 +286,7 @@ class EventBasedRiskTestCase(CalculatorTestCase):
         # Turkey with SHARE logic tree; TODO: add site model
         # it has 8 realizations but 4 of them have 0 ruptures
         out = self.run_calc(case_4.__file__, 'job.ini',
-                            calculation_mode='event_based',
-                            ground_motion_fields='false', exports='csv')
+                            calculation_mode='event_based', exports='csv')
         [f1, f2] = [f for f in out['hcurves', 'csv'] if 'mean' in f]
         self.assertEqualFiles('expected/hazard_curve-mean-PGA.csv', f1)
         self.assertEqualFiles('expected/hazard_curve-mean-SA(0.5).csv', f2)
