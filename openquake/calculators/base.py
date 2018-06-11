3862715728bd4033c25720af754582833bd6a8e8
Michele Simionato <michele.simionato@gmail.com>
2018-06-11 07:01:16 +0200

    Cleanup [skip hazardlib]

--- openquake/calculators/base.py
+++ openquake/calculators/base.py
@@ -619,9 +619,9 @@ class HazardCalculator(BaseCalculator):
         self.rlzs_assoc = self.csm.info.get_rlzs_assoc(self.oqparam.sm_lt_path)
         if not self.rlzs_assoc:
             raise RuntimeError('Empty logic tree: too much filtering?')
-
         self.datastore['csm_info'] = self.csm.info
         R = len(self.rlzs_assoc.realizations)
+        logging.info('There are %d realizations', R)
         if self.is_stochastic and R >= TWO16:
             # rlzi is 16 bit integer in the GMFs, so there is hard limit or R
             raise ValueError(
