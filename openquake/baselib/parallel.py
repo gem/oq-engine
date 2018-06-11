d46638c1866490d29ad65459ef08f3378e6a1c78
Michele Simionato <michele.simionato@gmail.com>
2018-06-11 15:48:58 +0200

    Small fix [skip CI]
    Former-commit-id: a7388b4bf50c1bd20404ed1054e22b148fa766ce [formerly 1f803de9d6f5439cb8cdba99f7d50a0e49db21e5]
    Former-commit-id: ea976060981d09d69d8c1974aa0e3246e99ebccc

--- openquake/baselib/parallel.py
+++ openquake/baselib/parallel.py
@@ -227,7 +227,8 @@ class Pickled(object):
     def __init__(self, obj):
         self.clsname = obj.__class__.__name__
         self.calc_id = str(getattr(obj, 'calc_id', ''))  # for monitors
-        self.username = '[%s]' % obj.username if hasattr(obj, 'user') else ''
+        self.username = ('[%s]' % obj.username if hasattr(obj, 'username')
+                         else '')
         try:
             self.pik = pickle.dumps(obj, pickle.HIGHEST_PROTOCOL)
         except TypeError as exc:  # can't pickle, show the obj in the message
