78940ca3105421ee2fab58e820c5cc20239cc837
Michele Simionato <michele.simionato@gmail.com>
2018-06-19 04:38:24 +0200

    Expose only loss_curves-stats [skip hazardlib][demos]
    Former-commit-id: b1c46b69cf1f66016a31d66978903968a5e8c9f7

--- openquake/engine/engine.py
+++ openquake/engine/engine.py
@@ -141,9 +141,9 @@ def expose_outputs(dstore, owner=getpass.getuser(), status='complete'):
     if 'avg_losses-stats' in dstore or (
             'avg_losses-rlzs' in dstore and len(rlzs)):
         dskeys.add('avg_losses-stats')
-    if 'curves-rlzs' in dstore:
+    if 'curves-rlzs' in dstore and len(rlzs) == 1:
         dskeys.add('loss_curves-rlzs')
-    if 'curves-stats' in dstore:
+    if 'curves-stats' in dstore and len(rlzs) > 1:
         dskeys.add('loss_curves-stats')
     if oq.conditional_loss_poes:  # expose loss_maps outputs
         if 'loss_curves-stats' in dstore:
