9f9fc5b70ab47e41b8f7420d98abbc87f6777204
Michele Simionato <michele.simionato@gmail.com>
2018-06-19 06:01:01 +0200

    Cleanup [skip hazardlib][demos]
    Former-commit-id: 87967e0d86402ca2ce8c4f119dc5a703a6661fd0

--- openquake/calculators/base.py
+++ openquake/calculators/base.py
@@ -817,7 +817,6 @@ def save_gmdata(calc, n_rlzs):
         gmv = data[:-1] / events / n_sites
         array[rlzi] = tuple(gmv) + (events,)
     calc.datastore['gmdata'] = array
-    calc.num_events = array['events']
 
 
 def save_gmfs(calculator):
