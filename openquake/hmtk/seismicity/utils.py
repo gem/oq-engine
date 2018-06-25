042b14f43ab20ad4b2d6f6bf41a983bff2852874
Marco Pagani <marco.pagani@globalquakemodel.org>
2018-06-25 15:32:28 +0200

    Adding a test for the calculation of decimal time
    Former-commit-id: 7bca6214dd8d47a554c84442c16bd6849413075e

--- openquake/hmtk/seismicity/utils.py
+++ openquake/hmtk/seismicity/utils.py
@@ -157,7 +157,20 @@ def decimal_time(year, month, day, hour, minute, second):
     tho = np.zeros_like(year, dtype=int)
     tmi = np.zeros_like(year, dtype=int)
     tse = np.zeros_like(year, dtype=float)
-
+    #
+    # Checking inputs
+    if any(month < 1) or any(month > 12):
+        raise ValueError('Month must be in [1, 12]')
+    if any(day < 1) or any(day > 31):
+        raise ValueError('Day must be in [1, 31]')
+    if any(hour < 0) or any(hour > 24):
+        raise ValueError('Hour must be in [0, 24]')
+    if any(minute < 0) or any(minute > 60):
+        raise ValueError('Minute must be in [0, 60]')
+    if any(second < 0) or any(second > 60):
+        raise ValueError('Second must be in [0, 60]')
+    #
+    # Initialising values
     if any(month):
         tmo = month
     if any(day):
@@ -168,14 +181,15 @@ def decimal_time(year, month, day, hour, minute, second):
         tmi = minute
     if any(second):
         tse = second
-
+    #
+    # Computing decimal
     tmonth = tmo - 1
     day_count = MARKER_NORMAL[tmonth] + tda - 1
     id_leap = leap_check(year)
     leap_loc = np.where(id_leap)[0]
     day_count[leap_loc] = MARKER_LEAP[tmonth[leap_loc]] + tda[leap_loc] - 1
-    year_secs = (day_count.astype(float) * SECONDS_PER_DAY) + tse + \
-        (60. * tmi.astype(float)) + (3600. * tho.astype(float))
+    year_secs = ((day_count.astype(float) * SECONDS_PER_DAY) + tse +
+                 (60. * tmi.astype(float)) + (3600. * tho.astype(float)))
     dtime = year.astype(float) + (year_secs / (365. * 24. * 3600.))
     dtime[leap_loc] = year[leap_loc].astype(float) + \
         (year_secs[leap_loc] / (366. * 24. * 3600.))
