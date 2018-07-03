36cc0315a6ee3c61d8bc3be08d0a9939b2770f3e
Michele Simionato <michele.simionato@gmail.com>
2018-07-03 07:35:33 +0200

    Big cleanup
    Former-commit-id: 6f0163251df3c7aa162f82043655be2e5610a395

--- openquake/calculators/extract.py
+++ openquake/calculators/extract.py
@@ -30,7 +30,6 @@ else:
 from openquake.baselib.hdf5 import ArrayWrapper
 from openquake.baselib.general import DictArray, group_array
 from openquake.baselib.python3compat import encode
-from openquake.hazardlib import stats
 from openquake.calculators import getters
 from openquake.commonlib import calc, util
 
@@ -528,8 +527,12 @@ def extract_mfd(dstore, what):
 
 
 @extract.add('mean_std_curves')
-def extract_mean_std(dstore, what):
+def extract_mean_std_curves(dstore, what):
     """
     Yield mean and stddev for each IMT for each site
     """
-    return getters.PmapGetter(dstore).gen_mean_std()
+    getter = getters.PmapGetter(dstore)
+    arr = getter.get_mean().array
+    for imt in getter.imtls:
+        yield 'imls/' + imt, getter.imtls[imt]
+        yield 'poes/' + imt, arr[:, getter.imtls.slicedic[imt]]
