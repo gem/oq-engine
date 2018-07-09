5968ca06e68b4d2c3bd6b11c619632fa7c390347
Michele Simionato <michele.simionato@gmail.com>
2018-07-09 13:29:41 +0200

    Fixed demos [demos][skip hazardlib]
    Former-commit-id: 84b4625d417dd853296c725ecad6bf940641fd6d [formerly e7d6123acfebb114307c6caa6218f0533f839ac5]
    Former-commit-id: 0d7d80a10bae123f3e95fdcd8939986de1c93d32

--- openquake/calculators/tests/event_based_risk_test.py
+++ openquake/calculators/tests/event_based_risk_test.py
@@ -246,7 +246,7 @@ class EventBasedRiskTestCase(CalculatorTestCase):
         assetcol = self.calc.datastore['assetcol']
         self.assertEqual(len(sitecol), 21)
         self.assertGreater(sitecol.vs30.sum(), 0)
-        self.assertEqual(len(assetcol), 548)
+        self.assertEqual(len(assetcol), 21)
 
     @attr('qa', 'risk', 'event_based_risk')
     def test_case_7a(self):
