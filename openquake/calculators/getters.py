b69079e8357a5b5dce19c5d931fdb2c4cde8cc27
Michele Simionato <michele.simionato@gmail.com>
2018-07-13 04:50:06 +0200

    Fixed rupgeoms [skip hazardlib]
    Former-commit-id: 2897d4944fcf1090ba7afe94f4f34b6b103e2869 [formerly 3c2147ca7424dbf122dd6164a3786d9d221bdb4f]
    Former-commit-id: cf6a03882a1c4fc1eb05133f238baaa6723f8aaa

--- openquake/calculators/getters.py
+++ openquake/calculators/getters.py
@@ -525,8 +525,7 @@ class RuptureGetter(object):
             evs = events[rec['eidx1']:rec['eidx2']]
             if self.grp_id is not None and self.grp_id != rec['grp_id']:
                 continue
-            geom = rupgeoms[ridx]
-            mesh = geom['points'].reshape(3, rec['sy'], rec['sz'])
+            mesh = rupgeoms[ridx].reshape(3, rec['sy'], rec['sz'])
             rupture_cls, surface_cls = code2cls[rec['code']]
             rupture = object.__new__(rupture_cls)
             rupture.serial = serial
