5f875602f7bce153abb01b65bc3a4d90c996864c
Michele Simionato <michele.simionato@gmail.com>
2018-08-16 09:16:48 +0200

    Small fix [skip hazardlib][demos]
    Former-commit-id: c54372462585f3b620089411ab0ab70d1fee8dd3 [formerly c54372462585f3b620089411ab0ab70d1fee8dd3 [formerly 4ab636bbdc4eb1838db4a7cd913e5f5aa02b5d45]]
    Former-commit-id: 012f29cd51686c1ea81f73112758b3096ffffbc8
    Former-commit-id: 93ab95c64b998118e8a82ada83191c09d30626bb

--- openquake/commonlib/readinput.py
+++ openquake/commonlib/readinput.py
@@ -532,6 +532,7 @@ def get_source_models(oqparam, gsim_lt, source_model_lt, in_memory=True):
     if oqparam.calculation_mode.startswith('ucerf'):
         [grp] = nrml.to_python(oqparam.inputs["source_model"], converter)
     elif in_memory:
+        logging.info('Pickling the source model(s)')
         pik = logictree.pickle_source_models(
             gsim_lt, source_model_lt, converter)
 
