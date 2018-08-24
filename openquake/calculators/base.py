29858c2e4aa2a40c8d1a2712845e93b3507e459b
Michele Simionato <michele.simionato@gmail.com>
2018-08-24 17:29:30 +0200

    Split by taxonomy [skip hazardlib]
    Former-commit-id: e9af597476c5521580847714b930c68981acd774 [formerly c200e2e050920d861f94932e1fac43e5ae9b0a5a] [formerly c200e2e050920d861f94932e1fac43e5ae9b0a5a [formerly 7907a09888558d4d244d87d83dda2319ec05b633]]
    Former-commit-id: f3f0c9b163c8cd0a848e0a1215c757e67334d706
    Former-commit-id: 0893bb45cc2f43bfec756eafc463761333934219

--- openquake/calculators/base.py
+++ openquake/calculators/base.py
@@ -51,6 +51,7 @@ U64 = numpy.uint64
 F32 = numpy.float32
 TWO16 = 2 ** 16
 logversion = True
+MAX_ASSETS_PER_SITE = 500  # hard-coded for the moment
 
 
 class InvalidCalculationID(Exception):
@@ -805,9 +806,13 @@ class RiskCalculator(HazardCalculator):
             else:
                 # the datastore must be closed to avoid the HDF5 fork bug
                 assert dstore.hdf5 == (), '%s is not closed!' % dstore
-            ri = riskinput.RiskInput(getter, [assets], reduced_eps)
-            ri.weight = len(assets)
-            yield ri
+            if len(assets) > MAX_ASSETS_PER_SITE:
+                # if there are lots of assets on the same site split them
+                # by taxonomy to avoid slow tasks
+                for ass in general.groupby(assets, get_taxonomy).values():
+                    yield riskinput.RiskInput(getter, [ass], reduced_eps)
+            else:
+                yield riskinput.RiskInput(getter, [assets], reduced_eps)
 
     def execute(self):
         """
