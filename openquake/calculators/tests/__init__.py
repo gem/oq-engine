b1547e99a3ece00a1f58da2f827b3a211ab16486
Michele Simionato <michele.simionato@gmail.com>
2018-08-25 05:03:55 +0200

    Removed some shutdown [demos]

--- openquake/calculators/tests/__init__.py
+++ openquake/calculators/tests/__init__.py
@@ -107,8 +107,10 @@ class CalculatorTestCase(unittest.TestCase):
                 result.update(exported)
             duration[inis[1]] = calc._monitor.duration
             self.calc = calc
-        # reopen datastore, since some tests need to export from it
-        dstore = datastore.read(self.calc.datastore.calc_id)
+
+        if not self.calc.datastore.hdf5:
+            # reopen datastore, since some tests need to export from it
+            dstore = datastore.read(self.calc.datastore.calc_id)
         self.calc.datastore = dstore
         self.__class__.duration += duration
         return result
