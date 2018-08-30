8d997935c0157e646c5a280c77b45ca0c55ed491
Michele Simionato <michele.simionato@gmail.com>
2018-08-30 06:25:36 +0200

    More refactoring
    Former-commit-id: 6c4e8d1dfe3f15bc1058cf3a51b1ba0536fc7d75

--- openquake/hazardlib/tests/calc/stochastic_test.py
+++ openquake/hazardlib/tests/calc/stochastic_test.py
@@ -48,8 +48,7 @@ class StochasticEventSetTestCase(unittest.TestCase):
         lonlat = 135.68, 35.68
         site = Site(geo.Point(*lonlat), 800, True, z1pt0=100., z2pt5=1.)
         s_filter = SourceFilter(SiteCollection([site]), {})
-        param = dict(ses_per_logic_tree_path=10,
-                     filter_distance='rjb', samples=1)
+        param = dict(ses_per_logic_tree_path=10, filter_distance='rjb')
         gsims = [SiMidorikawa1999SInter()]
         dic = sample_ruptures(group, s_filter, gsims, param)
         self.assertEqual(dic['num_ruptures'], 19)  # total ruptures
