cc97ba89b22390946f88ae8ef5c41ed8dd552777
Robin Gee <robin.gee@globalquakemodel.org>
2018-08-30 15:02:37 +0200

    add comment and update changelog
    Former-commit-id: 0623c25ca8d25af6195368d135c9cd94fc5a7cf4 [formerly 4f4c48b360a6f4dc7aae22c0479854d829e2d90c]
    Former-commit-id: e6fc86e77c5287898719dd0d593e2bf47277a1b0

--- openquake/hazardlib/valid.py
+++ openquake/hazardlib/valid.py
@@ -261,7 +261,7 @@ class SimpleId(object):
             "Invalid ID '%s': the only accepted chars are a-zA-Z0-9_-" % value)
 
 
-MAX_ID_LENGTH = 75
+MAX_ID_LENGTH = 75  # length required for some sources in US14 collapsed model
 ASSET_ID_LENGTH = 100
 
 simple_id = SimpleId(MAX_ID_LENGTH)
