fe8551142cef19a513ab1f62f36cf2b40d180768
Michele Simionato <michele.simionato@gmail.com>
2018-08-31 10:51:26 +0200

    Avoid rtree error in Index_Create [skip hazardlib]
    Former-commit-id: 3443d89da097c44f579d625fb8afc62961ad97b7 [formerly 1c217f0b591ffc76cc2f1c3f830cc2aba908ee31] [formerly 1c217f0b591ffc76cc2f1c3f830cc2aba908ee31 [formerly 851b8006b278806f9ca70200104aede97c76b424]]
    Former-commit-id: a0e96a8532ab8026430928074a986ce933cbd2bd
    Former-commit-id: 826509e23a28313072191548a1714d102a8568ff

--- openquake/calculators/base.py
+++ openquake/calculators/base.py
@@ -329,18 +329,22 @@ class HazardCalculator(BaseCalculator):
         self.hdf5cache = self.datastore.hdf5cache()
         src_filter = SourceFilter(self.sitecol.complete, oq.maximum_distance,
                                   self.hdf5cache)
-        if (oq.prefilter_sources == 'numpy' or rtree is None or
-                config.distribution.multi_node):
-            logging.info('Prefiltering the sources with numpy')
-            csm = self.csm.pfilter(src_filter, oq.concurrent_tasks, mon)
-        elif oq.prefilter_sources == 'rtree':
+        dist = os.environ['OQ_DISTRIBUTE']
+        if oq.prefilter_sources == 'no':
+            logging.info('Not prefiltering the sources')
+            csm = self.csm
+        elif oq.prefilter_sources == 'rtree' and dist in ('no', 'processpool'):
+            # rtree can be used only with processpool, otherwise one gets an
+            # RTreeError: Error in "Index_Create": Spatial Index Error:
+            # IllegalArgumentException: SpatialIndex::DiskStorageManager:
+            # Index/Data file cannot be read/writen.
             logging.info('Prefiltering the sources with rtree')
             prefilter = RtreeFilter(self.sitecol.complete, oq.maximum_distance,
                                     self.hdf5cache)
             csm = self.csm.pfilter(prefilter, oq.concurrent_tasks, mon)
-        else:  # prefilter_sources='no'
-            logging.info('Not prefiltering the sources')
-            csm = self.csm
+        else:
+            logging.info('Prefiltering the sources with numpy')
+            csm = self.csm.pfilter(src_filter, oq.concurrent_tasks, mon)
         logging.info('There are %d realizations', csm.info.get_num_rlzs())
         return src_filter, csm
 
