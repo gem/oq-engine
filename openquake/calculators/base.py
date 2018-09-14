d69033f87ed377d68a8cee8e62d631555e2772f3
Michele Simionato <michele.simionato@gmail.com>
2018-09-14 19:08:03 +0200

    [skip CI]

--- openquake/calculators/base.py
+++ openquake/calculators/base.py
@@ -662,9 +662,9 @@ class HazardCalculator(BaseCalculator):
         else:
             ids, vals = zip(*sorted(calc_times.items()))
             vals = numpy.array(vals)  # shape (n, 3)
-            source_info[ids, 'weight'] = vals[:, 0]
-            source_info[ids, 'num_sites'] = vals[:, 1]
-            source_info[ids, 'calc_time'] = vals[:, 2]
+            source_info[ids, 'weight'] += vals[:, 0]
+            source_info[ids, 'num_sites'] += vals[:, 1]
+            source_info[ids, 'calc_time'] += vals[:, 2]
 
     def post_process(self):
         """For compatibility with the engine"""
