781d6bef1c034070fd54e37f5fdcf3834ac604fd
Michele Simionato <michele.simionato@gmail.com>
2018-09-27 06:56:00 +0200

    Fixed test [skip hazardlib]

--- openquake/calculators/tests/event_based_test.py
+++ openquake/calculators/tests/event_based_test.py
@@ -308,8 +308,7 @@ class EventBasedTestCase(CalculatorTestCase):
             'hazard_curve-rlz-004.csv',
         ]
         # test --hc functionality, i.e. that the ruptures are read correctly
-        out = self.run_calc(case_17.__file__, 'job.ini,job.ini', exports='csv',
-                            concurrent_tasks='0')
+        out = self.run_calc(case_17.__file__, 'job.ini,job.ini', exports='csv')
         fnames = out['hcurves', 'csv']
         for exp, got in zip(expected, fnames):
             self.assertEqualFiles('expected/%s' % exp, got)
