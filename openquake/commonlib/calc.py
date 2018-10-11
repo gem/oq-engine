80e83f46b5b13ac556569b92b3c7a5965ee9bb76
Michele Simionato <michele.simionato@gmail.com>
2018-10-11 08:50:12 +0200

    Fixed tests [demos]

--- openquake/commonlib/calc.py
+++ openquake/commonlib/calc.py
@@ -317,8 +317,8 @@ class RuptureData(object):
         self.params = sorted(self.cmaker.REQUIRES_RUPTURE_PARAMETERS -
                              set('mag strike dip rake hypo_depth'.split()))
         self.dt = numpy.dtype([
-            ('rup_id', U32), ('multiplicity', U16), ('eidx', U32),
-            ('occurrence_rate', F64),
+            ('rup_id', U32), ('srcidx', U32), ('multiplicity', U16),
+            ('eidx', U32), ('occurrence_rate', F64),
             ('mag', F32), ('lon', F32), ('lat', F32), ('depth', F32),
             ('strike', F32), ('dip', F32), ('rake', F32),
             ('boundary', hdf5.vstr)] + [(param, F32) for param in self.params])
@@ -342,7 +342,7 @@ class RuptureData(object):
             except AttributeError:  # for nonparametric sources
                 rate = numpy.nan
             data.append(
-                (ebr.serial, ebr.multiplicity, ebr.eidx1, rate,
+                (ebr.serial, ebr.srcidx, ebr.multiplicity, ebr.eidx1, rate,
                  rup.mag, point.x, point.y, point.z, rup.surface.get_strike(),
                  rup.surface.get_dip(), rup.rake,
                  'MULTIPOLYGON(%s)' % decode(bounds)) + ruptparams)
