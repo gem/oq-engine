bfd73e7300207a3b5e5f900af593902116c4f8f3
Michele Simionato <michele.simionato@gmail.com>
2018-10-12 09:43:51 +0200

    Added a functional test for the site term feature [skip hazardlib]

--- openquake/calculators/tests/event_based_test.py
+++ openquake/calculators/tests/event_based_test.py
@@ -36,7 +36,7 @@ from openquake.calculators.tests import CalculatorTestCase
 from openquake.qa_tests_data.event_based import (
     blocksize, case_1, case_2, case_3, case_4, case_5, case_6, case_7,
     case_8, case_9, case_10, case_12, case_13, case_14, case_15, case_16,
-    case_17,  case_18, mutex)
+    case_17,  case_18, case_19, mutex)
 from openquake.qa_tests_data.event_based.spatial_correlation import (
     case_1 as sc1, case_2 as sc2, case_3 as sc3)
 
@@ -346,6 +346,12 @@ PGA     SA(0.3) SA(0.6)
         [fname, _sitefile] = out['gmf_data', 'csv']
         self.assertEqualFiles('expected/%s' % strip_calc_id(fname), fname,
                               delta=1E-6)
+
+    @attr('qa', 'hazard', 'event_based')
+    def test_case_19(self):  # test for Vancouver using the NRCan15SiteTerm
+        self.run_calc(case_19.__file__, 'job.ini')
+        [gmf, site] = export(('gmf_data', 'csv'), self.calc.datastore)
+        self.assertEqualFiles('expected/gmf-data.csv', gmf)
 
     @attr('qa', 'hazard', 'event_based')
     def test_overflow(self):
