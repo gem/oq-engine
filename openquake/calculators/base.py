0cb395ba28881b4f417bf0f692a275dc75e3e051
Michele Simionato <michele.simionato@gmail.com>
2018-10-17 10:53:18 +0200

    Added check for missing exposure [skip hazardlib][demos]
    Former-commit-id: 020ee747ecca44b031197859305470c76f0616a8

--- openquake/calculators/base.py
+++ openquake/calculators/base.py
@@ -609,6 +609,8 @@ class HazardCalculator(BaseCalculator):
             parent = self.datastore.parent
             if 'assetcol' in parent:
                 check_time_event(oq, parent['assetcol'].occupancy_periods)
+            elif 'exposure' not in oq.inputs:
+                raise ValueError('Missing exposure both in hazard and risk!')
             if oq_hazard.time_event and oq_hazard.time_event != oq.time_event:
                 raise ValueError(
                     'The risk configuration file has time_event=%s but the '
