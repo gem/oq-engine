e56a5f27d59e169d0bd91e03cdef44cabae18d1a
Michele Simionato <michele.simionato@gmail.com>
2018-10-22 11:12:17 +0200

    Decided the syntax for the job.ini file with reqv files
    Former-commit-id: 43979c0b1f4f82a8f36d40ebf41315f3157083d2

--- openquake/commonlib/oqvalidation.py
+++ openquake/commonlib/oqvalidation.py
@@ -181,8 +181,11 @@ class OqParam(valid.ParamSet):
         """
         :returns: an instance of class:`RjbEquivalent` if reqv_hdf5 is set
         """
-        if 'reqv' in self.inputs:
-            return valid.RjbEquivalent(self.inputs['reqv'])
+        dic = {}  # a dict trt -> lookup.hdf5 file
+        for key, fname in self.inputs.items():
+            if key.startswith('reqv_'):
+                dic[key[5:]] = valid.RjbEquivalent(fname)
+        return dic
 
     def __init__(self, **names_vals):
         super().__init__(**names_vals)
