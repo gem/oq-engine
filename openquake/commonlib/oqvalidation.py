fdd08e7a45273903c57363a2c6705b9fe485add7
Michele Simionato <michele.simionato@gmail.com>
2018-10-24 05:42:20 +0200

    Stored riskinput_info [skip hazardlib][demos]
    Former-commit-id: eed71b2856660647797f79add30c0ec9d7b53883

--- openquake/commonlib/oqvalidation.py
+++ openquake/commonlib/oqvalidation.py
@@ -52,6 +52,7 @@ class OqParam(valid.ParamSet):
         valid.NoneOr(valid.positivefloat), None)
     asset_correlation = valid.Param(valid.NoneOr(valid.FloatRange(0, 1)), 0)
     asset_life_expectancy = valid.Param(valid.positivefloat)
+    assets_per_site_limit = valid.Param(valid.positivefloat, 1000)
     avg_losses = valid.Param(valid.boolean, True)
     base_path = valid.Param(valid.utf8, '.')
     calculation_mode = valid.Param(valid.Choice(), '')  # -> get_oqparam
