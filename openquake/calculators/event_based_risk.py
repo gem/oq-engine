a0080c181050ffef70063ba4b52713594ba2920a
Michele Simionato <michele.simionato@gmail.com>
2018-10-25 05:39:21 +0200

    Removed unused import

--- openquake/calculators/event_based_risk.py
+++ openquake/calculators/event_based_risk.py
@@ -20,7 +20,6 @@ import operator
 import numpy
 
 from openquake.baselib.python3compat import zip, encode
-from openquake.baselib.general import AccumDict
 from openquake.hazardlib.stats import set_rlzs_stats
 from openquake.risklib import riskinput
 from openquake.calculators import base
