6c76a26dc35d01494328df7d4e33df571189ed4e
Michele Simionato <michele.simionato@gmail.com>
2018-10-28 17:59:23 +0100

    Fixed non-ucerf tests
    Former-commit-id: 18d27b5909f6af6e6aed398396de77d906340982

--- openquake/calculators/ucerf_event_based.py
+++ openquake/calculators/ucerf_event_based.py
@@ -218,7 +218,8 @@ def compute_hazard(sources, src_filter, rlzs_by_gsim, param, monitor):
     sitecol = src_filter.sitecol
     cmaker = ContextMaker(rlzs_by_gsim, src_filter.integration_distance)
     num_ses = len(param['ses_seeds'])
-    for sam_idx in range(src.samples):
+    num_rlzs = sum(len(rlzs) for rlzs in rlzs_by_gsim.values())
+    for sam_idx in range(num_rlzs):
         for ses_idx, ses_seed in param['ses_seeds']:
             seed = sam_idx * TWO16 + ses_seed
             with sampl_mon:
@@ -229,7 +230,7 @@ def compute_hazard(sources, src_filter, rlzs_by_gsim, param, monitor):
                     serial += 1
             with filt_mon:
                 ebrs = stochastic.build_eb_ruptures(
-                    src, slice(0, 1), num_ses, cmaker, sitecol,
+                    src, slice(0, num_rlzs), num_ses, cmaker, sitecol,
                     zip(rups, n_occs), (sam_idx, ses_idx))
                 ebruptures.extend(ebrs)
     res.num_events = sum(ebr.multiplicity for ebr in ebruptures)
