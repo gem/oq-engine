b110bf6edb21c43a1a7a1aeba7c751a6c562a5e9
Michele Simionato <michele.simionato@gmail.com>
2018-11-06 07:10:25 +0100

    Renamed input_zip -> input/zip [skip hazardlib][demos]

--- openquake/server/db/actions.py
+++ openquake/server/db/actions.py
@@ -293,7 +293,7 @@ DISPLAY_NAME = {
     'disagg_by_src': 'Disaggregation by Source',
     'realizations': 'Realizations',
     'fullreport': 'Full Report',
-    'input_zip': 'Input Files'
+    'input': 'Input Files'
 }
 
 # sanity check, all display name keys must be exportable
