b685f4b3688e2e08b8a2a312fb3746b43718dfa3
Michele Simionato <michele.simionato@gmail.com>
2018-11-18 04:11:12 +0100

    Restored prefilter_sources [skip hazardlib]
    Former-commit-id: fc66c0f8798a30c6479d02f98500af31ee8de50d [formerly 718570d028203a5add21c0096c29a6b6dc2e45cc]
    Former-commit-id: 04b5ad5a315f219fa02c30ea1126fe9e1f3716e1

--- openquake/commonlib/readinput.py
+++ openquake/commonlib/readinput.py
@@ -862,10 +862,11 @@ def get_composite_source_model(oqparam, monitor=None, in_memory=True,
     if monitor.hdf5:
         csm.info.gsim_lt.store_gmpe_tables(monitor.hdf5)
 
-    # splitting assumes that the serials have been initialized already
-    split = (split_all and oqparam.calculation_mode not in
-             'ucerf_hazard ucerf_risk')
-    csm = parallel_split_filter(csm, srcfilter, split, monitor('prefilter'))
+    if oqparam.prefilter_sources != 'no':
+        mon = monitor('split_prefilter')
+        split = (split_all and oqparam.calculation_mode not in
+                 'ucerf_hazard ucerf_risk')
+        csm = parallel_split_filter(csm, srcfilter, split, mon)
     return csm
 
 
