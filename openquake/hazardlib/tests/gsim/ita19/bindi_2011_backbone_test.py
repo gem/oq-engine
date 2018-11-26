ca677057e2b01deb446f5f3e00111621d52c0074
Marco Pagani <marco.pagani@globalquakemodel.org>
2018-11-26 14:29:41 +0100

    Added tests for the computation of delta
    Former-commit-id: 7d25d7cb9a805fff0901f945c7928e0d7f7092a0 [formerly 7d25d7cb9a805fff0901f945c7928e0d7f7092a0 [formerly e4fdffbfcbdacd71aa169a3c2b07ae0e81ad53db]]
    Former-commit-id: 6603193938937ee69427801e758a6d2e0a81c7ac
    Former-commit-id: 9511c4d6d3386ca613e3883e3c502fc6dab01698

--- openquake/hazardlib/tests/gsim/ita19/bindi_2011_backbone_test.py
+++ openquake/hazardlib/tests/gsim/ita19/bindi_2011_backbone_test.py
@@ -19,15 +19,38 @@
 import numpy as np
 import unittest
 
-from openquake.hazardlib.imt import PGA
+from openquake.hazardlib.imt import PGA, SA, PGV
 from openquake.hazardlib.gsim.ita19.bindi_2011_backbone import BindiEtAl2011Low
+
 
 class GetDeltaTest(unittest.TestCase):
 
-    def test_pga(self):
-        m = np.array([5., 6., 7.])
+    def test_mag5(self):
+        m = 5.0
         gmm = BindiEtAl2011Low()
-        imt = PGA()
-        delta = gmm._get_delta(imt, m)
+        imts = [PGA(), SA(0.05), SA(0.1), SA(0.15), SA(0.2),
+                SA(0.3), SA(0.4), SA(0.5), SA(0.75), SA(1.00), SA(2.00),
+                SA(3.00), SA(4.00), PGV()]
+        computed = []
+        for imt in imts:
+            dlt = gmm._get_delta(imt, m)
+            computed.append(dlt)
+        expected = np.array([0.4, 0.366, 0.381, 0.362, 0.421, 0.384, 0.406,
+                             0.428, 0.413, 0.39, 0.353, 0.363, 0.416, 0.345])
+        np.testing.assert_array_almost_equal(computed, expected)
 
+    def test_mag7pt2(self):
+        m = 7.2
+        gmm = BindiEtAl2011Low()
+        imts = [PGA(), SA(0.05), SA(0.1), SA(0.15), SA(0.2),
+                SA(0.3), SA(0.4), SA(0.5), SA(0.75), SA(1.00), SA(2.00),
+                SA(3.00), SA(4.00), PGV()]
+        computed = []
+        for imt in imts:
+            dlt = gmm._get_delta(imt, m)
+            computed.append(dlt)
+        expected = np.array([0.61164, 0.5662, 0.56228, 0.61236, 0.707, 0.65284,
+                             0.62512, 0.64052, 0.59472, 0.54224, 0.32704,
+                             0.3498, 0.46264, 0.48624])
+        np.testing.assert_array_almost_equal(computed, expected)
 
