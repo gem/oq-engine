257d0a5165291949d32f842a38e5f5ff0891f172
Michele Simionato <michele.simionato@gmail.com>
2018-11-28 04:11:07 +0100

    Monitoring eid2rlz [skip hazardlib]
    Former-commit-id: 44941902e45d185529497a0bd9583fbcaef3f4f9 [formerly ee1ad8589eb65bfd92173604e74961b454a763c5]
    Former-commit-id: c26fafbb883758ed65677032ef0beaf306e72f89

--- openquake/calculators/tests/ucerf_test.py
+++ openquake/calculators/tests/ucerf_test.py
@@ -32,10 +32,11 @@ class UcerfTestCase(CalculatorTestCase):
         self.run_calc(ucerf.__file__, 'job.ini')
         gmv_uc = view('global_gmfs', self.calc.datastore)
         # check the distribution of the events
-        self.assertEventsByRlz([58, 1, 1, 1, 3, 4, 1, 1, 2, 2, 4, 3, 2, 2, 3,
-                                3, 3, 3, 1, 1, 1, 1, 3, 3, 1, 1, 1, 1, 1, 1,
-                                1, 1, 2, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 3,
-                                3, 2, 2])
+        self.assertEventsByRlz([2, 2, 2, 2, 6, 6, 2, 2, 2, 2, 6, 6, 2, 2, 3,
+                                3, 6, 6, 1, 1, 1, 1, 6, 6, 2, 2, 3, 3, 2, 2,
+                                2, 2, 3, 3, 2, 2, 3, 3, 3, 3, 2, 2, 3, 3,
+                                3, 3, 3, 3])
+
         # NB: there are no 58 events for the first realization, it is a bug,
         # but it will be fixed in the future
 
@@ -67,7 +68,7 @@ class UcerfTestCase(CalculatorTestCase):
         self.run_calc(ucerf.__file__, 'job_ebh.ini')
 
         # check the distribution of the events
-        self.assertEventsByRlz([35, 19])
+        self.assertEventsByRlz([29, 25])
         # NB: there are no 35 events for the first realization, it is a bug,
         # but it will be fixed in the future
 
