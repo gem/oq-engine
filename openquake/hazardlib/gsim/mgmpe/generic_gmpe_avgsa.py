b718a7302fc72516765d5afff0ffd310de94389e
klunk386 <poggi.valerio@gmail.com>
2018-12-03 15:28:25 +0100

    added standard deviation and correlation
    Former-commit-id: ae6644630ebbbb81bd869f70fec77769152d8eac

--- openquake/hazardlib/gsim/mgmpe/generic_gmpe_avgsa.py
+++ openquake/hazardlib/gsim/mgmpe/generic_gmpe_avgsa.py
@@ -49,6 +49,7 @@ class GenericGmpeAvgSA(GMPE):
         self.gmpe = registry[gmpe_name]()
         self.set_parameters()
         self.avg_periods = [float(t) for t in avg_periods.split(',')]
+        self.tnum = len(self.avg_periods)
 
         # Check if this GMPE has the necessary requirements
         # TO DO
@@ -59,8 +60,10 @@ class GenericGmpeAvgSA(GMPE):
         <.base.GroundShakingIntensityModel.get_mean_and_stddevs>`
         for spec of input and result values.
         """
-        mean_avgsa = 0.
-        tnum = len(self.avg_periods)
+
+        mean_list = []
+        stddvs_list = []
+
         # Loop over averaging periods
         for period in self.avg_periods:
             imt_local = SA(float(period))
@@ -68,7 +71,68 @@ class GenericGmpeAvgSA(GMPE):
             mean, stddvs = self.gmpe.get_mean_and_stddevs(sites, rup, dists,
                                                           imt_local,
                                                           stds_types)
-            mean_avgsa += (1./tnum) * mean
+            mean_list.append(mean)
+            stddvs_list.append(stddvs[0]) # Support only for total!
 
-        return mean_avgsa, stddvs
+        mean_avgsa = 0.
+        stddvs_avgsa = 0.
 
+        for i1 in range(self.tnum):
+            mean_avgsa += mean_list[i1]
+            for i2 in range(self.tnum):
+                rho = baker_jayaram_correlation(self.avg_periods[i1],
+                                                self.avg_periods[i2])
+                stddvs_avgsa += stddvs_list[i1] * stddvs_list[i2]
+
+        mean_avgsa *= (1./self.tnum)
+        stddvs_avgsa *= (1./self.tnum)**2
+
+        return mean_avgsa, [stddvs_avgsa]
+
+
+def baker_jayaram_correlation(t1, t2):
+    """
+    NOTE: subroutine taken from: https://usgs.github.io/shakemap/shakelib
+    
+    Produce inter-period correlation for any two spectral periods.
+
+    Based upon:
+    Baker, J.W. and Jayaram, N., "Correlation of spectral acceleration
+    values from NGA ground motion models," Earthquake Spectra, (2007).
+
+    Args:
+        t1, t2 (float):
+            The two periods of interest.
+
+    Returns:
+        rho (float): The predicted correlation coefficient
+
+    """
+    t_min = min(t1, t2)
+    t_max = max(t1, t2)
+
+    c1 = 1.0 - np.cos(np.pi / 2.0 - np.log(t_max / max(t_min, 0.109)) * 0.366)
+
+    if t_max < 0.2:
+        c2 = 1.0 - 0.105 * (1.0 - 1.0 / (1.0 + np.exp(100.0 * t_max - 5.0)))
+        c2 *= (t_max - t_min) / (t_max - 0.0099)
+    else:
+        c2 = 0
+
+    if t_max < 0.109:
+        c3 = c2
+    else:
+        c3 = c1
+
+    c4 = c1 + 0.5 * (np.sqrt(c3) - c3) * (1.0 + np.cos(np.pi * t_min / 0.109))
+
+    if t_max <= 0.109:
+        rho = c2
+    elif t_min > 0.109:
+        rho = c1
+    elif t_max < 0.2:
+        rho = min(c2, c4)
+    else:
+        rho = c4
+
+    return rho
