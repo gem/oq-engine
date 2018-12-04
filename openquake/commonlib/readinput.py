2ff2b5763084706742b3d4faa5141a15a12d2000
Michele Simionato <michele.simionato@gmail.com>
2018-12-04 11:49:12 +0100

    More logging [skip hazardlib][demos]
    Former-commit-id: 4f17f36f1d5681245c81273fec1bb803c3b65c7d [formerly 9920d74a1e5ec58573f11b88119fd35c85206690] [formerly 9920d74a1e5ec58573f11b88119fd35c85206690 [formerly 017fb1a317997cccd7fa079bae88a2fad8708ac4]]
    Former-commit-id: a84ba0be4ef3e6b00d58bc9ead6ac5b901d6d726
    Former-commit-id: 3cd205ef3052da043911c1b495cd141ff77d95f8

--- openquake/commonlib/readinput.py
+++ openquake/commonlib/readinput.py
@@ -830,13 +830,16 @@ def get_composite_source_model(oqparam, monitor=None, in_memory=True,
             raise ValueError('Unknown TRT=%s in %s [reqv]' %
                              (trt, oqparam.inputs['job_ini']))
     gsim_lt = get_gsim_lt(oqparam, trts or ['*'])
-    if oqparam.number_of_logic_tree_samples == 0:
-        p = source_model_lt.num_paths * gsim_lt.get_num_paths()
-        if ('event_based' in oqparam.calculation_mode
-                and p > oqparam.max_potential_paths):
-            raise ValueError(
-                'There are too many potential logic tree paths (%d) '
-                'use sampling instead of full enumeration' % p)
+    p = source_model_lt.num_paths * gsim_lt.get_num_paths()
+    if ('event_based' in oqparam.calculation_mode
+            and p > oqparam.max_potential_paths):
+        raise ValueError(
+            'There are too many potential logic tree paths (%d) '
+            'use sampling instead of full enumeration' % p)
+    if oqparam.number_of_logic_tree_samples:
+        logging.info('Considering {:,d} logic tree paths out of {:,d}'.format(
+            oqparam.number_of_logic_tree_samples, p))
+    else:
         logging.info('Potential number of logic tree paths = {:,d}'.format(p))
 
     if source_model_lt.on_each_source:
