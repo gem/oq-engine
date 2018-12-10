3e8b01eee6448223d7c417b7b0b6f7a58bdbb825
klunk386 <poggi.valerio@gmail.com>
2018-12-10 10:15:54 +0100

    adding REQ and test template
    Former-commit-id: a22eed47922db04326d2f102e8d5779ec798c429 [formerly a22eed47922db04326d2f102e8d5779ec798c429 [formerly 6142a24f060eb01d229a453c901dc02de110e527]]
    Former-commit-id: 897dfc6391a38abb94c5cf88ef3afeb9a1e7eab1
    Former-commit-id: c7de6f1c2f3ae499b19a6e983fc31c419feee813

---
+++ openquake/hazardlib/tests/gsim/mgmpe/generic_gmpe_avgsa_test.py
@@ -0,0 +1,28 @@
+# The Hazard Library
+# Copyright (C) 2012-2018 GEM Foundation
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Implements the tests for the metaGMPE class to compute average spectral
+response from a generic base GMPE.
+"""
+
+import numpy as np
+import unittest
+
+class GenericGmpeAvgSATestCase(unittest.TestCase):
+
+    def test_1(self):
+        pass
