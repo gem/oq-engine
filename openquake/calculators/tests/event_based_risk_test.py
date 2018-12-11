ee575a287d11eccf51a5386fae731d74ba42ffd0
Michele Simionato <michele.simionato@gmail.com>
2018-12-11 06:56:12 +0100

    Restored ContextMaker filtering [demos]
    Former-commit-id: ace3f01f9386adcf0afd5f712243068d73f68cc5

--- openquake/calculators/tests/event_based_risk_test.py
+++ openquake/calculators/tests/event_based_risk_test.py
@@ -171,7 +171,7 @@ class EventBasedRiskTestCase(CalculatorTestCase):
 
         # test the number of bytes saved in the rupture records
         nbytes = self.calc.datastore.get_attr('ruptures', 'nbytes')
-        self.assertEqual(nbytes, 3180)
+        self.assertEqual(nbytes, 2544)
 
         # test postprocessing
         self.calc.datastore.close()
