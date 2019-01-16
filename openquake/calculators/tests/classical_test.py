5160851c03692cb20caeed6023cab2609449c665
Marco <pgnmrc69@gmail.com>
2019-01-16 08:54:34 +0100

    Changes to the engine component
    Former-commit-id: 47810ddc662b53c3d3f8f73931aa946f94b77f2f [formerly 47810ddc662b53c3d3f8f73931aa946f94b77f2f [formerly b813f848e790f983e1eb5dbe923fb8dcde84a37a]]
    Former-commit-id: ed30d72c5aad21ea7480e2f3794899ca093d55fd
    Former-commit-id: 7bea397e1e61c13eb8ae009dd7c1ece0f11b3437

--- openquake/calculators/tests/classical_test.py
+++ openquake/calculators/tests/classical_test.py
@@ -30,7 +30,8 @@ from openquake.qa_tests_data.classical import (
     case_1, case_2, case_3, case_4, case_5, case_6, case_7, case_8, case_9,
     case_10, case_11, case_12, case_13, case_14, case_15, case_16, case_17,
     case_18, case_19, case_20, case_21, case_22, case_23, case_24, case_25,
-    case_26, case_27, case_28, case_29, case_30, case_31, case_32, case_33)
+    case_26, case_27, case_28, case_29, case_30, case_31, case_32, case_33,
+    case_35)
 
 
 class ClassicalTestCase(CalculatorTestCase):
@@ -500,3 +501,9 @@ hazard_uhs-std.csv
     def test_case_33(self):
         # directivity
         self.assert_curves_ok(['hazard_curve-mean-PGA.csv'], case_33.__file__)
+
+
+    @attr('qa', 'hazard', 'classical')
+    def test_case_35(self):
+        # cluster
+        self.assert_curves_ok(['hazard_curve-mean-PGA.csv'], case_35.__file__)
