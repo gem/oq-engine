c31da1810d86c98e33dc9d8714000f88ce8647ba
klunk386 <poggi.valerio@gmail.com>
2019-01-27 16:37:58 +0100

    Abstract and derived class for correlation model
    Former-commit-id: b7bb68a51a81a3df6b613dbe1fdf97b61b4e3275

--- openquake/hazardlib/tests/gsim/mgmpe/generic_gmpe_avgsa_test.py
+++ openquake/hazardlib/tests/gsim/mgmpe/generic_gmpe_avgsa_test.py
@@ -63,6 +63,7 @@ class GenericGmpeAvgSATestCase(unittest.TestCase):
                 except ValueError:
                     pass
 
+
     def test_calculation_Akkar_valueerror(self):
         """
         """
