f8b691d246e47316c4aecf2fa9b2c80bedc1ecd7
klunk386 <poggi.valerio@gmail.com>
2019-01-29 17:06:33 +0100

    various fix and pep check
    Former-commit-id: 30f81778d74c4cbd2547f0e26cfd1af935799b69

--- openquake/calculators/tests/classical_test.py
+++ openquake/calculators/tests/classical_test.py
@@ -31,7 +31,7 @@ from openquake.qa_tests_data.classical import (
     case_10, case_11, case_12, case_13, case_14, case_15, case_16, case_17,
     case_18, case_19, case_20, case_21, case_22, case_23, case_24, case_25,
     case_26, case_27, case_28, case_29, case_30, case_31, case_32, case_33,
-    case_34)
+    case_34, case_35)
 
 
 class ClassicalTestCase(CalculatorTestCase):
@@ -507,3 +507,9 @@ hazard_uhs-std.csv
         # spectral averaging
         self.assert_curves_ok([
             'hazard_curve-mean-AverageSA.csv'], case_34.__file__)
+
+    @attr('qa', 'hazard', 'classical')
+    def test_case_35(self):
+        # cluster
+        self.assert_curves_ok(['hazard_curve-rlz-000-PGA.csv'],
+                               case_35.__file__)
