f2a0c3ddc21df631c9b8d19ac41ee30ecdaa2f80
Michele Simionato <michele.simionato@gmail.com>
2019-01-29 04:53:37 +0100

    Postfilter the ruptures after sampling [demos]
    Former-commit-id: 8e5285cc340e3972ed05c315649ba5753cea3da9 [formerly 8e5285cc340e3972ed05c315649ba5753cea3da9 [formerly 28e362823c1959c30cf502450cdbd783ff82733d]]
    Former-commit-id: a07af04920e48f36221e2e077c0fd108eb539655
    Former-commit-id: ee7f7f0d8d0db88e49caf0c79bd2aa37e14e24b0

--- openquake/hazardlib/calc/filters.py
+++ openquake/hazardlib/calc/filters.py
@@ -338,9 +338,14 @@ class SourceFilter(object):
            the site indices within the bounding box enlarged by the integration
            distance for the given TRT and magnitude
         """
-        if not self.integration_distance:  # do not filter
+        if self.sitecol is None:
+            return []
+        elif not self.integration_distance:  # do not filter
             return self.sitecol.sids
-        bbox = rec['minlon'], rec['minlat'], rec['maxlon'], rec['maxlat']
+        if hasattr(rec, 'dtype'):
+            bbox = rec['minlon'], rec['minlat'], rec['maxlon'], rec['maxlat']
+        else:
+            bbox = rec  # assume it is a 4-tuple
         maxdist = self.integration_distance(trt, mag)
         a1 = min(maxdist * KM_TO_DEGREES, 90)
         a2 = min(angular_distance(maxdist, bbox[1], bbox[3]), 180)
