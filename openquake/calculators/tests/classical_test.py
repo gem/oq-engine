53142a6a5992db29aa50edad61626cb5aafdb2b7
klunk386 <poggi.valerio@gmail.com>
2019-02-04 14:20:08 +0100

    conflict resolved
    Former-commit-id: e92c5d92373e3c1cd7b802a57fac5ea51b583349 [formerly b53d1d9cdc5ae07d81568e564120b1468942ac21]
    Former-commit-id: e56dfd4678890210f10b58f4cdfd6fd75e571572

--- openquake/calculators/tests/classical_test.py
+++ openquake/calculators/tests/classical_test.py
@@ -31,7 +31,7 @@ from openquake.qa_tests_data.classical import (
     case_10, case_11, case_12, case_13, case_14, case_15, case_16, case_17,
     case_18, case_19, case_20, case_21, case_22, case_23, case_24, case_25,
     case_26, case_27, case_28, case_29, case_30, case_31, case_32, case_33,
-    case_34, case_35)
+    case_34, case_35, case_36)
 
 
 class ClassicalTestCase(CalculatorTestCase):
@@ -512,4 +512,10 @@ hazard_uhs-std.csv
     def test_case_35(self):
         # cluster
         self.assert_curves_ok(['hazard_curve-rlz-000-PGA.csv'],
-                               case_35.__file__)
+                              case_35.__file__)
+
+    @attr('qa', 'hazard', 'classical')
+    def test_case_36(self):
+        # test with advanced applyToSources and preclassical
+        self.run_calc(case_36.__file__, 'job.ini')
+
