3075db05525e0101b063e667b9e913a3945b2e21
Marco <pgnmrc69@gmail.com>
2019-02-15 14:40:06 +0100

    Fix bug
    Former-commit-id: 5e69f4175094acf9cdc14e4c84b8ccafdc3bc3ee [formerly 5e69f4175094acf9cdc14e4c84b8ccafdc3bc3ee [formerly 58a26db88a0be8e084e56fb9637f7295f411200c]]
    Former-commit-id: 43e6399108065e076516abf545a2bd232b290756
    Former-commit-id: 2f1756eecf89c1f1c6eee16ba0a206421f823a84

--- openquake/hazardlib/tests/source/base_test.py
+++ openquake/hazardlib/tests/source/base_test.py
@@ -90,5 +90,6 @@ class GenerateOneRuptureTestCase(unittest.TestCase):
         groups = nrml.to_python(source_model, SourceConverter(
             investigation_time=50., rupture_mesh_spacing=2.))
         src = groups[0].sources[0]
+        src.seed = 0
         rup = src.get_one_rupture()
-        self.assertEqual(rup.mag, 5.0)
+        self.assertEqual(rup.mag, 5.2)
