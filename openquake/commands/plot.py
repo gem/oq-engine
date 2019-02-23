476696552af75ce1be3b949c6fa2a7735ea86b3f
Michele Simionato <michele.simionato@gmail.com>
2019-02-23 07:06:06 +0100

    [skip hazardlib][demos]
    Former-commit-id: 0cf2bf6fe9812aa7b22520dc1a04160f9130bd26 [formerly 0cf2bf6fe9812aa7b22520dc1a04160f9130bd26 [formerly a557466b2df4db8f4e2e3f22ceb1ca7a683b3724]]
    Former-commit-id: 54da9741fce1356b25a897d9edb4eebe944af1a9
    Former-commit-id: c48b2e964f8d25065fbf0b6bd14ee11f41f82c45

--- openquake/commands/plot.py
+++ openquake/commands/plot.py
@@ -16,21 +16,28 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake. If not, see <http://www.gnu.org/licenses/>.
 from openquake.baselib import sap, general
-from openquake.calculators.extract import Extractor, WebExtractor
+from openquake.calculators.extract import Extractor, WebExtractor, parse
 
 
-def make_figure(site, inv_time, imtls, spec_curves, other_curves=(), label=''):
+def basemap(projection, lons, lats):
+    from mpl_toolkits.basemap import Basemap  # costly import
+    bmap = Basemap(projection=projection,
+                   llcrnrlon=lons.min() - 1, llcrnrlat=lats.min() - 1,
+                   urcrnrlon=lons.max() + 1, urcrnrlat=lats.max() + 1,
+                   lat_0=lats.mean(), lon_0=lons.mean())
+    bmap.drawcoastlines()
+    return bmap
+
+
+def make_figure_hcurves(site, inv_time, imtls, curves):
     """
     :param site: ID of the site to plot
     :param inv_time: investigation time
     :param imtls: ordered dictionary with the IMTs and levels
-    :param spec_curves: a dictionary of curves sid -> levels
-    :param other_curves: dictionaries sid -> levels
-    :param label: the label associated to `spec_curves`
+    :param curves: a dictionary of curves sid -> levels
     """
     # NB: matplotlib is imported inside since it is a costly import
     import matplotlib.pyplot as plt
-
     fig = plt.figure()
     n_imts = len(imtls)
     for j, imt in enumerate(imtls):
@@ -38,22 +45,86 @@ def make_figure(site, inv_time, imtls, spec_curves, other_curves=(), label=''):
         imt_slice = imtls(imt)
         ax = fig.add_subplot(1, n_imts, j + 1)
         ax.grid(True)
-        ax.set_xlabel('site %d, %s, inv_time=%dy' % (site, imt, inv_time))
+        ax.set_xlabel('site %s, %s, inv_time=%dy' % (site, imt, inv_time))
         if j == 0:  # set Y label only on the leftmost graph
             ax.set_ylabel('PoE')
-        if spec_curves is not None:
-            ax.loglog(imls, spec_curves[0, imt_slice], '--', label=label)
-        for key, curves in other_curves.items():
-            ax.loglog(imls, curves[0, imt_slice], label=key)
+        for key, curve in curves.items():
+            ax.loglog(imls, curve[0, imt_slice], label=key)
         ax.legend()
     return plt
 
 
+def make_figure_uhs(site, oq, uhs_dict):
+    """
+    :param site: ID of the site under analysis
+    :param oq: instance of OqParam
+    :param uhs_dict: a dictionary of uniform hazard spectra
+    """
+    # NB: matplotlib is imported inside since it is a costly import
+    import matplotlib.pyplot as plt
+
+    fig = plt.figure()
+    n_poes = len(oq.poes)
+    periods = [imt.period for imt in oq.imt_periods()]
+    for j, poe in enumerate(oq.poes):
+        ax = fig.add_subplot(1, n_poes, j + 1)
+        ax.grid(True)
+        ax.set_xlim([periods[0], periods[-1]])
+        ax.set_xlabel(
+            'UHS on site %s, poe=%s\nperiod in seconds, inv_time=%dy' %
+            (site, poe, oq.investigation_time))
+        if j == 0:  # set Y label only on the leftmost graph
+            ax.set_ylabel('SA')
+        for kind, uhs in uhs_dict.items():
+            ax.plot(periods, uhs[0, :, j], label=kind)
+    plt.legend()
+    return plt
+
+
+def make_figure_hmaps(lons, lats, itime, imt, imls, poes, hmaps):
+    """
+    :param lons: site longitudes
+    :param lats: site latitudes
+    :param itime: investigation time
+    :param imt: intensity measure type
+    :param imls: intensity measure levels
+    :param poes: PoEs used to compute the hazard maps
+    :param hmaps: mean hazard maps as an array of shape (N, M, P)
+    """
+    # NB: matplotlib is imported inside since it is a costly import
+    import matplotlib.pyplot as plt
+    fig = plt.figure()
+    n_poes = len(poes)
+    i = 0
+    for j, poe in enumerate(poes):
+        ax = fig.add_subplot(1, n_poes, i * n_poes + j + 1)
+        ax.grid(True)
+        ax.set_xlabel('hmap for IMT=%s, poe=%s\ninv_time=%dy' %
+                      (imt, poe, itime))
+        bmap = basemap('cyl', lons, lats)
+        bmap.scatter(lons, lats, c=hmaps[:, 0, j], cmap='jet')
+    return plt
+
+
 @sap.Script
-def plot(imt, calc_id=-1, other_id=None, site=0, webapi=False):
+def plot(what, calc_id=-1, other_id=None, webapi=False):
     """
-    Hazard curves plotter.
+    Hazard curves plotter. Here is an example of use:
+
+    $ oq plot 'hcurves?kind=mean&imt=PGA&site_id=0' -1 -2
     """
+    if '?' not in what:
+        raise SystemExit('Missing ? in %r' % what)
+    prefix, rest = what.split('?', 1)
+    assert prefix in 'hcurves hmaps uhs', prefix
+    if prefix in 'hcurves hmaps' and 'imt=' not in rest:
+        raise SystemExit('Missing imt= in %r' % what)
+    if prefix in 'hcurves uhs' and 'site_id=' not in rest:
+        rest += '&site_id=0'
+    if prefix == 'hmaps' and 'kind=' not in rest:
+        rest += '&kind=mean'
+    if prefix == 'uhs' and 'imt=' in rest:
+        raise SystemExit('Invalid IMT in %r' % what)
     if webapi:
         x1 = WebExtractor(calc_id)
         x2 = WebExtractor(other_id) if other_id else None
@@ -61,28 +132,40 @@ def plot(imt, calc_id=-1, other_id=None, site=0, webapi=False):
         x1 = Extractor(calc_id)
         x2 = Extractor(other_id) if other_id else None
     oq = x1.oqparam
-    stats = dict(oq.hazard_stats())
-    imls = oq.imtls[imt]
-    imtls = general.DictArray({imt: imls})
     itime = oq.investigation_time
-
-    if x2 is None:
-        stats.pop('mean')
-        mean_curves = x1.get('hcurves?kind=mean&imt=%s&site_id=%d' %
-                             (imt, site))
-        others = {
-            stat: x1.get('hcurves/%s?imt=%s&site_id=%d' % (stat, imt, site))
+    stats = dict(oq.hazard_stats())
+    suffix, index, qdict = parse(rest, stats)
+    if prefix == 'hcurves':
+        [site] = qdict['site_id']
+        imtls = general.DictArray({imt: oq.imtls[imt] for imt in qdict['imt']})
+        curves = {
+            stat: x1.get('hcurves?%s&kind=%s' % (rest, stat))
             for stat in stats}
-        plt = make_figure(site, itime, imtls, mean_curves, others, 'mean')
-    else:
-        mean1 = x1.get('hcurves?kind=mean&imt=%s?site_id=%d' % (imt, site))
-        mean2 = x2.get('hcurves?kind=mean&imt=%s?site_id=%d' % (imt, site))
-        plt = make_figure(site, itime, imtls, mean1, [mean2], 'reference')
+        if x2 is not None:
+            for stat in stats:
+                curves['%s-%d' % (stat, other_id)] = x2.get(
+                    'hcurves?kind=%s&imt=%s&site_id=%d' % stat)
+        plt = make_figure_hcurves(site, itime, imtls, curves)
+    if prefix == 'hmaps':
+        [imt] = qdict['imt']
+        sitecol = x1.get('sitecol')
+        lons, lats = sitecol['lon'], sitecol['lat']
+        plt = make_figure_hmaps(lons, lats, oq.investigation_time,
+                                imt, oq.imtls[imt], oq.poes,
+                                x1.get('hmaps?' + rest))
+    elif prefix == 'uhs':
+        [site] = qdict['site_id']
+        uhs_dict = {stat: x1.get('uhs?kind=%s&site_id=%s' % (stat, site))
+                    for stat in stats}
+        if x2 is not None:
+            for stat in stats:
+                uhs_dict['%s-%d' % (stat, other_id)] = x2.get(
+                    'uhs?kind=%s&site_id=%s' % (stat, site))
+        plt = make_figure_uhs(site, oq, uhs_dict)
     plt.show()
 
 
-plot.arg('imt', 'intensity measure type')
+plot.arg('what', 'what to extract')
 plot.arg('calc_id', 'computation ID', type=int)
 plot.arg('other_id', 'ID of another computation', type=int)
-plot.opt('site', 'ID of the site to plot', type=int)
 plot.flg('webapi', 'if given, pass through the WebAPI')
