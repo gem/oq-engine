ab24729e7880d5adde87551b4ca965779f90ef59
Michele Simionato <michele.simionato@gmail.com>
2019-03-12 08:39:45 +0100

    [demos]
    Former-commit-id: 9ce7797de6148bde652a730e70febe9b6b2d96d0

--- bin/run-demos.sh
+++ bin/run-demos.sh
@@ -25,7 +25,6 @@ oq engine --eos -1 /tmp
 oq engine --run $1/hazard/LogicTreeCase3ClassicalPSHA/job.ini --reuse-hazard
 
 # do something with the generated data
-oq extract hazard/rlzs -1
 oq engine --lhc
 MPLBACKEND=Agg oq plot 'hcurves?kind=stats&imt=PGA' -1
 MPLBACKEND=Agg oq plot 'hmaps?kind=mean&imt=PGA' -1
