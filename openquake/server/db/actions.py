302311508d97f2822963820d20a588bf4c53729a
Michele Simionato <michele.simionato@gmail.com>
2019-04-02 12:07:07 +0200

    Exported Exposure+Risk for multi risk
    Former-commit-id: 463625efa159b8ff1dc34b74f97f94be7e475adc [formerly 83d72748f65bb317a2543a155fc5fa4b9fbc4229]
    Former-commit-id: a75fab93e7c4ecde740badaaf50d1c2cda78fbac

--- openquake/server/db/actions.py
+++ openquake/server/db/actions.py
@@ -250,6 +250,7 @@ def get_outputs(db, job_id):
 
 
 DISPLAY_NAME = {
+    'asset_risk': 'Exposure + Risk',
     'gmf_data': 'Ground Motion Fields',
     'dmg_by_asset': 'Average Asset Damages',
     'dmg_by_event': 'Aggregate Event Damages',
