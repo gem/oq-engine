4b4aa160e2ba6d3c1204c85a9b34268d7ee98f16
Michele Simionato <michele.simionato@gmail.com>
2019-04-04 06:59:16 +0200

    Fixed tests [skip hazardlib]

--- openquake/calculators/tests/multi_risk_test.py
+++ openquake/calculators/tests/multi_risk_test.py
@@ -15,11 +15,11 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake. If not, see <http://www.gnu.org/licenses/>.
-import unittest
 import numpy
 from openquake.qa_tests_data.multi_risk import case_1
 from openquake.calculators.tests import CalculatorTestCase
 from openquake.calculators.export import export
+from openquake.calculators.extract import extract
 
 aae = numpy.testing.assert_almost_equal
 
@@ -29,38 +29,34 @@ class MultiRiskTestCase(CalculatorTestCase):
     def test_case_1(self):
         # case with volcanic multiperil ASH, LAVA, LAHAR, PYRO
         self.run_calc(case_1.__file__, 'job.ini')
-        raise unittest.SkipTest
-        [fname] = export(('dmg_by_asset', 'csv'), self.calc.datastore)
-        self.assertEqualFiles('expected/dmg_by_asset.csv', fname)
-        fnames = export(('gmf_data', 'csv'), self.calc.datastore)
-        self.assertEqual(len(fnames), 2)  # gmfs and sites, no sigma_epsilon
 
-        [fname] = export(('losses_by_asset', 'csv'), self.calc.datastore)
-        self.assertEqualFiles('expected/losses_by_asset.csv', fname)
+        [fname] = export(('asset_risk', 'csv'), self.calc.datastore)
+        self.assertEqualFiles('expected/asset_risk.csv', fname)
 
-        w = 'asset_risk?tag=name_1&tag=name_2'
-        [fname] = export(('aggregate_by/' + w, 'csv'), self.calc.datastore)
-        self.assertEqualFiles('expected/lahars_by_name_12.csv', fname)
-
-        w = 'asset_risk?kind=rlz-3&tag=name_1&tag=name_2'
-        [fname] = export(('aggregate_by/' + w, 'csv'), self.calc.datastore)
-        self.assertEqualFiles('expected/pyro_by_name_12.csv', fname)
+        # TODO: check extract
 
     def test_case_1_bis(self):
         # case with volcanic lava
         self.run_calc(case_1.__file__, 'job.ini',
                       multi_risk_csv="{'LAVA': 'lava_flow.csv'}")
-        raise unittest.SkipTest
-        w = 'asset_risk?tag=name_1&tag=name_2'
-        [fname] = export(('aggregate_by/' + w, 'csv'), self.calc.datastore)
-        self.assertEqualFiles('expected/lava_by_name_12.csv', fname)
 
-        # check invalid key structura_fragility_file
+        # TODO: check extract
+
+        # check invalid key structural_fragility_file
         with self.assertRaises(ValueError):
             self.run_calc(case_1.__file__, 'job.ini',
                           structura_fragility_file='fragility_model.xml')
 
-        # check invalid key structura_consequence_file
+        # check invalid key structural_consequence_file
         with self.assertRaises(ValueError):
             self.run_calc(case_1.__file__, 'job.ini',
                           structura_consequence_file='consequence_model.xml')
+
+    def test_case_2(self):
+        # case with two damage states
+        self.run_calc(case_1.__file__, 'job_2.ini')
+
+        [fname] = export(('asset_risk', 'csv'), self.calc.datastore)
+        self.assertEqualFiles('expected/asset_risk_2.csv', fname)
+
+        # TODO: check extract
