1c23ab2cfd3496d5da9a3e0a8f0577f8912ad5b2
Michele Simionato <michele.simionato@gmail.com>
2019-04-11 13:13:56 +0200

    [skip hazardlib]

--- openquake/calculators/tests/classical_risk_test.py
+++ openquake/calculators/tests/classical_risk_test.py
@@ -30,7 +30,7 @@ class ClassicalRiskTestCase(CalculatorTestCase):
         self.run_calc(case_1.__file__, 'job_risk.ini', exports='csv')
 
         # check loss ratios
-        lrs = self.calc.datastore['vulnerability/VF/structural']
+        lrs = self.calc.datastore['vulnerability/VF-vulnerability/structural']
         got = scientificformat(lrs.mean_loss_ratios, '%.2f')
         self.assertEqual(got, '0.05 0.10 0.20 0.40 0.80')
 
