e530b659431900c0107d7390358104321d1c1e6d
Michele Simionato <michele.simionato@gmail.com>
2019-04-12 15:54:16 +0200

    [skip hazardlib]
    Former-commit-id: dc9e56a45a409b93989be0b70d38f676f3bc6417 [formerly 11c7318e17c7afcb11c17cdcd0a03c003d33b40b] [formerly 11c7318e17c7afcb11c17cdcd0a03c003d33b40b [formerly b7f1332f8b2359011958087c6fcbd4505e7194eb]]
    Former-commit-id: 5605cb47e6369b2d80ce0cf1233cc405edbff09a
    Former-commit-id: 1ec8129714a8b943c33d9d831de48f6ac9133fc8

--- openquake/calculators/ebrisk.py
+++ openquake/calculators/ebrisk.py
@@ -101,7 +101,7 @@ def ebrisk(rupgetter, srcfilter, param, monitor):
         eidx = [eid2idx[eid] for eid in haz['eid']]
         haz['eid'] = eidx
         with mon_risk:
-            out = riskmodel.get_output(assets_by_taxo, haz, rupgetter.eps)
+            out = riskmodel.get_output(assets_by_taxo, haz, eps=())
         with mon_agg:
             for a, asset in enumerate(assets_on_sid):
                 aid = asset['ordinal']
