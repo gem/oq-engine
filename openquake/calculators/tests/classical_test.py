50379915fc55af673ce6654b60fbd7ce0cf77d4f
Michele Simionato <michele.simionato@gmail.com>
2019-04-15 15:34:23 +0200

    Raised a NotImplementedError for IMT-dependend weights + sampling
    Former-commit-id: 9738443fb9301117d197e127a3e3611521f8344c [formerly d40bd7f5b0f450ab5bfc42007d9a9b6e4324f8cf] [formerly d40bd7f5b0f450ab5bfc42007d9a9b6e4324f8cf [formerly 40634c0a4562ed1851a617f23a24a1a0fa4b777a]]
    Former-commit-id: b58610aa55db9d0d086a08d6f0ffa862671aa69f
    Former-commit-id: abed61baaf6b5a3c2a741d5675c256ec5fcd8b2e

--- openquake/calculators/tests/classical_test.py
+++ openquake/calculators/tests/classical_test.py
@@ -452,9 +452,10 @@ hazard_uhs-std.csv
             numpy.testing.assert_equal(best_rlz, [2, 9, 2, 3, 1])
 
     def test_case_30_sampling(self):
-        self.assert_curves_ok(
-            ['hcurve-PGA.csv', 'hcurve-SA(1.0).csv'],
-            case_30.__file__, number_of_logic_tree_samples='10')
+        with self.assertRaises(NotImplementedError):
+            self.assert_curves_ok(
+                ['hcurve-PGA.csv', 'hcurve-SA(1.0).csv'],
+                case_30.__file__, number_of_logic_tree_samples='10')
 
     def test_case_31(self):
         # source specific logic tree
