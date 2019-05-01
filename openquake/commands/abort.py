9febf494a2cc2511df9c1903011682a2985988bf
Michele Simionato <michele.simionato@gmail.com>
2019-05-01 09:28:41 +0200

    Do not manage SIGTERM anymore [skip hazardlib][demos]
    Former-commit-id: f6099a61166789ee2a9050c1143f81645da6d3a7

--- openquake/commands/abort.py
+++ openquake/commands/abort.py
@@ -39,7 +39,7 @@ def abort(job_id):
     for p in psutil.process_iter():
         if p.name() == name:
             try:
-                os.kill(p.pid, signal.SIGTERM)
+                os.kill(p.pid, signal.SIGINT)
                 logs.dbcmd('set_status', job.id, 'aborted')
                 print('Job %d aborted' % job.id)
             except Exception as exc:
