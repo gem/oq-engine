fff879ff6f544f18211352c40dc17006f2c680b4
g-weatherill <gaweatherill82@yahoo.co.uk>
2019-05-06 17:30:35 +0200

    Fixes typo in test case
    Former-commit-id: 6dc322ca7e41105befd353f7cdaa9e7beb3e291f

--- openquake/calculators/tests/classical_test.py
+++ openquake/calculators/tests/classical_test.py
@@ -30,7 +30,7 @@ from openquake.qa_tests_data.classical import (
     case_10, case_11, case_12, case_13, case_14, case_15, case_16, case_17,
     case_18, case_19, case_20, case_21, case_22, case_23, case_24, case_25,
     case_26, case_27, case_28, case_29, case_30, case_31, case_32, case_33,
-    case_34, case_35, case_36, case_37)
+    case_34, case_35, case_36, case_37, case_38)
 
 
 class ClassicalTestCase(CalculatorTestCase):
@@ -492,7 +492,7 @@ hazard_uhs-std.csv
 
     def test_case_38(self):
         # BC Hydro GMPEs with epistemic adjustments
-        selfl.assert_curves_ok(["hazard_curve-mean-PGA.csv",
-                                "quantile_curve-0.5-PGA.csv",
-                                "quantile_curve-0.16-PGA.csv",
-                                "quantile_curve-0.84-PGA.csv"], case_38.__file)
+        self.assert_curves_ok(["hazard_curve-mean-PGA.csv",
+                               "quantile_curve-0.5-PGA.csv",
+                               "quantile_curve-0.16-PGA.csv",
+                               "quantile_curve-0.84-PGA.csv"], case_38.__file)
