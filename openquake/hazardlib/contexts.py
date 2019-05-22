c17ba3e5d53c1cb80355b6c63c8d40ef29cb3aed
Michele Simionato <michele.simionato@gmail.com>
2019-05-22 06:30:09 +0200

    Added comments [skip CI]
    Former-commit-id: b89d13d67b73bd2f6c76314dc1e90c1507706da4 [formerly cab5ae3d78c3e1da0465e053836401a0e16488ba]
    Former-commit-id: dec93c7d02586433d4eff5ca5b9784fff4a0cf73

--- openquake/hazardlib/contexts.py
+++ openquake/hazardlib/contexts.py
@@ -315,7 +315,8 @@ class ContextMaker(object):
             for imt in imtls:
                 slc = imtls(imt)
                 if hasattr(gsim, 'weight') and gsim.weight[imt] == 0:
-                    # ignore the gsim
+                    # set by the engine when parsing the gsim logictree;
+                    # when 0 ignore the gsim: see _build_trts_branches
                     pno = numpy.ones((nsites, slc.stop - slc.start))
                 else:
                     poes = gsim.get_poes(
@@ -449,6 +450,7 @@ class DistancesContext(BaseContext):
             if small_distances.any():
                 array = numpy.array(array)  # make a copy first
                 array[small_distances] = minimum_distance
+                array.flags.writeable = False
             setattr(ctx, dist, array)
         return ctx
 
