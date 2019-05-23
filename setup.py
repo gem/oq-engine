a60664f11597cb13661cce2070e58467c5821c39
Daniele Viganò <daniele@vigano.me>
2019-05-23 10:21:59 +0200

    Add pytest to dev requirements
    Former-commit-id: 574697bcd8f095e604790fbc7851620ecd0b932d

--- setup.py
+++ setup.py
@@ -84,7 +84,8 @@ extras_require = {
         'pyproj >=1.9',
     ],
     'dev':  [
-        'nose >=1.3, <1.4',
+        'nose >=1.3',
+        'pytest >=4.5',
         'flake8 >=3.5, <3.8',
         'pdbpp',
         'ipython',
