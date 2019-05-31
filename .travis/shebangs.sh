0c0f7ed37c8491af6d1011db2c03a2dc206cdfc2
Daniele Viganò <daniele@vigano.me>
2019-05-31 10:48:19 +0200

    Improve a check [skip ci]

--- .travis/shebangs.sh
+++ .travis/shebangs.sh
@@ -20,4 +20,4 @@
 
 checkcmd find
 
-find . -not -path '*/\.*' -type f -executable -exec grep -lE '#!.*env python$|python2' '{}' \; | grep -E '.'; test $? -eq 1
+find . -not -path '*/\.*' -type f -executable -exec grep -lE '#!.*python$|#!.*python2.*$' '{}' \; | grep -E '.'; test $? -eq 1
