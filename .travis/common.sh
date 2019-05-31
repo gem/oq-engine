0c36b6f917a66f720685296e1f29440d86a1cc55
Daniele Viganò <daniele@vigano.me>
2019-05-31 10:40:20 +0200

    Add copyright statements [skip ci]

--- .travis/common.sh
+++ .travis/common.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+#
+# common.sh  Copyright (C) 2019 GEM Foundation
+#
+# OpenQuake is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# OpenQuake is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>
+#
 # This file is meant to be sourced
 
 checkcmd() {
