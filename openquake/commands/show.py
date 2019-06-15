135f9e446f0caa101704f4f75550b55ae93b4343
Michele Simionato <michele.simionato@gmail.com>
2019-06-15 04:25:59 +0200

    [skip hazardlib][demos]
    Former-commit-id: ad4303000f68d9fe16aa531f9fcd5d6fccc4cdcd

--- openquake/commands/show.py
+++ openquake/commands/show.py
@@ -30,18 +30,6 @@ from openquake.calculators.views import view
 from openquake.calculators.extract import extract
 
 
-def get_hcurves_and_means(dstore):
-    """
-    Extract hcurves from the datastore and compute their means.
-
-    :returns: curves_by_rlz, mean_curves
-    """
-    rlzs_assoc = dstore['csm_info'].get_rlzs_assoc()
-    getter = getters.PmapGetter(dstore, rlzs_assoc)
-    pmaps = getter.get_pmaps()
-    return dict(zip(getter.rlzs, pmaps)), dstore['hcurves/mean']
-
-
 def str_or_int(calc_id):
     try:
         return int(calc_id)
@@ -79,21 +67,7 @@ def show(what='contents', calc_id=-1, extra=()):
     ds = util.read(calc_id)
 
     # this part is experimental
-    if what == 'rlzs' and 'poes' in ds:
-        min_value = 0.01  # used in rmsep
-        getter = getters.PmapGetter(ds)
-        pmaps = getter.get_pmaps()
-        weights = [rlz.weight for rlz in getter.rlzs]
-        mean = stats.compute_pmap_stats(
-            pmaps, [numpy.mean], weights, getter.imtls)
-        dists = []
-        for rlz, pmap in zip(getter.rlzs, pmaps):
-            dist = util.rmsep(mean.array, pmap.array, min_value)
-            dists.append((dist, rlz))
-        print('Realizations in order of distance from the mean curves')
-        for dist, rlz in sorted(dists):
-            print('%s: rmsep=%s' % (rlz, dist))
-    elif view.keyfunc(what) in view:
+    if view.keyfunc(what) in view:
         print(view(what, ds))
     elif what.split('/', 1)[0] in extract:
         print(extract(ds, what, *extra))
