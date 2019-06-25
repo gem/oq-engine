c3e3800235cf2e0648e03e329eecfd8c4fd10557
Michele Simionato <michele.simionato@gmail.com>
2019-06-25 09:31:26 +0200

    Small cleanup of oq check_input [skip CI]
    Former-commit-id: feb23f18b9173b3631e33fb4f8219a1218d9b41b [formerly 40ce76b93d306a4784bc29c189434190ef16e9ea]
    Former-commit-id: fa5344f5a795aec605d77b6326efce5180090267

--- openquake/commands/check_input.py
+++ openquake/commands/check_input.py
@@ -15,6 +15,7 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake. If not, see <http://www.gnu.org/licenses/>.
+import sys
 from openquake.baselib import sap
 from openquake.commonlib import readinput
 from openquake.calculators import base
@@ -25,7 +26,10 @@ from openquake.risklib import read_nrml  # this is necessary
 @sap.script
 def check_input(job_ini_or_zip_or_nrml):
     if job_ini_or_zip_or_nrml.endswith('.xml'):
-        print(nrml.to_python(job_ini_or_zip_or_nrml))
+        try:
+            print(nrml.to_python(job_ini_or_zip_or_nrml))
+        except Exception as exc:
+            sys.exit(exc)
     else:
         calc = base.calculators(readinput.get_oqparam(job_ini_or_zip_or_nrml))
         calc.read_inputs()
