0da7b9487f891ef22957ca5ef8c8cb8fe6574c5a
Michele Simionato <michele.simionato@gmail.com>
2019-07-08 09:47:50 +0200

    Added a default of 1 to taxonomy_mapping[weight] [skip hazardlib]
    Former-commit-id: ce7db0c78afc164c0987563851af7355a79e304b [formerly ce7db0c78afc164c0987563851af7355a79e304b [formerly b9e1d17730a58ee6d4f185de29f7acfcd7eae0c1]]
    Former-commit-id: 1f3cf99244f4ca95e5ed447f8a8399dabb1cd65b
    Former-commit-id: 8c92358ff3770f6d2487962a0cbabb10b21f804c

--- openquake/baselib/general.py
+++ openquake/baselib/general.py
@@ -1158,3 +1158,25 @@ def getsizeof(o, ids=None):
         return nbytes + sum(getsizeof(x, ids) for x in o)
 
     return nbytes
+
+
+def add_defaults(array, **kw):
+    """
+    :param array: a structured array
+    :param kw: a dictionary field name -> default value
+    :returns: a new array with additional fields with default values
+    """
+    dtlist = [(name, array.dtype[name]) for name in array.dtype.names]
+    for k, v in kw.items():
+        if k not in array.dtype.names:
+            dtlist.append((k, type(v)))
+    tuples = []
+    for rec in array:
+        lst = []
+        for name in array.dtype.names:
+            lst.append(rec[name])
+        for k, v in kw.items():
+            if k not in array.dtype.names:
+                lst.append(v)
+        tuples.append(tuple(lst))
+    return numpy.array(tuples, dtlist)
