3cfb693308b8ca73ffc51154553437d81e9f5108
Michele Simionato <michele.simionato@gmail.com>
2019-07-17 10:06:03 +0200

    [demos][skip hazardlib]
    Former-commit-id: 243424a66b8f59422c015d42bd0cf3fb90c9f498

--- openquake/commands/engine.py
+++ openquake/commands/engine.py
@@ -19,7 +19,8 @@ import os
 import sys
 import getpass
 import logging
-from openquake.baselib import sap, config, datastore
+from openquake.baselib import (
+    sap, config, datastore, workerpool as w)
 from openquake.baselib.general import safeprint
 from openquake.hazardlib import valid
 from openquake.commonlib import logs, readinput
@@ -33,6 +34,8 @@ from openquake.commands.abort import abort
 
 HAZARD_CALCULATION_ARG = "--hazard-calculation-id"
 MISSING_HAZARD_MSG = "Please specify '%s=<id>'" % HAZARD_CALCULATION_ARG
+ZMQ = os.environ.get(
+    'OQ_DISTRIBUTE', config.distribution.oq_distribute) == 'zmq'
 
 
 def get_job_id(job_id, username=None):
@@ -60,6 +63,9 @@ def run_job(job_ini, log_level='info', log_file=None, exports='',
     :param kw:
         Extra parameters like hazard_calculation_id and calculation_mode
     """
+    if ZMQ:  # start zworkers
+        master = w.WorkerMaster(config.dbserver.host, **config.zworkers)
+        logging.warning(master.start())
     job_id = logs.init('job', getattr(logging, log_level.upper()))
     with logs.handle(job_id, log_level, log_file):
         job_ini = os.path.abspath(job_ini)
@@ -68,6 +74,8 @@ def run_job(job_ini, log_level='info', log_file=None, exports='',
         eng.run_calc(job_id, oqparam, exports, **kw)
         for line in logs.dbcmd('list_outputs', job_id, False):
             safeprint(line)
+    if ZMQ:  # stop zworkers
+        logging.warning(master.stop())
     return job_id
 
 
