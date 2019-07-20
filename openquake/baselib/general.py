43449539d23a29fe536d8f77e1f88df7645908be
Michele Simionato <michele.simionato@gmail.com>
2019-07-20 09:24:55 +0200

    Removed unused function [demos]
    Former-commit-id: 562a392078160f70c45e18001743d5e98c947579 [formerly 423bb4d361e28fce804bc85bb37cbbec744ab0c9] [formerly 423bb4d361e28fce804bc85bb37cbbec744ab0c9 [formerly 051196256884174c5f966a1e7c1a78cf6ffd791e]]
    Former-commit-id: 863a85a44af24ba8af94d33f2b8e01b34c6e5ec5
    Former-commit-id: 4f0623c0b9f02743ce192ccf4d144c02c9e6d5a3

--- openquake/baselib/general.py
+++ openquake/baselib/general.py
@@ -501,25 +501,6 @@ def assert_independent(package, *packages):
                 raise CodeDependencyError('%s depends on %s' % (package, pkg))
 
 
-def search_module(module, syspath=sys.path):
-    """
-    Given a module name (possibly with dots) returns the corresponding
-    filepath, or None, if the module cannot be found.
-
-    :param module: (dotted) name of the Python module to look for
-    :param syspath: a list of directories to search (default sys.path)
-    """
-    lst = module.split(".")
-    pkg, submodule = lst[0], ".".join(lst[1:])
-    try:
-        fileobj, filepath, descr = imp.find_module(pkg, syspath)
-    except ImportError:
-        return
-    if submodule:  # recursive search
-        return search_module(submodule, [filepath])
-    return filepath
-
-
 class CallableDict(dict):
     r"""
     A callable object built on top of a dictionary of functions, used
