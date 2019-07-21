c0d80ee3f868302032cea17fe81a18cbd6925cd5
Michele Simionato <michele.simionato@gmail.com>
2019-07-21 18:23:38 +0200

    Fixed gen_slices

--- openquake/baselib/general.py
+++ openquake/baselib/general.py
@@ -269,16 +269,19 @@ def split_in_slices(number, num_slices):
     return slices
 
 
-def gen_slices(n, block_size):
+def gen_slices(start, stop, blocksize):
     """
-    Yields slices of lenght at most block_size
+    Yields slices of lenght at most block_size.
+
+    >>> list(gen_slices(1, 6, 2))
+    [slice(1, 3, None), slice(3, 5, None), slice(5, 6, None)]
     """
-    start = 0
+    assert start <= stop, (start, stop)
+    assert blocksize > 0, blocksize
     while True:
-        stop = start + block_size
-        yield slice(start, min(stop, n))
-        start = stop
-        if start >= n:
+        yield slice(start, min(start + blocksize, stop))
+        start += blocksize
+        if start >= stop:
             break
 
 
