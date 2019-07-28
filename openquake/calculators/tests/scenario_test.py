94b95595a27272a0e987ee9faf357070c721b15d
Michele Simionato <michele.simionato@gmail.com>
2019-07-28 05:53:46 +0200

    Removed some experimental code [demos]
    Former-commit-id: 0d7e32970d34135e3f2afcccc846a890540d1a29 [formerly 65b26d5c40de73ce2136f4ee0d2195ff1284111b] [formerly 65b26d5c40de73ce2136f4ee0d2195ff1284111b [formerly 00eb513d806444d5e7d3877db14fe0d8ccda95de]]
    Former-commit-id: f7625d0b7af15b9ef91d01571b250f9a052278f6
    Former-commit-id: 074536fd0fef4fa2f2b9cbd24831e75c73490f85

--- openquake/calculators/tests/scenario_test.py
+++ openquake/calculators/tests/scenario_test.py
@@ -20,10 +20,9 @@ from numpy.testing import assert_almost_equal as aae
 
 from openquake.qa_tests_data.scenario import (
     case_1, case_2, case_3, case_4, case_5, case_6, case_7, case_8,
-    case_9, case_10, case_11)
+    case_9, case_10)
 from openquake.calculators.export import export
 from openquake.calculators.tests import CalculatorTestCase
-from openquake.hazardlib.contexts import ContextMaker
 
 
 def count_close(gmf_value, gmvs_site_one, gmvs_site_two, delta=0.1):
@@ -132,19 +131,3 @@ class ScenarioTestCase(CalculatorTestCase):
         # test importing an exposure with automatic gridding
         self.run_calc(case_10.__file__, 'job.ini')
         self.assertEqual(len(self.calc.datastore['sitecol']), 66)
-
-    def test_case_11(self):
-        # compute the limit distances for a GMV of 0.01g
-        self.run_calc(case_11.__file__, 'job.ini')
-        self.calc.rup.tectonic_region_type = 'Subduction Deep'
-        oq = self.calc.oqparam
-        gsim_lt = self.calc.datastore['csm_info/gsim_lt']
-        mags = {'Subduction Deep': [self.calc.rup.mag]}
-        dist = gsim_lt.get_integration_distance(mags, oq)
-        aae(dist['Subduction Deep'], 162.24509294)
-
-        cmaker = ContextMaker(
-            'Subduction Deep', self.calc.gsims, oq.maximum_distance)
-        dist = cmaker.get_limit_distance(
-            self.calc.sitecol, self.calc.rup, oq.imtls, oq.minimum_intensity)
-        aae(dist, 163.5109114)
