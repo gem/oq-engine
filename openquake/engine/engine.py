ede103064e4d52383b6015d2f05fa43435edc1a0
Michele Simionato <michele.simionato@gmail.com>
2019-07-29 09:46:23 +0200

    [skip CI]
    Former-commit-id: 7a0b1a2e201f4a71fa2037c996ad1912a6f66b2f [formerly 295ba41098113369bd7c6baacad7661f9927aa26]
    Former-commit-id: fcc9dd6a4e2303139416580b3141d87e6fc1aaef

--- openquake/engine/engine.py
+++ openquake/engine/engine.py
@@ -331,12 +331,12 @@ def run_calc(job_id, oqparam, exports, hazard_calculation_id=None, **kw):
             del data  # save memory
 
         poll_queue(job_id, _PID, poll_time=15)
-        if OQ_DISTRIBUTE.startswith(('celery', 'zmq')):
-            set_concurrent_tasks_default(job_id)
         if OQ_DISTRIBUTE == 'zmq':  # start zworkers
             master = w.WorkerMaster(config.dbserver.listen, **config.zworkers)
             logs.dbcmd('start_zworkers', master)
             logging.info('WorkerPool %s',  master.wait_pools(seconds=30))
+        if OQ_DISTRIBUTE.startswith(('celery', 'zmq')):
+            set_concurrent_tasks_default(job_id)
         t0 = time.time()
         calc.run(exports=exports,
                  hazard_calculation_id=hazard_calculation_id,
