d21b37dfe804729086d85cd7e9991bd17a7402a5
Michele Simionato <michele.simionato@gmail.com>
2019-07-30 14:17:40 +0200

    [skip CI]

--- openquake/calculators/tests/classical_test.py
+++ openquake/calculators/tests/classical_test.py
@@ -211,6 +211,7 @@ source_model     grp_id trt                  eff_ruptures tot_ruptures
 ================ ====== ==================== ============ ============
 simple_fault.xml 0      Active Shallow Crust 447          447         
 ================ ====== ==================== ============ ============""")
+
         # test classical
         self.assert_curves_ok([
             'hazard_curve-smltp_simple_fault-gsimltp_AbrahamsonSilva2008.csv',
