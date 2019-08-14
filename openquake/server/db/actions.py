c89b258babfc2aa3be7b7262d2bfa0669e8f567e
Anirudh Rao <rao.anirudh@gmail.com>
2019-08-14 10:27:47 +0200

    Remove a duplicate output display name
    `dmg_by_event` was repeated in the dict of display names; remove the second appearance.

--- openquake/server/db/actions.py
+++ openquake/server/db/actions.py
@@ -263,7 +263,6 @@ DISPLAY_NAME = {
     'events': 'Events',
     'damages-rlzs': 'Asset Damage Distribution',
     'damages-stats': 'Asset Damage Statistics',
-    'dmg_by_event': 'Aggregate Event Damages',
     'avg_losses': 'Average Asset Losses',
     'avg_losses-rlzs': 'Average Asset Losses',
     'avg_losses-stats': 'Average Asset Losses Statistics',
