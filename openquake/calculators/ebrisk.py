cbaf486e6e97b1bccbc9a344b2fab382cdaac530
Michele Simionato <michele.simionato@gmail.com>
2019-08-25 08:55:56 +0200

    [skip hazardlib]
    Former-commit-id: 14590ff91513738ccc8a05b0147b86d56a3fc67c [formerly 14590ff91513738ccc8a05b0147b86d56a3fc67c [formerly 68116c63236023061c9cc8b4fcce7f062d2fe946]]
    Former-commit-id: c79ba945f7b84a1dd9f40a561b520fe9301b5fa8
    Former-commit-id: 58556615740b4ec3362c1f01813b1dd28611c38d

--- openquake/calculators/ebrisk.py
+++ openquake/calculators/ebrisk.py
@@ -191,13 +191,18 @@ class EbriskCalculator(event_based.EventBasedCalculator):
         if parent:
             hdf5path = parent.filename
             grp_indices = parent['ruptures'].attrs['grp_indices']
+            nruptures = len(parent['ruptures'])
         else:
             hdf5path = self.datastore.hdf5cache()
             grp_indices = self.datastore['ruptures'].attrs['grp_indices']
+            nruptures = len(self.datastore['ruptures'])
             with hdf5.File(hdf5path, 'r+') as cache:
                 self.datastore.hdf5.copy('weights', cache)
                 self.datastore.hdf5.copy('ruptures', cache)
                 self.datastore.hdf5.copy('rupgeoms', cache)
+        ruptures_per_block = numpy.ceil(nruptures / (oq.concurrent_tasks or 1))
+        logging.info('Using %d ruptures per block (over %d)',
+                     ruptures_per_block, nruptures)
         self.set_param(
             maxweight=oq.ebrisk_maxweight,
             task_duration=oq.task_duration or 300,  # 5min
@@ -213,11 +218,13 @@ class EbriskCalculator(event_based.EventBasedCalculator):
         ngroups = 0
         for grp_id, rlzs_by_gsim in rlzs_by_gsim_grp.items():
             start, stop = grp_indices[grp_id]
-            indices = list(range(start, stop))
-            if indices:
-                ngroups += 1
+            if start == stop:  # no ruptures for the given grp_id
+                continue
+            ngroups += 1
+            for indices in general.block_splitter(
+                    range(start, stop), ruptures_per_block):
                 rgetter = getters.RuptureGetter(
-                    hdf5path, indices, grp_id,
+                    hdf5path, list(indices), grp_id,
                     trt_by_grp[grp_id], samples[grp_id], rlzs_by_gsim,
                     first_event)
                 first_event += rgetter.num_events
