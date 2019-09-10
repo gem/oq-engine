7864f51995e27f25e19692acc9a822da00b9214f
Michele Simionato <michele.simionato@gmail.com>
2019-09-10 04:06:51 +0200

    [skip hazardlib]
    Former-commit-id: e11426e8306e2127f51e8233c95037db76b0d480

--- openquake/commonlib/source.py
+++ openquake/commonlib/source.py
@@ -288,13 +288,6 @@ class CompositionInfo(object):
                 dic[src_group.id] = getattr(src_group, name)
         return dic
 
-    def _get_rlzs(self, smodel, gsim_lt, seed):
-        if self.num_samples:
-            rlzs = gsim_lt.sample(smodel.samples, seed)
-        else:  # full enumeration
-            rlzs = logictree.get_effective_rlzs(gsim_lt)
-        return rlzs
-
     def __repr__(self):
         info_by_model = {}
         for sm in self.source_models:
