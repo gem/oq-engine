7b4143a39707c8099142cb06c929e43d4bf9d504
Marco <pgnmrc69@gmail.com>
2019-09-13 18:06:45 +0200

    draft implementation

---
+++ openquake/hazardlib/tests/gsim/morikawa_fujiwara_2013_test.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+#
+# Copyright (C) 2019 GEM Foundation
+#
+# OpenQuake is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# OpenQuake is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with OpenQuake. If not, see <http://www.gnu.org/licenses/>.
+
+import numpy as np
+import unittest
+from openquake.hazardlib.imt import PGA
+from openquake.hazardlib.contexts import DistancesContext
+from openquake.hazardlib.tests.gsim.mgmpe.dummy import Dummy
+from openquake.hazardlib.gsim.morikawa_fujiwara_2013 import \
+        MorikawaFujiwara2013
+
+
+class MorikawaFujiwara2013Test(unittest.TestCase):
+    """
+    """
+
+    def test_instantiation(self):
+        dists = DistancesContext()
+        dists.rrup = np.array([10.])
+        imt = PGA()
+        mag = 6.0
+        rup = Dummy.get_rupture(mag=mag, hypo_depth=10.)
+        slen = len(dists.rrup)
+        sites = Dummy.get_site_collection(slen, vs30=760., z1pt4=100., xvf=10.)
+        gmm = MorikawaFujiwara2013(model='model1', region=None)
+        mean, _ = gmm.get_mean_std(sites, rup, dists, [imt])
