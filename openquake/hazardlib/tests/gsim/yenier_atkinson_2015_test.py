07ce04a51e1f8d6918736da3708f3e5158c7e70f
Kendra Johnson <kendra.johnson@globalquakemodel.org>
2019-09-16 11:00:21 +0200

    adding test table for yenier&atkinson2015

--- openquake/hazardlib/tests/gsim/yenier_atkinson_2015_test.py
+++ openquake/hazardlib/tests/gsim/yenier_atkinson_2015_test.py
@@ -16,48 +16,16 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake. If not, see <http://www.gnu.org/licenses/>.
 
-import numpy as np
-import unittest
-from openquake.hazardlib.imt import PGA
-from openquake.hazardlib.contexts import DistancesContext
-from openquake.hazardlib.tests.gsim.mgmpe.dummy import Dummy
 from openquake.hazardlib.gsim.yenier_atkinson_2015 import (
     YenierAtkinson2015BSSA)
+from openquake.hazardlib.tests.gsim.utils import BaseGSIMTestCase
 
 
-class YenierAtkinson2015BSSATest(unittest.TestCase):
+class YenierAtkinson2015BSSA(BaseGSIMTestCase):
 
-    """
-    def test_f_z(self):
-        mag = 6.0
-        rrup = [10., 50., 100.]
-        rrup = np.array([10.])
-        imt = PGA()
-        gmm = YenierAtkinson2015BSSA(focal_depth=10.)
-        computed = gmm._get_f_z(gmm.TAB2[imt], imt, rrup, mag)
-        expected = np.array([-3.537768])
-        np.testing.assert_allclose(computed, expected)
+    GSIM_CLASS = YenierAtkinson2015BSSA
 
-    def test_delta_sigma(self):
-        mag = 6.0
-        rrup = [10., 50., 100.]
-        rrup = np.array([10.])
-        imt = PGA()
-        gmm = YenierAtkinson2015BSSA(focal_depth=10.)
-        computed = gmm._get_delta_sigma(gmm.TAB2[imt], imt, mag)
-        expected = np.array([-3.537768])
-        print(computed)
-        np.testing.assert_allclose(computed, expected)
-    """
+    def test_mean(self):
+       self.check('YA15/ya15_mean_cena.csv',
+                   max_discrep_percentage=0.3)
 
-    def test_gm(self):
-        dists = DistancesContext()
-        dists.rrup = np.array([10.])
-        imt = PGA()
-        mag = 6.0
-        rup = Dummy.get_rupture(mag=mag)
-        slen = len(dists.rrup)
-        sites = Dummy.get_site_collection(slen, vs30=760.)
-        gmm = YenierAtkinson2015BSSA(focal_depth=10.)
-        mean, _ = gmm.get_mean_std(sites, rup, dists, [imt])
-        print('mean:', mean)
