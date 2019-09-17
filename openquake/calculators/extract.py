843373dba4b70cd465b9fd8799137c8ecc9107d7
Michele Simionato <michele.simionato@gmail.com>
2019-09-17 10:31:33 +0200

    Extended agg_curves to the case with tags [skip hazardlib]
    Former-commit-id: 48cad6c00f96fa16d08ded8d14d889f9f5c83168 [formerly 742464f662513e0972a8e0ed6684069fa08384fa]
    Former-commit-id: b85f582e85b82c35ca4d2f22a95469bc287e3abe

--- openquake/calculators/extract.py
+++ openquake/calculators/extract.py
@@ -530,32 +530,42 @@ def extract_agg_curves(dstore, what):
     qdic = parse(what, info)
     k = qdic['k']  # rlz or stat index
     [l] = qdic['loss_type']  # loss type index
-    if qdic['rlzs']:
-        kinds = ['rlz-%d' % r for r in k]
-        arr = dstore['agg_curves-rlzs'][:, k, l]  # shape P, T...
-        rps = dstore.get_attr('agg_curves-rlzs', 'return_periods')
-    else:
-        kinds = list(info['stats'])
-        arr = dstore['agg_curves-stats'][:, k, l]  # shape P, T...
-        rps = dstore.get_attr('agg_curves-stats', 'return_periods')
     tagnames = qdic.get('tagname', [])
     if set(tagnames) != set(info['tagnames']):
         raise ValueError('Expected tagnames=%s, got %s' %
                          (info['tagnames'], tagnames))
     tagvalues = qdic.get('tagvalue', [])
+    tagidx = []
+    if tagnames:
+        tagcol = dstore['assetcol/tagcol']
+        for tagname, tagvalue in zip(tagnames, tagvalues):
+            values = list(getattr(tagcol, tagname)[1:])
+            tagidx.append(values.index(tagvalue))
+    tup = tuple([slice(None), k, l] + tagidx)
+    if qdic['rlzs']:
+        kinds = ['rlz-%d' % r for r in k]
+        arr = dstore['agg_curves-rlzs'][tup]  # shape P, T...
+        rps = dstore.get_attr('agg_curves-rlzs', 'return_periods')
+    else:
+        kinds = list(info['stats'])
+        arr = dstore['agg_curves-stats'][tup]  # shape P, T...
+        rps = dstore.get_attr('agg_curves-stats', 'return_periods')
     if qdic['absolute'] == [1]:
         pass
     elif qdic['absolute'] == [0]:
         aggname = '_'.join(['agg'] + tagnames)
-        evalue = dstore['exposed_values/' + aggname][l]  # shape T...
+        tl = tuple(tagidx) + (l,)
+        evalue = dstore['exposed_values/' + aggname][tl]  # shape T...
         arr /= evalue
     else:
         raise ValueError('"absolute" must be 0 or 1 in %s' % what)
     attrs = dict(shape_descr=['return_period', 'kind'] + tagnames)
-    attrs['return_period'] = [numpy.nan] + list(rps)
-    attrs['kind'] = ['?'] + kinds
+    attrs['return_period'] = list(rps)
+    attrs['kind'] = kinds
     for tagname, tagvalue in zip(tagnames, tagvalues):
         attrs[tagname] = [tagvalue]
+    if tagnames:
+        arr = arr.reshape(arr.shape + (1,) * len(tagnames))
     return ArrayWrapper(arr, attrs)
 
 
