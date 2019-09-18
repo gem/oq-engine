fd6370c37f20f3cac254b90961b9b6f7422a1c18
Marco <pgnmrc69@gmail.com>
2019-09-18 22:47:26 +0200

    Tests missing
    Former-commit-id: ba2514c6766a94f866e631b75d6e60080f75cede [formerly b35cb3d0db0d7cc424b2f92014cafa02149f9a04]
    Former-commit-id: 808e78293315a9aec4b08b493f2b9206c85fb51e

--- openquake/hazardlib/tests/gsim/morikawa_fujiwara_2013_test.py
+++ openquake/hazardlib/tests/gsim/morikawa_fujiwara_2013_test.py
@@ -18,6 +18,7 @@
 
 import numpy as np
 import unittest
+from openquake.hazardlib import const
 from openquake.hazardlib.imt import PGA
 from openquake.hazardlib.contexts import DistancesContext
 from openquake.hazardlib.tests.gsim.mgmpe.dummy import Dummy
@@ -31,11 +32,12 @@ class MorikawaFujiwara2013Test(unittest.TestCase):
 
     def test_instantiation(self):
         dists = DistancesContext()
-        dists.rrup = np.array([10.])
+        dists.rrup = np.array([10., 50., 100.])
         imt = PGA()
         mag = 6.0
         rup = Dummy.get_rupture(mag=mag, hypo_depth=10.)
         slen = len(dists.rrup)
         sites = Dummy.get_site_collection(slen, vs30=760., z1pt4=100., xvf=10.)
+        stdt = [const.StdDev.TOTAL]
         gmm = MorikawaFujiwara2013(model='model1', region=None)
-        mean, _ = gmm.get_mean_std(sites, rup, dists, [imt])
+        mean, stds = gmm.get_mean_and_stddevs(sites, rup, dists, imt, stdt)
