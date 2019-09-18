be1f59094309ee68adc86bc11582719ada2c5756
Michele Simionato <michele.simionato@gmail.com>
2019-09-18 16:17:24 +0200

    Some refactoring [skip hazardlib]
    Former-commit-id: d8deb61975d68226472f648c013e3d254fdda024

--- openquake/calculators/disaggregation.py
+++ openquake/calculators/disaggregation.py
@@ -307,9 +307,7 @@ class DisaggregationCalculator(base.HazardCalculator):
                 for slc in gen_slices(start, stop, blocksize):
                     allargs.append((self.datastore, slc, cmaker,
                                     self.iml2s, trti, self.bin_edges))
-        for key in self.datastore:
-            self.datastore.set_nbytes(key)
-        self.datastore.hdf5.swmr_mode = True
+        self.datastore.swmr_on()
         results = parallel.Starmap(
             compute_disagg, allargs, h5=self.datastore.hdf5
         ).reduce(self.agg_result, AccumDict(accum={}))
