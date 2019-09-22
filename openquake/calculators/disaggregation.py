16d8d604a6a0b7af04782d6f0b2ee49fa0d59355
Michele Simionato <michele.simionato@gmail.com>
2019-09-22 18:27:05 +0200

    [skip CI]
    Former-commit-id: 55d14ba455659aedc83ab36213ef377a220d7268 [formerly 081ce5a89b15e706715722fce8b8706ed52d3c8f]
    Former-commit-id: 7616d21b979308dbff466303f9608529d84dde30

--- openquake/calculators/disaggregation.py
+++ openquake/calculators/disaggregation.py
@@ -309,7 +309,6 @@ class DisaggregationCalculator(base.HazardCalculator):
             compute_disagg, allargs, h5=self.datastore.hdf5,
             num_cores=oq.num_cores
         ).reduce(self.agg_result, AccumDict(accum={}))
-        #self.datastore.open('r+')
         return results  # sid -> trti-> 7D array
 
     def agg_result(self, acc, result):
