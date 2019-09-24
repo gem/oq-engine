34deb2ace33f9809e9ca2143c1e72b8c3431ee9a
Michele Simionato <michele.simionato@gmail.com>
2019-09-24 17:38:56 +0200

    [demos]
    Former-commit-id: 37410bfbd289f4597680a6013bfb7a8dc74433b9 [formerly 37410bfbd289f4597680a6013bfb7a8dc74433b9 [formerly d8a0a27266698a93c3115666977120ac26ce1bce]]
    Former-commit-id: 9d4ffd171d91b5ec5b6f615a8baea8154f424e6a
    Former-commit-id: abc77cd79e9af8846de07f6a1b629884c0c7088c

--- openquake/engine/engine.py
+++ openquake/engine/engine.py
@@ -333,7 +333,7 @@ def run_calc(job_id, oqparam, exports, hazard_calculation_id=None, **kw):
         poll_queue(job_id, _PID, poll_time=15)
         if OQ_DISTRIBUTE.endswith('pool'):
             logs.LOG.warning('Using %d cores on %s',
-                             parallel.Starmap.cpu_count, platform.node())
+                             parallel.Starmap.num_cores, platform.node())
         if OQ_DISTRIBUTE == 'zmq':
             logs.dbcmd('zmq_start')  # start zworkers
             logs.dbcmd('zmq_wait')  # wait for them to go up
