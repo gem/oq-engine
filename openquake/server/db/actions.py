4f63397996256dbd3774d4d29b76280645ee38e8
Michele Simionato <michele.simionato@gmail.com>
2019-09-24 14:38:13 +0200

    Changed the state from failed to deleted [skip hazardlib][demos]
    Former-commit-id: 3df1c1072488ef5c32a905456ddff6d4fb5f3959 [formerly 3df1c1072488ef5c32a905456ddff6d4fb5f3959 [formerly 63a814f9970500e4dfd4383bba516622b357e882]]
    Former-commit-id: 6110c12fb98ea6cd39c6ae30378926caaf0e59bf
    Former-commit-id: 18cb9ad4d020fac23f7936cccd1a82ea42406b18

--- openquake/server/db/actions.py
+++ openquake/server/db/actions.py
@@ -352,8 +352,8 @@ def del_calc(db, job_id, user, force=False):
         return {"error": 'Cannot delete calculation %d:'
                 ' ID does not exist' % job_id}
 
-    deleted = db("UPDATE job SET status='deleted' WHERE id=?x AND user_name=?x",
-                 job_id, user).rowcount
+    deleted = db("UPDATE job SET status='deleted' WHERE id=?x AND "
+                 "user_name=?x", job_id, user).rowcount
     if not deleted:
         return {"error": 'Cannot delete calculation %d: it belongs to '
                 '%s and you are %s' % (job_id, owner, user)}
