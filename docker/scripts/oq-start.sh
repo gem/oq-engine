0fb47fa74709f2fe38283c2e94a29998dd2f8bc1
Daniele Viganò <daniele@vigano.me>
2019-09-30 14:08:47 +0200

    More fixes for Docker [skip ci]
    Former-commit-id: 6e6b1dd5d10a6c4cf38a8ad0695c22b1be84daff

--- docker/scripts/oq-start.sh
+++ docker/scripts/oq-start.sh
@@ -16,28 +16,10 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake. If not, see <http://www.gnu.org/licenses/>.
-
 # This is required to load a custom  local_settings.py when 'oq webui' is used.
 export PYTHONPATH=$HOME
 
-cleanup() {
-    # SIGTERM is propagated to children.
-    # Timeout is managed directly by Docker, via it's '-t' flag:
-    # if SIGTERM does not teminate the entrypoint, after the time
-    # defined by '-t' (default 10 secs) the container is killed
-    kill $WEBUI_PID $DBSERVER_PID
-}
-
-waitfor() {
-    # Make startup syncronous
-    while ! pidof $1 >/dev/null; do
-        sleep 1
-    done
-    pidof $1
-}
-
 oq dbserver start &
-DBSERVER_PID=$(waitfor oq-dbserver)
 
 # Wait the DbServer to come up; may be replaced with a "oq dbserver wait"
 while :
@@ -55,15 +37,11 @@ if [ -n "$LOCKDOWN" ]; then
     oq webui migrate
     echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | oq shell 2>&1 >/dev/null
 fi
-
 if [ -t 1 ]; then
     # TTY mode
-    oq webui start 0.0.0.0:8800 &>> $HOME/oqdata/webui.log &
-    WEBUI_PID=$(waitfor oq-webui)
+    exec oq webui start 0.0.0.0:8800 -s &>> $HOME/oqdata/webui.log &
     /bin/bash
 else
     # Headless mode
-    oq webui start 0.0.0.0:8800 &
-    WEBUI_PID=$(waitfor oq-webui)
-    wait $WEBUI_PID
+    exec oq webui start 0.0.0.0:8800 -s
 fi
