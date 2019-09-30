8342ac4cb0509dfc30bc0831423aa7be657583be
Daniele Viganò <daniele@vigano.me>
2019-09-30 11:33:51 +0200

    Fix the WebUI start and improve SIG management

--- docker/scripts/oq-start.sh
+++ docker/scripts/oq-start.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
-
 # This is required to load a custom  local_settings.py when 'oq webui' is used.
 export PYTHONPATH=$HOME
 
-oq dbserver start
+cleanup() {
+    # SIGTERM is propagated to children.
+    # Timeout is managed directly by Docker, via it's '-t' flag:
+    # if SIGTERM does not teminate the entrypoint, after the time
+    # defined by '-t' (default 10 secs) the container is killed
+    kill $WEBUI_PID $DBSERVER_PID
+}
+
+waitfor() {
+    # Make startup syncronous
+    while ! pidof $1 >/dev/null; do
+        sleep 1
+    done
+    pidof $1
+}
+
+oq dbserver start &
+DBSERVER_PID=$(waitfor oq-dbserver)
 
 # Wait the DbServer to come up; may be replaced with a "oq dbserver wait"
 while :
@@ -24,9 +40,12 @@ fi
 
 if [ -t 1 ]; then
     # TTY mode
-    oq webui start 0.0.0.0:8800 &> /tmp/webui.log &
+    oq webui start 0.0.0.0:8800 &>> $HOME/oqdata/webui.log &
+    WEBUI_PID=$(waitfor oq-webui)
     /bin/bash
 else
     # Headless mode
-    oq webui start 0.0.0.0:8800 2>&1 | tee /tmp/webui.log
+    oq webui start 0.0.0.0:8800 &
+    WEBUI_PID=$(waitfor oq-webui)
+    wait $WEBUI_PID
 fi
