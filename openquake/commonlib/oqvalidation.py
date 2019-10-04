36a65f2a8c27302fd7e6c6d12f3df42ca4c99221
Michele Simionato <michele.simionato@gmail.com>
2019-10-04 15:04:02 +0200

    [skip hazardlib]
    Former-commit-id: ce144920c3b4c41ce90e2b53b5925f5398bb31c7 [formerly 200cb6cc0fe60ff1fe81424134e2706922b12379]
    Former-commit-id: c3dc4e61abd1817540776a01d10ac375aa8fab42

--- openquake/commonlib/oqvalidation.py
+++ openquake/commonlib/oqvalidation.py
@@ -110,7 +110,6 @@ class OqParam(valid.ParamSet):
     iml_disagg = valid.Param(valid.floatdict, {})  # IMT -> IML
     individual_curves = valid.Param(valid.boolean, False)
     inputs = valid.Param(dict, {})
-    multi_peril = valid.Param(valid.namelist, [])
     ash_wet_amplification_factor = valid.Param(valid.positivefloat, 1.0)
     intensity_measure_types = valid.Param(valid.intensity_measure_types, '')
     intensity_measure_types_and_levels = valid.Param(
