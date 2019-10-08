4c70fa0c0c4cc29e8f01eb632ec7c0dd1e7cefe4
Marco <pgnmrc69@gmail.com>
2019-10-08 10:17:16 +0200

    Fixing bug
    Former-commit-id: d3577afe68862eeec1899c643283c36286a03ede [formerly 68f1d65fbbdfd21e560dac33e4ba6bf35bf038a0]
    Former-commit-id: 538dba29bf9e3608f5ba7086b63777b4aff1fcda

--- openquake/hazardlib/source/point.py
+++ openquake/hazardlib/source/point.py
@@ -166,7 +166,6 @@ class PointSource(ParametricSeismicSource):
         Generate one rupture for each combination of magnitude, nodal plane
         and hypocenter depth.
         """
-        print('>>>>>', kwargs)
         shift_hypo = kwargs['shift_hypo'] if 'shift_hypo' in kwargs else False
         for mag, mag_occ_rate in self.get_annual_occurrence_rates():
             yield from self.gen_ruptures(mag, mag_occ_rate, collapse=False,
