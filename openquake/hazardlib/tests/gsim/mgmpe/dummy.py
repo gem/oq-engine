f0b3374a407911a94ad0cf738895e87a49f80399
Marco <pgnmrc69@gmail.com>
2019-10-11 09:40:44 +0200

    Making get_rupture more flexible

--- openquake/hazardlib/tests/gsim/mgmpe/dummy.py
+++ openquake/hazardlib/tests/gsim/mgmpe/dummy.py
@@ -15,6 +15,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import numpy as np
+from openquake.hazardlib.scalerel.wc1994 import WC1994
+from openquake.hazardlib.geo.geodetic import point_at
 from openquake.hazardlib.geo import Point, Line
 from openquake.hazardlib.geo.surface import SimpleFaultSurface
 from openquake.hazardlib.site import Site, SiteCollection
@@ -43,36 +45,51 @@ class Dummy:
         return SiteCollection(sites)
 
     @classmethod
-    def get_surface(self):
-        """ """
+    def get_surface(self, msr=None, asr=1.0, mag=6.0, rake=90, dip=90):
+        """
+        Create a rupture surface. The strike is NS, and the dip direction is
+        east.
+
+        :param msr:
+            An instance of a magnitude scaling relationship.
+        :param asr:
+            Aspect ratio
+        """
+        # Set msr
+        if msr is None:
+            msr = WC1994()
+        # set hypocenter
         hyp = Line([Point(0, 0.5)])
-        trc = Line([Point(0, 0), Point(0, 1)])
+        # set rupture trace
+        area = msr.get_median_area(mag, rake)
+        rup_wdt = (area / asr)**0.5
+        rup_len = rup_wdt * asr
+        south = point_at(hyp.longitude, hyp.latitude, 180, rup_len/2)
+        north = point_at(hyp.longitude, hyp.latitude, 0, rup_len/2)
+        trc = Line([south, north])
+        lsd = np.cos(90.-dip) * rup_wdt + 5.
         sfc = SimpleFaultSurface.from_fault_data(fault_trace=trc,
                                                  upper_seismogenic_depth=0,
-                                                 lower_seismogenic_depth=15,
-                                                 dip=90.,
+                                                 lower_seismogenic_depth=lsd,
+                                                 dip=dip,
                                                  mesh_spacing=10.)
         return sfc, hyp
 
     @classmethod
     def get_rupture(self, **kwargs):
+        """
+        Returns a rupture
+        """
         # Parameters
-        if 'mag' in kwargs:
-            mag = kwargs['mag']
-        else:
-            mag = 6.0
-        if 'rake' in kwargs:
-            rake = kwargs['rake']
-        else:
-            rake = 0
-        if 'trt' in kwargs:
-            trt = kwargs['trt']
-        else:
-            trt = 0
+        asr = kwargs['asr'] if 'asr' in kwargs else 1.0
+        dip = kwargs['dip'] if 'dip' in kwargs else 90.0
+        mag = kwargs['mag'] if 'mag' in kwargs else None
+        msr = kwargs['msr'] if 'msr' in kwargs else None
+        rake = kwargs['rake'] if 'rake' in kwargs else None
+        trt = kwargs['trt'] if 'trt' in kwargs else None
         # Get surface
-        sfc, hyp = self.get_surface()
+        sfc, hyp = self.get_surface(msr, asr, mag, rake, dip)
         # Create rupture
         rup = BaseRupture(mag, rake, trt, hyp, sfc)
-        vars(rup).update(kwargs)
-        # Set attributes
+        # Return rupture
         return rup
