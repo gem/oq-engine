9963123603f84912a28ea49827b37ef501555b05
Michele Simionato <michele.simionato@gmail.com>
2019-10-16 15:51:07 +0200

    [skip hazardlib]
    Former-commit-id: 5fc685cfdc96259c88d6dc2c6c47cc1963e82ce7 [formerly 5fc685cfdc96259c88d6dc2c6c47cc1963e82ce7 [formerly 075fe268e8e13bfc9afb73672204d4d7e2728b67]]
    Former-commit-id: c5d964ae66b5662ac756152ee898f6dd1f862384
    Former-commit-id: 6e5080c49e0e8ebceb1bf20743d827368db71f94

--- openquake/calculators/ucerf_classical.py
+++ openquake/calculators/ucerf_classical.py
@@ -55,7 +55,8 @@ class UcerfClassicalCalculator(ClassicalCalculator):
         self.maxdists = []
         param = dict(imtls=oq.imtls, truncation_level=oq.truncation_level,
                      filter_distance=oq.filter_distance, max_weight=1E10,
-                     max_sites_disagg=oq.max_sites_disagg)
+                     max_sites_disagg=oq.max_sites_disagg,
+                     task_multiplier=oq.task_multiplier)
         self.calc_times = general.AccumDict(accum=np.zeros(3, np.float32))
         [gsims] = self.csm.info.get_gsims_by_trt().values()
         sample = .001 if os.environ.get('OQ_SAMPLE_SOURCES') else None
