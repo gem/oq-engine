c3a6eb43068dedd934309732c70093e937701b2d
Michele Simionato <michele.simionato@gmail.com>
2019-10-19 08:31:03 +0200

    Fixed assoc_dist for site model without grid
    Former-commit-id: 43dfd152a3ee867cd93a8f6769021115f4ee2b63 [formerly 43dfd152a3ee867cd93a8f6769021115f4ee2b63 [formerly f4d85cecd655f25c451b284af90e6c90036a4d9c]]
    Former-commit-id: e9684cc45af09d21ea8b3b2870d5232b218b7848
    Former-commit-id: da461fffba6664a76eee3f7d08e0523d5211dcff

--- openquake/calculators/base.py
+++ openquake/calculators/base.py
@@ -703,7 +703,7 @@ class HazardCalculator(BaseCalculator):
         if hasattr(self, 'sitecol') and self.sitecol:
             if 'site_model' in oq.inputs:
                 assoc_dist = (oq.region_grid_spacing * 1.414
-                              if oq.region_grid_spacing else 0)
+                              if oq.region_grid_spacing else 5)  # Graeme's 5km
                 sm = readinput.get_site_model(oq)
                 self.sitecol.complete.assoc(sm, assoc_dist)
             self.datastore['sitecol'] = self.sitecol.complete
