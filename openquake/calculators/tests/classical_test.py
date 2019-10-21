b0f85fbcaa7e60b67d0e6e0ed2b8f0b84b433ef6
Michele Simionato <michele.simionato@gmail.com>
2019-10-21 04:57:02 +0200

    Removed case_45 files
    Former-commit-id: 0595967462e8b609c18fd627b7f2658a1b935f7f

--- openquake/calculators/tests/classical_test.py
+++ openquake/calculators/tests/classical_test.py
@@ -32,7 +32,7 @@ from openquake.qa_tests_data.classical import (
     case_18, case_19, case_20, case_21, case_22, case_23, case_24, case_25,
     case_26, case_27, case_28, case_29, case_30, case_31, case_32, case_33,
     case_34, case_35, case_36, case_37, case_38, case_39, case_40, case_41,
-    case_42, case_43, case_44, case_45)
+    case_42, case_43, case_44)
 
 
 class ClassicalTestCase(CalculatorTestCase):
@@ -546,14 +546,10 @@ hazard_uhs-std.csv
                                "hazard_map-mean-PGA.csv"], case_43.__file__)
 
     def test_case_44(self):
-        # this is a test for shift_hypo = true - We computed the results
+        # this is a test for shift_hypo. We computed independently the results
         # using the same input and a simpler calculator implemented in a
         # jupyter notebook
-
-        self.assert_curves_ok(["hazard_curve-mean-PGA.csv"], case_44.__file__)
-
-    def test_case_45(self):
-        # this is a test for shift_hypo = false - We computed the results
-        # using the same input and a simpler calculator implemented in a
-        # jupyter notebook
-        self.assert_curves_ok(["hazard_curve-mean-PGA.csv"], case_45.__file__)
+        self.assert_curves_ok(["hc-shift-hypo-PGA.csv"], case_44.__file__,
+                              shift_hypo='true')
+        self.assert_curves_ok(["hazard_curve-mean-PGA.csv"], case_44.__file__,
+                              shift_hypo='false')
