fe71f59679bdff0f7c152105d9e09132e888c5df
Michele Simionato <michele.simionato@gmail.com>
2019-10-22 10:33:23 +0200

    Removed the case of noDamageLimit None [skip hazardlib]
    Former-commit-id: 101430f121330b14eb6db67f1da9a1c8792fe6e5

--- openquake/risklib/read_nrml.py
+++ openquake/risklib/read_nrml.py
@@ -162,7 +162,7 @@ def ffconvert(fname, limit_states, ff, min_iml=1E-10):
     with context(fname, ff):
         ffs = ff[1:]
         imls = ff.imls
-    nodamage = imls.attrib.get('noDamageLimit')
+    nodamage = imls.attrib.get('noDamageLimit', 0)
     if nodamage == 0:
         # use a cutoff to avoid log(0) in GMPE.to_distribution_values
         logging.warning('Found a noDamageLimit=0 in %s, line %s, '
@@ -303,7 +303,7 @@ def convert_fragility_model_04(node, fname, fmcounter=itertools.count(1)):
     for ffs in node[2:]:
         IML = ffs.IML
         # NB: noDamageLimit = None is different than zero
-        nodamage = ffs.attrib.get('noDamageLimit')
+        nodamage = ffs.attrib.get('noDamageLimit', 0)
         ff = Node('fragilityFunction', {'format': fmt})
         ff['id'] = ~ffs.taxonomy
         ff['shape'] = convert_type[ffs.attrib.get('type', 'lognormal')]
