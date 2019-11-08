3ac34330771196c54dc0f43caa9038c3891ceef0
Michele Simionato <michele.simionato@gmail.com>
2019-11-08 07:41:37 +0100

    [skip hazardlib]
    Former-commit-id: 792024ef4ea8e929a1262208ed198a1340fcf105 [formerly e81f7fcc5cee4faa0a600c219e1aef4c5605dc64]
    Former-commit-id: b98576ac31b59bab83c393e953f0e3ade76936eb

--- openquake/calculators/tests/event_based_test.py
+++ openquake/calculators/tests/event_based_test.py
@@ -328,13 +328,16 @@ class EventBasedTestCase(CalculatorTestCase):
         [fname] = export(('ruptures', 'xml'), self.calc.datastore)
         self.assertEqualFiles('expected/ruptures.xml', fname)
 
-        # testing extracting ruptures, including nonparametric ones
+        # testing extracting ruptures
         rup0 = extract(self.calc.datastore, 'rupture/0').array
-        self.assertGot(
+        self.assertGot(  # planar rupture
             rup0, os.path.join(self.testdir, 'expected/rupture_0.toml'))
         rup1 = extract(self.calc.datastore, 'rupture/1').array
-        self.assertGot(
+        self.assertGot(  # gridded rupture
             rup1, os.path.join(self.testdir, 'expected/rupture_1.toml'))
+
+        # test running scenario from event based
+        self.run_calc(case_15.__file__, 'scenario.ini')
 
     def test_case_16(self):
         # an example with site model raising warnings and autogridded exposure
