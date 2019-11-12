9fc043f1e9ee5efa77753eac2e1dd8001dba10b4
Michele Simionato <michele.simionato@gmail.com>
2019-11-12 11:02:51 +0100

    [demos]
    Former-commit-id: 7b762be18c620a60597eb5d3a38eacead083943d [formerly 7b762be18c620a60597eb5d3a38eacead083943d [formerly 1b4fd0d2c5ea9a9a49a13d9d171d5949090e0580]]
    Former-commit-id: 245264dba38c45b6441a3a2e57f9f25545fd54b6
    Former-commit-id: c1b0e1aa18888c99c6b416808f50c15e4af01d01

--- openquake/calculators/extract.py
+++ openquake/calculators/extract.py
@@ -452,6 +452,21 @@ def extract_uhs(dstore, what):
     yield from params.items()
 
 
+@extract.add('effect')
+def extract_effect(dstore, what):
+    """
+    Extracts the effect of ruptures. Use it as /extract/effect
+    """
+    grp = dstore['effect']
+    dist_bins = dict(grp.attrs)
+    ndists = len(dist_bins[next(iter(dist_bins))])
+    arr = numpy.zeros((len(grp), ndists, len(dist_bins)))
+    for i, mag in enumerate(grp):
+        arr[i] = dstore['effect/' + mag][()]
+    return ArrayWrapper(arr, dict(dist_bins=dist_bins, ndists=ndists,
+                                  mags=[float(mag) for mag in grp]))
+
+
 @extract.add('sources')
 def extract_sources(dstore, what):
     """
