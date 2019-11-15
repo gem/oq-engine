9b4a649fbd1e508667e2a2d1a73c4aa535849fb7
Michele Simionato <michele.simionato@gmail.com>
2019-11-15 03:50:09 +0100

    Removed collapse_factor
    Former-commit-id: b21a71386d534b2666e4ae01b88c7a3860857c15 [formerly b21a71386d534b2666e4ae01b88c7a3860857c15 [formerly 4753afe76c797c67943a59ac876e551a83efb2df]]
    Former-commit-id: 481da3d2686c6a2cf49c038cda133037c79095b0
    Former-commit-id: 460f5e7d30e5a20e35fbf8ff372e464a3c5fa99b

--- openquake/commonlib/oqvalidation.py
+++ openquake/commonlib/oqvalidation.py
@@ -176,7 +176,6 @@ class OqParam(valid.ParamSet):
     spatial_correlation = valid.Param(valid.Choice('yes', 'no', 'full'), 'yes')
     specific_assets = valid.Param(valid.namelist, [])
     split_by_magnitude = valid.Param(valid.boolean, False)
-    collapse_factor = valid.Param(valid.positivefloat, 3)
     task_duration = valid.Param(valid.positiveint, None)  # used in ebrisk
     task_multiplier = valid.Param(valid.positiveint, 10)  # used in classical
     taxonomies_from_model = valid.Param(valid.boolean, False)
