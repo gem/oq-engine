d9af322560f7c30d8568693cfac15b4ea0e3f26a
Michele Simionato <michele.simionato@gmail.com>
2019-11-19 07:47:41 +0100

    [skip hazardlib][demos]
    Former-commit-id: 9d9257ce91d09343735517db239f61f6117be666 [formerly af1b0096bb6d681d339a6aaeb8538067c69a1692] [formerly af1b0096bb6d681d339a6aaeb8538067c69a1692 [formerly 356b23a5e847072dcf44d3f077f0be86cb956bad]]
    Former-commit-id: 22eb3fc9984bd63bd67d3d108e521cf140170a00
    Former-commit-id: 0f59478c698f294df56cb5f395f1d138620d02f5

--- openquake/hazardlib/source/base.py
+++ openquake/hazardlib/source/base.py
@@ -61,7 +61,7 @@ class BaseSeismicSource(metaclass=abc.ABCMeta):
             nr = self.num_ruptures / rescale
         else:
             nr = self.num_ruptures
-        return nr * self.nsites
+        return nr * (self.nsites + 100) / 100
 
     @property
     def nsites(self):
