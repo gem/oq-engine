fc1e6e4c897819c95b4f478b6965665047f1f150
Michele Simionato <michele.simionato@gmail.com>
2019-11-29 10:56:38 +0100

    [skip hazardlib][demos]
    Former-commit-id: 5338edef8d825651af7ee144372f52c5d67ad8f6 [formerly 5338edef8d825651af7ee144372f52c5d67ad8f6 [formerly e4925fa0f6d1a4f309952c211f2fb0f8d229a440]]
    Former-commit-id: 09edab534ee844fc848f3f45aa8fb1b4cd6f9dd3
    Former-commit-id: a94f4ce9553106b5ea845ebb1c67d89cc307010a

--- openquake/calculators/base.py
+++ openquake/calculators/base.py
@@ -862,7 +862,8 @@ class RiskCalculator(HazardCalculator):
             riskinputs = list(self._gen_riskinputs(kind))
         assert riskinputs
         logging.info('Built %d risk inputs', len(riskinputs))
-        self.datastore.swmr_on()
+        if self.oqparam.calculation_mode == 'event_based_damage':
+            self.datastore.swmr_on()
         return riskinputs
 
     def get_getter(self, kind, sid):
@@ -887,7 +888,7 @@ class RiskCalculator(HazardCalculator):
                 raise RuntimeError(
                     'There are no GMFs available: perhaps you set '
                     'ground_motion_fields=False or a large minimum_intensity')
-        if (self.oqparam.calculation_mode == 'event_based_risk' and
+        if (self.oqparam.calculation_mode != 'event_based_damage'and
                 dstore is self.datastore):
             # hack to make h5py happy; I could not get this to work with
             # the SWMR mode
