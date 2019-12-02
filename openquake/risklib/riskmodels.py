d8d9ce58c4f288e4b90734d0331e23dacb420b89
Michele Simionato <michele.simionato@gmail.com>
2019-12-02 05:22:01 +0100

    Changed the asset_loss_table in scenario_risk [skip hazardlib][demos]

--- openquake/risklib/riskmodels.py
+++ openquake/risklib/riskmodels.py
@@ -609,6 +609,13 @@ class CompositeRiskModel(collections.abc.Mapping):
         D = len(self.damage_states)
         return numpy.dtype([('eid', U32), ('dmg', (F32, (L, D)))])
 
+    def aid_eid_loss_dt(self):
+        """
+        :returns: a dtype (aid, eid, loss)
+        """
+        L = len(self.lti),
+        return numpy.dtype([('aid', U32), ('eid', U32), ('loss', (F32, L))])
+
     def vectorize_cons_model(self, tagcol):
         """
         Convert the dictionaries tag -> coeffs in the consequence model
