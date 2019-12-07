aa86c6735e0d671e54ca09f17e0452839807be0b
Michele Simionato <michele.simionato@gmail.com>
2019-12-07 08:13:25 +0100

    [skip hazardlib]
    Former-commit-id: 26886a8f9c5c9c5b9f7ca87e13ec70eec10a7165 [formerly 808ce9d5394aa2ef1bf19f47f2d186c0025203e7]
    Former-commit-id: c953ee3070f742200c2d5ff3b33cac19b42cf396

--- openquake/calculators/views.py
+++ openquake/calculators/views.py
@@ -169,14 +169,14 @@ def view_slow_sources(token, dstore, maxrows=20):
 
 
 @view.add('slow_ruptures')
-def view_slow_ruptures(token, dstore, maxrows=20):
+def view_slow_ruptures(token, dstore, maxrows=200000):
     """
     Show the slowest ruptures
     """
     fields = ['id', 'grp_id', 'code', 'n_occ', 'mag']
     rups = dstore.parent['ruptures'] if dstore.parent else dstore['ruptures']
     time = dstore['gmf_data/time_by_rup'][()]
-    arr = util.compose_arrays(time, rups[()][fields], 'time')
+    arr = util.compose_arrays(rups[()][fields], time)
     arr = arr[arr['code'] == 153]
     arr.sort(order='time')
     return rst_table(arr[-maxrows:])
