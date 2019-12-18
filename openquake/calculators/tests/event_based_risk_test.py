f54f4c7d79e6a3eae655dda714cf0b3399dd761b
Michele Simionato <michele.simionato@gmail.com>
2019-12-18 11:03:04 +0100

    [skip hazardlib]
    Former-commit-id: 78a7f5b0f23b6e75866b96429d90619e916c7141 [formerly d9550fc03cc42ab89def1e3bc9324503b43af687]
    Former-commit-id: e58889230660be51643960f8de2f718eb78cfbdd

--- openquake/calculators/tests/event_based_risk_test.py
+++ openquake/calculators/tests/event_based_risk_test.py
@@ -102,7 +102,7 @@ class EventBasedRiskTestCase(CalculatorTestCase):
 
         # check on the asset_loss_table, num_losses per asset
         aids = self.calc.datastore['asset_loss_table/data']['asset_id']
-        numpy.testing.assert_equal(numpy.bincount(aids), [18,  8, 14, 14])
+        numpy.testing.assert_equal(numpy.bincount(aids), [18, 8, 14, 14])
 
         [fname] = export(('avg_losses-stats', 'csv'), self.calc.datastore)
         self.assertEqualFiles('expected/%s' % strip_calc_id(fname), fname)
@@ -307,13 +307,12 @@ class EventBasedRiskTestCase(CalculatorTestCase):
     def test_case_master_eb(self):
         self.run_calc(case_master.__file__, 'job.ini',
                       calculation_mode='ebrisk', exports='',
-                      concurrent_tasks='4',
-                      aggregate_by='id', asset_loss_table='1')
+                      concurrent_tasks='4', aggregate_by='id')
 
         # check on the asset_loss_table, num_losses per asset
         aids = self.calc.datastore['asset_loss_table/data']['asset_id']
         numpy.testing.assert_equal(numpy.bincount(aids),
-                                   [17, 32, 32, 32, 32, 32, 32])
+                                   [6, 32, 22, 32, 32, 32, 31])
 
         # tot_losses-rlzs has shape (L=5, R=9)
         # tot_losses-stats has shape (L=5, S=4)
