9ea515f502353004aa453ef03882ab63f482d77b
Michele Simionato <michele.simionato@gmail.com>
2020-01-08 10:46:22 +0100

    Using 2 bytes for the amplification code
    Former-commit-id: 65ffc275ceb5cd70dbbe99dc0d4cf2652bf0cce7 [formerly 39d9b909538464a84e67e81f66e79ed1ad064392]
    Former-commit-id: f1c270efb684a61d89442b287bbc390becfa32f5

--- openquake/calculators/base.py
+++ openquake/calculators/base.py
@@ -639,7 +639,7 @@ class HazardCalculator(BaseCalculator):
                 haz_sitecol = dstore['sitecol'].complete
                 if ('amplification' in oq.inputs and
                         'amplification' not in haz_sitecol.array.dtype.names):
-                    haz_sitecol.add_col('amplification', (numpy.string_, 1))
+                    haz_sitecol.add_col('amplification', (numpy.string_, 2))
         else:
             haz_sitecol = readinput.get_site_collection(oq)
             if hasattr(self, 'rup'):
