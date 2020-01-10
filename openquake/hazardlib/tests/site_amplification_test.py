dfb361c5ec34ebfa85aa117794815f7ec607ddd4
Michele Simionato <michele.simionato@gmail.com>
2020-01-10 05:38:15 +0100

    More tests
    Former-commit-id: 1f059b1ca57eb0f4ec39ba522f39b413faf27640 [formerly 47f2c9034c4a340282801c50ca9cb102d16ce012]
    Former-commit-id: d23502eaa7dbe17939c3442ba7f1054de67acc3c

--- openquake/hazardlib/tests/site_amplification_test.py
+++ openquake/hazardlib/tests/site_amplification_test.py
@@ -16,12 +16,25 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
 
+import unittest
 import numpy
 from openquake.baselib.hdf5 import read_csv
 from openquake.baselib.general import gettemp
 from openquake.hazardlib.site_amplification import Amplifier
 
-ampl_func = '''#,,,,,,,"vs30_ref=760, imls=[.001, .01, .05, .1, .2, .5, 1., 1.21]"
+trivial_ampl_func = '''#,,,,,,,"vs30_ref=760, imls=[.001, .01, .05, .1, .2, .5, 1., 1.21]"
+amplification,level,PGA,SA(0.3),SA(0.6),SA(1.0),SA(1.5)
+A,0,1,1,1,1,1
+A,1,1,1,1,1,1
+A,2,1,1,1,1,1
+A,3,1,1,1,1,1
+A,4,1,1,1,1,1
+A,5,1,1,1,1,1
+A,6,1,1,1,1,1
+A,7,1,1,1,1,1
+'''
+
+simple_ampl_func = '''#,,,,,,,"vs30_ref=760, imls=[.001, .01, .05, .1, .2, .5, 1., 1.21]"
 amplification,level,PGA,SA(0.3),SA(0.6),SA(1.0),SA(1.5),sigma_PGA,sigma_SA(0.3),sigma_SA(0.6),sigma_SA(1.0),sigma_SA(1.5)
 A,0,1.01,1,1,1.1,1.1,.1,.1,.1,.1,.1
 A,1,1.05,1,1,1.1,1.1,.1,.1,.1,.1,.1
@@ -34,26 +47,47 @@ A,7,1,1,1,1.1,1.1,.1,.1,.1,.1,.1
 '''
 
 
-def test():
-    fname = gettemp(ampl_func)
+class AmplifierTestCase(unittest.TestCase):
     imls = [.001, .002, .005, .01, .02, .05, .1, .2, .5, 1., 1.2]
     soil_levels = numpy.array([.002, .005, .01, .02, .05, .1, .2])
     imtls = {'PGA': imls, 'SA(0.1)': imls, 'SA(0.2)': imls, 'SA(0.5)': imls}
-    aw = read_csv(fname, {'amplification': 'S2', 'level': numpy.uint8,
-                          None: numpy.float64})
-    a = Amplifier(imtls, aw, soil_levels)
     hcurve = [[.999, .995, .99, .98, .95, .9, .8, .7, .1, .05, .01],  # PGA
               [.999, .995, .99, .98, .95, .9, .8, .7, .1, .05, .01],  # SA(0.1)
               [.999, .995, .99, .98, .95, .9, .8, .7, .1, .05, .01],  # SA(0.2)
               [.999, .995, .99, .98, .95, .9, .8, .7, .1, .05, .01]]  # SA(0.5)
-    poes = a.amplify_one(b'A', 'SA(0.1)', hcurve[1])
-    numpy.testing.assert_allclose(
-        poes, [0.982699, 0.975398, 0.960744, 0.924573, 0.84, 0.74, 0.39],
-        atol=1E-6)
-    poes = a.amplify_one(b'A', 'SA(0.2)', hcurve[2])
-    numpy.testing.assert_allclose(
-        poes, [0.982699, 0.975398, 0.960744, 0.924573, 0.84, 0.74, 0.39],
-        atol=1E-6)
-    poes = a.amplify_one(b'A', 'SA(0.5)', hcurve[3])
-    numpy.testing.assert_allclose(
-        poes, [0.9825, 0.975, 0.955, 0.915, 0.84, 0.74, 0.39], atol=1E-6)
+
+    def test_trivial(self):
+        # using the heaviside function, i.e. amplify_one has contributions
+        # only for soil_intensity < rock_intensity
+        fname = gettemp(trivial_ampl_func)
+        aw = read_csv(fname, {'amplification': 'S2', 'level': numpy.uint8,
+                              None: numpy.float64})
+        a = Amplifier(self.imtls, aw, self.soil_levels)
+        poes = a.amplify_one(b'A', 'SA(0.1)', self.hcurve[1])
+        numpy.testing.assert_allclose(
+            poes, [0.9825, 0.975, 0.955, 0.915, 0.84, 0.74, 0.39],
+            atol=1E-6)
+        poes = a.amplify_one(b'A', 'SA(0.2)', self.hcurve[2])
+        numpy.testing.assert_allclose(
+            poes, [0.9825, 0.975, 0.955, 0.915, 0.84, 0.74, 0.39],
+            atol=1E-6)
+        poes = a.amplify_one(b'A', 'SA(0.5)', self.hcurve[3])
+        numpy.testing.assert_allclose(
+            poes, [0.9825, 0.975, 0.955, 0.915, 0.84, 0.74, 0.39], atol=1E-6)
+
+    def test_simple(self):
+        fname = gettemp(simple_ampl_func)
+        aw = read_csv(fname, {'amplification': 'S2', 'level': numpy.uint8,
+                              None: numpy.float64})
+        a = Amplifier(self.imtls, aw, self.soil_levels)
+        poes = a.amplify_one(b'A', 'SA(0.1)', self.hcurve[1])
+        numpy.testing.assert_allclose(
+            poes, [0.982699, 0.975398, 0.960744, 0.924573, 0.84, 0.74, 0.39],
+            atol=1E-6)
+        poes = a.amplify_one(b'A', 'SA(0.2)', self.hcurve[2])
+        numpy.testing.assert_allclose(
+            poes, [0.982699, 0.975398, 0.960744, 0.924573, 0.84, 0.74, 0.39],
+            atol=1E-6)
+        poes = a.amplify_one(b'A', 'SA(0.5)', self.hcurve[3])
+        numpy.testing.assert_allclose(
+            poes, [0.9825, 0.975, 0.955, 0.915, 0.84, 0.74, 0.39], atol=1E-6)
