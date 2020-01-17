38efb31f272c0d1ed3bf911b08af81b2e14ed848
Michele Simionato <michele.simionato@gmail.com>
2020-01-17 16:00:01 +0100

    Less monitoring [skip CI]
    Former-commit-id: 4faa15552c45870e97b37fde9a553687dff9591b [formerly 847310b9aec32546e75007c09c36057463f627f1]
    Former-commit-id: 9b341b2040b99e99037a9ed32c27fdc5c524aca5

--- openquake/calculators/post_risk.py
+++ openquake/calculators/post_risk.py
@@ -218,15 +218,14 @@ class PostRiskCalculator(base.RiskCalculator):
                 h5=self.datastore.hdf5)
         else:
             # do everything in process since it is really fast
-            with self.monitor('building total loss curves'):
-                elt = ds.read_df('losses_by_event', ['event_id', 'rlzi'])
-                smap = []
-                for rlzi, losses_df in elt.groupby('rlzi'):
-                    losses = numpy.array(losses_df)
-                    smap.append(
-                        {'tot_curves': builder.build_curves(losses, rlzi),
-                         'tot_losses': losses.sum(axis=0) * oq.ses_ratio,
-                         'rlzi': rlzi})
+            elt = ds.read_df('losses_by_event', ['event_id', 'rlzi'])
+            smap = []
+            for rlzi, losses_df in elt.groupby('rlzi'):
+                losses = numpy.array(losses_df)
+                smap.append(
+                    {'tot_curves': builder.build_curves(losses, rlzi),
+                     'tot_losses': losses.sum(axis=0) * oq.ses_ratio,
+                     'rlzi': rlzi})
         for dic in smap:
             if not dic:
                 continue
