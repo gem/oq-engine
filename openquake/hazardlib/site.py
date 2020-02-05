25b7bd456dd9e86798854fdbd719f0016445d999
g-weatherill <gaweatherill82@yahoo.co.uk>
2020-02-05 13:51:48 +0100

    Adds forearc/backarc taper to SERA BC Hydro GMPE

--- openquake/hazardlib/site.py
+++ openquake/hazardlib/site.py
@@ -122,6 +122,7 @@ site_param_dt = {
     'z1pt4': numpy.float64,
     'backarc': numpy.bool,
     'xvf': numpy.float64,
+    'backarc_distance': numpy.float64,
 
     # Parameters for site amplification
     'ampcode': (numpy.string_, 2),
--- openquake/hazardlib/tests/gsim/bchydro_2016_epistemic_test.py
+++ openquake/hazardlib/tests/gsim/bchydro_2016_epistemic_test.py
@@ -16,13 +16,53 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake. If not, see <http://www.gnu.org/licenses/>
 
+import unittest
+import numpy as np
 from openquake.hazardlib.gsim.bchydro_2016_epistemic import (
     BCHydroSERASInter, BCHydroSERASInterHigh, BCHydroSERASInterLow,
-    BCHydroSERASSlab, BCHydroSERASSlabHigh, BCHydroSERASSlabLow)
+    BCHydroSERASSlab, BCHydroSERASSlabHigh, BCHydroSERASSlabLow,
+    FABATaperGaussian, FABATaperStep, FABATaperLinear, FABATaperSFunc,
+    FABATaperSigmoid)
 from openquake.hazardlib.tests.gsim.utils import BaseGSIMTestCase
 
 
 MAX_DISCREP = 0.01
+
+
+# Test the tapering functions
+class FABATaperTestCase(unittest.TestCase):
+    """
+    Verifies the values in each of the FABA tapering objects
+    """
+    def setUp(self):
+        self.x = np.array([-150., -100., -30., 0., 30., 100., 150.])
+
+    def test_faba_step_taper(self):
+        model = FABATaperStep()
+        expected = np.array([0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0])
+        np.testing.assert_array_almost_equal(model(self.x), expected, 7)
+
+    def test_faba_linear_taper(self):
+        model = FABATaperLinear(width=100.0)
+        expected = np.array([0.000, 0.000, 0.200, 0.500, 0.800, 1.000, 1.000])
+        np.testing.assert_array_almost_equal(model(self.x), expected, 7)
+
+    def test_faba_sfunc_taper(self):
+        model = FABATaperSFunc(a=-100., b=100.)
+        expected = np.array([0.000, 0.000, 0.245, 0.500, 0.755, 1.000, 1.000])
+        np.testing.assert_array_almost_equal(model(self.x), expected, 7)
+
+    def test_faba_sigmoid_taper(self):
+        model = FABATaperSigmoid(c=30.0)
+        expected = np.array([0.006692851, 0.034445196, 0.268941421, 0.5,
+                             0.731058579, 0.965554804, 0.993307149])
+        np.testing.assert_array_almost_equal(model(self.x), expected, 7)
+
+    def test_faba_sigmoid_gaussian(self):
+        model = FABATaperGaussian(sigma=30., a=-100., b=100.)
+        expected = np.array([0.0, 0.0, 0.158362087, 0.5,
+                             0.841637913, 1.0, 1.0])
+        np.testing.assert_array_almost_equal(model(self.x), expected, 7)
 
 
 # Subduction Interface
@@ -69,7 +109,34 @@ class BCHydroSERASInterHighSigmaMuTestCase(BaseGSIMTestCase):
                    sigma_mu_epsilon=1.0)
 
 
+class BCHydroSERASInterLinearFABATaperTestCase(BaseGSIMTestCase):
+    GSIM_CLASS = BCHydroSERASInter
+
+    def test_mean(self):
+        self.check("sera_bchydro/BCHydroSERA_SINTER_LINEAR_FABA_TAPER_MEAN.csv",
+                   max_discrep_percentage=MAX_DISCREP,
+                   faba_taper_model="Linear", width=100.)
+
+
+class BCHydroSERASInterSFuncFABATaperTestCase(BaseGSIMTestCase):
+    GSIM_CLASS = BCHydroSERASInter
+
+    def test_mean(self):
+        self.check("sera_bchydro/BCHydroSERA_SINTER_SFUNC_FABA_TAPER_MEAN.csv",
+                   max_discrep_percentage=MAX_DISCREP,
+                   faba_taper_model="SFunc", a=-100.0, b=100.0)
+
+
+class BCHydroSERASInterGaussianFABATaperTestCase(BaseGSIMTestCase):
+    GSIM_CLASS = BCHydroSERASInter
+
+    def test_mean(self):
+        self.check("sera_bchydro/BCHydroSERA_SINTER_GAUSSIAN_FABA_TAPER_MEAN.csv",
+                   max_discrep_percentage=MAX_DISCREP,
+                   faba_taper_model="Gaussian", sigma=30.0, a=-100.0, b=100.0)
+
 # Subduction Slab
+
 
 class BCHydroSERASSlabCentralTestCase(BaseGSIMTestCase):
     GSIM_CLASS = BCHydroSERASSlab
@@ -111,3 +178,30 @@ class BCHydroSERASSlabHighSigmaMuTestCase(BaseGSIMTestCase):
         self.check("sera_bchydro/BCHydroSERA_SSLAB_HIGH_SIGMA_MU_MEAN.csv",
                    max_discrep_percentage=MAX_DISCREP,
                    sigma_mu_epsilon=1.0)
+
+
+class BCHydroSERASSlabLinearFABATaperTestCase(BaseGSIMTestCase):
+    GSIM_CLASS = BCHydroSERASSlab
+
+    def test_mean(self):
+        self.check("sera_bchydro/BCHydroSERA_SSLAB_LINEAR_FABA_TAPER_MEAN.csv",
+                   max_discrep_percentage=MAX_DISCREP,
+                   faba_taper_model="Linear", width=100.)
+
+
+class BCHydroSERASSlabSFuncFABATaperTestCase(BaseGSIMTestCase):
+    GSIM_CLASS = BCHydroSERASSlab
+
+    def test_mean(self):
+        self.check("sera_bchydro/BCHydroSERA_SSLAB_SFUNC_FABA_TAPER_MEAN.csv",
+                   max_discrep_percentage=MAX_DISCREP,
+                   faba_taper_model="SFunc", a=-100.0, b=100.0)
+
+
+class BCHydroSERASSlabGaussianFABATaperTestCase(BaseGSIMTestCase):
+    GSIM_CLASS = BCHydroSERASSlab
+
+    def test_mean(self):
+        self.check("sera_bchydro/BCHydroSERA_SSLAB_GAUSSIAN_FABA_TAPER_MEAN.csv",
+                   max_discrep_percentage=MAX_DISCREP,
+                   faba_taper_model="Gaussian", sigma=30.0, a=-100.0, b=100.0)
