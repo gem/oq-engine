e1e5e16769d059ab8ece9428e0cc6a2f65b5723a
Michele Simionato <michele.simionato@gmail.com>
2020-02-23 08:09:33 +0100

    [skip hazardlib]

--- openquake/calculators/event_based.py
+++ openquake/calculators/event_based.py
@@ -179,18 +179,11 @@ class EventBasedCalculator(base.HazardCalculator):
         with self.monitor('store source_info'):
             self.store_source_info(calc_times)
         logging.info('Reordering the ruptures and storing the events')
-        attrs = self.datastore.getitem('ruptures').attrs
         sorted_ruptures = self.datastore.getitem('ruptures')[()]
         # order the ruptures by rup_id
         sorted_ruptures.sort(order='serial')
-        ngroups = len(self.csm.info.trt_by_grp)
-        grp_indices = numpy.zeros((ngroups, 2), U32)
-        grp_ids = sorted_ruptures['grp_id']
-        for grp_id, [startstop] in get_indices(grp_ids).items():
-            grp_indices[grp_id] = startstop
         self.datastore['ruptures'] = sorted_ruptures
         self.datastore['ruptures']['id'] = numpy.arange(len(sorted_ruptures))
-        self.datastore.set_attrs('ruptures', grp_indices=grp_indices, **attrs)
         with self.monitor('saving events'):
             self.save_events(sorted_ruptures)
 
