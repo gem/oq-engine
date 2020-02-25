33a9b59d80cafb76becb300fc317e0d1506b5185
Michele Simionato <michele.simionato@gmail.com>
2020-02-25 09:21:58 +0100

    [skip hazardlib]

--- openquake/commands/tests/commands_test.py
+++ openquake/commands/tests/commands_test.py
@@ -78,7 +78,7 @@ class Print(object):
 
 class InfoTestCase(unittest.TestCase):
     EXPECTED = '''<CompositionInfo
-b1, x15.xml, grp=[0], weight=1.0: 1 realization(s)>
+b1, x15.xml, weight=1.0: 1 realization(s)>
 See http://docs.openquake.org/oq-engine/stable/effective-realizations.html for an explanation
 <RlzsAssoc(size=1, rlzs=1)>'''
 
