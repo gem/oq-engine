ed01bf6fb4e7813c7e76ccea9a1209d0eeb81ac6
Michele Simionato <michele.simionato@gmail.com>
2020-03-02 15:43:54 +0100

    [skip CI]

--- openquake/calculators/scenario_damage.py
+++ openquake/calculators/scenario_damage.py
@@ -213,15 +213,15 @@ class ScenarioDamageCalculator(base.RiskCalculator):
         eid_dmg_dt = self.crmodel.eid_dmg_dt()
         d_event = numpy.array(sorted(result['d_event'].items()), eid_dmg_dt)
         # sanity check on the total number of assets, same for each event
-        sums = set(d['dmg'].sum() for d in d_event)
+        num_sums = len(set(d['dmg'].sum() for d in d_event))
         if self.param['approx_ddd']:
-            if len(sums) > 1:
+            if num_sums > 1:
                 msg = ('Due to numeric errors the sum of assets in each damage'
                        ' state is not the same for each event')
                 logging.warning(msg)
-        elif len(sums) > 1:
+        elif num_sums > 1:
             raise ValueError('The sum of assets in each damage state is not '
-                             'the same for each event! %s', sums)
+                             'the same for each event!')
         self.datastore['dmg_by_event'] = d_event
 
         # consequence distributions
