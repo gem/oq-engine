33d7f2e8e3514b276173937ef243c7965a6f85f1
Michele Simionato <michele.simionato@gmail.com>
2020-03-05 04:28:10 +0100

    [skip hazardlib]

--- openquake/commonlib/source.py
+++ openquake/commonlib/source.py
@@ -369,7 +369,7 @@ class CompositeSourceModel(collections.abc.Sequence):
                     src.src_group_id = [s.src_group_id for s in srcs]
                 lst.append(src)
             dic[trt] = sourceconverter.SourceGroup(trt, lst)
-        self.src_groups = atomic + list(dic.values())
+        self.src_groups = list(dic.values()) + atomic
         if event_based:
             serial = ses_seed
             for sg in self.src_groups:
