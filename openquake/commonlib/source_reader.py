a63caa285696b845f5132b7ffab89ab9a8361099
Michele Simionato <michele.simionato@gmail.com>
2020-03-07 06:40:39 +0100

    Using protocol=4 for the source checksum [skip hazardlib]

--- openquake/commonlib/source_reader.py
+++ openquake/commonlib/source_reader.py
@@ -94,8 +94,7 @@ class SourceReader(object):
                 mags.update(srcmags)
                 dic = {k: v for k, v in vars(src).items()
                        if k != 'id' and k != 'src_group_id'}
-                src.checksum = zlib.adler32(
-                    pickle.dumps(dic, pickle.HIGHEST_PROTOCOL))
+                src.checksum = zlib.adler32(pickle.dumps(dic, protocol=4))
                 src._wkt = src.wkt()
         return dict(fname_hits=fname_hits, sm=sm, mags=mags,
                     ordinal=ordinal, fileno=fileno)
