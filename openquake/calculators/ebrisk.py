44bc0f4b47e8c4df2da7db5b0161f62d9f08dedd
Michele Simionato <michele.simionato@gmail.com>
2020-03-31 14:20:28 +0200

    Fixed minimum_asset_loss warning [skip CI]

--- openquake/calculators/ebrisk.py
+++ openquake/calculators/ebrisk.py
@@ -193,8 +193,9 @@ class EbriskCalculator(event_based.EventBasedCalculator):
         mal = {lt: getdefault(oq.minimum_asset_loss, lt)
                for lt in oq.loss_names}
         logging.info('minimum_asset_loss=%s', mal)
-        if oq.aggregate_by and self.E * A > oq.max_potential_gmfs and any(
-                val == 0 for val in mal.values()):
+        if (oq.aggregate_by and self.E * A > oq.max_potential_gmfs and
+                any(val == 0 for val in mal.values()) and not
+                oq.minimum_asset_loss):
             logging.warning('The calculation is really big; you should set '
                             'minimum_asset_loss')
         self.param['minimum_asset_loss'] = mal
