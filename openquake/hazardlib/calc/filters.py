359f7080d057e4b3e6f9c769da85c3f275538d74
Michele Simionato <michele.simionato@gmail.com>
2020-03-31 06:54:05 +0200

    Added a warning again magnitude-dependent maximum_distance

--- openquake/hazardlib/calc/filters.py
+++ openquake/hazardlib/calc/filters.py
@@ -83,15 +83,8 @@ class IntegrationDistance(collections.abc.Mapping):
     400.0
     """
     def __init__(self, dic):
-        self.dic = {}  # TRT -> float or list of pairs
+        self.dic = dic  # TRT -> float
         self.magdist = {}  # TRT -> (magnitudes, distances), set by the engine
-        for trt, value in dic.items():
-            if isinstance(value, (list, numpy.ndarray)):
-                # assume a list of pairs (magstring, dist)
-                self.magdist[trt] = {'%.2f' % mag: dist for mag, dist in value}
-                self.dic[trt] = value[-1][-1]
-            else:
-                self.dic[trt] = float(value)
 
     def __call__(self, trt, mag=None):
         if mag and trt in self.magdist:
