2a1b78809e554b267a8d4054a188a46396a3d3bf
Michele Simionato <michele.simionato@gmail.com>
2020-04-08 04:53:36 +0200

    Fixed docstring in make_figure_tot_curves [skip CI]

--- openquake/commands/plot.py
+++ openquake/commands/plot.py
@@ -465,7 +465,7 @@ def make_figure_agg_curves(extractors, what):
 
 def make_figure_tot_curves(extractors, what):
     """
-    $ oq plot 'tot_curves?loss_type=structural' -1
+    $ oq plot 'tot_curves?loss_type=structural&kind=rlz-000&absolute=1'
     """
     import matplotlib.pyplot as plt
     fig = plt.figure()
