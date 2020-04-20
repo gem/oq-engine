dfb531323cbdaa8b3ec0eda2b92ba692f05e462b
Michele Simionato <michele.simionato@gmail.com>
2020-04-20 08:32:54 +0200

    Fixed the usual hdf5 damn bug [skip hazardlib]

--- openquake/risklib/asset.py
+++ openquake/risklib/asset.py
@@ -570,7 +570,7 @@ def build_asset_array(assets_by_site, tagnames=(), time_event=None):
     int_fields = [(str(name), U32) for name in tagnames]
     tagi = {str(name): i for i, name in enumerate(tagnames)}
     asset_dt = numpy.dtype(
-        [('asset_id', hdf5.vstr), ('ordinal', U32), ('lon', F32), ('lat', F32),
+        [('asset_id', '<S20'), ('ordinal', U32), ('lon', F32), ('lat', F32),
          ('site_id', U32), ('number', F32), ('area', F32)] + [
              (str(name), float) for name in float_fields] + int_fields)
     num_assets = sum(len(assets) for assets in assets_by_site)
