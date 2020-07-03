8d3c0906764364802849489a0a1e3fdca210531e
Michele Simionato <michele.simionato@gmail.com>
2020-07-03 10:48:37 +0200

    [skip CI]

--- bin/run-mosaic.py
+++ bin/run-mosaic.py
@@ -6,8 +6,8 @@ import glob
 def main(mosaic_root):
     # $ time python bin/run-mosaic.py ~/mosaic | bash
     o = 'OQ_SAMPLE_SITES=.0005'
-    p = 'calculation_mode=preclassical,number_of_logic_tree_samples=10'
-    e = 'calculation_mode=event_based,number_of_logic_tree_samples=10'
+    p = 'calculation_mode=preclassical,number_of_logic_tree_samples=10,save_disk_space=1'
+    e = 'calculation_mode=event_based,number_of_logic_tree_samples=10,save_disk_space=1'
     lf = '%s/x.log' % mosaic_root
     if os.path.exists(lf):
         os.remove(lf)
