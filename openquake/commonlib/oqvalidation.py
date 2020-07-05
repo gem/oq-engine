93f20ec9503b7ba79f881ae5342df06ad4c48caa
Marco <marco.pagani@globalquakemodel.org>
2020-07-05 12:42:01 +0200

    more work

--- openquake/commonlib/oqvalidation.py
+++ openquake/commonlib/oqvalidation.py
@@ -92,6 +92,7 @@ class OqParam(valid.ParamSet):
         siteclass='reference_siteclass',
         backarc='reference_backarc')
     aggregate_by = valid.Param(valid.namelist, [])
+    amplification_method = valid.Choices(['convolution', 'kernel'], None)
     minimum_asset_loss = valid.Param(valid.floatdict, {'default': 0})
     area_source_discretization = valid.Param(
         valid.NoneOr(valid.positivefloat), None)
