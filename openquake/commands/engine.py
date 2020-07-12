40b5c097460730a84394f48fa43fe1df82d15f47
Michele Simionato <michele.simionato@gmail.com>
2020-07-12 03:53:33 +0200

    Stopping the zworkers in a finally clause

--- openquake/commands/engine.py
+++ openquake/commands/engine.py
@@ -79,14 +79,17 @@ def run_jobs(job_inis, log_level='info', log_file=None, exports='',
         logging.info('Asking the DbServer to start the workers')
         logs.dbcmd('zmq_start')  # start the zworkers
         logs.dbcmd('zmq_wait')  # wait for them to go up
-    allargs = [(job_id, oqparam, exports, log_level, log_file)
-               for job_id, oqparam in jobparams]
-    for args in allargs:
-        eng.run_calc(*args)
-    if dist == 'zmq' and config.zworkers['host_cores']:
-        logs.dbcmd('zmq_stop')  # stop the zworkers
-    if dist.startswith('celery'):
-        eng.celery_cleanup(config.distribution.terminate_workers_on_revoke)
+    try:
+        allargs = [(job_id, oqparam, exports, log_level, log_file)
+                   for job_id, oqparam in jobparams]
+        for args in allargs:
+            eng.run_calc(*args)
+    finally:
+        if dist == 'zmq' and config.zworkers['host_cores']:
+            logging.info('Stopping the zworkers')
+            logs.dbcmd('zmq_stop')
+        elif dist.startswith('celery'):
+            eng.celery_cleanup(config.distribution.terminate_workers_on_revoke)
     return jobparams
 
 
