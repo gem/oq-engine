1fd34ebb0dbf7c8237e8cfb3ff8bb1fc5de0d384
Michele Simionato <michele.simionato@gmail.com>
2020-09-28 11:37:43 +0200

    Small change related to maximum_intensity

--- openquake/commonlib/oqvalidation.py
+++ openquake/commonlib/oqvalidation.py
@@ -308,7 +308,7 @@ class OqParam(valid.ParamSet):
                 for imt in self.hazard_imtls:
                     i1 = calc.filters.getdefault(self.minimum_intensity, 1E-3)
                     i2 = calc.filters.getdefault(self.maximum_intensity, imt)
-                    self.hazard_imtls[imt] = list(valid.logscale(i1, i2, 25))
+                    self.hazard_imtls[imt] = list(valid.logscale(i1, i2, 20))
             delattr(self, 'intensity_measure_types')
         self._risk_files = get_risk_files(self.inputs)
 
