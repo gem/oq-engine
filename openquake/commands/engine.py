a3f9518af1414710dce090d4c2fb908209bf0803
Michele Simionato <michele.simionato@gmail.com>
2020-10-08 18:29:02 +0200

    Fixed oq engine --dc -1

--- openquake/commands/engine.py
+++ openquake/commands/engine.py
@@ -116,7 +116,8 @@ def del_calculation(job_id, confirmed=False):
     """
     Delete a calculation and all associated outputs.
     """
-    if logs.dbcmd('get_job', job_id) is None:
+    job = logs.dbcmd('get_job', job_id)
+    if not job:
         print('There is no job %d' % job_id)
         return
 
@@ -124,13 +125,13 @@ def del_calculation(job_id, confirmed=False):
             'Are you sure you want to (abort and) delete this calculation and '
             'all associated outputs?\nThis action cannot be undone. (y/n): '):
         try:
-            abort(job_id)
-            resp = logs.dbcmd('del_calc', job_id, getpass.getuser())
+            abort(job.id)
+            resp = logs.dbcmd('del_calc', job.id, getpass.getuser())
         except RuntimeError as err:
             safeprint(err)
         else:
             if 'success' in resp:
-                print('Removed %d' % job_id)
+                print('Removed %d' % job.id)
             else:
                 print(resp['error'])
 
