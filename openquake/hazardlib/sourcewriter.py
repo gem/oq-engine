d0bd54bc7716527cc624f0b372368f3f04cff4dd
Michele Simionato <michele.simionato@gmail.com>
2020-11-05 06:24:02 +0100

    Fixed test

--- openquake/hazardlib/sourcewriter.py
+++ openquake/hazardlib/sourcewriter.py
@@ -186,7 +186,7 @@ def build_truncated_gr_mfd(mfd):
     """
     if hasattr(mfd, 'slip_rate'):
         return Node("truncGutenbergRichterMFD",
-                    {"aValue": mfd.a_val, "slipRate": mfd.slip_rate,
+                    {"bValue": mfd.b_val, "slipRate": mfd.slip_rate,
                      "rigidity": mfd.rigidity,
                      "minMag": mfd.min_mag, "maxMag": mfd.max_mag})
     return Node("truncGutenbergRichterMFD",
