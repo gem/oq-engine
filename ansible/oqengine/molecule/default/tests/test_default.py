fdb5cf5985e37489429c444f0ae20360c39d4a3c
Antonio Ettorre <antonio.ettorre@globalquakemodel.org>
2020-11-05 18:13:28 +0100

    add lint to check syntax

--- ansible/oqengine/molecule/default/tests/test_default.py
+++ ansible/oqengine/molecule/default/tests/test_default.py
@@ -1,5 +1,10 @@
-"""Role testing files using testinfra."""
+import os
+import pytest
 
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 def test_hosts_file(host):
     """Validate /opt/openquake/bin/oq file."""
