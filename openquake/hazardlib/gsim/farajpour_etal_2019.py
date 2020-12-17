52b141c48cf66c33938961313a19239d1fc9e124
hasan <66379394+hasanmousavi@users.noreply.github.com>
2020-12-17 13:57:49 +0330

    Update farajpour_etal_2019.py

--- openquake/hazardlib/gsim/farajpour_etal_2019.py
+++ openquake/hazardlib/gsim/farajpour_etal_2019.py
@@ -77,8 +77,8 @@ class FarajpourEtAl2019(GMPE):
         stddevs = self._get_stddevs(C, stddev_types)
         return mean, stddevs
 
-    def get_pga_rock_values(self, rup, dists):
-        pga_rock = (self._get_magnitudescaling_rock_term(rup) +
+    #def get_pga_rock_values(self, rup, dists):
+     #   pga_rock = (self._get_magnitudescaling_rock_term(rup) +
                     self._get_styleoffaulting_rock_term(rup) +
                     self._get_faultdip_rock_term(rup) +
                     self._get_hypocentraldepth_rock_term(rup) +
