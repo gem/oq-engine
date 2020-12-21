93408c920039216af0067c347188f1ead55f07d3
Michele Simionato <michele.simionato@gmail.com>
2020-12-21 07:03:52 +0100

    Removed tot_losses-XXX

--- openquake/server/db/actions.py
+++ openquake/server/db/actions.py
@@ -277,8 +277,6 @@ DISPLAY_NAME = {
     'agg_losses-stats': 'Aggregate Losses Statistics',
     'agg_risk': 'Total Risk',
     'agglosses': 'Aggregate Asset Losses',
-    'tot_losses-rlzs': 'Total Losses',
-    'tot_losses-stats': 'Total Losses Statistics',
     'tot_curves-rlzs': 'Total Loss Curves',
     'tot_curves-stats': 'Total Loss Curves Statistics',
     'bcr-rlzs': 'Benefit Cost Ratios',
