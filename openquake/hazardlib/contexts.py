3ae80e0c9d4b8af1a3b7a1248c764b66e29228b9
Michele Simionato <michele.simionato@gmail.com>
2020-12-30 10:26:22 +0100

    Small change to the ContextMaker

--- openquake/hazardlib/contexts.py
+++ openquake/hazardlib/contexts.py
@@ -175,8 +175,7 @@ class ContextMaker(object):
     """
     REQUIRES = ['DISTANCES', 'SITES_PARAMETERS', 'RUPTURE_PARAMETERS']
 
-    def __init__(self, trt, gsims, param=None, monitor=Monitor()):
-        param = param or {}
+    def __init__(self, trt, gsims, param, monitor=Monitor()):
         self.af = param.get('af', None)
         self.max_sites_disagg = param.get('max_sites_disagg', 10)
         self.collapse_level = param.get('collapse_level', False)
