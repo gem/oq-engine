b9c773e842871e0f229357eadf30311afd91ea27
Michele Simionato <michele.simionato@gmail.com>
2020-12-31 14:53:22 +0100

    Cleanup

--- openquake/calculators/base.py
+++ openquake/calculators/base.py
@@ -708,9 +708,7 @@ class HazardCalculator(BaseCalculator):
         if len(self.crmodel):
             logging.info('Storing risk model')
             self.datastore.create_dframe('crm', self.crmodel.to_dframe())
-            self.datastore['risk_model'] = rm = self.crmodel
-            attrs = self.datastore.getitem('risk_model').attrs
-            attrs['min_iml'] = hdf5.array_of_vstr(sorted(rm.min_iml.items()))
+            self.datastore['risk_model'] = self.crmodel
 
     def _read_risk_data(self):
         # read the exposure (if any), the risk model (if any) and then the
