313a6f96a57c278382a0ae8e9d001823eba61ba6
Antonio Ettorre <antonio.ettorre@globalquakemodel.org>
2021-01-12 12:05:45 +0100

    add ssh -f -T and 0.0.0.0 for start the workerpool remotely

--- openquake/baselib/workerpool.py
+++ openquake/baselib/workerpool.py
@@ -105,11 +105,11 @@ class WorkerMaster(object):
             if self.status(host)[0][1] == 'running':
                 print('%s:%s already running' % (host, self.ctrl_port))
                 continue
-            ctrl_url = 'tcp://%s:%s' % (host, self.ctrl_port)
+            ctrl_url = 'tcp://%s:%s' % (0.0.0.0, self.ctrl_port)
             if host == '127.0.0.1':  # localhost
                 args = [sys.executable]
             else:
-                args = ['ssh', host, self.remote_python]
+                args = ['ssh -f -T', host, self.remote_python]
             args += ['-m', 'openquake.baselib.workerpool', ctrl_url,
                      '-n', cores]
             if host != '127.0.0.1':
