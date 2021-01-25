39838d0daad6edd5499699fefea6375584e22f20
Michele Simionato <michele.simionato@gmail.com>
2021-01-25 06:16:16 +0100

    Better logging [skip CI]

--- openquake/baselib/parallel.py
+++ openquake/baselib/parallel.py
@@ -876,8 +876,8 @@ class Starmap(object):
         self.tasks.clear()
         if len(self.busytime) > 1:
             times = numpy.array(list(self.busytime.values()))
-            logging.info('Busy time in the workers: %.1fs, std=%.1fs',
-                         times.mean(), times.std())
+            logging.info('Busy time in the workers: %.1f-%.1fs',
+                         times.min(), times.max())
 
 
 def sequential_apply(task, args, concurrent_tasks=CT,
