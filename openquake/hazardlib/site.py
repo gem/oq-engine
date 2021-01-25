3b24fe685f6c71d3321d58f4eac90b6ae6501803
Michele Simionato <michele.simionato@gmail.com>
2021-01-25 09:05:06 +0100

    Fixed test

--- openquake/hazardlib/site.py
+++ openquake/hazardlib/site.py
@@ -117,12 +117,12 @@ site_param_dt = {
     'lat': numpy.float64,
     'depth': numpy.float64,
     'vs30': numpy.float64,
-    'vs30measured': numpy.uint8,  # numpy.bool cast "0" to True
+    'vs30measured': numpy.bool,
     'z1pt0': numpy.float64,
     'z2pt5': numpy.float64,
     'siteclass': (numpy.string_, 1),
     'z1pt4': numpy.float64,
-    'backarc': numpy.uint8,  # numpy.bool cast "0" to True
+    'backarc': numpy.bool,
     'xvf': numpy.float64,
 
     # Parameters for site amplification
