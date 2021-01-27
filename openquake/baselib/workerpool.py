22f8784311ec05355d3c4724f021c86e35010b13
Michele Simionato <michele.simionato@gmail.com>
2021-01-27 06:11:37 +0100

    Removed annoying prints [skip CI]

--- openquake/baselib/workerpool.py
+++ openquake/baselib/workerpool.py
@@ -85,7 +85,6 @@ class WorkerMaster(object):
         stopped = []
         for host, _ in self.host_cores:
             if not general.socket_ready((host, self.ctrl_port)):
-                print('%s not running' % host)
                 continue
             ctrl_url = 'tcp://%s:%s' % (host, self.ctrl_port)
             with z.Socket(ctrl_url, z.zmq.REQ, 'connect') as sock:
@@ -108,7 +107,6 @@ class WorkerMaster(object):
         killed = []
         for host, _ in self.host_cores:
             if not general.socket_ready((host, self.ctrl_port)):
-                print('%s not running' % host)
                 continue
             ctrl_url = 'tcp://%s:%s' % (host, self.ctrl_port)
             with z.Socket(ctrl_url, z.zmq.REQ, 'connect') as sock:
