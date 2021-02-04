a3cbf5874c344b75368f0732954d0e4a88caf188
Michele Simionato <michele.simionato@gmail.com>
2021-02-04 15:04:55 +0100

    More refactoring

--- openquake/commands/compare.py
+++ openquake/commands/compare.py
@@ -53,23 +53,6 @@ def _print_diff(a1, a2, idx1, idx2, col):
             print(col, idx2[i], a2[i])
 
 
-# works only locally for the moment
-def compare_rups(calc_1, calc_2):
-    """
-    Compare the ruptures of two calculations as pandas DataFrames
-    """
-    with util.read(calc_1) as ds1, util.read(calc_2) as ds2:
-        df1 = ds1.read_df('rup').sort_values(['src_id', 'mag'])
-        df2 = ds2.read_df('rup').sort_values(['src_id', 'mag'])
-    cols = [col for col in df1.columns if col not in
-            {'probs_occur_', 'clon_', 'clat_'}]
-    for col in cols:
-        a1 = df1[col].to_numpy()
-        a2 = df2[col].to_numpy()
-        assert len(a1) == len(a2), (len(a1), len(a2))
-        _print_diff(a1, a2, df1.index, df2.index, col)
-
-
 class Comparator(object):
     def __init__(self, calc_ids):
         self.extractors = [Extractor(calc_id) for calc_id in calc_ids]
@@ -81,7 +64,7 @@ class Comparator(object):
         for ex in self.extractors:
             time = ex.get('performance_data')['time_sec'].sum()
             data.append((ex.calc_id, time))
-        return views.rst_table(data, ['calc_id', 'time'])
+        print(views.rst_table(data, ['calc_id', 'time']))
 
     def getsids(self, samplesites):
         sids = self.sitecol['sids']
@@ -120,8 +103,80 @@ class Comparator(object):
             extractor.close()
         return numpy.array(arrays)  # shape (C, N, L)
 
+    def compare(self, what, imt, files, samplesites, atol, rtol):
+        sids = self.getsids(samplesites)
+        arrays = self.getdata(what, imt, sids)
+        head = ['site_id'] if files else ['site_id', 'calc_id']
+        header = head + (['%.5f' % lvl for lvl in self.oq.imtls[imt]]
+                         if what == 'hcurves' else
+                         [str(poe) for poe in self.oq.poes])
+        rows = collections.defaultdict(list)
+        diff_idxs = get_diff_idxs(arrays, rtol, atol)
+        if len(diff_idxs) == 0:
+            print('There are no differences within the tolerances '
+                  'atol=%s, rtol=%d%%, sids=%s' % (atol, rtol * 100, sids))
+            return
+        arr = arrays.transpose(1, 0, 2)  # shape (N, C, L)
+        for sid, array in sorted(zip(sids[diff_idxs], arr[diff_idxs])):
+            for ex, cols in zip(self.extractors, array):
+                if files:
+                    rows[calc_id].append([sid] + list(cols))
+                else:
+                    rows['all'].append([sid, ex.calc_id] + list(cols))
+        if files:
+            fdict = {ex.calc_id: open('%s.txt' % calc_id, 'w')
+                     for ex in self.extractors}
+            for calc_id, f in fdict.items():
+                f.write(views.rst_table(rows[calc_id], header))
+                print('Generated %s' % f.name)
+        else:
+            print(views.rst_table(rows['all'], header))
+        return arrays
 
-def main(what, imt, calc_ids: int,
+# works only locally for the moment
+def compare_rups(calc_1: int, calc_2: int):
+    """
+    Compare the ruptures of two calculations as pandas DataFrames
+    """
+    with util.read(calc_1) as ds1, util.read(calc_2) as ds2:
+        df1 = ds1.read_df('rup').sort_values(['src_id', 'mag'])
+        df2 = ds2.read_df('rup').sort_values(['src_id', 'mag'])
+    cols = [col for col in df1.columns if col not in
+            {'probs_occur_', 'clon_', 'clat_'}]
+    for col in cols:
+        a1 = df1[col].to_numpy()
+        a2 = df2[col].to_numpy()
+        assert len(a1) == len(a2), (len(a1), len(a2))
+        _print_diff(a1, a2, df1.index, df2.index, col)
+
+
+def compare_cumtime(calc1: int, calc2: int):
+    """
+    Compare the cumulative times between too calculations
+    """
+    return Comparator([calc1, calc2]).cumtime()
+
+
+def compare_hmaps(imt, calc_ids: int,
+         files=False,
+         *,
+         samplesites='',
+         rtol: float = 0,
+         atol: float = 1E-3):
+    """
+    Compare the hazard maps of two or more calculations.
+    """
+    c = Comparator(calc_ids)
+    arrays = c.compare('hmaps', imt, files, samplesites, atol, rtol)
+    if len(calc_ids) == 2:
+        ms = numpy.mean((arrays[0] - arrays[1])**2, axis=0)  # P
+        maxdiff = numpy.abs(arrays[0] - arrays[1]).max(axis=0)  # P
+        rows = [(str(poe), rms, md) for poe, rms, md in zip(
+            c.oq.poes, numpy.sqrt(ms), maxdiff)]
+        print(views.rst_table(rows, ['poe', 'rms-diff', 'max-diff']))
+
+
+def compare_hcurves(imt, calc_ids: int,
          files=False,
          *,
          samplesites='',
@@ -129,55 +184,20 @@ def main(what, imt, calc_ids: int,
          atol: float = 1E-3):
     """
     Compare the hazard curves or maps of two or more calculations.
-    Also used to compare the times with `oq compare cumtime of -1 -2`.
     """
-    if what == 'rups':
-        return compare_rups(int(imt), calc_ids[0])
     c = Comparator(calc_ids)
-    if what == 'cumtime':
-        print(c.cumtime())
-        return
-    sids = c.getsids(samplesites)
-    arrays = c.getdata(what, imt, sids)
-    head = ['site_id'] if files else ['site_id', 'calc_id']
-    if what == 'hcurves':
-        header = head + ['%.5f' % lvl for lvl in c.oq.imtls[imt]]
-    else:  # hmaps
-        header = head + [str(poe) for poe in c.oq.poes]
-    rows = collections.defaultdict(list)
-    diff_idxs = get_diff_idxs(arrays, rtol, atol)
-    if len(diff_idxs) == 0:
-        print('There are no differences within the tolerances '
-              'atol=%s, rtol=%d%%, sids=%s' % (atol, rtol * 100, sids))
-        return
-    arr = arrays.transpose(1, 0, 2)  # shape (N, C, L)
-    for sid, array in sorted(zip(sids[diff_idxs], arr[diff_idxs])):
-        for calc_id, cols in zip(calc_ids, array):
-            if files:
-                rows[calc_id].append([sid] + list(cols))
-            else:
-                rows['all'].append([sid, calc_id] + list(cols))
-    if files:
-        fdict = {calc_id: open('%s.txt' % calc_id, 'w')
-                 for calc_id in calc_ids}
-        for calc_id, f in fdict.items():
-            f.write(views.rst_table(rows[calc_id], header))
-            print('Generated %s' % f.name)
-    else:
-        print(views.rst_table(rows['all'], header))
-        if len(calc_ids) == 2 and what == 'hmaps':
-            ms = numpy.mean((arrays[0] - arrays[1])**2, axis=0)  # P
-            maxdiff = numpy.abs(arrays[0] - arrays[1]).max(axis=0)  # P
-            rows = [(str(poe), rms, md) for poe, rms, md in zip(
-                c.oq.poes, numpy.sqrt(ms), maxdiff)]
-            print(views.rst_table(rows, ['poe', 'rms-diff', 'max-diff']))
+    c.compare('hcurves', imt, files, samplesites, atol, rtol)
 
 
-main.what = dict(help='"hmaps", "hcurves" or "cumtime of"',
-                 choices={'rups', 'hmaps', 'hcurves', 'cumtime'})
-main.imt = 'intensity measure type to compare'
-main.calc_ids = dict(help='calculation IDs', nargs='+')
-main.files = 'write the results in multiple files'
-main.samplesites = 'sites to sample (or fname with site IDs)'
-main.rtol = 'relative tolerance'
-main.atol = 'absolute tolerance'
+main = dict(rups=compare_rups,
+            cumtime=compare_cumtime,
+            hmaps=compare_hmaps,
+            hcurves=compare_hcurves)
+
+for f in (compare_hmaps, compare_hcurves):
+    f.imt = 'intensity measure type to compare'
+    f.calc_ids = dict(help='calculation IDs', nargs='+')
+    f.files = 'write the results in multiple files'
+    f.samplesites = 'sites to sample (or fname with site IDs)'
+    f.rtol = 'relative tolerance'
+    f.atol = 'absolute tolerance'
