8ac20c0b27b11da4ee83e5a538d46665e383e268
Michele Simionato <michele.simionato@gmail.com>
2021-02-04 11:03:42 +0100

    Refactored oq compare

--- openquake/commands/compare.py
+++ openquake/commands/compare.py
@@ -23,26 +23,28 @@ from openquake.calculators.extract import Extractor
 from openquake.calculators import views
 
 
-def getdata(what, calc_ids, sitecol, sids):
-    extractors = [Extractor(calc_id) for calc_id in calc_ids]
+def getdata(what, imt, extractors, sitecol, sids):
     extractor = extractors[0]
     oq = extractor.oqparam
     imtls = oq.imtls
     poes = oq.poes
-    arrays = [extractor.get(what + '?kind=mean').mean[sids]]
+    if imt not in imtls:
+        sys.exit('%s not found. The available IMTs are %s' % (imt, list(imtls)))
+    imti = {imt: i for i, imt in enumerate(imtls)}
+    what += '?kind=mean'
+    arrays = [extractor.get(what).mean[sids]]
     extractor.close()
     for extractor in extractors[1:]:
         oq = extractor.oqparam
         numpy.testing.assert_array_equal(
             extractor.get('sitecol')[['lon', 'lat']], sitecol[['lon', 'lat']])
-        if what == 'hcurves':
-            for imt in imtls:
-                numpy.testing.assert_array_equal(oq.imtls[imt], imtls[imt])
-        elif what == 'hmaps':
+        if what.startswith('hcurves'):
+            numpy.testing.assert_array_equal(oq.imtls[imt], imtls[imt])
+        elif what.startswith('hmaps'):
             numpy.testing.assert_array_equal(oq.poes, poes)
-        arrays.append(extractor.get(what + '?kind=mean').mean[sids])
+        arrays.append(extractor.get(what).mean[sids])
         extractor.close()
-    return imtls, poes, numpy.array(arrays)  # shape (C, N, L)
+    return imti, poes, numpy.array(arrays)  # shape (C, N, L)
 
 
 def get_diff_idxs(array, rtol, atol):
@@ -110,7 +112,9 @@ def main(what, imt, calc_ids: int,
         return
     if what == 'rups':
         return compare_rups(int(imt), calc_ids[0])
-    sitecol = Extractor(calc_ids[0]).get('sitecol')
+    extractors = [Extractor(calc_id) for calc_id in calc_ids]
+    ex = extractors[0]
+    sitecol = ex.get('sitecol')
     sids = sitecol['sids']
     if samplesites:
         try:
@@ -123,20 +127,13 @@ def main(what, imt, calc_ids: int,
                 sids = numpy.random.choice(
                     len(sitecol), numsamples, replace=False)
     sids.sort()
-    imtls, poes, arrays = getdata(what, calc_ids, sitecol, sids)
-    imti = {imt: i for i, imt in enumerate(imtls)}
-    try:
-        levels = imtls[imt]
-    except KeyError:
-        sys.exit(
-            '%s not found. The available IMTs are %s' % (imt, list(imtls)))
-    imt2idx = {imt: i for i, imt in enumerate(imtls)}
+    imti, poes, arrays = getdata(what, imt, extractors, sitecol, sids)
     head = ['site_id'] if files else ['site_id', 'calc_id']
     if what == 'hcurves':
         array_imt = arrays[:, :, imti[imt], :]  # shape (C, N, L1)
-        header = head + ['%.5f' % lvl for lvl in levels]
+        header = head + ['%.5f' % lvl for lvl in ex.oqparam.imtls[imt]]
     else:  # hmaps
-        array_imt = arrays[:, :, imt2idx[imt]]
+        array_imt = arrays[:, :, imti[imt]]
         header = head + [str(poe) for poe in poes]
     rows = collections.defaultdict(list)
     diff_idxs = get_diff_idxs(array_imt, rtol, atol)
