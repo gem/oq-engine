a4ac4bea96e37bcf59651e7b66af9e503b6efbe7
Nicolas Schmid <nicolas@breiten.ch>
2021-04-15 16:24:28 +0200

    fix: small fixes

--- openquake/hazardlib/shakemap/parsers.py
+++ openquake/hazardlib/shakemap/parsers.py
@@ -103,7 +103,7 @@ get_array = CallableDict()
 
 
 @get_array.add('shapefile')
-def get_array_shapefile(_kind, fname):
+def get_array_shapefile(kind, fname):
     """
     Download and parse data saved as a shapefile.
     :param fname: url or filepath for the shapefiles,
@@ -157,7 +157,7 @@ def get_array_shapefile(_kind, fname):
 
 
 @get_array.add('usgs_xml')
-def get_array_usgs_xml(_kind, grid_url, uncertainty_url=None):
+def get_array_usgs_xml(kind, grid_url, uncertainty_url=None):
 
     grid_url = path2url(grid_url)
 
@@ -171,7 +171,7 @@ def get_array_usgs_xml(_kind, grid_url, uncertainty_url=None):
 
 
 @get_array.add('usgs_id')
-def get_array_usgs_id(_kind, usgs_id):
+def get_array_usgs_id(kind, usgs_id):
     url = SHAKEMAP_URL.format(usgs_id)
     logging.info('Downloading %s', url)
     contents = json.loads(urlopen(url).read())[
@@ -186,7 +186,7 @@ def get_array_usgs_id(_kind, usgs_id):
 
 
 @get_array.add('file_npy')
-def get_array_file_npy(_kind, fname):
+def get_array_file_npy(kind, fname):
     return numpy.load(fname)
 
 
