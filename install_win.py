3c92e5a30aa04a46a6d749b4776582cfd5f0a03e
Antonio Ettorre <antonio.ettorre@globalquakemodel.org>
2021-04-19 15:07:20 +0200

    use python instead of py because py is alias fo default windows installation

--- install_win.py
+++ install_win.py
@@ -214,23 +214,23 @@ def install(inst, version):
         print('Created %s' % inst.VENV)
 
     # upgrade pip
-    subprocess.check_call(['py', '-m', 'pip', 'install', '--upgrade', 'pip', 'wheel'])
+    subprocess.check_call(['python', '-m', 'pip', 'install', '--upgrade', 'pip', 'wheel'])
 
     # install the requirements
     req = 'https://raw.githubusercontent.com/gem/oq-engine/master/' \
         'requirements-py%d%d-%s.txt' % (PYVER + PLATFORM[sys.platform])
-    subprocess.check_call(['py', '-m', 'pip', 'install', '-r', req])
+    subprocess.check_call(['python', '-m', 'pip', 'install', '-r', req])
 
     if inst is devel:  # install from the local repo
-        subprocess.check_call(['py', '-m', 'pip', 'install', '-e', '.'])
+        subprocess.check_call(['python', '-m', 'pip', 'install', '-e', '.'])
     elif version is None:  # install the stable version
-        subprocess.check_call(['py', '-m', 'pip', 'install',
+        subprocess.check_call(['python', '-m', 'pip', 'install',
                                '--upgrade', 'openquake.engine'])
     elif '.' in version:  # install an official version
-        subprocess.check_call(['py', '-m', 'pip', 'install',
+        subprocess.check_call(['python', '-m', 'pip', 'install',
                                '--upgrade', 'openquake.engine==' + version])
     else:  # install a branch from github
-        subprocess.check_call(['py', '-m', 'pip', 'install',
+        subprocess.check_call(['python', '-m', 'pip', 'install',
                                '--upgrade', GITBRANCH % version])
 
     install_standalone(inst.VENV)
