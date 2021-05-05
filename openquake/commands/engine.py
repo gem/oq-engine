fec2c7e8bffff7aae9a5ccecde0afbc7a1fd692e
Michele Simionato <michele.simionato@gmail.com>
2021-05-05 14:39:51 +0200

    Fixed hazard_calculation_id not passed

--- openquake/commands/engine.py
+++ openquake/commands/engine.py
@@ -165,6 +165,7 @@ def main(
                            hc_id, multi)
         for job in jobs:
             job.params.update(pars)
+            job.params['hazard_calculation_id'] = hc_id
             job.params['exports'] = exports
         run_jobs(jobs)
 
