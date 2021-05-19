5d2ef418e27729f9a85f9fd6284a9c44388364bc
Marco Pagani <marco.pagani@globalquakemodel.org>
2021-05-19 08:12:53 +0200

    As per reviewer suggestion

--- openquake/hazardlib/geo/surface/kite_fault.py
+++ openquake/hazardlib/geo/surface/kite_fault.py
@@ -102,14 +102,9 @@ class KiteSurface(BaseSurface):
 
         # Building the polygon
         pnts = []
-        pnts += [(lo, la, de) for lo, la, de in zip(self.mesh.lons[0, iupp],
-                 self.mesh.lats[0, iupp], self.mesh.depths[0, iupp])]
-        pnts += [(lo, la, de) for lo, la, de in zip(self.mesh.lons[irig, -1],
-                 self.mesh.lats[irig, -1], self.mesh.depths[irig, -1])]
-        pnts += [(lo, la, de) for lo, la, de in zip(self.mesh.lons[-1, ilow],
-                 self.mesh.lats[-1, ilow], self.mesh.depths[-1, ilow])]
-        pnts += [(lo, la, de) for lo, la, de in zip(self.mesh.lons[ilef, 0],
-                 self.mesh.lats[ilef, 0], self.mesh.depths[ilef, 0])]
+        for corner in [(0, iupp), (irig, -1), (-1, ilow), (ilef, 0)]:
+            pnts.extend(zip(self.mesh.lons[corner]. self.mesh.lats[corner],
+                            self.mesh.depths[corner]))
         perimeter = np.array(pnts)
 
         distances = geodetic.min_geodetic_distance(
@@ -137,7 +132,6 @@ class KiteSurface(BaseSurface):
             polygon, mesh_xx, mesh_yy)
 
         return distances
-
 
     def _fix_right_hand(self):
         # This method fixes the mesh used to represent the grid surface so
