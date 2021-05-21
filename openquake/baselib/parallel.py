25af88631eab0876822545e3e3b934f2841feef2
Michele Simionato <michele.simionato@gmail.com>
2021-05-21 03:46:58 +0200

    Fix

--- openquake/baselib/parallel.py
+++ openquake/baselib/parallel.py
@@ -987,7 +987,7 @@ def workers_stop():
     elif OQDIST == 'celery':
         app.control.shutdown()
     elif OQDIST == 'zmq':
-        workerpool.WorkerMaster().kill()
+        workerpool.WorkerMaster().stop()
     return 'stopped'
 
 
