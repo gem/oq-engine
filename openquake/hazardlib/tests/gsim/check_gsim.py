2a8ccf32c9bc9aa5136897acba23fdf7f2881ec4
Michele Simionato <michele.simionato@gmail.com>
2021-06-20 14:14:34 +0200

    Replaced 3 contexts with 1

--- openquake/hazardlib/tests/gsim/check_gsim.py
+++ openquake/hazardlib/tests/gsim/check_gsim.py
@@ -36,12 +36,11 @@ from openquake.hazardlib.imt import registry
 from openquake.hazardlib.imt import from_string
 
 
-def set_read_only(*ctxs):
-    for ctx in ctxs:
-        for slot in vars(ctx):
-            arr = getattr(ctx, slot)
-            if isinstance(arr, numpy.ndarray):
-                arr.flags.writeable = False
+def set_read_only(ctx):
+    for slot in vars(ctx):
+        arr = getattr(ctx, slot)
+        if isinstance(arr, numpy.ndarray):
+            arr.flags.writeable = False
 
 
 def check_gsim(gsim, datafile, max_discrep_percentage, debug=False):
@@ -73,11 +72,11 @@ def check_gsim(gsim, datafile, max_discrep_percentage, debug=False):
     for testcase in _parse_csv(
             datafile, debug, gsim.REQUIRES_SITES_PARAMETERS):
         linenum += 1
-        (sctx, rctx, dctx, stddev_types, expected_results, result_type) \
+        (ctx, stddev_types, expected_results, result_type) \
             = testcase
+        set_read_only(ctx)
         for imt, expected_result in expected_results.items():
-            set_read_only(sctx, dctx, rctx)
-            mean, stddevs = gsim.get_mean_and_stddevs(sctx, rctx, dctx,
+            mean, stddevs = gsim.get_mean_and_stddevs(ctx, ctx, ctx,
                                                       imt, stddev_types)
             if result_type == 'MEAN':
                 if str(imt) == 'MMI':
@@ -110,8 +109,7 @@ def check_gsim(gsim, datafile, max_discrep_percentage, debug=False):
         if debug and errors:
             break
     return (errors,
-            _format_stats(time.time() - started, discrepancies, errors),
-            sctx, rctx, dctx)
+            _format_stats(time.time() - started, discrepancies, errors))
 
 
 def _format_stats(time_spent, discrepancies, errors):
@@ -169,34 +167,32 @@ def _parse_csv(datafile, debug, req_site_params):
     """
     reader = iter(csv.reader(datafile))
     headers = [param_name.lower() for param_name in next(reader)]
-    sctx, rctx, dctx, stddev_types, expected_results, result_type \
+    ctx, stddev_types, expected_results, result_type \
         = _parse_csv_line(headers, next(reader), req_site_params)
-    sattrs = sctx._slots_
-    dattrs = [slot for slot in DistancesContext._slots_
-              if hasattr(dctx, slot)]
+    slots = set(vars(ctx))
+    attrs = list(req_site_params) + [s for s in DistancesContext._slots_
+                                     if s in slots]
     for line in reader:
-        (sctx2, rctx2, dctx2, stddev_types2, expected_results2, result_type2) \
+        (ctx2, stddev_types2, expected_results2, result_type2) \
             = _parse_csv_line(headers, line, req_site_params)
-        if not debug \
-                and stddev_types2 == stddev_types \
-                and result_type2 == result_type \
-                and all(getattr(rctx2, slot, None) == getattr(rctx, slot, None)
-                        for slot in RuptureContext._slots_):
-            for slot in sattrs:
-                setattr(sctx, slot, numpy.hstack((getattr(sctx, slot),
-                                                  getattr(sctx2, slot))))
-            for slot in dattrs:
-                setattr(dctx, slot, numpy.hstack((getattr(dctx, slot),
-                                                  getattr(dctx2, slot))))
+        rp_equal = [
+            numpy.all(getattr(ctx, slot, None) == getattr(ctx2, slot, None))
+            for slot in RuptureContext._slots_]
+        all(rp_equal)
+        if not debug and stddev_types2 == stddev_types \
+           and result_type2 == result_type and all(rp_equal):
+            for slot in attrs:
+                setattr(ctx, slot, numpy.hstack((getattr(ctx, slot),
+                                                 getattr(ctx2, slot))))
             for imt in expected_results:
                 expected_results[imt] = numpy.hstack((expected_results[imt],
                                                       expected_results2[imt]))
         else:
-            yield sctx, rctx, dctx, stddev_types, expected_results, result_type
-            (sctx, rctx, dctx, stddev_types, expected_results,
-             result_type) = (sctx2, rctx2, dctx2, stddev_types2,
+            yield ctx, stddev_types, expected_results, result_type
+            (ctx, stddev_types, expected_results,
+             result_type) = (ctx2, stddev_types2,
                              expected_results2, result_type2)
-    yield sctx, rctx, dctx, stddev_types, expected_results, result_type
+    yield ctx, stddev_types, expected_results, result_type
 
 
 def _parse_csv_line(headers, values, req_site_params):
@@ -209,17 +205,9 @@ def _parse_csv_line(headers, values, req_site_params):
         A list of values of a single row to parse.
     :returns:
         A tuple of the following values (in specified order):
-
-        sctx
-            An instance of :class:`openquake.hazardlib.gsim.base.SitesContext`
-            with attributes populated by the information from in row in a form
-            of single-element numpy arrays.
-        rctx
+        ctx
             An instance of
             :class:`openquake.hazardlib.gsim.base.RuptureContext`.
-        dctx
-            An instance of
-            :class:`openquake.hazardlib.gsim.base.DistancesContext`.
         stddev_types
             An empty list, if the ``result_type`` column says "MEAN"
             for that row, otherwise it is a list with one item --
@@ -232,9 +220,7 @@ def _parse_csv_line(headers, values, req_site_params):
             A string literal, one of ``'STDDEV'`` or ``'MEAN'``. Value
             is taken from column ``result_type``.
     """
-    rctx = RuptureContext()
-    sctx = SitesContext(slots=req_site_params)
-    dctx = DistancesContext()
+    ctx = RuptureContext()
     expected_results = {}
     stddev_types = result_type = damping = None
 
@@ -268,11 +254,11 @@ def _parse_csv_line(headers, values, req_site_params):
                 attr = param[len('site_'):-1]
             else:  # vs30s etc
                 attr = param[len('site_'):]
-            setattr(sctx, attr, numpy.array([value]))
+            setattr(ctx, attr, numpy.array([value]))
         elif param.startswith('dist_'):
             # value is a distance measure
             value = float(value)
-            setattr(dctx, param[len('dist_'):], numpy.array([value]))
+            setattr(ctx, param[len('dist_'):], numpy.array([value]))
         elif param.startswith('rup_'):
             # value is a rupture context attribute
             try:
@@ -280,8 +266,7 @@ def _parse_csv_line(headers, values, req_site_params):
             except ValueError:
                 if value != 'undefined':
                     raise
-
-            setattr(rctx, param[len('rup_'):], value)
+            setattr(ctx, param[len('rup_'):], value)
         elif param == 'component_type':
             pass
         else:
@@ -301,7 +286,7 @@ def _parse_csv_line(headers, values, req_site_params):
             expected_results[imt] = numpy.array([value])
 
     assert result_type is not None
-    return sctx, rctx, dctx, stddev_types, expected_results, result_type
+    return ctx, stddev_types, expected_results, result_type
 
 
 if __name__ == '__main__':
