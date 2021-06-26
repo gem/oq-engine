77887823462bf3d9a61fe589c68ec21510aa4c83
Michele Simionato <michele.simionato@gmail.com>
2021-06-26 07:45:36 +0200

    Refactoring lanzano_luzi_2019

--- openquake/hazardlib/gsim/lanzano_luzi_2019.py
+++ openquake/hazardlib/gsim/lanzano_luzi_2019.py
@@ -56,7 +56,7 @@ def _compute_magnitude(rup, C):
     """
     Compute the magnitude function, equation (9):
     """
-    return C['a'] + (C['b'] * (rup.mag))
+    return C['a'] + C['b'] * rup.mag
 
 
 def _get_site_amplification(sites, C):
@@ -199,21 +199,6 @@ class LanzanoLuzi2019shallow(GMPE):
     """)
 
 
-def _get_stddevs2(C, stddev_types, num_sites):
-    """
-    Return standard deviations components.
-    """
-    stddevs = []
-    for stddev_type in stddev_types:
-        if stddev_type == const.StdDev.TOTAL:
-            stddevs.append(C['sigma'] + np.zeros(num_sites))
-        elif stddev_type == const.StdDev.INTRA_EVENT:
-            stddevs.append(C['phi'] + np.zeros(num_sites))
-        elif stddev_type == const.StdDev.INTER_EVENT:
-            stddevs.append(C['tau'] + np.zeros(num_sites))
-    return stddevs
-
-
 def _compute_distance2(rval2, C):
     """
     Compute the distance function, equation (5).
@@ -221,46 +206,6 @@ def _compute_distance2(rval2, C):
     h2 = 5
     rval = np.sqrt(rval2 ** 2 + (h2) ** 2)
     return C['c2'] * np.log10(rval) + C['c3'] * rval
-
-
-def _compute_magnitude2(rup, C):
-    """
-    Compute the magnitude function, equation (9):
-    """
-    return C['a'] + (C['b'] * (rup.mag))
-
-
-def _get_site_amplification2(sites, C):
-    """
-    Compute the site amplification function given by FS = eiSi, for
-    i = 1,2,3 where Si are the coefficients determined through regression
-    analysis, and ei are dummy variables (0 or 1) used to denote the
-    different EC8 site classes.
-    """
-    ssb, ssc = _get_site_type_dummy_variables2(sites)
-    return (C['sB'] * ssb) + (C['sC'] * ssc)
-
-
-def _get_site_type_dummy_variables2(sites):
-    """
-    Get site type dummy variables, which classified the sites into
-    different site classes based on the shear wave velocity in the
-    upper 30 m (Vs30) according to the EC8 (CEN 2003):
-    class A: Vs30 > 800 m/s
-    class B: Vs30 = 360 - 800 m/s
-    class C: Vs30 = 180 - 360 m/s
-    class D: Vs30 < 180 m/s
-    """
-    ssb = np.zeros(len(sites.vs30))
-    ssc = np.zeros(len(sites.vs30))
-    # Class C; Vs30 < 360 m/s.
-    idx = (sites.vs30 < 360.0)
-    ssc[idx] = 1.0
-    # Class B; 360 m/s <= Vs30 <= 800 m/s.
-    idx = (sites.vs30 >= 360.0) & (sites.vs30 < 800.0)
-    ssb[idx] = 1.0
-
-    return ssb, ssc
 
 
 class LanzanoLuzi2019deep(GMPE):
@@ -299,13 +244,13 @@ class LanzanoLuzi2019deep(GMPE):
         # intensity measure type
         C = self.COEFFS[imt]
 
-        imean = (_compute_magnitude2(rup, C) +
+        imean = (_compute_magnitude(rup, C) +
                  _compute_distance2(dists.rhypo, C) +
-                 _get_site_amplification2(sites, C))
+                 _get_site_amplification(sites, C))
 
-        istddevs = _get_stddevs2(C, stddev_types, sites.vs30.size)
+        istddevs = _get_stddevs(C, stddev_types, sites.vs30.size)
 
-	# Convert units to g,
+        # Convert units to g,
         # but only for PGA and SA (not PGV):
         if imt.name in "SA PGA":
             mean = np.log((10.0 ** (imean - 2.0)) / g)
