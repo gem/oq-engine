6e42ac3c59c05e71e93bc1bd39e59517646a8421
Michele Simionato <michele.simionato@gmail.com>
2021-07-13 08:04:26 +0200

    Removed print

--- openquake/hazardlib/gsim/zhao_2006.py
+++ openquake/hazardlib/gsim/zhao_2006.py
@@ -364,8 +364,6 @@ class ZhaoEtAl2006SSlab(ZhaoEtAl2006Asc):
 
             # convert from cm/s**2 to g
             mean[m] = np.log(np.exp(mean[m]) * 1e-2 / g)
-            if imt.string == 'PGA':
-                print(mean[m])
             _set_stddevs(sig[m], tau[m], phi[m], C['sigma'], C_SSLAB['tauS'])
 
     #: Coefficient table containing subduction slab coefficients taken from
