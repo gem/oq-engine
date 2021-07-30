bbe429aa189097b4695865018b1cf4e680e4a95f
Michele Simionato <michele.simionato@gmail.com>
2021-07-30 10:02:25 +0200

    Fixed comment [ci skip]

--- openquake/hazardlib/tests/gsim/gmpe_table_test.py
+++ openquake/hazardlib/tests/gsim/gmpe_table_test.py
@@ -508,7 +508,7 @@ class GSIMTableGoodTestCase(unittest.TestCase):
                                              5)
         np.testing.assert_array_almost_equal(sigma[0], expected_sigma, 5)
 
-        # event based check
+        # StdDev.ALL check
         contexts.get_mean_stds([gsim], ctx, imts)
 
     def test_get_mean_and_stddevs_good_amplified(self):
