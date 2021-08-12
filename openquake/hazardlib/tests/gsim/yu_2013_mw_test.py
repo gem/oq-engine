a0b66fb938200308bc8db94e13ac28f453a77acf
Michele Simionato <michele.simionato@gmail.com>
2021-08-12 07:42:31 +0200

    Ported more

--- openquake/hazardlib/tests/gsim/yu_2013_mw_test.py
+++ openquake/hazardlib/tests/gsim/yu_2013_mw_test.py
@@ -27,46 +27,38 @@ from openquake.hazardlib.tests.gsim.utils import BaseGSIMTestCase
 class YuEtAl2013MwActiveTestCase(BaseGSIMTestCase):
     GSIM_CLASS = YuEtAl2013Mw
 
-    def test_mean(self):
-        self.check('YU2013Mw/yu_2013_mean_active.csv',
-                   max_discrep_percentage=0.4)
-
-    def test_std_total(self):
-        self.check('YU2013Mw/yu_2013_stddev_active.csv',
-                   max_discrep_percentage=0.1)
+    def test_all(self):
+        self.check_all('YU2013Mw/yu_2013_mean_active.csv',
+                       'YU2013Mw/yu_2013_stddev_active.csv',
+                       mean_discrep_percentage=0.4,
+                       std_discrep_percentage=0.1)
 
 
 class YuEtAl2013MwTibetTestCase(BaseGSIMTestCase):
     GSIM_CLASS = YuEtAl2013MwTibet
 
-    def test_mean(self):
-        self.check('YU2013Mw/yu_2013_mean_tibetan.csv',
-                   max_discrep_percentage=0.4)
-
-    def test_std_total(self):
-        self.check('YU2013Mw/yu_2013_stddev_tibetan.csv',
-                   max_discrep_percentage=0.1)
+    def test_all(self):
+        self.check_all('YU2013Mw/yu_2013_mean_tibetan.csv',
+                       'YU2013Mw/yu_2013_stddev_tibetan.csv',
+                       mean_discrep_percentage=0.4,
+                       std_discrep_percentage=0.1)
 
 
 class YuEtAl2013MwEasternTestCase(BaseGSIMTestCase):
     GSIM_CLASS = YuEtAl2013MwEastern
 
-    def test_mean(self):
-        self.check('YU2013Mw/yu_2013_mean_eastern.csv',
-                   max_discrep_percentage=0.4)
-
-    def test_std_total(self):
-        self.check('YU2013Mw/yu_2013_stddev_eastern.csv',
-                   max_discrep_percentage=0.1)
+    def test_all(self):
+        self.check_all('YU2013Mw/yu_2013_mean_eastern.csv',
+                       'YU2013Mw/yu_2013_stddev_eastern.csv',
+                       mean_discrep_percentage=0.4,
+                       std_discrep_percentage=0.1)
 
 
 class YuEtAl2013MwStableTestCase(BaseGSIMTestCase):
     GSIM_CLASS = YuEtAl2013MwStable
 
-    def test_mean(self):
-        self.check('YU2013Mw/yu_2013_mean_stable.csv',
-                   max_discrep_percentage=0.4)
-
-    def test_std_total(self):
-        self.check('YU2013Mw/yu_2013_stddev_stable.csv',
-                   max_discrep_percentage=0.1)
+    def test_all(self):
+        self.check_all('YU2013Mw/yu_2013_mean_stable.csv',
+                       'YU2013Mw/yu_2013_stddev_stable.csv',
+                       mean_discrep_percentage=0.4,
+                       std_discrep_percentage=0.1)
