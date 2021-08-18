Fixing bug in calculation of a

openquake/hmtk/seismicity/occurrence/kijko_smit.py
@@ -141,7 +141,24 @@ class KijkoSmit(SeismicityOccurrence):
         """
         Calculates the rate of events >= ref_mag using the b-value estimator
         and Eq. 10 of Kijko & Smit
-        """
 
-        denominator = np.sum(nyr * np.exp(-bval * (cmag - ref_mag)))
-        return nvalue / denominator
+        :param bval:
+            b-value
+        :param nvalue:
+            Number of earthquakes within the completeness window
+        :param nyr:
+            A vector with the duration [yr] of each completeness interval
+        :param cmag:
+            A vector with the magnitude lower limit of each completeness
+            interval
+        :param
+        """
+        # Computing the rate for eqs above the min magnitude included in the
+        # completeness window
+        mmin = min(cmag)
+        denominator = np.sum(nyr * np.exp(-bval * (cmag - mmin)))
+        rate_above_mmin = nvalue / denominator
+        # Computing aGR
+        agr = np.log10(rate_above_mmin) + bval * mmin
+        # Returning rate above the reference magnitude provided
+        return 10**(agr-bval*ref_mag)

openquake/hmtk/tests/seismicity/occurrence/kijko_smit_test.py
@@ -105,8 +105,8 @@ class KijkoSmitTest(unittest.TestCase):
         """
         bval, sigma_b, aval, sigma_a = self.ks_ml.calculate(
             self.catalogue, self.config, self.compl)
-        print(bval, sigma_b)
         self.assertAlmostEqual(self.bval, bval, 1)
+        self.assertAlmostEqual(5.81, aval, 2)
 
     def test_kijko_smit_set_reference_magnitude(self):
         completeness_table = np.array([[1900, 1.0]])
@@ -114,4 +114,6 @@ class KijkoSmitTest(unittest.TestCase):
             {'magnitude': np.array([5.0, 6.0]),
              'year': np.array([2000, 2000])})
         config = {'reference_magnitude': 0.0}
-        self.ks_ml.calculate(catalogue, config, completeness_table)
+        bval, sigma_b, aval, sigma_a = self.ks_ml.calculate(
+            catalogue, config, completeness_table)
+        self.assertAlmostEqual(-0.9136, aval, 2)