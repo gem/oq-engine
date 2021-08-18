59d5a5f71395d9d0785200755c6cbf41495c1853
Prajakta Jadhav <prajakta.jadhav@ubc.ca>
2021-08-17 18:08:19 -0700

    Update youd_etal_2002.py
    Updated the GMPE file in accordance with the new format

--- openquake/hazardlib/gsim/youd_etal_2002.py
+++ openquake/hazardlib/gsim/youd_etal_2002.py
@@ -17,12 +17,41 @@
 # along with OpenQuake. If not, see <http://www.gnu.org/licenses/>.
 
 """
-Module exports :class:`YoudEtAl2002`.
+Module
+:mod:`openquake.hazardlib.gsim.youd_etal_2002`
+exports
+:class:`YoudEtAl2002`
 """
+import warnings
 import numpy as np
-from openquake.hazardlib.gsim.base import GMPE, CoeffsTable
+
 from openquake.hazardlib import const, site
 from openquake.hazardlib.imt import PGD
+from openquake.hazardlib.gsim.base import GMPE, CoeffsTable
+
+
+def __get_mag__(rup, C):
+    """
+    Returns the magnitude scaling term
+    """
+    return C["c0"] + (C["c1"] * rup.mag)
+
+
+def __get_dist__(dists, R, C):
+    """
+    Returns the distance scaling term
+    """
+    return (C["c3"] * dists.repi) + (C["c2"] * np.log10(R))
+
+
+def __get_soil__(sites, C):
+    """
+    Returns the distance scaling term
+    """
+    return (C["c4"] * np.log10(sites.slope)) + \
+           (C["c5"] * np.log10(sites.T_15)) + \
+           (C["c6"] * np.log10(100 - sites.F_15)) + \
+           (C["c7"] * np.log10(sites.D50_15 + 0.1))
 
 
 class YoudEtAl2002(GMPE):
@@ -70,63 +99,27 @@ class YoudEtAl2002(GMPE):
     #: not verified warning
     non_verified = True
 
-    def get_mean_and_stddevs(self, sites, rup, dists, imt, stddev_types):
-        """
-        See :meth:`superclass method
-        <.base.GroundShakingIntensityModel.get_mean_and_stddevs>`
-        for spec of input and result values.
-        """
-        if rup.hypo_depth >= 50.0:
-            dists.repi.setflags(write=1)
-            dists.repi[dists.repi < 5.0] = 5.0
-        R = (10 ** ((0.89 * rup.mag) - 5.64)) + dists.repi
+    def compute(self, ctx, imts, mean, sig, tau, phi):
+        for m, imt in enumerate(imts):
 
-        if sites.freeface_ratio.all() == 0.0:
-            C = self.COEFFS_SLOPE[imt]
-        else:
-            C = self.COEFFS_FREEFACE[imt]
-            sites.slope = sites.freeface_ratio
-        imean = (
-            self.__get_mag__(C, rup.mag) +
-            self.__get_dist__(C, dists.repi, R) +
-            self.__get_soil__(C, sites.slope,
-                              sites.T_15,
-                              sites.F_15, sites.D50_15))
-        mean = np.log(10.0 ** imean)
-        stddevs = self.__get_stddevs__(C, len(dists.repi), stddev_types)
-        return mean, stddevs
+            if ctx.hypo_depth >= 50.0:
+                ctx.repi.setflags(write=1)
+                ctx.repi[ctx.repi < 5.0] = 5.0
 
-    def __get_mag__(self, C, mag):
-        """
-        Returns the magnitude scaling term
-        """
-        return C["c0"] + (C["c1"] * mag)
+            R = (10 ** ((0.89 * ctx.mag) - 5.64)) + ctx.repi
 
-    def __get_dist__(self, C, repi, R):
-        """
-        Returns the distance scaling term
-        """
-        return (C["c3"] * repi) + (C["c2"] * np.log10(R))
+            if ctx.freeface_ratio.all() == 0.0:
+                C = self.COEFFS_SLOPE[imt]
+            else:
+                C = self.COEFFS_FREEFACE[imt]
+                ctx.slope = ctx.freeface_ratio
 
-    def __get_soil__(self, C, slope, T_15, F_15, D50_15):
-        """
-        Returns the distance scaling term
-        """
-        return (C["c4"] * np.log10(slope)) + (C["c5"] * np.log10(T_15)) + \
-            (C["c6"] * np.log10(100 - F_15)) + \
-            (C["c7"] * np.log10(D50_15 + 0.1))
-
-    def __get_stddevs__(self, C, num_sites, stddev_types):
-        """
-        Returns the total standard deviation
-        """
-        stddevs = []
-        for stddev_type in stddev_types:
-            assert stddev_type in self.DEFINED_FOR_STANDARD_DEVIATION_TYPES
-            if stddev_type == const.StdDev.TOTAL:
-                stddevs.append(
-                    np.log(10.0 ** C["sigma"]) + np.zeros(num_sites))
-        return stddevs
+            mean[m] = (
+                __get_mag__(ctx, C) +
+                __get_dist__(ctx, R, C) +
+                __get_soil__(ctx, C))
+            mean[m] = np.log(10.0 ** mean[m])
+            sig[m] = np.log(10.0 ** C["sigma"])
 
     COEFFS_SLOPE = CoeffsTable(table="""\
     IMT      c0     c1       c2      c3     c4     c5     c6      c7     sigma
