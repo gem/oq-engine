d16b260fa4b3a81b8af47a09d66d354149fcc790
Michele Simionato <michele.simionato@gmail.com>
2021-09-06 17:51:41 +0200

    Removed unused import [ci skip]

--- openquake/server/v1/calc_urls.py
+++ openquake/server/v1/calc_urls.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake. If not, see <http://www.gnu.org/licenses/>.
 
-from django.urls import re_path, include
+from django.urls import re_path
 from openquake.server import views
 
 # each url is prefixed with /v1/calc/
