71b9dbe359da176891eb5bd6ada99bf0b8963489
Michele Simionato <michele.simionato@gmail.com>
2021-09-13 10:22:22 +0200

    Fixed test

--- openquake/hazardlib/tests/gsim/utils.py
+++ openquake/hazardlib/tests/gsim/utils.py
@@ -227,5 +227,4 @@ class BaseGSIMTestCase(unittest.TestCase):
                             msg[par] = getattr(ctx, par)[idx]
                         for par in cmaker.REQUIRES_RUPTURE_PARAMETERS:
                             msg[par] = getattr(ctx, par)
-                        import pdb; pdb.set_trace()
                         raise ValueError(msg)
