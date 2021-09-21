7e8234ae572aa3ee1b61265cea2fd148d2574694
Michele Simionato <michele.simionato@gmail.com>
2021-09-21 12:13:58 +0200

    Added comment [ci skip]

--- openquake/hazardlib/contexts.py
+++ openquake/hazardlib/contexts.py
@@ -637,7 +637,19 @@ class ContextMaker(object):
         probs = self.tom.get_probability_one_or_more_occurrences(
             numpy.array([ctx.occurrence_rate for ctx in ctxs]))  # shape C
         for n in range(N):
-            idxs = numpy.arange(n, N * C, N)  # C indices
+            # NB: to understand the code below, consider the case with
+            # N=3 sites and C=2 contexts; then the indices N*C are
+            # 0: first site
+            # 1: second site
+            # 2: third site
+            # 3: first site
+            # 4: second site
+            # 5: third site
+            # i.e. idxs = [0, 3], [1, 4], [2, 5] for sites 0, 1, 2
+            if N > 1:
+                idxs = numpy.arange(n, N * C, N)  # C indices
+            else:  # fast lane
+                idxs = slice(None)
             for g, (mu_, sig_) in enumerate(mean_stds):
                 mu = mu_[:, idxs]  # shape (M, C)
                 sig = sig_[:, idxs]  # shape (M, C)
@@ -648,11 +660,10 @@ class ContextMaker(object):
                     poes = _truncnorm_sf(self.trunclevel, eps)  # shape C
                     ws = -numpy.log(
                         (1. - probs) ** poes) / self.investigation_time
-                    s[n, p] = w = ws.sum()  # weights not summing up to 1
+                    s[n, p] = ws.sum()  # weights not summing up to 1
                     for m in m_range:
                         c[m, n, 0, p] = ws @ (mu[m] + rho[m] * eps * sig[m])
                         c[m, n, 1, p] = ws @ (sig[m]**2 * (1. - rho[m]**2))
-                    val = c[:, n, 0, p].mean()
         return out
 
     def gen_poes(self, ctxs):
