67a4dfbd41af12db3c99974837ec0d7840d72cf9
Michele Simionato <michele.simionato@gmail.com>
2021-10-11 17:15:21 +0200

    Fixed test

--- openquake/calculators/base.py
+++ openquake/calculators/base.py
@@ -545,10 +545,12 @@ class HazardCalculator(BaseCalculator):
         elif oq.hazard_calculation_id:
             parent = datastore.read(oq.hazard_calculation_id)
             oqparent = parent['oqparam']
-            weights = numpy.unique(parent['weights'][:])
-            if oq.collect_rlzs and len(weights) > 1:
-                raise ValueError('collect_rlzs=true can be specified only if '
-                                 'the realizations have identical weights')
+            if 'weights' in parent:
+                weights = numpy.unique(parent['weights'][:])
+                if oq.collect_rlzs and len(weights) > 1:
+                    raise ValueError(
+                        'collect_rlzs=true can be specified only if '
+                        'the realizations have identical weights')
             check_imtls(self.oqparam.imtls, oqparent.imtls)
             self.check_precalc(oqparent.calculation_mode)
             self.datastore.parent = parent
