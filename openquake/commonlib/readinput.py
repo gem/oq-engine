d0aa683a1b745767d5604bc54d8f9163dcf956ad
Michele Simionato <michele.simionato@gmail.com>
2021-10-19 14:45:25 +0200

    Removed obsolete check on old-style GMPEs [ci skip]

--- openquake/commonlib/readinput.py
+++ openquake/commonlib/readinput.py
@@ -519,17 +519,8 @@ def get_site_model(oqparam):
     return numpy.concatenate(arrays)
 
 
-def count_old_style(gsim_lt):
-    # count the number of old style GMPEs
-    old = 0
-    for gsims in gsim_lt.values.values():
-        for gsim in gsims:
-            old += 'get_mean_and_stddevs' in gsim.__class__.__dict__
-    return old
-
-
 def count_no_vect(gsim_lt):
-    # count the number of not vectorize GMPEs
+    # count the number of nonvectorized GMPEs
     no = 0
     for gsims in gsim_lt.values.values():
         for gsim in gsims:
@@ -622,10 +613,7 @@ def get_gsim_lt(oqparam, trts=('*',)):
         gsim_lt = gsim_lt.collapse(oqparam.collapse_gsim_logic_tree)
     gsim_lt_cache[key] = gsim_lt
     if trts != ('*',):  # not in get_input_files
-        old_style = count_old_style(gsim_lt)
         no_vect = count_no_vect(gsim_lt)
-        if old_style:
-            logging.info('There are %d old style GMPEs', old_style)
         if no_vect:
             logging.info('There are %d not vectorized GMPEs', no_vect)
     return gsim_lt
