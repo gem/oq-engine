e22d9e95cd2088afa4b57cf767d012c4c7d4d5bc
Michele Simionato <michele.simionato@gmail.com>
2021-10-26 06:23:12 +0200

    Fixed bug with mixed list-scalar maximum distance

--- openquake/hazardlib/calc/filters.py
+++ openquake/hazardlib/calc/filters.py
@@ -97,9 +97,13 @@ def floatdict(value):
     value = ast.literal_eval(value)
     if isinstance(value, (int, float, list)):
         return {'default': value}
-    dic = {'default': max(value.values())}
-    dic.update(value)
-    return dic
+    maxval = 0
+    for val in value.values():
+        if isinstance(val, list):
+            val = val[-1][1]
+        maxval = max(val, maxval)
+    value['default'] = maxval
+    return value
 
 
 class MagDepDistance(dict):
