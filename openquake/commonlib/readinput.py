4b4a0e51b2ae1e68470a9850a80beb9967ee06cf
Michele Simionato <michele.simionato@gmail.com>
2021-10-27 16:44:20 +0200

    Geohashing the sitecol when sites_slice is given

--- openquake/commonlib/readinput.py
+++ openquake/commonlib/readinput.py
@@ -559,7 +559,9 @@ def get_site_collection(oqparam, h5=None):
     ss = oqparam.sites_slice  # can be None or (start, stop)
     if ss:
         if 'custom_site_id' not in sitecol.array.dtype.names:
-            raise RuntimeError('sites_slice requires a custom_site_id!')
+            gh = sitecol.geohash(6)
+            assert len(numpy.unique(gh)) == len(gh), 'geohashes are not unique'
+            sitecol.add_col('custom_site_id', 'S6', gh)
         mask = (sitecol.sids >= ss[0]) & (sitecol.sids < ss[1])
         sitecol = sitecol.filter(mask)
         sitecol.make_complete()
