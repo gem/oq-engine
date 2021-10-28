09db66382ca886136ee5c20c7b82ef6d428d4b8e
Michele Simionato <michele.simionato@gmail.com>
2021-10-28 09:32:43 +0200

    Better monitoring [ci skip]

--- openquake/hazardlib/contexts.py
+++ openquake/hazardlib/contexts.py
@@ -264,7 +264,7 @@ class ContextMaker(object):
     def init_monitoring(self, monitor):
         # instantiating child monitors, may be called in the workers
         self.ctx_mon = monitor('make_contexts', measuremem=False)
-        self.gmf_mon = monitor('computing mean_std', measuremem=False)
+        self.gmf_mon = monitor('computing mean_std', measuremem=True)
         self.poe_mon = monitor('get_poes', measuremem=False)
         self.pne_mon = monitor('composing pnes', measuremem=False)
         self.task_no = getattr(monitor, 'task_no', 0)
