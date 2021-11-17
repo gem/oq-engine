874223904c58f7434b63f73d5e7682cfa1481aaf
Michele Simionato <michele.simionato@gmail.com>
2021-11-17 10:28:54 +0100

    Fixed typo [ci skip]

--- openquake/engine/engine.py
+++ openquake/engine/engine.py
@@ -218,7 +218,7 @@ def poll_queue(job_id, poll_time):
                                {'status': 'submitted', 'pid': _PID})
                     first_time = False
                     # the logging is not yet initialized, so use a print
-                    print('Waiting for jobs #%d' % previous.id)
+                    print('Waiting for jobs %s' % previous)
                 time.sleep(poll_time)
             else:
                 break
