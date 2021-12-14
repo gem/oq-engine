85abe46d39bf1e8ac456ec379b5e87595afebebf
Michele Simionato <michele.simionato@gmail.com>
2021-12-14 09:37:45 +0100

    Fixed lt_test

--- openquake/hazardlib/tests/lt_test.py
+++ openquake/hazardlib/tests/lt_test.py
@@ -102,7 +102,7 @@ class CollapseTestCase(unittest.TestCase):
             srcs[0].tectonic_region_type, self.gsims, params)
         res = classical(srcs, self.sitecol, cmaker)
         pmap = res['pmap']
-        effrups = sum(nr for nr, ns, dt in res['source_data'].values())
+        effrups = sum(res['source_data']['nrupts'])
         curve = pmap.array(N)[0, :, 0]
         return curve, srcs, effrups, weights
 
