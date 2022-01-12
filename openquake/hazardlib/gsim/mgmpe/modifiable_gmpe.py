c2b02ba986324c7b1b64d9801285a8892c3bb030
Marco Pagani <marco.pagani@globalquakemodel.org>
2022-01-12 18:45:47 +0100

    Adding import

--- openquake/hazardlib/gsim/mgmpe/modifiable_gmpe.py
+++ openquake/hazardlib/gsim/mgmpe/modifiable_gmpe.py
@@ -21,10 +21,12 @@ Module :mod:`openquake.hazardlib.mgmpe.modifiable_gmpe` implements
 """
 import numpy as np
 from openquake.hazardlib.gsim.base import GMPE, registry, CoeffsTable
+from openquake.hazardlib.contexts import STD_TYPES
 from openquake.hazardlib.const import StdDev
 from openquake.hazardlib import const, contexts
 from openquake.hazardlib.imt import from_string, PGA, PGV
 from openquake.hazardlib.const import IMC
+
 
 IMT_DEPENDENT_KEYS = ["set_scale_median_vector",
                       "set_scale_total_sigma_vector",
