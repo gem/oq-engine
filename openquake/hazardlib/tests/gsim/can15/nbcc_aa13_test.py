ae3b0b49f4e0a4c4eab03d6af51a08c56a845205
Michele Simionato <michele.simionato@gmail.com>
2022-02-10 09:42:31 +0100

    Updated changelog [ci skip]

--- openquake/hazardlib/tests/gsim/can15/nbcc_aa13_test.py
+++ openquake/hazardlib/tests/gsim/can15/nbcc_aa13_test.py
@@ -34,7 +34,7 @@ GLT_XML = os.path.join(CWD, 'data', 'CAN15', 'gmmLT.xml')
 # is parametric; it is easier to build a scenario using ALL of the
 # combinations used in the Canada model 2015
 class NBCC2015_AA13TestCase(unittest.TestCase):
-    def test_mean(self):
+    def test_gmf(self):
         param = dict(rupture_model_file=RUP_XML,
                      gsim_logic_tree_file=GLT_XML,
                      number_of_ground_motion_fields=1,
