df07577dcbcac647f091a17580613eccebea5449
Michele Simionato <michele.simionato@globalquakemodel.org>
2022-02-14 06:39:19 +0100

    Fixed bradley_2013

--- openquake/hazardlib/gsim/bradley_2013.py
+++ openquake/hazardlib/gsim/bradley_2013.py
@@ -66,27 +66,6 @@ def _check_in_cbd_polygon(lons, lats):
     return in_cbd
 
 
-def _check_in_cshm_polygon(rup):
-    """
-    Checks if any part of the rupture surface mesh is located within the
-    intended boundaries of the Canterbury Seismic Hazard Model in
-    Gerstenberger et al. (2014), Seismic hazard modelling for the recovery
-    of Christchurch, Earthquake Spectra, 30(1), 17-29.
-    """
-    lats = np.ravel(rup.surface.mesh.array[1])
-    lons = np.ravel(rup.surface.mesh.array[0])
-    # These coordinates are provided by M Gerstenberger (personal
-    # communication, 10 August 2018)
-    polygon = shapely.geometry.Polygon([(171.6, -43.3), (173.2, -43.3),
-                                        (173.2, -43.9), (171.6, -43.9)])
-    points_in_polygon = [
-        shapely.geometry.Point(lons[i], lats[i]).within(polygon)
-        for i in np.arange(len(lons))]
-    in_cshm = any(points_in_polygon)
-
-    return in_cshm
-
-
 def _get_SRF_phi(imt_per):
     """
     Table 7 and equation 19 of 2013 report. NB change in notation,
@@ -830,9 +809,21 @@ class Bradley2013bChchMaps(Bradley2013bChchCBD):
 
     def set_parameters(self, rup):
         """
-        Check if the rupture is located in the CSHM region
+        Checks if any part of the rupture surface mesh is located within the
+        intended boundaries of the Canterbury Seismic Hazard Model in
+        Gerstenberger et al. (2014), Seismic hazard modelling for the recovery
+        of Christchurch, Earthquake Spectra, 30(1), 17-29.
         """
-        self.in_cshm = _check_in_cshm_polygon(rup)
+        lats = np.ravel(rup.surface.mesh.array[1])
+        lons = np.ravel(rup.surface.mesh.array[0])
+        # These coordinates are provided by M Gerstenberger (personal
+        # communication, 10 August 2018)
+        polygon = shapely.geometry.Polygon([(171.6, -43.3), (173.2, -43.3),
+                                            (173.2, -43.9), (171.6, -43.9)])
+        points_in_polygon = [
+            shapely.geometry.Point(lons[i], lats[i]).within(polygon)
+            for i in np.arange(len(lons))]
+        self.in_cshm = any(points_in_polygon)
 
     def compute(self, ctx: np.recarray, imts, mean, sig, tau, phi):
         """
