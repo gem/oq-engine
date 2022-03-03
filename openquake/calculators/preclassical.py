af24adb01c19367e5645f60f589ba04478f93328
Michele Simionato <michele.simionato@gmail.com>
2022-03-03 15:15:58 +0100

    Fixed split_sources [ci skip]

--- openquake/calculators/preclassical.py
+++ openquake/calculators/preclassical.py
@@ -132,7 +132,9 @@ def run_preclassical(calc):
                 pointlike.append(src)
             elif hasattr(src, 'sections'):  # multifault
                 # this is essential to make the preclassical in UCERF fast
-                for ss in split_source(src):
+                splits = (split_source(src) if calc.oqparam.split_sources else
+                          [src])
+                for ss in splits:
                     ss.num_ruptures = ss.count_ruptures()
                     multifaults[grp_id].append(ss)
             else:
