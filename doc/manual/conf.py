dd1eac6e53c46a59eab33e0de62305393312c99c
Paul Henshaw <pslh@openquake.org>
2022-04-07 19:28:14 +0200

    Line Numbers in xml code listings, figure references, removed references to pages

--- doc/manual/conf.py
+++ doc/manual/conf.py
@@ -105,6 +105,10 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
+#
+# Added by pslh
+#
+numfig = True
 
 # -- Options for HTML output ----------------------------------------------
 
