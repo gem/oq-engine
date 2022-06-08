8e7e90c28a29471dba677bf1c19c817ee2aa2142
Michele Simionato <michele.simionato@gmail.com>
2022-06-08 18:26:12 +0200

    Speedup of _check_csm

--- openquake/commonlib/readinput.py
+++ openquake/commonlib/readinput.py
@@ -736,6 +736,8 @@ def _check_csm(csm, oqparam, h5):
     lons = []
     lats = []
     for src in srcs:
+        if hasattr(src, 'location'):  # ignoring point sources
+            continue
         try:
             box = srcfilter.get_enlarged_box(src)
         except BBoxError as exc:
@@ -745,6 +747,8 @@ def _check_csm(csm, oqparam, h5):
         lats.append(box[1])
         lons.append(box[2])
         lats.append(box[3])
+    if not lons:
+        return
     if cross_idl(*(list(sitecol.lons) + lons)):
         lons = numpy.array(lons) % 360
     else:
@@ -754,9 +758,6 @@ def _check_csm(csm, oqparam, h5):
         raise BBoxError(
             'The bounding box of the sources is larger than half '
             'the globe: %d degrees' % (bbox[2] - bbox[0]))
-    sids = sitecol.within_bbox(bbox)
-    if len(sids) == 0:
-        raise RuntimeError('All sources were discarded!?')
 
 
 def get_composite_source_model(oqparam, h5=None, branchID=None):
