c1f2ce49514691dc3d7108ecdf882a8d12c3697c
Michele Simionato <michele.simionato@gmail.com>
2022-07-13 16:25:46 +0200

    Small cleanup [ci skip]

--- openquake/hazardlib/calc/disagg.py
+++ openquake/hazardlib/calc/disagg.py
@@ -69,8 +69,10 @@ def get_edges_shapedic(oq, sitecol, mags_by_trt):
         mags.update(float(mag) for mag in _mags)
         trts.append(trt)
     mags = sorted(mags)
-    n1 = int(numpy.floor(min(mags) / oq.mag_bin_width))
-    n2 = int(numpy.ceil(max(mags) / oq.mag_bin_width) + 1)
+    min_mag = mags[0]
+    max_mag = mags[-1]
+    n1 = int(numpy.floor(min_mag / oq.mag_bin_width))
+    n2 = int(numpy.ceil(max_mag / oq.mag_bin_width) + 1)
     if n2 == n1 + 1:  # happens when there is a single magnitude
         n2 = n1 + 2
     mag_edges = oq.mag_bin_width * numpy.arange(n1, n2)
