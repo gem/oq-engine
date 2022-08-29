ce647dd62712f87c3b1a7edf69faf7521aab2d91
Michele Simionato <michele.simionato@gmail.com>
2022-08-29 09:49:25 +0200

    Better error message

--- install.py
+++ install.py
@@ -339,8 +339,8 @@ def install(inst, version):
             if 'died with <Signals.SIGABRT' in str(exc):
                 shutil.rmtree(inst.VENV)
                 raise RuntimeError(
-                    'Could not execute ensurepip --upgrade: %s %s'
-                    % (pycmd, 'Probably you are using the system Python (%s)'
+                    'Could not execute ensurepip --upgrade: %s'
+                    % ('Probably you are using the system Python (%s)'
                        % sys.executable))
         subprocess.check_call([pycmd, '-m', 'pip', 'install', '--upgrade',
                                'pip', 'wheel'])
