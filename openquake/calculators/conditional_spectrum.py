0720dc137d88d7563b730d980c3c6f122f105aeb
Michele Simionato <michele.simionato@gmail.com>
2022-10-12 08:57:05 +0200

    More renaming

--- openquake/calculators/conditional_spectrum.py
+++ openquake/calculators/conditional_spectrum.py
@@ -23,7 +23,7 @@ import logging
 import numpy
 
 from openquake.baselib import general, performance
-from openquake.commonlib.calc import compute_hazard_maps
+from openquake.commonlib.calc import compute_hazard_maps, get_mean_curve
 from openquake.hazardlib.imt import from_string
 from openquake.hazardlib.contexts import read_cmakers
 from openquake.hazardlib.calc.cond_spectra import get_cs_out, outdict
@@ -34,18 +34,18 @@ U32 = numpy.uint32
 
 
 # helper function to be used when saving the spectra as an array
-def to_spectra(cwdic, n, p):
+def to_spectra(outdic, n, p):
     """
-    :param cwdic: dictionary rlz_id -> array MNOP
+    :param outdic: dictionary rlz_id -> array MNOP
     :param n: site index in the range 0..N-1
     :param p: IMLs index in the range 0..P-1
     :returns: conditional spectra as an array of shape (R, M, 2)
     """
-    R = len(cwdic)
-    M = len(cwdic[0])
+    R = len(outdic)
+    M = len(outdic[0])
     out = numpy.zeros((R, M, 2))
     for r in range(R):
-        c = cwdic[r]
+        c = outdic[r]
         out[r, :, 0] = numpy.exp(c[:, n, 1, p])  # NB: seems wrong!
         out[r, :, 1] = numpy.sqrt(c[:, n, 2, p])
     return out
@@ -87,17 +87,11 @@ class ConditionalSpectrumCalculator(base.HazardCalculator):
         rdata = numpy.zeros(totrups, rdt)
         rdata['idx'] = numpy.arange(totrups)
         rdata['grp_id'] = dstore['rup/grp_id'][:]
-        # trt_smrs = dstore['trt_smrs'][:]
-        # rlzs_by_gsim = self.full_lt.get_rlzs_by_gsim_list(trt_smrs)
-        # _G = sum(len(rbg) for rbg in rlzs_by_gsim)
         self.periods = [from_string(imt).period for imt in self.imts]
-        if oq.imls_ref:
-            self.imls = oq.imls_ref
-        else:  # extract imls from the "mean" hazard map
-            curve = self.datastore.sel(
-                'hcurves-stats', stat='mean')[0, 0, imti]
-            [self.imls] = compute_hazard_maps(
-                curve, oq.imtls[oq.imt_ref], oq.poes)  # there is 1 site
+        # extract imls from the "mean" hazard map
+        curve = get_mean_curve(self.datastore, oq.imt_ref)
+        # there is 1 site
+        [self.imls] = compute_hazard_maps(curve, oq.imtls[oq.imt_ref], oq.poes)
         self.P = P = len(self.imls)
         self.datastore.create_dset(
             'cs-rlzs', float, (self.R, M, self.N, 2, self.P))
@@ -130,7 +124,7 @@ class ConditionalSpectrumCalculator(base.HazardCalculator):
         # N is the number of sites
         # 3 is the number of statistical momenta
         # P is the the number of IMLs
-        cwdic, cwmean = self._apply_weights(out)
+        outdic, outmean = self._apply_weights(out)
 
         # Computing standard deviation
         for gid, start, stop in performance.idx_start_stop(rdata['grp_id']):
@@ -138,19 +132,19 @@ class ConditionalSpectrumCalculator(base.HazardCalculator):
             cmaker.poes = oq.poes
             ctxs = cmaker.read_ctxs(dstore, slice(start, stop))
             for ctx in ctxs:
-                res = get_cs_out(cmaker, ctx, imti, self.imls, cwmean[0])
+                res = get_cs_out(cmaker, ctx, imti, self.imls, outmean[0])
                 for g in res:
                     out[g][:, :, 2] += res[g][:, :, 2]  # STDDEV
         return out
 
-    def convert_and_save(self, dsetname, cwdic):
+    def convert_and_save(self, dsetname, outdic):
         """
         Save the conditional spectra
         """
         for n in range(self.N):
             for p in range(self.P):  # shape (R, M, N, 2, P)
                 self.datastore[dsetname][:, :, n, :, p] = to_spectra(
-                    cwdic, n, p)  # shape (R, M, 2)
+                    outdic, n, p)  # shape (R, M, 2)
         attrs = dict(imls=self.imls, periods=self.periods)
         if self.oqparam.poes:
             attrs['poes'] = self.oqparam.poes
@@ -158,24 +152,24 @@ class ConditionalSpectrumCalculator(base.HazardCalculator):
 
     def post_execute(self, acc):
         # apply weights
-        cwdic, cwmean = self._apply_weights(acc)
+        outdic, outmean = self._apply_weights(acc)
         # convert dictionaries into spectra and save them
-        self.convert_and_save('cs-rlzs', cwdic)
-        self.convert_and_save('cs-stats', cwmean)
+        self.convert_and_save('cs-rlzs', outdic)
+        self.convert_and_save('cs-stats', outmean)
 
     def _apply_weights(self, acc):
         # build conditional spectra for each realization
         rlzs_by_g = self.datastore['rlzs_by_g'][()]
-        cwdic = outdict(self.M, self.N, self.P, 0, self.R)
+        outdic = outdict(self.M, self.N, self.P, 0, self.R)
         for _g, rlzs in enumerate(rlzs_by_g):
             for r in rlzs:
-                cwdic[r] += acc[_g]
-        self.convert_and_save('cs-rlzs', cwdic)
+                outdic[r] += acc[_g]
+        self.convert_and_save('cs-rlzs', outdic)
 
         # build final conditional mean and std
         weights = self.datastore['weights'][:]
-        cwmean = outdict(self.M, self.N, self.P, 0, 1)  # dict with key 0
+        outmean = outdict(self.M, self.N, self.P, 0, 1)  # dict with key 0
         for r, weight in enumerate(weights):
-            cwmean[0] += cwdic[r] * weight
+            outmean[0] += outdic[r] * weight
 
-        return cwdic, cwmean
+        return outdic, outmean
