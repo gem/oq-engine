43e37f7029ebbdf2fadc79128d6637051d07e94a
Michele Simionato <michele.simionato@gmail.com>
2022-10-17 12:02:00 +0200

    More logging [ci skip]

--- openquake/risklib/reinsurance.py
+++ openquake/risklib/reinsurance.py
@@ -348,7 +348,8 @@ def by_policy_event(agglosses_df, policy_df, treaty_df, mon=Monitor()):
     """
     dfs = []
     for _, policy in policy_df.iterrows():
-        logging.info("Processing policy %d", policy.policy)
+        if policy.policy % 100 == 0:  # starts from 1
+            logging.info("Processing policy %d", policy.policy)
         df = by_policy(agglosses_df, dict(policy), treaty_df)
         df['policy_grp'] = build_policy_grp(policy, treaty_df)
         dfs.append(df)
