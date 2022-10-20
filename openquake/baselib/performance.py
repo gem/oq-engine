0ed494dc6d3d8c09389c727a8472fe85d2a502d6
Michele Simionato <michele.simionato@gmail.com>
2022-10-20 10:24:28 +0200

    Fixed the memory monitoring again [ci skip]

--- openquake/baselib/performance.py
+++ openquake/baselib/performance.py
@@ -242,7 +242,7 @@ class Monitor(object):
         self.exc = exc
         if self.measuremem:
             self.stop_mem = self.measure_mem()
-            self.mem = self.stop_mem - self.start_mem
+            self.mem += self.stop_mem - self.start_mem
         self._stop_time = time.time()
         self.duration += self._stop_time - self._start_time
         self.counts += 1
@@ -344,6 +344,7 @@ class Monitor(object):
         """
         while True:
             try:
+                self.mem = 0
                 with self:
                     obj = next(genobj)
             except StopIteration:
