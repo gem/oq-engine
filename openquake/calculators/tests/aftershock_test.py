8fee69fb3bf2ef27e4db3ba0e0a56f49154390a0
Michele Simionato <michele.simionato@gmail.com>
2022-10-21 06:24:27 +0200

    Cleanup [ci skip]

--- openquake/calculators/tests/aftershock_test.py
+++ openquake/calculators/tests/aftershock_test.py
@@ -34,8 +34,8 @@ class AftershockTestCase(CalculatorTestCase):
 
         # run classical
         hc_id = str(self.calc.datastore.calc_id)
-        self.calc.datastore.close()
         self.run_calc(case_1.__file__, 'job.ini', hazard_calculation_id=hc_id)
 
+        # checking hazard curves
         [fname] = export(('hcurves', 'csv'), self.calc.datastore)
         self.assertEqualFiles('expected/hcurves.csv', fname)
