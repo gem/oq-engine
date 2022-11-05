73faa23b7008794baeaa0fb95229b4b5bef2272e
Michele Simionato <michele.simionato@gmail.com>
2022-11-05 15:05:10 +0100

    Fixed case_48

--- openquake/hazardlib/source/point.py
+++ openquake/hazardlib/source/point.py
@@ -348,7 +348,8 @@ class PointSource(ParametricSeismicSource):
         Bounding box of the point, enlarged by the maximum distance
         """
         self.max_radius()  # set .radius by side effect
-        delta = self.ps_grid_spacing * .707  # ps_grid * sqrt(2)/2
+        # delta = 2. in classical case_48
+        delta = 2. + self.ps_grid_spacing * .707  # ps_grid * sqrt(2)/2
         return get_bounding_box([self.location], maxdist + delta)
 
     def wkt(self):
