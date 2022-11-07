9ad2d62365ebe907c5da45ffce6cddf7b195aa7c
Michele Simionato <michele.simionato@gmail.com>
2022-11-07 15:27:26 +0100

    Moved output outside event_based_risk

--- openquake/calculators/event_based_risk.py
+++ openquake/calculators/event_based_risk.py
@@ -88,7 +88,6 @@ def aggreg(outputs, crmodel, ARK, aggids, rlz_id, monitor):
     """
     mon_agg = monitor('aggregating losses', measuremem=False)
     mon_avg = monitor('averaging losses', measuremem=False)
-    mon_df = monitor('building event loss table', measuremem=True)
     oq = crmodel.oqparam
     xtypes = oq.ext_loss_types
     loss_by_AR = {ln: [] for ln in xtypes}
@@ -118,20 +117,7 @@ def aggreg(outputs, crmodel, ARK, aggids, rlz_id, monitor):
                     aids = alt.aid.to_numpy()
                     for kids in aggids[:, aids]:
                         fast_agg(eids + U64(kids), values, correl, li, acc)
-    with mon_df:
-        dic = general.AccumDict(accum=[])
-        for ukey, arr in acc.items():
-            eid, kid = divmod(ukey, TWO32)
-            for li in range(L):
-                if arr[li].any():
-                    dic['event_id'].append(eid)
-                    dic['agg_id'].append(kid)
-                    dic['loss_id'].append(LOSSID[xtypes[li]])
-                    for c, col in enumerate(value_cols):
-                        dic[col].append(arr[li, c])
-        fix_dtypes(dic)
-        df = pandas.DataFrame(dic)
-    return loss_by_AR, df
+    return loss_by_AR, acc
 
 
 def ebr_from_gmfs(sbe, oqparam, dstore, monitor):
@@ -175,6 +161,25 @@ def ebr_from_gmfs(sbe, oqparam, dstore, monitor):
             yield event_based_risk, df[s0:s1], oqparam
 
 
+def outputs(taxo_assets, df, crmodel, rng, monitor):
+    mon_risk = monitor('computing risk', measuremem=True)
+    fil_mon = monitor('filtering GMFs', measuremem=False)
+    for s0, s1 in performance.split_slices(df.eid.to_numpy(), 250_000):
+        grp = df[s0:s1]
+        for taxo, adf in taxo_assets:
+            with fil_mon:
+                # *crucial* for the performance
+                # of the next step, 'computing risk'
+                gmf_df = grp[numpy.isin(
+                    grp.sid.to_numpy(), adf.site_id.to_numpy())]
+            if len(gmf_df) == 0:
+                continue
+            with mon_risk:  # this is using a lot of memory
+                out = crmodel.get_output(
+                    adf, gmf_df, crmodel.oqparam._sec_losses, rng)
+            yield out
+
+
 def event_based_risk(df, oqparam, monitor):
     """
     :param df: a DataFrame of GMFs with fields sid, eid, gmv_X, ...
@@ -199,26 +204,23 @@ def event_based_risk(df, oqparam, monitor):
         rng = MultiEventRNG(oqparam.master_seed, df.eid.unique(),
                             int(oqparam.asset_correlation))
 
-    def outputs():
-        mon_risk = monitor('computing risk', measuremem=True)
-        fil_mon = monitor('filtering GMFs', measuremem=False)
-        for s0, s1 in performance.split_slices(df.eid.to_numpy(), 250_000):
-            grp = df[s0:s1]
-            for taxo, adf in taxo_assets:
-                with fil_mon:
-                    # *crucial* for the performance
-                    # of the next step, 'computing risk'
-                    gmf_df = grp[numpy.isin(
-                        grp.sid.to_numpy(), adf.site_id.to_numpy())]
-                if len(gmf_df) == 0:
-                    continue
-                with mon_risk:  # this is using a lot of memory
-                    out = crmodel.get_output(
-                        adf, gmf_df, oqparam._sec_losses, rng)
-                yield out
-
-    avg, alt = aggreg(outputs(), crmodel, ARK, aggids, rlz_id, monitor)
-    return dict(avg=avg, alt=alt)
+    avg, acc = aggreg(outputs(taxo_assets, df, crmodel, rng, monitor),
+                      crmodel, ARK, aggids, rlz_id, monitor)
+    xtypes = oqparam.ext_loss_types
+    lis = range(len(xtypes))
+    with monitor('building event loss table', measuremem=True):
+        dic = general.AccumDict(accum=[])
+        for ukey, arr in acc.items():
+            eid, kid = divmod(ukey, TWO32)
+            for li in lis:
+                if arr[li].any():
+                    dic['event_id'].append(eid)
+                    dic['agg_id'].append(kid)
+                    dic['loss_id'].append(LOSSID[xtypes[li]])
+                    for c, col in enumerate(['variance', 'loss']):
+                        dic[col].append(arr[li, c])
+        fix_dtypes(dic)
+    return dict(avg=avg, alt=pandas.DataFrame(dic))
 
 
 def ebrisk(proxies, full_lt, oqparam, dstore, monitor):
