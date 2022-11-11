731a19427601e837545af3db8798d4ff75ded429
Michele Simionato <michele.simionato@gmail.com>
2022-11-11 09:40:28 +0100

    Updated docstrings [ci skip]

--- openquake/engine/postproc/rupture_histogram.py
+++ openquake/engine/postproc/rupture_histogram.py
@@ -4,6 +4,14 @@ from openquake.hazardlib import contexts
 from openquake.commonlib import datastore
 
 def compute_hist(dstore, slc, cmaker, magbins, dstbins):
+    """
+    :param dstore: parent datastore
+    :param slc: a slice object referring to a slice of contexts
+    :param cmaker: a ContextMaker instance
+    :param magbins: array of magnitude bins
+    :param magbins: array of distance bins
+    :returns: a dictionary (mag_index, dst_index) -> counts
+    """
     dic = general.AccumDict(accum=0)  # (magi, dsti) -> counts
     with dstore:
         [ctxt] = cmaker.read_ctxs(dstore, slc)
@@ -19,6 +27,10 @@ def compute_hist(dstore, slc, cmaker, magbins, dstbins):
 
 def main(parent_id: int, mbins=100, dbins=100):
     """
+    :param parent_id: ID of the parent calculation with the contexts
+    :param mbins: number of magnitude bins
+    :param dbins: number of distance bins
+    
     NB: this is meant to work only for parametric ruptures!
     """
     parent = datastore.read(parent_id)
@@ -26,14 +38,14 @@ def main(parent_id: int, mbins=100, dbins=100):
     magbins = numpy.linspace(2, 10.2, mbins)
     dstbins = contexts.sqrscale(0, 1000., dbins)
     with dstore, log:
-        cmakers = contexts.read_cmakers(dstore.parent)
+        cmakers = contexts.read_cmakers(parent)
         grp_ids = dstore.parent['rup/grp_id'][:]
         dstore.swmr_on()
         smap = parallel.Starmap(compute_hist, h5=dstore)
         for grp_id, slices in performance.get_slices(grp_ids).items():
             cmaker = cmakers[grp_id]
             for s0, s1 in slices:
-                smap.submit((dstore, slice(s0, s1), cmaker, magbins, dstbins))
+                smap.submit((parent, slice(s0, s1), cmaker, magbins, dstbins))
         acc = smap.reduce()
         counts = numpy.zeros((mbins, dbins), int)
         for k, v in acc.items():
