f9152d098614a5e03491eb474961ef84e980be3c
Anirudh Rao <anirudh.rao@globalquakemodel.org>
2022-11-14 10:23:00 +0100

    Redirect the scenario/event_based gmf calculator to conditioned_gmfs if station_data is in the inputs

--- openquake/calculators/base.py
+++ openquake/calculators/base.py
@@ -484,6 +484,13 @@ class HazardCalculator(BaseCalculator):
             check_amplification(df, self.sitecol)
             self.af = AmplFunction.from_dframe(df)
 
+        if 'station_data' in oq.inputs:
+            logging.info('Reading station data from %s', oq.inputs['station_data'])
+            self.station_data, self.station_sites, self.observed_imts = readinput.get_station_data(oq)
+            self.datastore.create_df('station_data', self.station_data)
+            self.datastore.create_df('station_sites', self.station_sites)
+            oq.observed_imts = self.observed_imts
+
         if (oq.calculation_mode == 'disaggregation' and
                 oq.max_sites_disagg < len(self.sitecol)):
             raise ValueError(
