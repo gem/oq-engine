77d80cc3b284b80aa964027d487ce74b2ac35995
Michele Simionato <michele.simionato@gmail.com>
2022-11-20 06:15:46 +0100

    Refined num_tasks

--- openquake/calculators/classical.py
+++ openquake/calculators/classical.py
@@ -335,7 +335,7 @@ def decide_num_tasks(dstore, concurrent_tasks):
     cmakers = read_cmakers(dstore)
     weight = dstore.read_df('source_info')[
         ['grp_id', 'weight']].groupby('grp_id').sum().weight.to_numpy()
-    maxw = weight.sum() / concurrent_tasks / 2
+    maxw = weight.sum() / concurrent_tasks / 2.5
     dtlist = [('grp_id', U16), ('cmakers', U16), ('tiles', U16)]
     ntasks = []
     for cm in sorted(cmakers, key=lambda cm: weight[cm.grp_id], reverse=True):
