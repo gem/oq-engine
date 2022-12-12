7bb571c3f36a26859c30bcf0da5ecf12030b5a2a
tomchartier <thomaschartier0@gmail.com>
2022-12-12 12:06:54 +0100

    adding loop for tests

--- openquake/hazardlib/tests/gsim/lanzano_2019_mod2022_test.py
+++ openquake/hazardlib/tests/gsim/lanzano_2019_mod2022_test.py
@@ -38,6 +38,48 @@ class Lanzano2019Modified2022Test(unittest.TestCase):
         ctx.rrup = np.array([1., 10., 30., 70.])
         ctx.rjb = np.array([1., 10., 30., 70.])
 
+
+        # manually computed correction coefficients
+        # iml, Vs30, kappa, coef
+        self.manual_coef = [[PGA(), 760., 0.02, -0.124167819],
+                            [PGA(), 1500., 0.02, -0.236062501],
+                            [PGA(), 900., 0.02, -0.148654094],
+                            [PGA(), 900., 0.04, -0.384154094],
+                            [SA(period=0.1), 900., 0.02, -0.152040029],
+                            [SA(period=0.1), 900., 0.01, -0.0019],
+                            [SA(period=0.1), 900., 0.04, -0.452320029],
+                            [SA(period=1.), 900., 0.02, -0.109817918],
+                            [SA(period=1.), 1500., 0.02, -0.265333892]]
+
+
+    def test_adjustement_to_ref(self):
+        """ Checks that the modified GMM provides the expected values """
+
+        for i_test in range(len(self.manual_coef)):
+            sites = Dummy.get_site_collection(4, vs30=self.manual_coef[i_test][1])
+            for name in sites.array.dtype.names:
+                setattr(ctx, name, sites[name])
+            self.imt = self.manual_coef[i_test][0]
+
+            stds_types = [const.StdDev.TOTAL, const.StdDev.INTRA_EVENT,
+                          const.StdDev.INTER_EVENT]
+            gmm = LanzanoEtAl2019_RJB_OMO(kappa0=self.manual_coef[i_test][2])
+            out = gmm.get_mean_and_stddevs(self.ctx, self.ctx, self.ctx,
+                                           self.imt, stds_types)
+            # Expected results hand computed
+            breakpoint()
+            gmm_no_correction = LanzanoEtAl2019_RJB_OMO(kappa0=None)
+            out_no_correction = gmm.get_mean_and_stddevs(self.ctx,
+                                    self.ctx, self.ctx,
+                                     self.imt, stds_types)
+            # Correction for PGA Vs30 760 and kappa 0.02
+            c = self.manual_coef[i_test][3]
+            expected = np.exp(out_no_correction[0]) + c
+
+            aae = np.testing.assert_array_almost_equal
+            aae(expected, np.exp(out[0]))
+
+
     def test_adjustement_to_ref_1(self):
         """ Checks that the modified GMM provides the expected values """
 
@@ -56,8 +98,8 @@ class Lanzano2019Modified2022Test(unittest.TestCase):
         gmm_no_correction = LanzanoEtAl2019_RJB_OMO(kappa0=None)
         out_no_correction = gmm.get_mean_and_stddevs(self.ctx, self.ctx, self.ctx,
                                  self.imt, stds_types)
-        # Correction for PGA Vs30 780 and kappa 0.02
-        c = -0.124167819
+        # Correction for PGA Vs30 760 and kappa 0.02
+        c = -0.1197231
         expected = np.exp(out_no_correction[0]) + c
 
         aae = np.testing.assert_array_almost_equal
