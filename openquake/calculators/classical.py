2c0138039b281b1ba8b7bc66def45cb115efac97
Michele Simionato <michele.simionato@gmail.com>
2022-12-16 06:27:39 +0100

    Fix

--- openquake/calculators/classical.py
+++ openquake/calculators/classical.py
@@ -639,7 +639,7 @@ class ClassicalCalculator(base.HazardCalculator):
         """
         assert self.N > self.oqparam.max_sites_disagg, self.N
         logging.info('Running %d tiles', ntiles)
-        for n, tile in enumerate(self.sitecol.split_in_tiles(ntiles)):
+        for n, tile in enumerate(self.sitecol.split(ntiles)):
             self.run_one(tile, maxw)
             logging.info('Finished tile %d of %d', n+1, ntiles)
 
