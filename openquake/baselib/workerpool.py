9fd6a12343f62aaccd33e7acbf7566480ed31ca5
Michele Simionato <michele.simionato@gmail.com>
2023-01-13 07:58:24 +0100

    Fixed MasterWorker.kill [ci skip]

--- openquake/baselib/workerpool.py
+++ openquake/baselib/workerpool.py
@@ -123,7 +123,7 @@ class WorkerMaster(object):
         """
         killed = []
         for host, cores, args in ssh_args(self.zworkers):
-            args = args[:-1] + ['killall', '-r', 'oq-zworker']
+            args = args[:-1] + ['killall', '-r', 'oq-zworker|multiprocessing']
             print(' '.join(args))
             subprocess.run(args)
             killed.append(host)
