e9c4ab0969d9156f0d8f7646ca6cf7380b0b583f
Marco Pagani <marco.pagani@globalquakemodel.org>
2023-01-17 12:36:29 +0100

    Implementing the adjustement

--- openquake/hazardlib/tests/gsim/lanzano_2019_mod2022_test.py
+++ openquake/hazardlib/tests/gsim/lanzano_2019_mod2022_test.py
@@ -17,64 +17,45 @@
 # along with OpenQuake. If not, see <http://www.gnu.org/licenses/>.
 #
 
-import unittest
-import numpy as np
-from openquake.hazardlib import const
-from openquake.hazardlib.imt import PGA, SA
-from openquake.hazardlib.contexts import RuptureContext
-from openquake.hazardlib.tests.gsim.mgmpe.dummy import Dummy
-from openquake.hazardlib.gsim.lanzano_2019 import LanzanoEtAl2019_RJB_OMO
-from openquake.hazardlib.gsim.lanzano_2019 import LanzanoEtAl2019_RJB_OMO
+from openquake.hazardlib.tests.gsim.utils import BaseGSIMTestCase
+from openquake.hazardlib.gsim.lanzano_2019 import (
+    LanzanoEtAl2019_RJB_OMO_RefRock)
 
 
-class Lanzano2019Modified2022Test(unittest.TestCase):
+class LanzanoEtAl2019_RJB_OMO_RefRock_TestCase(BaseGSIMTestCase):
+    GSIM_CLASS = LanzanoEtAl2019_RJB_OMO_RefRock
 
-    def setUp(self):
-        self.ctx = ctx = RuptureContext()
-        ctx.mag = 6.
-        ctx.rake = 0.
-        ctx.hypo_depth = 10.
-        ctx.occurrence_rate = .001
-        ctx.rrup = np.array([1., 10., 30., 70.])
-        ctx.rjb = np.array([1., 10., 30., 70.])
-        self.ctx = ctx
+    # Tables provided by original authors
+    MEAN_FILE = "LAN2019/ITA18_MOD22_RJB_MEAN.csv"
+    STD_FILE = "LAN2019/ITA18_MOD22_RJB_STD.csv"
 
-        # manually computed correction coefficients
-        # iml, Vs30, kappa, coef
-        self.manual_coef = [[PGA(), 760., 0.02, -0.1197231],
-                            [PGA(), 1500., 0.02, -0.236062501],
-                            [PGA(), 900., 0.02, -0.148654094],
-                            [PGA(), 900., 0.04, -0.384154094],
-                            [SA(period=0.1), 900., 0.02, -0.152040029],
-                            [SA(period=0.1), 900., 0.01, -0.0019],
-                            [SA(period=0.1), 900., 0.04, -0.452320029],
-                            [SA(period=1.), 900., 0.02, -0.109817918],
-                            [SA(period=1.), 1500., 0.02, -0.265333892]]
+    def test_all(self):
+        self.check(self.MEAN_FILE, self.STD_FILE,
+                   max_discrep_percentage=0.1)
 
 
-    def test_adjustement_to_ref(self):
-        """ Checks that the modified GMM provides the expected values """
+"""
+class LanzanoEtAl2019_RUP_OMO_TestCase(BaseGSIMTestCase):
+    GSIM_CLASS = LanzanoEtAl2019_RUP_OMO
 
-        for i_test in range(len(self.manual_coef)):
-            sites = Dummy.get_site_collection(4, vs30=self.manual_coef[i_test][1])
-            for name in sites.array.dtype.names:
-                setattr(self.ctx, name, sites[name])
-            self.imt = self.manual_coef[i_test][0]
+    # Tables provided by original authors
+    MEAN_FILE = "LAN2019/ITA18_Rrup_MEAN.csv"
+    STD_FILE = "LAN2019/ITA18_Rrup_STD_TOTAL.csv"
 
-            stds_types = [const.StdDev.TOTAL, const.StdDev.INTRA_EVENT,
-                          const.StdDev.INTER_EVENT]
-            gmm = LanzanoEtAl2019_RJB_OMO(kappa0=self.manual_coef[i_test][2])
-            out = gmm.get_mean_and_stddevs(self.ctx, self.ctx, self.ctx,
-                                           self.imt, stds_types)
-            # Expected results hand computed
-            breakpoint()
-            gmm_no_correction = LanzanoEtAl2019_RJB_OMO(kappa0=None)
-            out_no_correction = gmm.get_mean_and_stddevs(self.ctx,
-                                    self.ctx, self.ctx,
-                                     self.imt, stds_types)
-            # Correction for PGA Vs30 760 and kappa 0.02
-            c = self.manual_coef[i_test][3]
-            expected = np.exp(out_no_correction[0]) + c
+    def test_all(self):
+        self.check(self.MEAN_FILE, self.STD_FILE,
+                   max_discrep_percentage=0.1)
 
-            aae = np.testing.assert_array_almost_equal
-            aae(expected, np.exp(out[0]))
+
+class LanzanoEtAl2019_RJB_OMOscaled_TestCase(BaseGSIMTestCase):
+    GSIM_CLASS = LanzanoEtAl2019_RJB_OMOscaled
+
+    # Tables provided by original authors
+
+    MEAN_FILE = "LAN2019/ITA18_RJB_MEAN_scaled.csv"
+    STD_FILE = "LAN2019/ITA18_RJB_STD_TOTAL_scaled.csv"
+
+    def test_all(self):
+        self.check(self.MEAN_FILE, self.STD_FILE,
+                   max_discrep_percentage=0.1)
+"""
