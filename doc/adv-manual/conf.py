077d45c910c98fe561ecc2629f5c5382982e7c4e
Matteo Nastasi <nastasi@openquake.org>
2023-01-24 18:04:03 +0100

    version switcher moved at the begin of header

--- doc/adv-manual/conf.py
+++ doc/adv-manual/conf.py
@@ -139,7 +139,7 @@ html_theme = 'pydata_sphinx_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    "navbar_end": ["version-switcher"],
+    "navbar_begin": ["version-switcher"],
     "switcher": {
         "json_url": "../../.ddown_adv.json",
         "version_match": "master"
