7cea7553abc395c29f43ce23eb042d8ef6d4b47b
Michele Simionato <michele.simionato@gmail.com>
2023-02-01 13:47:09 +0100

    Using more tiles [ci skip]

--- openquake/calculators/classical.py
+++ openquake/calculators/classical.py
@@ -591,7 +591,7 @@ class ClassicalCalculator(base.HazardCalculator):
 
         t0 = time.time()
         req = get_pmaps_gb(self.datastore)
-        ntiles = 1 + int(req / (oq.pmap_max_gb * 40))  # 40 GB
+        ntiles = 1 + int(req / (oq.pmap_max_gb * 30))  # 30 GB
         self.n_outs = AccumDict(accum=0)
         if ntiles > 1:
             self.execute_seq(maxw, ntiles)
