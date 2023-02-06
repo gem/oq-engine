23a3a1dd3b09fe07bcb58db71f6d271a1a0ba699
Michele Simionato <michele.simionato@gmail.com>
2023-02-06 17:46:12 +0100

    Changed the wait a bit [ci skip]

--- openquake/baselib/parallel.py
+++ openquake/baselib/parallel.py
@@ -458,10 +458,9 @@ def sendback(res, zsocket, sentbytes):
     nbytes = len(res.pik)
     # avoid output congestion by waiting a bit
     wait = config.performance.slowdown_rate * nbytes
-    #if res.mon.duration > wait:
-    #    # slow outputs do not wait
-    #    wait = 0
-    time.sleep(wait/2)
+    if res.mon.duration < wait:
+        # fast outputs must wait a bit
+        time.sleep(wait/2)
     try:
         zsocket.send(res)
         time.sleep(wait/2)
