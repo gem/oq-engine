5977d7d5173cb8b2e3830a36cf29c848cd0dd558
Michele Simionato <michele.simionato@gmail.com>
2023-02-06 07:30:20 +0100

    Better logging

--- openquake/baselib/parallel.py
+++ openquake/baselib/parallel.py
@@ -473,7 +473,7 @@ def sendback(res, zsocket, sentbytes):
     wait = config.performance.slowdown_rate * nbytes
     if res.msg == '' and res.mon.duration < wait:  # fast output
         if DEBUG:
-            dblog('DEBUG', calc_id, task_no, 'waiting %.2fs' % wait)
+            dblog('DEBUG', calc_id, task_no, 'wait for %.2f s' % wait)
         # avoid output congestion by waiting a bit
         time.sleep(wait)
     return sentbytes + nbytes
