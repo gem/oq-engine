f0f223ae52a17898d29b28c3bee9374b5df6cc12
Michele Simionato <michele.simionato@gmail.com>
2023-02-07 05:13:35 +0100

    Cleanup [ci skip]

--- openquake/calculators/classical.py
+++ openquake/calculators/classical.py
@@ -593,7 +593,7 @@ class ClassicalCalculator(base.HazardCalculator):
 
         t0 = time.time()
         req = get_pmaps_gb(self.datastore)
-        ntiles = 1 + int(req / (oq.pmap_max_gb * 80))  # 40 GB
+        ntiles = 1 + int(req / (oq.pmap_max_gb * 60))  # 30 GB
         self.n_outs = AccumDict(accum=0)
         if ntiles > 1:
             self.execute_seq(maxw, ntiles)
