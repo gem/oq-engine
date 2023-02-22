d8a1bd12a5883fd3d98ea2b82c95bdc20b4bdab4
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2023-02-22 10:45:11 +0100

    Add a note about the customization of UserAdmin

--- openquake/server/admin.py
+++ openquake/server/admin.py
@@ -5,6 +5,13 @@ from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
 
+# NOTE: this customization only moves the email to a more visible section, as
+#       a way to remind to the creator of the user that the email is an
+#       important field. In fact, it is used for email notifications. In order
+#       to avoid tricky issues related to the modification of the standard
+#       django User model, the email field still remains optional. However,
+#       email notifications will be disabled in case the email is not
+#       specified.
 class UserAdmin(UserAdmin):
     fieldsets = (
         (None, {'fields': ('username', 'password', 'email')}),
