9600f83c03d8b0bd769d2c03acaa589f641a8419
Michele Simionato <michele.simionato@gmail.com>
2023-02-27 14:30:19 +0100

    Contributed the GMPE Arteta et al. (2023)

---
+++ openquake/hazardlib/tests/gsim/arteta_2023_nosam_test.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+#
+# Copyright (C) 2015-2023 GEM Foundation
+#
+# OpenQuake is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# OpenQuake is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with OpenQuake. If not, see <http://www.gnu.org/licenses/>.
+
+from openquake.hazardlib.gsim.arteta_2023_nosam import (
+    ArtetaEtAl2023_NoSAm_Vs30, ArtetaEtAl2023_NoSAm)
+from openquake.hazardlib.tests.gsim.utils import BaseGSIMTestCase
+
+"""
+Module imports :class:`ArtetaEtAl2023_NoSAm_Vs30`,`ArtetaEtAl2023_NoSAm`
+The test tables were created from a *.xlsx file created by the authors
+"""
+
+class ArtetaEtAl2023_NoSAm_Vs30Test(BaseGSIMTestCase):
+    """
+    Test the default model, the total standard deviation and the within-event
+    standard deviation. The between events std is implicitly tested
+    """
+
+    GSIM_CLASS = ArtetaEtAl2023_NoSAm_Vs30
+
+    def test_median(self):
+        self.check('arteta2023_NoSam/ARTETAETAL2023_Vs30_NoSAm_MEAN.csv',
+                   max_discrep_percentage=0.1)
+
+    def test_std(self):
+        self.check('arteta2023_NoSam/ARTETAETAL2023_Vs30_NoSAm_Sigma.csv',
+                   'arteta2023_NoSam/ARTETAETAL2023_Vs30_NoSAm_Phi.csv',
+                   'arteta2023_NoSam/ARTETAETAL2023_Vs30_NoSAm_Tau.csv',
+                   max_discrep_percentage=0.1)
+
+
+class ArtetaEtAl2023_NoSAm_Test(BaseGSIMTestCase):
+    """
+    Test the default model, the total standard deviation and the within-event
+    standard deviation. The between events std is implicitly tested
+    """
+
+    GSIM_CLASS = ArtetaEtAl2023_NoSAm
+
+    def test_median(self):
+        self.check('arteta2023_NoSam/ARTETAETAL2023_NoSAm_MEAN.csv',
+                   max_discrep_percentage=0.1)
+
+    def test_std(self):
+        self.check('arteta2023_NoSam/ARTETAETAL2023_NoSAm_Sigma.csv',
+                   'arteta2023_NoSam/ARTETAETAL2023_NoSAm_Phi.csv',
+                   'arteta2023_NoSam/ARTETAETAL2023_NoSAm_Tau.csv',
+                   max_discrep_percentage=0.1)
+
+
