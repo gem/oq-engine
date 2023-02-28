1eb87b780c44cc69e4567f45c447830e3597e60d
Michele Simionato <michele.simionato@gmail.com>
2023-02-28 07:07:28 +0100

    Fixed test

--- openquake/hazardlib/logictree.py
+++ openquake/hazardlib/logictree.py
@@ -318,7 +318,7 @@ class SourceModelLogicTree(object):
         dic = dict(filename='fake.xml', seed=0, num_samples=0,
                    sampling_method='early_weights', num_paths=1,
                    sms_by_src=AccumDict(accum=[]), trt_by_src={},
-                   is_source_specific=0,
+                   srcs_by_path=AccumDict(accum=[]), is_source_specific=0,
                    tectonic_region_types=set(),
                    bsetdict='{"bs0": {"uncertaintyType": "sourceModel"}}')
         self.__fromh5__(arr, dic)
