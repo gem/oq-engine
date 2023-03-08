d1242cc8d6bbb6743eaad1e5b1dbc99cc90d9268
Michele Simionato <michele.simionato@gmail.com>
2023-03-08 16:53:13 +0100

    Added comment [ci skip]

--- openquake/calculators/tests/logictree_test.py
+++ openquake/calculators/tests/logictree_test.py
@@ -264,6 +264,7 @@ hazard_uhs-std.csv
         self.assertEqual(aw.site_id, 0)
         self.assertEqual(aw.imt, 'PGA')
         self.assertEqual(aw.poe, .001)
+        # the numbers are quite different on macOS :-(
         aac(aw.array['poe'], [6.467104e-05, 0, 0], atol=1E-7)
 
         # testing view_relevant_sources
