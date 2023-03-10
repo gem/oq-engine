f0b63179e241367c34e95d6fa6e6e57e96d8a675
Michele Simionato <michele.simionato@gmail.com>
2023-03-10 07:38:44 +0100

    Better logging [ci skip]

--- openquake/engine/postproc/compute_mrd.py
+++ openquake/engine/postproc/compute_mrd.py
@@ -79,8 +79,8 @@ def main(parent_id, config):
         N = len(parent['sitecol'])
         L1 = oq.imtls.size // len(oq.imtls) - 1
         if L1 > 24:
-            logging.warning('There are many levels (%d), the calculation can '
-                            'be pretty slow', L1 + 1)
+            logging.warning('There are many intensity levels (%d), the '
+                            'calculation can be pretty slow', L1 + 1)
         assert N <= 10, 'Too many sites: %d' % N
         cmakers = contexts.read_cmakers(parent)
         ctx_by_grp = contexts.read_ctx_by_grp(dstore)
