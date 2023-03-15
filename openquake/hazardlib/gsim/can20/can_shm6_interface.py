d9e73b227354f61c38dc697974f11a9bb813d3fb
Shreyasvi <shreyasvic@globalqaukemodel.org>
2023-03-15 12:11:00 +0530

    fixed intergface GMPE

--- openquake/hazardlib/gsim/can20/can_shm6_interface.py
+++ openquake/hazardlib/gsim/can20/can_shm6_interface.py
@@ -28,7 +28,7 @@ import openquake.hazardlib.gsim.atkinson_macias_2009 as AM09
 import openquake.hazardlib.gsim.can20.can_shm6_active_crust as CanadaSHM6_ASC
 import openquake.hazardlib.gsim.ghofrani_atkinson_2014 as GA14
 import openquake.hazardlib.gsim.zhao_2006 as ZH06
-
+import openquake.hazardlib.gsim.can20.can_shm6_inslab as CanadaSHM6_InSlab
 from scipy.constants import g
 from openquake.hazardlib import const
 from openquake.hazardlib.imt import PGA, SA, PGV
@@ -158,8 +158,11 @@ class CanadaSHM6_Interface_ZhaoEtAl2006SInterCascadia(
         """
         for m, imt in enumerate(imts):
 
-            # Extrapolation
-            extrapolate, imt, target_imt = _set_extrapolation(imt, self)
+            if imt == PGV():
+                extrapolate, imt, target_imt = False, SA(1.92), PGV
+            else:
+                # Extrapolation
+                extrapolate, imt, target_imt = _set_extrapolation(imt, self)
 
             # extracting dictionary of coefficients specific to required
             # intensity measure type.
@@ -171,23 +174,31 @@ class CanadaSHM6_Interface_ZhaoEtAl2006SInterCascadia(
             # faulting style and intraslab terms (that is FR, SS, SSL = 0) and
             # inter and intra event terms, plus the magnitude-squared term
             # correction factor (equation 5 p. 909)
+            
             mean[m] = ZH06._compute_magnitude_term(C, ctx.mag) +\
                 ZH06._compute_distance_term(C, ctx.mag, ctx.rrup) +\
                 ZH06._compute_focal_depth_term(C, self.HYPO_DEPTH) +\
-                ZH06._compute_site_class_term(C, ctx.vs30) + \
+                CanadaSHM6_InSlab._compute_site_class_term_CanadaSHM6(C, ctx, imt) +\
                 ZH06._compute_magnitude_squared_term(P=0.0, M=6.3,
                                                 Q=C_SINTER['QI'],
                                                 W=C_SINTER['WI'],
                                                 mag=ctx.mag) +\
                 C_SINTER['SI']
+             # convert from cm/s**2 to g
+            mean[m] = np.log((np.exp(mean[m]) * C_SF["MF"]) * 1e-2 / g)
 
-            if target_imt == PGV():
-                mean[m] = (0.897*mean[m]) + 4.835
-
-            # convert from cm/s**2 to g
-            mean[m] = np.log(np.exp(mean[m] * C_SF["MF"]) * 1e-2 / g)
             ZH06._set_stddevs(
                 sig[m], tau[m], phi[m], C['sigma'], C_SINTER['tauI'])
+           
+
+            if extrapolate:
+                mean[m] += extrapolation_factor(
+                    self.extrapolate_GMM, ctx, imt, target_imt)
+
+            if target_imt == PGV:
+                
+                mean[m] = (0.897*mean[m]) + 4.835
+            
 
 
     # Coefs taken from ZhaoEtAl2006SInter
@@ -279,12 +290,12 @@ class CanadaSHM6_Interface_AtkinsonMacias2009(AtkinsonMacias2009):
             C = AtkinsonMacias2009.COEFFS[imt]
 
             # AM09 is for Vs30 = 760m/s
-            imean = _get_mean_760_am09(ctx, imt)
-            imean += _site_term_am09(ctx, imt)
+            mean[m] = _get_mean_760_am09(ctx, imt)
+            mean[m] += _site_term_am09(ctx, imt)
             sig[m] = np.log(10.0 ** C["sigma"])
 
             if fix:
-                mean[m] = (0.897*imean) + 4.835
+                mean[m] = (0.897*mean[m]) + 4.835
 
 # =============================================================================
 # =============================================================================
@@ -317,8 +328,7 @@ def _set_extrapolation(imt, model):
     target_imt = None
     if imt == PGV():
         extrapolate = False
-        imt = SA(1.92)
-    elif imt.period < model.MIN_SA and imt.period >= model.MIN_SA_EXTRAP:
+    if imt.period < model.MIN_SA and imt.period >= model.MIN_SA_EXTRAP:
         target_imt = imt
         imt = SA(model.MIN_SA)
         extrapolate = True
@@ -365,7 +375,7 @@ class CanadaSHM6_Interface_GhofraniAtkinson2014Cascadia(
         for spec of input and result values.
         """
         for m, imt in enumerate(imts):
-
+            
             # Extrapolation
             extrapolate, imt, target_imt = _set_extrapolation(imt, self)
 
@@ -373,7 +383,7 @@ class CanadaSHM6_Interface_GhofraniAtkinson2014Cascadia(
             C = self.COEFFS[imt]
 
             # Compute the median
-            imean = (GA14._get_magnitude_term(C, ctx.mag) +
+            mean[m] = (GA14._get_magnitude_term(C, ctx.mag) +
                      GA14._get_distance_term(
                          C, ctx.rrup, np.bool_(ctx.backarc)) +
                      _get_site_term_ga14(C, ctx.vs30, imt) +
@@ -382,12 +392,12 @@ class CanadaSHM6_Interface_GhofraniAtkinson2014Cascadia(
             # Convert mean from cm/s and cm/s/s and from common logarithm to
             # natural logarithm
             if imt.string.startswith(('PGA', 'SA')):
-                mean[m] = np.log((10.0 ** (imean - 2.0)) / g)
+                mean[m] = np.log((10.0 ** (mean[m] - 2.0)) / g)
             else:
-                mean[m] = np.log((10.0 ** (imean)))
+                mean[m] = np.log((10.0 ** (mean[m])))
 
             if extrapolate:
-                mean += extrapolation_factor(
+                mean[m] += extrapolation_factor(
                     self.extrapolate_GMM, ctx, imt, target_imt)
 
             sig[m] = np.log(10.0 ** np.sqrt(C["tau"] ** 2. + C["sigma"] ** 2.))
