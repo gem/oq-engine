9342cf8b45835b68920548ac08d17e0fb960c481
Michele Simionato <michele.simionato@gmail.com>
2023-03-31 14:46:37 +0200

    Restored Marco's test comparing direct and indirect methods

--- openquake/calculators/tests/postproc_test.py
+++ openquake/calculators/tests/postproc_test.py
@@ -16,16 +16,142 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
 
-#from openquake.calculators.export import export
+import numpy as np
+import matplotlib.pyplot as plt
+from openquake.baselib.performance import Monitor
+from openquake.hazardlib.calc.mrd import (
+    update_mrd, get_uneven_bins_edges, calc_mean_rate_dist)
+from openquake.hazardlib.contexts import read_cmakers, read_ctx_by_grp
+from openquake.hazardlib.cross_correlation import BakerJayaram2008
 from openquake.calculators.tests import CalculatorTestCase
 from openquake.qa_tests_data.postproc import case_mrd
+
+PLOT = False
 
 
 class PostProcTestCase(CalculatorTestCase):
     def test_mrd(self):
+        # Computes the mean rate density using a simple PSHA input model
         self.run_calc(case_mrd.__file__, 'job.ini', postproc_func='dummy')
         hc_id = str(self.calc.datastore.calc_id)
 
         self.run_calc(case_mrd.__file__, 'job.ini', hazard_calculation_id=hc_id)
         mrd = self.calc.datastore['mrd'][:]
-        assert abs(mrd.mean() - 8.403599676e-6) < 1e-10
+        assert abs(mrd.mean() - 2.334333e-07) < 1e-12, mrd.mean()
+
+        # Settings
+        imts = ['SA(0.2)', 'SA(1.0)']  # subset of the parent IMTs
+
+        # Load datastore
+        dstore = self.calc.datastore
+        oqp = dstore['oqparam']
+
+        # Read the context maker and set the IMTLS
+        cmaker = read_cmakers(dstore)[0].restrict(imts)
+
+        # Read contexts
+        ctx = read_ctx_by_grp(dstore)[0]
+
+        # Set the cross correlation model
+        self.crosscorr = BakerJayaram2008()
+
+        # Compute the MRD
+        imls1 = oqp.imtls[imts[0]]
+        imls2 = oqp.imtls[imts[1]]
+        len1 = len(imls1) - 1
+        len2 = len(imls2) - 1
+        mrd = np.zeros((len1, len2, len(cmaker.gsims)))
+        update_mrd(ctx, cmaker, self.crosscorr, mrd)
+
+        # Loading Hazard Curves.
+        # The poes array is 4D: |sites| x |stats| x |IMTs| x |IMLs|
+        poes = dstore['hcurves-stats'][:]
+        afe = - np.log(1-poes)
+        afo = afe[:, :, :, :-1] - afe[:, :, :, 1:]
+
+        _imts = list(oqp.imtls)
+        idx1 = _imts.index(imts[0])
+        idx2 = _imts.index(imts[1])
+
+        afo1 = afo[0, 0, idx1, :]
+        afo2 = afo[0, 0, idx2, :]
+
+        c1 = imls1[:-1] + np.diff(imls1) / 2
+        c2 = imls2[:-1] + np.diff(imls2) / 2
+
+        # Compute marginal
+        marg1 = mrd.sum(axis=0)[:, 0]
+        marg2 = mrd.sum(axis=1)[:, 0]
+
+        # Test very different
+        #np.testing.assert_almost_equal(marg1, afo1, decimal=5)
+        #np.testing.assert_almost_equal(marg2, afo2, decimal=5)
+
+        if PLOT:
+            plt.title('Direct method test')
+            plt.plot(c1, afo1, label=f'HC {imts[0]}')
+            plt.plot(c1, marg1, 'o', mfc='None')
+            plt.plot(c2, afo2, label=f'HC {imts[1]}')
+            plt.plot(c2, marg2, 'o', mfc='None', )
+            plt.xlabel('Spectral Acceleration, S$_a$ [g]')
+            plt.ylabel('Annual Rate of Occurrence')
+            plt.legend()
+            plt.grid(which='minor', ls=':', color='lightgrey')
+            plt.grid(which='major', ls='--', color='grey')
+            plt.show()
+
+        ## test_indirect
+
+        # Bin edges
+        lefts = [-3, -2, 1, 2]
+        numb = [80, 80, 10]
+        be_mea = get_uneven_bins_edges(lefts, numb)
+        be_sig = np.arange(0.50, 0.70, 0.01)
+
+        # Compute the MRD
+        mon = Monitor('multivariate')
+        mrdi = calc_mean_rate_dist(ctx, 1, cmaker, self.crosscorr,
+                                   imts[0], imts[1], be_mea, be_sig, mon)
+        print(mon)
+
+        # Compute marginal
+        marg1 = mrdi.sum(axis=0)[:, 0, 0]
+        marg2 = mrdi.sum(axis=1)[:, 0, 0]
+
+        # Test very different
+        #np.testing.assert_almost_equal(marg1, afo1, decimal=5)
+        #np.testing.assert_almost_equal(marg2, afo2, decimal=5)
+
+        if PLOT:
+            plt.title('Indirect method test')
+            plt.plot(c1, afo1, label=f'HC {imts[0]}')
+            plt.plot(c1, marg1, 'o', mfc='None')
+            plt.plot(c2, afo2, label=f'HC {imts[1]}')
+            plt.plot(c2, marg2, 'o', mfc='None', )
+            plt.xlabel('Spectral Acceleration, S$_a$ [g]')
+            plt.ylabel('Annual Rate of Occurrence')
+            plt.legend()
+            plt.grid(which='minor', ls=':', color='lightgrey')
+            plt.grid(which='major', ls='--', color='grey')
+            plt.show()
+
+        ## test_compare
+
+        # Bin edges
+        be_mea = get_uneven_bins_edges([-3, -2, 1, 2], [80, 80, 10])
+        be_sig = np.arange(0.50, 0.70, 0.01)
+
+        # very different
+        # np.testing.assert_almost_equal(mrdi[:, :, 0], mrd)
+
+        if PLOT:
+            fig, axs = plt.subplots(1, 1)
+            fig.set_size_inches(9, 6)
+            plt1 = plt.contourf(np.log(c1), np.log(c2), mrd[:, :, 0, 0])
+            _ = plt.contour(np.log(c1), np.log(c2), mrdi[:, :, 0, 0],
+                            colors='orange', linestyles='dashed')
+            _ = plt.colorbar(mappable=plt1)
+            _ = plt.title('MRD')
+            axs.set_xlabel(f'{imts[0]}')
+            axs.set_ylabel(f'{imts[1]}')
+            plt.show()
