462c80e23c7e2f9789294eb4aa0464fc40b939bb
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2023-04-20 10:31:11 +0200

    Log username, ip address and referer of user on login (either successful or failed) and logout

---
+++ openquake/server/signals.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+#
+# Copyright (C) 2016-2023 GEM Foundation
+#
+# OpenQuake is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# OpenQuake is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.contrib.auth.signals import (
+    user_logged_in, user_logged_out, user_login_failed)
+
+logger = logging.getLogger(__name__)
+
+
+@receiver(user_logged_in)
+def log_user_login(sender, request, user, **kwargs):
+    logger.info(
+        f'User "{user.username}" (IP: {request.META.get("REMOTE_ADDR")})'
+        f' logged in through {request.META.get("HTTP_REFERER")}')
+
+
+@receiver(user_login_failed)
+def log_user_login_failed(sender, credentials, request, **kwargs):
+    username = credentials.get("username")
+    msg = (
+        f'User "{username}"'
+        f' (IP: {request.META.get("REMOTE_ADDR")}) failed to log in'
+        f' through {request.META.get("HTTP_REFERER")}')
+    if not User.objects.filter(username=username).exists():
+        msg += ' (no user with such a username exists)'
+    logger.warning(msg)
+
+
+@receiver(user_logged_out)
+def log_user_logout(sender, request, user, **kwargs):
+    logger.info(
+        f'User "{user.username}" (IP: {request.META.get("REMOTE_ADDR")})'
+        f' logged out through {request.META.get("HTTP_REFERER")}')
