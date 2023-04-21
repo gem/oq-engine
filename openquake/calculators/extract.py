e75bbb2c729a564972ecb2bb090bf84808a622f0
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2023-04-21 09:39:31 +0200

    In case of collect_rlzs, indicate 'Collected rlzs' instead of 'rlz-000'

--- openquake/calculators/extract.py
+++ openquake/calculators/extract.py
@@ -755,7 +755,7 @@ def extract_agg_curves(dstore, what):
         oq = dstore['oqparam']
         if oq.collect_rlzs:
             R = 1
-            qdic['kind'] = ['rlz-000']  # TODO: try to change that
+            qdic['kind'] = ['Collected rlzs']
         else:
             R = len(qdic['kind'])
         arr = numpy.zeros((P, R))
