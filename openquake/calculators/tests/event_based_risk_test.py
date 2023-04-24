fb2ee31615df4a23370a7374a828659862e93433
Michele Simionato <michele.simionato@gmail.com>
2023-04-24 09:46:30 +0200

    Removed numpy.random.seed from calc.gmf

--- openquake/calculators/tests/event_based_risk_test.py
+++ openquake/calculators/tests/event_based_risk_test.py
@@ -110,7 +110,7 @@ agg_id
 
         aw = extract(self.calc.datastore, 'agg_losses/structural')
         self.assertEqual(aw.stats, ['mean'])
-        numpy.testing.assert_allclose(aw.array, [880.4989], atol=.001)
+        numpy.testing.assert_allclose(aw.array, [733.92126], atol=.001)
 
         fnames = export(('aggrisk', 'csv'), self.calc.datastore)
         for fname in fnames:
