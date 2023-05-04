eb918ddf054beca584b733d83ef1efc211dad707
Kendra Johnson <kendra.johnson@globalquakemodel.org>
2023-05-04 09:51:59 +0200

    adds solution and test for multifault disagg by TRT with two+ sites

--- openquake/hazardlib/geo/surface/multi.py
+++ openquake/hazardlib/geo/surface/multi.py
@@ -180,19 +180,27 @@ class MultiSurface(BaseSurface):
         # the mesh points then the calculation is skipped
         lons = np.empty_like(mesh.lons.flatten())
         lats = np.empty_like(mesh.lats.flatten())
+        
         depths = None if mesh.depths is None else \
             np.empty_like(mesh.depths.flatten())
-        for ii, srf in enumerate(self.surfaces):
-            # tested in hazardlib.geo.surface.multi.test_get_closest_point
-            if not idx[ii, :].any():
-                continue
-            else:
-                surf, i = srf, ii
-        cps = surf.get_closest_points(mesh)
-        lons[idx[i, :]] = cps.lons.flatten()[idx[i, :]]
-        lats[idx[i, :]] = cps.lats.flatten()[idx[i, :]]
-        if depths is not None:
-            depths[idx[i, :]] = cps.depths.flatten()[idx[i, :]]
+
+        # the centroid info for the sites must be evaluated and populated
+        # one site at a time
+        for j,jdx in enumerate(idx.T):
+            #for ii, srf in enumerate(self.surfaces):
+                # tested in hazardlib.geo.surface.multi.test_get_closest_point
+            #    if not jdx[ii]:
+            #        continue
+            #    else:
+            #        surf, i = srf, ii
+            i = int(np.where(jdx)[0])
+            surf = self.surfaces[i]
+
+            cps = surf.get_closest_points(mesh)
+            lons[idx[i, :]] = cps.lons.flatten()[idx[i, :]]
+            lats[idx[i, :]] = cps.lats.flatten()[idx[i, :]]
+            if depths is not None:
+                depths[idx[i, :]] = cps.depths.flatten()[idx[i, :]]
         lons = lons.reshape(mesh.lons.shape)
         lats = lats.reshape(mesh.lats.shape)
         if depths is not None:
