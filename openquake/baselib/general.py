f0fef42c6c74c5cef1545429478fdc3bd8f4542c
Michele Simionato <michele.simionato@gmail.com>
2023-05-10 09:05:49 +0200

    Changed the ordering of the imtls

--- openquake/baselib/general.py
+++ openquake/baselib/general.py
@@ -754,7 +754,6 @@ def copyobj(obj, **kwargs):
 class DictArray(Mapping):
     """
     A small wrapper over a dictionary of arrays with the same lenghts.
-    Ordered by the lexicographic order of the keys.
     """
     def __init__(self, imtls):
         levels = imtls[next(iter(imtls))]
@@ -762,12 +761,12 @@ class DictArray(Mapping):
         self.L1 = len(levels)
         self.size = self.M * self.L1
         self.dt = numpy.dtype([(str(imt), F64, (self.L1,))
-                               for imt, imls in sorted(imtls.items())])
+                               for imt, imls in imtls.items()])
         self.array = numpy.zeros((self.M, self.L1), F64)
         self.slicedic = {}
         n = 0
         self.mdic = {}
-        for m, (imt, imls) in enumerate(sorted(imtls.items())):
+        for m, (imt, imls) in enumerate(imtls.items()):
             if len(imls) != self.L1:
                 raise ValueError('imt=%s has %d levels, expected %d' %
                                  (imt, len(imls), self.L1))
--- openquake/calculators/tests/event_based_risk_test.py
+++ openquake/calculators/tests/event_based_risk_test.py
@@ -110,7 +110,7 @@ agg_id
 
         aw = extract(self.calc.datastore, 'agg_losses/structural')
         self.assertEqual(aw.stats, ['mean'])
-        numpy.testing.assert_allclose(aw.array, [870.47925], atol=.001)
+        numpy.testing.assert_allclose(aw.array, [840.0337], atol=.001)
 
         fnames = export(('aggrisk', 'csv'), self.calc.datastore)
         for fname in fnames:
