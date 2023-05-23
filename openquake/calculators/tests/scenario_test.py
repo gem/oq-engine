2f81ae65f0f781e0be7c8b29071f2a439359e6d3
Michele Simionato <michele.simionato@gmail.com>
2023-05-23 06:06:38 +0200

    Fixed test

--- openquake/calculators/tests/scenario_test.py
+++ openquake/calculators/tests/scenario_test.py
@@ -176,7 +176,7 @@ class ScenarioTestCase(CalculatorTestCase):
             sorted(assetcol.array.dtype.names),
             sorted(['id', 'ordinal', 'lon', 'lat', 'site_id', 'value-area',
                     'value-contents', 'value-nonstructural', 'value-number',
-                    'occupants_night', 'value-structural',
+                    'occupants_avg', 'occupants_night', 'value-structural',
                     'ideductible', 'taxonomy', 'NAME_2', 'ID_2', 'ID_1',
                     'OCCUPANCY', 'NAME_1']))
 
