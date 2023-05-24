650ba7dd3d69f4672d3c313b12286417f0578437
Michele Simionato <michele.simionato@gmail.com>
2023-05-24 08:58:19 +0200

    Removed a sorting

--- openquake/hazardlib/geo/utils.py
+++ openquake/hazardlib/geo/utils.py
@@ -211,9 +211,7 @@ class _GeographicObjects(object):
             raise SiteAssociationError(
                 'Could not associate any site to any assets within the '
                 'asset_hazard_distance of %s km' % assoc_dist)
-        assets_by_site = [
-            sorted(assets_by_sid[sid], key=operator.attrgetter('ordinal'))
-            for sid in sids]
+        assets_by_site = [assets_by_sid[sid] for sid in sids]
         data = [(asset.asset_id, asset.lon, asset.lat) for asset in discarded]
         discarded = numpy.array(data, asset_dt)
         return self.objects.filtered(sids), assets_by_site, discarded
