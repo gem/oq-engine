670c3760f0da11a4a7478c6a6b90cfef9cf9ba6a
Michele Simionato <michele.simionato@gmail.com>
2023-07-14 07:07:28 +0200

    Backported critical fix https://github.com/gem/oq-engine/pull/8902

--- openquake/calculators/getters.py
+++ openquake/calculators/getters.py
@@ -267,7 +267,6 @@ def get_rupture_getters(dstore, ct=0, srcfilter=None):
     rup_array = dstore['ruptures'][:]
     if len(rup_array) == 0:
         raise NotFound('There are no ruptures in %s' % dstore)
-    rup_array.sort(order=['trt_smr', 'n_occ', 'seed'])
     proxies = [RuptureProxy(rec) for rec in rup_array]
     maxweight = rup_array['n_occ'].sum() / (ct / 2 or 1)
     rgetters = []
