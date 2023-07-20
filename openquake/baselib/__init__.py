2f9d947a924e868549935b029f3e852c49d63a62
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2023-07-20 16:32:19 +0200

    Update changelog and version to v3.17.2

--- openquake/baselib/__init__.py
+++ openquake/baselib/__init__.py
@@ -130,4 +130,4 @@ else:  # linux
     config.multi_user = install_user in ('root', 'openquake')
 
 # the version is managed by packager.sh with a sed
-__version__ = '3.17.1'
+__version__ = '3.17.2'
