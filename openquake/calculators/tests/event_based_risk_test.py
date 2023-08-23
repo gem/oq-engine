f02ad20f2ea0040f05a85b1a304215981143c8ac
Michele Simionato <michele.simionato@gmail.com>
2023-08-23 12:26:52 +0200

    Raised tolerance

--- openquake/calculators/tests/event_based_risk_test.py
+++ openquake/calculators/tests/event_based_risk_test.py
@@ -544,7 +544,8 @@ agg_id
         out = self.run_calc(case_8.__file__,  'job.ini', exports='csv',
                             concurrent_tasks='4')
         for fname in out['aggrisk', 'csv']:
-            self.assertEqualFiles('expected/' + strip_calc_id(fname), fname)
+            self.assertEqualFiles('expected/' + strip_calc_id(fname), fname,
+                                  delta=1E-5)
 
     # NB: big difference between Ubuntu 18 and 20
     def test_asset_loss_table(self):
