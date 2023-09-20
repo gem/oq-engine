81670b2a53001be7ac9771d7e1d0b61741d44e67
LanaT <lana.todorovic@iusspavia.it>
2023-09-20 16:43:16 +0200

    Clean up.

--- openquake/sep/tests/liquefaction/test_vertical_settlement.py
+++ openquake/sep/tests/liquefaction/test_vertical_settlement.py
@@ -11,16 +11,6 @@ class TestHazusVerticalSettlement(unittest.TestCase):
         self.mag = 7.5
         self.pga = np.array([0.15, 0.20, 0.25, 0.30, 0.35, 0.40])
 
-
-    # def test_hazus_vertical_settlement_single_m(self):
-    #     v_settle = hazus_vertical_settlement('vh', mag=self.mag, pga=self.pga[0], return_unit='m')
-    #     np.testing.assert_almost_equal(v_settle, 0.0722051)
-
-    # def test_hazus_vertical_settlement_single_in(self):
-    #     v_settle = hazus_vertical_settlement('vh', mag=self.mag, pga=self.pga[0], return_unit='in')
-    #     np.testing.assert_almost_equal(v_settle, 2.8427208)
-
-
     def test_hazus_vertical_settlement_list_m(self):
         v_settle = hazus_vertical_settlement(self.all_liq_types, mag=self.mag,pga=self.pga, return_unit='m')
         np.testing.assert_array_almost_equal(
