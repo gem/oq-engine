c4706dbebbd01d6f08635437ef651dd3bd37fba9
LanaT <lana.todorovic@iusspavia.it>
2023-09-20 16:30:16 +0200

    Minor fix.

--- openquake/sep/liquefaction/vertical_settlement.py
+++ openquake/sep/liquefaction/vertical_settlement.py
@@ -29,7 +29,7 @@ HAZUS_VERT_SETTLEMENT_TABLE = {
 
 
 def hazus_vertical_settlement(
-    liq_susc_cat: Union[str, List[Union[str, bytes]]],
+    liq_susc_cat: Union[str, List[str]],
     pga: Union[float, np.ndarray],
     mag: Union[float, np.ndarray],
     settlement_table: dict = HAZUS_VERT_SETTLEMENT_TABLE,
@@ -60,25 +60,18 @@ def hazus_vertical_settlement(
     :returns:
         Displacements from vertical settlement in meters or inches.
     """
+    
     if isinstance(liq_susc_cat, pd.Series):
         liq_susc_cat = liq_susc_cat.tolist()
 
-    if not isinstance(liq_susc_cat, list):
-        liq_susc_cat = [liq_susc_cat]
+    liq_susc_cat = [x.encode() if isinstance(x, str) else x for x in liq_susc_cat]
 
-    vert_settlement = []
+    if isinstance(liq_susc_cat, list):
+        vert_settlement = np.array([settlement_table[susc_cat] for susc_cat in liq_susc_cat])
+    else:
+        vert_settlement = settlement_table[liq_susc_cat]
 
-    for susc_cat in liq_susc_cat:
-        if isinstance(susc_cat, str):
-            key = susc_cat.encode()
-        elif isinstance(susc_cat, bytes):
-            key = susc_cat
-        
-        vert_settlement.append(settlement_table[key])
-
-    vert_settlement = np.array(vert_settlement)
-
-    liquefaction_prob = hazus_liquefaction_probability(pga, mag, liq_susc_cat,
+    liquefaction_prob = hazus_liquefaction_probability(pga, mag, liq_susc_cat, 
                                                        groundwater_depth, do_map_proportion_correction)
 
     vert_settlement *= liquefaction_prob
@@ -89,5 +82,6 @@ def hazus_vertical_settlement(
         vert_settlement /= INCH_PER_M
     else:
         raise ValueError("Please choose 'm' or 'in' for return_unit.")
+
 
     return vert_settlement
