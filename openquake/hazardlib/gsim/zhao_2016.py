a78fe02c9e30e878b4f0aad711d27fcf711c6f08
Michele Simionato <michele.simionato@gmail.com>
2023-09-21 10:27:09 +0200

    Removed clon, clat in REQUIRES_DISTANCES

--- openquake/hazardlib/gsim/zhao_2016.py
+++ openquake/hazardlib/gsim/zhao_2016.py
@@ -949,10 +949,10 @@ class ZhaoEtAl2016SSlabPErg(ZhaoEtAl2016Asc):
                                    'ztor', 'rake', 'strike', 'dip'}
     
     # Requires site coordinates for ray tracing
-    REQUIRES_SITES_PARAMETERS = {'vs30','lon','lat'}
+    REQUIRES_SITES_PARAMETERS = {'vs30', 'lon', 'lat'}
 
-    #: Required distance measure is Rrup, Rvolc and closest_point,
-    REQUIRES_DISTANCES = {'rrup', 'rvolc', 'closest_point', 'clon', 'clat'}
+    #: Required distance measure is Rrup, Rvolc and closest_point
+    REQUIRES_DISTANCES = {'rrup', 'rvolc', 'closest_point'}
     
     # Set coeff tables
     COEFFS = COEFFS_SLAB
