0f2bf9f87123e972567ee42a0e8fded807e354c3
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2023-10-11 10:24:16 +0200

    Restore check on mean_rates_ss

--- openquake/calculators/postproc/compute_rtgm.py
+++ openquake/calculators/postproc/compute_rtgm.py
@@ -506,8 +506,7 @@ def main(dstore, csm):
     if not rtgmpy:
         logging.warning('Missing module rtgmpy: skipping AELO calculation')
         return
-    # FIXME: double-check this (it was checking mean_rates_ss)
-    if dstore['mean_rates_by_src'][:].max() < 1E-3:
+    if dstore['mean_rates_ss'][:].max() < 1E-3:
         logging.warning('Ultra-low hazard: skipping AELO calculation')
         return
     logging.info('Computing Risk Targeted Ground Motion')
