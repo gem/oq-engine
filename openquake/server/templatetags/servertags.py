d1b546b9c2f76424628d9d42046d4aa8ce414fb8
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2023-10-12 16:29:53 +0200

    Improve get_disagg_by_src_imt templatetag

--- openquake/server/templatetags/servertags.py
+++ openquake/server/templatetags/servertags.py
@@ -8,4 +8,4 @@ def get_disagg_by_src_imt(string):
     """
     Get imt from a string like 'disagg_by_src-PGA.png'
     """
-    return string.split('-')[1][:-4]
+    return string.split('-')[1].rstrip('.png')
