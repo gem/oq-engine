8eaf15a77b17ae01e921f508dd24b8af9d175fe5
Matteo Nastasi <nastasi@openquake.org>
2023-10-18 12:02:55 +0200

    unblock sep_models hang [att. 7.7]

--- doc/manual/conf.py
+++ doc/manual/conf.py
@@ -343,7 +343,6 @@ latex_elements = {
     'inputenc':     '\\usepackage[utf8]{inputenc}',
     'utf8extra': r"""
 \DeclareUnicodeCharacter{22EE}{\ensuremath{\vdots}}
-\DeclareUnicodeCharacter{2212}{\textendash}
 """,
 }
 
