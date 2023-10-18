defe69a08b8f923fdf58d35a1aa6633fdb2afc0e
Matteo Nastasi <nastasi@openquake.org>
2023-10-18 11:05:55 +0200

    unblock sep_models hang [att. 7.1]

--- doc/manual/conf.py
+++ doc/manual/conf.py
@@ -340,10 +340,11 @@ latex_elements = {
 
     # Latex figure (float) alignment
     # 'figure_align': 'htbp',
+    'inputenc':     '\\usepackage[utf8]{inputenc}',
     'utf8extra': r"""
 \DeclareUnicodeCharacter{22EE}{\ensuremath{\vdots}}
-\DeclareUnicodeCharacter{2212}{−}
 """,
+    # \DeclareUnicodeCharacter{2212}{−}
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
