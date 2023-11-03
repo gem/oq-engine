15b4a0ca687bf193621d6706c06c2e3585f2e168
Matteo Nastasi <nastasi@openquake.org>
2023-11-03 14:25:56 +0100

    typo fixed

--- doc23/conf.py
+++ doc23/conf.py
@@ -5,12 +5,13 @@
 import sys
 import os
 import shutil
-from openquake import engine
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
+
+from openquake import engine
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
