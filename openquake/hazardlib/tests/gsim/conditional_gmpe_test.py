8d1d69ac67ea48b68f9307c36133d272e533b697
Graeme Weatherill <weatherilloqhazard@gmail.com>
2023-11-03 16:30:29 +0100

    Implementation conditional GMM and Macedo (2019)

--- openquake/hazardlib/tests/gsim/conditional_gmpe_test.py
+++ openquake/hazardlib/tests/gsim/conditional_gmpe_test.py
@@ -79,7 +79,7 @@ class ConditionalGMPETestCase(unittest.TestCase):
                 const.StdDev.TOTAL,
             }
 
-            REQUIRES_SITES_PARAMETERS = set()
+            REQUIRES_SITES_PARAMETERS = {}
             REQUIRES_RUPTURE_PARAMETERS = {
                 "mag",
             }
