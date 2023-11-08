cc1c08234f442dd0d16fe6cf4bd4e93635654ba7
Graeme Weatherill <weatherilloqhazard@gmail.com>
2023-11-08 11:14:00 +0100

    Adds classical hazard test for Macedo GMM, revises IMT combinations

--- openquake/calculators/tests/classical_test.py
+++ openquake/calculators/tests/classical_test.py
@@ -32,64 +32,14 @@ from openquake.calculators.export import export
 from openquake.calculators.extract import extract
 from openquake.calculators.tests import CalculatorTestCase
 from openquake.qa_tests_data.classical import (
-    case_01,
-    case_02,
-    case_03,
-    case_04,
-    case_12,
-    case_18,
-    case_22,
-    case_23,
-    case_24,
-    case_25,
-    case_26,
-    case_27,
-    case_29,
-    case_32,
-    case_33,
-    case_34,
-    case_35,
-    case_37,
-    case_38,
-    case_40,
-    case_41,
-    case_42,
-    case_43,
-    case_44,
-    case_47,
-    case_48,
-    case_49,
-    case_50,
-    case_51,
-    case_53,
-    case_54,
-    case_55,
-    case_57,
-    case_60,
-    case_61,
-    case_62,
-    case_63,
-    case_64,
-    case_65,
-    case_66,
-    case_67,
-    case_69,
-    case_70,
-    case_72,
-    case_74,
-    case_75,
-    case_76,
-    case_77,
-    case_78,
-    case_80,
-    case_81,
-    case_82,
-    case_83,
-    case_84,
-    case_85,
-    case_86,
-    case_87,
-)
+    case_01, case_02, case_03, case_04, case_12, case_18, case_22, case_23,
+    case_24, case_25, case_26, case_27, case_29, case_32, case_33,
+    case_34, case_35, case_37, case_38, case_40, case_41,
+    case_42, case_43, case_44, case_47, case_48, case_49,
+    case_50, case_51, case_53, case_54, case_55, case_57,
+    case_60, case_61, case_62, case_63, case_64, case_65, case_66,
+    case_67, case_69, case_70, case_72, case_74, case_75, case_76, case_77,
+    case_78, case_80, case_81, case_82, case_83, case_84, case_85, case_86, case_87)
 
 ae = numpy.testing.assert_equal
 aac = numpy.testing.assert_allclose
@@ -981,11 +931,8 @@ class ClassicalTestCase(CalculatorTestCase):
     def test_case_87(self):
         # Check execution of NGAEastUSGSGMPE with the Coastal Plains
         # site amplification model within a classical PSHA calculation
-        self.assert_curves_ok(
-            [
-                "hazard_curve-mean-SA(0.2).csv",
-                "hazard_curve-mean-SA(1.0).csv",
-                "hazard_curve-mean-SA(2.0).csv",
-            ],
-            case_87.__file__,
-        )
+        self.assert_curves_ok([
+            'hazard_curve-mean-SA(0.2).csv',
+            'hazard_curve-mean-SA(1.0).csv',
+            'hazard_curve-mean-SA(2.0).csv'],
+            case_87.__file__)
--- openquake/hazardlib/tests/gsim/conditional_gmpe_test.py
+++ openquake/hazardlib/tests/gsim/conditional_gmpe_test.py
@@ -79,13 +79,9 @@ class ConditionalGMPETestCase(unittest.TestCase):
                 const.StdDev.TOTAL,
             }
 
-            REQUIRES_SITES_PARAMETERS = {}
-            REQUIRES_RUPTURE_PARAMETERS = {
-                "mag",
-            }
-            REQUIRES_DISTANCES = {
-                "rrup",
-            }
+            REQUIRES_SITES_PARAMETERS = set()
+            REQUIRES_RUPTURE_PARAMETERS = {"mag", }
+            REQUIRES_DISTANCES = {"rrup", }
 
             def compute(
                 self,
