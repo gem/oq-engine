b8a74d93561189c0fe35d3e194825be5b27cfdfb
Graeme Weatherill <weatherilloqhazard@gmail.com>
2023-11-08 11:14:00 +0100

    Adds classical hazard test for Macedo GMM, revises IMT combinations

--- openquake/calculators/tests/classical_test.py
+++ openquake/calculators/tests/classical_test.py
@@ -39,7 +39,7 @@ from openquake.qa_tests_data.classical import (
     case_50, case_51, case_53, case_54, case_55, case_57,
     case_60, case_61, case_62, case_63, case_64, case_65, case_66,
     case_67, case_69, case_70, case_72, case_74, case_75, case_76, case_77,
-    case_78, case_80, case_81, case_82, case_83, case_84, case_86, case_87)
+    case_78, case_80, case_81, case_82, case_83, case_84, case_85, case_86, case_87)
 
 ae = numpy.testing.assert_equal
 aac = numpy.testing.assert_allclose
@@ -824,3 +824,9 @@ class ClassicalTestCase(CalculatorTestCase):
             'hazard_curve-mean-SA(1.0).csv',
             'hazard_curve-mean-SA(2.0).csv'],
             case_87.__file__)
+
+    def test_case_85(self):
+        # Conditional GMM (Macedo et al. (2019)) for Arias Inensity
+        self.run_calc(case_85.__file__, 'job.ini')
+        [f1] = export(('hcurves/mean', 'csv'), self.calc.datastore)
+        self.assertEqualFiles('expected/hazard_curve-mean-IA.csv', f1)
