9e2ed3a67b3656b3dfc15e5fb836912dbfac0202
Antonio Ettorre <56338190+vot4anto@users.noreply.github.com>
2023-11-09 16:04:37 +0100

    Update utils.py
    remove  import for gdal

--- openquake/sep/utils.py
+++ openquake/sep/utils.py
@@ -6,10 +6,6 @@ import logging
 import numpy as np
 import pandas as pd
 import pyproj as pj
-try:
-    from osgeo import gdal
-except ImportError:
-    gdal = None
 from numpy.lib.stride_tricks import as_strided
 
 
