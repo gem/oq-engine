1bdfab78650727240af32a2c48052fe01ad8c8ed
Michele Simionato <michele.simionato@gmail.com>
2023-12-01 10:28:40 +0100

    Cleanup of the GlobalModelGetter [ci skip]

--- openquake/commonlib/global_model_getter.py
+++ openquake/commonlib/global_model_getter.py
@@ -5,7 +5,6 @@ import pprint
 import logging
 import time
 import csv
-import sys
 import os
 from shapely.geometry import Point, shape
 from collections import Counter
@@ -22,7 +21,7 @@ class GlobalModelGetter:
     """
     def __init__(self, kind='mosaic', shapefile_path=None):
         if kind not in ('mosaic', 'global_risk'):
-            raise ValueError('Model getter for {kind} is not implemented')
+            raise ValueError(f'Model getter for {kind} is not implemented')
         self.kind = kind
         if self.kind == 'mosaic':
             self.model_code = 'code'
@@ -30,13 +29,12 @@ class GlobalModelGetter:
             self.model_code = 'shapeGroup'
         if shapefile_path is None:  # read from openquake.cfg
             if kind == 'mosaic':
-                mosaic_dir = os.path.dirname(mosaic.__file__)
-                shapefile_path = os.path.join(
-                    mosaic_dir, 'ModelBoundaries.shp')
+                self.dir = os.path.dirname(mosaic.__file__)
+                shapefile_path = os.path.join(self.dir, 'ModelBoundaries.shp')
             elif kind == 'global_risk':
-                global_risk_dir = os.path.dirname(global_risk.__file__)
+                self.dir = os.path.dirname(global_risk.__file__)
                 shapefile_path = os.path.join(
-                    global_risk_dir, 'geoBoundariesCGAZ_ADM0.shp')
+                    self.dir, 'geoBoundariesCGAZ_ADM0.shp')
         self.shapefile_path = shapefile_path
 
     def get_models_list(self):
