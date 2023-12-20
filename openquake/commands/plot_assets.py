b9e45481eb12bde76209e88c2e9045305e505b19
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2023-12-20 16:40:59 +0100

    Plot also country boundaries when using oq plot_assets

--- openquake/commands/plot_assets.py
+++ openquake/commands/plot_assets.py
@@ -16,19 +16,31 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
 import numpy
+import fiona
 import shapely
+from shapely.geometry import MultiPolygon, shape
 from openquake.commonlib import datastore
 from openquake.hazardlib.geo.utils import cross_idl
+from openquake.qa_tests_data import global_risk
 
 
 def main(calc_id: int = -1, site_model=False):
     """
     Plot the sites and the assets
     """
+
     # NB: matplotlib is imported inside since it is a costly import
     import matplotlib.pyplot as p
     from openquake.hmtk.plotting.patch import PolygonPatch
+
+    shapefile_dir = os.path.dirname(global_risk.__file__)
+    shapefile_path = os.path.join(shapefile_dir, 'geoBoundariesCGAZ_ADM0.shp')
+    polys = [shape(pol['geometry']) for pol in fiona.open(shapefile_path)]
+    cm = p.get_cmap('RdBu')
+    num_colours = len(polys)
+
     dstore = datastore.read(calc_id)
     try:
         region = dstore['oqparam'].region
@@ -62,7 +74,26 @@ def main(calc_id: int = -1, site_model=False):
         disc = numpy.unique(dstore['discarded']['lon', 'lat'])
         p.scatter(disc['lon'], disc['lat'], marker='x', color='red',
                   label='discarded')
+
+    for idx, poly in enumerate(polys):
+        colour = cm(1. * idx / num_colours)
+        if isinstance(poly, MultiPolygon):
+            for onepoly in poly.geoms:
+                ax.add_patch(PolygonPatch(onepoly, fc=colour, alpha=0.1))
+        else:
+            ax.add_patch(PolygonPatch(poly, fc=colour, alpha=0.1))
+
+    minx = assetcol['lon'].min()
+    maxx = assetcol['lon'].max()
+    miny = assetcol['lat'].min()
+    maxy = assetcol['lat'].max()
+    w, h = maxx - minx, maxy - miny
+    ax.set_xlim(minx - 0.2 * w, maxx + 0.2 * w)
+    ax.set_ylim(miny - 0.2 * h, maxy + 0.2 * h)
+    # ax.set_aspect(1)
     ax.legend()
+    # p.title("Assets and sites")
+    p.savefig('assets.png', alpha=True, dpi=300)
     p.show()
 
 
