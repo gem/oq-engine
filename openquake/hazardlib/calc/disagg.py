4790246027ef7a93fcc45efe62cfac61e877920f
Michele Simionato <michele.simionato@gmail.com>
2024-01-15 06:29:35 +0100

    More cleanup

--- openquake/hazardlib/calc/disagg.py
+++ openquake/hazardlib/calc/disagg.py
@@ -22,7 +22,6 @@
 extracting a specific PMF from the result of :func:`disaggregation`.
 """
 
-import re
 import operator
 import collections
 import itertools
@@ -37,6 +36,7 @@ from openquake.baselib.python3compat import decode
 from openquake.hazardlib.imt import from_string
 from openquake.hazardlib.calc import filters
 from openquake.hazardlib.stats import truncnorm_sf
+from openquake.hazardlib.logictree import corename
 from openquake.hazardlib.geo.utils import get_longitudinal_extent
 from openquake.hazardlib.geo.utils import (angular_distance, KM_TO_DEGREES,
                                            cross_idl)
@@ -730,7 +730,7 @@ def disagg_source(groups, sitecol, reduced_lt, edges_shapedic,
         reduced_lt.init()
     edges, s = edges_shapedic
     drates4D = numpy.zeros((s['mag'], s['dist'], s['eps'], len(imldic)))
-    source_id = re.split('[!:;.]', groups[0].sources[0].source_id)[0]
+    source_id = corename(groups[0].sources[0].source_id)
     rmap, ctxs, cmakers = calc_rmap(groups, reduced_lt, sitecol, oq)
     trt_rlzs = [numpy.uint32(rlzs) + cm.trti * TWO24 for cm in cmakers
                 for rlzs in cm.gsims.values()]
