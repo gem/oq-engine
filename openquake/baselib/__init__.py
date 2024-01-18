6d2d447db79116364d79cf5c068751db5b09cf0e
Michele Simionato <michele.simionato@gmail.com>
2024-01-18 07:21:24 +0100

    Refined config.paths

--- openquake/baselib/__init__.py
+++ openquake/baselib/__init__.py
@@ -57,13 +57,12 @@ def positiveint(flag):
 config = DotDict()  # global configuration
 d = os.path.dirname
 base = os.path.join(d(d(__file__)), 'engine', 'openquake.cfg')
-# 'virtualenv' still uses 'real_prefix' also on Python 3
-# removal of this breaks Travis
-if hasattr(sys, 'real_prefix') or (
-        hasattr(sys, 'base_prefix') and sys.base_prefix != sys.prefix):
-    config.paths = [base, os.path.join(sys.prefix, 'openquake.cfg')]
-else:  # installation from sources or packages, search in $HOME or /etc
-    config.paths = [base, '/etc/openquake/openquake.cfg', '~/openquake.cfg']
+home = os.path.expanduser('~/openquake.cfg')
+if sys.prefix != sys.base_prefix:
+    # installation in the venv identified by sys.prefix
+    config.paths = [base, os.path.join(sys.prefix, 'openquake.cfg'), home]
+else:  # other kind of installation
+    config.paths = [base, '/etc/openquake/openquake.cfg', home]
 cfgfile = os.environ.get('OQ_CONFIG_FILE')
 if cfgfile:
     config.paths.append(cfgfile)
