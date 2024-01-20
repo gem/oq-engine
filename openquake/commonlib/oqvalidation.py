6b831c1dc8e5373fbd93262ed4541573caefc580
Michele Simionato <michele.simionato@gmail.com>
2024-01-20 18:58:50 +0100

    Better logging [ci skip]

--- openquake/commonlib/oqvalidation.py
+++ openquake/commonlib/oqvalidation.py
@@ -1949,8 +1949,8 @@ class OqParam(valid.ParamSet):
                 os.path.join(self.input_dir, self.export_dir))
         if not self.export_dir:
             self.export_dir = os.path.expanduser('~')  # home directory
-            logging.warning('export_dir not specified. Using export_dir=%s'
-                            % self.export_dir)
+            logging.info('export_dir not specified. Using export_dir=%s'
+                         % self.export_dir)
             return True
         if not os.path.exists(self.export_dir):
             try:
