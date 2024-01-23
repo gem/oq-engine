537fa8c0bce49f8b0fdda1d50d1ef9a6d1de6c8b
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2024-01-23 11:09:51 +0100

    Add to Announcement a 'show' flag True by default to make it possible to edit and reuse old announcements; explicitly define primary key to avoid warning

--- openquake/server/db/models.py
+++ openquake/server/db/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 
 class Announcement(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     content = models.TextField()
+    show = models.BooleanField(default=True)
 
     def __str__(self):
         return f'{self.title}: {self.content}'
