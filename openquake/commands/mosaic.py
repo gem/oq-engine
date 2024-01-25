9b63e87edb6f93d3a03ee7884f71116de37c8572
Michele Simionato <michele.simionato@gmail.com>
2024-01-25 06:14:54 +0100

    Fixed ordering [ci skip]

--- openquake/commands/mosaic.py
+++ openquake/commands/mosaic.py
@@ -101,7 +101,7 @@ def from_file(fname, mosaic_dir, concurrent_jobs):
     models = geolocate(lonlats, mosaic_df)
     count_sites_per_model = collections.Counter(models)
     print(count_sites_per_model)
-    triples = zip(models, sites_df.ID, map(tuple, lonlats))
+    triples = zip(models, map(tuple, lonlats), sites_df.ID)
     for model, id, lonlat in sorted(triples):
         if model in ('???', 'USA', 'GLD'):
             continue
