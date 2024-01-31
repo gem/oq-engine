4e1b68f8708c373bca5b376cfe38e7434877beb1
Michele Simionato <michele.simionato@gmail.com>
2024-01-31 09:06:47 +0100

    Fixed some tests

--- openquake/hazardlib/tests/geo/surface/multi_kite_test.py
+++ openquake/hazardlib/tests/geo/surface/multi_kite_test.py
@@ -215,8 +215,6 @@ class MultiSurfaceWithNaNsTestCase(unittest.TestCase):
                               [-70.57740671, 19.66979434, 0.0]]),
                     np.array([[-70.10327766, 19.67957463, 0.0],
                               [-70.33, 19.65, 0.0]])]
-        self.msrf._set_tor()
-
         if PLOTTING:
             _, ax = plt.subplots(1, 1)
             for sfc in self.msrf.surfaces:
