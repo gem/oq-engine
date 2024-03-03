a725953da97e74b67dc9c2cdad207be42e56a437
Michele Simionato <michele.simionato@gmail.com>
2024-03-03 11:15:31 +0100

    Cleanup

--- openquake/calculators/classical.py
+++ openquake/calculators/classical.py
@@ -102,6 +102,18 @@ def store_ctxs(dstore, rupdata_list, grp_id):
             else:
                 hdf5.extend(dstore['rup/' + par], numpy.full(nr, numpy.nan))
 
+
+def todict(pnemap, tiling):
+    if not tiling:
+        return pnemap
+    rates = pnemap.to_rates()  # shape (N, L1, G)
+    idxs, lids, gids = rates.nonzero()
+    if len(idxs) == 0:  # happens in case_60
+        return {}
+    sids = pnemap.sids[idxs]
+    return dict(sid=sids, gid=gids, lid=lids, rate=rates[idxs, lids, gids])
+
+
 #  ########################### task functions ############################ #
 
 
@@ -129,7 +141,7 @@ def classical(sources, sitecol, cmaker, dstore, monitor):
                 cmaker.rup_indep)
             result = hazclassical(srcs, sitecol, cmaker, pmap)
             result['pnemap'] = ~pmap
-            result['pnemap'].trt_smrs = cmaker.trt_smrs
+            result['trt_smrs'] = cmaker.trt_smrs
             yield result
     else:
         # size_mb is the maximum size of the pmap array in GB
@@ -149,7 +161,7 @@ def classical(sources, sitecol, cmaker, dstore, monitor):
                 result['pnemap'] = ~pmap.remove_zeros()
             else:  # keep the shape of the underlying array in store_mean_rates
                 result['pnemap'] = ~pmap
-            result['pnemap'].trt_smrs = cmaker.trt_smrs
+            result['trt_smrs'] = cmaker.trt_smrs
             yield result
 
 
@@ -359,7 +371,7 @@ class ClassicalCalculator(base.HazardCalculator):
             # accumulate the rates for the given source
             pm = ~pnemap
             pm.grp_id = grp_id
-            pm.trt_smrs = pnemap.trt_smrs
+            pm.trt_smrs = dic['trt_smrs']
             acc[source_id] += self.haz.get_rates(pm)
         G = pnemap.array.shape[2]
         for i, gid in enumerate(self.gids[grp_id]):
