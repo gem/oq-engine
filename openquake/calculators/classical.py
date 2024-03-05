1cbe9bb21fb960b61ac6e2cf798a374d2ae8071c
Michele Simionato <michele.simionato@gmail.com>
2024-03-05 04:48:49 +0100

    Cleanup

--- openquake/calculators/classical.py
+++ openquake/calculators/classical.py
@@ -129,7 +129,6 @@ def classical(sources, sitecol, cmaker, dstore, monitor):
                 cmaker.rup_indep)
             result = hazclassical(srcs, sitecol, cmaker, pmap)
             result['pnemap'] = ~pmap
-            result['trt_smrs'] = cmaker.trt_smrs
             yield result
     else:
         # size_mb is the maximum size of the pmap array in GB
@@ -149,7 +148,6 @@ def classical(sources, sitecol, cmaker, dstore, monitor):
                 result['pnemap'] = ~pmap.remove_zeros()
             else:  # keep the shape of the underlying array in store_mean_rates
                 result['pnemap'] = ~pmap
-            result['trt_smrs'] = cmaker.trt_smrs
             yield result
 
 
@@ -357,7 +355,6 @@ class ClassicalCalculator(base.HazardCalculator):
             # accumulate the rates for the given source
             pm = ~pnemap
             pm.grp_id = grp_id
-            pm.trt_smrs = dic['trt_smrs']
             acc[source_id] += self.haz.get_rates(pm)
         G = pnemap.array.shape[2]
         for i, gid in enumerate(self.gids[grp_id]):
