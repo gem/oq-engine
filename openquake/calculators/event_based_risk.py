116215a3abc9839ed6cac7e22f7ff9391c42cfd2
Michele Simionato <michele.simionato@gmail.com>
2024-03-05 16:28:18 +0100

    Fixed test

--- openquake/calculators/event_based_risk.py
+++ openquake/calculators/event_based_risk.py
@@ -253,7 +253,8 @@ def set_oqparam(oq, assetcol, dstore):
     sec_losses = []  # one insured loss for each loss type with a policy
     try:
         policy_df = dstore.read_df('policy')
-        policy_df['loss_type'] = python3compat.decode(policy_df['loss_type'])
+        policy_df['loss_type'] = python3compat.decode(
+            policy_df['loss_type'].to_numpy())
     except KeyError:
         pass
     else:
