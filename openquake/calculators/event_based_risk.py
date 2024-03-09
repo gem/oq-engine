a2474ae8572947f7a5dbf48fb8054783a790f800
Michele Simionato <michele.simionato@gmail.com>
2024-03-09 07:42:28 +0100

    Raise an error when the avg_losses can send the server out of memory

--- openquake/calculators/event_based_risk.py
+++ openquake/calculators/event_based_risk.py
@@ -358,13 +358,13 @@ class EventBasedRiskCalculator(event_based.EventBasedCalculator):
         oq.maxweight = int(oq.ebrisk_maxsize / ct)
         self.A = A = len(self.assetcol)
         self.L = L = len(oq.loss_types)
-        if (oq.calculation_mode == 'event_based_risk' and
-                not oq.collect_rlzs and oq.avg_losses):
-            if A * self.R > 10_000_000:
+        if oq.calculation_mode == 'event_based_risk' and oq.avg_losses:
+            if A > 1_000_000:
                 raise ValueError('For large exposures you must set '
-                                 'avg_losses=false or use sampling')
-            elif A * self.R > 100_000:
-                logging.warning('We recommend using sampling for performance')
+                                 'avg_losses=false')
+            elif A * self.R > 1_000_000:
+                raise ValueError('For large exposures you must set '
+                                 'collect_rlzs = true')
         if (oq.aggregate_by and self.E * A > oq.max_potential_gmfs and
                 all(val == 0 for val in oq.minimum_asset_loss.values())):
             logging.warning('The calculation is really big; consider setting '
