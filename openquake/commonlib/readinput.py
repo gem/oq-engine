b2cdb494adee6dad7564bf1fbf7f103325fcc2f1
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2024-03-19 08:21:57 +0100

    Get loss types and minimum asset loss only if 'crm' is present

--- openquake/commonlib/readinput.py
+++ openquake/commonlib/readinput.py
@@ -975,9 +975,10 @@ def get_exposure(oqparam, h5=None):
             gh3 = numpy.array(sorted(set(geohash3(sm['lon'], sm['lat']))))
             exposure = Global.exposure = asset.Exposure.read_around(fname, gh3)
             with hdf5.File(fname) as f:
-                loss_types = f['crm'].attrs['loss_types']
-            oq.all_cost_types = loss_types
-            oq.minimum_asset_loss = {lt: 0 for lt in loss_types}
+                if 'crm' in f:
+                    loss_types = f['crm'].attrs['loss_types']
+                    oq.all_cost_types = loss_types
+                    oq.minimum_asset_loss = {lt: 0 for lt in loss_types}
         else:
             exposure = Global.exposure = asset.Exposure.read_all(
                 oq.inputs['exposure'], oq.calculation_mode,
