50dd099ae7b3d62b531372c7259bbfc5a7ff75c3
Michele Simionato <michele.simionato@gmail.com>
2024-06-07 09:12:56 +0200

    Fixed test

--- openquake/risklib/tests/expo_to_hdf5_test.py
+++ openquake/risklib/tests/expo_to_hdf5_test.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
 import os
-from openquake.commonlib.datastore import read
+from openquake.baselib import hdf5
 from openquake.risklib.expo_to_hdf5 import main
 
 
@@ -24,7 +24,7 @@ def test_main():
     # only tests that it runs
     expo_xml = os.path.join(os.path.dirname(__file__), 'data', 'grm_exposure.xml')
     out = main([expo_xml])
-    with read(out) as dstore:
+    with hdf5.File(out) as dstore:
         assets = list(dstore['assets/ASSET_ID'])
         assert assets == [b'TWNRes_0', b'TWNRes_1', b'TWNRes_2', b'TWNRes_3',
                           b'TWNRes_4', b'TWNRes_5', b'TWNRes_6', b'TWNRes_7',
