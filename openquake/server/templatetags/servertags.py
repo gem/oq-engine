5f84fa92fc0d1799b042b79b4821c435da4301e0
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2024-06-17 15:32:06 +0200

    Show a different cookie bar if only required cookie groups are present

--- openquake/server/templatetags/servertags.py
+++ openquake/server/templatetags/servertags.py
@@ -9,3 +9,13 @@ def get_imt(string):
     Get imt from a string like 'disagg_by_src-PGA.png'
     """
     return string.split('-')[1].rstrip('.png')
+
+
+@register.simple_tag()
+def no_optional_cookie_groups_exist(request):
+    from cookie_consent.models import CookieGroup
+    if (CookieGroup.objects.filter(is_required=False).count() == 0 and
+            CookieGroup.objects.filter(is_required=True).count() > 0):
+        return True
+    else:
+        return False
