663a1c2f95eeb4ac8ce0db67eb151c41113cdfa7
manuela.villani@globalquakemodel.org <manuela.villani@globalquakemodel.org>
2024-07-09 11:28:14 +0100

    adjust _

--- openquake/calculators/postproc/compute_rtgm.py
+++ openquake/calculators/postproc/compute_rtgm.py
@@ -557,7 +557,7 @@ def main(dstore, csm):
             logging.info('(%.1f,%.1f) Computed MCE: Zero hazard\n%s', loc.x,
                          loc.y, mce_df)
         elif warning.startswith(('The MCE', 'Only probabilistic MCE')):
-            prob_mce_out, mce, det_mce, a07, mce_df = get_mce_asce07(
+            _prob_mce_out, mce, _det_mce, a07, mce_df = get_mce_asce07(
                 job_imts, dummy_det, DLLs[sid], rtgm_df, sid, vs30, low_haz=True)
             logging.info('(%.1f,%.1f) Computed MCE: Only Prob\n%s', loc.x,
                          loc.y, mce_df)
