2ecec2e57a03736cd1b3e0d54a0cc7112f8b44be
Michele Simionato <michele.simionato@gmail.com>
2024-07-26 08:50:40 +0200

    Fixed a small bug in conditional spectrum calculations

--- openquake/calculators/postproc/conditional_spectrum.py
+++ openquake/calculators/postproc/conditional_spectrum.py
@@ -158,8 +158,9 @@ def _apply_weights(dstore, acc, N, R, M, P, trt_rlzs):
     # build conditional spectra for each realization
     outdic = outdict(M, N, P, 0, R)
     for g, trs in enumerate(trt_rlzs):
-        for r in trs % TWO24:
-            outdic[r] += acc[g]
+        if g in acc:
+            for r in trs % TWO24:
+                outdic[r] += acc[g]
 
     # build final conditional mean and std
     weights = dstore['weights'][:]
