b97e2dbd55ac9b65d339eb7b38b5c1b3a16a5c52
Michele Simionato <michele.simionato@gmail.com>
2024-09-02 06:22:39 +0200

    Fixed unused imports

--- openquake/commands/run.py
+++ openquake/commands/run.py
@@ -16,8 +16,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake. If not, see <http://www.gnu.org/licenses/>.
 
-import stat
-import time
 import logging
 import os.path
 import socket
