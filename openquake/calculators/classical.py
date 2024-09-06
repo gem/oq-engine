b8a925d6ced36e5d44765ef1d433c8fcf3e01c54
Michele Simionato <michele.simionato@gmail.com>
2024-09-06 14:50:16 +0200

    Removed print

--- openquake/calculators/classical.py
+++ openquake/calculators/classical.py
@@ -166,7 +166,6 @@ def classical(sources, sitegetter, cmaker, dstore, monitor):
             rmap = result.pop('rmap').remove_zeros()
         # print(f"{monitor.task_no=} {rmap=}")
 
-        print('-------------', cmaker.blocks)
         if rmap.size_mb and cmaker.blocks == 1 and not cmaker.disagg_by_src:
             del result['source_data']
             if cmaker.custom_tmp:
