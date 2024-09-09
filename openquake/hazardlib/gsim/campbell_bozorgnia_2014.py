f406fa4251826060cd4a3df98cac450b92835e25
emabcede1998 <ema98.wp@gmail.com>
2024-09-09 13:39:04 +0800

    philnAF is not constant

--- openquake/hazardlib/gsim/campbell_bozorgnia_2014.py
+++ openquake/hazardlib/gsim/campbell_bozorgnia_2014.py
@@ -35,7 +35,7 @@ from openquake.hazardlib.imt import PGA, PGV, SA, IA, CAV
 CONSTS = {"h4": 1.0,
           "c": 1.88,
           "n": 1.18,}
-          #"philnAF": 0.3} not constant in CB19 test
+          #"philnAF": 0.3} this is not constant in CB19
 
 
 def _get_alpha(C, vs30, pga_rock):
