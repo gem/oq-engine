f3aab96860ef735e1d172ccfbdd2bcf375cfcd9c
Michele Simionato <michele.simionato@gmail.com>
2024-09-11 08:52:10 +0200

    Bigger weight [ci skip]

--- openquake/hazardlib/source_reader.py
+++ openquake/hazardlib/source_reader.py
@@ -683,7 +683,7 @@ class CompositeSourceModel:
                 logging.info(msg.format(src, src.num_ruptures, spc))
         assert tot_weight
         max_weight = tot_weight / (oq.concurrent_tasks or 1)
-        max_weight *= 1.05  # increased to produce fewer tasks
+        max_weight *= 1.1  # increased to produce fewer tasks
         logging.info('tot_weight={:_d}, max_weight={:_d}, num_sources={:_d}'.
                      format(int(tot_weight), int(max_weight), len(srcs)))
         return max_weight
