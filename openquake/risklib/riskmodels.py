96159b2582e18d320effffde5944a12322cc3cdb
Michele Simionato <michele.simionato@gmail.com>
2024-10-07 16:18:00 +0200

    Removed unused import

--- openquake/risklib/riskmodels.py
+++ openquake/risklib/riskmodels.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake. If not, see <http://www.gnu.org/licenses/>.
 import re
-import ast
 import json
 import copy
 import logging
