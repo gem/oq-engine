c7015aa2824ca57a08b3be3939bf58ce24abf992
Michele Simionato <michele.simionato@gmail.com>
2024-10-16 05:50:31 +0200

    Skipping test_aristotle if exposure.hdf5 is missing

--- openquake/engine/tests/aristotle_test.py
+++ openquake/engine/tests/aristotle_test.py
@@ -16,7 +16,9 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
 import pathlib
+import unittest
 import pytest
 from openquake.calculators.checkers import check
 
@@ -25,4 +27,6 @@ cd = pathlib.Path(__file__).parent
 
 @pytest.mark.parametrize('n', [1, 2])
 def test_aristotle(n):
+    if not os.path.exists(cd.parent.parent.parent / 'exposure.hdf5'):
+        raise unittest.SkipTest('Please download exposure.hdf5')
     check(cd / f'aristotle{n}/job.ini', what='aggrisk')
