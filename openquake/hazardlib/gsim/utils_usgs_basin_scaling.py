fixing units/conditionals in usgs basin scaling

openquake/hazardlib/gsim/utils_usgs_basin_scaling.py
@@ -30,7 +30,7 @@ def _get_z1pt0_usgs_basin_scaling(z, period):
     Get the USGS basin model scaling factor for z1pt0-based GMM basin terms.
     """
     z_scale = _get_usgs_basin_scaling(
-        z, basin_upper=0.3, basin_lower=0.5, period=period)
+        z, basin_upper=0.3*1000, basin_lower=0.5*1000, period=period)
 
     return z_scale
 
@@ -45,15 +45,21 @@ def _get_z2pt5_usgs_basin_scaling(z, period):
     return z_scale
 
 
-def _get_usgs_basin_scaling(z2pt5, basin_upper, basin_lower, period):
+def _get_usgs_basin_scaling(z, basin_upper, basin_lower, period):
     """
     Get the USGS basin model scaling factor to be applied to the GMM's
     basin amplification term.
     """
-    constr = np.clip(z2pt5, basin_upper, basin_lower)
-    basin_range = basin_lower - basin_upper
-    z_scale = (constr - basin_upper) / basin_range
-    if period == 0.75:
-        return 0.585 * z_scale
+    z_scale = np.ones(len(z))
+    if period <= 0.5:
+        return z_scale # Only apply basin scaling to T > 0.5 s
     else:
-        return z_scale
+        constr = np.clip(z, basin_upper, basin_lower)
+        basin_range = basin_lower - basin_upper
+        baf = (constr - basin_upper) / basin_range
+        # Apply for sites with z > basin_upper (i.e. z_min)
+        z_scale[z > basin_upper] = baf[z > basin_upper]
+        if period == 0.75:
+            return 0.585 * z_scale
+        else:
+            return z_scale

openquake/hazardlib/tests/gsim/utils_usgs_basin_scaling_test.py
@@ -22,15 +22,15 @@ from openquake.hazardlib.gsim.utils_usgs_basin_scaling import (
 
 aae = np.testing.assert_almost_equal
 
-exp_z1pt0_baf = np.array([[0.5, 0., 0., 0.5, 1.],
-                          [0.2925, 0., 0., 0.2925, 0.585],
-                          [0.5, 0., 0., 0.5, 1.],
-                          [0.5, 0., 0., 0.5, 1.]])
+exp_z1pt0_baf = np.array([[1., 1., 1., 1., 1.],
+                          [0.585, 0.585, 0.4593127, 0.5828355, 0.585],
+                          [1., 1., 0.78515, 0.9963, 1.],
+                          [1., 1., 0.78515, 0.9963, 1.]])
 
-exp_z2pt5_baf = np.array([[0.5, 0., 1., 1., 1.],
-                          [0.2925, 0., 0.585, 0.585, 0.585],
-                          [0.5, 0., 1., 1., 1.],
-                          [0.5, 0., 1., 1., 1.]])
+exp_z2pt5_baf = np.array([[1., 1., 1., 1., 1.],
+                          [0.585, 0.585, 0.21645, 0.40365, 0.585],
+                          [1., 1., 0.37, 0.69, 1.],
+                          [1., 1., 0.37, 0.69, 1.]])
 
 
 class USGSBasinScalingTestCase(unittest.TestCase):
@@ -39,12 +39,14 @@ class USGSBasinScalingTestCase(unittest.TestCase):
     factors (scaling factors can be applied to basin terms in GMMs)
     """
     def test_usgs_basin_scaling(self):
-        # Some example z1pt0 and z2pt5
-        z1pt0 = np.array([0.4, 0.3, 0.2, 0.4, 1])
-        z2pt5 = np.array([2, 1, 4, 6, 3])
+        # Some physically correlated z1pt0 and z2pt5 (from US sites model)
+        z1pt0 = np.array([522.31, 0.03, 457.03, 499.26, 520.23]) # units is
+                                                                 # metres
+        z2pt5 = np.array([6.29, 0.18, 1.74, 2.38, 4.39])  # units is km
         # Some periods
         periods = np.array([0.0, 0.75, 2, 3.])
 
+        # Get the baf per period at each site and check against exp values
         for idx, period in enumerate(periods):
             z1pt0_baf = _get_z1pt0_usgs_basin_scaling(z1pt0, period)
             aae(z1pt0_baf, exp_z1pt0_baf[idx])