310b09c016a6a36780d814bfe5f83c7202dcdb99
Michele Simionato <michele.simionato@gmail.com>
2024-12-10 07:49:23 +0100

    Fixed run_jobs [ci skip]

--- openquake/engine/engine.py
+++ openquake/engine/engine.py
@@ -31,7 +31,7 @@ import getpass
 import logging
 import platform
 import functools
-import multiprocessing
+import multiprocessing.pool
 from os.path import getsize
 from datetime import datetime
 import psutil
