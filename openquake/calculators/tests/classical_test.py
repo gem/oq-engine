413b44f8ea2f81433f880b2a2822c0d9580d283c
Graeme Weatherill <weatherilloqhazard@gmail.com>
2024-12-18 11:24:09 +0100

    Fixed merge conflict in classical_test, but test_case_85 still failing

--- openquake/calculators/tests/classical_test.py
+++ openquake/calculators/tests/classical_test.py
@@ -37,15 +37,9 @@ from openquake.qa_tests_data.classical import (
     case_34, case_35, case_37, case_38, case_40, case_41,
     case_42, case_43, case_44, case_47, case_48, case_49,
     case_50, case_51, case_53, case_54, case_55, case_57,
-<<<<<<< HEAD
-    case_60, case_61, case_62, case_63, case_64, case_65,
-    case_66, case_69, case_70, case_72, case_74, case_75, case_76, case_77,
-    case_78, case_80, case_81, case_82, case_83, case_84, case_85)
-=======
     case_60, case_61, case_62, case_63, case_64, case_65, case_66,
     case_67, case_69, case_70, case_72, case_74, case_75, case_76, case_77,
-    case_78, case_80, case_81, case_82, case_83, case_84, case_86)
->>>>>>> master
+    case_78, case_80, case_81, case_82, case_83, case_84, case_85, case_86)
 
 ae = numpy.testing.assert_equal
 aac = numpy.testing.assert_allclose
@@ -787,13 +781,12 @@ class ClassicalTestCase(CalculatorTestCase):
         [f] = export(('mean_rates_by_src', 'csv'), self.calc.datastore)
         self.assertEqualFiles('expected/rbs.csv', f)
 
-<<<<<<< HEAD
     def test_case_85(self):
         # Conditional GMM (Macedo et al. (2019)) for Arias Inensity
         self.run_calc(case_85.__file__, 'job.ini')
         [f1] = export(('hcurves/mean', 'csv'), self.calc.datastore)
         self.assertEqualFiles('expected/hazard_curve-mean-IA.csv', f1)
-=======
+    
     def test_case_86(self):
         # Comparing the revised indirect GMPE and the direct AvgSA GMPE
         # for AvgSA at multiple spectral periods
@@ -808,4 +801,3 @@ class ClassicalTestCase(CalculatorTestCase):
             'hazard_curve-rlz-001-AvgSA(0.75).csv',
             'hazard_curve-rlz-001-AvgSA(2.0).csv'],
             case_86.__file__)
->>>>>>> master
