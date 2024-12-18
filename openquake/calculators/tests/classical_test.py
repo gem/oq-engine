bff54b27d2f639231638add628c2e64604440c9d
Graeme Weatherill <weatherilloqhazard@gmail.com>
2024-12-18 11:24:09 +0100

    Fixed merge conflict in classical_test, but test_case_85 still failing

--- openquake/calculators/tests/classical_test.py
+++ openquake/calculators/tests/classical_test.py
@@ -899,17 +899,11 @@ class ClassicalTestCase(CalculatorTestCase):
         self.assertEqualFiles("expected/rbs.csv", f)
 
     def test_case_85(self):
-        # Conditional GMM (Macedo et al. (2019)) for Arias Intensity
-        self.run_calc(case_85.__file__, "job.ini")
-        [f1] = export(("hcurves/mean", "csv"), self.calc.datastore)
-        self.assertEqualFiles("expected/hazard_curve-mean-IA.csv", f1)
-
-    def test_case_85(self):
         # Conditional GMM (Macedo et al. (2019)) for Arias Inensity
-        self.run_calc(case_85.__file__, "job.ini")
-        [f1] = export(("hcurves/mean", "csv"), self.calc.datastore)
-        self.assertEqualFiles("expected/hazard_curve-mean-IA.csv", f1)
-
+        self.run_calc(case_85.__file__, 'job.ini')
+        [f1] = export(('hcurves/mean', 'csv'), self.calc.datastore)
+        self.assertEqualFiles('expected/hazard_curve-mean-IA.csv', f1)
+    
     def test_case_86(self):
         # Comparing the revised indirect GMPE and the direct AvgSA GMPE
         # for AvgSA at multiple spectral periods
