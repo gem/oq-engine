1ea06b55ed5c8bd7e686b49a0e17d28b367cc2df
Michele Simionato <michele.simionato@gmail.com>
2025-01-09 08:01:11 +0100

    Cleanup

--- openquake/calculators/event_based_damage.py
+++ openquake/calculators/event_based_damage.py
@@ -231,10 +231,9 @@ class DamageCalculator(EventBasedRiskCalculator):
         prc.assetcol = self.assetcol
         if hasattr(self, 'exported'):
             prc.exported = self.exported
-        with prc.datastore:
-            prc.pre_execute()
-            res = prc.execute()
-            prc.post_execute(res)
+        prc.pre_execute()
+        res = prc.execute()
+        prc.post_execute(res)
 
         P, _A, _R, L, _Dc = self.dmgcsq.shape
         D = len(self.crmodel.damage_states)
