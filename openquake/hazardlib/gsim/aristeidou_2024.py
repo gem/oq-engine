e7dab5179ca4e22d07b7c754dafd7b2d17cf110a
Savvinos Aristeidou <savvinos.aristeidou@iusspavia.it>
2025-01-25 11:01:44 +0100

    Changes in aristeidou_2024.py gsim and add tests

--- openquake/hazardlib/gsim/aristeidou_2024.py
+++ openquake/hazardlib/gsim/aristeidou_2024.py
@@ -21,71 +21,81 @@ Created on Mon Apr 08 2024
 Authors: savvinos.aristeidou@iusspavia.it, davit.shahnazaryan@iusspavia.it
 
 Module exports :class:`AristeidouEtAl2024`
+               :class:`AristeidouEtAl2024Geomean`
+               :class:`AristeidouEtAl2024RotD100`
 """
 
 from pathlib import Path
-import warnings
 import numpy as np
 from scipy.interpolate import interp1d
 from openquake.hazardlib import const
-from openquake.hazardlib.imt import RSD575, RSD595, Sa_avg2, Sa_avg3, SA, PGA, PGV, PGD, FIV3
+from openquake.hazardlib.imt import (
+    RSD575, RSD595, Sa_avg2, Sa_avg3, SA, PGA, PGV, PGD, FIV3)
 from openquake.hazardlib.gsim.base import GMPE
-import re
-from typing import Union
 import json
 
-def read_json(filename: Union[Path, dict]):
-    if isinstance(filename, Path) or isinstance(filename, str):
-        filename = Path(filename)
+ASSET_DIR = Path(__file__).resolve().parent / "aristeidou_2024_assets"
 
-        with open(filename) as f:
-            filename = json.load(f)
 
+def read_json(filename: Path):
+    with open(filename) as f:
+        filename = json.load(f)
     return filename
 
 
-def get_period_im(name: str):
-    pattern = r"\((\d+\.\d+)\)?"
+# Load background information about the model from a json file (i.e., weight,
+# biases, standard devation values, etc.)
+DATA = read_json(ASSET_DIR / "gmm_ann.json")
 
+
+def get_period_im(name: str):
+    """
+    Returns the period of IM and IM type, given the IM name string
+    """
     if '(' in name:
         im_type = name.split('(', 1)[0].strip()
+        period = float(name.split('(')[-1].split(')')[0])
     else:
         im_type = name
-
-    if re.search(pattern, name):
-        period = float(re.search(pattern, name).group(1))
-    else:
         period = None
 
     return im_type, period
 
 
 def linear(x):
+    """
+    Calculate the linear activation function
+    """
     return x
 
 
 def tanh(x):
+    """
+    Calculate the tanh activation function
+    """
     return np.tanh(x)
 
 
 def softmax(x):
+    """
+    Calculate the softmax activation function
+    """
     exp_x = np.exp(x - np.max(x, axis=-1, keepdims=True))
     return exp_x / np.sum(exp_x, axis=-1, keepdims=True)
 
 
 def sigmoid(x):
+    """
+    Calculate the sigmoid activation function
+    """
     return 1 / (1 + np.exp(-x))
-
-
-cwd_path = Path(__file__).resolve().parent
-asset_dir = cwd_path / "aristeidou_2024_assets"
 
 
 def _get_style_of_faulting_term(rake):
     """
     Get fault type dummy variables
-    Fault type (Strike-slip, Normal, Reverse, Reverse-Oblique, Normal-oblique) is
-    derived from rake angle.
+    Fault type (Strike-slip, Normal, Reverse, Reverse-Oblique,
+    Normal-oblique) is derived from rake angle.
                         SOF encoding    Rake Angles
     _______________________________________________________
     Strike-slip     |    0            | -180 < rake < -150
@@ -102,57 +112,62 @@ def _get_style_of_faulting_term(rake):
     Normal-oblique  |    4            | -150 < rake < -120
                     |                 | -60 < rake < -30
                     |                 |
-
     Note that the 'Unspecified' case is not considered here as
     rake is always given.
     """
     sof = np.full_like(rake, 0)
 
-    sof[((rake >= -180) & (rake <= -150)) | ((rake > -30) & 
+    sof[((rake >= -180) & (rake <= -150)) | ((rake > -30) &
         (rake <= 30)) | ((rake > 150) & (rake <= 180))] = 0
-    
+
     sof[(rake > -120) & (rake <= -60)] = 1
 
     sof[(rake > 60) & (rake <= 120)] = 2
 
-    sof[((rake > 30) & (rake <= 60)) | ((rake > 120) & 
+    sof[((rake > 30) & (rake <= 60)) | ((rake > 120) &
         (rake <= 150))] = 3
 
-    sof[((rake > -150) & (rake <= -120)) | ((rake > -60) & 
-    (rake <= -30))] = 4
+    sof[((rake > -150) & (rake <= -120)) |
+        ((rake > -60) & (rake <= -30))] = 4
 
     return sof
 
-# Function to extract the required part
+
 def extract_base(im):
-    # Remove the text after the last underscore and any trailing parentheses with numbers
-    base = re.sub(r'_[^_]+$', '', im)
-    base = re.sub(r'\(\d+(\.\d+)?\)$', '', base)
+    """
+    Remove the text after the last underscore and any trailing
+    parentheses with numbers
+    """
+    # Keep the im text before the last occurance of '_'
+    base = im.rsplit('_', 1)[0]
+    # Keep the im text before the occurance of '('
+    base = base.split('(')[0]
     return base
 
-def _get_means_stddevs(DATA, imt, means, stddevs, component_definition):
 
+def _get_means_stddevs(DATA, imt, means, stddevs, component_definition):
+    """
+    Extract the means and standard deviations of the requested IMs and
+    horizontal compoent definitions
+    """
     supported_ims = np.asarray(DATA["output-ims"])
-    supported_im_types = np.unique(np.array([extract_base(supported_im) for supported_im in supported_ims]))
-    
-    if imt.string == "RSD575":
-        im_name = "Ds575"
-    elif imt.string == "RSD595":
-        im_name = "Ds595"
-    elif imt.string.startswith("Sa_avg2"):
-        im_name = "Sa_avg2"
-    elif imt.string.startswith("Sa_avg3"):
-        im_name = "Sa_avg3"
-    elif imt.string.startswith("SA") or imt.string.startswith("Sa"):
-        im_name = "SA"
-    elif imt.string.startswith("FIV3"):
-        im_name = "FIV3"
-    elif imt.string.startswith("PGA"):
-        im_name = "PGA"
-    elif imt.string.startswith("PGV"):
-        im_name = "PGV"
-    elif imt.string.startswith("PGD"):
-        im_name = "PGD"
+    supported_im_types = np.unique(np.array(
+        [extract_base(supported_im) for supported_im in supported_ims]))
+
+    im_name_mapping = {
+        "RSD575": "Ds575",
+        "RSD595": "Ds595",
+        "Sa_avg2": "Sa_avg2",
+        "Sa_avg3": "Sa_avg3",
+        "SA": "SA",
+        "Sa": "SA",
+        "FIV3": "FIV3",
+        "PGA": "PGA",
+        "PGV": "PGV",
+        "PGD": "PGD",
+    }
+
+    im_name = im_name_mapping[extract_base(imt.string)]
 
     if im_name not in supported_im_types:
         raise NameError(
@@ -165,7 +180,8 @@ def _get_means_stddevs(DATA, imt, means, stddevs, component_definition):
         # Find the position of the first parenthesis
         pos = imt.string.index("(")
         # Insert using slicing
-        im_name = imt.string[:pos] + f'_{component_definition}' + imt.string[pos:]
+        im_name = (imt.string[:pos] + f'_{component_definition}'
+                   + imt.string[pos:])
     elif im_name.startswith("FIV3"):
         im_name = imt.string
 
@@ -176,10 +192,10 @@ def _get_means_stddevs(DATA, imt, means, stddevs, component_definition):
         idx = np.where(supported_ims == im_name)[0][0]
         return means[:, idx], stddevs[idx, :]
 
-    # im_type, period = get_period_im(im_name)
-
     # Period not supported, perform linear interpolation
-    idxs = np.where(np.char.find(supported_ims, im_name[:im_name.index("(")]) != -1)
+    idxs = np.where(np.char.find(
+        supported_ims, im_name[:im_name.index("(")]) != -1)
+
     ims = supported_ims[idxs]
     means = means[:, idxs]
     stddevs = stddevs[idxs, :]
@@ -194,64 +210,40 @@ def _get_means_stddevs(DATA, imt, means, stddevs, component_definition):
     interp_stddevs = interp1d(np.log(periods), stddevs, axis=1)
     interp_means = interp1d(np.log(periods), means)
 
-    mean, stddev = np.squeeze(interp_means(np.log(imt.period))), np.squeeze(interp_stddevs(np.log(imt.period)))
-
+    mean, stddev = (np.squeeze(interp_means(np.log(imt.period))),
+                    np.squeeze(interp_stddevs(np.log(imt.period))))
     return mean, stddev
 
 
-def _validate_input(DATA, x, SUGGESTED_LIMITS):
-    pars = DATA["parameters"]
-
-    min_max = np.asarray([SUGGESTED_LIMITS[par] for par in pars])
-
-    idxs = np.where((x < min_max[:, 0]) | (x > min_max[:, 1]))
-
-    for row, col in zip(*idxs):
-        val = x[row, col]
-        warnings.warn(
-            f"Value of {pars[col]}: {val} is not within the "
-            f"suggested limits {SUGGESTED_LIMITS[pars[col]]}")
-
-def log10_reverse(x):
-    return 10 ** x
-
 def _generate_function(x, biases, weights):
+    """
+    Returns the output of a layer based on the input, biases, and weights
+    """
     biases = np.asarray(biases)
     weights = np.asarray(weights).T
-
     return biases.reshape(1, -1) + np.dot(weights, x.T).T
 
-def _minmax_scaling(DATA, data, SUGGESTED_LIMITS, feature_range=(-3, 3)):
+
+def _minmax_scaling(DATA, x, SUGGESTED_LIMITS, feature_range=(-3, 3)):
+    """
+    Returns the min-max transformation scaling of input features
+    """
     pars = DATA["parameters"]
     min_max = np.asarray([
         SUGGESTED_LIMITS[par] for par in pars])
 
-    scaled_data = (data - min_max[:, 0]) / (min_max[:, 1] - min_max[:, 0])
+    scaled_data = (x - min_max[:, 0]) / (min_max[:, 1] - min_max[:, 0])
     scaled_data = scaled_data * \
         (feature_range[1] - feature_range[0]) + feature_range[0]
-
     return scaled_data
 
-# def get_suggested_parameter_limits():
-#     """Returns the suggested limits of the input parameters
-
-#     Returns
-#     -------
-#     dict
-#         Suggested input parameter limitations
-#     """
-#     return self.SUGGESTED_LIMITS
-
-# def get_supported_ims(self):
-#     return self.DATA["output-ims"]
 
 class AristeidouEtAl2024(GMPE):
     """
     Aristeidou, S., Shahnazaryan, D. and O’Reilly, G.J. (2024) ‘Artificial
-    neural network-based ground motion model for next-generation seismic 
+    neural network-based ground motion model for next-generation seismic
     intensity measures’, Under Review.
     """
-
     #: Supported tectonic region type is subduction interface
     DEFINED_FOR_TECTONIC_REGION_TYPE = const.TRT.ACTIVE_SHALLOW_CRUST
 
@@ -264,7 +256,8 @@ class AristeidouEtAl2024(GMPE):
 
     #: Supported standard deviation types
     DEFINED_FOR_STANDARD_DEVIATION_TYPES = {
-        const.StdDev.TOTAL, const.StdDev.INTER_EVENT, const.StdDev.INTRA_EVENT}
+        const.StdDev.TOTAL, const.StdDev.INTER_EVENT,
+        const.StdDev.INTRA_EVENT}
 
     #: Requires sites parameters
     REQUIRES_SITES_PARAMETERS = {'vs30', 'z2pt5'}
@@ -288,9 +281,6 @@ class AristeidouEtAl2024(GMPE):
         "Ztor": [0, 16.23],
     }
 
-    DATA = read_json(asset_dir / "gmm_ann.json")
-
-    # TODO: check how to have the option of different component definitions
     component_definition = "RotD50"
 
     def compute(self, ctx: np.recarray, imts, mean, sig, tau, phi):
@@ -299,13 +289,8 @@ class AristeidouEtAl2024(GMPE):
         <.base.GroundShakingIntensityModel.compute>`
         for spec of input and result values.
         """
+        breakpoint()
         for m, imt in enumerate(imts):
-            # imt = str(imt)
-
-            # supported_ims = np.asarray(self.DATA["output-ims"])
-
-            # if imt not in supported_ims:
-            #     raise NameError(f'{imt} not supported by this GMM')
 
             mag = np.array(ctx.mag).reshape(-1, 1)
             rjb = np.array(ctx.rjb).reshape(-1, 1)
@@ -319,20 +304,18 @@ class AristeidouEtAl2024(GMPE):
             rx = np.array(ctx.rx).reshape(-1, 1)
             ztor = np.array(ctx.ztor).reshape(-1, 1)
 
-            x = np.column_stack([
+            ctx_params = np.column_stack([
                 rjb, rrup, d_hyp, mag, vs30, mechanism, z2pt5, rx, ztor
             ])
 
-            # Validate the input parameters and raise warnings if not within limits
-            _validate_input(self.DATA, x, self.SUGGESTED_LIMITS)
-
             # Get biases and weights of the ANN model
-            biases = self.DATA["biases"]
-            weights = self.DATA["weights"]
+            biases = DATA["biases"]
+            weights = DATA["weights"]
 
             # Input layer
             # Transform the input
-            x_transformed = _minmax_scaling(self.DATA, x, self.SUGGESTED_LIMITS)
+            x_transformed = _minmax_scaling(
+                DATA, ctx_params, self.SUGGESTED_LIMITS)
 
             _data = _generate_function(
                 x_transformed, biases[0], weights[0])
@@ -348,24 +331,25 @@ class AristeidouEtAl2024(GMPE):
             _data = _generate_function(
                 a2, biases[2], weights[2]
             )
-            output = linear(_data)
+            output_log10 = linear(_data)
 
             # Reverse log10
-            output = log10_reverse(output)
+            output = 10 ** output_log10
 
             # Means (shape=(cases, n_im)) and standard deviations (n_im, )
             means = np.squeeze(np.log(output))
 
-            stddevs = np.asarray((self.DATA["total-stdev"],
-                                 self.DATA["inter-stdev"],
-                                 self.DATA["intra-stdev"])).T
+            stddevs = np.asarray((DATA["total-stdev"],
+                                 DATA["inter-stdev"],
+                                 DATA["intra-stdev"])).T
 
-            # Transform the standard deviations from log10 to natural logarithm
+            # Transform the standard deviations from log10 to natural
+            # logarithm
             stddevs = np.log(10**stddevs)
 
             # Get the means and stddevs at index corresponding to the IM
-            mean[m], stddevs = _get_means_stddevs(self.DATA, imt, means, stddevs, self.component_definition)
-
+            mean[m], stddevs = _get_means_stddevs(
+                DATA, imt, means, stddevs, self.component_definition)
 
             sig[m] = stddevs[0]
             tau[m] = stddevs[1]
