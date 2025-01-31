94532b3d8f5c3be674c7d1c773c752e4ca137aed
Michele Simionato <michele.simionato@globalquakemodel.org>
2025-01-31 07:01:22 +0100

    Cleanup

--- openquake/server/views.py
+++ openquake/server/views.py
@@ -750,23 +750,13 @@ def aristotle_get_rupture_data(request):
                         status=200)
 
 
-def copy_to_temp_dir_with_unique_name(source_file_path):
-    temp_dir = config.directory.custom_tmp or tempfile.gettempdir()
-    temp_file = tempfile.NamedTemporaryFile(delete=False, dir=temp_dir, suffix='.xml')
-    temp_file_path = temp_file.name
-    # Close the NamedTemporaryFile to prevent conflicts on Windows
-    temp_file.close()
-    shutil.copy(source_file_path, temp_file_path)
-    return temp_file_path
-
-
 def get_uploaded_file_path(request, filename):
     file = request.FILES.get(filename)
     if file:
         # NOTE: we could not find a reliable way to avoid the deletion of the
         # uploaded file right after the request is consumed, therefore we need
         # to store a copy of it
-        return copy_to_temp_dir_with_unique_name(file.temporary_file_path())
+        return gettemp(open(file.temporary_file_path()).read())
 
 
 @csrf_exempt
