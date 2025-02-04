73eef4e22f9011551a4d4c1f9d58d98f54c768c5
Michele Simionato <michele.simionato@globalquakemodel.org>
2025-02-04 07:06:11 +0100

    Fixed AttributeError: module django.core.mail has no attribute outbox

--- openquake/server/__init__.py
+++ openquake/server/__init__.py
@@ -15,6 +15,3 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake. If not, see <http://www.gnu.org/licenses/>.
-
-import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'openquake.server.settings')
