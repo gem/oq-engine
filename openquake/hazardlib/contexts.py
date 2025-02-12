d2dcd1bcce4a48173a382acee86622141c1ffa9a
Michele Simionato <michele.simionato@globalquakemodel.org>
2025-02-12 09:57:24 +0100

    Refined S-weight

--- openquake/hazardlib/contexts.py
+++ openquake/hazardlib/contexts.py
@@ -1312,6 +1312,8 @@ class ContextMaker(object):
         weight = src.dt * src.num_ruptures / self.num_rups
         if src.code == b'F':  # increase weight in the USA model
             weight *= 1.5
+        elif src.code == b'S':  # increase weight in the EUR model
+            weight *= 2
         elif src.code == b'N':  # increase weight in MEX and SAM
             weight *= 5.
         return max(weight, EPS), int(esites)
