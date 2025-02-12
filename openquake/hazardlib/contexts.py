312a3110eaaee39767b4f82a1d5befab639d92c7
Michele Simionato <michele.simionato@globalquakemodel.org>
2025-02-12 10:48:52 +0100

    Cleanup

--- openquake/hazardlib/contexts.py
+++ openquake/hazardlib/contexts.py
@@ -1311,7 +1311,7 @@ class ContextMaker(object):
                   self.num_rups * multiplier)  # num_rups from get_ctx_iter
         weight = src.dt * src.num_ruptures / self.num_rups
         if src.code == b'S':  # increase weight in the EUR model
-            weight *= 2
+            weight *= 3
         elif src.code == b'N':  # increase weight in MEX and SAM
             weight *= 5.
         return max(weight, EPS), int(esites)
