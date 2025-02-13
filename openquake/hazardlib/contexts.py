d494434f67a7f9d8e6893ba121d90724a1c0726a
Michele Simionato <michele.simionato@globalquakemodel.org>
2025-02-13 10:04:13 +0100

    Fixed minimum magnitude filtering

--- openquake/hazardlib/contexts.py
+++ openquake/hazardlib/contexts.py
@@ -333,7 +333,7 @@ def _quartets(cmaker, src, sitecol, cdist, magdist, planardict):
     if src.count_nphc() == 1:
         # one rupture per magnitude
         for m, (mag, pla) in enumerate(planardict.items()):
-            if minmag < mag < maxmag:
+            if minmag <= mag <= maxmag:
                 yield m, mag, pla, sitecol
     else:
         for m, rup in enumerate(src.iruptures()):
@@ -1068,7 +1068,7 @@ class ContextMaker(object):
                 for i, rup in enumerate(allrups):
                     rup.rup_id = src.offset + i
                 allrups = sorted([rup for rup in allrups
-                                  if minmag < rup.mag < maxmag],
+                                  if minmag <= rup.mag <= maxmag],
                                  key=bymag)
                 self.num_rups = len(allrups) or 1
                 if not allrups:
