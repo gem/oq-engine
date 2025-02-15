faec683e27f3c21a05bba085ffef9789e6f37385
Michele Simionato <michele.simionato@globalquakemodel.org>
2025-02-15 08:39:11 +0100

    Refined weight for EUR [ci skip]

--- openquake/hazardlib/contexts.py
+++ openquake/hazardlib/contexts.py
@@ -1295,7 +1295,7 @@ class ContextMaker(object):
         :param srcfilter: a SourceFilter instance
         :returns: (weight, estimate_sites)
         """
-        eps = .02 * EPS if src.code == 'S' else EPS  # needed for EUR
+        eps = .01 * EPS if src.code == 'S' else EPS  # needed for EUR
         src.dt = 0
         if src.nsites == 0:  # was discarded by the prefiltering
             return eps, 0
