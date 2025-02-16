7e341735f5adb201583e62a6a4b336ef809eae21
Michele Simionato <michele.simionato@globalquakemodel.org>
2025-02-16 11:04:32 +0100

    Refined weight

--- openquake/hazardlib/contexts.py
+++ openquake/hazardlib/contexts.py
@@ -1313,7 +1313,7 @@ class ContextMaker(object):
                   self.num_rups * multiplier)  # num_rups from get_ctx_iter
         weight = src.dt * src.num_ruptures / self.num_rups
         if src.code == b'S':  # increase weight in the EUR model
-            weight *= 2
+            weight *= 1.5
         elif src.code == b'N':  # increase weight in MEX and SAM
             weight *= 5.
         elif src.code == b'N':  # increase weight in USA
