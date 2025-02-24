b90383ed3ecbc008577de81f2115a3e9ff27b459
Michele Simionato <michele.simionato@globalquakemodel.org>
2025-02-24 10:36:21 +0100

    Make injured works like fatalities

--- openquake/risklib/scientific.py
+++ openquake/risklib/scientific.py
@@ -1820,8 +1820,10 @@ def get_agg_value(consequence, agg_values, agg_id, xltype, time_event):
     if consequence not in KNOWN_CONSEQUENCES:
         raise NotImplementedError(consequence)
     aval = agg_values[agg_id]
-    if xltype in ['affectedpop', 'injured']:
+    if xltype == 'affectedpop':
         return aval['residents']
+    elif xltype == 'injured':  # like fatalities
+        return aval[f'occupants_{time_event}']
     if consequence in ['collapsed', 'non_operational']:
         return aval['number']
     elif consequence in ['injured', 'fatalities']:
