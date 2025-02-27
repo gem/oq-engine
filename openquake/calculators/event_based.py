bf433e23723cc57361584da6769cf372e6949425
Michele Simionato <michele.simionato@globalquakemodel.org>
2025-02-27 06:10:18 +0100

    Fixed compute_avg_gmf

--- openquake/calculators/event_based.py
+++ openquake/calculators/event_based.py
@@ -464,7 +464,7 @@ def compute_avg_gmf(gmf_df, weights, min_iml):
         gmvs = numpy.zeros((E, C), F32)
         gmvs[eid.to_numpy()] = df.to_numpy()
         for c, mini in enumerate(min_iml):
-            gmvs[gmvs[:, c] == 0, c] = mini
+            gmvs[gmvs[:, c] < mini, c] = mini
         dic[sid] = geom_avg_std(gmvs, weights)
     return dic
 
