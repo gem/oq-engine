966a9686aba4ed3cf3d716d521e3452d8f67b737
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2025-02-28 15:23:17 +0100

    When resetting the rupture-related fields, also hide the plot showing the rupture

--- openquake/server/static/js/engine.js
+++ openquake/server/static/js/engine.js
@@ -513,6 +513,7 @@ function capitalizeFirstLetter(val) {
         // nodal planes are re-populated when loading rupture data; msrs are populated only once
         $('select#nodal_plane').empty();
         $('select#msr').val('WC1994');
+        $('#rupture-map').hide();
     }
 
     /* classic event management */
