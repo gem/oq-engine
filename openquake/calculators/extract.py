6051d8085e3af3d0fe8591cf7df83e474880af06
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2025-03-10 16:04:52 +0100

    Add a row for each ID_1 with the total values

--- openquake/calculators/extract.py
+++ openquake/calculators/extract.py
@@ -898,6 +898,10 @@ def extract_aggrisk_tags(dstore, what):
                 for qfield, qvalue in zip(qfields, qvalues):
                     acc[qfield].append(qvalue)
     df = pandas.DataFrame(acc)
+    total_df = df.groupby(aggby, as_index=False).sum()
+    total_df['loss_type'] = 'total'
+    df = pandas.concat([df, total_df], ignore_index=True)
+
     return df
 
 
