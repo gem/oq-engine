3420aaaf7e3191d97141c9a3f76a8eb361c6fccf
Michele Simionato <michele.simionato@globalquakemodel.org>
2025-03-16 05:11:42 +0100

    Optimized calc_average

--- openquake/hazardlib/source/point.py
+++ openquake/hazardlib/source/point.py
@@ -85,8 +85,8 @@ def calc_average(pointsources):
         a dict with average strike, dip, rake, lon, lat, dep,
         upper_seismogenic_depth, lower_seismogenic_depth
     """
-    acc = dict(lon=[], lat=[], dep=[], strike=[], dip=[], rake=[],
-               upper_seismogenic_depth=[], lower_seismogenic_depth=[],
+    dic = dict(strike=[], dip=[], rake=[], dep=[], weight=[], dep_weight=[])
+    acc = dict(lon=[], lat=[], upper_seismogenic_depth=[], lower_seismogenic_depth=[],
                rupture_aspect_ratio=[])
     rates = []
     trt = pointsources[0].tectonic_region_type
@@ -94,21 +94,23 @@ def calc_average(pointsources):
         assert src.tectonic_region_type == trt
         rates.append(sum(r for m, r in src.get_annual_occurrence_rates()))
         ws, ds = zip(*src.nodal_plane_distribution.data)
-        strike = numpy.average([np.strike for np in ds], weights=ws)
-        dip = numpy.average([np.dip for np in ds], weights=ws)
-        rake = numpy.average([np.rake for np in ds], weights=ws)
+        dic['strike'].extend([np.strike for np in ds])
+        dic['dip'].extend([np.dip for np in ds])
+        dic['rake'].extend([np.rake for np in ds])
+        dic['weight'].extend(ws)
         ws, deps = zip(*src.hypocenter_distribution.data)
-        dep = numpy.average(deps, weights=ws)
+        dic['dep'].extend(deps)
+        dic['dep_weight'].extend(ws)
         acc['lon'].append(src.location.x)
         acc['lat'].append(src.location.y)
-        acc['dep'].append(dep)
-        acc['strike'].append(strike)
-        acc['dip'].append(dip)
-        acc['rake'].append(rake)
         acc['upper_seismogenic_depth'].append(src.upper_seismogenic_depth)
         acc['lower_seismogenic_depth'].append(src.lower_seismogenic_depth)
         acc['rupture_aspect_ratio'].append(src.rupture_aspect_ratio)
-    dic = {key: numpy.average(acc[key], weights=rates) for key in acc}
+    for key in ('dip', 'strike', 'rake', 'dep'):
+        dic[key] = numpy.average(
+            dic[key], weights=dic['dep_weight'] if key=='dep' else dic['weight'])
+    del dic['weight'], dic['dep_weight']
+    dic.update({key: numpy.average(acc[key], weights=rates) for key in acc})
     dic['lon'] = numpy.round(dic['lon'], 6)
     dic['lat'] = numpy.round(dic['lat'], 6)
     return dic
