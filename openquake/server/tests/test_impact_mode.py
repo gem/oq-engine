d7a34b5016538348eabd5bcc8f1f5a3118b09e3c
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2025-03-17 16:08:30 +0100

    Fix test_run_by_usgs_id_then_remove_calc, removing rupture-related info

--- openquake/server/tests/test_impact_mode.py
+++ openquake/server/tests/test_impact_mode.py
@@ -248,16 +248,12 @@ class EngineServerTestCase(django.test.TestCase):
     def test_run_by_usgs_id_then_remove_calc(self):
         data = dict(usgs_id='us6000jllz',
                     approach='use_shakemap_from_usgs',
-                    lon=37.0143, lat=37.2256, dep=10.0, mag=7.8,
-                    rake=0.0, dip=90, strike=0,
                     time_event='night',
                     maximum_distance=100,
                     mosaic_model='MIE',
                     trt='Active Shallow Crust', truncation_level=3,
                     number_of_ground_motion_fields=2,
-                    asset_hazard_distance=15, ses_seed=42,
-                    local_timestamp='2023-02-06 04:17:34+03:00',
-                    maximum_distance_stations='')
+                    asset_hazard_distance=15, ses_seed=42)
         self.impact_run_then_remove(data)
 
     # check that the URL 'run' cannot be accessed in ARISTOTLE mode
