4a3eb950d87dbae6b38232d1a0fdff0b3baefff7
Michele Simionato <michele.simionato@globalquakemodel.org>
2025-03-31 06:23:05 +0200

    Manually unzip shapefiles on Windows

--- openquake/hazardlib/shakemap/parsers.py
+++ openquake/hazardlib/shakemap/parsers.py
@@ -21,26 +21,29 @@ https://earthquake.usgs.gov/scenario/product/shakemap-scenario/sclegacyshakeout2
 to numpy composite arrays.
 """
 
+import io
+import os
+import sys
+import pathlib
+import logging
+import json
+import base64
+import zipfile
+import tempfile
+from dataclasses import dataclass
 from urllib.request import urlopen, pathname2url
 from urllib.error import URLError
 from collections import defaultdict
 from xml.parsers.expat import ExpatError
-import io
-import os
-import pathlib
-import logging
-import json
-import zipfile
-import base64
-from dataclasses import dataclass
-import pandas as pd
 from datetime import datetime
 from zoneinfo import ZoneInfo
+
 from shapely.geometry import shape
+import pandas as pd
 import numpy
 import fiona
 
-from openquake.baselib import performance
+from openquake.baselib import performance, config
 from openquake.baselib.general import gettemp
 from openquake.baselib.node import node_from_xml
 from openquake.hazardlib import nrml, sourceconverter, valid
@@ -144,7 +147,16 @@ def get_array_shapefile(kind, fname):
     """
     fname = path2url(fname)
     if fname.endswith('.zip'):
-        fname = 'zip://' + fname[5:]
+        if sys.platform == 'win32':
+            # fiona cannot automatically unzip, so unzip manually
+            targetdir = tempfile.mkdtemp(
+                dir=config.directory.custom_tmp or None)
+            with zipfile.ZipFile(fname) as archive:
+                archive.extractall(targetdir)
+            [fname] = [os.path.join(targetdir, f)
+                       for f in os.listdir(targetdir) if f.endswith('.shp')] 
+        else:
+            fname = 'zip://' + fname[5:]
     else:
         fname = fname[5:]  # strip file:
     polygons = []
