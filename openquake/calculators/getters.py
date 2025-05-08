361215595808b68a55ce7aa2828179c5c410862a
Michele Simionato <michele.simionato@globalquakemodel.org>
2025-05-08 10:18:28 +0200

    Small fix

--- openquake/calculators/getters.py
+++ openquake/calculators/getters.py
@@ -354,14 +354,15 @@ def get_ebruptures(dstore):
     """
     ebrs = []
     trts = list(dstore['full_lt/gsim_lt'].values)
-    if 'trt_smr_start_stop' in dstore:  # event based
+    if 'trt_smr_start_stop' in dstore:  # regular case
         for trt_smr, start, stop in dstore['trt_smr_start_stop']:
             trt = trts[trt_smr // TWO24]
             for proxy in get_proxies(dstore.filename, slice(start, stop)):
                 ebrs.append(proxy.to_ebr(trt))
-    else:  # scenario
-        for proxy in get_proxies(dstore.filename):
-            ebrs.append(proxy.to_ebr('*'))
+    else:  # OQImpact calculations, I have no test for this :-(
+        1/0
+        for proxy in get_proxies(dstore.filename, slice(None)):
+            ebrs.append(proxy.to_ebr(trts[0]))
     return ebrs
 
 
