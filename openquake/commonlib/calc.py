dbf03861cc542205ca919d7aa5b277231f9d781c
Michele Simionato <michele.simionato@globalquakemodel.org>
2025-07-06 09:52:29 +0200

    Fix

--- openquake/commonlib/calc.py
+++ openquake/commonlib/calc.py
@@ -428,13 +428,15 @@ def get_slices(sbe, data, num_assets):
     return out
 
 
-def count_outputs(sbe, maxw, weight, size=int(config.memory.max_gmvs_chunk)):
+def count_outputs(eids, sbe, maxw, weight,
+                  size=int(config.memory.max_gmvs_chunk)):
     """
     Count the alt and avg outputs for each task
     """
     tot = 0
     for blk in general.block_splitter(sbe, maxw, weight):
-        alts = list(general.gen_slices(blk[0]['start'], blk[-1]['stop'], size))
+        s0, s1 = blk[0]['start'], blk[-1]['stop']
+        alts = performance.split_slices(eids[s0:s1], size)
         tot += len(alts) + 1  # 1 avg output, multiple alt outputs
     return tot
 
@@ -473,6 +475,7 @@ def starmap_from_gmfs(task_func, oq, dstore, mon):
         slices = []
         logging.info('Reading event weights')
         slices = get_slices(sbe, data, num_assets)
+    eids = data['eid'][:]
     dstore.swmr_on()
     maxw = slices['weight'].sum() / (oq.concurrent_tasks or 1) or 1.
     logging.info('maxw = {:_d}'.format(int(maxw)))
@@ -480,7 +483,7 @@ def starmap_from_gmfs(task_func, oq, dstore, mon):
     smap = parallel.Starmap.apply(
         task_func, (slices, oq, ds),
         maxweight=maxw, weight=w, h5=dstore.hdf5)
-    smap.expected_outputs = count_outputs(slices, maxw, w)
+    smap.expected_outputs = count_outputs(eids, slices, maxw, w)
     logging.info('Expected outputs =%s', smap.expected_outputs)
     return smap
 
