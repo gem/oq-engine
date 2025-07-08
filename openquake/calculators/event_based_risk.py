3102fba4b15021fb52882f34a1472d7323ee741f
Michele Simionato <michele.simionato@globalquakemodel.org>
2025-07-08 11:02:45 +0200

    Using numba

--- openquake/calculators/event_based_risk.py
+++ openquake/calculators/event_based_risk.py
@@ -74,7 +74,7 @@ def update(loss_by_AX, lti, X, alt, rlz_id, collect_rlzs):
             dict(aid=alt.aid.to_numpy(), loss=alt.loss.to_numpy()))
         tot = ldf.groupby('aid').loss.sum()
         aids = tot.index.to_numpy()
-        rlzs = numpy.zeros_like(tot)
+        rlzs = numpy.zeros_like(tot, U32)
     else:
         # rare case
         ldf = pandas.DataFrame(
@@ -347,6 +347,13 @@ def ebrisk(proxies, cmaker, sitecol, stations, dstore, monitor):
                 yield event_based_risk(gmf_df, crmodel, monitor)
 
 
+@performance.compile("(f4[:,:,:], i4[:], i4[:], f8[:], i8)")
+def fast_add(avg_losses, row, col, data, X):
+    for r, c, d in zip(row, col, data):
+        rlz, lti = divmod(c, X)
+        avg_losses[r, lti, rlz] += d
+
+
 @base.calculators.add('ebrisk', 'scenario_risk', 'event_based_risk')
 class EventBasedRiskCalculator(event_based.EventBasedCalculator):
     """
@@ -542,8 +549,7 @@ class EventBasedRiskCalculator(event_based.EventBasedCalculator):
             # avg_losses are stored as coo matrices
             with self.monitor('saving avg_losses'):
                 coo = dic.pop('avg')
-                rlzs, xlts = numpy.divmod(coo.col, self.X)
-                self.avg_losses[coo.row, xlts, rlzs] += coo.data
+                fast_add(self.avg_losses, coo.row, coo.col, coo.data, self.X)
 
     def post_execute(self, dummy):
         """
