8e54e9265aad302b715e827d8bc7350af8303737
Michele Simionato <michele.simionato@globalquakemodel.org>
2025-10-28 04:31:04 +0100

    Fixed number of tiles

--- openquake/hazardlib/source_reader.py
+++ openquake/hazardlib/source_reader.py
@@ -785,11 +785,12 @@ class CompositeSourceModel:
         G = len(cmaker.gsims)
         splits = G * mb_per_gsim / max_mb
         hint = sg.weight / max_weight
-        tiles = max(hint, splits)
         if sg.atomic or tiling:
             blocks = [sg.grp_id]
+            tiles = max(hint, splits)
         else:
             blocks = list(general.split_in_blocks(sg, hint, lambda s: s.weight))
+            tiles = splits
         tilegetters = list(sitecol.split(tiles, oq.max_sites_disagg))
         extra = dict(codes=sg.codes,
                      num_chunks=num_chunks,
