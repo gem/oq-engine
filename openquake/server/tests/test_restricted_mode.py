13863fa7c7e531987639b31505294af55064abf3
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2025-10-28 17:24:20 +0100

    When testing tags, use a random name for the tag; change how we check the difference between the lists of all jobs and only the preferred ones

--- openquake/server/tests/test_restricted_mode.py
+++ openquake/server/tests/test_restricted_mode.py
@@ -1,8 +1,10 @@
 import django
+import random
+import string
 from django.test import Client
 from openquake.commonlib.dbapi import db
 from openquake.engine.engine import create_jobs
-from openquake.server.tests.views_test import get_or_create_user
+from openquake.server.tests.views_test import get_or_create_user, random_string
 
 
 class RestrictedModeTestCase(django.test.TestCase):
@@ -151,51 +153,54 @@ class RestrictedModeTestCase(django.test.TestCase):
                {'status': 'complete', 'is_running': 0}, job.calc_id)
         self.c.login(username=self.user2.username, password=self.password2)
 
-        tag_name = 'first_tag'
+        # generate a random tag name, 10 characters long
+        first_tag = random_string(10)
         for job in jobs:
             # add the same tag to the two jobs
-            ret = self.get(f'{job.calc_id}/add_tag/{tag_name}')
+            ret = self.get(f'{job.calc_id}/add_tag/{first_tag}')
             self.assertEqual(ret.status_code, 200)
-            self.assertIn(f'The tag {tag_name} was added to job {job.calc_id}',
+            self.assertIn(f'The tag {first_tag} was added to job {job.calc_id}',
                           ret.content.decode('utf8'))
             # set the first job as preferred
-            ret = self.get(f'{job.calc_id}/set_preferred_job_for_tag/{tag_name}')
+            ret = self.get(f'{job.calc_id}/set_preferred_job_for_tag/{first_tag}')
             self.assertEqual(ret.status_code, 200)
             # when setting the second job as preferred, the preferred flag should be
             # reset for all jobs sharing that tag and the flag should be set to the
             # second job without raising errors
-            self.assertIn(f'Job {job.calc_id} was set as preferred for tag {tag_name}',
+            self.assertIn(f'Job {job.calc_id} was set as preferred for tag {first_tag}',
                           ret.content.decode('utf8'))
 
         # get the preferred job for the tag
-        ret = self.get(f'get_preferred_job_for_tag/{tag_name}')
+        ret = self.get(f'get_preferred_job_for_tag/{first_tag}')
         self.assertEqual(ret.status_code, 200)
         self.assertEqual(ret.json()['job_id'], jobs[1].calc_id)
 
         # list all jobs (preferred and not preferred)
         ret = self.get('list')
         self.assertEqual(ret.status_code, 200)
-        self.assertGreater(len(ret.json()), 1)
-
+        n_all_jobs = len(ret.json())
         # list only the preferred jobs
         ret = self.get('list?preferred_only=1')
         self.assertEqual(ret.status_code, 200)
-        self.assertEqual(len(ret.json()), 1)
+        n_preferred_jobs = len(ret.json())
+        # NOTE: we might want to check that all the listed jobs are not preferred, but
+        # it is a bit more complex, involving a query on job_tag for each
+        self.assertGreater(n_all_jobs, n_preferred_jobs)
 
         # try to re-add the same first_tag to the second job
-        ret = self.get(f'{jobs[1].calc_id}/add_tag/{tag_name}')
+        ret = self.get(f'{jobs[1].calc_id}/add_tag/{first_tag}')
         self.assertEqual(ret.status_code, 403)
         self.assertIn('error', ret.json())
         self.assertIn("UNIQUE constraint failed", ret.json()['error'])
 
         # remove the first_tag from the second job
-        ret = self.get(f'{jobs[1].calc_id}/remove_tag/first_tag')
+        ret = self.get(f'{jobs[1].calc_id}/remove_tag/{first_tag}')
         self.assertEqual(ret.status_code, 200)
-        self.assertIn(f'Tag first_tag was removed from job {jobs[1].calc_id}',
+        self.assertIn(f'Tag {first_tag} was removed from job {jobs[1].calc_id}',
                       ret.content.decode('utf8'))
 
         # get the preferred job for the tag
-        ret = self.get(f'get_preferred_job_for_tag/{tag_name}')
+        ret = self.get(f'get_preferred_job_for_tag/{first_tag}')
         self.assertEqual(ret.status_code, 200)
         self.assertIsNone(ret.json()['job_id'])
 
