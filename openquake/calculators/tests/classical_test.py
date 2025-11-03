dab2429eb5c8ff6710368243d560d2e7906340ad
Michele Simionato <michele.simionato@gmail.com>
2025-11-03 10:02:04 +0100

    Executing the tests for Macedo (2019) and Kuehn

--- openquake/calculators/tests/classical_test.py
+++ openquake/calculators/tests/classical_test.py
@@ -33,9 +33,9 @@ from openquake.calculators.extract import extract
 from openquake.calculators.tests import CalculatorTestCase
 from openquake.qa_tests_data.classical import (
     case_01, case_02, case_03, case_04, case_05, case_06, case_07, case_08,
-    case_12, case_18, case_22, case_23, case_24, case_25, case_26, case_27,
-    case_29, case_32, case_33, case_34, case_35, case_37, case_38, case_40,
-    case_41, case_42, case_43, case_44, case_47, case_48, case_49,
+    case_09, case_12, case_18, case_22, case_23, case_24, case_25, case_26,
+    case_27, case_29, case_32, case_33, case_34, case_35, case_37, case_38,
+    case_40, case_41, case_42, case_43, case_44, case_47, case_48, case_49,
     case_50, case_51, case_53, case_54, case_55, case_57,
     case_60, case_61, case_62, case_63, case_64, case_65, case_66,
     case_67, case_69, case_70, case_72, case_74, case_75, case_76, case_77,
@@ -187,6 +187,12 @@ class ClassicalTestCase(CalculatorTestCase):
         self.run_calc(case_08.__file__, 'job.ini')
         [fname] = export(('hcurves/mean', 'csv'), self.calc.datastore)
         self.assertEqualFiles('expected/hcurves.csv', fname)
+
+    def test_case_09(self):
+        # Macedo 2019 conditional GMPE based on KuehnEtAl2020SInter
+        self.run_calc(case_09.__file__, 'job.ini')
+        [f1] = export(('hcurves/mean', 'csv'), self.calc.datastore)
+        self.assertEqualFiles('expected/hazard_curve-mean-IA.csv', f1)
 
     def test_wrong_smlt(self):
         with self.assertRaises(InvalidFile):
