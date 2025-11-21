e398d8e6ff321c38e699aae0c08bd1cc667c39fb
Michele Simionato <michele.simionato@globalquakemodel.org>
2025-11-21 02:58:29 +0100

    Better filtering

--- openquake/calculators/event_based.py
+++ openquake/calculators/event_based.py
@@ -346,30 +346,31 @@ def get_allargs(oq, sitecol, assetcol, station_data_sites, dstore):
             rlzs_by_gsim[model, trt_smr] = rbg
     allrups = dstore['ruptures'][:]
     logging.info(f'Read {len(allrups):_d} ruptures')
+
+    # NB: it is faster to filter a huge number of ruptures
+    # upfront rather than looping on each (model, trt_smr)
+    if len(sitecol) > oq.max_sites_disagg:
+        # can manage 2 million sites in 13 minutes for IND
+        filrups = close_ruptures(allrups, sitecol, assetcol)
+        logging.info(f'Selected {len(filrups):_d} ruptures')
+    else:
+        filrups = allrups
     totw = 0
-    nrups = 0
     nsites = 0
     affected = 0
     acc = {}
     pairs = numpy.unique(allrups[['model', 'trt_smr']])
     for model, trt_smr in pairs:
-        ok = (allrups['model'] == model) & (allrups['trt_smr'] == trt_smr)
-        if len(sitecol) > oq.max_sites_disagg:
-            # can manage 2 million sites in 13 minutes for IND
-            filrups = close_ruptures(allrups[ok], sitecol, assetcol)
-        else:
-            filrups = allrups[ok]
-        logging.info(f'Filtered {len(filrups):_d}/{ok.sum():_d} ruptures')
-        if len(filrups):
-            acc[model, trt_smr] = filrups
-            totw += rup_weight(filrups).sum()
-            nrups += len(filrups)
-            nsites += filrups['nsites'].sum()
-            affected = max(affected, filrups['nsites'].sum())
+        ok = (filrups['model'] == model) & (filrups['trt_smr'] == trt_smr)
+        rups = filrups[ok]
+        if len(rups):
+            acc[model, trt_smr] = rups
+            totw += rup_weight(rups).sum()
+            nsites += rups['nsites'].sum()
+            affected = max(affected, rups['nsites'].sum())
     assert totw, 'All ruptures have been filtered out'
-    logging.info(f'Selected {nrups:_d} ruptures close to the sites')
     logging.info('Affected assets/sites ~%.0f per rupture, max=%.0f',
-                 nsites/nrups, affected)
+                 nsites / len(filrups), affected)
     maxw = totw / (oq.concurrent_tasks or 1)
     logging.info(f'{round(maxw)=}')
 
@@ -448,8 +449,10 @@ def starmap_from_rups(func, oq, full_lt, sitecol, assetcol,
         mea, tau, phi = computer.get_mea_tau_phi(dstore.hdf5)
         del proxy.geom  # to reduce data transfer
 
-    allargs = get_allargs(
-        oq, sitecol, assetcol, (station_data, station_sites), dstore)
+    with performance.Monitor(
+            'building arguments', measuremem=True, h5=dstore.hdf5):
+        allargs = get_allargs(
+            oq, sitecol, assetcol, (station_data, station_sites), dstore)
     assert len(allargs) < TWO16, len(allargs)
     dstore.swmr_on()
     smap = parallel.Starmap(func, h5=dstore.hdf5)
