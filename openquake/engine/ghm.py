d10c8b9cef3053854abe6f4571fe43aeebc31626
Michele Simionato <michele.simionato@globalquakemodel.org>
2025-11-29 06:48:41 +0100

    Cleanup

--- openquake/engine/ghm.py
+++ openquake/engine/ghm.py
@@ -24,36 +24,53 @@ from openquake.baselib.gitwrapper import git
 from openquake.engine import engine
 
 def read(manifest_toml):
+    """
+    Read the manifest file and set 'mosaic_dir' and 'inis'
+    """
+    mosaic_dir = os.path.dirname(manifest_toml)
     with open(manifest_toml, encoding='utf8') as f:
         manifest = toml.load(f)
-    manifest['Global']['mosaic_dir'] = os.path.dirname(manifest_toml)
+    inis = []
+    for model, dic in manifest['Hazard'].items():
+        repo = os.path.join(mosaic_dir, model)
+        ini = dic.get('ini', 'job_vs30.ini')
+        inis.append(os.path.join(repo, 'in', ini))
+    manifest['mosaic_dir'] = mosaic_dir
+    manifest['inis'] = inis
     return manifest
 
 
 def checkout(manifest_toml):
+    """
+    Checkout the repositories listed in the manifest
+    """
     manifest = read(manifest_toml)
-    glob = manifest['Global']
-    inis = []
     t0 = time.time()
+    # do not checkout folders in the oq-engine tree
+    change = 'oq-engine' not in os.path.abspath(manifest['mosaic_dir'])
     for model, dic in manifest['Hazard'].items():
-        repo = os.path.join(glob['mosaic_dir'], model)
-        if 'oq-engine' not in repo:
+        repo = os.path.join(manifest['mosaic_dir'], model)
+        if change:
             print(f'============================== {repo}')
             git(repo, ['clean', '-f'])
             git(repo, ['reset', '--hard', dic['checkout']])
-        ini = dic.get('ini', 'job_vs30.ini')
-        inis.append(os.path.join(repo, 'in', ini))
     dt = time.time() - t0
-    print(f'Checked out the repositories in {dt:.0f} seconds')
-    return inis
+    if change:
+        print(f'Checked out the repositories in {dt:.0f} seconds')
+    return manifest
 
 
 def run(manifest_toml, *, concurrent_jobs: int=1):
-    manifest, inis = checkout(manifest_toml)
-    ss = manifest['Global'].get('sample_sites')
-    if ss:
+    """
+    Run the models listed in the manifest
+    """
+    manifest = read(manifest_toml)
+    gl = manifest['Global']
+    if ss := gl.get('sample_sites'):
         os.environ['OQ_SAMPLE_SITES'] = str(ss)
-    jobs = engine.create_jobs(inis, tag=manifest['Global']['description'])
+    if ss := gl.get('sample_sources'):
+        os.environ['OQ_SAMPLE_SOURCES'] = str(ss)
+    jobs = engine.create_jobs(manifest['inis'], tag=gl['description'])
     engine.run_jobs(jobs, concurrent_jobs)
 
 
