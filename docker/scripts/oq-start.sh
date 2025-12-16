85fde38d30f7ea6139498a73252f413bb0d2de77
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2025-12-16 14:13:35 +0100

    Remove some debug prints

--- docker/scripts/oq-start.sh
+++ docker/scripts/oq-start.sh
@@ -41,10 +41,8 @@ if [ "$OQ_APPLICATION_MODE" = "RESTRICTED" ]; then
 	cd ${oq_basedir}/openquake/server
     python3 manage.py migrate
     if [ -n "$OQ_ADMIN_LOGIN" ]; then
-	    echo "OQ_ADMIN_LOGIN is set"
         python3 manage.py createuser ${OQ_ADMIN_LOGIN} ${OQ_ADMIN_EMAIL} --level 2 --password ${OQ_ADMIN_PASSWORD} --no-email --staff --superuser
     else
-	    echo "OQ_ADMIN_LOGIN is NOT set"
         python3 manage.py createuser admin admin@example.com --level 2 --password admin --no-email --staff --superuser
     fi
 fi
