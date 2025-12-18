962484b5189a1b0e0bc4362b95970bfe81e262b9
Michele Simionato <michele.simionato@globalquakemodel.org>
2025-12-18 11:37:16 +0100

    Using oq.years

--- openquake/engine/engine.py
+++ openquake/engine/engine.py
@@ -494,10 +494,10 @@ class _Workflow:
             oqs.append(oq)
         if 'classical' in oq.calculation_mode:
             for oq in oqs[1:]:
-                if oq.return_periods != oqs[0].return_periods:
+                if oq.years != oqs[0].years:
                     raise NameError(
-                        f'Expected return_periods = {oqs[0].return_periods}, '
-                        f'got {oq.return_periods}')
+                        f'Expected return_periods = {oqs[0].years}, '
+                        f'got {oq.years}')
         if 'risk' in oq.calculation_mode or 'damage' in oq.calculation_mode:
             for oq in oqs[1:]:
                 if oq.eff_time != oqs[0].eff_time:
