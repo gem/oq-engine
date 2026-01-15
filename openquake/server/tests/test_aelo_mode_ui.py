3e1701ff9fcf1d821cfe05e4a81a5716c8beeb9d
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2026-01-15 10:52:53 +0100

    Use a timeout of 10 seconds also waiting for the button to click to be available

--- openquake/server/tests/test_aelo_mode_ui.py
+++ openquake/server/tests/test_aelo_mode_ui.py
@@ -19,6 +19,6 @@ def test_aelo_run_job(authenticated_page):
 
     job_row = page.latest_job_row()
     expect(job_row.get_by_text("executing")).to_be_visible(timeout=10_000)
-    job_row.get_by_role("link", name="AbortFIXME").click()
-    authenticated_page.get_by_role("button", name="Yes, abort").click()
+    job_row.get_by_role("link", name="AbortFIXME").click(timeout=10_000)
+    authenticated_page.get_by_role("button", name="Yes, abort").click(timeout=10_000)
     expect(job_row.get_by_text("aborted")).to_be_visible(timeout=10_000)
