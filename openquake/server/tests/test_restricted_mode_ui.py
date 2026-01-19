f1adddfa9c184b78686f9c024551d2596f5fc050
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2026-01-19 11:53:17 +0100

    Fix test_restrictd_mode_ui to use a level 0 user in RESTRICTED mode

--- openquake/server/tests/test_restricted_mode_ui.py
+++ openquake/server/tests/test_restricted_mode_ui.py
@@ -1,6 +1,10 @@
+import pytest
 from playwright.sync_api import expect
 
 
-def test_login(ui_logged_in_page, test_credentials):
+@pytest.mark.parametrize("user", [0], indirect=True)
+@pytest.mark.parametrize("application_mode", ["RESTRICTED"], indirect=True)
+def test_login(application_mode, ui_logged_in_page, user, test_credentials):
     expect(ui_logged_in_page.get_by_text(
         f"Hello, {test_credentials['username']}")).to_be_visible()
+    ui_logged_in_page.wait_for_load_state("networkidle")
