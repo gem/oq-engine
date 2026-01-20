d9376ad448c5e5404b4c956927870b7d8d8db1d1
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2026-01-20 17:28:53 +0100

    Remove the prepending 'AELO for' from the description when getting the site name to print in the email notification

--- openquake/server/views.py
+++ openquake/server/views.py
@@ -937,7 +937,8 @@ def aelo_callback(
     from_email = settings.EMAIL_HOST_USER
     to = [job_owner_email]
     reply_to = settings.EMAIL_SUPPORT
-    site_name = inputs['description']
+    # e.g. 'AELO for CCA'->'CCA'
+    site_name = inputs['description'][9:]
     lon, lat = inputs['sites'].split()
     site_class = inputs['site_class']
     vs30s = inputs['vs30'].split()
