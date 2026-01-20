66a8531884e9d8c99224cbec7c8d18243d2589ca
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2026-01-20 11:10:56 +0100

    Add tests for all impact workflows, both for level 1 and level 2 users

--- openquake/server/tests/pages/impact_page.py
+++ openquake/server/tests/pages/impact_page.py
@@ -1,30 +1,49 @@
-from openquake.server.tests.pages.engine_page import EnginePage
+from importlib.resources import files
 from playwright.sync_api import expect
+from openquake.server.tests.pages.engine_page import EnginePage
 
 
 class ImpactPage(EnginePage):
     def set_rupture_identifier(self, usgs_id):
         self.page.locator('#usgs_id').fill(usgs_id)
 
-    def retrieve_shakemap_data(self):
+    def select_shakemap_version(self):
+        self.page.wait_for_function(
+            "document.querySelector('select#shakemap_version').options.length > 0",
+            timeout=30_000
+        )
+
+    def retrieve_data(self):
         self.page.locator("#submit_impact_get_rupture").click()
 
     def run_impact_calc(self):
         self.page.locator("#submit_impact_calc").click()
-
-    def local_timestamp(self):
-        return self.page.locator('input#local_timestamp')
 
     def set_time_of_the_event(self, value):
         selector = self.page.locator('select#time_event')
         expect(selector).to_be_editable()
         selector.select_option(label=value)
 
+    def set_no_uncertainty(self):
+        self.no_uncertainty_ckb().check()
+
+    def rupture_identifier(self):
+        return self.page.locator('input#usgs_id')
+
+    def local_timestamp(self):
+        return self.page.locator('input#local_timestamp')
+
     def no_uncertainty_ckb(self):
         return self.page.locator('input#no_uncertainty')
 
-    def set_no_uncertainty(self):
-        self.no_uncertainty_ckb().check()
+    def rupture_map(self):
+        return self.page.locator('div#rupture-map')
+
+    def intensity_map(self):
+        return self.page.locator('div#intensity-map')
+
+    def pga_map(self):
+        return self.page.locator('div#pga-map')
 
 
 class ImpactPageLevel0(ImpactPage):
@@ -36,4 +55,60 @@ class ImpactPageLevel1(ImpactPage):
 
 
 class ImpactPageLevel2(ImpactPage):
-    pass
+
+    def set_approach(self, approach_label):
+        # We could find a better approach to make sure the handler is ready
+        self.page.wait_for_timeout(200)
+
+        approach = self.page.get_by_role("radio", name=approach_label)
+        approach.check()
+        expect(approach).to_be_checked()
+
+    def set_longitude(self, lon):
+        self.page.locator("input#lon").fill(str(lon))
+
+    def set_latitude(self, lat):
+        self.page.locator("input#lat").fill(str(lat))
+
+    def set_depth(self, dep):
+        self.page.locator("input#dep").fill(str(dep))
+
+    def set_magnitude(self, mag):
+        self.page.locator("input#mag").fill(str(mag))
+
+    def set_aspect_ratio(self, ar):
+        self.page.locator("input#aspect_ratio").fill(str(ar))
+
+    def set_rake(self, rake):
+        self.page.locator("input#rake").fill(str(rake))
+
+    def set_dip(self, dip):
+        self.page.locator("input#dip").fill(str(dip))
+
+    def set_strike(self, strike):
+        self.page.locator("input#strike").fill(str(strike))
+
+    def choose_rupture_model(self):
+        expect(self.page.locator("input#rupture_file_input")).to_be_visible()
+        rupture_file = (
+            files("openquake.hazardlib.tests.shakemap.data")
+            / "fault_rupture.xml"
+        )
+
+        self.page.set_input_files(
+            "input[type=file]#rupture_file_input",
+            rupture_file,
+        )
+
+    def confirm_relocated_hypocenter_warning(self):
+        expect(self.page.get_by_text("it was moved")).to_be_visible(timeout=15_000)
+        modal = self.page.get_by_text("it was moved")
+        self.page.get_by_role("button", name="Close").click()
+        expect(modal).not_to_be_visible()
+
+    def select_nodal_plane(self):
+        expect(self.page.locator('select#nodal_plane')).to_be_visible()
+        self.page.wait_for_function(
+            "document.querySelector('select#nodal_plane').options.length > 0",
+            timeout=30_000
+        )
--- openquake/server/tests/test_impact_mode_ui.py
+++ openquake/server/tests/test_impact_mode_ui.py
@@ -1,12 +1,13 @@
 import pytest
-from openquake.server.tests.pages.impact_page import ImpactPage
+from openquake.server.tests.pages.impact_page import (
+    ImpactPageLevel0, ImpactPageLevel1, ImpactPageLevel2)
 from playwright.sync_api import expect
 
 
 @pytest.mark.parametrize("user", [0], indirect=True)
 @pytest.mark.parametrize("application_mode", ["IMPACT"], indirect=True)
 def test_impact_ui_level_0(application_mode, authenticated_page, user):
-    page = ImpactPage(authenticated_page)
+    page = ImpactPageLevel0(authenticated_page)
     # TODO: check that:
     #       * there is no input form
     #       * only shared calculations are visible
@@ -15,10 +16,76 @@ def test_impact_ui_level_0(application_mode, authenticated_page, user):
 @pytest.mark.parametrize("user", [1], indirect=True)
 @pytest.mark.parametrize("application_mode", ["IMPACT"], indirect=True)
 def test_impact_ui_level_1(application_mode, authenticated_page, user):
-    page = ImpactPage(authenticated_page)
+    page = ImpactPageLevel1(authenticated_page)
+    page.set_rupture_identifier('us6000phrk')
+    page.select_shakemap_version()
+    page.retrieve_data()
+    expect(page.intensity_map()).to_be_visible(timeout=30_000)
+    expect(page.pga_map()).to_be_visible(timeout=30_000)
+    expect(page.local_timestamp()).to_be_visible(timeout=30_000)
+    expect(page.local_timestamp()).not_to_be_editable()
+    page.set_time_of_the_event('Night')
+    expect(page.no_uncertainty_ckb()).to_be_visible()
+    expect(page.no_uncertainty_ckb()).not_to_be_checked()
+    page.set_no_uncertainty()
+    page.run_impact_calc()
+    page.abort_latest_job()
 
-    page.set_rupture_identifier('us6000rfbw')
-    page.retrieve_shakemap_data()
+
+@pytest.mark.parametrize("user", [2], indirect=True)
+@pytest.mark.parametrize("application_mode", ["IMPACT"], indirect=True)
+def test_impact_ui_level_2_use_shakemap(application_mode, authenticated_page, user):
+    page = ImpactPageLevel2(authenticated_page)
+    page.set_approach('Use ShakeMap from the USGS')
+    expect(page.rupture_identifier()).to_be_visible()
+    page.set_rupture_identifier('us6000phrk')
+    page.select_shakemap_version()
+    page.retrieve_data()
+    expect(page.intensity_map()).to_be_visible(timeout=30_000)
+    expect(page.pga_map()).to_be_visible(timeout=30_000)
+    expect(page.local_timestamp()).to_be_visible(timeout=30_000)
+    expect(page.local_timestamp()).not_to_be_editable()
+    page.set_time_of_the_event('Night')
+    expect(page.no_uncertainty_ckb()).to_be_visible()
+    expect(page.no_uncertainty_ckb()).not_to_be_checked()
+    page.set_no_uncertainty()
+    page.run_impact_calc()
+    page.abort_latest_job()
+
+
+@pytest.mark.parametrize("user", [2], indirect=True)
+@pytest.mark.parametrize("application_mode", ["IMPACT"], indirect=True)
+def test_impact_ui_level_2_use_point_rupture(
+        application_mode, authenticated_page, user):
+    page = ImpactPageLevel2(authenticated_page)
+    page.set_approach('Use point rupture from the USGS')
+    expect(page.rupture_identifier()).to_be_visible()
+    page.set_rupture_identifier('us6000jllz')
+    page.select_shakemap_version()
+    page.retrieve_data()
+    expect(page.rupture_map()).to_be_visible(timeout=30_000)
+    expect(page.local_timestamp()).to_be_visible(timeout=30_000)
+    expect(page.local_timestamp()).not_to_be_editable()
+    page.set_time_of_the_event('Night')
+    expect(page.no_uncertainty_ckb()).to_be_visible()
+    expect(page.no_uncertainty_ckb()).not_to_be_checked()
+    page.set_no_uncertainty()
+    page.run_impact_calc()
+    page.abort_latest_job()
+
+
+@pytest.mark.parametrize("user", [2], indirect=True)
+@pytest.mark.parametrize("application_mode", ["IMPACT"], indirect=True)
+def test_impact_ui_level_2_build_rupture(
+        application_mode, authenticated_page, user):
+    page = ImpactPageLevel2(authenticated_page)
+    page.set_approach('Build rupture from USGS nodal plane solutions')
+    expect(page.rupture_identifier()).to_be_visible()
+    page.set_rupture_identifier('usp0001ccb')
+    page.select_shakemap_version()
+    page.select_nodal_plane()
+    page.retrieve_data()
+    expect(page.rupture_map()).to_be_visible(timeout=30_000)
     expect(page.local_timestamp()).to_be_visible(timeout=30_000)
     expect(page.local_timestamp()).not_to_be_editable()
     page.set_time_of_the_event('Night')
@@ -32,10 +99,84 @@ def test_impact_ui_level_1(application_mode, authenticated_page, user):
 
 @pytest.mark.parametrize("user", [2], indirect=True)
 @pytest.mark.parametrize("application_mode", ["IMPACT"], indirect=True)
-def test_impact_ui_level_2(application_mode, authenticated_page, user):
-    page = ImpactPage(authenticated_page)
+def test_impact_ui_level_2_use_shakemap_fault_rupture(
+        application_mode, authenticated_page, user):
+    page = ImpactPageLevel2(authenticated_page)
+    page.set_approach('Use ShakeMap fault rupture from the USGS')
+    expect(page.rupture_identifier()).to_be_visible()
+    page.set_rupture_identifier('us6000phrk')
+    page.select_shakemap_version()
+    page.retrieve_data()
+    expect(page.rupture_map()).to_be_visible(timeout=30_000)
+    expect(page.local_timestamp()).to_be_visible(timeout=30_000)
+    expect(page.local_timestamp()).not_to_be_editable()
+    page.set_time_of_the_event('Night')
+    expect(page.no_uncertainty_ckb()).to_be_visible()
+    expect(page.no_uncertainty_ckb()).not_to_be_checked()
+    page.set_no_uncertainty()
 
-    page.set_rupture_identifier('us6000rfbw')
+    page.run_impact_calc()
+    page.abort_latest_job()
 
-    # page.run_impact_calc()
-    # page.abort_latest_job()
+
+@pytest.mark.parametrize("user", [2], indirect=True)
+@pytest.mark.parametrize("application_mode", ["IMPACT"], indirect=True)
+def test_impact_ui_level_2_use_finite_fault(
+        application_mode, authenticated_page, user):
+    page = ImpactPageLevel2(authenticated_page)
+    page.set_approach('Use finite fault model from the USGS')
+    expect(page.rupture_identifier()).to_be_visible()
+    page.set_rupture_identifier('us6000jllz')
+    page.select_shakemap_version()
+    page.retrieve_data()
+    expect(page.rupture_map()).to_be_visible(timeout=30_000)
+    expect(page.local_timestamp()).to_be_visible(timeout=30_000)
+    expect(page.local_timestamp()).not_to_be_editable()
+    page.set_time_of_the_event('Night')
+    expect(page.no_uncertainty_ckb()).to_be_visible()
+    expect(page.no_uncertainty_ckb()).not_to_be_checked()
+    page.set_no_uncertainty()
+    page.run_impact_calc()
+    page.abort_latest_job()
+
+
+@pytest.mark.parametrize("user", [2], indirect=True)
+@pytest.mark.parametrize("application_mode", ["IMPACT"], indirect=True)
+def test_impact_ui_level_2_provide_rupture_nrml(
+        application_mode, authenticated_page, user):
+    page = ImpactPageLevel2(authenticated_page)
+    page.set_approach('Provide earthquake rupture in OpenQuake NRML format')
+    page.choose_rupture_model()
+    page.retrieve_data()
+    expect(page.rupture_map()).to_be_visible(timeout=30_000)
+    page.confirm_relocated_hypocenter_warning()
+    # expect(page.local_timestamp()).not_to_be_visible(timeout=30_000)
+    expect(page.no_uncertainty_ckb()).to_be_visible()
+    expect(page.no_uncertainty_ckb()).not_to_be_checked()
+    page.set_no_uncertainty()
+    page.run_impact_calc()
+    page.abort_latest_job()
+
+
+@pytest.mark.parametrize("user", [2], indirect=True)
+@pytest.mark.parametrize("application_mode", ["IMPACT"], indirect=True)
+def test_impact_ui_level_2_provide_rupture_params(
+        application_mode, authenticated_page, user):
+    page = ImpactPageLevel2(authenticated_page)
+    page.set_approach('Provide earthquake rupture parameters')
+    page.set_longitude(37.6)
+    page.set_latitude(37.6)
+    page.set_depth(20.0)
+    page.set_magnitude(7.8)
+    page.set_aspect_ratio(2)
+    page.set_rake(0.0)
+    page.set_dip(90)
+    page.set_strike(51.9)
+    page.retrieve_data()
+    expect(page.rupture_map()).to_be_visible(timeout=30_000)
+    page.set_time_of_the_event('Night')
+    expect(page.no_uncertainty_ckb()).to_be_visible()
+    expect(page.no_uncertainty_ckb()).not_to_be_checked()
+    page.set_no_uncertainty()
+    page.run_impact_calc()
+    page.abort_latest_job()
