9f939a0174d83a0a9cd52f57117dda4316193d11
Michele Simionato <michele.simionato@globalquakemodel.org>
2026-01-28 09:30:17 +0100

    Removed step

--- openquake/hazardlib/contexts.py
+++ openquake/hazardlib/contexts.py
@@ -1496,7 +1496,9 @@ class RmapMaker(object):
             # tested in oq-risk-tests/test/classical/usa_ucerf
             tiles = sites.split_in_tiles(len(sites) // 5000)
         for tile in tiles:
-            for ctx in self.cmaker.get_ctx_iter(src, tile, step=step):
+            # NB: I am NOT passing the step, otherwise the
+            # weigths would be all wrong
+            for ctx in self.cmaker.get_ctx_iter(src, tile):
                 if self.cmaker.deltagetter:
                     # adjust occurrence rates in case of aftershocks
                     with self.cmaker.delta_mon:
