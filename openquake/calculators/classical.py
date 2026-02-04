67c8271742a8b9e4494d888020c471a9611f9115
Michele Simionato <michele.simionato@globalquakemodel.org>
2026-02-04 14:39:21 +0100

    Fixed g-index offset

--- openquake/calculators/classical.py
+++ openquake/calculators/classical.py
@@ -226,7 +226,10 @@ def classical(grp_keys, tilegetter, cmaker, dstore, monitor):
         blks = _split_src(list(grps[0]), 10)
         t0 = time.time()
         for i, blk in enumerate(blks, 1):
-            res += baseclassical(blk, sites, cmaker, 0)
+            out = baseclassical(blk, sites, cmaker, 0)
+            gid = out['rmap'].gid
+            gid -= gid[0]  # change g-index offset
+            res += out
             dt = time.time() - t0
             if dt > cmaker.oq.time_per_task:
                 rest = sum(blks[i:], [])
