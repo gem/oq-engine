0bb8f74575cf2b3d86c2684ee90be8bb419da8da
Michele Simionato <michele.simionato@globalquakemodel.org>
2026-02-12 03:11:53 +0100

    Better logging [ci skip]

--- openquake/calculators/classical.py
+++ openquake/calculators/classical.py
@@ -599,8 +599,6 @@ class ClassicalCalculator(base.HazardCalculator):
         maxtiles = 1
         max_gb, _, _ = getters.get_rmap_gb(self.datastore, self.full_lt)
         self.split_time = split_time = max(max_gb * 30, 20)
-        if not self.few_sites:
-            logging.info(f'{split_time=:.0f} seconds')
         for cmaker, tilegetters, grp_keys, atomic in data:
             cmaker.split_time = split_time
             if self.few_sites or oq.disagg_by_src or len(grp_keys) > 1:
@@ -618,6 +616,8 @@ class ClassicalCalculator(base.HazardCalculator):
                     for grp_key in grp_keys:
                         allargs.append(([grp_key], tgetter, cmaker, ds))
             maxtiles = max(maxtiles, len(tilegetters))
+        if not self.few_sites and self.rmap:
+            logging.info(f'{split_time=:.0f} seconds')
         logging.warning('This is a calculation with %d tasks, maxtiles=%d',
                         len(allargs), maxtiles)
 
