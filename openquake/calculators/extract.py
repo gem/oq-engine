ea69e85a79ddf0060f06b91686e1d8e560e97f69
Michele Simionato <michele.simionato@globalquakemodel.org>
2026-02-21 04:56:03 +0100

    Fixed test

--- openquake/calculators/extract.py
+++ openquake/calculators/extract.py
@@ -1376,7 +1376,7 @@ def extract_relevant_events(dstore, dummy=None):
     if 'relevant_events' not in dstore:
         all_events.sort(order='id')
         return all_events
-    rel_events = dstore['relevant_events'][:]
+    rel_events = dstore['relevant_events']['id']
     events = all_events[rel_events]
     events.sort(order='id')
     return events
