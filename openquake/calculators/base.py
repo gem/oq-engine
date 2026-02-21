c71d6f80d1ad9d688fff475f07f775d5a7851ed6
Michele Simionato <michele.simionato@globalquakemodel.org>
2026-02-21 07:46:16 +0100

    Removing unused code

--- openquake/calculators/base.py
+++ openquake/calculators/base.py
@@ -953,16 +953,9 @@ class HazardCalculator(BaseCalculator):
                           if oq.region_grid_spacing else ASSOC_DIST)
             # keep the sites of the parent close to the sites of the child
             # this is called by mosaic_for_ses/job_sites.csv
-            if oq.region:
-                self.sitecol, _array, _discarded = geo.utils.assoc(
-                    child, haz_sitecol, assoc_dist, 'filter')
-                self.datastore['sitecol'] = self.sitecol
-            elif oq.sites or 'site_model' in oq.inputs:
-                if 'site_model' not in oq.inputs:
-                    child.assoc(haz_sitecol.array, assoc_dist, 'warn')
-                self.datastore['sitecol'] = self.sitecol = child
-            else:
-                raise RuntimeError('No region, no site_model no sites??')
+            if 'site_model' not in oq.inputs:
+                child.assoc(haz_sitecol.array, assoc_dist, 'warn')
+            self.datastore['sitecol'] = self.sitecol = child
         else:  # base case
             self.sitecol = haz_sitecol
         if self.sitecol and oq.imtls:
