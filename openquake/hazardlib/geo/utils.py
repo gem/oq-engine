3d54d81e42098df6460335a571a7b4b553d7bde8
Michele Simionato <michele.simionato@globalquakemodel.org>
2026-03-05 11:18:17 +0100

    Fixed get_bounding_box

--- openquake/hazardlib/geo/utils.py
+++ openquake/hazardlib/geo/utils.py
@@ -510,10 +510,14 @@ def get_bounding_box(obj, maxdist):
         min_lon, max_lon = lons.min(), lons.max()
         if cross_idl(min_lon, max_lon):
             lons %= 360
-        bbox = lons.min(), lats.min(), lons.max(), lats.max()
+        min_lon, max_lon = lons.min(), lats.min()
+        if min_lon >= max_lon:  # swap
+            min_lon, max_lon = max_lon, min_lon
+        bbox = min_lon, lats.min(), max_lon, lats.max()
+
     a1 = min(maxdist * KM_TO_DEGREES, 90)
     a2 = angular_distance(maxdist, bbox[1], bbox[3])
-    delta = bbox[2] - bbox[0] + 2 * a2
+    delta = max_lon - min_lon + 2 * a2
     if delta > 180:
         raise BBoxError('The buffer of %d km is too large, the bounding '
                         'box is larger than half the globe: %d degrees' %
