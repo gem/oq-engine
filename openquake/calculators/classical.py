63acdcd83d5d51f60c6058960db6059d418f2dba
Michele Simionato <michele.simionato@globalquakemodel.org>
2026-03-05 04:15:13 +0100

    Use threads in save_rates

--- openquake/calculators/classical.py
+++ openquake/calculators/classical.py
@@ -652,17 +652,18 @@ class ClassicalCalculator(base.HazardCalculator):
         # save the rates and performs some checks
         oq = self.oqparam
         size_mb = sum(rmap.size_mb for rmap in self.rmap.values())
-        if len(self.rmap) > 1 and size_mb > int(config.memory.pmap_max_mb):
-            # tested in classical/case_06 and in oq-risk-tests ptiling
+        if size_mb > 100:
+            # tested in performance.zip
             L1 = oq.imtls.size // len(oq.imtls)
-            savemap = parallel.Starmap(save_rates, h5=self.datastore)
+            savemap = parallel.Starmap(save_rates, h5=self.datastore,
+                                       distribute='threadpool')
             for grp_id, rmap in self.rmap.items():
                 for rm in rmap.split(L1):
                     savemap.submit((rm, self.num_chunks, None))
             savemap.reduce()
         else:
             # store sequentially
-            logging.info('Saving %d RateMap', len(self.rmap))
+            logging.info('Saving %d RateMap(s)', len(self.rmap))
             for rmap in self.rmap.values():
                 for g in rmap.jid:
                     _store(rmap.to_array(g), self.num_chunks, self.datastore)
