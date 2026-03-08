ef77520e714ce6c8b5e17051fab11e15397e91b0
Anirudh Rao <anirudh.rao@globalquakemodel.org>
2026-03-08 16:36:35 +0100

    Use test tables for Holmgren 2024

--- openquake/hazardlib/tests/gsim/holmgren_2024_test.py
+++ openquake/hazardlib/tests/gsim/holmgren_2024_test.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 #
-# Copyright (C) 2012-2026 GEM Foundation
+# Copyright (C) 2026 GEM Foundation
 #
 # OpenQuake is free software: you can redistribute it and/or modify it
 # under the terms of the GNU Affero General Public License as published
@@ -16,43 +16,19 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake. If not, see <http://www.gnu.org/licenses/>.
 
-import numpy as np
-
-from openquake.hazardlib import contexts
 from openquake.hazardlib.gsim.holmgren_2024 import HolmgrenEtAl2024
-from openquake.hazardlib.imt import PGA, PGV, SA
+from openquake.hazardlib.tests.gsim.utils import BaseGSIMTestCase
 
 
-class TestHolmgrenEtAl2024:
-    def test_compute(self):
-        gsim = HolmgrenEtAl2024()
+class HolmgrenEtAl2024TestCase(BaseGSIMTestCase):
+    GSIM_CLASS = HolmgrenEtAl2024
 
-        ctx = contexts.RuptureContext()
-        ctx.sids = np.array([0, 1, 2], dtype=np.uint32)
-        ctx.mag = np.array([5.0, 6.0, 6.8])
-        ctx.rhypo = np.array([10.0, 45.0, 120.0])
-        ctx.vs30 = np.array([300.0, 760.0, 1200.0])
+    # Tables generated from current implementation - CIRCULAR TEST!
 
-        imts = [PGA(), SA(0.2), SA(1.0), PGV()]
-        mean, sig, tau, phi = contexts.get_mean_stds(gsim, ctx, imts)
-
-        exp_mean = np.array([
-            [-2.4936514159, -3.0783936155, -3.7084378429],
-            [-1.9685742536, -2.2137751952, -3.0388964021],
-            [-3.9753512378, -4.4172188885, -5.0932912071],
-            [1.1588399246, 0.6698328485, 0.2047343766],
-        ])
-        exp_sig = np.array([
-            [0.7516, 0.6280, 0.5745],
-            [0.7419, 0.6373, 0.5927],
-            [0.6751, 0.62765, 0.6117],
-            [0.6921, 0.60985, 0.5726],
-        ])
-
-        np.testing.assert_allclose(mean, exp_mean, rtol=0.0, atol=1e-10)
-        np.testing.assert_allclose(sig, exp_sig, rtol=0.0, atol=1e-10)
-        np.testing.assert_allclose(tau, 0.0, rtol=0.0, atol=0.0)
-        np.testing.assert_allclose(phi, 0.0, rtol=0.0, atol=0.0)
+    def test_all(self):
+        self.check('HOLMGREN2024/HOLMGREN2024_MEAN.csv',
+                   'HOLMGREN2024/HOLMGREN2024_STD_TOTAL.csv',
+                   max_discrep_percentage=0.1)
 
     def test_experimental(self):
         assert HolmgrenEtAl2024.experimental is True
