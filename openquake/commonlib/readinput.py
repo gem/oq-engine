d9934d7fab9b8d0e5e1dfd26d6f66d59d4786c7c
Matteo Nastasi <nastasi@openquake.org>
2026-03-10 11:56:19 +0100

    remote ini/zip status monitoring from info to warning

--- openquake/commonlib/readinput.py
+++ openquake/commonlib/readinput.py
@@ -290,8 +290,8 @@ def get_params(job_ini, kw={}):
         job_ini = str(job_ini)
     if job_ini.startswith(('http://', 'https://')):
         resp = requests.get(job_ini)
-        logging.info('Remote ini status: %d content length: %d' %
-                     (resp.status_code, len(resp.content)))
+        logging.warning('Remote ini status: %d content length: %d' %
+                        (resp.status_code, len(resp.content)))
 
         job_ini = gettemp(suffix='.zip')
         with open(job_ini, 'wb') as f:
