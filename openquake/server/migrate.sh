4cfbec21f8b2d61c76359b0a6d551d933c317afd
Michele Simionato <michele.simionato@globalquakemodel.org>
2026-03-20 06:22:57 +0100

    Tried without collectstatic

--- openquake/server/migrate.sh
+++ openquake/server/migrate.sh
@@ -5,4 +5,4 @@ done
 
 $1/manage.py migrate
 $1/manage.py loaddata openquake/server/fixtures/0001_cookie_consent_required_plus_hide_cookie_bar.json
-$1/manage.py collectstatic --noinput
+#$1/manage.py collectstatic --noinput
