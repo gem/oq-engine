6749fb2f4e4307e3d582b384af54a3128c87da12
Michele Simionato <michele.simionato@globalquakemodel.org>
2026-03-31 15:02:21 +0200

    Fixed debug_rupture

--- openquake/calculators/postproc/debug_rupture.py
+++ openquake/calculators/postproc/debug_rupture.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake.  If not, see <http://www.gnu.org/licenses/>.
 import os
-import numpy
 import pandas
 from openquake.baselib import sap
 from openquake.commonlib import datastore
@@ -25,13 +24,6 @@ from openquake.calculators.event_based import (
 from openquake.calculators.views import text_table
 
 
-def check_eids(frups, rupids, events):
-    rups = frups[numpy.isin(frups['id'], rupids)]
-    for rup in rups:
-        [ev] = events[events['id'] == rup['e0']]
-        assert ev['rup_id'] == rup['id'], (ev['rup_id'], rup['id'])
-    
-
 def main(calc_id: int, rup_id: int):
     """
     An utility to debug event based calculations
@@ -39,11 +31,10 @@ def main(calc_id: int, rup_id: int):
     """
     dstore = datastore.read(calc_id)  # read the original calculation
     oq = dstore['oqparam']
-    frups = dstore['filtered_ruptures'][:]
-    df = dstore.read_df('gmf_data', 'sid')
-    eids = df.eid.unique()
-    # rupids = dstore['gmf_data/rup_info']['rup_id']
-    # check_eids(frups, rupids, dstore['events'][:])
+    try:
+        frups = dstore['filtered_ruptures'][:]
+    except KeyError:
+        frups = dstore['ruptures'][:]
     
     rups = frups[frups['id'] == rup_id]
     print('model =', rups[0]['model'].decode('ascii'))
@@ -55,11 +46,7 @@ def main(calc_id: int, rup_id: int):
         dfs.append(pandas.DataFrame(res['gmfdata']))
     gmf_df = pandas.concat(dfs)
     print(text_table(gmf_df, ext='org'))
-    evs = dstore['events'][eids]
-    print(text_table(evs, ext='org'))
 
-    df = df[numpy.isin(df.eid, eids)]
-    print(text_table(df, ext='org'))
 
 if __name__ == '__main__':
     sap.run(main)
