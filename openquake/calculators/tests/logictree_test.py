fe1dc89045713e63a276c3d597e8142f7d875339
Michele Simionato <michele.simionato@globalquakemodel.org>
2026-05-26 04:55:53 +0200

    Improved test

--- openquake/calculators/tests/logictree_test.py
+++ openquake/calculators/tests/logictree_test.py
@@ -765,6 +765,7 @@ hazard_uhs-std.csv
         self.assertEqualFiles(fname_em, fname_ex)
 
     def test_case_83_eb(self):
+        # event based sampling with double extendModel
         self.run_calc(case_83.__file__, 'job_eb.ini')
         
         [f] = export(('ruptures', 'csv'), self.calc.datastore)
@@ -772,6 +773,13 @@ hazard_uhs-std.csv
         
         [fname] = export(('avg_gmf', 'csv'), self.calc.datastore)
         self.assertEqualFiles('expected/avg_gmf.csv', fname)
+
+        df = view('event_based_mfd', self.calc.datastore)
+        self.assertEqual(str(df), '''\
+     freq  occ_rate
+mag                
+4.0  2.83      2.75
+4.5  1.63      1.64''')
 
     def test_case_84(self):
         # test maxMagGRRelativeNoMoBalance
