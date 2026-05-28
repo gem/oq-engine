a8c30c7dcffc5d6b60418d930e141f511aff1a18
unknown <christopher.brooks@globalquakemodel.org>
2026-05-28 15:47:33 +0200

    eb test

--- openquake/calculators/tests/logictree_test.py
+++ openquake/calculators/tests/logictree_test.py
@@ -816,3 +816,13 @@ mag
         self.run_calc(case_34.__file__, 'job_xml_disagg.ini')
         [f] = export(('mean_disagg_by_src', 'csv'), self.calc.datastore)
         self.assertEqualFiles('expected/mean_disagg_by_src.csv', f)
+
+        # event_based with RuntimeSourceModelLT (sampling) - no XML-based
+        # SSC LT comparison because source_id embeds the branch name, making
+        # crc32(source_id, ses_seed) in source/base.py differ between approaches
+        # and producing different ruptures
+        self.run_calc(case_34.__file__, 'job_runtime_eb_sampling.ini')
+        [f] = export(('ruptures', 'csv'), self.calc.datastore)
+        self.assertEqualFiles('expected/ruptures_eb_sampling.csv', f)
+        [f, _, _] = export(('gmf_data', 'csv'), self.calc.datastore)
+        self.assertEqualFiles('expected/gmf_data_eb_sampling.csv', f)
