69ddbae2efdca21d5830d590ea7240fd1f98d7df
Michele Simionato <michele.simionato@globalquakemodel.org>
2026-05-31 13:48:35 +0200

    Saving memory in classical calculations

--- openquake/hazardlib/contexts.py
+++ openquake/hazardlib/contexts.py
@@ -1066,8 +1066,8 @@ class ContextMaker(object):
         ctxs = self.gen_contexts(rups_sites, src_id)
         with self.ctx_mon:
             if len(rups_sites) == 1 and not self.minimum_distance:
-                return list(ctxs)
-            return [self.recarray([c]) for c in ctxs]
+                return ctxs
+            return (self.recarray([c]) for c in ctxs)
 
     def max_intensity(self, sitecol1, mags, dists):
         """
