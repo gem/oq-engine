cd312ebf8004185edb654f5226b7de9174d1fc73
Michele Simionato <michele.simionato@globalquakemodel.org>
2026-07-04 03:38:26 +0200

    Fixed genconcat

--- openquake/baselib/general.py
+++ openquake/baselib/general.py
@@ -319,18 +319,23 @@ def block_splitter(items, max_weight, weight=lambda item: 1, key=nokey,
         yield ws
 
 
-def genconcat(dframes, size=512*1024**2):
+def genconcat(dframes, size_mb=512):
     """
     :paran dframes: iterator over dataframes
     Yield concatenated dataframes of size < 512 MB
     """
     dfs = []
-    nbytes = 0
+    size = 0
     for df in dframes:
-        nbytes += df.memory_usage().sum()
-        if nbytes > size:
-            yield pandas.concat(dfs)
-            nbytes = 0
+        mb = df.memory_usage().sum() / 1024**2
+        size += mb
+        if size > size_mb:
+            if dfs:
+                yield pandas.concat(dfs)
+            else:
+                print(f'There is a large gmf_df of {mb:.0f} MB')
+                yield df
+            size = 0
             dfs = []
         else:
             dfs.append(df)
