cddee88f9d16118a2e17311b83bf0e9133f9d318
Michele Simionato <michele.simionato@globalquakemodel.org>
2026-07-13 03:30:29 +0200

    Added a docstring [ci skip]

--- openquake/commonlib/logs.py
+++ openquake/commonlib/logs.py
@@ -86,6 +86,9 @@ def dbcmd(action, *args):
 
 
 def get_job_info(job_id):
+    """
+    :retuns: dictionary field -> value extract from the job record
+    """
     job = dbcmd('get_job', int(job_id))
     job_info = {key: val for key, val in zip(job.__dict__['_fields'],
                                              job.__dict__['_values'])}
