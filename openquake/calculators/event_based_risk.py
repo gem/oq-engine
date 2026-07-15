2fa6c0b32137e5e70d1fd99f0aaeb78674353dc9
Michele Simionato <michele.simionato@globalquakemodel.org>
2026-07-15 03:41:38 +0200

    Debug print

--- openquake/calculators/event_based_risk.py
+++ openquake/calculators/event_based_risk.py
@@ -331,6 +331,7 @@ def ebrisk(allrups, cmakers, sids, secperils, hdf5path, monitor):
         allrups, cmakers, sids, secperils, hdf5path, monitor)
            if len(dic['gmfdata']))
     blks = list(general.block_splitter(dfs, 1E7, len))
+    print(f'{monitor.task_no=} {len(blks)=}')
     last = len(blks) - 1
     for b, blk in enumerate(blks):
         # NB: it is essential to concatenate the small dataframes to have
