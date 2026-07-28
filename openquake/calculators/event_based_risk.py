d1ea997e57d80aaf8c1ddb2240ebe57a4f069a83
Michele Simionato <michele.simionato@gmail.com>
2026-07-28 21:16:39 +0200

    Fix

--- openquake/calculators/event_based_risk.py
+++ openquake/calculators/event_based_risk.py
@@ -342,7 +342,7 @@ def ebrisk(allrups, cmakers, sids, secperils, dstore, monitor):
         # long arrays (around GMF_MB) and hence a good performance
         mb = round(sum(size_mb(df) for df in blk))
         na = numpy.round([num_assets[df.sid].sum() for df in blk])
-        aff = sum(na)
+        aff = int(na.sum())
         # print(f'{monitor.task_no=}, {na/1E6=}')
         if b == 0 or aff < AE_MIN:  # don't spawn small tasks
             yield event_based_risk(pandas.concat(blk), monitor)
