386794bd0791699cbb9f18b52485b3b41716e7ed
Amir Banimahd <125797511+banimahd@users.noreply.github.com>
2026-09-18 01:15:54 +0330

    Update banimahd_turkiye_2026.py

--- openquake/hazardlib/gsim/banimahd_turkiye_2026.py
+++ openquake/hazardlib/gsim/banimahd_turkiye_2026.py
@@ -105,6 +105,7 @@ class Banimahd2026Turkiye(GMPE):
 
     _PERIODS = [0.03, 0.05, 0.075, 0.1, 0.15, 0.2, 0.25, 0.3, 0.4,
                 0.5, 0.75, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0]
+    
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         with gzip.open(_ONNX_FILE, "rb") as f:
