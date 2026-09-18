e2a2fbbb72952b0b29ad12e67c05bf14c802f5c5
unknown <christopher.brooks@globalquakemodel.org>
2026-09-18 15:31:49 +0200

    std dev tets

--- openquake/hazardlib/tests/gsim/novakovic_2018_test.py
+++ openquake/hazardlib/tests/gsim/novakovic_2018_test.py
@@ -21,10 +21,17 @@ from openquake.hazardlib.gsim.novakovic_2018 import (
 from openquake.hazardlib.tests.gsim.utils import BaseGSIMTestCase
 
 # Verification tables created using the .xls spreadsheet provided as an
-# elctronic supplement to the BSSA paper
+# electronic supplement to the BSSA paper
+# NOTE: These tests use the "default" Oklahoma adjustment table, from
+# which the between-event and within-event sigma are taken (and combined
+# to provide the total sigma)
 
 class NovakovicEtAl2018Test(BaseGSIMTestCase):
     GSIM_CLASS = NovakovicEtAl2018
 
-    def test_mean(self):
-        self.check('NEA18/NEA18_MEAN.csv', max_discrep_percentage=0.01)
+    def test_all(self):
+        self.check('NEA18/NEA18_MEAN.csv',
+                   'NEA18/NEA18_TOTAL_STDDEV.csv',
+                   'NEA18/NEA18_INTER_EVENT_STDDEV.csv',
+                   'NEA18/NEA18_INTRA_EVENT_STDDEV.csv',
+                   max_discrep_percentage=0.01)
