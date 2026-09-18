61c1a6386d1a3987b98570e2ff210343c5c18b58
Paolo Tormene <paolo.tormene@globalquakemodel.org>
2026-09-18 10:10:18 +0200

    Change the script that extracts impact reports, so it opens both the PDF and PNG versions; add a test running the script and verifying the output file formats and signatures

--- bin/extract_impact_reports.py
+++ bin/extract_impact_reports.py
@@ -1,5 +1,6 @@
 import sys
 import subprocess
+import shutil
 from pathlib import Path
 from openquake.commonlib import datastore
 
@@ -15,15 +16,19 @@ def main(calc_id: int):
     print(f"Found ISO3 reports: {iso3_codes}")
     for iso3 in iso3_codes:
         country_group = impact_group[iso3]
-        if "report_pdf" not in country_group:
-            print(f"No report_pdf found for {iso3}")
-            continue
-        pdf_bytes = country_group["report_pdf"][()]
-        fname = Path(f"impact_report_{calc_id}_{iso3}.pdf")
-        with open(fname, "wb") as f:
-            f.write(pdf_bytes)
-        print(f"Wrote {fname}")
-        subprocess.Popen(["xdg-open", str(fname)])
+        for file_format in ("pdf", "png"):
+            report_name = f"report_{file_format}"
+            if report_name not in country_group:
+                print(f"No {report_name} found for {iso3}")
+                continue
+            report_bytes = country_group[report_name][()]
+            fname = Path(
+                f"impact_report_{calc_id}_{iso3}.{file_format}")
+            with open(fname, "wb") as f:
+                f.write(report_bytes)
+            print(f"Wrote {fname}")
+            if shutil.which("xdg-open"):
+                subprocess.Popen(["xdg-open", str(fname)])
     dstore.close()
 
 
