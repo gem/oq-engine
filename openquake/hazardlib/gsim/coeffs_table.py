f0e62c375bcbe44e65520b066f96c4a0dce80b7e
unknown <christopher.brooks@globalquakemodel.org>
2026-09-22 09:18:17 +0200

    formatting

--- openquake/hazardlib/gsim/coeffs_table.py
+++ openquake/hazardlib/gsim/coeffs_table.py
@@ -126,7 +126,8 @@ class CoeffsTable(object):
     >>> ct[imt.SA(period=0.005, damping=5)]
     Traceback (most recent call last):
         ...
-    ValueError: Cannot interpolate SA(0.005): PGA-anchored fallback cannot extrapolate below the PGA anchor at 0.01 s
+    ValueError: Cannot interpolate SA(0.005): PGA-anchored fallback cannot
+    extrapolate below the PGA anchor at 0.01 s
 
     It is also possible to instantiate a table from a tuple of dictionaries,
     corresponding to the SA coefficients and non-SA coefficients:
