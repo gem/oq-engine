3c599180d0f3949c039646f821937628a9bc2afb
Michele Simionato <michele.simionato@gmail.com>
2026-09-24 05:57:31 +0200

    Fixed Sphinx doc build warnings
    - conf.py: set DJANGO_SETTINGS_MODULE and call django.setup() so
      autodoc can import the openquake.server modules (removed 4
      ImproperlyConfigured autodoc warnings)
    - conf.py: make the branch-regex a raw string (removed SyntaxWarning)
    - api-reference: fix stray blank line in the taherian_2024_offshore
      section title underline (removed 1 RST CRITICAL)

--- doc/conf.py
+++ doc/conf.py
@@ -13,6 +13,10 @@ sys.path.insert(0, os.path.abspath('..'))
 
 from openquake import engine
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openquake.server.settings")
+import django
+django.setup()
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -32,7 +36,7 @@ try:
         it_is_master = True
 
     # vcs_branch = 'engine-3.15'
-    if re.compile('engine-[0-9]+\.[0-9]+.*').match(vcs_branch):
+    if re.compile(r'engine-[0-9]+\.[0-9]+.*').match(vcs_branch):
         branch = ''
     else:
         if vcs_branch == '':
