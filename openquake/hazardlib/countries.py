f27eb00dcfff5f186517b71d7edc1c90aace3546
Michele Simionato <michele.simionato@gmail.com>
2026-09-25 11:02:19 +0200

    Reflected the change of name JPN->JPA

--- openquake/hazardlib/countries.py
+++ openquake/hazardlib/countries.py
@@ -3,9 +3,9 @@ from openquake.baselib import general
 
 # these are in one-to-one correspondence with the file mosaic.gpkg
 MODELS = sorted('''
-ALS ARB AUS CND CCA CEA CHN EUR GLD HAW IDN IND JPN KOR MEX MIE NAF NEA NWA
+ALS ARB AUS CND CCA CEA CHN EUR GLD HAW IDN IND JPA KOR MEX MIE NAF NEA NWA
 NZL OAT OIN OPA PAC PHL PNG SAM SEA SSA TEM TUR USA WAF ZAF PAN PAR'''.split())
-ALIASES = dict(TWN='TEM')  # manage change of name
+ALIASES = dict(TWN='TEM', JPN='JPA')  # old->new
 
 REGIONS = """\
 Africa
